0e9f2d8f6a48e0d9c0b43fdc725e5a3eac19aa02
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-10 14:09:22 +0300

    Added description

---
+++ Python_Solutions/CodeWars/3kyu/Prime_counting.py
@@ -0,0 +1,10 @@
+# You need to count the number of prime numbers less than or equal to some natural n.
+#
+# For example:
+# count_primes_less_than(34) = 11
+# count_primes_less_than(69) = 19
+# count_primes_less_than(420) = 81
+# count_primes_less_than(666) = 121
+# In this kata all the tests will be with 1 ⩽ � ⩽ 1 0 10 1⩽n⩽10 10
+# Code length limited to 3000 characters to avoid hardcoding.
+# Good luck!
