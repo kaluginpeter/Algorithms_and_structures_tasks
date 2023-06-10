b8c387541d6901b9652286c5085195c31b17fcad
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-10 14:03:07 +0300

    Added solution

--- Python_Solutions/CodeWars/8kyu/Quadrants.py
+++ Python_Solutions/CodeWars/8kyu/Quadrants.py
@@ -18,3 +18,6 @@
 # Task Series
 # Quadrants (this kata)
 # Quadrants 2: Segments
+# Solution
+def quadrant(x, y):
+    return 1 if x > 0 and y > 0 else 2 if x < 0 and y > 0 else 3 if x < 0 and y < 0 else 4
