7ec542eda204835749c61d42bb5cb7407527054f
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-11 19:16:12 +0300

    Added solution "1. Two Sum"

--- Python_Solutions/LeetCode/Easy/1._Two_Sum.py
+++ Python_Solutions/LeetCode/Easy/1._Two_Sum.py
@@ -24,3 +24,10 @@
 # Only one valid answer exists.
 #
 # Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+# Solution
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, j in enumerate(nums):
+            if target - j in d: return d[target-j], i
+            d[j] = i
