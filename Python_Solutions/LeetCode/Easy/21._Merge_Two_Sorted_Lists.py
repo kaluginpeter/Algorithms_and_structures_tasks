9ee13dc26c9de41bc54860abe63419970543a8a5
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-11 19:24:33 +0300

    Added solution "21. Merge Two Sorted Lists"

--- Python_Solutions/LeetCode/Easy/21._Merge_Two_Sorted_Lists.py
+++ Python_Solutions/LeetCode/Easy/21._Merge_Two_Sorted_Lists.py
@@ -20,3 +20,23 @@
 # The number of nodes in both lists is in the range [0, 50].
 # -100 <= Node.val <= 100
 # Both list1 and list2 are sorted in non-decreasing order.
+# Solution
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        if l1 == None and l2 == None:
+            return None
+        if l1 == None:
+            return l2
+        if l2 == None:
+            return l1
+        if l1.val <= l2.val:
+            l1.next = self.mergeTwoLists(l1.next,l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1,l2.next)
+            return l2
