69e389804d20a01e70d1d3e40b215df8e097d9ef
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-11 19:40:08 +0300

    Added solution "70. Climbing Stairs"

--- Python_Solutions/LeetCode/Easy/70._Climbing_Stairs.py
+++ Python_Solutions/LeetCode/Easy/70._Climbing_Stairs.py
@@ -19,3 +19,14 @@
 #
 # Constraints:
 # 1 <= n <= 45
+# Solution
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1: return 1
+        f = [None for _ in range(2)]
+        f[0] = 1
+        f[1] = 2
+        for _ in range(2, n):
+            new = f[0] + f[1]
+            f[0], f[1] = f[1], new
+        return f[1]
