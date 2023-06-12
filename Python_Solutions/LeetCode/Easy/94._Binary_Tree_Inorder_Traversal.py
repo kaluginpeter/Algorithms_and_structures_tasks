20b980e4dd247eaee2b13483b0a56481124aa64c
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-12 14:32:35 +0300

    Added solution "94. Binary Tree Inorder Traversal"

--- Python_Solutions/LeetCode/Easy/94._Binary_Tree_Inorder_Traversal.py
+++ Python_Solutions/LeetCode/Easy/94._Binary_Tree_Inorder_Traversal.py
@@ -20,3 +20,27 @@
 # -100 <= Node.val <= 100
 #
 # Follow up: Recursive solution is trivial, could you do it iteratively?
+# Solution
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root):
+        if root is None:
+            return []
+        res = []
+        stack = [root]
+        while len(stack) > 0:
+            curr = stack.pop()
+            if not isinstance(curr, TreeNode):
+                res.append(curr)
+                continue
+            if curr.right is not None:
+                stack.append(curr.right)
+            stack.append(curr.val)
+            if curr.left is not None:
+                stack.append(curr.left)
+        return res
