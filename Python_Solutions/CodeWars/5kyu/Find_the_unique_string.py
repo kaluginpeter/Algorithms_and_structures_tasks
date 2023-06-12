508e88051f747364c611ad0eddaf0bbb68493b8a
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-12 09:14:10 +0300

    Added description "Find the unique string"

---
+++ Python_Solutions/CodeWars/5kyu/Find_the_unique_string.py
@@ -0,0 +1,14 @@
+# There is an array of strings. All strings contains similar letters except one. Try to find it!
+#
+# find_uniq([ 'Aa', 'aaa', 'aaaaa', 'BbBb', 'Aaaa', 'AaAaAa', 'a' ]) # => 'BbBb'
+# find_uniq([ 'abc', 'acb', 'bac', 'foo', 'bca', 'cab', 'cba' ]) # => 'foo'
+# Strings may contain spaces. Spaces are not significant, only non-spaces symbols matters.
+# E.g. string that contains only spaces is like empty string.
+#
+# It’s guaranteed that array contains more than 2 strings.
+#
+# This is the second kata in series:
+#
+# Find the unique number
+# Find the unique string (this kata)
+# Find The Unique
