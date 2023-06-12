01aaa933d453780897b8241c7c5535abbcd4fc7b
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-12 08:52:18 +0300

    Added solution "Pete, the baker"

--- Python_Solutions/CodeWars/5kyu/Pete,_the_baker.py
+++ Python_Solutions/CodeWars/5kyu/Pete,_the_baker.py
@@ -14,3 +14,6 @@
 # cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200})
 # # must return 0
 # cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000})
+# Solution
+def cakes(recipe, available):
+    return min([available[i] // recipe[i] if i in available else 0 for i in recipe])
