17b34e2783a8f8e26433c87f8889af4548316c22
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-12 08:54:44 +0300

    Added solution "Mean Square Error"

--- Python_Solutions/CodeWars/5kyu/Mean_Square_Error.py
+++ Python_Solutions/CodeWars/5kyu/Mean_Square_Error.py
@@ -8,3 +8,6 @@
 # [1, 2, 3], [4, 5, 6]              -->   9   because (9 + 9 + 9) / 3
 # [10, 20, 10, 2], [10, 25, 5, -2]  -->  16.5 because (0 + 25 + 25 + 16) / 4
 # [-1, 0], [0, -1]                  -->   1   because (1 + 1) / 2
+# Solution
+def solution(a, b):
+    return sum((k - v)**2 for k, v in zip(a, b)) / len(a)
