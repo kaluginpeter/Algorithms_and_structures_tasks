e9da0186ed2296ccecdd61e293116c11a9c6f53a
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-12 08:47:55 +0300

    Added solution "Simple Pig Latin"

--- Python_Solutions/CodeWars/5kyu/Simple_Pig_Latin.py
+++ Python_Solutions/CodeWars/5kyu/Simple_Pig_Latin.py
@@ -4,3 +4,7 @@
 # Examples
 # pig_it('Pig latin is cool') # igPay atinlay siay oolcay
 # pig_it('Hello world !')     # elloHay orldway !
+# Solution
+def pig_it(text):
+    w = ' '.join(i[1:] + i[0] + 'ay' for i in text.split())
+    return w if w[-3] not in '!.,?' else w[:-2]
