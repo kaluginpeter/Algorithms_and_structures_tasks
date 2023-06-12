ec6ae75cf301dd02cbe9de837bdfd8275cf8bbe1
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-12 08:49:26 +0300

    Added solution "Moving Zeros To The End"

--- Python_Solutions/CodeWars/5kyu/Moving_Zeros_To_The_End.py
+++ Python_Solutions/CodeWars/5kyu/Moving_Zeros_To_The_End.py
@@ -2,3 +2,6 @@
 # to the end, preserving the order of the other elements.
 #
 # move_zeros([1, 0, 1, 2, 0, 1, 3]) # returns [1, 1, 2, 1, 3, 0, 0]
+# Solution
+def move_zeros(lst):
+    return [i for i in lst if i != 0] + [i for i in lst if i == 0]
