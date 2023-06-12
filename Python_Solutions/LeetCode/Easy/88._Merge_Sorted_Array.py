62149b490550b809228355111759da541301737e
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-12 14:30:54 +0300

    Added solution "88. Merge Sorted Array"

--- Python_Solutions/LeetCode/Easy/88._Merge_Sorted_Array.py
+++ Python_Solutions/LeetCode/Easy/88._Merge_Sorted_Array.py
@@ -36,3 +36,15 @@
 # -109 <= nums1[i], nums2[j] <= 109
 #
 # Follow up: Can you come up with an algorithm that runs in O(m + n) time?
+# Solution
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        while m > 0 and n > 0:
+            if nums1[m - 1] >= nums2[n - 1]:
+                nums1[m + n - 1] = nums1[m - 1]
+                m -= 1
+            else:
+                nums1[m + n - 1] = nums2[n - 1]
+                n -= 1
+        if n > 0:
+            nums1[:n] = nums2[:n]
