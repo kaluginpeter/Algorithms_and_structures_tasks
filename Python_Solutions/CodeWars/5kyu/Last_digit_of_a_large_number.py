3508f87b829e9160764c15fb41ac12fc306caf70
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-12 08:21:57 +0300

    Added description "Last digit of a large number"

---
+++ Python_Solutions/CodeWars/5kyu/Last_digit_of_a_large_number.py
@@ -0,0 +1,13 @@
+# Define a function that takes in two non-negative integers
+# b and returns the last decimal digit of
+# You may assume that the input will always be valid.
+# Examples
+# last_digit(4, 1)                # returns 4
+# last_digit(4, 2)                # returns 6
+# last_digit(9, 7)                # returns 9
+# last_digit(10, 10 ** 10)        # returns 0
+# last_digit(2 ** 200, 2 ** 300)  # returns 6
+# Remarks
+# JavaScript, C++, R, PureScript, COBOL
+# Since these languages don't have native arbitrarily large integers,
+# your arguments are going to be strings representing non-negative integers instead.
