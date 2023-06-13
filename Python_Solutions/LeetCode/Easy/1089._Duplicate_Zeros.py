11e30d94213bb06a7810fbfa51d869ee7eb208ea
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-13 09:06:19 +0300

    Added description "1160. Find Words That Can Be Formed by Characters"

--- Python_Solutions/LeetCode/Easy/1089._Duplicate_Zeros.py
+++ Python_Solutions/LeetCode/Easy/1089._Duplicate_Zeros.py
@@ -1,4 +1,4 @@
-1089. Duplicate Zeros# Given a fixed-length integer array arr, duplicate each occurrence of zero,
+# Given a fixed-length integer array arr, duplicate each occurrence of zero,
 # shifting the remaiing elements to the right.
 # Note that elements beyond the length of the original array are not written.
 # Do the above modifications to the input array in place and do not return anything.
