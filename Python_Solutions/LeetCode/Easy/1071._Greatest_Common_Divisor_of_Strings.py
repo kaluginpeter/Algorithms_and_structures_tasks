064006fd9d405f9042ad1a09b4236d716118ea7d
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-13 09:02:16 +0300

    Added solution "1071. Greatest Common Divisor of Strings"

--- Python_Solutions/LeetCode/Easy/1071._Greatest_Common_Divisor_of_Strings.py
+++ Python_Solutions/LeetCode/Easy/1071._Greatest_Common_Divisor_of_Strings.py
@@ -18,3 +18,11 @@
 # Constraints:
 # 1 <= str1.length, str2.length <= 1000
 # str1 and str2 consist of English uppercase letters.
+# Solution
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+                 l1, l2 =len(str1),len(str2)
+                 if l1==0 :return str2
+                 if l1<l2 :return self.gcdOfStrings(str2,str1)
+                 if str1[0:l2] !=str2: return ""
+                 return self.gcdOfStrings(str1[l2:],str2)
