3f1a80758c32183131d6ae0b31d42aa962439a73
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-13 08:52:20 +0300

    Added solution "783. Minimum Distance Between BST Nodes"

--- Python_Solutions/LeetCode/Easy/783._Minimum_Distance_Between_BST_Nodes.py
+++ Python_Solutions/LeetCode/Easy/783._Minimum_Distance_Between_BST_Nodes.py
@@ -14,3 +14,22 @@
 # The number of nodes in the tree is in the range [2, 100].
 # 0 <= Node.val <= 105
 # Note: This question is the same as 530: https://leetcode.com/problems/minimum-absolute-difference-in-bst/
+# Solution
+class Solution(object):
+    def minDiffInBST(self, root):
+        tmp = []
+        if root.left:
+            curr = root.left
+            while curr.right:
+                curr = curr.right
+            tmp.append(root.val - curr.val)
+            if root.left.left or root.left.right:
+                tmp.append(self.minDiffInBST(root.left))
+        if root.right:
+            curr = root.right
+            while curr.left:
+                curr = curr.left
+            tmp.append(curr.val - root.val)
+            if root.right.left or root.right.right:
+                tmp.append(self.minDiffInBST(root.right))
+        return min(tmp)
