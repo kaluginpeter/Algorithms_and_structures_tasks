0d5fb80bc7eaced92107b953ee043192db3739bd
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-13 08:58:04 +0300

    Added solution "977. Squares of a Sorted Array"

--- Python_Solutions/LeetCode/Easy/977._Squares_of_a_Sorted_Array.py
+++ Python_Solutions/LeetCode/Easy/977._Squares_of_a_Sorted_Array.py
@@ -18,3 +18,7 @@
 # nums is sorted in non-decreasing order.
 # Follow up: Squaring each element and sorting the new array is very trivial,
 # could you find an O(n) solution using a different approach?
+# Solution
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        return sorted(i**2 for i in nums)
