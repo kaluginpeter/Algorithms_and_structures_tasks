971eda44dfd4729bd6ed73c34dd3f9a017b26a81
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-13 09:18:13 +0300

    Added solution "1337. The K Weakest Rows in a Matrix"

--- Python_Solutions/LeetCode/Easy/1337._The_K_Weakest_Rows_in_a_Matrix.py
+++ Python_Solutions/LeetCode/Easy/1337._The_K_Weakest_Rows_in_a_Matrix.py
@@ -47,3 +47,15 @@
 # 2 <= n, m <= 100
 # 1 <= k <= m
 # matrix[i][j] is either 0 or 1.
+# Solution
+class Solution:
+    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+        y, x = len(mat), len(mat[0])
+        vis, ans = [0] * y, []
+        for j in range(x+1):
+            for i in range(y):
+                if not vis[i] and (j == x or not mat[i][j]):
+                    ans.append(i)
+                    vis[i] = 1
+                if len(ans) == k:
+                     return ans
