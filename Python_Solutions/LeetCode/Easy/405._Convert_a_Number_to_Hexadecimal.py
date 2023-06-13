07835527b23b04e49bffe298cfddaafb8fad1b45
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-13 08:19:04 +0300

    Added solution "405. Convert a Number to Hexadecimal"

--- Python_Solutions/LeetCode/Easy/405._Convert_a_Number_to_Hexadecimal.py
+++ Python_Solutions/LeetCode/Easy/405._Convert_a_Number_to_Hexadecimal.py
@@ -15,3 +15,15 @@
 #
 # Constraints:
 # -231 <= num <= 231 - 1
+# Solution
+class Solution:
+    def toHex(self, num: int) -> str:
+        if num == 0:
+            return '0'
+        chars = '0123456789abcdef'
+        s = []
+        for i in range(7, -1, -1):
+            x = (num >> (4 * i)) & 0xF
+            if s or x != 0:
+                s.append(chars[x])
+        return ''.join(s)
