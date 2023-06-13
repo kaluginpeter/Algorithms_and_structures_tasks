36a70a5425b3b29e95178d475d601253c73ca708
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-13 08:32:33 +0300

    Added solution "541. Reverse String II"

--- Python_Solutions/LeetCode/Easy/541._Reverse_String_II.py
+++ Python_Solutions/LeetCode/Easy/541._Reverse_String_II.py
@@ -17,3 +17,17 @@
 # 1 <= s.length <= 104
 # s consists of only lowercase English letters.
 # 1 <= k <= 104
+# Solution
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        size = len(s)
+        output_str = ""
+        i = 0
+        while size - i*2*k >= 2*k:
+            output_str += s[i*2*k : i*2*k+k][::-1] + s[i*2*k+k : (i+1)*2*k]
+            i += 1
+        if size - i*2*k < k:
+            output_str += s[i*2*k:][::-1]
+        else:
+            output_str += s[i*2*k:i*2*k+k][::-1] + s[i*2*k+k:]
+        return output_str
