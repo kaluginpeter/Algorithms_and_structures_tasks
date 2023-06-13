ad19e4047e32e6a50aa2409288fb1191b519cc5a
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-13 08:17:53 +0300

    Added solution "392. Is Subsequence"

--- Python_Solutions/LeetCode/Easy/392._Is_Subsequence.py
+++ Python_Solutions/LeetCode/Easy/392._Is_Subsequence.py
@@ -18,3 +18,12 @@
 # s and t consist only of lowercase English letters.
 # Follow up: Suppose there are lots of incoming s, say s1, s2, ..., sk where k >= 109,
 # and you want to check one by one to see if t has its subsequence. In this scenario, how would you change your code?
+# Solution
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        try:
+            for i in s:
+                t = t[t.index(i)+1:]
+            return True
+        except:
+            return False
