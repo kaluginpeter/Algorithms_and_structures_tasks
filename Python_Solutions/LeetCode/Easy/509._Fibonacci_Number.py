20d31ccb5153d59c084c74a11edfc36e253a72f2
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-13 08:30:05 +0300

    Added solution "509. Fibonacci Number"

--- Python_Solutions/LeetCode/Easy/509._Fibonacci_Number.py
+++ Python_Solutions/LeetCode/Easy/509._Fibonacci_Number.py
@@ -22,3 +22,10 @@
 #
 # Constraints:
 # 0 <= n <= 30
+# Solution
+class Solution:
+    def fib(self, n):
+        if n == 0 or n == 1:
+            return n
+        else:
+            return self.fib(n-1)+self.fib(n-2)
