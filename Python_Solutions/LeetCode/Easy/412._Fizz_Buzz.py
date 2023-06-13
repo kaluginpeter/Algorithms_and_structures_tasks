d113009937341dfe2ffcda1541c78f3f0c55a4bd
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-13 08:20:08 +0300

    Added solution "412. Fizz Buzz"

--- Python_Solutions/LeetCode/Easy/412._Fizz_Buzz.py
+++ Python_Solutions/LeetCode/Easy/412._Fizz_Buzz.py
@@ -19,3 +19,8 @@
 #
 # Constraints:
 # 1 <= n <= 104
+# Solution
+class Solution:
+    def fizzBuzz(self, n):
+        return [str(i) * (i % 3 != 0 and i % 5 != 0) + "Fizz" * (i % 3 == 0) + "Buzz" * (i % 5==0)
+                for i in range(1, n + 1)]
