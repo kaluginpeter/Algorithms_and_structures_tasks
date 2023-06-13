a598b8058e562f25b88f5d694182e2931a02e373
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-13 08:14:22 +0300

    Added solution "383. Ransom Note"

--- Python_Solutions/LeetCode/Easy/383._Ransom_Note.py
+++ Python_Solutions/LeetCode/Easy/383._Ransom_Note.py
@@ -18,3 +18,17 @@
 # Constraints:
 # 1 <= ransomNote.length, magazine.length <= 105
 # ransomNote and magazine consist of lowercase English letters.
+# Solution
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        cnt = [0] * 26
+
+        for ch in magazine:
+            cnt[ord(ch) - 97] += 1
+
+        for ch in ransomNote:
+            if cnt[ord(ch) - 97] == 0:
+                return False
+            cnt[ord(ch) - 97] -= 1
+
+        return True
