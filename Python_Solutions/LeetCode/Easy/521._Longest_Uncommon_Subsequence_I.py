933dcda9067810e28ffe4cad78cae9488d2bed85
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-13 08:31:25 +0300

    Added solution "521. Longest Uncommon Subsequence I"

--- Python_Solutions/LeetCode/Easy/521._Longest_Uncommon_Subsequence_I.py
+++ Python_Solutions/LeetCode/Easy/521._Longest_Uncommon_Subsequence_I.py
@@ -27,3 +27,7 @@
 # Constraints:
 # 1 <= a.length, b.length <= 100
 # a and b consist of lower-case English letters.
+# Solution
+class Solution:
+    def findLUSlength(self, a, b):
+        return max(len(a), len(b)) if a != b else -1
