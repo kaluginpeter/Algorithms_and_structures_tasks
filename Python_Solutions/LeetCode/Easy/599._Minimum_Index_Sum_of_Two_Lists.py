27fced5fe13b90d3b7e0ba6613c8350abd59d94e
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-13 08:35:01 +0300

    Added solution "599. Minimum Index Sum of Two Lists"

--- Python_Solutions/LeetCode/Easy/599._Minimum_Index_Sum_of_Two_Lists.py
+++ Python_Solutions/LeetCode/Easy/599._Minimum_Index_Sum_of_Two_Lists.py
@@ -32,3 +32,18 @@
 # All the strings of list1 are unique.
 # All the strings of list2 are unique.
 # There is at least a common string between list1 and list2.
+# Solution
+class Solution:
+    def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
+        ans = []
+        mp = {v: i for i, v in enumerate(list2)}
+        mi = 2000
+        for i, v in enumerate(list1):
+            if v in mp:
+                t = i + mp[v]
+                if t < mi:
+                    mi = t
+                    ans = [v]
+                elif t == mi:
+                    ans.append(v)
+        return ans
