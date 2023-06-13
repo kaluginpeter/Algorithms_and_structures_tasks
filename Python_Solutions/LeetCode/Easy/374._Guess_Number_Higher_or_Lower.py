5ddb9eb2d3ae00007ad396c0644944539b532edc
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-13 08:13:20 +0300

    Added solution "374. Guess Number Higher or Lower"

--- Python_Solutions/LeetCode/Easy/374._Guess_Number_Higher_or_Lower.py
+++ Python_Solutions/LeetCode/Easy/374._Guess_Number_Higher_or_Lower.py
@@ -23,3 +23,15 @@
 # Constraints:
 # 1 <= n <= 231 - 1
 # 1 <= pick <= n
+# Solution
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        low = 1
+        high = n
+        while low <= high:
+            mid = (low+high) // 2
+            if guess(mid) == 1:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return low
