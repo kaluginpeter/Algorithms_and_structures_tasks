f36fb906e8abbb20999d672d0dab4d260fcea729
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-13 08:54:44 +0300

    Added solution "876. Middle of the Linked List"

--- Python_Solutions/LeetCode/Easy/876._Middle_of_the_Linked_List.py
+++ Python_Solutions/LeetCode/Easy/876._Middle_of_the_Linked_List.py
@@ -15,3 +15,16 @@
 # Constraints:
 # The number of nodes in the list is in the range [1, 100].
 # 1 <= Node.val <= 100
+# Solution
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        fast = slow = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
