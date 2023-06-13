5a08231424ea30dff46567726f4ab095e9ba1486
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-13 08:04:29 +0300

    Added solution "303. Range Sum Query - Immutable"

--- Python_Solutions/LeetCode/Easy/303._Range_Sum_Query_-_Immutable.py
+++ Python_Solutions/LeetCode/Easy/303._Range_Sum_Query_-_Immutable.py
@@ -24,3 +24,9 @@
 # -105 <= nums[i] <= 105
 # 0 <= left <= right < nums.length
 # At most 104 calls will be made to sumRange.
+# Solution
+class NumArray:
+    def __init__(self, nums: List[int]):
+        self.s = list(accumulate(nums, initial=0))
+    def sumRange(self, left: int, right: int) -> int:
+        return self.s[right + 1] - self.s[left]
