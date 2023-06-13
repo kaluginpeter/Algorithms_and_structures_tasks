a6c49b36610a560f1cda517cea30e38e2796f67b
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-13 07:57:49 +0300

    Added solution "234. Palindrome Linked List"

--- Python_Solutions/LeetCode/Easy/234._Palindrome_Linked_List.py
+++ Python_Solutions/LeetCode/Easy/234._Palindrome_Linked_List.py
@@ -14,3 +14,37 @@
 # The number of nodes in the list is in the range [1, 105].
 # 0 <= Node.val <= 9
 # Follow up: Could you do it in O(n) time and O(1) space?
+# Solution
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        def get_mid_node(head):
+            slow = fast = head
+            while fast and fast.next:
+                slow = slow.next
+                fast = fast.next.next
+            return slow
+
+        def reverse_ll(head):
+            prev = None
+            while head is not None:
+                curr = head
+                head = head.next
+                curr.next = prev
+                prev = curr
+            return prev
+
+        def is_ll_palindrome(L1, L2):
+            while L1 and L2:
+                if L1.val != L2.val:
+                    return False
+                L1, L2 = L1.next, L2.next
+            return True
+
+        mid_node = get_mid_node(head)
+        L2 = reverse_ll(mid_node)
+        return is_ll_palindrome(head, L2)
