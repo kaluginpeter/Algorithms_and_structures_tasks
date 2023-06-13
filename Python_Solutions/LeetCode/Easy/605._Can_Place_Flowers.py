6096f47db771afa51da9d66da96cc1dbe0c922c6
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-13 08:36:03 +0300

    Added solution "605. Can Place Flowers"

--- Python_Solutions/LeetCode/Easy/605._Can_Place_Flowers.py
+++ Python_Solutions/LeetCode/Easy/605._Can_Place_Flowers.py
@@ -18,3 +18,10 @@
 # flowerbed[i] is 0 or 1.
 # There are no two adjacent flowers in flowerbed.
 # 0 <= n <= flowerbed.length
+class Solution(object):
+    def canPlaceFlowers(self, flowerbed, n):
+        for i in range(len(flowerbed)):
+            if flowerbed[i]==0 and (i==0 or flowerbed[i-1]==0) and (i==len(flowerbed)-1 or flowerbed[i+1]==0):
+                flowerbed[i]=1
+                n-=1
+        return n <= 0
