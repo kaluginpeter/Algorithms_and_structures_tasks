1370584b02e3d69330e3329be627a5134bff0ef4
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-14 17:40:56 +0300

    Added solution "1805. Number of Different Integers in a String"

--- Python_Solutions/LeetCode/Easy/1805._Number_of_Different_Integers_in_a_String.py
+++ Python_Solutions/LeetCode/Easy/1805._Number_of_Different_Integers_in_a_String.py
@@ -24,3 +24,19 @@
 # Constraints:
 # 1 <= word.length <= 1000
 # word consists of digits and lowercase English letters.
+# Solution
+class Solution:
+    def numDifferentIntegers(self, word: str) -> int:
+        s = set()
+        i, n = 0, len(word)
+        while i < n:
+            if word[i].isdigit():
+                while i < n and word[i] == '0':
+                    i += 1
+                j = i
+                while j < n and word[j].isdigit():
+                    j += 1
+                s.add(word[i:j])
+                i = j
+            i += 1
+        return len(s)
