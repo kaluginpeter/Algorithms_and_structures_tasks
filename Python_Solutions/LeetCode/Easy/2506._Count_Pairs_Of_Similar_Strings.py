387c533b6c67e8919aef9997d94895d4482f818a
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-14 18:30:50 +0300

    Added solution "2506. Count Pairs Of Similar Strings"

--- Python_Solutions/LeetCode/Easy/2506._Count_Pairs_Of_Similar_Strings.py
+++ Python_Solutions/LeetCode/Easy/2506._Count_Pairs_Of_Similar_Strings.py
@@ -30,3 +30,11 @@
 # 1 <= words.length <= 100
 # 1 <= words[i].length <= 100
 # words[i] consist of only lowercase English letters.
+# Solution
+class Solution:
+    def similarPairs(self, words: List[str]) -> int:
+        c = 0
+        for i in range(len(words)):
+            for j in range(i+1, len(words)):
+                if set(words[i]) == set(words[j]): c += 1
+        return c
