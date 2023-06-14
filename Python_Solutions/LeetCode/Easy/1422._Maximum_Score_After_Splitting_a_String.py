f356a0baddf83bdd49de2a0ec9a15af202028b97
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-14 07:28:51 +0300

    Added solution "1422. Maximum Score After Splitting a String"

--- Python_Solutions/LeetCode/Easy/1422._Maximum_Score_After_Splitting_a_String.py
+++ Python_Solutions/LeetCode/Easy/1422._Maximum_Score_After_Splitting_a_String.py
@@ -27,3 +27,7 @@
 # Constraints:
 # 2 <= s.length <= 500
 # The string s consists of characters '0' and '1' only.
+# Solution
+class Solution:
+    def maxScore(self, s: str) -> int:
+        return max(s[:i].count('0') + s[i:].count('1') for i in range(1, len(s)))
