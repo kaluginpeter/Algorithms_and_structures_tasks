21e97bb6f7f8dd4e7fd7388f91acc00cddb48581
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-14 07:03:18 +0300

    Added solution "504. Base 7"

--- Python_Solutions/LeetCode/Easy/504._Base_7.py
+++ Python_Solutions/LeetCode/Easy/504._Base_7.py
@@ -11,3 +11,13 @@
 #
 # Constraints:
 # -107 <= num <= 107
+# Solution
+class Solution:
+    def convertToBase7(self, num: int) -> str:
+        # import numpy
+        # return numpy.base_repr(num, 7)
+        n, resul = abs(num), ''
+        while n:
+            resul = str(n % 7) + resul
+            n //= 7
+        return '-' * (num < 0) + resul or '0'
