6a4334957e3f503d71d2f18718478c9d4bcf243a
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-14 08:02:46 +0300

    Added solution "1550. Three Consecutive Odds"

--- Python_Solutions/LeetCode/Easy/1550._Three_Consecutive_Odds.py
+++ Python_Solutions/LeetCode/Easy/1550._Three_Consecutive_Odds.py
@@ -15,3 +15,13 @@
 # Constraints:
 # 1 <= arr.length <= 1000
 # 1 <= arr[i] <= 1000
+# Solution
+class Solution:
+    def threeConsecutiveOdds(self, arr: List[int]) -> bool:
+        c = 0
+        for i in arr:
+            if i % 2 != 0:
+                c += 1
+                if c == 3: return True
+            else: c = 0
+        return False
