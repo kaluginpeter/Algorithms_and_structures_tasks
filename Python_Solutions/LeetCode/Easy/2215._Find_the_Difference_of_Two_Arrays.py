320cfbed9081ffe7b3f011ae6336a15e5fd35487
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-14 18:12:12 +0300

    Added solution "2215. Find the Difference of Two Arrays"

--- Python_Solutions/LeetCode/Easy/2215._Find_the_Difference_of_Two_Arrays.py
+++ Python_Solutions/LeetCode/Easy/2215._Find_the_Difference_of_Two_Arrays.py
@@ -24,3 +24,7 @@
 # Constraints:
 # 1 <= nums1.length, nums2.length <= 1000
 # -1000 <= nums1[i], nums2[i] <= 1000
+# Solution
+class Solution:
+    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        return [list(set([i for i in nums1 if i not in nums2])), list(set([i for i in nums2 if i not in nums1]))]
