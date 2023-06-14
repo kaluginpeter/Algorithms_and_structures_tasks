6d442afe7ee84984f7eff8a52941b981c7503703
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-14 18:42:15 +0300

    Added description "Prime Factors"

---
+++ Python_Solutions/CodeWars/6kyu/Prime_Factors.py
@@ -0,0 +1,13 @@
+# Prime Factors
+# Inspired by one of Uncle Bob's TDD Kata
+# Write a function that generates factors for a given number.
+# The function takes an integer as parameter and returns a list of integers
+# (ObjC: array of NSNumbers representing integers). That list contains the prime factors in numerical sequence.
+#
+# Examples
+# 1  ==>  []
+# 3  ==>  [3]
+# 8  ==>  [2, 2, 2]
+# 9  ==>  [3, 3]
+# 12 ==>  [2, 2, 3]
+# ALGORITHMS
