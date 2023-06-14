29a3f6862c3dbf5809a4c92de2fb930a37eafa5a
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-14 18:08:55 +0300

    Added solution "2180. Count Integers With Even Digit Sum"

--- Python_Solutions/LeetCode/Easy/2180._Count_Integers_With_Even_Digit_Sum.py
+++ Python_Solutions/LeetCode/Easy/2180._Count_Integers_With_Even_Digit_Sum.py
@@ -18,3 +18,11 @@
 #
 # Constraints:
 # 1 <= num <= 1000
+# Solution
+class Solution:
+    def countEven(self, num: int) -> int:
+        count = 0
+        for i in range(1, num+1):
+            if sum(int(j) for j in str(i)) % 2 == 0:
+                count += 1
+        return count
