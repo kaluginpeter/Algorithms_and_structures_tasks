3a730fd1b8fcc63aa294b591b0b0cbd138774c4e
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-14 07:33:32 +0300

    Added solution "1464. Maximum Product of Two Elements in an Array"

--- Python_Solutions/LeetCode/Easy/1464._Maximum_Product_of_Two_Elements_in_an_Array.py
+++ Python_Solutions/LeetCode/Easy/1464._Maximum_Product_of_Two_Elements_in_an_Array.py
@@ -20,3 +20,8 @@
 # Constraints:
 # 2 <= nums.length <= 500
 # 1 <= nums[i] <= 10^3
+# Solution
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        a, b = sorted(nums, reverse=True)[:2]
+        return (a - 1) * (b - 1)
