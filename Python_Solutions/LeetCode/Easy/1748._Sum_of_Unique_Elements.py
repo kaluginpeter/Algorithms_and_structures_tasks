dce6ccb4598dc8718dfc67c95a44a9a1b4f9c6cf
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-14 17:36:08 +0300

    Added solution "1748. Sum of Unique Elements"

--- Python_Solutions/LeetCode/Easy/1748._Sum_of_Unique_Elements.py
+++ Python_Solutions/LeetCode/Easy/1748._Sum_of_Unique_Elements.py
@@ -21,3 +21,7 @@
 # Constraints:
 # 1 <= nums.length <= 100
 # 1 <= nums[i] <= 100
+# Solution
+class Solution:
+    def sumOfUnique(self, nums: List[int]) -> int:
+        return sum(i for i in nums if nums.count(i) == 1)
