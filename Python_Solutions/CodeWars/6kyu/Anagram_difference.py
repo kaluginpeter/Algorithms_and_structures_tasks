442d0ebc04ec901419ae54d9f6a87e07b2a89106
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-14 21:17:46 +0300

    Added description "Anagram difference"

---
+++ Python_Solutions/CodeWars/6kyu/Anagram_difference.py
@@ -0,0 +1,11 @@
+# Given two words, how many letters do you have to remove from them to make them anagrams?
+# Example
+# First word : c od e w ar s (4 letters removed)
+# Second word : ha c k er r a nk (6 letters removed)
+# Result : 10
+# Hints
+# A word is an anagram of another word if they have the same letters (usually in a different order).
+# Do not worry about case. All inputs will be lowercase.
+# When you're done with this kata, check out its
+# big brother/sister : https://www.codewars.com/kata/hardcore-anagram-difference
+# STRINGSALGORITHMSFUNDAMENTALS
