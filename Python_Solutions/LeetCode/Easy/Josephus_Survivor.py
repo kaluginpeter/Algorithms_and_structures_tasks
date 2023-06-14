4ad7d8b5389723d9249b301926463726f0515b26
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-14 07:22:19 +0300

    Added solution "Josephus Survivor"

--- Python_Solutions/LeetCode/Easy/Josephus_Survivor.py
+++ Python_Solutions/LeetCode/Easy/Josephus_Survivor.py
@@ -17,3 +17,10 @@
 # Notes and tips: using the solution to the other kata to check your function may be helpful,
 # but as much larger numbers will be used, using an array/list to compute the number of the
 # survivor may be too slow; you may assume that both n and k will always be >=1.
+# Solution
+def josephus_survivor(n,k):
+    i, j, n = 0, [], list(range(1, n+1))
+    while len(n) > 0:
+        i = (i + k - 1) % len(n)
+        j = n.pop(i)
+    return j
