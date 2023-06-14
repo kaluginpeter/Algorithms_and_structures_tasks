dd6417d3472bdbc299298d39486202f35b8c7b8c
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-14 21:21:56 +0300

    Added description "Permute a Palindrome"

---
+++ Python_Solutions/CodeWars/6kyu/Permute_a_Palindrome.py
@@ -0,0 +1,18 @@
+# Write a function that will check whether ANY permutation of the characters
+# of the input string is a palindrome. Bonus points for a solution that is efficient
+# and/or that uses only built-in language functions. Deem yourself brilliant if you
+# can come up with a version that does not use any function whatsoever.
+#
+# For this kata assume that all characters are lowercase.
+#
+# Example
+# madam -> True
+# adamm -> True
+# junk -> False
+#
+# Hint
+# The brute force approach would be to generate all the permutations
+# of the string and check each one of them whether it is a palindrome.
+# However, an optimized approach will not require this at all.
+#
+# ALGORITHMSFUNDAMENTALS
