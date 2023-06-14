2aa766fa64f5c4c1a7793af3bc2ff756183e072b
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-14 21:22:17 +0300

    Added solution "Permute a Palindrome"

--- Python_Solutions/CodeWars/6kyu/Permute_a_Palindrome.py
+++ Python_Solutions/CodeWars/6kyu/Permute_a_Palindrome.py
@@ -16,3 +16,5 @@
 # However, an optimized approach will not require this at all.
 #
 # ALGORITHMSFUNDAMENTALS
+# Solution
+permute_a_palindrome=lambda s:bool(len(list(filter(lambda x:x%2!=0,[s.count(char) for char in set(s)])))<2)
