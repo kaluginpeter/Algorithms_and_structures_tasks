c5b169ae93681dff7c5cf337c621b0ae92772dc3
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-15 15:28:43 +0300

    Added solution "Clock in Mirror"

--- Python_Solutions/CodeWars/6kyu/Clock_in_Mirror.py
+++ Python_Solutions/CodeWars/6kyu/Clock_in_Mirror.py
@@ -48,3 +48,27 @@
 # There is no 13:20, instead it is 01:20.
 #
 # FUNDAMENTALS
+# Solution
+def what_is_the_time(time_in_mirror):
+    hour = int(time_in_mirror[0:2])
+    minute = int(time_in_mirror[3:5])
+    if hour < 11:
+        hour1 = 11 - hour
+    else:
+        hour1 = 23 - hour
+    minute1 = 60 - minute
+    if minute1 == 60:
+        minute1 -=60
+        hour1 += 1
+    if hour1 > 12:
+        hour1 -=12
+    ans = ""
+    if hour1 > 9 :
+        ans = str(hour1) + ':'
+    else:
+        ans = '0' + str(hour1) + ':'
+    if minute1 > 9:
+        ans += str(minute1)
+    else:
+        ans += '0' + str(minute1)
+    return ans
