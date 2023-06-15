b17f0a0112e6c5f552c87b5c5ebd6df261d85c05
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-15 15:53:50 +0300

    Added description "Complete Fibonacci Series"

---
+++ Python_Solutions/CodeWars/6kyu/Complete_Fibonacci_Series.py
@@ -0,0 +1,9 @@
+# The function 'fibonacci' should return an array of fibonacci numbers.
+# The function takes a number as an argument to decide how many no. of elements to produce.
+# If the argument is less than or equal to 0 then return empty array
+#
+# Example:
+#
+# fibonacci(4) # should return  [0,1,1,2]
+# fibonacci(-1) # should return []
+# ALGORITHMS
