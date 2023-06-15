9301754fb877dc2a9b86a2edc2b94efb94019035
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-15 15:30:22 +0300

    Added description "Closest Sum"

---
+++ Python_Solutions/CodeWars/6kyu/Closest_Sum.py
@@ -0,0 +1,11 @@
+# Given an array (ints) of n integers, find three integers
+# in arr such that the sum is closest to a given number (num), target.
+#
+# Return the sum of the three integers. You may assume that each input would have exactly one solution.
+#
+# Example:
+#
+# closest_sum([-1, 2, 1, -4], 1) # 2 (-1 + 2 + 1 = 2)
+# Note: your solution should not modify the input array.
+#
+# FUNDAMENTALS
