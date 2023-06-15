97b363a20b698955f054d5e5160c9eea4a9fb495
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-15 13:08:34 +0300

    Added description "Arabian String"

---
+++ Python_Solutions/CodeWars/6kyu/Arabian_String.py
@@ -0,0 +1,10 @@
+# You must create a method that can convert a string from any format into PascalCase. This must support symbols too.
+#
+# Don't presume the separators too much or you could be surprised.
+#
+# For example: (Input --> Output)
+#
+# "example name" --> "ExampleName"
+# "your-NaMe-here" --> "YourNameHere"
+# "testing ABC" --> "TestingAbc"
+# STRINGSALGORITHMS
