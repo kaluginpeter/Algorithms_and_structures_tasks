4949b9fd295bb048c5a11ce633d2975cf1ea9472
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-15 13:44:46 +0300

    Added description "Bowling Pins"

---
+++ Python_Solutions/CodeWars/6kyu/Bowling_Pins.py
@@ -0,0 +1,26 @@
+# Mount the Bowling Pins!
+# Task:
+# Did you ever play Bowling? Short: You have to throw a bowl into 10 Pins arranged like this:
+#
+#
+# I I I I  # each Pin has a Number:    7 8 9 10
+#  I I I                                4 5 6
+#   I I                                  2 3
+#    I                                    1
+#
+# You will get an Array with Numbers, e.g.: [3,5,9] and remove them from the field like this:
+#
+#
+# I I   I
+#  I   I
+#   I
+#    I
+#
+# Return a string with the current field.
+#
+# Note that:
+# You begin a new line with \n
+# Each Line must be 7 chars long
+# Fill the missing pins with a whitespace
+# Have fun!
+# ARRAYSSTRINGSFUNDAMENTALS
