e7167d9f4ffc2c8fbad0c2f2f9dbc9201f4e30a6
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-15 14:25:28 +0300

    Added description "Build Tower Advanced"

---
+++ Python_Solutions/CodeWars/6kyu/Build_Tower_Advanced.py
@@ -0,0 +1,41 @@
+# Build Tower Advanced
+# Build Tower by the following given arguments:
+#
+# number of floors (integer and always greater than 0)
+# block size (width, height) (integer pair and always greater than (0, 0))
+# Tower block unit is represented as *. Tower blocks of block size (2, 1) and (2, 3) would look like respectively:
+#
+#   **
+#   **
+#   **
+#   **
+# for example, a tower of 3 floors with block size = (2, 3) looks like below
+#
+# [
+#   '    **    ',
+#   '    **    ',
+#   '    **    ',
+#   '  ******  ',
+#   '  ******  ',
+#   '  ******  ',
+#   '**********',
+#   '**********',
+#   '**********'
+# ]
+# and a tower of 6 floors with block size = (2, 1) looks like below
+#
+# [
+#   '          **          ',
+#   '        ******        ',
+#   '      **********      ',
+#   '    **************    ',
+#   '  ******************  ',
+#   '**********************'
+# ]
+# Don't return a whole string containing line-endings but a list/array/vector of strings instead!
+#
+# This kata might looks like a 5.5kyu one. So challenge yourself!
+#
+# Go take a look at Build Tower which is a more basic version :)
+#
+# STRINGSASCII ARTFUNDAMENTALS
