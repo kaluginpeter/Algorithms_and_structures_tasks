d3618577c97f70a9d2547e7a340896f47a6f9da2
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-15 13:03:23 +0300

    Added description "Alphabetized"

---
+++ Python_Solutions/CodeWars/6kyu/Alphabetized.py
@@ -0,0 +1,13 @@
+# The alphabetized kata
+# Re-order the characters of a string, so that they are concatenated into a
+# new string in "case-insensitively-alphabetical-order-of-appearance" order.
+# Whitespace and punctuation shall simply be removed!
+#
+# The input is restricted to contain no numerals and only words containing the english alphabet letters.
+#
+# Example:
+#
+# alphabetized("The Holy Bible") # "BbeehHilloTy"
+# Inspired by Tauba Auerbach
+#
+# STRINGSSORTING
