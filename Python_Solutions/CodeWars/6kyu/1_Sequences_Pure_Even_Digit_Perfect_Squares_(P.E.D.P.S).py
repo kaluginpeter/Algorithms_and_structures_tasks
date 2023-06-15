2637aef9c1ef2ad51c69823dd87300a64321f8f3
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-15 12:42:33 +0300

    Added description "#1 Sequences: Pure Even Digit Perfect Squares (P.E.D.P.S)"

---
+++ Python_Solutions/CodeWars/6kyu/1_Sequences_Pure_Even_Digit_Perfect_Squares_(P.E.D.P.S).py
@@ -0,0 +1,20 @@
+# The integer 64 is the first integer that has all of its digits even and furthermore, is a perfect square.
+#
+# The second one is 400 and the third one 484.
+#
+# Give the numbers of this sequence that are in the range [a,b] (both values inclusive)
+#
+# Examples:
+#
+# Even digit squares between 100 to 1000:  [400, 484]  (the output should be sorted)
+# Even digit squares between 1000 to 4000: []
+# Features of the random tests:
+#
+# number of tests = 167
+# maximum value for a = 1e10
+# maximum value for b = 1e12
+# You do not have to check the entries; a and b are always positive integers and a < b.
+#
+# Happy coding!!
+#
+# MATHEMATICSALGORITHMS
