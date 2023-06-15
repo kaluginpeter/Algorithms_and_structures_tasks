c7807ec9d9490a562551060f3dcc2d1377bb634f
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-15 12:50:49 +0300

    Added solution "A Rule of Divisibility by 13"

--- Python_Solutions/CodeWars/6kyu/A_Rule_of_Divisibility_by_13.py
+++ Python_Solutions/CodeWars/6kyu/A_Rule_of_Divisibility_by_13.py
@@ -53,3 +53,16 @@
 # thirt(321) calculates 48, 48 and returns 48
 #
 # FUNDAMENTALSALGORITHMSMATHEMATICS
+# Solution
+def thirt(n):
+    pattern = [1, 10, 9, 12, 3, 4]
+    sum = 0
+    while True:
+        current_sum = 0
+        for index, digit in enumerate(str(n)[::-1]):
+            current_index = index % len(pattern)
+            current_sum += int(digit) * pattern[current_index]
+        if sum == current_sum:
+            return sum
+        sum = current_sum
+        n = current_sum
