52e4247162f2eea4011beb987dd1777b916e7304
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-15 15:42:09 +0300

    Added description "Coding Meetup #9 - Higher-Order Functions Series - Is the meetup age-diverse?"

---
+++ Python_Solutions/CodeWars/6kyu/Coding_Meetup_#9_-_Higher-Order_Functions_Series_-_Is_the_meetup_age-diverse.py
@@ -0,0 +1,72 @@
+# You will be given an array of objects (associative arrays in PHP) representing data about developers who have
+# signed up to attend the next coding meetup that you are organising.
+#
+# Your task is to return:
+#
+# true if developers from all of the following age groups have signed up: teens, twenties, thirties, forties,
+# fifties, sixties, seventies, eighties, nineties, centenarian (at least 100 years young).
+# false otherwise.
+# For example, given the following input array:
+#
+# list1 = [
+#   { 'firstName': 'Harry', 'lastName': 'K.', 'country': 'Brazil', 'continent': 'Americas', 'age': 19, 'language': 'Python' },
+#   { 'firstName': 'Kseniya', 'lastName': 'T.', 'country': 'Belarus', 'continent': 'Europe', 'age': 29, 'language': 'JavaScript' },
+#   { 'firstName': 'Jing', 'lastName': 'X.', 'country': 'China', 'continent': 'Asia', 'age': 39, 'language': 'Ruby' },
+#   { 'firstName': 'Noa', 'lastName': 'A.', 'country': 'Israel', 'continent': 'Asia', 'age': 40, 'language': 'Ruby' },
+#   { 'firstName': 'Andrei', 'lastName': 'E.', 'country': 'Romania', 'continent': 'Europe', 'age': 59, 'language': 'C' },
+#   { 'firstName': 'Maria', 'lastName': 'S.', 'country': 'Peru', 'continent': 'Americas', 'age': 60, 'language': 'C' },
+#   { 'firstName': 'Lukas', 'lastName': 'X.', 'country': 'Croatia', 'continent': 'Europe', 'age': 75, 'language': 'Python' },
+#   { 'firstName': 'Chloe', 'lastName': 'K.', 'country': 'Guernsey', 'continent': 'Europe', 'age': 88, 'language': 'Ruby' },
+#   { 'firstName': 'Viktoria', 'lastName': 'W.', 'country': 'Bulgaria', 'continent': 'Europe', 'age': 98, 'language': 'PHP' },
+#   { 'firstName': 'Piotr', 'lastName': 'B.', 'country': 'Poland', 'continent': 'Europe', 'age': 128, 'language': 'JavaScript' }
+# ]
+# your function should return true as there is at least one developer from each age group.
+#
+# Notes:
+#
+# The input array will always be valid and formatted as in the example above.
+# Age is represented by a number which can be any positive integer up to 199.
+#
+#
+#
+#
+# This kata is part of the Coding Meetup series which includes a number of short and easy to follow katas
+# which have been designed to allow mastering the use of higher-order functions. In JavaScript this includes
+# methods like: forEach, filter, map, reduce, some, every, find, findIndex. Other approaches to solving the
+# katas are of course possible.
+#
+# Here is the full list of the katas in the Coding Meetup series:
+#
+# Coding Meetup #1 - Higher-Order Functions Series - Count the number of JavaScript developers coming from Europe
+#
+# Coding Meetup #2 - Higher-Order Functions Series - Greet developers
+#
+# Coding Meetup #3 - Higher-Order Functions Series - Is Ruby coming?
+#
+# Coding Meetup #4 - Higher-Order Functions Series - Find the first Python developer
+#
+# Coding Meetup #5 - Higher-Order Functions Series - Prepare the count of languages
+#
+# Coding Meetup #6 - Higher-Order Functions Series - Can they code in the same language?
+#
+# Coding Meetup #7 - Higher-Order Functions Series - Find the most senior developer
+#
+# Coding Meetup #8 - Higher-Order Functions Series - Will all continents be represented?
+#
+# Coding Meetup #9 - Higher-Order Functions Series - Is the meetup age-diverse?
+#
+# Coding Meetup #10 - Higher-Order Functions Series - Create usernames
+#
+# Coding Meetup #11 - Higher-Order Functions Series - Find the average age
+#
+# Coding Meetup #12 - Higher-Order Functions Series - Find GitHub admins
+#
+# Coding Meetup #13 - Higher-Order Functions Series - Is the meetup language-diverse?
+#
+# Coding Meetup #14 - Higher-Order Functions Series - Order the food
+#
+# Coding Meetup #15 - Higher-Order Functions Series - Find the odd names
+#
+# Coding Meetup #16 - Higher-Order Functions Series - Ask for missing details
+#
+# FUNCTIONAL PROGRAMMINGDATA STRUCTURESARRAYSFUNDAMENTALSALGORITHMSSTRINGS
