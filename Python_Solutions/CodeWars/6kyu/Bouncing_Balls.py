c0f552e9bbc6e75b9264716c0cce8553ae36ccbc
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-15 13:43:50 +0300

    Added solution "Bouncing Balls"

--- Python_Solutions/CodeWars/6kyu/Bouncing_Balls.py
+++ Python_Solutions/CodeWars/6kyu/Bouncing_Balls.py
@@ -25,3 +25,14 @@
 #
 # (Condition 2) not fulfilled).
 # PUZZLESALGORITHMSMATHEMATICS
+# Solution
+def bouncing_ball(h, bounce, window):
+    if h > 0 and 0 < bounce < 1 and h > window:
+        count = 0
+        while h > window:
+            count += 1
+            h = h * bounce
+            if h > window:
+                count += 1
+        return count
+    return -1
