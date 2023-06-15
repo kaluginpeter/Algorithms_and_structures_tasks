6c96d4d102f3d42207843ace419bcb3a58c0f39d
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-15 18:46:01 +0300

    Added description "Derivatives of type x^n"

---
+++ Python_Solutions/CodeWars/6kyu/Derivatives_of_type_x^n.py
@@ -0,0 +1,47 @@
+# You are provided with a function of the form f(x) = axⁿ,
+# that consists of a single term only and 'a' and 'n' are integers, e.g f(x) = 3x², f(x) = 5 etc.
+#
+# Your task is to create a function that takes f(x) as the argument and returns
+# the result of differentiating the function, that is, the derivative.
+#
+# If
+# �
+# (
+# �
+# )
+# =
+# �
+# �
+# �
+# f(x)=ax
+# n
+#  , then
+# �
+# ′
+# (
+# �
+# )
+# =
+# �
+# �
+# �
+# �
+# −
+# 1
+# f
+# ′
+#  (x)=nax
+# n−1
+#
+#
+# Input is a string, for example "5x^4". The function f(x) consists of a single term only. Variable is denoted by x.
+# Output should be a string, for example "20x^3".
+#
+# Examples
+# "3x^2"  => "6x"
+# "-5x^3" => "-15x^2"
+# "6x^-2" => "-12x^-3"
+# "5x"    => "5"
+# "-x"    => "-1"
+# "42"    => "0"
+# MATHEMATICS
