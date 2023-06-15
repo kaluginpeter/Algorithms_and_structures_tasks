1b7f2323ffe9e546fdd6fa40f3a2b1ab18edfe4e
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-15 15:56:30 +0300

    Added solution "Condi cipher"

--- Python_Solutions/CodeWars/6kyu/Condi_cipher.py
+++ Python_Solutions/CodeWars/6kyu/Condi_cipher.py
@@ -39,3 +39,19 @@
 # they appear in the message string
 # Check the test cases for samples
 # CIPHERSSTRINGSALGORITHMS
+# Solution
+def encode(message, key, shift, encode=True):
+    LOWER = "abcdefghijklmnopqrstuvwxyz"
+    key = sorted(LOWER, key=f"{key}{LOWER}".index)
+    l = []
+    for j in message:
+        if j in key:
+            i = key.index(j)
+            j = key[(i + shift) % 26]
+            shift = i + 1 if encode else -(key.index(j) + 1)
+        l.append(j)
+    return "".join(l)
+
+
+def decode(message, key, shift):
+    return encode(message, key, -shift, encode=False)
