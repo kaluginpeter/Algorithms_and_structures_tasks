53b24fbdd455a1c5bcafd3b6bd08bdf10406cd27
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-15 13:16:36 +0300

    Added description "Array Deep Count"

---
+++ Python_Solutions/CodeWars/6kyu/Array_Deep_Count.py
@@ -0,0 +1,11 @@
+# You are given an array. Complete the function that returns the number
+# of ALL elements within an array, including any nested arrays.
+#
+# Examples
+# []                   -->  0
+# [1, 2, 3]            -->  3
+# ["x", "y", ["z"]]    -->  4
+# [1, 2, [3, 4, [5]]]  -->  7
+# The input will always be an array.
+#
+# ARRAYSRECURSIONFUNDAMENTALS
