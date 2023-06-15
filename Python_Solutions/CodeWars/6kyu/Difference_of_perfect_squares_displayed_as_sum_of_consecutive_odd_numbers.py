e33d11a75c48c6f0f8cd70504a30b417e6ebc5e6
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-15 18:50:43 +0300

    Added description "Difference of perfect squares displayed as sum of consecutive odd numbers"

---
+++ Python_Solutions/CodeWars/6kyu/Difference_of_perfect_squares_displayed_as_sum_of_consecutive_odd_numbers.py
@@ -0,0 +1,30 @@
+# This kata will return a string that represents the difference of two perfect
+# squares as the sum of consecutive odd numbers.
+#
+# Specifications:
+#
+# • The first input minus the second input reveals the exact number of consecutive odd numbers
+# required for the solution - you can check this in the examples below.
+#
+# • The first input will always be larger than the second.
+#
+# • All inputs will be valid so no need for error checking.
+#
+# • Outputs will always be positive.
+#
+# • Inputs will range between 0 and 200, (inclusive).
+#
+# • In the returned string there are spaces before and after the plus sign,
+# the minus sign and the equals sign but nowhere else.
+#
+# Examples:
+#
+# squaresToOdd(9, 5) --> "9^2 - 5^2 = 11 + 13 + 15 + 17 = 56"
+#
+# squaresToOdd(10, 7) --> "10^2 - 7^2 = 15 + 17 + 19 = 51"
+#
+# squaresToOdd(100, 98) --> "100^2 - 98^2 = 197 + 199 = 396"
+#
+# squaresToOdd(100, 99) --> "100^2 - 99^2 = 199 = 199"
+#
+# ALGORITHMSMATHEMATICS
