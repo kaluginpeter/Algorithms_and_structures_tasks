5a3923dbb4322c048da0741f365b8a5d27f059ef
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-15 13:17:41 +0300

    Added description "Array Exchange"

---
+++ Python_Solutions/CodeWars/6kyu/Array_Exchange.py
@@ -0,0 +1,16 @@
+# Array Exchange and Reversing
+#
+# It's time for some array exchange!
+# The objective is simple: exchange the elements of
+# two arrays in-place in a way that their new content is also reversed.
+#
+# # before
+# my_list = ['a', 'b', 'c']
+# other_list = [1, 2, 3]
+#
+# exchange_with(my_list, other_list)
+#
+# # after
+# my_list == [3, 2, 1]
+# other_list == ['c', 'b', 'a']
+# ARRAYSALGORITHMSFUNDAMENTALS
