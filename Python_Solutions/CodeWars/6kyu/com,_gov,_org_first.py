fd8627c4880b097e17a1b18b312f38a311015134
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-15 12:41:10 +0300

    Added description ""com", "gov", "org" first"

---
+++ Python_Solutions/CodeWars/6kyu/com,_gov,_org_first.py
@@ -0,0 +1,32 @@
+# Write a code that orders collection of Uris based on it's domain next way that it will returns fisrt Uris
+# with domain "com", "gov", "org" (in alphabetical order of their domains) and then all other Uris ordered
+# in alphabetical order of their domains In addition to that
+#
+# content of Uri should not be changed,
+# other part of Uri doesn't affect sorting. (uris "c.com","b.com","a.com" can be placed
+# in any order inside their group, so both "c.com","b.com","a.com" and "a.com","c.com","b.com" are correct,
+# till they are stand before *.org)
+# e.g.
+#
+# "http://www.google.en/?x=jsdfkj"
+# "http://www.google.de/?x=jsdfkj"
+# "http://www.google.com/?x=jsdfkj"
+# "http://www.google.com/?x=jsdfkj"
+# "http://www.google.org/?x=jsdfkj"
+# "http://www.google.gov/?x=jsdfkj"
+# should be sorted into
+#
+# "http://www.google.com/?x=jsdfkj"
+# "http://www.google.com/?x=jsdfkj"
+# "http://www.google.gov/?x=jsdfkj"
+# "http://www.google.org/?x=jsdfkj"
+# "http://www.google.de/?x=jsdfkj"
+# "http://www.google.en/?x=jsdfkj"
+# In the final tests consecutive addresses with the same domain will be grouped and sorted before comparison, i.e.:
+#
+# Given your solution returns ["b.com", "a.com", "c.gov"], the tests will do this:
+#
+# Split the addresses into groups: [["b.com", "a.com"], ["c.gov"]]
+# Sort each group: [["a.com", "b.com"], ["c.gov"]]
+# Flatten them: ["a.com", "b.com", "c.gov"]
+# FUNDAMENTALSALGORITHMSSORTING
