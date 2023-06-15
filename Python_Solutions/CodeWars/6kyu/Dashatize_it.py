19dd298fd637d36e37367ff11fe1c709e401c83b
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-15 18:34:02 +0300

    Added description "Dashatize it"

---
+++ Python_Solutions/CodeWars/6kyu/Dashatize_it.py
@@ -0,0 +1,12 @@
+# Given a variable n,
+#
+# If n is an integer, Return a string with dash'-'marks before and after each odd integer,
+# but do not begin or end the string with a dash mark. If n is negative, then the negative sign should be removed.
+#
+# If n is not an integer, return the string "None".
+#
+# Ex:
+#
+# dashatize(274) -> '2-7-4'
+# dashatize(6815) -> '68-1-5'
+# STRINGSARRAYSREGULAR EXPRESSIONSFUNDAMENTALS
