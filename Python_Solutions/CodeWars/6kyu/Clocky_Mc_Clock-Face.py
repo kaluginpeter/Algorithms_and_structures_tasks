d23bab784f31458139bcd288e4bc061abeddf3cf
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-15 15:29:47 +0300

    Added solution "Clocky Mc Clock-Face"

--- Python_Solutions/CodeWars/6kyu/Clocky_Mc_Clock-Face.py
+++ Python_Solutions/CodeWars/6kyu/Clocky_Mc_Clock-Face.py
@@ -35,3 +35,18 @@
 # 7 minutes past midnight is 12:07
 # 7 minutes past noon is also 12:07
 # FUNDAMENTALS
+# Solution
+import math
+def what_time_is_it(angle):
+    calc = math.floor(angle / 30)
+    remain = angle % 30
+    min = math.floor(remain * 2)
+    if angle == 0: return "12:00"
+    elif calc == 0 and min < 10: return f"12:0{min}"
+    elif calc == 0 and min > 9: return f"12:{min}"
+    elif remain == 0 and calc < 10: return f"0{calc}:00"
+    elif remain == 0 and calc > 9: return f"{calc}:00"
+    elif min < 10 and calc < 10: return f"0{calc}:0{min}"
+    elif min < 10 and calc > 9: return f"{calc}:0{min}"
+    elif min > 9 and calc < 10: return f"0{calc}:{min}"
+    elif min > 9 and calc > 9: return f"{calc}:{min}"
