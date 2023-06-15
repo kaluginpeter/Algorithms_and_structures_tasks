bdbd4e5c8b6e9c848c5280d2c2f0e7af40b4d5f0
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-15 12:47:50 +0300

    Added description "2 Arrays 1 Sort"

---
+++ Python_Solutions/CodeWars/6kyu/2_Arrays_1_Sort.py
@@ -0,0 +1,32 @@
+# Imagine two arrays/lists where elements are linked by their positions in the array. For example:
+#
+# HowMany = [ 1   ,   6  ,  5  ,   0  ];
+# Type = ['house', 'car','pen','jeans'];
+# Means I have 1 house, 6 cars,5 pens and 0 jeans.
+#
+# Now if we sort one array we lose the connectivity.
+# The goal is to create a sorting function that keeps
+# the position link linkedSort(arrayToSort,linkedArray,compareFunction).
+# So for every element that moves in arrayToSort(HowMany in the example),
+# the corresponding element in linkedArray(Type in the example) needs to move similarly.
+#
+# For example in Javascript:
+#
+# //INPUT
+# HowMany = [ 1   ,   6  ,  5  ,   0  ];
+# Type = ['house', 'car','pen','jeans'];
+#
+# //SORT
+# res = linkedSort(HowMany,Type,function(a,b){return a-b;})
+#
+# //OUTPUT
+# HowMany === res === [ 0   ,   1   ,  5  ,  6  ];
+# Type       ===     ['jeans','house','pen','car'];
+# linkedSort(...) return the "arrayToSort" sorted only.
+#
+# If no compare function is provided you should handle like an alphabetical sorting would do, e.g:
+#
+# yoursortingfunction([-71,-6,35,0]) ===  [-6,-71,0,35] != [-71,-6,0,35]
+# Note: it is assumed that array are same length.
+#
+# ARRAYSALGORITHMSSORTING
