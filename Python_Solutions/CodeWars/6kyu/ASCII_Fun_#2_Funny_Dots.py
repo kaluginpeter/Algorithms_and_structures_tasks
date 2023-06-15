71be9159c5e83c424636c819dc750468adc67271
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-15 12:54:34 +0300

    Added description "ASCII Fun #2: Funny Dots"

---
+++ Python_Solutions/CodeWars/6kyu/ASCII_Fun_#2_Funny_Dots.py
@@ -0,0 +1,20 @@
+# Funny Dots
+# You will get two integers n (width) and m (height) and your task is to draw the following pattern.
+# Each line is seperated with a newline (\n)
+#
+# Both integers are equal or greater than 1; no need to check for invalid parameters.
+#
+# Examples
+#
+#                                             +---+---+---+
+#              +---+                          | o | o | o |
+# dot(1, 1) => | o |            dot(3, 2) =>  +---+---+---+
+#              +---+                          | o | o | o |
+#                                             +---+---+---+
+#
+# Serie: ASCII Fun
+# ASCII Fun #1: X-Shape
+# ASCII Fun #2: Funny Dots
+# ASCII Fun #3: Puzzle Tiles
+# ASCII Fun #4: Build a pyramid
+# ASCII ART
