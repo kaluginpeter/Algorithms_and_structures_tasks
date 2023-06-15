68c06837c9b0def9348118ae24aeb8b99c46c9ee
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-15 18:34:23 +0300

    Added solution "Dashatize it"

--- Python_Solutions/CodeWars/6kyu/Dashatize_it.py
+++ Python_Solutions/CodeWars/6kyu/Dashatize_it.py
@@ -10,3 +10,9 @@
 # dashatize(274) -> '2-7-4'
 # dashatize(6815) -> '68-1-5'
 # STRINGSARRAYSREGULAR EXPRESSIONSFUNDAMENTALS
+# Solution
+def dashatize(num):
+    num_str = str(num)
+    for i in ['1','3','5','7','9']:
+        num_str = num_str.replace(i,'-' + i + '-')
+    return num_str.strip('-').replace('--','-')
