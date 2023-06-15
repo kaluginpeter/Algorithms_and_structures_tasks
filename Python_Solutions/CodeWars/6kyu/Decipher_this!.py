ff8e3ada0f731bd069a5b616c5e85b2e773d0b12
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-15 18:40:34 +0300

    Added solution "Decipher this!"

--- Python_Solutions/CodeWars/6kyu/Decipher_this!.py
+++ Python_Solutions/CodeWars/6kyu/Decipher_this!.py
@@ -11,3 +11,27 @@
 # decipherThis('72olle 103doo 100ya'); // 'Hello good day'
 # decipherThis('82yade 115te 103o'); // 'Ready set go'
 # STRINGSARRAYSCIPHERSFUNDAMENTALS
+# Solution
+def decipher_this(string):
+    l = string.split(' ')
+    first = []
+    second = []
+    num, alpha, final = "", "", ""
+    for word in l:
+        for letter in word:
+            if letter.isdigit():
+                first.append(letter)
+            if letter.isalpha():
+                second.append(letter)
+        num = chr(int("".join(first)))
+        if second is not False:
+            if len(second) > 1:
+                temp = second[0]
+                second[0] = second[-1]
+                second[-1] = temp
+            alpha = "".join(second)
+        final = final + num + alpha + ' '
+        alpha = ''
+        del first[:]
+        del second[:]
+    return final[0:len(final)-1]
