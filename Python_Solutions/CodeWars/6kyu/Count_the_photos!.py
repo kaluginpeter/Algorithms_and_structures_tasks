12bd3a7c2d6946b0f0dcc9ef39ec322372745326
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-15 16:04:10 +0300

    Added description "Count the photos!"

---
+++ Python_Solutions/CodeWars/6kyu/Count_the_photos!.py
@@ -0,0 +1,17 @@
+# In a string we describe a road. There are cars that move to the right and we denote
+# them with ">" and cars that move to the left and we denote them with "<". There are
+# also cameras that are indicated by: " . ".
+# A camera takes a photo of a car if it moves to the direction of the camera.
+#
+# Task
+# Your task is to write a function such that, for the input string that represents a road
+# as described, returns the total number of photos that were taken by the cameras.
+# The complexity should be strictly O(N) in order to pass all the tests.
+#
+#
+# Examples
+# For ">>." -> 2 photos were taken
+# For ".>>" -> 0 photos were taken
+# For ">.<." -> 3 photos were taken
+# For ".><.>>.<<" -> 11 photos were taken
+# ALGORITHMSPERFORMANCEARRAYS
