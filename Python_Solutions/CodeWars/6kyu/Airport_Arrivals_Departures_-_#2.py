a3529ec09363295e3d6672c141b6a3f644123e65
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-15 12:59:50 +0300

    Added description "Airport Arrivals/Departures - #2"

---
+++ Python_Solutions/CodeWars/6kyu/Airport_Arrivals_Departures_-_#2.py
@@ -0,0 +1,28 @@
+# Well here you are again, still staring blankly at the same arrivals/departures flap display...
+#
+# Kata Task
+# Part #1
+# In Part #1 of this series you already figured out how the flap display mechanism works.
+#
+# You now know what the updated display will look like after applying a set of rotor moves.
+#
+# If you haven't already completed Part 1, then now is a good time to do it!
+#
+# Part #2
+# Now in this current Kata your task is the opposite.
+#
+# It's the same board with the same rules...
+#
+# But this time you are required to return the set of rotor moves needed to
+# transform the display from the before to the after state.
+#
+# Look at the example tests for guidance.
+#
+# And good luck!
+#
+# :-)
+#
+# Notes
+# For Java and C#, a convenient String of the rotor characters provided in Preloaded.ALPHABET
+# ALGORITHMS
+# Suggest kata description edits
