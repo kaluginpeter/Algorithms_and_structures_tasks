663580fd8445fef6d2676a46b009ad3a3d633ea4
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-15 15:54:12 +0300

    Added solution "Complete Fibonacci Series"

--- Python_Solutions/CodeWars/6kyu/Complete_Fibonacci_Series.py
+++ Python_Solutions/CodeWars/6kyu/Complete_Fibonacci_Series.py
@@ -7,3 +7,11 @@
 # fibonacci(4) # should return  [0,1,1,2]
 # fibonacci(-1) # should return []
 # ALGORITHMS
+# Solution
+def fibonacci(n):
+    l = []
+    a, b = 0, 1
+    for i in range(n):
+        l.append(a)
+        a, b = b, a + b
+    return l if n > 0 else []
