3c9792e984e187e2d01db98b4b346483102114ea
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-15 13:29:10 +0300

    Added description "Back and forth then Reverse!"

---
+++ Python_Solutions/CodeWars/6kyu/Back_and_forth_then_Reverse!.py
@@ -0,0 +1,27 @@
+# Task
+# A list S will be given. You need to generate a list T from it by following the given process:
+#
+# Remove the first and last element from the list S and add them to the list T.
+# Reverse the list S
+# Repeat the process until list S gets emptied.
+# The above process results in the depletion of the list S.
+# Your task is to generate list T without mutating the input List S.
+#
+# Example
+# S = [1,2,3,4,5,6]
+# T = []
+#
+# S = [2,3,4,5] => [5,4,3,2]
+# T = [1,6]
+#
+# S = [4,3] => [3,4]
+# T = [1,6,5,2]
+#
+# S = []
+# T = [1,6,5,2,3,4]
+# return T
+# Note
+# size of S goes up to 10^6
+# Keep the efficiency of your code in mind.
+# Do not mutate the Input.
+# ALGORITHMSPERFORMANCEARRAYS
