3c1186a45f410be6f0c927529b474c921ac848e4
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-15 13:00:06 +0300

    Added solution "Airport Arrivals/Departures - #2"

--- Python_Solutions/CodeWars/6kyu/Airport_Arrivals_Departures_-_#2.py
+++ Python_Solutions/CodeWars/6kyu/Airport_Arrivals_Departures_-_#2.py
@@ -26,3 +26,12 @@
 # For Java and C#, a convenient String of the rotor characters provided in Preloaded.ALPHABET
 # ALGORITHMS
 # Suggest kata description edits
+# Solution
+def flat_rotors(lines_before, lines_after):
+    ln = len(ALPHABET)
+    def nxt_rotor(wb, wa):
+        l = []
+        for i,j in zip(wb, wa):
+            l.append((ALPHABET.index(j) - ALPHABET.index(i) - sum(l)) % ln)
+        return l
+    return [nxt_rotor(i,j) for i,j in zip(lines_before, lines_after)]
