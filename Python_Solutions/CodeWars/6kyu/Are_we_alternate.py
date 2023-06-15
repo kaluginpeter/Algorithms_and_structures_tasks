72adf3c147d6967e3833248600aaee471c73e918
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-15 13:12:47 +0300

    Added description "Are we alternate?"

---
+++ Python_Solutions/CodeWars/6kyu/Are_we_alternate.py
@@ -0,0 +1,9 @@
+# Create a function isAlt() that accepts a string as an argument and
+# validates whether the vowels (a, e, i, o, u) and consonants are in alternate order.
+#
+# is_alt("amazon")  # True
+# is_alt("apple")   # False
+# is_alt("banana")  # True
+# Arguments consist of only lowercase letters.
+#
+# ALGORITHMSSTRINGS
