9c173215d2f00f5b4d3d3a0fbd3736c9430b92fd
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-15 18:47:23 +0300

    Added description "Determine the date by the day number"

---
+++ Python_Solutions/CodeWars/6kyu/Determine_the_date_by_the_day_number.py
@@ -0,0 +1,18 @@
+# What date corresponds to the nth day of the year?
+# The answer depends on whether the year is a leap year or not.
+#
+# Write a function that will help you determine the date if you know the number of the day in the year,
+# as well as whether the year is a leap year or not.
+# The function accepts the day number and a boolean value isLeap as arguments, and returns the corresponding
+# date of the year as a string "Month, day".
+# Only valid combinations of a day number and isLeap will be tested.
+#
+# Examples:
+# * With input `41, false` => return "February, 10"
+# * With input `60, false` => return "March, 1
+# * With input `60, true` => return "February, 29"
+# * With input `365, false` => return "December, 31"
+# * With input `366, true` => return "December, 31"
+# calendar
+#
+# FUNDAMENTALSDATE TIMEALGORITHMS
