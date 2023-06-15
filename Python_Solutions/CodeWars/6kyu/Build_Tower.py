e1f12c5f74da78b486d59e711c6b32430872831a
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-15 14:24:31 +0300

    Added solution 'Build Tower'

--- Python_Solutions/CodeWars/6kyu/Build_Tower.py
+++ Python_Solutions/CodeWars/6kyu/Build_Tower.py
@@ -22,3 +22,15 @@
 # Go challenge Build Tower Advanced once you have finished this :)
 #
 # STRINGSASCII ARTFUNDAMENTALS
+# Solution
+def tower_builder(n_floors):
+    list = []
+    for i in range(n_floors):
+        first_elem=''
+        second_elem=''
+        for j in range(i,n_floors-1):
+            first_elem+=' '
+        for k in range(2*i+1):
+            second_elem+='*'
+        list.append(first_elem + second_elem + first_elem)
+    return list
