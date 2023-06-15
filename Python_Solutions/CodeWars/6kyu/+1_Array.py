18e56880a5d2a61fdf951c3ed5d4afa1d62f3e3e
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-15 12:45:12 +0300

    Added solution "+1 Array"

--- Python_Solutions/CodeWars/6kyu/+1_Array.py
+++ Python_Solutions/CodeWars/6kyu/+1_Array.py
@@ -19,3 +19,30 @@
 # [1, 2, 33] is invalid because 33 is not a single-digit integer
 #
 # FUNDAMENTALSARRAYSALGORITHMS
+# Solution
+def up_array(arr):
+    integer = ''
+    list = []
+    print(arr)
+    if len(arr) < 1:
+        return None
+    elif len(arr) == 1 and arr[0] <= 9:
+        return [1]
+    for i in arr:
+        if len(str(i)) > 1 or i < 0:
+            return None
+    else:
+        for elem in arr:
+            if str(elem) in '1234567890':
+                integer += str(elem)
+    if integer[0] == '0' and integer[1] != '0':
+        integer = int(integer[1:]) + 1
+        integer = '0' + str(integer)
+    elif integer[0] == '0' and integer[1] == '0':
+        integer = int(integer[2:]) + 1
+        integer = '00' + str(integer)
+    elif integer[0] != '0':
+        integer = str(int(integer) + 1)
+    for char in integer:
+        list.append(int(char))
+    return list
