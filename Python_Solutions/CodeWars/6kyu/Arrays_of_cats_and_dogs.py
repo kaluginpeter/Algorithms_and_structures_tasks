1a090c86353dc60e14e6801ee708852da0ca7f2c
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-15 13:23:56 +0300

    Added description "Arrays of cats and dogs"

---
+++ Python_Solutions/CodeWars/6kyu/Arrays_of_cats_and_dogs.py
@@ -0,0 +1,15 @@
+# Consider an array containing cats and dogs.
+# Each dog can catch only one cat, but cannot catch a cat that is more than n elements away.
+# Your task will be to return the maximum number of cats that can be caught.
+#
+# For example:
+#
+# solve(['D','C','C','D','C'], 2) = 2, because the dog at index 0 (D0) catches C1 and D3 catches C4.
+# solve(['C','C','D','D','C','D'], 2) = 3, because D2 catches C0, D3 catches C1 and D5 catches C4.
+# solve(['C','C','D','D','C','D'], 1) = 2, because D2 catches C1, D3 catches C4. C0 cannot be caught because n == 1.
+# solve(['D','C','D','D','C'], 1) = 2, too many dogs, so all cats get caught!
+# Do not modify the input array.
+#
+# More examples in the test cases. Good luck!
+#
+# ALGORITHMS
