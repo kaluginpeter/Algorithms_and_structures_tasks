a547a39e0663213cbb0fc51c63c379340cb0de10
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-15 12:39:32 +0300

    Added description ""Stringing"+"Me"+"Along""

---
+++ Python_Solutions/CodeWars/6kyu/Stringing+Me+Along.py
@@ -0,0 +1,12 @@
+# Implement a function that receives a string, and lets you extend it with repeated calls.
+# When no argument is passed you should return a string consisting of space-separated words you've received earlier.
+#
+# Note: There will always be at least 1 string; all inputs will be non-empty.
+#
+# For example:
+# create_message("Hello")("World!")("how")("are")("you?")() == "Hello World! how are you?"
+# Tip (helpful, but not necessary): Try using classes!
+#
+# Good luck and happy coding!
+#
+# FUNCTIONAL PROGRAMMINGFUNDAMENTALS
