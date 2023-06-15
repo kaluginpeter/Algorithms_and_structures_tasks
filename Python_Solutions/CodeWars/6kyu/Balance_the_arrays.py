b4e26bd076a1d38ba93e25702df6c8ebdd603cdd
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-15 13:34:23 +0300

    Added description "Balance the arrays"

---
+++ Python_Solutions/CodeWars/6kyu/Balance_the_arrays.py
@@ -0,0 +1,9 @@
+# Check that the two provided arrays both contain the same number of different unique items,
+# regardless of order. For example in the following:
+#
+# [a,a,a,a,b,b] and [c,c,c,d,c,d]
+# Both arrays have four of one item and two of another, so balance should return true.
+#
+# #Have fun!
+#
+# FUNDAMENTALS
