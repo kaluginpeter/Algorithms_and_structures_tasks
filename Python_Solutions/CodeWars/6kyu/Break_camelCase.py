c6847a88fc11dc3afb033f92694c736372e219d3
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-15 13:46:40 +0300

    Added solution "Break camelCase"

--- Python_Solutions/CodeWars/6kyu/Break_camelCase.py
+++ Python_Solutions/CodeWars/6kyu/Break_camelCase.py
@@ -5,3 +5,6 @@
 # "identifier"   =>  "identifier"
 # ""             =>  ""
 # STRINGSFUNDAMENTALS
+# Solution
+def solution(s):
+    return ''.join(' ' + char if char.isupper() else char.strip() for char in s).strip()
