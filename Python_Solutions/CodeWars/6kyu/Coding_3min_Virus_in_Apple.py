907c48f4bc5e3a3c0c182f4a05dbdf4969ca08f3
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-15 15:31:33 +0300

    Added description "Coding 3min: Virus in Apple"

---
+++ Python_Solutions/CodeWars/6kyu/Coding_3min_Virus_in_Apple.py
@@ -0,0 +1,36 @@
+# This is the simple version of Shortest Code series. If you need some challenges, please try the challenge version
+#
+# Task
+# Your apple has a virus, and the infection is spreading.
+#
+# The apple is a two-dimensional array, containing strings "V" (virus)
+# and "A" (uninfected parts). For each hour, the infection spreads one space up, down, left and right.
+#
+# Input: 2D array apple and number n (n >= 0).
+#
+# Output: 2D array showing the apple after n hours.
+#
+# Series:
+# Bug in Apple
+# Father and Son
+# Jumping Dutch act
+# Planting Trees
+# Give me the equation
+# Find the murderer
+# Reading a Book
+# Eat watermelon
+# Special factor
+# Guess the Hat
+# Symmetric Sort
+# Are they symmetrical?
+# Max Value
+# Trypophobia
+# Virus in Apple
+# Balance Attraction
+# Remove screws I
+# Remove screws II
+# Regular expression compression
+# Collatz Array(Split or merge)
+# Tidy up the room
+# Waiting for a Bus
+# PUZZLESGAMES
