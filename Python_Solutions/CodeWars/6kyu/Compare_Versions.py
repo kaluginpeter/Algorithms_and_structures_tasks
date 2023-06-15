3da7b709b15489de546570fb38a628bcb7905010
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-15 15:45:33 +0300

    Added description "Compare Versions"

---
+++ Python_Solutions/CodeWars/6kyu/Compare_Versions.py
@@ -0,0 +1,24 @@
+# Karan's company makes software that provides different features based on the version of operating system of the user.
+#
+# For finding which version is more recent, Karan uses the following method:
+#
+# While this function worked for OS versions 10.6, 10.7, 10.8 and 10.9, the
+# Operating system company just released OS version 10.10.
+#
+# Karan's function fails for the new version:
+#
+# compare_versions ("10.9", "10.10");       # returns True, while it should return False
+# Karan now wants to spend some time to write a more robust version comparison function that works
+# for any future version/sub-version updates.
+#
+# Help Karan write this function. Here are a few sample cases:
+#
+# compare_versions("11", "10");                    # returns True
+# compare_versions("11", "11");                    # returns True
+# compare_versions("10.4.6", "10.4");              # returns True
+# compare_versions("10.4", "11");                  # returns False
+# compare_versions("10.4", "10.10");               # returns False
+# compare_versions("10.4.9", "10.5");              # returns False
+# It can be assumed that version strings are non empty and only contain numeric literals and the character '.'.
+#
+# STRINGSFUNDAMENTALS
