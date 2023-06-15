e50fd211e155ef4d497af4f0a0e567c7863db976
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-15 14:28:56 +0300

    Added solution "Buying a car"

--- Python_Solutions/CodeWars/6kyu/Buying_a_car.py
+++ Python_Solutions/CodeWars/6kyu/Buying_a_car.py
@@ -43,3 +43,18 @@
 # We don't take care of a deposit of savings in a bank:-)
 #
 # FUNDAMENTALSMATHEMATICS
+# Solution
+def nbMonths(oldCarPrice, newCarPrice, saving, loss):
+    months = 0
+    budget = oldCarPrice
+
+    while budget < newCarPrice:
+        months += 1
+        if months % 2 == 0:
+            loss += 0.5
+
+        oldCarPrice *= (100 - loss) / 100
+        newCarPrice *= (100 - loss) / 100
+        budget = saving * months + oldCarPrice
+
+    return [months, round(budget - newCarPrice)]
