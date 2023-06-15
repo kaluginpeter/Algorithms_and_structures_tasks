809a755baf526f6b2e73c0a321fcd4c1e10ea0dc
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-15 13:21:28 +0300

    Added description "Arrays Similar"

---
+++ Python_Solutions/CodeWars/6kyu/Arrays_Similar.py
@@ -0,0 +1,11 @@
+# Write a function that determines whether the passed in sequences are similar.
+# Similar means they contain the same elements, and the same number of occurrences of elements.
+#
+# arr1 = [1, 2, 2, 3, 4]
+# arr2 = [2, 1, 2, 4, 3]
+# arr3 = [1, 2, 3, 4]
+# arr4 = [1, 2, 3, "4"]
+# arrays_similar(arr1, arr2) # Should equal true
+# arrays_similar(arr2, arr3) # Should equal false
+# arrays_similar(arr3, arr4) # Should equal false
+# ARRAYSALGORITHMS
