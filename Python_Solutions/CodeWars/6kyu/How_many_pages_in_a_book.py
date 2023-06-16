86a8a91c76c6261fd96c30df8a891b49ed4951be
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-16 20:54:48 +0300

    Added description "How many pages in a book?"

---
+++ Python_Solutions/CodeWars/6kyu/How_many_pages_in_a_book.py
@@ -0,0 +1,16 @@
+# Every book has n pages with page numbers 1 to n.
+# The summary is made by adding up the number of digits of all page numbers.
+#
+# Task: Given the summary, find the number of pages n the book has.
+#
+# Example
+# If the input is summary=25, then the output must be n=17:
+# The numbers 1 to 17 have 25 digits in total: 1234567891011121314151617.
+#
+# Be aware that you'll get enormous books having up to 100.000 pages.
+#
+# All inputs will be valid.
+#
+# Here's the opposite kata, which is more complex (Paginating a huge book)
+#
+# PUZZLESALGORITHMS
