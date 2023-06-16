0113fa3c1ed6563df5914c04af3532ba57bdf298
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-16 19:59:26 +0300

    Added description "Find last Fibonacci digit [hardcore version]"

---
+++ Python_Solutions/CodeWars/6kyu/Find_last_Fibonacci_digit_[hardcore version].py
@@ -0,0 +1,11 @@
+# Just like in the "father" kata, you will have to return the last digit of the
+# nth element in the Fibonacci sequence (starting with 1,1, to be extra clear, not with 0,1 or other numbers).
+#
+# You will just get much bigger numbers, so good luck bruteforcing your way through it ;)
+#
+# last_fib_digit(1) == 1
+# last_fib_digit(2) == 1
+# last_fib_digit(3) == 2
+# last_fib_digit(1000) == 5
+# last_fib_digit(1000000) == 5
+# ALGORITHMS
