08ace55dae1dc3c3dc37cb43a8091a92fa7e2f7d
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-16 21:15:52 +0300

    Added solution "Jungerstein's Math Training Room: 1. How many zeros are at the end of n!! ?"

--- Python_Solutions/CodeWars/6kyu/Jungerstein's_Math_Training_Room_1._How_many_zeros_are_at_the_end_of_n!!.py
+++ Python_Solutions/CodeWars/6kyu/Jungerstein's_Math_Training_Room_1._How_many_zeros_are_at_the_end_of_n!!.py
@@ -13,3 +13,19 @@
 # 30!! = 2 * 4 * 6 * 8 * 10 * ... * 22 * 24 * 26 * 28 * 30
 # 30!! = 42849873690624000 (3 zeros at the end)
 # PUZZLESMATHEMATICSNUMBER THEORYDISCRETE MATHEMATICS
+# Solution
+import math
+def count_zeros_n_double_fact(n):
+    c = 0
+    if n % 2 == 0:
+        n = math.prod([i for i in range(2, n+1, 2)])
+        while str(n).endswith('0'):
+            c += 1
+            n = str(n)[:-1]
+        return c
+    if n % 2 != 0:
+        n = math.prod([i for i in range(1, n+1, 2)])
+        while str(n).endswith('0'):
+            c += 1
+            n = str(n)[:-1]
+        return c
