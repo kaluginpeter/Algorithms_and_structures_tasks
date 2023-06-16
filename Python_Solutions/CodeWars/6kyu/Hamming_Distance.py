e59f0be431dd030ab50288ca90f8d9700f3cb3e4
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-16 20:40:22 +0300

    Added description "Hamming Distance"

---
+++ Python_Solutions/CodeWars/6kyu/Hamming_Distance.py
@@ -0,0 +1,18 @@
+# The Hamming Distance is a measure of similarity between two strings of equal length.
+# Complete the function so that it returns the number of positions where the input strings do not match.
+#
+# Examples:
+# a = "I like turtles"
+# b = "I like turkeys"
+# Result: 3
+#
+# a = "Hello World"
+# b = "Hello World"
+# Result: 0
+#
+# a = "espresso"
+# b = "Expresso"
+# Result: 2
+# You can assume that the two inputs are ASCII strings of equal length.
+#
+# ALGORITHMS
