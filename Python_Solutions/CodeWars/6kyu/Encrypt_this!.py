3284c82ccfcc30cf8479a7e7360374714c5b34d6
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-16 19:26:55 +0300

    Added solution "Encrypt this!"

--- Python_Solutions/CodeWars/6kyu/Encrypt_this!.py
+++ Python_Solutions/CodeWars/6kyu/Encrypt_this!.py
@@ -16,3 +16,22 @@
 # encrypt_this("good") == "103doo"
 # encrypt_this("hello world") == "104olle 119drlo"
 # FUNDAMENTALSSTRINGSREGULAR EXPRESSIONSARRAYSCIPHERSALGORITHMSCRYPTOGRAPHYSECURITY
+# Solution
+def encrypt_this(text):
+    words = text.split(" ")
+    res = []
+    for i in words:
+        new = ""
+        temp = ""
+        for j in range(len(i)):
+            if j == 0:
+                new += str(ord(i[j]))
+            elif j == 1:
+                temp = i[j]
+                new += i[-1]
+            elif j == len(i) - 1:
+                new += temp
+            else:
+                new += i[j]
+        res.append(new)
+    return " ".join(list(filter(None, res)))
