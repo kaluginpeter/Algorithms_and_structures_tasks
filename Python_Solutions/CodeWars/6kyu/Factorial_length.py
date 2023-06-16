5975aad019c8b49d4ebca1afb08257cced47109b
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-16 19:47:27 +0300

    Added solution "Factorial length"

--- Python_Solutions/CodeWars/6kyu/Factorial_length.py
+++ Python_Solutions/CodeWars/6kyu/Factorial_length.py
@@ -12,3 +12,10 @@
 # Please also try:
 #
 # ALGORITHMS
+# Solution
+import math
+def count(n):
+    if (n < 0): return 0
+    if (n <= 1): return 1
+    x = ((n * math.log10(n / math.e) + math.log10(2 * math.pi * n) /2.0))
+    return math.floor(x) + 1
