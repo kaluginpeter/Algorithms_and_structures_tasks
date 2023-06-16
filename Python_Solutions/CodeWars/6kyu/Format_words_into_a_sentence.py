fd9a3b050bc19e76fe002e4eea8fae20e844eaee
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-16 20:18:37 +0300

    Added description "Format words into a sentence"

---
+++ Python_Solutions/CodeWars/6kyu/Format_words_into_a_sentence.py
@@ -0,0 +1,14 @@
+# Complete the method so that it formats the words into a single comma separated value.
+# The last word should be separated by the word 'and' instead of a comma. The method takes
+# in an array of strings and returns a single formatted string.
+#
+# Note:
+#
+# Empty string values should be ignored.
+# Empty arrays or null/nil/None values being passed into the method should result in an empty string being returned.
+# Example: (Input --> output)
+#
+# ['ninja', 'samurai', 'ronin'] --> "ninja, samurai and ronin"
+# ['ninja', '', 'ronin'] --> "ninja and ronin"
+# [] -->""
+# STRINGSALGORITHMS
