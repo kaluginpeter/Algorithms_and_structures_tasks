8e64d8de299d1f06b070091ad588d2f158e811f6
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-16 21:23:01 +0300

    Added description "Last Survivors Ep.3"

---
+++ Python_Solutions/CodeWars/6kyu/Last_Survivors_Ep.3.py
@@ -0,0 +1,25 @@
+# Given a list of strings (of letters and spaces), and a list of numbers:
+#
+# Considering the list of strings as a 2D character array,
+# the idea is to remove from each column, starting from bottom,
+# as many letters as indicated in the list of numbers. Then return the remaining letters in any order as a string.
+#
+# If there aren't enough letters, just remove those you can.
+# The strings in the list will all be of the same length.
+# The list of numbers will be of the same length as the strings in the list of strings.
+# Strings will contain only lowercase letters and spaces.
+# There can be duplicated letters and numbers.
+# Example:
+# strings
+#
+# ["abc",
+#  " z ",
+#  " a "]
+# numbers
+#
+#  [0,4,1]
+# the output would be "a".
+#
+# If you like this kata, check out another one: Survivors Ep.4
+#
+# FUNDAMENTALSARRAYS
