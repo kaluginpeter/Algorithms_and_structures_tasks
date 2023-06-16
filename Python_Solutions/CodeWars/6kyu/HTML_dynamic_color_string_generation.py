684ffa87e817b612a4c88f96c5f043564f005c87
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-16 20:39:30 +0300

    Added description "HTML dynamic color string generation"

---
+++ Python_Solutions/CodeWars/6kyu/HTML_dynamic_color_string_generation.py
@@ -0,0 +1,14 @@
+# Generate a valid randomly generated hexadecimal color string. Assume all of them always have 6 digits.
+#
+# Valid Output
+# #ffffff
+# #FfFfFf
+# #25a403
+# #000001
+# Non-Valid Output
+# #fff
+# #aaa
+# #zzzzz
+# cafebabe
+# #a567567676576756A7
+# PUZZLES
