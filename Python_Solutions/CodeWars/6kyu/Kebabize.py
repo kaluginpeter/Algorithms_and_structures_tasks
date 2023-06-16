f2c428ab1179885071a041a75d0ef57461dfd792
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-16 21:17:31 +0300

    Added description "Kebabize"

---
+++ Python_Solutions/CodeWars/6kyu/Kebabize.py
@@ -0,0 +1,9 @@
+# Modify the kebabize function so that it converts a camel case string into a kebab case.
+#
+# "camelsHaveThreeHumps"  -->  "camels-have-three-humps"
+# "camelsHave3Humps"  -->  "camels-have-humps"
+# "CAMEL"  -->  "c-a-m-e-l"
+# Notes:
+#
+# the returned string should only contain lowercase letters
+# FUNDAMENTALSSTRINGSREGULAR EXPRESSIONS
