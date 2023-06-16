2b16c81fc1e905ec366f58d735d06878edb6173e
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-16 21:32:03 +0300

    Added description "Longest Consecutive Sequence of Squares"

---
+++ Python_Solutions/CodeWars/6kyu/Longest_Consecutive_Sequence_of_Squares.py
@@ -0,0 +1,18 @@
+# In this kata you're expected to find the longest consecutive sequence of positive squares that sums up to a number.
+#
+# E.g,
+#
+# ** 595 = 62 + 72 + 82 + 92 + 102 + 112 + 122 **.
+#
+# Your task is to write the function longest_sequence(n) that either finds the longest
+# consecutive sequence of squares that sums to the number n, or determines that no such sequence exists.
+#
+# longest_sequence(50) # => [3, 4, 5]
+#     # 9 + 16 + 25 = 50
+#
+# longest_sequence(595) # => [6, 7, 8, 9, 10, 11, 12]
+#
+# longest_sequence(10) # => []
+# Return an empty list if no such sequence exists.
+#
+# MATHEMATICSALGORITHMS
