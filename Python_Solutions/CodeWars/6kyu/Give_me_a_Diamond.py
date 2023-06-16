ad9cd8627117ff5a64f36b56c67fdceae43f1573
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-16 20:30:46 +0300

    Added solution "Give me a Diamond"

--- Python_Solutions/CodeWars/6kyu/Give_me_a_Diamond.py
+++ Python_Solutions/CodeWars/6kyu/Give_me_a_Diamond.py
@@ -28,3 +28,12 @@
 #
 # "  *\n ***\n*****\n ***\n  *\n"
 # STRINGSASCII ARTFUNDAMENTALS
+# Solution
+def diamond(n):
+    if n < 1 or not n % 2:
+        return None
+    s = ''
+    for i in range(n):
+        ast = '*'*(i*2 + 1) if i <= n/2 else '*'*((n-i)*2 - 1)
+        s += ' '*int((n-len(ast)) / 2) + ast + '\n'
+    return s
