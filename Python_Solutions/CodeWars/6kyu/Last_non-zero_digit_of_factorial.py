b06352deb1077b084481b3542d3848ec60c0d7c1
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-16 21:25:05 +0300

    Added solution "Last non-zero digit of factorial"

--- Python_Solutions/CodeWars/6kyu/Last_non-zero_digit_of_factorial.py
+++ Python_Solutions/CodeWars/6kyu/Last_non-zero_digit_of_factorial.py
@@ -45,3 +45,7 @@
 # Calculating the whole factorial will timeout.
 #
 # MATHEMATICSALGORITHMS
+# Solution
+def last_digit(n):
+    if n < 10: return [1, 1, 2, 6, 4, 2, 2, 4, 2, 8][n]
+    return (4 if int(str(n)[-2]) % 2 else 6) * last_digit(n // 5) * last_digit(n % 10) % 10
