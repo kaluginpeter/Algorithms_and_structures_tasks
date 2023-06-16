d891b38fe156b74749fec9cef7570cfa87c99982
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-16 20:29:07 +0300

    Added description "Get your steppin' on son"

---
+++ Python_Solutions/CodeWars/6kyu/Get_your_steppin'_on_son.py
@@ -0,0 +1,22 @@
+# Write
+#
+# function wordStep(str)
+# that takes in a string and creates a step with that word.
+#
+# E.g
+#
+# wordStep('SNAKES SHOE EFFORT TRUMP POTATO') ===
+#
+# [['S','N','A','K','E','S',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+#  [' ',' ',' ',' ',' ','H',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+#  [' ',' ',' ',' ',' ','O',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+#  [' ',' ',' ',' ',' ','E','F','F','O','R','T',' ',' ',' ',' ',' '],
+#  [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','R',' ',' ',' ',' ',' '],
+#  [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','U',' ',' ',' ',' ',' '],
+#  [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','M',' ',' ',' ',' ',' '],
+#  [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','P','O','T','A','T','O']]
+# Every word will end with the character that the next word will start with.
+# You will start top left of the array and end bottom right. All cells that
+# are not occupied by a letter needs to be a space ' '
+#
+# ALGORITHMS
