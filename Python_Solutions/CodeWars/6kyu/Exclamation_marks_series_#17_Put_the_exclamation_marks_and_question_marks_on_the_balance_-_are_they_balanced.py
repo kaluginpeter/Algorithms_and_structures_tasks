186ed90806510d7b90d341ad320ac601e7c25eb8
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-16 19:44:52 +0300

    Added description "Exclamation marks series #17: Put the exclamation marks and question marks on the balance - are they balanced?"

---
+++ Python_Solutions/CodeWars/6kyu/Exclamation_marks_series_#17_Put_the_exclamation_marks_and_question_marks_on_the_balance_-_are_they_balanced.py
@@ -0,0 +1,12 @@
+# Each exclamation mark's weight is 2; each question mark's weight is 3.
+# Putting two strings left and right on the balance - are they balanced?
+#
+# If the left side is more heavy, return "Left"; if the right side is more heavy,
+# return "Right"; if they are balanced, return "Balance".
+#
+# Examples
+# "!!", "??"     -->  "Right"
+# "!??", "?!!"   -->  "Left"
+# "!?!!", "?!?"  -->  "Left"
+# "!!???!????", "??!!?!!!!!!!"  -->  "Balance"
+# FUNDAMENTALS
