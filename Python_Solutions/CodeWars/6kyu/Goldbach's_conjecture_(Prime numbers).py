b9224436c7587d3846021b00af56f45ca2fbb33b
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-16 20:32:47 +0300

    Added description "Goldbach's conjecture (Prime numbers)"

---
+++ Python_Solutions/CodeWars/6kyu/Goldbach's_conjecture_(Prime numbers).py
@@ -0,0 +1,27 @@
+# Goldbach's conjecture is one of the oldest and best-known unsolved
+# problems in number theory and all of mathematics. It states:
+#
+# Every even integer greater than 2 can be expressed as the sum of two primes. For example:
+#
+# 6 = 3 + 3
+# 8 = 3 + 5
+# 10 = 3 + 7 = 5 + 5
+# 12 = 5 + 7
+#
+# Some rules for the conjecture:
+#
+# pairs should be descending like [3,5] not [5,3]
+#
+# all pairs should be in ascending order based on the first element of the pair: [[5, 13], [7, 11]] is accepted
+# but [[7, 11],[5, 13]] is not accepted.
+#
+# Write the a function that find all identical pairs of prime numbers:
+#
+# def goldbach(even_number)
+# You should return an array of containing pairs of primes, like:
+#
+# [[5, 13], [7, 11]]  # even_number = 18
+# or
+#
+# [[3, 31], [5, 29], [11, 23], [17, 17]] # even_number = 34
+# MATHEMATICSFUNDAMENTALS
