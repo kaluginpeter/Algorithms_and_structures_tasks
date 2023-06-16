fa02846e3b8709b5b123602c2295513b8788c60a
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-16 19:55:49 +0300

    Added description "Find Added"

---
+++ Python_Solutions/CodeWars/6kyu/Find_Added.py
@@ -0,0 +1,12 @@
+# You are given two strings (st1, st2) as inputs.
+# Your task is to return a string containing the numbers in st2 which are not in str1.
+# Make sure the numbers are returned in ascending order. All inputs will be a string of numbers.
+#
+# Here are some examples:
+#
+# findAdded('4455446', '447555446666'); // '56667'
+# findAdded('44554466', '447554466'); // '7'
+# findAdded('9876521', '9876543211'); // '134'
+# findAdded('678', '876'); // ''
+# findAdded('678', '6'); // ''
+# FUNDAMENTALSSTRINGS
