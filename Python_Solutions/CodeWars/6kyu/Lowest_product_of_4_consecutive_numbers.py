5342ba836c73c68ccf7924df0eb1b673dd714a14
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-16 21:39:20 +0300

    Added solution "Lowest product of 4 consecutive numbers"

--- Python_Solutions/CodeWars/6kyu/Lowest_product_of_4_consecutive_numbers.py
+++ Python_Solutions/CodeWars/6kyu/Lowest_product_of_4_consecutive_numbers.py
@@ -6,3 +6,8 @@
 # lowest_product("123456789") --> 24 (1x2x3x4)
 # lowest_product("35") --> "Number is too small"
 # FUNDAMENTALS
+# Solution
+from operator import mul
+from functools import reduce
+def lowest_product(input):
+    return min(reduce(mul, [*map(int, input)][i:i + 4]) for i in range(0, len(input) - 3)) if len(input) > 3 else "Number is too small"
