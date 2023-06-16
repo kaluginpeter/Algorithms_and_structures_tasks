db8776a73267215769450eb40c3a0f84c7b8348c
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-16 19:42:32 +0300

    Added description "Exclamation marks series #16: Find the longest successive exclamation marks and question marks combination in the string"

---
+++ Python_Solutions/CodeWars/6kyu/Exclamation_marks_series_#16_Find_the_longest_successive_exclamation_marks_and_question_marks_combination_in_the_string.py
@@ -0,0 +1,22 @@
+# Description:
+# Find the longest successive exclamation marks and question marks combination in the string.
+# A successive exclamation marks and question marks combination must contains two part:
+# a substring of "!" and a substring "?", they are adjacent.
+#
+# If more than one result are found, return the one which at left side; If no such a combination found, return "".
+#
+# Examples
+# find("!!") === ""
+# find("!??") === "!??"
+# find("!?!!") === "?!!"
+# find("!!???!????") === "!!???"
+# find("!!???!?????") === "!?????"
+# find("!????!!!?") === "????!!!"
+# find("!?!!??!!!?") === "??!!!"
+# Note
+# Please don't post issue about difficulty or duplicate. Because:
+#
+# That's unfair on the kata creator. This is a valid kata and introduces new people to
+# javascript some regex or loops, depending on how they tackle this problem. --matt c
+#
+# FUNDAMENTALS
