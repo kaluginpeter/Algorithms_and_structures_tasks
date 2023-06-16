40ae308357c25e105087cc3e0641718f62227ac1
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-16 21:12:17 +0300

    Added description "Is Integer Array?"

---
+++ Python_Solutions/CodeWars/6kyu/Is_Integer_Array.py
@@ -0,0 +1,8 @@
+# Write a function with the signature shown below:
+#
+# def is_int_array(arr):
+#     return True
+# returns true  / True if every element in an array is an integer or a float with no decimals.
+# returns true  / True if array is empty.
+# returns false / False for every other input.
+# ARRAYSFUNDAMENTALS
