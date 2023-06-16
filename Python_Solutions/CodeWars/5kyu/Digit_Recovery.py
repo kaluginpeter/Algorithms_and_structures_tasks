4acd4fd5e7488717d9bbea6cffc7ec5c12a62c1a
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-16 17:44:19 +0300

    Added solution "Digit Recovery"

--- Python_Solutions/CodeWars/5kyu/Digit_Recovery.py
+++ Python_Solutions/CodeWars/5kyu/Digit_Recovery.py
@@ -27,3 +27,10 @@
 # const alph = {"ZERO":0,"ONE":1,"TWO":2,"THREE":3,"FOUR":4,"FIVE":5,"SIX":6,"SEVEN":7,"EIGHT":8,"NINE":9};
 #
 # ALGORITHMS
+# Solution
+def recover(st):
+    l = []
+    for i in range(len(st)):
+        for k, v in alph.items():
+            if sorted(k) == sorted(st[i:i+len(k)]): l.append(v)
+    return ''.join(map(str, l)) or "No digits found"
