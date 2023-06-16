5fb45659659146a55ec6db12c2a713e0f6c96083
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-16 20:52:47 +0300

    Added description "How many cows do you have?"

---
+++ Python_Solutions/CodeWars/6kyu/How_many_cows_do_you_have.py
@@ -0,0 +1,15 @@
+# Consider having a cow that gives a child every year from her fourth
+# year of life on and all her subsequent children do the same.
+#
+# After n years how many cows will you have?
+#
+# countCows(0); // should equal 1
+# countCows(1); // should equal 1
+# countCows(3); // should equal 2
+# countCows(4); // should equal 3
+# countCows(10);// should equal 28
+# Return null if n is not an integer.
+#
+# Note: Assume all the cows are alive after n years.
+#
+# MATHEMATICSPUZZLES
