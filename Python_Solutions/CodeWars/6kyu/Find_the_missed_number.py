fc2281d8a6aca23f540639c7d162b2d62365f06d
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-16 20:04:00 +0300

    Added description "Find the missed number"

---
+++ Python_Solutions/CodeWars/6kyu/Find_the_missed_number.py
@@ -0,0 +1,25 @@
+# In this kata, you will be given a string containing numbers from a to b, one number can
+# be missing from these numbers, then the string will be shuffled, you're expected to return
+# an array of all possible missing numbers.
+#
+# Examples (input => output)
+# Here's a string with numbers from 1 - 21, its missing one number and the string is then
+# shuffled, your'e expected to return a list of possible missing numbers.
+#
+# 1, 21, "2198765123416171890101112131415"  => [ 12, 21 ]
+# You won't be able to tell if its 21 or 12, you must return all possible values in an array.
+#
+# Another Example
+# 5, 10, "578910" => [ 6 ]
+# Documentation:
+# The parameters will be in order two numbers and a string:
+#
+# start => from
+# stop => to
+# string => numbers from start to stop in string form (shuffled), but missing one number
+# Note:
+#
+# if there're no missing numbers return an empty list
+# Too easy ? Try Range of Integers in an Unsorted String
+#
+# MATHEMATICSPUZZLESALGORITHMSLOGIC
