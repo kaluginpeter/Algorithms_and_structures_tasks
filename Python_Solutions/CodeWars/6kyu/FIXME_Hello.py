40f65c29f29f76b4cdb944ead0a29c42e9fac31d
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-16 19:46:35 +0300

    Added solution "FIXME: Hello"

--- Python_Solutions/CodeWars/6kyu/FIXME_Hello.py
+++ Python_Solutions/CodeWars/6kyu/FIXME_Hello.py
@@ -23,3 +23,32 @@
 # Fix the code so we can all go home early.
 #
 # DEBUGGING
+# Solution
+class Dinglemouse(object):
+
+    def __init__(self):
+        self.name = None
+        self.sex = None
+        self.age = None
+        self.hell = 'Hello.'
+
+    def setAge(self, age):
+        if self.age == None:
+            self.hell = self.hell + ' I am {age}.'
+        self.age = age
+        return self
+
+    def setSex(self, sex):
+        if self.sex == None:
+            self.hell = self.hell + ' I am {sex}.'
+        self.sex = "male" if sex == 'M' else "female"
+        return self
+
+    def setName(self, name):
+        if self.name == None:
+            self.hell = self.hell + ' My name is {name}.'
+        self.name = name
+        return self
+
+    def hello(self):
+        return self.hell.format(age=self.age, sex=self.sex, name=self.name)
