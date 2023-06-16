9f86626da902ebcbbb8c01442001852463e64a2d
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-16 19:31:30 +0300

    Added solution "Error correction #1 - Hamming Code"

--- Python_Solutions/CodeWars/6kyu/Error_correction_#1_-_Hamming_Code.py
+++ Python_Solutions/CodeWars/6kyu/Error_correction_#1_-_Hamming_Code.py
@@ -51,3 +51,9 @@
 # Hack the NSA
 #
 # ALGORITHMSBITS
+# Solution
+def encode(string):
+    return ''.join(map('{:08b}'.format, string.encode())).replace('0', '000').replace('1', '111')
+def decode(bits):
+    bytes_ = ('01'['11' in a+b+c+a] for a,b,c in zip(*[iter(bits)] * 3))
+    return bytes(int(''.join(i), 2) for i in zip(* [iter(bytes_)] * 8)).decode()
