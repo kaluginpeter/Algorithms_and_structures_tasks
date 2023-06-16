6a78e57223d92fb6fa6586175e364aa8e69bc45e
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-16 21:05:17 +0300

    Added description "Inserting multiple strings into another string"

---
+++ Python_Solutions/CodeWars/6kyu/Inserting_multiple_strings_into_another_string.py
@@ -0,0 +1,18 @@
+# Dear Coder,
+#
+# We at [SomeLargeCompany] have decided to expand on the functionality of our online text editor.
+#
+# We have written a new function that accepts a phrase, a word and an array of indexes.
+# We want this function to return the phrase, with the word inserted at each of the indexes given by the array.
+#
+# However, our current function only gets the first insertion right, but all of the following ones are out of place!
+#
+# Please fix this for us, and you will be showered with money.
+#
+# Yours Sincerely, [SomeLargeCompany]
+#
+# Note :
+#
+# the indicies are always in range and passed as a sorted array
+# note if the index array is empty, just return the initial phrase
+# STRINGSDEBUGGING
