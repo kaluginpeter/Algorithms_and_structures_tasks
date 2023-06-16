97a4c8e5e69223b574b50b6f84253f15e7a5910c
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-16 21:08:04 +0300

    Added description "Integer reduction"

---
+++ Python_Solutions/CodeWars/6kyu/Integer_reduction.py
@@ -0,0 +1,16 @@
+# In this Kata, you will be given two integers n and k and your task is to
+# remove k-digits from n and return the lowest number possible, without changing
+# the order of the digits in n. Return the result as a string.
+#
+# Let's take an example of solve(123056,4). We need to remove 4 digits from 123056
+# and return the lowest possible number. The best digits to remove are (1,2,3,6) so
+# that the remaining digits are '05'. Therefore, solve(123056,4) = '05'.
+#
+# Note also that the order of the numbers in n does not change: solve(1284569,2) = '12456',
+# because we have removed 8 and 9.
+#
+# More examples in the test cases.
+#
+# Good luck!
+#
+# ALGORITHMS
