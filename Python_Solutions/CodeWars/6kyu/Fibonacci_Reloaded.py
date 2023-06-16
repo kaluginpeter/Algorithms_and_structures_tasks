673d21e5af08a8a411f19f77f2f405d746410d8a
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-16 19:51:32 +0300

    Added description "Fibonacci Reloaded"

---
+++ Python_Solutions/CodeWars/6kyu/Fibonacci_Reloaded.py
@@ -0,0 +1,17 @@
+# And here is Fibonacci again. This time we want to go one step further.
+# Our fib() function must be faster! Can you do it?
+#
+# In case you don't know, what the Fibonacci number is:
+#
+# The nth Fibonacci number is defined by the sum of the two previous Fibonacci numbers.
+# In our case: fib(1) := 0 and fib(2) := 1. With these initial values you should be able
+# to calculate each following Fibonacci number.
+#
+# Examples:
+#
+# fib(1) // === 0
+# fib(2) // === 1
+# fib(3) // === 1
+# fib(4) // === 2
+# fib(5) // === 3
+# ALGORITHMS
