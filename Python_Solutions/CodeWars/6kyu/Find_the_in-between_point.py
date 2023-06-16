70e0e237816f2de34013eeda895892647ae75577
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-16 20:03:06 +0300

    Added description "Find the in-between point"

---
+++ Python_Solutions/CodeWars/6kyu/Find_the_in-between_point.py
@@ -0,0 +1,7 @@
+# For this kata, you are given three points (x1,y1,z1), (x2,y2,z2),
+# and (x3,y3,z3) that lie on a straight line in 3-dimensional space.
+# You have to figure out which point lies in between the other two.
+#
+# Your function should return 1, 2, or 3 to indicate which point is the in-between one.
+#
+# FUNDAMENTALSGEOMETRYMATHEMATICSALGORITHMS
