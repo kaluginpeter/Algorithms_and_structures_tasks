4b3636903c09c54cd4fa2c9990f0fe1566eceefe
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-16 20:29:29 +0300

    Added solution "Get your steppin' on son"

--- Python_Solutions/CodeWars/6kyu/Get_your_steppin'_on_son.py
+++ Python_Solutions/CodeWars/6kyu/Get_your_steppin'_on_son.py
@@ -20,3 +20,14 @@
 # are not occupied by a letter needs to be a space ' '
 #
 # ALGORITHMS
+# Solution
+def word_step(s):
+    wo = s.split(" ")
+    l, h = (sum(len(j) - 1 for j in wo[i::2]) + 1 for i in range(2))
+    arr = [[" " for i in range(l)] for j in range(h)]
+    l = [0, 0]
+    for i, w in enumerate(wo):
+        for j, c in enumerate(w):
+            l[i % 2] += 1 if j else 0
+            arr[l[1]][l[0]] = c
+    return arr
