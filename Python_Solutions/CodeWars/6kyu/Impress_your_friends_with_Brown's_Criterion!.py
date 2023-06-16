56124ee44b41e3bf764d871b6e83f3b27ba51663
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-16 21:04:39 +0300

    Added solution "Impress your friends with Brown's Criterion!"

--- Python_Solutions/CodeWars/6kyu/Impress_your_friends_with_Brown's_Criterion!.py
+++ Python_Solutions/CodeWars/6kyu/Impress_your_friends_with_Brown's_Criterion!.py
@@ -45,3 +45,16 @@
 # Good Luck!
 #
 # MATHEMATICSLOGICPUZZLES
+# Solution
+sequence = ((1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31),
+            (2, 3, 6, 7, 10, 11, 14, 15, 18, 19, 22, 23, 26, 27, 30, 31),
+            (4, 5, 6, 7, 12, 13, 14, 15, 20, 21, 22, 23, 28, 29, 30, 31),
+            (8, 9, 10, 11, 12, 13, 14, 15, 24, 25, 26, 27, 28, 29, 30, 31),
+            (16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31))
+
+
+def guess_number(answers):
+    return sum(sequence[i][0] for i in range(len(answers)) if answers[i] == 1)
+
+def answers_sequence(n):
+    return [1 if n in i else 0 for i in sequence]
