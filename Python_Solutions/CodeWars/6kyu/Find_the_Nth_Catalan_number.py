24a05f2aac0cb0c9de09c80f3d383534dfa32c06
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-16 20:01:18 +0300

    Added description "Find the Nth Catalan number"

---
+++ Python_Solutions/CodeWars/6kyu/Find_the_Nth_Catalan_number.py
@@ -0,0 +1,22 @@
+# What is a Catalan number?
+# In combinatorial mathematics, the Catalan numbers form a sequence of natural
+# numbers that occur in various counting problems, often involving recursively-defined objects.
+# They are named after the Belgian mathematician Eugène Charles Catalan (1814–1894).
+#
+# Using zero-based numbering, the nth Catalan number is given directly in terms of binomial coefficients by:
+#
+#
+# You can read more on Catalan numbers Here.
+#
+# Task:
+# Implement a function which calculates the Nth Catalan number.
+#
+# 0  =>  1
+# 3  =>  5
+# 7  =>  429
+# 9  =>  4862
+# Hint: avoid the use of floats
+#
+# Have fun :)
+#
+# BIG INTEGERSALGORITHMSMATHEMATICS
