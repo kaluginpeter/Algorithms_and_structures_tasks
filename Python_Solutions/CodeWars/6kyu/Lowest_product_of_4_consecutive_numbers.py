cc35c6bef63b0a87f85452773fdae953eee1ee69
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-16 21:39:03 +0300

    Added description "Lowest product of 4 consecutive numbers"

---
+++ Python_Solutions/CodeWars/6kyu/Lowest_product_of_4_consecutive_numbers.py
@@ -0,0 +1,8 @@
+# Create a function that returns the lowest product of 4 consecutive digits in a number given as a string.
+#
+# This should only work if the number has 4 digits or more. If not, return "Number is too small".
+#
+# Example
+# lowest_product("123456789") --> 24 (1x2x3x4)
+# lowest_product("35") --> "Number is too small"
+# FUNDAMENTALS
