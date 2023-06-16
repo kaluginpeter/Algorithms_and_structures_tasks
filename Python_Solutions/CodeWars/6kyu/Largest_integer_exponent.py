65a6b5ad788fbd76ba2ee67974bd8abaed34d07f
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-16 21:21:15 +0300

    Added description "Largest integer exponent"

---
+++ Python_Solutions/CodeWars/6kyu/Largest_integer_exponent.py
@@ -0,0 +1,5 @@
+# Write a method named getExponent(n,p) that returns the largest integer
+# exponent x such that px evenly divides n. if p<=1 the method should
+# return null/None (throw an ArgumentOutOfRange exception in C#).
+#
+# MATHEMATICSFUNDAMENTALS
