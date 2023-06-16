4fed484600423a9a08d76a7cf0bb935531b41965
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-16 20:27:37 +0300

    Added solution "Geometry Basics: Triangle Area in 2D"

--- Python_Solutions/CodeWars/6kyu/Geometry_Basics_Triangle_Area_in_2D.py
+++ Python_Solutions/CodeWars/6kyu/Geometry_Basics_Triangle_Area_in_2D.py
@@ -8,3 +8,7 @@
 # Tests round answers to 6 decimal places.
 #
 # GEOMETRYFUNDAMENTALS
+# Solution
+def triangle_area(triangle):
+    a, b, c = triangle.a, triangle.b, triangle.c
+    return round(abs(a.x * (b.y - c.y) + b.x * (c.y - a.y) + c.x * (a.y - b.y)) / 2, 6)
