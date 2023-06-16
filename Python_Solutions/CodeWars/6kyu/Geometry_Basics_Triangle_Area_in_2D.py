2f43fb555c32e382b23cf53735c99b85a0671f76
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-16 20:27:18 +0300

    Added description "Geometry Basics: Triangle Area in 2D"

---
+++ Python_Solutions/CodeWars/6kyu/Geometry_Basics_Triangle_Area_in_2D.py
@@ -0,0 +1,10 @@
+# This series of katas will introduce you to basics of doing geometry with computers.
+#
+# Point objects have x, y attributes. Triangle objects have attributes
+# a, b, c describing their corners, each of them is a Point.
+#
+# Write a function calculating area of a Triangle defined this way.
+#
+# Tests round answers to 6 decimal places.
+#
+# GEOMETRYFUNDAMENTALS
