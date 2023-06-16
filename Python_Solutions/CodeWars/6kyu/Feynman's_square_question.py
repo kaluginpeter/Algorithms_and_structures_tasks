6c3b5b0c649d71dc153f98357454496f8f51ce31
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-16 19:48:03 +0300

    Added description "Feynman's square question"

---
+++ Python_Solutions/CodeWars/6kyu/Feynman's_square_question.py
@@ -0,0 +1,23 @@
+# Feynman's squares
+# Richard Phillips Feynman was a well-known American physicist and a
+# recipient of the Nobel Prize in Physics. He worked in theoretical
+# physics and pioneered the field of quantum computing.
+#
+# Recently, an old farmer found some papers and notes that are believed to
+# have belonged to Feynman. Among notes about mesons and electromagnetism,
+# there was a napkin where he wrote a simple puzzle: "how many different squares are there in a grid of NxN squares?".
+#
+# For example, when N=2, the answer is 5: the 2x2 square itself, plus the four 1x1 squares in its corners:
+#
+#
+# Task
+# Complete the function that solves Feynman's question in general.
+# The input to your function will always be a positive integer.
+#
+# Examples
+# 1  -->   1
+# 2  -->   5
+# 3  -->  14
+# (Adapted from the Sphere Online Judge problem SAMER08F by Diego Satoba)
+#
+# PUZZLES
