318d37bc832b7b476bfaab331f0207944af52dec
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-16 19:37:04 +0300

    Added description "Even Odd Pattern #1"

---
+++ Python_Solutions/CodeWars/6kyu/Even_Odd_Pattern_#1.py
@@ -0,0 +1,10 @@
+# Write a function that takes an array/list of numbers and returns a number.
+#
+# See the examples and try to guess the pattern:
+#
+# even_odd([1,2,6,1,6,3,1,9,6]) => 393
+# even_odd([1,2,3]) => 5
+# even_odd([0,2,3]) => 3
+# even_odd([1,0,3]) => 3
+# even_odd([3,2])   => 6
+# LOGICARRAYSMATHEMATICSPUZZLES
