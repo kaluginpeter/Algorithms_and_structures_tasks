db1ea2935102a92ffcb8988b75ba82eb411a7b85
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-16 20:37:51 +0300

    Added description "Group in 10s"

---
+++ Python_Solutions/CodeWars/6kyu/Group_in_10s.py
@@ -0,0 +1,17 @@
+# Write a function groupIn10s which takes any number of arguments,
+# groups them into tens, and sorts each group in ascending order.
+#
+# The return value should be an array of arrays, so that numbers between
+# 0 and9 inclusive are in position 0, numbers between 10 and 19 are in position 1, etc.
+#
+# Here's an example of the required output:
+#
+# grouped = group_in_10s(8, 12, 38, 3, 17, 19, 25, 35, 50)
+#
+# grouped[0]     # [3, 8]
+# grouped[1]     # [12, 17, 19]
+# grouped[2]     # [25]
+# grouped[3]     # [35, 38]
+# grouped[4]     # None
+# grouped[5]     # [50]
+# ARRAYSLISTSFUNDAMENTALS
