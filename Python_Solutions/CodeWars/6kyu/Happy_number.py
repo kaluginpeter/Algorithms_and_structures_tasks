a9e034278af2b10250de44d79672631b6acf6aff
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-16 20:43:30 +0300

    Added solution "Happy numbers"

--- Python_Solutions/CodeWars/6kyu/Happy_number.py
+++ Python_Solutions/CodeWars/6kyu/Happy_number.py
@@ -17,3 +17,10 @@
 # Happy coding!
 #
 # ALGORITHMS
+# Solution
+def is_happy(n):
+    s = set()
+    while n not in s:
+        s.add(n)
+        n = sum(int(i)**2 for i in str(n))
+    return n == 1
