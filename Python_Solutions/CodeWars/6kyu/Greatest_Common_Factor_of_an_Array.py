f74e822cbd5d3bddb3a6bdd806c7d5e0fd348201
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-16 20:35:44 +0300

    Added description "Greatest Common Factor of an Array"

---
+++ Python_Solutions/CodeWars/6kyu/Greatest_Common_Factor_of_an_Array.py
@@ -0,0 +1,5 @@
+# Write a function that returns the greatest common factor of an array of positive integers.
+# Your return value should be a number, you will only receive positive integers.
+#
+# greatest_common_factor([46, 14, 20, 88]) # == 2
+# FUNDAMENTALSALGORITHMS
