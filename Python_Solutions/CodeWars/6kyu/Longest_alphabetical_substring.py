b3bf244bbeadc4bec5b7791ae12a684e7aa704e6
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-16 21:34:19 +0300

    Added solution "Longest alphabetical substring"

--- Python_Solutions/CodeWars/6kyu/Longest_alphabetical_substring.py
+++ Python_Solutions/CodeWars/6kyu/Longest_alphabetical_substring.py
@@ -11,3 +11,12 @@
 # Good luck :)
 #
 # FUNDAMENTALSSTRINGS
+# Solution
+import re
+def longest(s):
+    matches = re.findall('a*b*c*d*e*f*g*h*i*j*k*l*m*n*o*p*q*r*s*t*u*v*w*x*y*z*', s)
+    current_longest = matches[0]
+    for match in matches:
+        if len(match) > len(current_longest):
+            current_longest = match
+    return current_longest
