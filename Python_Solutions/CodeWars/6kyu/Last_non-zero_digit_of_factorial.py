eccef0476d3d0c9d7adf99ce097e758bd78e1fdc
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-16 21:24:45 +0300

    Added description "Last non-zero digit of factorial"

---
+++ Python_Solutions/CodeWars/6kyu/Last_non-zero_digit_of_factorial.py
@@ -0,0 +1,47 @@
+# Your task is to find the last non-zero digit of
+# �
+# !
+# n! (factorial).
+#
+# �
+# !
+# =
+# 1
+# ×
+# 2
+# ×
+# 3
+# ×
+# ⋯
+# ×
+# �
+# n!=1×2×3×⋯×n
+#
+# Example:
+# If
+# �
+# =
+# 12
+# n=12, your function should return
+# 6
+# 6 since
+# 12
+# !
+# =
+# 479001600
+# 12!=479001600
+#
+# Input
+# Non-negative integer n
+# Range: 0 - 2.5E6
+#
+# Output
+# Last non-zero digit of
+# �
+# !
+# n!
+#
+# Note
+# Calculating the whole factorial will timeout.
+#
+# MATHEMATICSALGORITHMS
