7a74074fa3103fd5762a50f2e76609fa2d66615e
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 16:56:31 +0300

    Added description "Prime Cycles"

---
+++ Python_Solutions/CodeWars/6kyu/Prime_Cycles.py
@@ -0,0 +1,15 @@
+# Given an integer n, we can construct a new integer with the following procedure:
+#
+# For each digit d in n, find the dth prime number. (If d=0, use 1)
+# Take the product of these prime numbers. This is our new integer.
+# For example, take 25: The 2nd prime is 3, and the 5th is 11. So 25 would evaluate to 3*11 = 33.
+#
+# If we iterate this procedure, we generate a sequence of integers.
+#
+# Write a function that, given a positive integer n, returns the maximum value in the sequence starting at n.
+#
+# Example: 8 -> 19 -> 46 -> 91 -> 46 -> 91 -> ...
+# So the maximum here would be 91.
+# 0 <= n <= 10^12
+#
+# PUZZLES
