1e5981044d03af02a90a148b079dc42679526cf1
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 16:22:23 +0300

    Added description "Numericals of a String"

---
+++ Python_Solutions/CodeWars/6kyu/Numericals_of_a_String.py
@@ -0,0 +1,16 @@
+# You are given an input string.
+#
+# For each symbol in the string if it's the first character occurrence,
+# replace it with a '1', else replace it with the amount of times you've already seen it.
+#
+# Examples:
+# input   =  "Hello, World!"
+# result  =  "1112111121311"
+#
+# input   =  "aaaaaaaaaaaa"
+# result  =  "123456789101112"
+# There might be some non-ascii characters in the string.
+#
+# Take note of performance
+#
+# PUZZLESPERFORMANCEALGORITHMS
