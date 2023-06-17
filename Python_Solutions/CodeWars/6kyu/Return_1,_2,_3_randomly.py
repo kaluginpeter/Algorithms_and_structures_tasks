6071ee3a25f036eea2dd81c39d6052eaf3de0c81
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 17:23:15 +0300

    Added description "Return 1, 2, 3 randomly"

---
+++ Python_Solutions/CodeWars/6kyu/Return_1,_2,_3_randomly.py
@@ -0,0 +1,10 @@
+# You have to create a function namedone_two (oneTwo for Java or Preloaded.
+# OneTwo for C#) that returns 1 or 2 with equal probabilities. one_two is
+# already defined in a global scope and can be called everywhere.
+#
+# Your goal is to create a function named one_two_three (oneTwoThree for Java
+# or OneTwoThree for C#) that returns 1, 2 or 3 with equal probabilities using only the one_two function.
+#
+# Do not try to cheat returning repeating non-random sequences. There is a randomness test especially for this case.
+#
+# PROBABILITYPUZZLES
