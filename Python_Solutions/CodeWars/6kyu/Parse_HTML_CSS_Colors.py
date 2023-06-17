9d313351ba5021ccd6c99b1eedf64f67dd535f9c
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 16:34:39 +0300

    Added description "Parse HTML/CSS Colors"

---
+++ Python_Solutions/CodeWars/6kyu/Parse_HTML_CSS_Colors.py
@@ -0,0 +1,26 @@
+# In this kata you parse RGB colors represented by strings.
+# The formats are primarily used in HTML and CSS. Your task is to implement a
+# function which takes a color as a string and returns the parsed color as a map (see Examples).
+#
+# Input:
+# The input string represents one of the following:
+#
+# 6-digit hexadecimal - "#RRGGBB"
+# e.g. "#012345", "#789abc", "#FFA077"
+# Each pair of digits represents a value of the channel in hexadecimal: 00 to FF
+#
+# 3-digit hexadecimal - "#RGB"
+# e.g. "#012", "#aaa", "#F5A"
+# Each digit represents a value 0 to F which translates to 2-digit hexadecimal: 0->00, 1->11, 2->22, and so on.
+#
+# Preset color name
+# e.g. "red", "BLUE", "LimeGreen"
+# You have to use the predefined map PRESET_COLORS (JavaScript, Python, Ruby), presetColors
+# (Java, C#, Haskell), or preset-colors (Clojure). The keys are the names of preset colors in
+# lower-case and the values are the corresponding colors in 6-digit hexadecimal (same as 1. "#RRGGBB").
+#
+# Examples:
+# parse_html_color('#80FFA0')   # => {'r': 128, 'g': 255, 'b': 160}
+# parse_html_color('#3B7')      # => {'r': 51,  'g': 187, 'b': 119}
+# parse_html_color('LimeGreen') # => {'r': 50,  'g': 205, 'b': 50 }
+# FUNDAMENTALSALGORITHMSSTRINGSPARSING
