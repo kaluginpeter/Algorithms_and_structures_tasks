d4cc95763e50e60623f61e77213c03dc80c8c26d
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 17:03:04 +0300

    Added solution "Prize Draw"

--- Python_Solutions/CodeWars/6kyu/Prize_Draw.py
+++ Python_Solutions/CodeWars/6kyu/Prize_Draw.py
@@ -38,3 +38,18 @@
 # See Examples Test Cases for more examples.
 #
 # FUNDAMENTALSSTRINGSSORTING
+# Solution
+def getWeight(name, weighting):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    weight = 0
+    for letter in name.lower():
+        weight += alphabet.index(letter) + 1
+    return {'name': name, 'weight': (weight + len(name))*weighting}
+def rank(st, we, n):
+    names = st.split(',')
+    if len(st) == 0:
+        return 'No participants'
+    if len(names) < n:
+        return 'Not enough participants'
+    weightings = sorted(list(map(getWeight, names, we)), key=lambda score_obj: (-score_obj['weight'], score_obj['name']))
+    return weightings[n-1]['name']
