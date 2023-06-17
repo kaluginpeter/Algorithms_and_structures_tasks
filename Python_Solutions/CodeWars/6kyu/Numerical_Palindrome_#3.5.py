87f73394cf57de9f493fe5c7faeb74734d8413d2
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 16:20:36 +0300

    Added description "Numerical Palindrome #3.5"

---
+++ Python_Solutions/CodeWars/6kyu/Numerical_Palindrome_#3.5.py
@@ -0,0 +1,29 @@
+# A palindrome is a word, phrase, number, or other sequence of characters which
+# reads the same backward as forward. Examples of numerical palindromes are: 2332, 110011, 54322345
+#
+# For a given number num, write a function which returns an array of all the numerical
+# palindromes contained within each number. The array should be sorted in ascending order
+# and any duplicates should be removed.
+#
+# In this kata, single digit numbers and numbers which start or end with zeros (such as 010 and 00)
+# are NOT considered valid numerical palindromes.
+#
+# If num contains no valid palindromes, return "No palindromes found". Otherwise, return "Not valid"
+# if the input is not an integer or is less than 0.
+#
+# Examples
+# palindrome(1221)      =>  [22, 1221]
+# palindrome(34322122)  =>  [22, 212, 343, 22122]
+# palindrome(1001331)   =>  [33, 1001, 1331]
+# palindrome(1294)      =>  "No palindromes found"
+# palindrome("1221")    =>  "Not valid"
+# Other Kata in this Series:
+# Numerical Palindrome #1
+# Numerical Palindrome #1.5
+# Numerical Palindrome #2
+# Numerical Palindrome #3
+# Numerical Palindrome #3.5
+# Numerical Palindrome #4
+# Numerical Palindrome #5
+#
+# ARRAYSFUNDAMENTALS
