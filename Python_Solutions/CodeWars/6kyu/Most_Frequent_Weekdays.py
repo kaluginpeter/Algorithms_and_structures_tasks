a0615b2465bad5e487ecad0360fa78d4785477c2
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 15:35:37 +0300

    Added solution "Most Frequent Weekdays"

--- Python_Solutions/CodeWars/6kyu/Most_Frequent_Weekdays.py
+++ Python_Solutions/CodeWars/6kyu/Most_Frequent_Weekdays.py
@@ -19,3 +19,17 @@
 # * 2185 -> ['Saturday']
 # * 2860 -> ['Thursday', 'Friday']
 # FUNDAMENTALS
+# Solution
+from datetime import date
+def most_frequent_days(year):
+    names = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    start = date(year,1,1).weekday()
+    end = date(year, 12, 31).weekday()
+    days = list(range(start, 7)) + list(range(0 ,end +1))
+    total = days.count(max(days,key=days.count))
+    result = []
+    for day in [0,1,2,3,4,5,6]:
+        if day in days:
+            if days.count(day) == total:
+                result.append(day)
+    return list(map(lambda x: names[x], result))
