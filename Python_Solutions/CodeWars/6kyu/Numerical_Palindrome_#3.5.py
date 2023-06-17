4eafb16f97d19e0267d9c20fc8947c5394abae66
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 16:20:53 +0300

    Added solution "Numerical Palindrome #3.5"

--- Python_Solutions/CodeWars/6kyu/Numerical_Palindrome_#3.5.py
+++ Python_Solutions/CodeWars/6kyu/Numerical_Palindrome_#3.5.py
@@ -27,3 +27,10 @@
 # Numerical Palindrome #5
 #
 # ARRAYSFUNDAMENTALS
+# Solution
+def palindrome(num):
+    if not isinstance(num, int) or num < 0: return "Not valid"
+    n = str(num)
+    l = len(n)
+    c = {int(n[i:j]) for i in range(l-1) for j in range(i+2, l+1) if int(n[i]) and n[i:j] == n[i:j][::-1]}
+    return sorted(c) if c else "No palindromes found"
