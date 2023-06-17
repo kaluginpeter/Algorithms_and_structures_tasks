f3ceb041aef49d6fbf8b1bb5f357d120667c34cc
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 16:39:03 +0300

    Added solution "Password generator"

--- Python_Solutions/CodeWars/6kyu/Password_generator.py
+++ Python_Solutions/CodeWars/6kyu/Password_generator.py
@@ -11,3 +11,10 @@
 # all characters should have less than 50% occurance. Based on extensive tests, the normal rate is around 35%.
 #
 # REGULAR EXPRESSIONSSECURITYFUNDAMENTALS
+# Solution
+from string import ascii_lowercase as LOWER, ascii_uppercase as UPPER, digits as DIGITS
+from random import choice, shuffle, randint
+def password_gen():
+    w = [choice(UPPER), choice(LOWER), choice(DIGITS)] + [choice(UPPER+LOWER+DIGITS) for i in range(randint(3, 17))]
+    shuffle(w)
+    return "".join(w)
