899d5f4e6ec46c2d596643b22d5458547455b331
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 16:34:55 +0300

    Added solution "Parse HTML/CSS Colors"

--- Python_Solutions/CodeWars/6kyu/Parse_HTML_CSS_Colors.py
+++ Python_Solutions/CodeWars/6kyu/Parse_HTML_CSS_Colors.py
@@ -24,3 +24,11 @@
 # parse_html_color('#3B7')      # => {'r': 51,  'g': 187, 'b': 119}
 # parse_html_color('LimeGreen') # => {'r': 50,  'g': 205, 'b': 50 }
 # FUNDAMENTALSALGORITHMSSTRINGSPARSING
+# Solution
+def parse_html_color(color):
+    color = PRESET_COLORS.get(color.lower(), color)
+    if len(color) == 7:
+        r, g, b = (int(color[i:i+2], 16) for i in range(1, 7, 2))
+    else:
+        r, g, b = (int(color[i+1]*2, 16) for i in range(3))
+    return dict(zip("rgb", (r, g, b)))
