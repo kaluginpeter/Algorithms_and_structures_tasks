2b65b2f5a8bdbb0151a39293ca5d3b3fe58182b7
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 18:27:35 +0300

    Added description "Simple Fun #64: Almost Increasing Sequence"

---
+++ Python_Solutions/CodeWars/6kyu/Simple_Fun_#64_Almost_Increasing_Sequence.py
@@ -0,0 +1,18 @@
+# Task
+# Given a sequence of integers, check whether it is possible to obtain a strictly increasing sequence by erasing no more than one element from it.
+#
+# Example
+# For sequence = [1, 3, 2, 1], the output should be false;
+#
+# For sequence = [1, 3, 2], the output should be true.
+#
+# Input/Output
+# [input] integer array sequence
+#
+# Constraints: 2 ≤ sequence.length ≤ 1000, -10000 ≤ sequence[i] ≤ 10000.
+#
+# [output] a boolean value
+#
+# true if it is possible, false otherwise.
+#
+# PUZZLES
