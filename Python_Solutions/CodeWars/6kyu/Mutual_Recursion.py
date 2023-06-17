48fcd67145890d5e11c33202a7ce59d3bad742e4
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 15:44:59 +0300

    Added solution "Mutual Recursion"

--- Python_Solutions/CodeWars/6kyu/Mutual_Recursion.py
+++ Python_Solutions/CodeWars/6kyu/Mutual_Recursion.py
@@ -13,3 +13,10 @@
 # Hofstadter Wikipedia Reference http://en.wikipedia.org/wiki/Hofstadter_sequence#Hofstadter_Female_and_Male_sequences
 #
 # MATHEMATICSALGORITHMSRECURSION
+# Solution
+def f(n):
+    if n == 0: return 1
+    return n - m(f(n-1))
+def m(n):
+    if n == 0: return 0
+    return n - f(m(n-1))
