805f07a0975c25ca6f975c7535a9313b3cfa1827
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 16:32:00 +0300

    Added description "Pairing brackets"

---
+++ Python_Solutions/CodeWars/6kyu/Pairing_brackets.py
@@ -0,0 +1,10 @@
+# Write a function which outputs the positions of matching bracket pairs.
+# The output should be a dictionary with keys the positions of the open brackets
+# '(' and values the corresponding positions of the closing brackets ')'.
+#
+# For example: input = "(first)and(second)" should return {0:6, 10:17}
+#
+# If brackets cannot be paired or if the order is invalid (e.g. ')(') return False.
+# In this kata we care only about the positions of round brackets '()', other types of brackets should be ignored.
+#
+# STRINGSPARSINGALGORITHMS
