68ef8f25331d28b03a927f647e93d6520bdae17f
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 17:56:42 +0300

    Added description "Simple Fun #156: Rotate Paper By 180 Degrees"

---
+++ Python_Solutions/CodeWars/6kyu/Simple_Fun_#156_Rotate_Paper_By_180_Degrees.py
@@ -0,0 +1,23 @@
+# Task
+# You are given a digital number written down on a sheet of paper.
+#
+# Your task is to figure out if you rotate the given sheet of paper by 180 degrees would the number still look exactly the same.
+#
+# Note: You can assume that the digital number is written like the following image:
+#
+#
+# Example
+# For number = "1", the result should be false
+#
+# For number = "29562", the result should be true.
+#
+# For number = "77", the result should be false.
+#
+# Input/Output
+# [input] string number
+# sequence of digital digits given as a string.
+#
+# [output] a boolean value
+# true if you rotate the given sheet of paper by 180 degrees then the number still look exactly the same. false otherwise.
+#
+# PUZZLES
