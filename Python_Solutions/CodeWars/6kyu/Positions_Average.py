c810ae40b00eacecb5b6465dc1085a9a401ce115
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 16:55:14 +0300

    Added solution "Positions Average"

--- Python_Solutions/CodeWars/6kyu/Positions_Average.py
+++ Python_Solutions/CodeWars/6kyu/Positions_Average.py
@@ -31,3 +31,23 @@
 # Notes
 # You can see other examples in the "Sample tests".
 # FUNDAMENTALSSTRINGS
+# Solution
+import numpy as np
+def pos_average(s):
+    s = s.replace(',','')
+    s = s.split(' ')
+    total = (len(s)*(len(s)-1))/2
+    sequence_array = []
+    for sequence in s:
+        sequence_array.append(list(sequence))
+    arr = np.array(sequence_array)
+    counter = 0
+    for i in range(0, arr.shape[1]):
+        print(arr[:,i])
+        unique, counts = np.unique(arr[:,i], return_counts=True)
+        print('unique', unique)
+        for k in range(0, len(counts)):
+            print(counts[k])
+            if counts[k] > 1:
+                counter += np.sum(np.arange(1, counts[k]))
+    return counter/(total*len(s[0]))*100
