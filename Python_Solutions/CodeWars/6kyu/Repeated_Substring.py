5ce886da3eda523c6d40d5fe5d4d3bcdca140999
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 17:20:26 +0300

    Added description "Repeated Substring"

---
+++ Python_Solutions/CodeWars/6kyu/Repeated_Substring.py
@@ -0,0 +1,29 @@
+# For a given nonempty string s find a minimum substring t and the maximum number k,
+# such that the entire string s is equal to t repeated k times.
+#
+# The input string consists of lowercase latin letters.
+#
+# Your function should return :
+#
+# a tuple (t, k) (in Python)
+# an array [t, k] (in Ruby and JavaScript)
+# in C, return k and write to the string t passed in parameter
+# Example #1:
+#
+# for string
+#
+# s = "ababab";
+# the answer is
+#
+# ("ab", 3)
+# Example #2:
+#
+# for string
+#
+# s = "abcde"
+# the answer is
+#
+# ("abcde", 1)
+# because for this string "abcde" the minimum substring t, such that s is t repeated k times, is itself.
+#
+# ALGORITHMS
