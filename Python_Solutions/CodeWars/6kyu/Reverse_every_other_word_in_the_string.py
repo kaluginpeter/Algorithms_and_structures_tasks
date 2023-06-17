a282517ba404121bed249d4b172119a00108a63c
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 17:26:19 +0300

    Added solution "Reverse every other word in the string"

--- Python_Solutions/CodeWars/6kyu/Reverse_every_other_word_in_the_string.py
+++ Python_Solutions/CodeWars/6kyu/Reverse_every_other_word_in_the_string.py
@@ -4,3 +4,11 @@
 # treated as if they are a part of the word in this kata.
 #
 # ARRAYSFUNDAMENTALS
+# Solution
+def reverse_alternate(s):
+    list = s.split()
+    for elem in list[1::2]:
+        word = elem[::-1]
+        list.insert(list.index(elem), word)
+        list.remove(elem)
+    return ' '.join(list)
