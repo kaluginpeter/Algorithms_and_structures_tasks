79a48ab3f06c3bc46f29a63091aed351245766fe
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 16:38:45 +0300

    Added description "Password generator"

---
+++ Python_Solutions/CodeWars/6kyu/Password_generator.py
@@ -0,0 +1,13 @@
+# You need to write a password generator that meets the following criteria:
+#
+# 6 - 20 characters long
+# contains at least one lowercase letter
+# contains at least one uppercase letter
+# contains at least one number
+# contains only alphanumeric characters (no special characters)
+# Return the random password as a string.
+#
+# Note: "randomness" is checked by counting the characters used in the generated passwords -
+# all characters should have less than 50% occurance. Based on extensive tests, the normal rate is around 35%.
+#
+# REGULAR EXPRESSIONSSECURITYFUNDAMENTALS
