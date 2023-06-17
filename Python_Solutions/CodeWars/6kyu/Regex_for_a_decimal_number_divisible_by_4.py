f3f5e15af6000585bc6625de04e964c6b9f89ece
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 17:16:42 +0300

    Added description "Regex for a decimal number divisible by 4"

---
+++ Python_Solutions/CodeWars/6kyu/Regex_for_a_decimal_number_divisible_by_4.py
@@ -0,0 +1,9 @@
+# Your task is to write a regular expression that matches positive decimal integers
+# divisible by 4. Negative numbers should be rejected, but leading zeroes are permitted.
+#
+# Random tests can consist of numbers, ascii letters, some puctuation and brackets.
+# But no need to check for line breaks (\n) and non-ASCII chatracters, nothing that fancy in the tests.
+#
+# There is 50 characters limit for this regex to avoid hardcoding and keep the "puzzle" status :) Good luck!
+#
+# REGULAR EXPRESSIONSPUZZLES
