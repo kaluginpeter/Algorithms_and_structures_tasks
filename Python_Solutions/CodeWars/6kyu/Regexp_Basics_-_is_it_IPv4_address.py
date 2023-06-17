496d28cb4d5ec881029fb83a3b160a2d55d22b47
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 17:17:41 +0300

    Added description "Regexp Basics - is it IPv4 address?"

---
+++ Python_Solutions/CodeWars/6kyu/Regexp_Basics_-_is_it_IPv4_address.py
@@ -0,0 +1,6 @@
+# Implement String#ipv4_address?, which should return true if given object is an IPv4 address
+# - four numbers (0-255) separated by dots.
+#
+# It should only accept addresses in canonical representation, so no leading 0s, spaces etc.
+#
+# REGULAR EXPRESSIONSFUNDAMENTALS
