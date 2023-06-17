06a3ccf590d4793a456f9b2183c8b427a4b5d54a
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 17:14:14 +0300

    Added solution "Rectangle into Squares"

--- Python_Solutions/CodeWars/6kyu/Rectangle_into_Squares.py
+++ Python_Solutions/CodeWars/6kyu/Rectangle_into_Squares.py
@@ -33,3 +33,17 @@
 # Return the string "nil" with Bash, PowerShell, Pascal and Fortran.
 #
 # FUNDAMENTALSGEOMETRYPUZZLES
+# Solution
+def sqInRect(lng, wdth):
+    if lng == wdth:
+        return None
+    if lng < wdth:
+        wdth, lng = lng, wdth
+    res = []
+    while lng != wdth:
+        res.append(wdth)
+        lng = lng - wdth
+        if lng < wdth:
+            wdth, lng = lng, wdth
+    res.append(wdth)
+    return res
