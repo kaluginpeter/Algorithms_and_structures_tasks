e78ad285e5d98b761c4d960789d870e74dada379
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 17:42:07 +0300

    Added description "Sequences and Series"

---
+++ Python_Solutions/CodeWars/6kyu/Sequences_and_Series.py
@@ -0,0 +1,14 @@
+# Have a look at the following numbers.
+#
+#  n | score
+# ---+-------
+#  1 |  50
+#  2 |  150
+#  3 |  300
+#  4 |  500
+#  5 |  750
+# Can you find a pattern in it? If so, then write a function getScore(n)/get_score(n)/GetScore(n) which returns the score for any positive number n.
+#
+# Note Real test cases consists of 100 random cases where 1 <= n <= 10000
+#
+# MATHEMATICSALGORITHMSPUZZLES
