6871b64760ba679f80f9ba45d9278ed52c8d0384
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 15:53:12 +0300

    Added description "Next multiple of 5"

---
+++ Python_Solutions/CodeWars/6kyu/Next_multiple_of_5.py
@@ -0,0 +1,25 @@
+# Task
+# Write a function that receives a non-negative integer n ( n >= 0 )
+# and returns the next higher multiple of five of that number, obtained
+# by concatenating the shortest possible binary string to the end of this number's binary representation.
+#
+# Examples
+# 1.  next_multiple_of_five(8)
+# Steps:
+#
+# 8 to binary == '1000'
+# concatenate shortest possible string '11' to obtain '1000' + '11' == '100011'
+# '100011' to decimal == 35 => the answer
+# ('001' would do the job too, but '11' is the shortest string here)
+#
+# 2.  next_multiple_of_five(5)
+# Steps:
+#
+# 5 to binary =='101'
+# concatenate shortest possible string '0' to obtain '101' + '0' == '1010'
+# '1010' to decimal == 10
+# (5 is already a multiple of 5, obviously, but we're looking for the next multiple of 5)
+#
+# Note
+# Numbers up to 1e10 will be tested, so you need to come up with something smart.
+# ALGORITHMSLOGICPERFORMANCE
