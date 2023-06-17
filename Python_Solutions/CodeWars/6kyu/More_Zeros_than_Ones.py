0326e371229e4d58d28bc68cefc981d4c162a9d2
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 15:34:26 +0300

    Added description "More Zeros than Ones"

---
+++ Python_Solutions/CodeWars/6kyu/More_Zeros_than_Ones.py
@@ -0,0 +1,18 @@
+# Create a moreZeros function which will receive a string for input, and return an array
+# (or null terminated string in C) containing only the characters from that string whose
+# binary representation of its ASCII value consists of more zeros than ones.
+#
+# You should remove any duplicate characters, keeping the first occurrence of any such duplicates,
+# so they are in the same order in the final array as they first appeared in the input string.
+#
+# Examples
+#
+# 'abcde' === ["1100001", "1100010", "1100011", "1100100", "1100101"]
+#                True       True       False      True       False
+#
+#         --> ['a','b','d']
+#
+# 'DIGEST'--> ['D','I','E','T']
+# All input will be valid strings of length > 0. Leading zeros in binary should not be counted.
+#
+# FUNDAMENTALS
