4c0bf46be30fd7bb6967c2b08f6d98397fb6b24a
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 17:41:13 +0300

    Added description "Sequence generator"

---
+++ Python_Solutions/CodeWars/6kyu/Sequence_generator.py
@@ -0,0 +1,30 @@
+# Write a generator sequence_gen ( sequenceGen in JavaScript) that, given the first terms of a sequence will generate a (potentially) infinite amount of terms, where each subsequent term is the sum of the previous x terms where x is the amount of initial arguments (examples of such sequences are the Fibonacci, Tribonacci and Lucas number sequences).
+#
+# ##Examples
+#
+# fib = sequence_gen(0, 1)
+# next(fib) = 0 # first term (provided)
+# next(fib) = 1 # second term (provided)
+# next(fib) = 1 # third term (sum of first and second terms)
+# next(fib) = 2 # fourth term (sum of second and third terms)
+# next(fib) = 3 # fifth term (sum of third and fourth terms)
+# next(fib) = 5 # sixth term (sum of fourth and fifth terms)
+# next(fib) = 8 # seventh term (sum of fifth and sixth terms)
+#
+# trib = sequence_gen(0,1,1)
+# next(trib) = 0 # first term (provided)
+# next(trib) = 1 # second term (provided)
+# next(trib) = 1 # third term (provided)
+# next(trib) = 2 # fourth term (sum of first, second and third terms)
+# next(trib) = 4 # fifth term (sum of second, third and fourth terms)
+# next(trib) = 7 # sixth term (sum of third, fourth and fifth terms)
+#
+# lucas = sequence_gen(2,1)
+# [next(lucas) for _ in range(10)] = [2, 1, 3, 4, 7, 11, 18, 29, 47, 76]
+# Note: You can assume you will get at least one argument and any arguments given will be valid (positive or negative integers) so no error checking is needed.
+#
+# **Note for Ruby users: ** sequence_gen should return an Enumerator object.
+#
+# Any feedback/suggestions would much appreciated.
+#
+# FUNDAMENTALS
