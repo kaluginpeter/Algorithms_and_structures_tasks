9a6f365b695b14cd034335e0396b3c33adc01a24
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 16:30:26 +0300

    Added description "Padovan numbers"

---
+++ Python_Solutions/CodeWars/6kyu/Padovan_numbers.py
@@ -0,0 +1,13 @@
+# The Padovan sequence is the sequence of integers defined by the initial values
+#
+# P(0) = P(1) = P(2) = 1
+# and the recurrence relation
+#
+# P(n) = P(n-2) + P(n-3)
+# The first few values of P(n) are:
+#
+# 1, 1, 1, 2, 2, 3, 4, 5, 7, 9, 12, 16, 21, 28, 37, 49, 65, 86, 114, 151, 200, 265, ...
+# Task
+# Your task is to write a method that returns nth Padovan number
+#
+# ALGORITHMS
