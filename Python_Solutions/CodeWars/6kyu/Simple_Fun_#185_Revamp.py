75bf4eb7c850af9eebdebfb4413ef5828c016d37
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 18:02:54 +0300

    Added description "Simple Fun #185: Revamp"

---
+++ Python_Solutions/CodeWars/6kyu/Simple_Fun_#185_Revamp.py
@@ -0,0 +1,26 @@
+# Task
+# Consider a string of lowercase Latin letters and space characters (" ").
+#
+# First, rearrange the letters in each word alphabetically.
+#
+# And then rearrange the words in ascending order of the sum of their characters' ASCII values.
+#
+# If two or more words have the same ASCII value, rearrange them by their length in ascending order; If their length still equals to each other, rearrange them alphabetically.
+#
+# Finally, return the result.
+#
+# Example
+# For s = "batman is bruce wayne", the result should be "is bceru aenwy aamntb".
+#
+#   After rearranging the letters the string turns into "aamntb is bceru aenwy". The ASCII values of each word are: [627, 220, 529, 548]. After sorting the words the following string is obtained: "is bceru aenwy aamntb" (with ASCII values of [220, 529, 548, 627]).
+#
+# For s = "peter parker is spiderman", the result should be "is eeprt aekprr adeimnprs"
+#
+# (ASCII values: [220, 554, 645, 963])
+#
+# Input/Output
+# [input] string s
+# A string of lowercase words. Each word is separated by exactly one space character.
+#
+# [output] a string
+# FUNDAMENTALS
