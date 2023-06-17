342e3c352d3cad6d2aa89e07292aa6fd1d6ec033
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 17:37:00 +0300

    Added description "Scrabble best word"

---
+++ Python_Solutions/CodeWars/6kyu/Scrabble_best_word.py
@@ -0,0 +1,13 @@
+# You're playing to scrabble. But counting points is hard.
+#
+# You decide to create a little script to calculate the best possible value.
+#
+# The function takes two arguments :
+#
+# `points` : an array of integer representing for each letters from A to Z the points that it pays
+# `words` : an array of strings, uppercase
+#
+# You must return the index of the shortest word which realize the highest score.
+# If the length and the score are the same for two elements, return the index of the first one.
+#
+# ARRAYSALGORITHMS
