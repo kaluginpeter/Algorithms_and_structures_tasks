04c9ed987486d3a19cfddbb75fafb748a17b502e
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 16:31:14 +0300

    Added description "Pair of gloves"

---
+++ Python_Solutions/CodeWars/6kyu/Pair_of_gloves.py
@@ -0,0 +1,15 @@
+# Pair of gloves
+# Winter is coming, you must prepare your ski holidays.
+# The objective of this kata is to determine the number of pair
+# of gloves you can constitute from the gloves you have in your drawer.
+#
+# Given an array describing the color of each glove, return the number
+# of pairs you can constitute, assuming that only gloves of the same color can form pairs.
+#
+# Examples:
+# input = ["red", "green", "red", "blue", "blue"]
+# result = 2 (1 red pair + 1 blue pair)
+#
+# input = ["red", "red", "red", "red", "red", "red"]
+# result = 3 (3 red pairs)
+# ARRAYSPUZZLES
