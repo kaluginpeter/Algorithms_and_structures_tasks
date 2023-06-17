754fdbbd4a34f13acc50ecf7e7becb7230873138
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 17:19:52 +0300

    Added solution "Remove the parentheses"

--- Python_Solutions/CodeWars/6kyu/Remove_the_parentheses.py
+++ Python_Solutions/CodeWars/6kyu/Remove_the_parentheses.py
@@ -13,3 +13,9 @@
 # There can be multiple parentheses.
 # The parentheses can be nested.
 # STRINGSALGORITHMSREGULAR EXPRESSIONSFUNDAMENTALS
+# Solution
+import re
+def remove_parentheses(s):
+    while re.findall(r"\([^()]*\)", s):
+        s = re.sub(r"\([^()]*\)", "", s)
+    return s
