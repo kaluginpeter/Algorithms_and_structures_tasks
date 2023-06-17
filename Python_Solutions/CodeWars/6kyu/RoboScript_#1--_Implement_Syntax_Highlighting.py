703d69d17cb88e53718e618a874e85239659b3b3
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 17:28:34 +0300

    Added solution "RoboScript #1 - Implement Syntax Highlighting"

--- Python_Solutions/CodeWars/6kyu/RoboScript_#1--_Implement_Syntax_Highlighting.py
+++ Python_Solutions/CodeWars/6kyu/RoboScript_#1--_Implement_Syntax_Highlighting.py
@@ -34,3 +34,11 @@
 # RoboScript #4 - RS3 Patterns to the Rescue
 # RoboScript #5 - The Final Obstacle (Implement RSU)
 # FUNDAMENTALS
+# Solution
+import re
+def highlight(code):
+    code = re.sub(r"(F+)", '<span style="color: pink">\g<1></span>', code)
+    code = re.sub(r"(L+)", '<span style="color: red">\g<1></span>', code)
+    code = re.sub(r"(R+)", '<span style="color: green">\g<1></span>', code)
+    code = re.sub(r"(\d+)", '<span style="color: orange">\g<1></span>', code)
+    return code
