1c3485fb24419c189ca4040d5f93e0d8113c1eb6
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 15:57:15 +0300

    Added description "Number Format"

---
+++ Python_Solutions/CodeWars/6kyu/Number_Format.py
@@ -0,0 +1,8 @@
+# Format any integer provided into a string with "," (commas) in the correct places.
+#
+# Example:
+#
+# For n = 100000 the function should return '100,000';
+# For n = 5678545 the function should return '5,678,545';
+# for n = -420902 the function should return '-420,902'.
+# REGULAR EXPRESSIONSFUNDAMENTALS
