daac13b96c5846d12a0180e1b4e906f76a6028e9
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 17:09:24 +0300

    Added solution "Rainfall"

--- Python_Solutions/CodeWars/6kyu/Rainfall.py
+++ Python_Solutions/CodeWars/6kyu/Rainfall.py
@@ -27,3 +27,29 @@
 # are adapted from: http://www.worldclimate.com
 #
 # FUNDAMENTALSSTRINGS
+# Solution
+import re
+def mean(town, strng):
+    data_split = re.findall(r'.+(?:\n|$)', strng)
+    for counter, town_info in enumerate(data_split):
+        if town in town_info:
+            town_name = re.match(r'\w+', town_info)
+            if town != town_name.group():
+                continue
+            numbers = re.findall(r'\d+\.?\d+', town_info)
+            float_numbers = [float(x) for x in numbers]
+            return sum(float_numbers)/len(float_numbers)
+    return -1
+def variance(town, strng):
+    data_split = re.findall(r'.+(?:\n|$)', strng)
+    for counter, town_info in enumerate(data_split):
+        if town in town_info:
+            town_name = re.match(r'\w+', town_info)
+            if town != town_name.group():
+                continue
+            numbers = re.findall(r'\d+\.?\d+', town_info)
+            float_numbers = [float(x) for x in numbers]
+            mean = sum(float_numbers)/len(float_numbers)
+            squared_nums = [(x-mean)**2 for x in float_numbers]
+            return sum(squared_nums)/(len(squared_nums))
+    return -1
