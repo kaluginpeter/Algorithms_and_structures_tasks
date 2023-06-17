049566545c6386e73c869e542734c5445da1ef7c
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 16:41:28 +0300

    Added description "Perfect Square."

---
+++ Python_Solutions/CodeWars/6kyu/Perfect_Square..py
@@ -0,0 +1,25 @@
+# Task
+# Write function which validates an input string. If the string is a perfect square return true,false otherwise.
+#
+# What is perfect square?
+# * We assume that character '.' (dot) is a perfect square (1x1) * Perfect squares can only
+# contain '.' (dot) and optionally '\n' (line feed) characters.
+# * Perfect squares must have same width and height -> cpt.Obvious
+# * Squares of random sizes will be tested!
+# Function input:
+# perfectSquare = "...\n...\n...";
+#
+# // This represents the following Perfect Square:
+#
+# `...
+#  ...
+#  ...`
+#
+# notPerfect = "..,\n..\n...";
+#
+# // This is not a Perfect Square:
+#
+# `..,
+#  ..
+#  ...`
+# REGULAR EXPRESSIONSFUNDAMENTALS
