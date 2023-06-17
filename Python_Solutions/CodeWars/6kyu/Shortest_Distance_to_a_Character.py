6a0ad682ce06ac45c7078695103f07322e912daa
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 17:43:39 +0300

    Added description "Shortest Distance to a Character"

---
+++ Python_Solutions/CodeWars/6kyu/Shortest_Distance_to_a_Character.py
@@ -0,0 +1,25 @@
+# Given a string s and a character c, return an array of integers representing the shortest distance from the current character in s to c.
+#
+# Notes
+# All letters will be lowercase.
+# If the string is empty, return an empty array.
+# If the character is not present, return an empty array.
+# Examples
+# s = "lovecodewars"
+# c = "e"
+# result = [3, 2, 1, 0, 1, 2, 1, 0, 1, 2, 3, 4]
+#
+# s = "aaaabbbb"
+# c = "b"
+# result = [4, 3, 2, 1, 0, 0, 0, 0]
+#
+# s = ""
+# c = "b"
+# result = []
+#
+# s = "abcde"
+# c = ""
+# result = []
+# If you liked it, please rate :D
+#
+# STRINGSALGORITHMS
