49921c01bfec522d29e3bd7973f62a55b51d202a
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 17:45:17 +0300

    Added description "Simple array rotation"

---
+++ Python_Solutions/CodeWars/6kyu/Simple_array_rotation.py
@@ -0,0 +1,20 @@
+# In this Kata, you will be given an array and your task will be to determine if an array is in ascending or descending order and if it is rotated or not.
+#
+# Consider the array [1,2,3,4,5,7,12]. This array is sorted in Ascending order. If we rotate this array once to the left, we get [12,1,2,3,4,5,7] and twice-rotated we get [7,12,1,2,3,4,5]. These two rotated arrays are in Rotated Ascending order.
+#
+# Similarly, the array [9,6,5,3,1] is in Descending order, but we can rotate it to get an array in Rotated Descending order: [1,9,6,5,3] or [3,1,9,6,5] etc.
+#
+# Arrays will never be unsorted, except for those that are rotated as shown above. Arrays will always have an answer, as shown in the examples below. Arrays will never contain duplicated elements.
+#
+# More examples:
+#
+# solve([1,2,3,4,5,7]) = "A" -- Ascending
+# solve([7,1,2,3,4,5]) = "RA" -- Rotated ascending
+# solve([4,5,6,1,2,3]) = "RA" -- Rotated ascending
+# solve([9,8,7,6]) = "D" -- Descending
+# solve([5,9,8,7,6]) = "RD" -- Rotated Descending
+# More examples in the test cases.
+#
+# Good luck!
+#
+# ARRAYSALGORITHMS
