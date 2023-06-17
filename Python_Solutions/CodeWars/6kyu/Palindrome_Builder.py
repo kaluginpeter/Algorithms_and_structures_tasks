07016437cbb5d6815179d3be2569847e71f33b11
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 16:33:43 +0300

    Added description "Palindrome Builder"

---
+++ Python_Solutions/CodeWars/6kyu/Palindrome_Builder.py
@@ -0,0 +1,14 @@
+# Task
+# Given a string, add the fewest number of characters possible from the front or back to make it a palindrome.
+#
+# Example
+# For the input cdcab, the output should be bacdcab
+#
+# Input/Output
+# Input is a string consisting of lowercase latin letters with length 3 <= str.length <= 10
+#
+# The output is a palindrome string satisfying the task.
+#
+# For s = ab either solution (aba or bab) will be accepted.
+#
+# ALGORITHMS
