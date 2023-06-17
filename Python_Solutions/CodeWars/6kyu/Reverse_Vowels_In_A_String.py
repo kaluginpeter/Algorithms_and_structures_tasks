8304aa4970d156bf996bef3859a58c1fffcb5715
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 17:25:09 +0300

    Added description "Reverse Vowels In A String"

---
+++ Python_Solutions/CodeWars/6kyu/Reverse_Vowels_In_A_String.py
@@ -0,0 +1,11 @@
+# In this kata, your goal is to write a function which will reverse the vowels in a string.
+# Any characters which are not vowels should remain in their original position. Here are some examples:
+#
+# "Hello!" => "Holle!"
+# "Tomatoes" => "Temotaos"
+# "Reverse Vowels In A String" => "RivArsI Vewols en e Streng"
+# For simplicity, you can treat the letter y as a consonant, not a vowel.
+#
+# Good luck!
+#
+# STRINGSFUNDAMENTALS
