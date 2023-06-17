f86853609347fc9e5afe7c1547b71543173e6b0b
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 16:25:04 +0300

    Added description "Odd/Even number of divisors"

---
+++ Python_Solutions/CodeWars/6kyu/Odd_Even_number_of_divisors.py
@@ -0,0 +1,12 @@
+# Given an integer n return "odd" if the number of its divisors is odd. Otherwise return "even".
+#
+# Note: big inputs will be tested.
+#
+# Examples:
+# All prime numbers have exactly two divisors (hence "even").
+#
+# For n = 12 the divisors are [1, 2, 3, 4, 6, 12] – "even".
+#
+# For n = 4 the divisors are [1, 2, 4] – "odd".
+#
+# MATHEMATICSPERFORMANCEALGORITHMS
