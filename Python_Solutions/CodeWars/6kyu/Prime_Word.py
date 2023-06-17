30801e6ae1feac004bdf2a50736a0d13869c4273
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 17:00:02 +0300

    Added solution "Prime Word"

--- Python_Solutions/CodeWars/6kyu/Prime_Word.py
+++ Python_Solutions/CodeWars/6kyu/Prime_Word.py
@@ -13,3 +13,12 @@
 # After adding 30 the values will be: 99 139 139 127
 # As 139 is prime number so "Emma" is a Prime Word.
 # FUNDAMENTALS
+# Solution
+from gmpy2 import is_prime
+def prime_word(array):
+    a = [ord(i) + array[0][1] for i in array[0][0]]
+    b = [ord(i) + array[1][1] for i in array[1][0]]
+    if len(array) == 3:
+        c = [ord(i) + array[2][1] for i in array[2][0]]
+    f = [1 if any(is_prime(i) for i in a) else 0, 1 if any(is_prime(i) for i in b) else 0]
+    return f if len(array) ==2 else f + [1 if any(is_prime(i) for i in c) else 0]
