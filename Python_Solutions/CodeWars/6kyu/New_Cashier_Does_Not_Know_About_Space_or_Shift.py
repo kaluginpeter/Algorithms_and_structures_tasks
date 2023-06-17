13407dc88e2ed2b47fbeef08b9a7c181d0c4e82b
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 15:51:46 +0300

    Added solution "New Cashier Does Not Know About Space or Shift"

--- Python_Solutions/CodeWars/6kyu/New_Cashier_Does_Not_Know_About_Space_or_Shift.py
+++ Python_Solutions/CodeWars/6kyu/New_Cashier_Does_Not_Know_About_Space_or_Shift.py
@@ -25,3 +25,7 @@
 # 7. Milkshake
 # 8. Coke
 # STRINGSFUNDAMENTALS
+# Solution
+def get_order(order):
+    menu = ["Burger", "Fries", "Chicken", "Pizza", "Sandwich", "Onionrings", "Milkshake", "Coke"]
+    return "".join([(i + " ") * order.count(i.lower()) for i in menu if i.lower() in order]).rstrip()
