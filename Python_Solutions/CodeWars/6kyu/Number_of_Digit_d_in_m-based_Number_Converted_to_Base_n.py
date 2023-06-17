85213e117d66181166659aa4583fc8295886e4b1
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 16:00:01 +0300

    Added description "Number of Digit d in m-based Number Converted to Base n"

---
+++ Python_Solutions/CodeWars/6kyu/Number_of_Digit_d_in_m-based_Number_Converted_to_Base_n.py
@@ -0,0 +1,31 @@
+# You have a number x in base m (xm). Count the number of digits d after converting xm to base n.
+#
+# Representation of numbers
+# base = 2: 0, 1
+# base = 3: 0, 1, 2
+# ...
+# base = 10: 0, 1, ... 8, 9
+# base = 11: 0 ... , 8, 9, a
+# base = 12: 0 ... , 8, 9, a, b
+# ...
+# base = 16: 0 ... , 8, 9, a, ..., e, f
+# base = 36: 0 ..., 8, 9, a, ..., y, z
+# Preconditions
+# 2 <= m <= 36
+# 2 <= n <= 36
+# xm is always a valid non-negative m-based number.
+# d is always a valid digit for base n
+# Examples
+# # Function defintion
+#
+# count_digit(number, digit, base=10, from_base=10)
+#
+# # number    -> xm (str)
+# # digit     -> d  (str)
+# # base      -> n  (int)
+# # from_base -> m  (int)
+#
+# count_digit("133", "3") == 2
+# count_digit("10", "a", base=11) == 1
+# count_digit("1100101110101", "d", base=15, from_base=2) == 1
+# FUNDAMENTALS
