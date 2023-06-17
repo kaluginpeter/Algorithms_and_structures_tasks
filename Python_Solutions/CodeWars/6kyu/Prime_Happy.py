0fd16cf9aba3215a88c50a4befd9f4a1fbab379a
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 16:58:09 +0300

    Added solution "Prime Happy"

--- Python_Solutions/CodeWars/6kyu/Prime_Happy.py
+++ Python_Solutions/CodeWars/6kyu/Prime_Happy.py
@@ -3,3 +3,8 @@
 # Write isPrimeHappy(n) which returns true if n is prime happy else false.
 #
 # FUNDAMENTALS
+# Solution
+from gmpy2 import is_prime
+def is_prime_happy(n):
+    if n < 5: return False
+    return sum([i for i in range(2, n) if is_prime(i)]) % n == 0
