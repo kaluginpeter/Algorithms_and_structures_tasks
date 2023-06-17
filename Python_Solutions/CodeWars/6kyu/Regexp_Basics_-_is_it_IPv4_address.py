669d25f1518d6782d0738131cf200beb44812a76
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 17:17:57 +0300

    Added solution "Regexp Basics - is it IPv4 address?"

--- Python_Solutions/CodeWars/6kyu/Regexp_Basics_-_is_it_IPv4_address.py
+++ Python_Solutions/CodeWars/6kyu/Regexp_Basics_-_is_it_IPv4_address.py
@@ -4,3 +4,20 @@
 # It should only accept addresses in canonical representation, so no leading 0s, spaces etc.
 #
 # REGULAR EXPRESSIONSFUNDAMENTALS
+# Solution
+import re
+def ipv4_address(address):
+    regex = re.compile(r'''
+    #      200-249   | 250-255 | 100-109| 0-99 110-199
+    \b([2]([0-4][0-9]|[5][0-5])|(10[0-9]|[1]?[1-9]?[0-9]))\.
+    ([2]([0-4][0-9]|[5][0-5])|(10[0-9]|[1]?[1-9]?[0-9]))\.
+    ([2]([0-4][0-9]|[5][0-5])|(10[0-9]|[1]?[1-9]?[0-9]))\.
+    ([2]([0-4][0-9]|[5][0-5])|(10[0-9]|[1]?[1-9]?[0-9]))\b    
+    ''', re.VERBOSE)
+    mo = regex.match(address)
+    if mo is None:
+        return False
+    elif  mo.group() == address:
+        return (bool(mo))
+    else:
+        return False
