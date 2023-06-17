26539bee7ff9beef69dc3972cbcb17b6322cb8e5
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 17:31:40 +0300

    Added solution "Round and Round"

--- Python_Solutions/CodeWars/6kyu/Round_and_Round.py
+++ Python_Solutions/CodeWars/6kyu/Round_and_Round.py
@@ -5,3 +5,7 @@
 # Quick, fix the bug before everyone in CodeWars notices that you can't even round a number correctly!
 #
 # FUNDAMENTALSDEBUGGING
+# Solution
+from decimal import Decimal, ROUND_HALF_UP
+def round_by_2_decimal_places(n):
+    return n.quantize(Decimal('.01'), rounding = ROUND_HALF_UP)
