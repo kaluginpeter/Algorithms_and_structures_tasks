6eb514d5cf58e6ad432248645b3931a360012eae
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 17:19:36 +0300

    Added description "Remove the parentheses"

---
+++ Python_Solutions/CodeWars/6kyu/Remove_the_parentheses.py
@@ -0,0 +1,15 @@
+# Remove the parentheses
+# In this kata you are given a string for example:
+#
+# "example(unwanted thing)example"
+# Your task is to remove everything inside the parentheses as well as the parentheses themselves.
+#
+# The example above would return:
+#
+# "exampleexample"
+# Notes
+# Other than parentheses only letters and spaces can occur in the string.
+# Don't worry about other brackets like "[]" and "{}" as these will never appear.
+# There can be multiple parentheses.
+# The parentheses can be nested.
+# STRINGSALGORITHMSREGULAR EXPRESSIONSFUNDAMENTALS
