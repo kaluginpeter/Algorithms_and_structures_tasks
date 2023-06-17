6ec5e2c5595a9051adc61f941fd29be8f5791f8a
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 16:29:57 +0300

    Added solution "PI approximation"

--- Python_Solutions/CodeWars/6kyu/PI_approximation.py
+++ Python_Solutions/CodeWars/6kyu/PI_approximation.py
@@ -33,3 +33,15 @@
 # About PI : http://www.geom.uiuc.edu/~huberty/math5337/groupe/expresspi.html
 #
 # ARRAYSMATHEMATICSFUNDAMENTALS
+# Solution
+from math import pi
+def iter_pi(epsilon):
+    count = 1
+    my_pi = 4.0
+    while abs(pi - my_pi) > epsilon:
+        if count % 2:
+            my_pi -= (1.0 / (count * 2 + 1)) * 4
+        else:
+            my_pi += (1.0 / (count * 2 + 1)) * 4
+        count += 1
+    return [count, round(my_pi, 10)]
