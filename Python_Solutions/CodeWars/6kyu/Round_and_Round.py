2c43cb45eff47af8ce34a6f4621c5bfeab86d37e
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-17 17:31:22 +0300

    Added description "Round and Round"

---
+++ Python_Solutions/CodeWars/6kyu/Round_and_Round.py
@@ -0,0 +1,7 @@
+# Since there are lots of katas requiring you to round numbers to 2 decimal places, you decided to extract the method to ease out the process.
+#
+# And you can't even get this right!
+#
+# Quick, fix the bug before everyone in CodeWars notices that you can't even round a number correctly!
+#
+# FUNDAMENTALSDEBUGGING
