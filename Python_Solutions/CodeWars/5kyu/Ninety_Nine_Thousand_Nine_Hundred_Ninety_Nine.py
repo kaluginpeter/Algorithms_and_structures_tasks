8c8626d8caf9bafbb8a1e49a2e8f800e878b7d24
kaluginpeter <streetworkout200412@gmail.com>
2023-06-18 13:58:01 +0300

    Added description "Ninety Nine Thousand Nine Hundred Ninety Nine"

---
+++ Python_Solutions/CodeWars/5kyu/Ninety_Nine_Thousand_Nine_Hundred_Ninety_Nine.py
@@ -0,0 +1,11 @@
+# Write a method that takes a number and returns a string of that number in English.
+#
+# Your method should be able to handle any number between 0 and 99999. If the given number is outside of that range or not an integer, the method should return an empty string.
+#
+# Examples
+# 0      -->  "zero"
+# 27     -->  "twenty seven"
+# 100    -->  "one hundred"
+# 7012   -->  "seven thousand twelve"
+# 99205  -->  "ninety nine thousand two hundred five"
+# STRINGSPARSINGALGORITHMS
