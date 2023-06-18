a4c90bd2bab2d3ca4ad37a0ee5ad8f68de94c382
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-19 00:21:36 +0300

    Added description "Computing the complex logarithm function"

---
+++ Python_Solutions/CodeWars/6kyu/Computing_the_complex_logarithm_function.py
@@ -0,0 +1,5 @@
+# Compute the complex logarithm at any given complex number, accurate to at least 1 in 10^-12. The imaginary part should be inside the interval (−π, π] (i.e if the imaginary part is exactly π, keep it as is).
+#
+# Note: You shouldn't try to compute the value of this function at the poles. Please return null/NULL/nil/None (C#: throw an ArgumentException, Java: throw an ArithmeticException) if this happens.
+#
+# MATHEMATICSALGORITHMS
