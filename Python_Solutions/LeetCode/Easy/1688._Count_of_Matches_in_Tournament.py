39759a5b7e1b75075ec5119f3314a041aa9c212a
kaluginpeter <streetworkout200412@gmail.com>
2023-06-19 16:19:11 +0300

    Added solution "1688. Count of Matches in Tournament"

--- Python_Solutions/LeetCode/Easy/1688._Count_of_Matches_in_Tournament.py
+++ Python_Solutions/LeetCode/Easy/1688._Count_of_Matches_in_Tournament.py
@@ -30,3 +30,15 @@
 # Constraints:
 #
 # 1 <= n <= 200
+# Solution
+class Solution:
+    def numberOfMatches(self, n: int) -> int:
+        count = 0
+        while n != 1:
+            if n % 2 == 0:
+                n = n / 2
+                count += n
+                continue
+            n = (n - 1) / 2
+            count += n + 1
+        return int(count)
