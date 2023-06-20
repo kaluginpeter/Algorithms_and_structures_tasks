7b0ca65776142ba2f98a4fe7b8e2e31729636d9b
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-20 20:08:27 +0300

    Added description "Fibonacci Streaming"

---
+++ Python_Solutions/CodeWars/5kyu/Fibonacci_Streaming.py
@@ -0,0 +1,8 @@
+# You're going to provide a needy programmer a utility method that generates an infinite amount of sequential fibonacci numbers.
+#
+# to do this write a 'generator' starting with 1
+#
+# A fibonacci sequence starts with two 1s. Every element afterwards is the sum of the two previous elements. See:
+#
+# 1, 1, 2, 3, 5, 8, 13, ..., 89, 144, 233, 377, ...
+# ALGORITHMS
