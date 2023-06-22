c18ea798f71f12d37b7a1c667eca5fd862d23b9b
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-22 08:12:06 +0300

    Added solution "342. Power of Four"

--- Python_Solutions/LeetCode/Easy/342._Power_of_Four.py
+++ Python_Solutions/LeetCode/Easy/342._Power_of_Four.py
@@ -24,3 +24,12 @@
 #
 #
 # Follow up: Could you solve it without loops/recursion?
+# Solution
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        i = 0
+        while 4 ** i <= n:
+            if 4 ** i == n:
+                return True
+            i += 1
+        return False
