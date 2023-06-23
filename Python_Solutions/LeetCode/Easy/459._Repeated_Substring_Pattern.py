b30bc486ead1a079a1148e765798dd0a9af414aa
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-23 20:16:59 +0300

    Added solution "459. Repeated Substring Pattern"

--- Python_Solutions/LeetCode/Easy/459._Repeated_Substring_Pattern.py
+++ Python_Solutions/LeetCode/Easy/459._Repeated_Substring_Pattern.py
@@ -22,3 +22,51 @@
 #
 # 1 <= s.length <= 104
 # s consists of lowercase English letters.
+# Solution
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        # Solution 1
+        return s in s[1:] + s[:-1]
+        # Solution 2
+        l = len(s)
+        j = l
+        for i in range(l // 2):
+            while len(s[:i+1] * j) >= l:
+                j -= 1
+            if s[:i+1] * (j+1) == s:
+                return True
+        return False
+        # Solution 3
+        def kmp_table(W):
+            n = len(W)
+            T = [0 for _ in range(n)]
+            T[0] = -1
+            pos = 0
+            for i in range(1, n):
+                if W[i] == W[pos]:
+                    T[i] = T[pos]
+                else:
+                    T[i] = pos
+                    pos = T[pos]
+                    while pos >= 0 and W[i] != W[pos]:
+                        pos = T[pos]
+                pos += 1
+            return T
+        string, substring = s[1:] + s[:-1], s
+        m = len(substring)
+        n = len(string)
+        T = kmp_table(substring)
+        pos = 0
+        i = 0
+        while i < n:
+            if string[i] != substring[pos]:
+                pos = T[pos]
+                if pos < 0:
+                    pos += 1
+                    i += 1
+            else:
+                i += 1
+                pos += 1
+                if pos == m:
+                    return True
+        return False
