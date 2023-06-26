45632d9bc0702265cca2aad99675c737e4301c3b
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-26 16:06:23 +0300

    Added description "Base64 Encoding"

---
+++ Python_Solutions/CodeWars/5kyu/Base64_Encoding.py
@@ -0,0 +1,9 @@
+# Extend the String object (JS) or create a function (Python, C#) that converts the value of the String to and from Base64 using the ASCII (UTF-8 for C#) character set.
+#
+# Example (input -> output):
+# 'this is a string!!' -> 'dGhpcyBpcyBhIHN0cmluZyEh'
+# You can learn more about Base64 encoding and decoding here.
+#
+# Note: This kata uses the non-padding version ("=" is not added to the end).
+#
+# BINARYSTRINGSALGORITHMS
