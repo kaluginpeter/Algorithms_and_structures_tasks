7bd8be9c3f766eead50f5ddfb2d28da1543e814d
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-26 16:06:44 +0300

    Added solution "Base64 Encoding"

--- Python_Solutions/CodeWars/5kyu/Base64_Encoding.py
+++ Python_Solutions/CodeWars/5kyu/Base64_Encoding.py
@@ -7,3 +7,16 @@
 # Note: This kata uses the non-padding version ("=" is not added to the end).
 #
 # BINARYSTRINGSALGORITHMS
+# Solution
+from base64 import b64encode, b64decode
+
+
+def to_base_64(string):
+    return b64encode(string.encode('ascii')).decode('ascii').replace('=', '')
+
+
+def from_base_64(string):
+    try:
+        return b64decode((string + '=').encode('ascii'), '-_').decode('ascii')
+    except:
+        return b64decode((string + '==').encode('ascii'), '-_').decode('ascii')
