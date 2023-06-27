293c2b3fdf0518369c0023ddf68fff379020d2a5
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-27 12:13:48 +0300

    Added solution "Did you mean ...?"

--- Python_Solutions/CodeWars/5kyu/Did_you_mean_....py
+++ Python_Solutions/CodeWars/5kyu/Did_you_mean_....py
@@ -24,3 +24,23 @@
 #
 # there is always exactly one possible correct solution
 # ALGORITHMS
+# Solution
+class Dictionary:
+    def __init__(self,words):
+        self.words=words
+    def find_most_similar(self,term):
+        return self.words[sorted((self.levenstein(term, v), k) for k, v in enumerate(self.words))[0][1]]
+    def levenstein(self, str_1, str_2):
+        n, m = len(str_1), len(str_2)
+        if n > m:
+            str_1, str_2 = str_2, str_1
+            n, m = m, n
+        current_row = range(n + 1)
+        for i in range(1, m + 1):
+            previous_row, current_row = current_row, [i] + [0] * n
+            for j in range(1, n + 1):
+                add, delete, change = previous_row[j] + 1, current_row[j - 1] + 1, previous_row[j - 1]
+                if str_1[j - 1] != str_2[i - 1]:
+                    change += 1
+                current_row[j] = min(add, delete, change)
+        return current_row[n]
