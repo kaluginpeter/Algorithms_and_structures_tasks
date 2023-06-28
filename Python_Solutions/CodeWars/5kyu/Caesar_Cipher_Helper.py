bed6681d95753e94319a84dd64627c3aba99224a
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-28 08:53:11 +0300

    Added description "Caesar Cipher Helper"

---
+++ Python_Solutions/CodeWars/5kyu/Caesar_Cipher_Helper.py
@@ -0,0 +1,11 @@
+# Write a class that, when given a string, will return an uppercase string with each letter shifted forward in the alphabet by however many spots the cipher was initialized to.
+#
+# For example:
+#
+# c = CaesarCipher(5); # creates a CipherHelper with a shift of five
+# c.encode('Codewars') # returns 'HTIJBFWX'
+# c.decode('BFKKQJX') # returns 'WAFFLES'
+# If something in the string is not in the alphabet (e.g. punctuation, spaces), simply leave it as is.
+# The shift will always be in range of [1, 26].
+#
+# CIPHERSOBJECT-ORIENTED PROGRAMMINGSTRINGSALGORITHMS
