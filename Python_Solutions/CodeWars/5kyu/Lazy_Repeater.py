63d92f1f9daf5508d8594c1683a9a6d61daa368b
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-29 11:33:42 +0300

    Added description "Lazy Repeater"

---
+++ Python_Solutions/CodeWars/5kyu/Lazy_Repeater.py
@@ -0,0 +1,10 @@
+# The makeLooper() function (make_looper in Python) takes a string (of non-zero length) as an argument. It returns a function. The function it returns will return successive characters of the string on successive invocations. It will start back at the beginning of the string once it reaches the end.
+#
+# For example:
+#
+# abc = make_looper('abc')
+# abc() # should return 'a' on this first call
+# abc() # should return 'b' on this second call
+# abc() # should return 'c' on this third call
+# abc() # should return 'a' again on this fourth call
+# ITERATORSALGORITHMS
