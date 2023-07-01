e95e8b8bdd17ebf07dddb50e4c76ad6dd4962420
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-07-01 15:07:32 +0300

    Added description "The Clockwise Spiral"

---
+++ Python_Solutions/CodeWars/5kyu/The_Clockwise_Spiral.py
@@ -0,0 +1,28 @@
+# Do you know how to make a spiral? Let's test it!
+# Classic definition: A spiral is a curve which emanates from a central point, getting progressively farther away as it revolves around the point.
+#
+# Your objective is to complete a function createSpiral(N) that receives an integer N and returns an NxN two-dimensional array with numbers 1 through NxN represented as a clockwise spiral.
+#
+# Return an empty array if N < 1 or N is not int / number
+#
+# Examples:
+#
+# N = 3 Output: [[1,2,3],[8,9,4],[7,6,5]]
+#
+# 1    2    3
+# 8    9    4
+# 7    6    5
+# N = 4 Output: [[1,2,3,4],[12,13,14,5],[11,16,15,6],[10,9,8,7]]
+#
+# 1   2   3   4
+# 12  13  14  5
+# 11  16  15  6
+# 10  9   8   7
+# N = 5 Output: [[1,2,3,4,5],[16,17,18,19,6],[15,24,25,20,7],[14,23,22,21,8],[13,12,11,10,9]]
+#
+# 1   2   3   4   5
+# 16  17  18  19  6
+# 15  24  25  20  7
+# 14  23  22  21  8
+# 13  12  11  10  9
+# ARRAYSPUZZLES
