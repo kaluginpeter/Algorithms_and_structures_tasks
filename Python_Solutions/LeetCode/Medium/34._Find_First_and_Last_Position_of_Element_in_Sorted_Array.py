077330d9ef4880b6ff4ed44619f10a86514e6bf1
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-07-12 21:16:56 +0300

    Added solution "34. Find First and Last Position of Element in Sorted Array"

--- Python_Solutions/LeetCode/Medium/34._Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
+++ Python_Solutions/LeetCode/Medium/34._Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
@@ -27,3 +27,29 @@
 # -109 <= nums[i] <= 109
 # nums is a non-decreasing array.
 # -109 <= target <= 109
+# Solution
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        if len(nums) == 0:
+            return [-1, -1]
+        pos, left, right = [-1, -1], 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                pos[0], right = mid, mid - 1
+            if nums[mid] < target:
+                left = mid + 1
+            if nums[mid] > target:
+                right = mid - 1
+        if pos[0] == -1:
+            return pos
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                pos[1], left = mid, mid + 1
+            if nums[mid] < target:
+                left = mid + 1
+            if nums[mid] > target:
+                right = mid - 1
+        return pos
