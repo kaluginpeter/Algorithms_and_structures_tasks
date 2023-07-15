1f55c02d7ece0787d6e5802100ea39e5f639c869
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-07-15 18:30:06 +0300

    Added description "Remove consecutive duplicate words"

---
+++ Python_Solutions/CodeWars/7kyu/Remove_consecutive_duplicate_words.py
@@ -0,0 +1,8 @@
+# Your task is to remove all consecutive duplicate words from a string, leaving only first words entries. For example:
+#
+# "alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"
+#
+# --> "alpha beta gamma delta alpha beta gamma delta"
+# Words will be separated by a single space. There will be no leading or trailing spaces in the string. An empty string (0 words) is a valid input.
+#
+# STRINGSREGULAR EXPRESSIONSALGORITHMS
