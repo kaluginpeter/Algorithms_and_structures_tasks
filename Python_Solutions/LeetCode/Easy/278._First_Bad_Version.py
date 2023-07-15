d22361af2ca255398095636394ac2c490f9fd0aa
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-07-15 18:24:19 +0300

    Added solution "278. First Bad Version"

--- Python_Solutions/LeetCode/Easy/278._First_Bad_Version.py
+++ Python_Solutions/LeetCode/Easy/278._First_Bad_Version.py
@@ -24,3 +24,17 @@
 # Constraints:
 #
 # 1 <= bad <= n <= 231 - 1
+# Solution
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        l, r = 1, n
+        while l < r:
+            mid = (l + r) // 2
+            if isBadVersion(mid):
+                r = mid
+            else:
+                l = mid + 1
+        return l
