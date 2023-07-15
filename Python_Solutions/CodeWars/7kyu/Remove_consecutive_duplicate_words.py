43881013cca1fe9d2cc024a8b608a48f742a4ca0
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-07-15 18:30:37 +0300

    Added solution "Remove consecutive duplicate words"

--- Python_Solutions/CodeWars/7kyu/Remove_consecutive_duplicate_words.py
+++ Python_Solutions/CodeWars/7kyu/Remove_consecutive_duplicate_words.py
@@ -6,3 +6,13 @@
 # Words will be separated by a single space. There will be no leading or trailing spaces in the string. An empty string (0 words) is a valid input.
 #
 # STRINGSREGULAR EXPRESSIONSALGORITHMS
+# Solution
+def remove_consecutive_duplicates(s):
+    if not s:
+        return ''
+    s= s.split()
+    w = [s[0]]
+    for i in range(1, len(s)):
+        if s[i-1] != s[i]:
+            w.append(s[i])
+    return ' '.join(w)
