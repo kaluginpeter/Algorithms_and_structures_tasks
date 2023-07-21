f69274e0000107d120a922e140a0c06185d6aec0
kaluginpeter <streetworkout200412@gmail.com>
2023-07-22 00:50:12 +0300

    Added description "Two numbers are positive"

---
+++ Python_Solutions/CodeWars/7kyu/Two_numbers_are_positive.py
@@ -0,0 +1,11 @@
+# Task:
+# Your job is to write a function, which takes three integers a, b, and c as arguments, and returns True if exactly two of of the three integers are positive numbers (greater than zero), and False - otherwise.
+#
+# Examples:
+# two_are_positive(2, 4, -3) == True
+# two_are_positive(-4, 6, 8) == True
+# two_are_positive(4, -6, 9) == True
+# two_are_positive(-4, 6, 0) == False
+# two_are_positive(4, 6, 10) == False
+# two_are_positive(-14, -3, -4) == False
+# FUNDAMENTALS
