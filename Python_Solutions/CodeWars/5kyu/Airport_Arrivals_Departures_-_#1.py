45964eeb6c785e6b3f31b52088610a6f6a5bd9b8
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-07-23 19:40:36 +0300

    Added description "Airport Arrivals Departures - #1"

---
+++ Python_Solutions/CodeWars/5kyu/Airport_Arrivals_Departures_-_#1.py
@@ -0,0 +1,10 @@
+# Given the initial display lines and the rotor moves for each line, determine what the board will say after it has been fully updated.
+#
+# For your convenience the characters of each rotor are in the pre-loaded constant ALPHABET which is a string.
+#
+# And don't forget to try my other flap display Katas!
+#
+# :-)
+#
+# ALGORITHMS
+# Solution
