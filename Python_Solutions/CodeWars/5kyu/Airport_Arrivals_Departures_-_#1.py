08f47bec62ca0eb1890ae66b75a04ae82aa01052
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-07-23 19:41:25 +0300

    Added solution "Airport Arrivals Departures - #1"

--- Python_Solutions/CodeWars/5kyu/Airport_Arrivals_Departures_-_#1.py
+++ Python_Solutions/CodeWars/5kyu/Airport_Arrivals_Departures_-_#1.py
@@ -8,3 +8,19 @@
 #
 # ALGORITHMS
 # Solution
+def flap_display(lines, rotors):
+    word, steps, out = '', [], []
+    al = ALPHABET + ALPHABET
+    for i in rotors:
+        top = []
+        for j in range(len(i) + 1):
+            top.append(sum(i[:j + 1]))
+        steps.append(top)
+    for j in range(len(lines)):
+        for i in range(len(lines[j])):
+            if steps[j][i] > len(ALPHABET):
+                steps[j][i] = steps[j][i] % len(ALPHABET)
+            word += al[al.index(lines[j][i]) + steps[j][i]]
+        out.append(word)
+        word = ''
+    return out
