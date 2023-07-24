21412340eba27f0c53533721fcc0e3c1cb1c2b19
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-07-24 20:30:29 +0300

    Added solution "50. Pow(x, n)"

--- Python_Solutions/LeetCode/Medium/50._Pow(x,_n).py
+++ Python_Solutions/LeetCode/Medium/50._Pow(x,_n).py
@@ -24,3 +24,16 @@
 # n is an integer.
 # Either x is not zero or n > 0.
 # -104 <= xn <= 104
+# Solution
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1
+        if n < 0:
+            x = 1 / x
+            n *= -1
+        if n % 2 != 0:
+            return x * self.myPow(x, n - 1)
+        else:
+            temp = self.myPow(x, n/2)
+            return temp * temp
