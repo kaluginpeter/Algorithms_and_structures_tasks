3bbab852402602e0b628b66c329e4650500e9757
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-07-26 21:51:42 +0300

    Added solution "1870. Minimum Speed to Arrive on Time"

--- Python_Solutions/LeetCode/Medium/1870._Minimum_Speed_to_Arrive_on_Time.py
+++ Python_Solutions/LeetCode/Medium/1870._Minimum_Speed_to_Arrive_on_Time.py
@@ -41,3 +41,22 @@
 # 1 <= dist[i] <= 105
 # 1 <= hour <= 109
 # There will be at most two digits after the decimal point in hour.
+# Solution
+class Solution:
+    def minSpeedOnTime(self, dist: List[int], hour: float) -> int:
+        def mintime(speed, dist, hour):
+            import math
+            top = 0
+            for i in range(len(dist)-1):
+                top += math.ceil(dist[i]/speed)
+            top += dist[-1] / speed
+            return top <= hour
+        l, r, m = 1, 10 ** 7, -1
+        while l <= r:
+            mid = (l + r) // 2
+            if mintime(mid, dist, hour):
+                m = mid
+                r = mid - 1
+            else:
+                l = mid + 1
+        return m
