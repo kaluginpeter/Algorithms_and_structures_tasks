32c1f825f1272e2308898fa71fa28554b772542e
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-07-27 21:33:45 +0300

    Added solution "263. Ugly Number"

--- Python_Solutions/LeetCode/Easy/263._Ugly_Number.py
+++ Python_Solutions/LeetCode/Easy/263._Ugly_Number.py
@@ -24,3 +24,15 @@
 # Constraints:
 #
 # -231 <= n <= 231 - 1
+# Solution
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        while n % 2 == 0:
+            n //= 2
+        while n % 3 == 0:
+            n //= 3
+        while n % 5 == 0:
+            n //= 5
+        return n == 1
