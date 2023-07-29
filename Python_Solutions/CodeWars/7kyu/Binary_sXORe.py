7a1b0b4b43d9c42807d717b07d0de14485e9bb5c
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-07-29 22:41:28 +0300

    Added description "Binary sXORe"

---
+++ Python_Solutions/CodeWars/7kyu/Binary_sXORe.py
@@ -0,0 +1,12 @@
+# Objective
+# Given a number n we will define it's sXORe to be 0 XOR 1 XOR 2 ... XOR n where XOR is the bitwise XOR operator.
+#
+# Write a function that takes n and returns it's sXORe.
+#
+# Examples
+# n	sXORe n
+# 0	0
+# 1	1
+# 50	51
+# 1000000	1000000
+# BINARYALGORITHMS
