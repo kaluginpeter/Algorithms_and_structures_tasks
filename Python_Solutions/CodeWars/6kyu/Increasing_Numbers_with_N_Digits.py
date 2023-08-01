851ee0ff2924230872e9d9207387d7d41c369425
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-01 19:57:34 +0300

    Added description "Increasing Numbers with N Digits"

---
+++ Python_Solutions/CodeWars/6kyu/Increasing_Numbers_with_N_Digits.py
@@ -0,0 +1,7 @@
+# An non decreasing number is one containing no two consecutive digits (left to right), whose the first is higer than the second. For example, 1235 is an non decreasing number, 1229 is too, but 123429 isn't.
+#
+# Write a function that finds the number of non decreasing numbers up to 10**N (exclusive) where N is the input of your function. For example, if N=3, you have to count all non decreasing numbers from 0 to 999.
+#
+# You'll definitely need something smarter than brute force for large values of N!
+#
+# MATHEMATICSFUNDAMENTALS
