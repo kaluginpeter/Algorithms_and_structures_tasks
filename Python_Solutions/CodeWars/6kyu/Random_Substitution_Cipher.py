2e70432c4b903f16fb7b9db1791fd52d4f147bc5
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-02 22:01:26 +0300

    Added solution "Random Substitution Cipher"

--- Python_Solutions/CodeWars/6kyu/Random_Substitution_Cipher.py
+++ Python_Solutions/CodeWars/6kyu/Random_Substitution_Cipher.py
@@ -9,3 +9,16 @@
 # The output from your solution will be tested for a high probability of randomness. Special thanks to @halcarleton and @Vaults for showing me how to do this. There might be a clever way to cheat, but where's the fun in that?
 #
 # FUNDAMENTALS
+# Solution
+import random
+def random_sub():
+    d = {"a":"c", "b":"p", "c":"j", "d":"a", "e":"v", "f":"d",
+         "g":"g", "h":"u", "i":"l", "j":"t", "k":"n", "l":"w",
+         "m":"m", "n":"o", "o":"i", "p":"s", "q":"f", "r":"r", "s":"x", "t":"b",
+         "u":"h", "v":"y", "w":"q", "x":"e", "y":"k", "z":"z"}
+    en_al = 'abcdefghijklmnopqrstuvwxyz'
+    for k, v in d.items():
+        let = random.choice(en_al)
+        d[k] = let
+        en_al = en_al.replace(let, '')
+    return d
