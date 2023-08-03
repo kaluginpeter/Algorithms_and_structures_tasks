ea2a4ddb27abde7da1e8b6480d08f0a450f45a70
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-03 19:56:49 +0300

    Added solution "Fast cooking pancakes"

--- Python_Solutions/CodeWars/7kyu/Fast_cooking_pancakes.py
+++ Python_Solutions/CodeWars/7kyu/Fast_cooking_pancakes.py
@@ -1,3 +1,7 @@
 # You need to cook pancakes, but you are very hungry. As known, one needs to fry a pancake one minute on each side. What is the minimum time you need to cook n pancakes, if you can put on the frying pan only m pancakes at a time? n and m are positive integers between 1 and 1000.
 #
 # LOGICFUNDAMENTALS
+# Solution
+import math
+def cook_pancakes(n, m):
+    return math.ceil(n / m * 2) if n >= m else 2
