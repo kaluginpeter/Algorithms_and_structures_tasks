75cb6e80bd64f404ff3ee64c50707b18d88a157f
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-04 21:32:29 +0300

    Added solution "ASCII Shift Encryption/Decryption"

--- Python_Solutions/CodeWars/7kyu/ASCII_Shift_Encryption/Decryption.py
+++ Python_Solutions/CodeWars/7kyu/ASCII_Shift_Encryption/Decryption.py
@@ -13,3 +13,14 @@
 # - 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 # Shift (subtract)
 #   p | a | s | s | w | o | r | d # Plaintext
 # ALGORITHMS
+# Solution
+def ascii_encrypt(plaintext):
+    if not plaintext:
+        return plaintext
+    return ''.join(chr(ord(plaintext[i]) + i) for i in range(len(plaintext)))
+
+
+def ascii_decrypt(encrypted):
+    if not encrypted:
+        return encrypted
+    return ''.join(chr(ord(encrypted[i]) - i) for i in range(len(encrypted)))
