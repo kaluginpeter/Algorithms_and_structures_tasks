b974fc43341522c097c2226577d6cfed1417a56f
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-06 19:54:02 +0300

    Added solution "Point in Polygon"

--- Python_Solutions/CodeWars/6kyu/Point_in_Polygon.py
+++ Python_Solutions/CodeWars/6kyu/Point_in_Polygon.py
@@ -15,3 +15,22 @@
 # To help you visualize your test cases, the visualize_polygon(polygon, point) function is preloaded. It draws the polygon and point.
 #
 # GEOMETRYMATHEMATICSALGORITHMS
+# Solution
+from typing import List, Tuple
+from preloaded import visualize_polygon
+def point_in_polygon(poly, point) -> bool:
+    x,y  = point
+    n = len(poly)
+    inside = False
+    p1x,p1y = poly[0]
+    for i in range(n+1):
+        p2x,p2y = poly[i % n]
+        if y > min(p1y,p2y):
+            if y <= max(p1y,p2y):
+                if x <= max(p1x,p2x):
+                    if p1y != p2y:
+                        xints = (y-p1y)*(p2x-p1x)/(p2y-p1y)+p1x
+                    if p1x == p2x or x <= xints:
+                        inside = not inside
+        p1x,p1y = p2x,p2y
+    return inside
