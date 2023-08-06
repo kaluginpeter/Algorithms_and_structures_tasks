73ea3095ffec3a0a8085f76317b0dd40b8c353ce
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-06 19:53:25 +0300

    Added description "Point in Polygon"

---
+++ Python_Solutions/CodeWars/6kyu/Point_in_Polygon.py
@@ -0,0 +1,17 @@
+# The problem
+# In this kata, you're going write a function called point_in_polygon to test if a point is inside a polygon.
+#
+# Points will be represented as tuples (x,y).
+#
+# The polygon will be an array of points which are the polygon's vertices. The last point in the array connects back to the first point.
+#
+# You can assume:
+#
+# The polygon will be a valid simple polygon. That is, it will have at least three points, none of its edges will cross each other, and exactly two edges will meet at each vertex.
+#
+# In the tests, the point will never fall exactly on an edge of the polygon.
+#
+# Testing
+# To help you visualize your test cases, the visualize_polygon(polygon, point) function is preloaded. It draws the polygon and point.
+#
+# GEOMETRYMATHEMATICSALGORITHMS
