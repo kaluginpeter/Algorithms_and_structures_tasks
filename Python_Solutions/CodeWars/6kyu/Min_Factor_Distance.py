762e8fccb57aa69e113492702349cd8c6a5fab5b
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-08 21:24:11 +0300

    Added description "Min Factor Distance"

---
+++ Python_Solutions/CodeWars/6kyu/Min_Factor_Distance.py
@@ -0,0 +1,9 @@
+# Write a function that returns the smallest distance between two factors of a number. The input will always be a number greater than one.
+#
+# Example:
+#
+# 13013 has factors: [ 1, 7, 11, 13, 77, 91, 143, 169, 1001, 1183, 1859, 13013]
+#
+# Hence the asnwer will be 2 (=13-11)
+#
+# FUNDAMENTALS
