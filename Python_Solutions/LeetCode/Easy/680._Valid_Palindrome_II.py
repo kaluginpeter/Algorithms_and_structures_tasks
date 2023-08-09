256c818ed75497623e5c86b1224d24fc565d2d8e
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-09 22:42:21 +0300

    Added solution "680. Valid Palindrome II"

--- Python_Solutions/LeetCode/Easy/680._Valid_Palindrome_II.py
+++ Python_Solutions/LeetCode/Easy/680._Valid_Palindrome_II.py
@@ -21,3 +21,13 @@
 #
 # 1 <= s.length <= 105
 # s consists of lowercase English letters.
+# Solution
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        start, end = 0, len(s) - 1
+        while start < end:
+            if s[start] != s[end]:
+                first, second = s[start:end], s[start+1:end+1]
+                return first == first[::-1] or second == second[::-1]
+            start, end = start + 1, end - 1
+        return True
