2ea6046e7b7cb9b5b5641dd1ccb1e1b4a1656a91
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-13 19:56:02 +0300

    Added solution "2570. Merge Two 2D Arrays by Summing Values"

--- Python_Solutions/LeetCode/Easy/2570._Merge_Two_2D_Arrays_by_Summing_Values.py
+++ Python_Solutions/LeetCode/Easy/2570._Merge_Two_2D_Arrays_by_Summing_Values.py
@@ -35,3 +35,25 @@
 # 1 <= idi, vali <= 1000
 # Both arrays contain unique ids.
 # Both arrays are in strictly ascending order by id.
+# Solution
+class Solution:
+    def mergeArrays(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
+        res, right, left = [], 0, 0
+        while left < len(nums1) and right < len(nums2):
+            if nums1[left][0] == nums2[right][0]:
+                res.append([nums1[left][0], nums1[left][1] + nums2[right][1]])
+                left, right = left + 1, right + 1
+            else:
+                if nums1[left][0] < nums2[right][0]:
+                    res.append([nums1[left][0], nums1[left][1]])
+                    left += 1
+                else:
+                    res.append([nums2[right][0], nums2[right][1]])
+                    right += 1
+        while left < len(nums1):
+            res.append(nums1[left])
+            left += 1
+        while right < len(nums2):
+            res.append(nums2[right])
+            right += 1
+        return res
