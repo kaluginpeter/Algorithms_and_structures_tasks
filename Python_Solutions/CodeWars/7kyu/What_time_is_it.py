5176edf017e615d0fee6167213c423c0a8816388
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-14 19:34:49 +0300

    Added solution "What time is it?"

--- Python_Solutions/CodeWars/7kyu/What_time_is_it.py
+++ Python_Solutions/CodeWars/7kyu/What_time_is_it.py
@@ -9,3 +9,8 @@
 # "12:00:01AM"  -->  "00:00:01"
 # "11:46:47PM"  -->  "23:46:47"
 # DATE TIMEFUNDAMENTALS
+# Solution
+from datetime import datetime
+def get_military_time(time):
+    in_time = datetime.strptime(time, "%I:%M:%S%p")
+    return datetime.strftime(in_time, "%H:%M:%S")
