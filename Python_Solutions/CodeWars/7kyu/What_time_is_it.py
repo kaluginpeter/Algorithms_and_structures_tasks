8ff52e21252d52d84dea605db93470fb59f3e4db
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-14 19:34:26 +0300

    Added description "What time is it?"

---
+++ Python_Solutions/CodeWars/7kyu/What_time_is_it.py
@@ -0,0 +1,11 @@
+# Given a time in AM/PM format as a string, convert it to 24-hour military time time as a string.
+#
+# Midnight is 12:00:00AM on a 12-hour clock, and 00:00:00 on a 24-hour clock. Noon is 12:00:00PM on a 12-hour clock, and 12:00:00 on a 24-hour clock
+#
+# Try not to use built-in Date/Time libraries.
+#
+# Examples
+# "07:05:45PM"  -->  "19:05:45"
+# "12:00:01AM"  -->  "00:00:01"
+# "11:46:47PM"  -->  "23:46:47"
+# DATE TIMEFUNDAMENTALS
