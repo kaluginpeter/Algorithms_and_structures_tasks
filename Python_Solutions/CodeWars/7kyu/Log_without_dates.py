f0073de4719b17f91d2790d8706f64874b6c5caa
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-15 19:55:08 +0300

    Added description "Log without dates"

---
+++ Python_Solutions/CodeWars/7kyu/Log_without_dates.py
@@ -0,0 +1,22 @@
+# You will be given an array of events, which are represented by strings. The strings are dates in HH:MM:SS format.
+#
+# It is known that all events are recorded in chronological order and two events can't occur in the same second.
+#
+# Return the minimum number of days during which the log is written.
+#
+# Example:
+#
+# Input -> ["00:00:00", "00:01:11", "02:15:59", "23:59:58", "23:59:59"]
+# Output -> 1
+#
+# Input -> ["12:12:12"]
+# Output -> 1
+#
+# Input -> ["12:00:00", "23:59:59", "00:00:00"]
+# Output -> 2
+#
+# Input -> []
+# Output -> 0
+# Good luck
+#
+# DATE TIMEFILTERING
