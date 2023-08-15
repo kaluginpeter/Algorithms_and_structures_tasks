bde5e31b1c32fa8baba97a1e78a17b376185587e
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-15 20:05:04 +0300

    Added description "Converting 12-hour time to 24-hour time"

---
+++ Python_Solutions/CodeWars/7kyu/Converting_12_hour_time_to_24_hour_time.py
@@ -0,0 +1,10 @@
+# Converting a 12-hour time like "8:30 am" or "8:30 pm" to 24-hour time (like "0830" or "2030") sounds easy enough, right? Well, let's see if you can do it!
+#
+# You will have to define a function, which will be given an hour (always in the range of 1 to 12, inclusive), a minute (always in the range of 0 to 59, inclusive), and a period (either "am" or "pm") as input.
+#
+# Your task is to return a four-digit string that encodes that time in 24-hour time.
+#
+# Notes
+# By convention, noon is 12:00 pm, and midnight is 12:00 am.
+# On 12-hours clock, there is no 0 hour, and time just after midnight is denoted as, for example, 12:15 am. On 24-hour clock, this translates to 0015.
+# DATE TIMEALGORITHMS
