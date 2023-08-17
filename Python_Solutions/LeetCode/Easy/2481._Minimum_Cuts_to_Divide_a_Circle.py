b59a7d89937c34a0b3c6b40cfa69d0575e891da5
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-17 19:34:44 +0300

    Added solution "2481. Minimum Cuts to Divide a Circle"

--- Python_Solutions/LeetCode/Easy/2481._Minimum_Cuts_to_Divide_a_Circle.py
+++ Python_Solutions/LeetCode/Easy/2481._Minimum_Cuts_to_Divide_a_Circle.py
@@ -30,3 +30,9 @@
 # Constraints:
 #
 # 1 <= n <= 100
+# Solution
+class Solution:
+    def numberOfCuts(self, n: int) -> int:
+        if n == 1:
+            return 0
+        return n // 2 if n % 2 == 0 else n
