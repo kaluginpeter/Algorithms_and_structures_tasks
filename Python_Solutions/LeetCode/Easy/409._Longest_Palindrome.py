fa2dc63a310dd4864be6ba3127ad68e96efb2c99
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-20 17:59:15 +0300

    Added solution "409. Longest Palindrome"

--- Python_Solutions/LeetCode/Easy/409._Longest_Palindrome.py
+++ Python_Solutions/LeetCode/Easy/409._Longest_Palindrome.py
@@ -20,3 +20,15 @@
 #
 # 1 <= s.length <= 2000
 # s consists of lowercase and/or uppercase English letters only.
+# Solution
+class Solution(object):
+    def longestPalindrome(self, s):
+        d, count = {}, 0
+        for i in s:
+            if i in d:
+                d[i] += 1
+                if d[i] % 2 == 0:
+                    count += 2
+            else:
+                d[i] = 1
+        return count + 1 if len(s) - count != 0 else count
