66e83e3fbb3cb6f57cf1af5d883bb106805e5a22
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-22 22:07:26 +0300

    Added description "Simplify the number!"

---
+++ Python_Solutions/CodeWars/6kyu/Simplify_the_number!.py
@@ -0,0 +1,15 @@
+# Task
+# Given a positive integer as input, return the output as a string in the following format:
+#
+# Each element, corresponding to a digit of the number, multiplied by a power of 10 in such a way that with the sum of these elements you can obtain the original number.
+#
+# Examples
+# Input	Output
+# 0	""
+# 56	"5*10+6"
+# 60	"6*10"
+# 999	"9*100+9*10+9"
+# 10004	"1*10000+4"
+# Note: input >= 0
+#
+# FUNDAMENTALS
