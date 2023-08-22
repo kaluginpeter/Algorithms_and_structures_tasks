147a7c61c60fe55a1a7a9c951bab8cd5a1aae888
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-22 22:13:19 +0300

    Added solution "Sort Arrays (Ignoring Case)"

--- Python_Solutions/CodeWars/6kyu/Sort_Arrays_Ignoring_Case.py
+++ Python_Solutions/CodeWars/6kyu/Sort_Arrays_Ignoring_Case.py
@@ -3,3 +3,6 @@
 # ["Hello", "there", "I'm", "fine"]  -->  ["fine", "Hello", "I'm", "there"]
 # ["C", "d", "a", "B"])              -->  ["a", "B", "C", "d"]
 # ARRAYSSORTING
+# Solution
+def sortme(words):
+    return sorted(words, key=lambda x: x.lower())
