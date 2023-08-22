9324ba79e543ab299764baa45a656405ed755517
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-22 21:56:24 +0300

    Added description "Simple arithmetic progression"

---
+++ Python_Solutions/CodeWars/6kyu/Simple_arithmetic_progression.py
@@ -0,0 +1,9 @@
+# In this Kata, you will be given an array of integers and your task is to return the number of arithmetic progressions of size 3 that are possible from that list. In each progression, the differences between the elements must be the same.
+#
+# [1, 2, 3, 5, 7, 9] ==> 5
+# // [1, 2, 3], [1, 3, 5], [1, 5, 9], [3, 5, 7], and [5, 7, 9]
+# All array elements will be unique and sorted. More examples in test cases.
+#
+# Good luck!
+#
+# ALGORITHMS
