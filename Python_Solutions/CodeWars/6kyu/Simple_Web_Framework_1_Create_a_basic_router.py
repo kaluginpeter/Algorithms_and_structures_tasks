d232acba8ef4f94a3ff27a1846493d6c7d18e576
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-22 21:55:33 +0300

    Added description "Simple Web Framework #1: Create a basic router"

---
+++ Python_Solutions/CodeWars/6kyu/Simple_Web_Framework_1_Create_a_basic_router.py
@@ -0,0 +1,18 @@
+# In this Kata, you have to design a simple routing class for a web framework.
+#
+# The router should accept bindings for a given url, http method and an action.
+#
+# Then, when a request with a bound url and method comes in, it should return the result of the action.
+#
+# Example usage:
+#
+# router = Router()
+# router.bind('/hello', 'GET', lambda: 'hello world')
+#
+# router.runRequest('/hello', 'GET') // returns 'hello world'
+# When asked for a route that doesn't exist, router should return:
+#
+# 'Error 404: Not Found'
+# The router should also handle modifying existing routes. See the example tests for more details.
+#
+# OBJECT-ORIENTED PROGRAMMINGARRAYSFUNDAMENTALS
