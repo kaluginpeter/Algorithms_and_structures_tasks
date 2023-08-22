8b6fa3137998af4a7e3ccc8aa433f93949b092fc
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-22 22:09:54 +0300

    Added solution "Single character palindromes"

--- Python_Solutions/CodeWars/6kyu/Single_character_palindromes.py
+++ Python_Solutions/CodeWars/6kyu/Single_character_palindromes.py
@@ -12,3 +12,10 @@
 # If you like this Kata, please try Single Character Palindromes II
 #
 # ALGORITHMS
+# Solution
+def solve(s):
+    if s == s[::-1]: return 'OK'
+    for i in range(len(s)):
+        if s[:i] + s[i+1:] == (s[:i] + s[i+1:])[::-1]:
+            return 'remove one'
+    return 'not possible'
