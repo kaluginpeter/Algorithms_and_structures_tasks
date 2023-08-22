580f214de8a008e64b50c63b065fe3f8e8a333c6
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-22 22:16:05 +0300

    Added solution "Sort the odd"

--- Python_Solutions/CodeWars/6kyu/Sort_the_odd.py
+++ Python_Solutions/CodeWars/6kyu/Sort_the_odd.py
@@ -6,3 +6,7 @@
 # [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
 # [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
 # FUNDAMENTALSARRAYSSORTING
+# Solution
+def sort_array(source_array):
+    odds = iter(sorted(elem for elem in source_array if elem % 2))
+    return [next(odds) if elem % 2 else elem for elem in source_array]
