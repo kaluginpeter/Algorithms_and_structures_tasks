044b248e5f769b2a7b17dd1b1402abe26a523a7e
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-22 22:10:37 +0300

    Added description "Smallest Permutation"

---
+++ Python_Solutions/CodeWars/6kyu/Smallest_Permutation.py
@@ -0,0 +1,10 @@
+# Given a number, find the permutation with the smallest absolute value (no leading zeros).
+#
+# -20 => -20
+# -32 => -23
+# 0 => 0
+# 10 => 10
+# 29394 => 23499
+# The input will always be an integer.
+#
+# FUNDAMENTALS
