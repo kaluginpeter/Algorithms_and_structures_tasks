f6ff9639ef35c8e13fd8b8972f1468a65e544b92
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-22 22:10:56 +0300

    Added solution "Smallest Permutation"

--- Python_Solutions/CodeWars/6kyu/Smallest_Permutation.py
+++ Python_Solutions/CodeWars/6kyu/Smallest_Permutation.py
@@ -8,3 +8,20 @@
 # The input will always be an integer.
 #
 # FUNDAMENTALS
+# Solution
+def min_permutation(n):
+    if n == 0: return n
+    flag, count = True if n < 0 else False, 0
+    out = ''.join(sorted(str(n)))
+    if not flag:
+        while out.startswith('0'):
+            out = out[1:]
+            count += 1
+        out = int(out[0] + '0' * count + out[1:])
+    if flag:
+        out = out[1:]
+        while out.startswith('0'):
+            out = out[1:]
+            count += 1
+        out = int('-' + out[0] + '0' * count + out[1:])
+    return out
