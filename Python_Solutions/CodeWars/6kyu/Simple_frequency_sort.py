9ea1fbc70542cd0179b298383565dea5b12c666f
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-22 21:59:18 +0300

    Added description "Simple frequency sort"

---
+++ Python_Solutions/CodeWars/6kyu/Simple_frequency_sort.py
@@ -0,0 +1,12 @@
+# In this kata, you will sort elements in an array by decreasing frequency of elements. If two elements have the same frequency, sort them by increasing value.
+#
+# solve([2,3,5,3,7,9,5,3,7]) = [3,3,3,5,5,7,7,2,9]
+# -- We sort by highest frequency to lowest frequency.
+# -- If two elements have same frequency, we sort by increasing value.
+# More examples in test cases.
+#
+# Good luck!
+#
+# Please also try Simple time difference
+#
+# ALGORITHMSSORTINGARRAYS
