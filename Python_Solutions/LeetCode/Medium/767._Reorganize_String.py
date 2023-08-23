7a6cc02ffb261e246bd1ec4f82dd172b31fab8d9
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-23 23:00:20 +0300

    Added description "767. Reorganize String"

---
+++ Python_Solutions/LeetCode/Medium/767._Reorganize_String.py
@@ -0,0 +1,26 @@
+# Given a string s, rearrange the characters of s so that any two adjacent characters are not the same.
+#
+# Return any possible rearrangement of s or return "" if not possible.
+#
+#
+#
+# Example 1:
+#
+# Input: s = "aab"
+# Output: "aba"
+# Example 2:
+#
+# Input: s = "aaab"
+# Output: ""
+#
+#
+# Constraints:
+#
+# 1 <= s.length <= 500
+# s consists of lowercase English letters.
+# Accepted
+# 325.3K
+# Submissions
+# 600.3K
+# Acceptance Rate
+# 54.2%
