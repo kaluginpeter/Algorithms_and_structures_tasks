b2300fac66f1a30b06a9522c9c67c5ee3a210f44
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-24 22:16:19 +0300

    Added description "Special Multiples"

---
+++ Python_Solutions/CodeWars/6kyu/Special_Multiples.py
@@ -0,0 +1,14 @@
+# Some numbers have the property to be divisible by 2 and 3. Other smaller subset of numbers have the property to be divisible by 2, 3 and 5. Another subset with less abundant numbers may be divisible by 2, 3, 5 and 7. These numbers have something in common: their prime factors are contiguous primes.
+#
+# Implement a function that finds the amount of numbers that have the first N primes as factors below a given limit.
+#
+# Let's see some cases:
+#
+# count_specMult(3, 200)  =>  6
+#
+# The first 3 primes are: 2, 3 and 5.
+#
+# And the found numbers below 200 are: 30, 60, 90, 120, 150, 180.
+# Happy coding!!
+#
+# MATHEMATICSALGORITHMSFUNDAMENTALS
