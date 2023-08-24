8eb03511263c2e04b784f4502daa7e9bbbe7ab6c
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-24 22:18:01 +0300

    Added solution "Split Strings"

--- Python_Solutions/CodeWars/6kyu/Split_Strings.py
+++ Python_Solutions/CodeWars/6kyu/Split_Strings.py
@@ -5,3 +5,6 @@
 # * 'abc' =>  ['ab', 'c_']
 # * 'abcdef' => ['ab', 'cd', 'ef']
 # REGULAR EXPRESSIONSSTRINGSALGORITHMS
+# Solution
+def solution(s):
+    return [s[i:i+2] if len(s[i:i+2]) == 2 else s[i:i+2] + '_' for i in range(0, len(s), 2)]
