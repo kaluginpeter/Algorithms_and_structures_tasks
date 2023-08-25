15302fecefd8542cff4e6dd40134c368ee865674
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-25 22:33:12 +0300

    Added description "SumFibs"

---
+++ Python_Solutions/CodeWars/6kyu/SumFibs.py
@@ -0,0 +1,10 @@
+# Create a function that takes an argument n and sums even Fibonacci numbers up to n's index in the Fibonacci sequence.
+#
+# Example:
+#
+# sum_fibs(5) == 2 # (0, 1, 1, 2, 3, 5); 2 is the only even number in the sequence and doesn't have another number in the sequence to get added to in the indexed Fibonacci sequence.
+# Example:
+#
+# sum_fibs(9) == 44 # (0, 1, 1, 2, 3, 5, 8, 13, 21, 34)
+# # 2 + 8 + 34 = 44
+# ALGORITHMS
