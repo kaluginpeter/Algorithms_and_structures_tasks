2124998c7fd99e07e6de5cc784cb506e0af4135e
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-25 22:35:28 +0300

    Added description "Summation Of Primes"

---
+++ Python_Solutions/CodeWars/6kyu/Summation_Of_Primes.py
@@ -0,0 +1,6 @@
+# Summation Of Primes
+# The sum of the primes below or equal to 10 is 2 + 3 + 5 + 7 = 17. Find the sum of all the primes below or equal to the number passed in.
+#
+# From Project Euler's Problem #10.
+#
+# ALGORITHMSMATHEMATICS
