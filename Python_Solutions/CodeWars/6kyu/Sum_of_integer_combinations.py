63db843b7393fd22d00eb4c76fcacbbf623fa5b5
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-25 22:27:41 +0300

    Added description "Sum of integer combinations"

---
+++ Python_Solutions/CodeWars/6kyu/Sum_of_integer_combinations.py
@@ -0,0 +1,16 @@
+# Consider the array [3,6,9,12]. If we generate all the combinations with repetition that sum to 12, we get 5 combinations: [12], [6,6], [3,9], [3,3,6], [3,3,3,3]. The length of the sub-arrays (such as [3,3,3,3] should be less than or equal to the length of the initial array ([3,6,9,12]).
+#
+# Given an array of positive integers and a number n, count all combinations with repetition of integers that sum to n. For example:
+#
+# find([3,6,9,12],12) = 5.
+# More examples in the test cases.
+#
+# Good luck!
+#
+# If you like this Kata, please try:
+#
+# Array combinations
+#
+# Sum of prime-indexed elements
+#
+# ARRAYSFUNDAMENTALS
