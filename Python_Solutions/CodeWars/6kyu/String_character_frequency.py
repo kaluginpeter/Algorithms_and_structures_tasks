2bac53307b4d660d151cbffc16925eda00745df1
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-25 22:15:26 +0300

    Added solution "String character frequency"

--- Python_Solutions/CodeWars/6kyu/String_character_frequency.py
+++ Python_Solutions/CodeWars/6kyu/String_character_frequency.py
@@ -11,3 +11,7 @@
 # Good luck!
 #
 # STRINGSALGORITHMS
+# Solution
+from collections import Counter
+def solve(s):
+    return any(len(set(Counter(s[:i] + s[i+1:]).values())) == 1 for i in range(len(s)))
