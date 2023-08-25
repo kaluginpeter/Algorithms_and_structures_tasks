e10ca201d0abf7dccd22dcb770a18c688ac3d9d7
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-25 22:42:47 +0300

    Added description "T.T.T.17: Split odd and even"

---
+++ Python_Solutions/CodeWars/6kyu/T.T.T.17_Split_odd_and_even.py
@@ -0,0 +1,17 @@
+# Complete function splitOddAndEven, accept a number n(n>0), return an array that contains the continuous parts of odd or even digits.
+#
+# Please don't worry about digit 0, it won't appear ;-)
+#
+# Examples
+# splitOddAndEven(123)  ===  [1,2,3]
+#
+# splitOddAndEven(223)  ===  [22,3]
+#
+# splitOddAndEven(111)  ===  [111]
+#
+# splitOddAndEven(13579)  ===  [13579]
+#
+# splitOddAndEven(135246)  ===  [135,246]
+#
+# splitOddAndEven(123456)  ===  [1,2,3,4,5,6]
+# PUZZLESGAMES
