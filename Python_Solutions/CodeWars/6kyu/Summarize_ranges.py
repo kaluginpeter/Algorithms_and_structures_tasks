015ac20fac62641b0ad99244e1ac8661a4019ee5
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-25 22:34:15 +0300

    Added solution "Summarize ranges"

--- Python_Solutions/CodeWars/6kyu/Summarize_ranges.py
+++ Python_Solutions/CodeWars/6kyu/Summarize_ranges.py
@@ -8,3 +8,12 @@
 # summary_ranges([0, 1, 2, 3, 3, 3, 4, 4, 5, 6, 7, 7, 9, 9, 10]) == ["0->7", "9->10"]
 # summary_ranges([-2, 0, 1, 2, 3, 3, 3, 4, 4, 5, 6, 7, 7, 9, 9, 10, 12]) == ["-2", "0->7", "9->10", "12"]
 # ARRAYSALGORITHMS
+# Solution
+def summary_ranges(nums):
+    nums.append(float("inf"))
+    i, l = nums[0], []
+    for a, b in zip(nums, nums[1:]):
+        if b - a > 1:
+            l.append(str(i) if i == a else f"{i}->{a}")
+            i = b
+    return l
