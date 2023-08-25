2d81d22aaaed4c6f7bceeb1ea55ff2af6bae1e96
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-25 22:30:46 +0300

    Added solution "Sum of prime-indexed elements"

--- Python_Solutions/CodeWars/6kyu/Sum_of_prime-indexed_elements.py
+++ Python_Solutions/CodeWars/6kyu/Sum_of_prime-indexed_elements.py
@@ -11,3 +11,8 @@
 # Consonant value
 #
 # LISTSFUNDAMENTALS
+# Solution
+def is_prime(n):
+    return n >= 2 and all(n%i for i in range(2, 1+int(n**.5)))
+def total(arr):
+    return sum(n for i, n in enumerate(arr) if is_prime(i))
