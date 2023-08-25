0224ae80d425e87589b3f46318e318700f9923fb
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-25 22:26:52 +0300

    Added description "Sum of all numbers with the same digits (performance edition)"

---
+++ Python_Solutions/CodeWars/6kyu/Sum_of_all_numbers_with_the_same_digits_performance_edition.py
@@ -0,0 +1,10 @@
+# Description
+# Find the sum of all numbers with the same digits (permutations) as the input number, including duplicates. However, due to the fact that this is a performance edition kata, the input can go up to 10**10000. That's a number with 10001 digits (at most)! Be sure to use efficient algorithms and good luck! All numbers tested for will be positive.
+#
+# Examples
+# 98    -->  187    ;  89 + 98  =  187
+# 123   -->  1332   ;  123 + 132 + 213 + 231 + 312 + 321  =  1332
+# 1185  -->  99990  ;  1185 + 1158 + 1815 + 1851 + 1518 + 1581 + 1185 + 1158 + 1815 +
+#                    + 1851 + 1518 + 1581 + 8115 + 8151 + 8115 + 8151 + 8511 + 8511 +
+#                    + 5118 + 5181 + 5118 + 5181 + 5811 + 5811  =  99990
+# PERFORMANCEPERMUTATIONSFUNDAMENTALS
