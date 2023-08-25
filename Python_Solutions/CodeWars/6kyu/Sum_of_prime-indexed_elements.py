cd94504a8d14a14629c29c78d8dfb6caccc8a1a2
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-25 22:30:29 +0300

    Added description "Sum of prime-indexed elements"

---
+++ Python_Solutions/CodeWars/6kyu/Sum_of_prime-indexed_elements.py
@@ -0,0 +1,13 @@
+# In this Kata, you will be given an integer array and your task is to return the sum of elements occupying prime-numbered indices.
+#
+# The first element of the array is at index 0.
+#
+# Good luck!
+#
+# If you like this Kata, try:
+#
+# Dominant primes. It takes this idea a step further.
+#
+# Consonant value
+#
+# LISTSFUNDAMENTALS
