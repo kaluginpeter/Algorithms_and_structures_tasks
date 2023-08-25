b41c2cfc07c96360ddf0db2f14bcc92668e6698d
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-25 22:14:32 +0300

    Added solution "String average"

--- Python_Solutions/CodeWars/6kyu/String_average.py
+++ Python_Solutions/CodeWars/6kyu/String_average.py
@@ -5,3 +5,9 @@
 # If the string is empty or includes a number greater than 9, return "n/a"
 #
 # STRINGSALGORITHMS
+# Solution
+def average_string(s):
+    d = {'zero':0, 'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8, 'nine':9}
+    if s and all(i in d for i in s.split()):
+        return list(d.keys())[list(d.values()).index(int(sum(d[i] for i in s.split()) / len(s.split())))]
+    return 'n/a'
