9cc51092e6031153ede86e88950cddeb521abfd2
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-25 21:53:28 +0300

    Added solution "Roman Numerals Encoder"

--- Python_Solutions/CodeWars/6kyu/Roman_Numerals_Encoder.py
+++ Python_Solutions/CodeWars/6kyu/Roman_Numerals_Encoder.py
@@ -20,3 +20,14 @@
 # More about roman numerals - http://en.wikipedia.org/wiki/Roman_numerals
 #
 # ALGORITHMS
+# Solution
+def solution(n):
+    integer = ''
+    rom = [(1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'), (100, 'C'), (90, 'XC'),
+           (50, 'L'), (40, 'XL'), (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')]
+    while n > 0:
+        for k,v in rom:
+            while n >= k:
+                integer += v
+                n -= k
+    return integer
