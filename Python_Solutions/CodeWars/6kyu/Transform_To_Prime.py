582cb66f0ab4405ee70dce6fb69192f9e5485d8c
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-26 15:10:46 +0300

    Added solution "Transform To Prime"

--- Python_Solutions/CodeWars/6kyu/Transform_To_Prime.py
+++ Python_Solutions/CodeWars/6kyu/Transform_To_Prime.py
@@ -27,3 +27,21 @@
 # Enjoy Learning !!
 # Zizou
 # FUNDAMENTALSARRAYS
+# Solution
+def minimum_number(numbers):
+    def sieveOfEratosthenes(n):
+        if n <= 2:
+            return []
+        sieve = list(range(3, n, 2))
+        top = len(sieve)
+        for si in sieve:
+            if si:
+                bottom = (si*si - 3) // 2
+                if bottom >= top:
+                    break
+                sieve[bottom::si] = [0] * -((bottom - top) // si)
+        return [2] + [el for el in sieve if el]
+    lst =  sieveOfEratosthenes(sum(numbers)+100)
+    for elem in lst:
+        if elem >= sum(numbers):
+            return elem - sum(numbers)
