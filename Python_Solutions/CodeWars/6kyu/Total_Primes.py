268c4dba6c52f8d7788ebde0f87fbd3cf2ad75de
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-26 15:09:31 +0300

    Added description "Total Primes"

---
+++ Python_Solutions/CodeWars/6kyu/Total_Primes.py
@@ -0,0 +1,10 @@
+# The number 23 is special in the sense that all of its digits are prime numbers. Furthermore, it's a prime itself. There are 4 such numbers between 10 and 100: 23, 37, 53, 73. Let's call these numbers "total primes".
+#
+# Complete the function that takes a range (a, b) and returns the number of total primes within that range (a <= primes < b). The test ranges go up to 107.
+#
+# Examples
+# (10, 100)  ==> 4  # 23, 37, 53, 73
+# (500, 600) ==> 3  # 523, 557, 577
+# Happy coding!
+#
+# MATHEMATICSALGORITHMS
