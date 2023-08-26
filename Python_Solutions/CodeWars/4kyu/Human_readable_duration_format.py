aa59f10e6ada8970d6172907ece4bf427b4c81cf
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-26 14:21:08 +0300

    Added solution "Human readable duration format"

--- Python_Solutions/CodeWars/4kyu/Human_readable_duration_format.py
+++ Python_Solutions/CodeWars/4kyu/Human_readable_duration_format.py
@@ -26,3 +26,22 @@
 # A unit of time must be used "as much as possible". It means that the function should not return 61 seconds, but 1 minute and 1 second instead. Formally, the duration specified by of a component must not be greater than any valid more significant unit of time.
 #
 # STRINGSDATE TIMEALGORITHMS
+# Solution
+def format_duration(s):
+    if s == 0:
+        return 'now'
+    time = {'year': 365*24*60*60, 'day': 24*60*60, 'hour': 60*60, 'minute': 60, 'second': 1}
+    for k,v in time.items():
+        count = 0
+        while s >= v:
+            s -= v
+            count += 1
+        time[k] = count
+    dates = []
+    for k,v in time.items():
+        if v > 0:
+            dates.append((v, k if v == 1 else k + 's'))
+    if len(dates) > 1:
+        return ', '.join(str(date[0]) + ' ' + date[1] for date in dates[:-1]) \
+    + ' and ' + str(dates[-1][0]) + ' ' + dates[-1][1]
+    return str(dates[0][0]) + ' ' + dates[0][1]
