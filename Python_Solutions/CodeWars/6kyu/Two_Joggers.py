ef59ca48af66152d08be4370a7e0b8eb587dfecb
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-26 17:48:19 +0300

    Added solution "Two Joggers"

--- Python_Solutions/CodeWars/6kyu/Two_Joggers.py
+++ Python_Solutions/CodeWars/6kyu/Two_Joggers.py
@@ -25,3 +25,8 @@
 # nbr_of_laps(5, 3) # returns (3, 5)
 # nbr_of_laps(4, 6) # returns (3, 2)
 # MATHEMATICSALGORITHMS
+# Solution
+def nbr_of_laps(x, y):
+    for i in range(min(x, y), 0, -1):
+        if x % i == 0 and y % i == 0: break
+    return (y // i, x // i)
