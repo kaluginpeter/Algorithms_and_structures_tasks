d921cd4e742a2a59d8d3db77b56e80954526a9ba
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-26 15:11:27 +0300

    Added description "Traverse array elements diagonally"

---
+++ Python_Solutions/CodeWars/6kyu/Traverse_array_elements_diagonally.py
@@ -0,0 +1,28 @@
+# In this kata you're given an n x n array and you're expected to traverse the elements diagonally from the bottom right to the top left.
+#
+# Example
+#   1 6 7
+#   7 2 4
+#   3 5 9
+# your solution should return elements in the following order
+#
+# 9
+# 4 5
+# 7 2 3
+# 6 7
+# 1
+# //=> [9, 4, 5, 7, 2, 3, 6, 7, 1]
+#
+# Your task is to write the function diagonal() that returns the array elements in the above manner.
+#
+# Another Example
+# arr = [
+#  [4, 5, 7],
+#  [3, 9, 1],
+#  [7, 6, 2]
+# ]
+#
+# diagonal(arr) //=> [2, 1, 6, 7, 9, 7, 5, 3, 4]
+# You can assume the test cases are well formed.
+#
+# ARRAYSLOGICALGORITHMS
