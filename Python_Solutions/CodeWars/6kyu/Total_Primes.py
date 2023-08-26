28354c981f912d0dc2aafe07acaba92dcec6ffb4
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-26 15:09:46 +0300

    Added solution "Total Primes"

--- Python_Solutions/CodeWars/6kyu/Total_Primes.py
+++ Python_Solutions/CodeWars/6kyu/Total_Primes.py
@@ -8,3 +8,14 @@
 # Happy coding!
 #
 # MATHEMATICSALGORITHMS
+# Solution
+import gmpy2
+import itertools
+import math
+def get_total_primes(a, b):
+    l, c = [], 0
+    for i in range(math.ceil(math.log10(a)),math.ceil(math.log10(b))+1):
+        l += list(map("".join, itertools.product('2357',repeat = i)))
+    for i in l:
+        if gmpy2.is_prime(int(i)) and int(i) < b and int(i) >= a: c += 1
+    return c
