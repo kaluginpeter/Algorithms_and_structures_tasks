4638674229fd7d0c7439ffc0c6ec794b71d2f552
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-26 17:40:34 +0300

    Added description "Turn String Input into Hash"

---
+++ Python_Solutions/CodeWars/6kyu/Turn_String_Input_into_Hash.py
@@ -0,0 +1,7 @@
+# Please write a function that will take a string as input and return a hash. The string will be formatted as such. The key will always be a symbol and the value will always be an integer.
+#
+# "a=1, b=2, c=3, d=4"
+# This string should return a hash that looks like
+#
+# { 'a': 1, 'b': 2, 'c': 3, 'd': 4}
+# PARSINGALGORITHMS
