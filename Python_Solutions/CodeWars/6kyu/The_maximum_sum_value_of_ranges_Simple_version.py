07c88e23a8d092a912f0d91ad163a08763374c3f
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-26 15:01:15 +0300

    Added description "The maximum sum value of ranges -- Simple version"

---
+++ Python_Solutions/CodeWars/6kyu/The_maximum_sum_value_of_ranges_Simple_version.py
@@ -0,0 +1,24 @@
+# When no more interesting kata can be resolved, I just choose to create the new kata, to solve their own, to enjoy the process --myjinxin2015 said
+#
+# Description:
+# For example:
+#
+#  Given arr = [1,-2,3,4,-5,-4,3,2,1],
+#        range = [[1,3],[0,4],[6,8]]
+#  should return 6
+#
+#  calculation process:
+#  range[1,3] = arr[1]+arr[2]+arr[3] = 5
+#  range[0,4] = arr[0]+arr[1]+arr[2]+arr[3]+arr[4] = 1
+#  range[6,8] = arr[6]+arr[7]+arr[8] = 6
+#  So the maximum sum value is 6
+# Note:
+# arr/$a always has at least 5 elements;
+# range/$range/ranges always has at least 1 element;
+# All inputs are valid;
+# This is a simple version, if you want some challenge, please try the challenge version.
+# Some Examples
+#  maxSum([1,-2,3,4,-5,-4,3,2,1],[[1,3],[0,4],[6,8]]) === 6
+#  maxSum([1,-2,3,4,-5,-4,3,2,1],[[1,3]]) === 5
+#  maxSum([1,-2,3,4,-5,-4,3,2,1],[[1,4],[2,5]]) === 0
+# FUNDAMENTALS
