fb62b422896d28cc1987a4f39eee92b42ba26c5a
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-26 14:58:54 +0300

    Added solution "The Vowel Code"

--- Python_Solutions/CodeWars/6kyu/The_Vowel_Code.py
+++ Python_Solutions/CodeWars/6kyu/The_Vowel_Code.py
@@ -14,3 +14,12 @@
 # For the sake of simplicity, you can assume that any numbers passed into the function will correspond to vowels.
 #
 # ARRAYSSTRINGSREGULAR EXPRESSIONSFUNDAMENTALS
+# Solution
+def encode(st):
+    dict = {'a': '1', 'e': '2', 'i': '3', 'o': '4', 'u': '5'}
+    return ''.join([dict[char] if char in dict else char for char in st])
+
+
+def decode(st):
+    dict = {'1': 'a', '2': 'e', '3': 'i', '4': 'o', '5': 'u'}
+    return ''.join([dict[char] if char in dict else char for char in st])
