79f8e27f815dca03377f7209790632a8b4452286
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-27 20:36:57 +0300

    Added description "Baby shark lyrics generator"

---
+++ Python_Solutions/CodeWars/7kyu/Baby_shark_lyrics_generator.py
@@ -0,0 +1,34 @@
+# Baby Shark Lyrics
+# baby shark
+#
+# Create a function, as short as possible, that returns this lyrics.
+# Your code should be less than 300 characters. Watch out for the three points at the end of the song.
+#
+# Baby shark, doo doo doo doo doo doo
+# Baby shark, doo doo doo doo doo doo
+# Baby shark, doo doo doo doo doo doo
+# Baby shark!
+# Mommy shark, doo doo doo doo doo doo
+# Mommy shark, doo doo doo doo doo doo
+# Mommy shark, doo doo doo doo doo doo
+# Mommy shark!
+# Daddy shark, doo doo doo doo doo doo
+# Daddy shark, doo doo doo doo doo doo
+# Daddy shark, doo doo doo doo doo doo
+# Daddy shark!
+# Grandma shark, doo doo doo doo doo doo
+# Grandma shark, doo doo doo doo doo doo
+# Grandma shark, doo doo doo doo doo doo
+# Grandma shark!
+# Grandpa shark, doo doo doo doo doo doo
+# Grandpa shark, doo doo doo doo doo doo
+# Grandpa shark, doo doo doo doo doo doo
+# Grandpa shark!
+# Let's go hunt, doo doo doo doo doo doo
+# Let's go hunt, doo doo doo doo doo doo
+# Let's go hunt, doo doo doo doo doo doo
+# Let's go hunt!
+# Run away,…
+# Good Luck!
+#
+# STRINGSLISTSFUNDAMENTALSRESTRICTED
