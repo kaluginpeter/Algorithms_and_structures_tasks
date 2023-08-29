bee68eb986bcac6858cc7bfe860cf8c02d3d1458
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-29 22:21:25 +0300

    Added description "All Star Code Challenge #3"

---
+++ Python_Solutions/CodeWars/7kyu/All_Star_Code_Challenge_#3.py
@@ -0,0 +1,7 @@
+# This Kata is intended as a small challenge for my students
+#
+# Create a function, called removeVowels (or remove_vowels), that takes a string argument and returns that same string with all vowels removed (vowels are "a", "e", "i", "o", "u").
+#
+# removeVowels("drake") // => "drk"
+# removeVowels("aeiou") // => ""
+# FUNDAMENTALS
