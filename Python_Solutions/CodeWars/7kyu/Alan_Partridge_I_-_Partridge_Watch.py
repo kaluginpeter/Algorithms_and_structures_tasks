f6a36591f6bf9cb5a91956ead4bc6085a09ae471
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-29 22:12:12 +0300

    Added description "Alan Partridge I - Partridge Watch"

---
+++ Python_Solutions/CodeWars/7kyu/Alan_Partridge_I_-_Partridge_Watch.py
@@ -0,0 +1,24 @@
+# Backstory
+#
+# To celebrate today's launch of my Hero's new book: Alan Partridge: Nomad, We have a new series of kata arranged around the great man himself.
+#
+# Task
+# Given an array of terms, if any of those terms relate to Alan Partridge, return Mine's a Pint!
+#
+# The number of exclamation mark (!) after the t should be determined by the number of Alan related terms you find in the given array (x). The related terms are as follows:
+#
+# Partridge
+# PearTree
+# Chat
+# Dan
+# Toblerone
+# Lynn
+# AlphaPapa
+# Nomad
+# If you don't find any related terms, return Lynn, I've pierced my foot on a spike!!
+#
+# Other katas in this series:
+# Alan Partridge II - Apple Turnover
+# Alan Partridge III - London
+#
+# FUNDAMENTALSARRAYSSTRINGS
