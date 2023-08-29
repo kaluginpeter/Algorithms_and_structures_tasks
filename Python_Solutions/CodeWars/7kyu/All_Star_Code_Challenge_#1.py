200c82cfe7bb96c9a078c825598dc5f8147b2c22
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-29 22:15:37 +0300

    Added solution "All Star Code Challenge #1"

--- Python_Solutions/CodeWars/7kyu/All_Star_Code_Challenge_#1.py
+++ Python_Solutions/CodeWars/7kyu/All_Star_Code_Challenge_#1.py
@@ -9,3 +9,6 @@
 # jordan =  { 'team': 'bulls', 'ppg': 20.2 }
 # sum_ppg(iverson, jordan) # => 31.4
 # FUNDAMENTALS
+# Solution
+def sum_ppg(player_one, player_two):
+    return player_one['ppg'] + player_two['ppg']
