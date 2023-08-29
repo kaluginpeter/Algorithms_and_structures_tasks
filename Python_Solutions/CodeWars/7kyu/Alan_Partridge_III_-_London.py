adc019ba2cdb7d4d382eca415d1f04a37df9910f
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-29 22:13:14 +0300

    Added description "Alan Partridge III - London"

---
+++ Python_Solutions/CodeWars/7kyu/Alan_Partridge_III_-_London.py
@@ -0,0 +1,20 @@
+# Backstory
+#
+# Ever the learned traveller, Alan Partridge has pretty strong views on London:
+#
+# "Go to London. I guarantee you'll either be mugged or not appreciated.
+# Catch the train to London, stopping at Rejection, Disappointment, Backstabbing Central and Shattered Dreams Parkway."
+# Task
+# Your job is to check that the provided list / array of stations contains all of the stops Alan mentions. The list of stops are as follows:
+#
+# Rejection
+# Disappointment
+# Backstabbing Central
+# Shattered Dreams Parkway
+# If all the stops appear in the given list / array, return Smell my cheese you mother!, if not, return No, seriously, run. You will miss it..
+#
+# Other katas in this series:
+# Alan Partridge I - Partridge Watch
+# Alan Partridge II - Apple Turnover
+#
+# FUNDAMENTALSARRAYSSTRINGS
