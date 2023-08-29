7672a4d96a58bf84f453604d4e800d4f021e75c9
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-29 22:15:17 +0300

    Added description "All Star Code Challenge #1"

---
+++ Python_Solutions/CodeWars/7kyu/All_Star_Code_Challenge_#1.py
@@ -0,0 +1,11 @@
+# This Kata is intended as a small challenge for my students
+#
+# All Star Code Challenge #1
+#
+# Write a function, called sumPPG, that takes two NBA player objects/struct/Hash/Dict/Class and sums their PPG
+#
+# Examples:
+# iverson = { 'team': '76ers', 'ppg': 11.2 }
+# jordan =  { 'team': 'bulls', 'ppg': 20.2 }
+# sum_ppg(iverson, jordan) # => 31.4
+# FUNDAMENTALS
