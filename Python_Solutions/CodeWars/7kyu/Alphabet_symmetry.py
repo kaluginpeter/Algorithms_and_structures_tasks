e3ff5b96e17da03b67886b9e76b64e2e2fcba52e
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-29 22:43:20 +0300

    Added solution "Alphabet symmetry"

--- Python_Solutions/CodeWars/7kyu/Alphabet_symmetry.py
+++ Python_Solutions/CodeWars/7kyu/Alphabet_symmetry.py
@@ -16,3 +16,11 @@
 # Alternate capitalization
 #
 # STRINGSFUNDAMENTALS
+# Solution
+def solve(arr):
+    output = []
+    for str in arr:
+        str = list(map(lambda x: ord(x.lower())-96, list(str)))
+        str = list(map(lambda x: x[0] == x[1], zip(str, range(1,len(str)+1))))
+        output.append(str.count(True))
+    return output
