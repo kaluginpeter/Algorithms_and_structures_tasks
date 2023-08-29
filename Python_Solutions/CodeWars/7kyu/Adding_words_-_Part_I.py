b317e9744f68f88bf85d1f47c022e486bb6eb19d
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-29 21:57:42 +0300

    Added description "Adding words - Part I"

---
+++ Python_Solutions/CodeWars/7kyu/Adding_words_-_Part_I.py
@@ -0,0 +1,14 @@
+# This is the first part of this kata series. Second part is here and third part is here
+#
+# Add two English words together!
+#
+# Implement a class Arith (struct struct Arith{value : &'static str,} in Rust) such that
+#
+#   //javascript
+#   var k = new Arith("three");
+#   k.add("seven"); //this should return "ten"
+# Input - Will be between zero and ten and will always be in lower case
+#
+# Output - Word representation of the result of the addition. Should be in lower case
+#
+# MATHEMATICSFUNDAMENTALS
