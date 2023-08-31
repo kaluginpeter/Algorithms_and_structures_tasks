f81ade73944a3e0f934ec346ac045ee6a5eac31b
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-31 20:39:06 +0300

    Added solution "Sum Strings as Numbers"

--- Python_Solutions/CodeWars/4kyu/Sum_Strings_as_Numbers.py
+++ Python_Solutions/CodeWars/4kyu/Sum_Strings_as_Numbers.py
@@ -10,3 +10,28 @@
 # Python: your solution need to work with huge numbers (about a milion digits), converting to int will not work.
 #
 # STRINGSBIG INTEGERSALGORITHMS
+# Solution
+def sum_strings(x, y):
+    if len(x)> len(y):
+        temp = x
+        x = y
+        y = temp
+    str3, n1, n2 = "", len(x), len(y)
+    diff, carry = n2 - n1, 0
+    for i in range(n1-1, -1, -1):
+        sum = ((ord(x[i]) - ord('0')) + int((ord(y[i + diff]) - ord('0'))) + carry)
+        str3 = str3 + str(sum%10)
+        carry = sum//10
+    for i in range(n2-n1-1, -1, -1):
+        sum = ((ord(y[i]) - ord('0')) + carry)
+        str3 = str3 + str(sum%10 )
+        carry = sum//10
+    if carry:
+        print(carry, str3)
+        str3 += str(carry)
+    str3 = str3[::-1]
+    if not str3:
+        return '0'
+    if str3[0] == '0':
+        str3 = str3[1:]
+    return str3 if str3 else '0'
