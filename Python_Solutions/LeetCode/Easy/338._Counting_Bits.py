ae9014efb3d9c8a883e9ce57abbfd974b2a6efa8
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-09-01 20:36:47 +0300

    Added solution "338. Counting Bits"

--- Python_Solutions/LeetCode/Easy/338._Counting_Bits.py
+++ Python_Solutions/LeetCode/Easy/338._Counting_Bits.py
@@ -36,3 +36,10 @@ class Solution:
         for i in range(1, num+1):
             ret.append(ret[i>>1] + (i&1))
         return ret
+# Solution 2
+class Solution:
+    def countBits(self, num):
+        ret = [0] * (num + 1)
+        for i in range(1, num+1):
+            ret[i] = ret[i>>1] + (i&1)
+        return ret
