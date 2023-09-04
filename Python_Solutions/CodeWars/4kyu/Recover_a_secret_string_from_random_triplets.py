8e3b01a455d8b43cb7c21003fd1d4791467dc4e8
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-09-04 22:29:10 +0300

    Added solution "Recover a secret string from random triplets"

--- Python_Solutions/CodeWars/4kyu/Recover_a_secret_string_from_random_triplets.py
+++ Python_Solutions/CodeWars/4kyu/Recover_a_secret_string_from_random_triplets.py
@@ -7,3 +7,26 @@
 # You can assume nothing about the triplets given to you other than that they are valid triplets and that they contain sufficient information to deduce the original string. In particular, this means that the secret string will never contain letters that do not occur in one of the triplets given to you.
 #
 # ALGORITHMS
+# Solution
+def recoverSecret(triplets):
+    unique_letters = set()
+    letter_order = {}
+    for triplet in triplets:
+        for letter in triplet:
+            unique_letters.add(letter)
+    for triplet in triplets:
+        for i in range(2):
+            if triplet[i + 1] not in letter_order:
+                letter_order[triplet[i + 1]] = set()
+            letter_order[triplet[i + 1]].add(triplet[i])
+    sorted_letters = []
+    while unique_letters:
+        for letter in unique_letters:
+            if letter not in letter_order or not letter_order[letter]:
+                sorted_letters.append(letter)
+                unique_letters.remove(letter)
+                for key in letter_order:
+                    if letter in letter_order[key]:
+                        letter_order[key].remove(letter)
+                break
+    return ''.join(sorted_letters)
