a18ec00155836121b0f4d12162a2c6975b670efa
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-09-09 13:32:37 +0300

    Added solution "Ninja vs Samurai: Strike"

--- Python_Solutions/CodeWars/7kyu/Ninja_vs_Samurai_Strike.py
+++ Python_Solutions/CodeWars/7kyu/Ninja_vs_Samurai_Strike.py
@@ -8,3 +8,13 @@
 # Can you figure out what is wrong?
 #
 # DEBUGGING
+# Solution
+class Warrior:
+    def __init__(self, name):
+        self.name = name
+        self.health = 100
+
+    def strike(self, enemy, swings):
+        # health cannot go below zero
+        enemy.health = max([0, enemy.health - (swings * 10)])
+        return enemy.health
