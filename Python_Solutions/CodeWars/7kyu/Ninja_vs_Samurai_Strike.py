0fe9659b7f26489a259326d1777808f75e272c27
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-09-09 13:32:23 +0300

    Added description "Ninja vs Samurai: Strike"

---
+++ Python_Solutions/CodeWars/7kyu/Ninja_vs_Samurai_Strike.py
@@ -0,0 +1,10 @@
+# Something is wrong with our Warrior class. The strike method does not work correctly. The following shows an example of this code being used:
+#
+# ninja = Warrior('Ninja')
+# samurai = Warrior('Samurai')
+#
+# samurai.strike(ninja, 3)
+# # ninja.health should == 70
+# Can you figure out what is wrong?
+#
+# DEBUGGING
