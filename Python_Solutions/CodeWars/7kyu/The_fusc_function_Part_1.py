3f397d4b8d562b1ea44e5398ed6025b0357d664f
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-09-13 21:36:55 +0300

    Added solution "The fusc function -- Part 1"

--- Python_Solutions/CodeWars/7kyu/The_fusc_function_Part_1.py
+++ Python_Solutions/CodeWars/7kyu/The_fusc_function_Part_1.py
@@ -21,3 +21,12 @@
 # When done, move on to Part 2.
 #
 # ALGORITHMS
+# Solution
+def fusc(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    if n % 2 == 0:
+        return fusc(n / 2)
+    return fusc(n // 2) + fusc(n // 2 + 1)
