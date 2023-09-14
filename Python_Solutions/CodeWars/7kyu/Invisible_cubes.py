e88a5554572ff81100622476dfe8a4b2684a0c87
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-09-14 20:26:22 +0300

    Added description "Invisible cubes"

---
+++ Python_Solutions/CodeWars/7kyu/Invisible_cubes.py
@@ -0,0 +1,12 @@
+# Imagine there's a big cube consisting of
+# �
+# 3
+# n
+# 3
+#   small cubes. Calculate, how many small cubes are not visible from outside.
+#
+# For example, if we have a cube which has 4 cubes in a row, then the function should return 8, because there are 8 cubes inside our cube (2 cubes in each dimension)
+#
+# For a visual representation: --> https://imgur.com/a/AN8A5DJ
+#
+# PUZZLES
