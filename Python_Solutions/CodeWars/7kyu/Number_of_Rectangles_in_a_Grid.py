23a07ac2a863b99da915f92b73b25a7662626f15
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-09-15 19:21:46 +0300

    Added solution "Number of Rectangles in a Grid"

--- Python_Solutions/CodeWars/7kyu/Number_of_Rectangles_in_a_Grid.py
+++ Python_Solutions/CodeWars/7kyu/Number_of_Rectangles_in_a_Grid.py
@@ -32,3 +32,10 @@
 # There is a very simple solution to this!
 #
 # GEOMETRYPUZZLES
+# Solution
+def number_of_rectangles(m, n):
+    count = 0
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            count += i * j
+    return count
