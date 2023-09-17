f2579b174d83af2203d46e45e37aef2b0e1ce69a
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-09-17 16:27:24 +0300

    Added description "Keypad horror"

---
+++ Python_Solutions/CodeWars/7kyu/Keypad_horror.py
@@ -0,0 +1,22 @@
+# Having two standards for a keypad layout is inconvenient!
+# Computer keypad's layout:
+# 7 8 9  \n
+# 4 5 6  \n
+# 1 2 3  \n
+#   0 \n
+#
+# Cell phone keypad's layout:
+# 1 2 3\n
+# 4 5 6\n
+# 7 8 9\n
+#   0\n
+#
+# Solve the horror of unstandardized keypads by providing a function that converts computer input to a number as if it was typed on a phone.
+#
+# Example:
+# "789" -> "123"
+#
+# Notes:
+# You get a string with numbers only
+#
+# STRINGS
