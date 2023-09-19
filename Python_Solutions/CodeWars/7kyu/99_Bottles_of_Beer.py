10b9afaf99151ad119e52d8faf522756de485fe8
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-09-19 21:35:41 +0300

    Added description "99 Bottles of Beer"

---
+++ Python_Solutions/CodeWars/7kyu/99_Bottles_of_Beer.py
@@ -0,0 +1,42 @@
+# Instructions
+# Complete the function that returns the lyrics for the song 99 Bottles of Beer as an array of strings: each line should be a separate element - see the example at the bottom.
+#
+# Note: in order to avoid hardcoded solutions, the size of your code is limited to 1000 characters
+#
+# Lyrics
+# 99 bottles of beer on the wall, 99 bottles of beer.
+# Take one down and pass it around, 98 bottles of beer on the wall.
+#
+# 98 bottles of beer on the wall, 98 bottles of beer.
+# Take one down and pass it around, 97 bottles of beer on the wall.
+#
+# ...and so on...
+#
+# 3 bottles of beer on the wall, 3 bottles of beer.
+# Take one down and pass it around, 2 bottles of beer on the wall.
+#
+# 2 bottles of beer on the wall, 2 bottles of beer.
+# Take one down and pass it around, 1 bottle of beer on the wall.
+#
+# 1 bottle of beer on the wall, 1 bottle of beer.
+# Take one down and pass it around, no more bottles of beer on the wall.
+#
+# No more bottles of beer on the wall, no more bottles of beer.
+# Go to the store and buy some more, 99 bottles of beer on the wall.
+#
+# Example
+# [ "99 bottles of beer on the wall, 99 bottles of beer.",
+#   "Take one down and pass it around, 98 bottles of beer on the wall.",
+#   "98 bottles of beer on the wall, 98 bottles of beer.",
+#
+#   ...and so on...
+#
+#   "3 bottles of beer on the wall, 3 bottles of beer.",
+#   "Take one down and pass it around, 2 bottles of beer on the wall.",
+#   "2 bottles of beer on the wall, 2 bottles of beer.",
+#   "Take one down and pass it around, 1 bottle of beer on the wall.",
+#   "1 bottle of beer on the wall, 1 bottle of beer.",
+#   "Take one down and pass it around, no more bottles of beer on the wall.",
+#   "No more bottles of beer on the wall, no more bottles of beer.",
+#   "Go to the store and buy some more, 99 bottles of beer on the wall." ]
+# STRINGSALGORITHMS
