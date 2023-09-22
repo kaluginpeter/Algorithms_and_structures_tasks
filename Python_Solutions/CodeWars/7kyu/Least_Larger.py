0c56a6593806206a3627b97f8aee69bf774cc8d3
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-22 18:49:39 +0300

    Added description "Least Larger"

---
+++ Python_Solutions/CodeWars/7kyu/Least_Larger.py
@@ -0,0 +1,12 @@
+# Task
+# Given an array of numbers and an index, return either the index of the smallest number that is larger than the element at the given index, or -1 if there is no such index ( or, where applicable, Nothing or a similarly empty value ).
+#
+# Notes
+# Multiple correct answers may be possible. In this case, return any one of them.
+# The given index will be inside the given array.
+# The given array will, therefore, never be empty.
+#
+# Example
+# least_larger( [4, 1, 3, 5, 6], 0 )  ->  3
+# least_larger( [4, 1, 3, 5, 6], 4 )  -> -1
+# ALGORITHMSARRAYS
