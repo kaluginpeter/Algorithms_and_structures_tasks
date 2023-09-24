ca69b2931fb07f6f4b522c77f00706e66830d631
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-24 18:08:56 +0300

    Added description "Alphabetically ordered"

---
+++ Python_Solutions/CodeWars/7kyu/Alphabetically_ordered.py
@@ -0,0 +1,18 @@
+# Your task is very simple. Just write a function that takes an input string of lowercase letters and returns true/false depending on whether the string is in alphabetical order or not.
+#
+# Examples (input -> output)
+# "kata" -> false ('a' comes after 'k')
+# "ant" -> true (all characters are in alphabetical order)
+# Good luck :)
+#
+# Check my other katas:
+#
+# Find Nearest square number
+#
+# Case-sensitive!
+#
+# Not prime numbers
+#
+# Find your caterer
+#
+# STRINGSFUNDAMENTALS
