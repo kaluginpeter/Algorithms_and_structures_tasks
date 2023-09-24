dbaf7919eebab207d03995123a75da8935f41645
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-24 18:06:42 +0300

    Added solution "Alphabet war"

--- Python_Solutions/CodeWars/7kyu/Alphabet_war.py
+++ Python_Solutions/CodeWars/7kyu/Alphabet_war.py
@@ -31,3 +31,19 @@
 # Alphabet wars - nuclear strike
 # Alphabet war - Wo lo loooooo priests join the war
 # FUNDAMENTALSSTRINGS
+# Solution
+def alphabet_war(fight):
+    left_side = {'w': 4, 'p': 3, 'b': 2, 's': 1}
+    right_side = {'m': 4, 'q': 3, 'd': 2, 'z': 1}
+    left_count = 0
+    right_count = 0
+    for char in fight:
+        if char in left_side:
+            left_count += left_side[char]
+        elif char in right_side:
+            right_count += right_side[char]
+    if left_count > right_count:
+        return 'Left side wins!'
+    elif right_count > left_count:
+        return 'Right side wins!'
+    return "Let's fight again!"
