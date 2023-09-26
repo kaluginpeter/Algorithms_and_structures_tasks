45ecb29ad850c025efd66efb6a6c9f560a4696b9
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-26 21:46:54 +0300

    Added description "Calculate Meal Total"

---
+++ Python_Solutions/CodeWars/7kyu/Calculate_Meal_Total.py
@@ -0,0 +1,5 @@
+# Create a function that returns the total of a meal including tip and tax. You should not tip on the tax.
+#
+# You will be given the subtotal, the tax as a percentage and the tip as a percentage. Please round your result to two decimal places.
+#
+# FUNDAMENTALS
