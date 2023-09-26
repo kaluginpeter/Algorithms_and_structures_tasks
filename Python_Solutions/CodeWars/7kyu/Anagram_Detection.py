378e2fbd0b5ba6404057655bb58dc792ec754350
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-26 19:44:55 +0300

    Added solution "Anagram Detection"

--- Python_Solutions/CodeWars/7kyu/Anagram_Detection.py
+++ Python_Solutions/CodeWars/7kyu/Anagram_Detection.py
@@ -10,3 +10,6 @@
 # "Buckethead" is an anagram of "DeathCubeK"
 #
 # STRINGSFUNDAMENTALS
+# Solution
+def is_anagram(test, original):
+    return set(test.lower()) == set(original.lower()) and len(test) == len(original)
