d448fe01f78765632df6499354b83220dcfacce2
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-26 19:21:43 +0300

    Added solution "A Letter's Best Friend"

--- Python_Solutions/CodeWars/8kyu/A_Letter's_Best_Friend.py
+++ Python_Solutions/CodeWars/8kyu/A_Letter's_Best_Friend.py
@@ -20,3 +20,12 @@
 # Notes
 # All sentences will be given in lowercase.
 # FUNDAMENTALSSTRINGS
+# Solution
+def best_friend(txt, a, b):
+    for i in range(len(txt) - 1):
+        if txt[i] == a:
+            if txt[i + 1] != b:
+                return False
+    if txt[-1] == a:
+        return False
+    return True
