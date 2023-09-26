4f787db6b9fec657fe293102748a3970c660e646
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-26 19:56:33 +0300

    Added description "Array comparator"

---
+++ Python_Solutions/CodeWars/7kyu/Array_comparator.py
@@ -0,0 +1,3 @@
+# You have two arrays in this kata, every array contains unique elements only. Your task is to calculate number of elements in the first array which are also present in the second array.
+#
+# ARRAYSFUNDAMENTALS
