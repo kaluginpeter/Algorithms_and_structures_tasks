a9abbdbc7896454e8bad47b5f36fd16719c4d46c
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-26 21:59:22 +0300

    Added description "Changing letters"

---
+++ Python_Solutions/CodeWars/7kyu/Changing_letters.py
@@ -0,0 +1,11 @@
+# When provided with a String, capitalize all vowels
+#
+# For example:
+#
+# Input : "Hello World!"
+#
+# Output : "HEllO WOrld!"
+#
+# Note: Y is not a vowel in this kata.
+#
+# FUNDAMENTALSSTRINGS
