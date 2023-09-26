3abc773ad53798b74d45caeabe60fe3f65999b3b
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-26 21:46:10 +0300

    Added description "Caffeine Script"

---
+++ Python_Solutions/CodeWars/7kyu/Caffeine_Script.py
@@ -0,0 +1,16 @@
+# Complete the function which takes a non-zero integer as its argument.
+#
+# If the integer is divisible by 3, return the string "Java".
+#
+# If the integer is divisible by 3 and divisible by 4, return the string "Coffee"
+#
+# If one of the condition above is true and the integer is even, add "Script" to the end of the string.
+#
+# If none of the condition is true, return the string "mocha_missing!"
+#
+# Examples
+# 1   -->  "mocha_missing!"
+# 3   -->  "Java"
+# 6   -->  "JavaScript"
+# 12  -->  "CoffeeScript"
+# FUNDAMENTALSSTRINGS
