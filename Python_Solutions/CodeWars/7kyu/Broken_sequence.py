acdfeb02fa1c99357fd8690d346c2d5a3a847c74
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-26 21:37:38 +0300

    Added solution "Broken sequence"

--- Python_Solutions/CodeWars/7kyu/Broken_sequence.py
+++ Python_Solutions/CodeWars/7kyu/Broken_sequence.py
@@ -16,3 +16,15 @@
 #
 # "1 5"      ==>  return 2, because the sequence is missing 2, 3, 4 and 2 is the lowest
 # STRINGSFUNDAMENTALS
+# Solution
+def find_missing_number(sequence):
+    if not sequence:
+        return 0
+    try:
+        sequence = set(int(a) for a in sequence.split())
+    except ValueError:
+        return 1
+    for b in range(1, max(sequence) + 1):
+        if b not in sequence:
+            return b
+    return 0
