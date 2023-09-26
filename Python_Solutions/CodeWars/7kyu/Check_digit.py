ba14c915d6c4f4ce8e3f29543bf006c7a44d16ce
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-26 22:02:09 +0300

    Added solution "Check digit"

--- Python_Solutions/CodeWars/7kyu/Check_digit.py
+++ Python_Solutions/CodeWars/7kyu/Check_digit.py
@@ -28,3 +28,6 @@
 #   checkDigit(87996599994565, 2, 5, 1) -> false, 1 doesn't exist in 9965
 #
 # FUNDAMENTALSSTRINGSPUZZLES
+# Solution
+def check_digit(number, index1, index2, digit):
+    return str(digit) in str(number)[min(index1, index2):max(index1, index2)+1]
