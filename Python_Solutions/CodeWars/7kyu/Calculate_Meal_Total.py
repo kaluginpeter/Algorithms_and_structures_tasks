f929afc8af2baa2c1889eefe74fc5592abe80dfa
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-26 21:47:21 +0300

    Added solution "Calculate Meal Total"

--- Python_Solutions/CodeWars/7kyu/Calculate_Meal_Total.py
+++ Python_Solutions/CodeWars/7kyu/Calculate_Meal_Total.py
@@ -3,3 +3,6 @@
 # You will be given the subtotal, the tax as a percentage and the tip as a percentage. Please round your result to two decimal places.
 #
 # FUNDAMENTALS
+# Solution
+def calculate_total(subtotal, tax, tip):
+    return round(subtotal * (1 + tax / 100 + tip /100), 2)
