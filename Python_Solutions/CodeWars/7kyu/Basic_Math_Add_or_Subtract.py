816312895728baa46bb55a80053a071d9c99450e
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-26 20:29:59 +0300

    Added solution "Basic Math (Add or Subtract)"

--- Python_Solutions/CodeWars/7kyu/Basic_Math_Add_or_Subtract.py
+++ Python_Solutions/CodeWars/7kyu/Basic_Math_Add_or_Subtract.py
@@ -6,3 +6,9 @@
 # "1plus2plus3plus4"  --> "10"
 # "1plus2plus3minus4" -->  "2"
 # STRINGSFUNDAMENTALS
+# Solution
+import re
+def calculate(s):
+    s = re.sub('plus', '+', s)
+    s = re.sub('minus','-', s)
+    return str(eval(s))
