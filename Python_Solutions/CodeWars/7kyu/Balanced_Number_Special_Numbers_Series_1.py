f5dfad9ac7813d213cb71864a7ac162fece516f5
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-26 20:04:04 +0300

    Added solution "Balanced Number (Special Numbers Series #1 )"

--- Python_Solutions/CodeWars/7kyu/Balanced_Number_Special_Numbers_Series_1.py
+++ Python_Solutions/CodeWars/7kyu/Balanced_Number_Special_Numbers_Series_1.py
@@ -37,3 +37,9 @@
 # Playing With Lists/Arrays Series
 #
 # FUNDAMENTALSMATHEMATICS
+# Solution
+def balanced_num(number):
+    s = str(number)
+    l = (len(s)-1)//2
+    same = len(s) < 3 or sum(map(int, s[:l])) == sum(map(int, s[-l:]))
+    return "Balanced" if same else "Not Balanced"
