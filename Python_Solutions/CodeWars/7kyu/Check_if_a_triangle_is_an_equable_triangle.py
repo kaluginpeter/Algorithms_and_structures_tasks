bfc8835216b7a5496542a212adbad1ba0e65dd11
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-26 22:03:40 +0300

    Added solution "Check if a triangle is an equable triangle!"

--- Python_Solutions/CodeWars/7kyu/Check_if_a_triangle_is_an_equable_triangle.py
+++ Python_Solutions/CodeWars/7kyu/Check_if_a_triangle_is_an_equable_triangle.py
@@ -1,3 +1,7 @@
 # A triangle is called an equable triangle if its area equals its perimeter. Return true, if it is an equable triangle, else return false. You will be provided with the length of sides of the triangle. Happy Coding!
 #
 # FUNDAMENTALSGEOMETRY
+# Solution
+def equable_triangle(a,b,c):
+    p = (a + b + c) / 2
+    return (p*(p-a)*(p-b)*(p-c)) ** .5 == a + b + c
