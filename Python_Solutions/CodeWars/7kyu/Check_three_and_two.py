e0002eb287644061703bbeb0da5b77e60e393167
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-26 22:06:13 +0300

    Added solution "Check three and two"

--- Python_Solutions/CodeWars/7kyu/Check_three_and_two.py
+++ Python_Solutions/CodeWars/7kyu/Check_three_and_two.py
@@ -5,3 +5,12 @@
 # ["a", "b", "c", "b", "c"] ==> false // 1x "a", 2x "b" and 2x "c"
 # ["a", "a", "a", "a", "a"] ==> false // 5x "a"
 # ARRAYSFUNDAMENTALS
+# Solution
+import collections
+def check_three_and_two(array):
+    count_letter = collections.Counter()
+    for letter in array:
+        count_letter[letter] += 1
+    if max(count_letter.values()) == 3 and min(count_letter.values()) == 2:
+        return True
+    return False
