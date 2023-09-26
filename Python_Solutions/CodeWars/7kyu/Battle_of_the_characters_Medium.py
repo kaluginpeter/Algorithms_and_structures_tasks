4d479bff8ae208f60ae8f8134e61cd193f8207d3
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-26 20:34:01 +0300

    Added description "Battle of the characters (Medium)"

---
+++ Python_Solutions/CodeWars/7kyu/Battle_of_the_characters_Medium.py
@@ -0,0 +1,16 @@
+# Groups of characters decided to make a battle. Help them to figure out what group is more powerful. Create a function that will accept 2 variables and return the one who's stronger.
+#
+# Rules
+# Each character has its own power:
+#   A = 1, B = 2, ... Y = 25, Z = 26
+#   a = 0.5, b = 1, ... y = 12.5, z = 13
+# Only alphabetical characters can and will participate in a battle.
+# Only two groups to a fight.
+# Group whose total power (a + B + c + ...) is bigger wins.
+# If the powers are equal, it's a tie.
+# Examples
+# "One", "Two"  -->  "Two"
+# "ONE", "NEO"  -->  "Tie!"
+# Related kata
+# Battle of the characters (Easy)
+# ALGORITHMS
