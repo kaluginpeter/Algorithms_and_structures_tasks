8ed40190c9459c7871b4dd31909ee7c0530def2e
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-26 21:49:51 +0300

    Added description "Calculator: Coin Combination"

---
+++ Python_Solutions/CodeWars/7kyu/Calculator_Coin_Combination.py
@@ -0,0 +1,12 @@
+# The function takes cents value (int) and needs to return the minimum number of coins combination of the same value.
+#
+# The function should return an array where
+# coins[0] = pennies ==> $00.01
+# coins[1] = nickels ==> $00.05
+# coins[2] = dimes ==> $00.10
+# coins[3] = quarters ==> $00.25
+#
+# So for example:
+# coinCombo(6) --> [1, 1, 0, 0]
+#
+# FUNDAMENTALSALGORITHMS
