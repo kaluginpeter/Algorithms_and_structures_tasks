be5d8d8ddd7c9d810d42e7f59c5313c163f48f82
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-26 22:08:59 +0300

    Added solution "Circle area inside square"

--- Python_Solutions/CodeWars/7kyu/Circle_area_inside_square.py
+++ Python_Solutions/CodeWars/7kyu/Circle_area_inside_square.py
@@ -12,3 +12,7 @@
 # This kata might be simpler than you expect, but good luck!
 #
 # MATHEMATICSGEOMETRYALGORITHMS
+# Solution
+from math import pi
+def square_area_to_circle(size):
+    return size * pi / 4
