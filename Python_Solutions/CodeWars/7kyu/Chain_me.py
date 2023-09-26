0c717471eab7216dacc1dc7e7196f8e09fe84ed8
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-26 21:57:28 +0300

    Added description "Chain me"

---
+++ Python_Solutions/CodeWars/7kyu/Chain_me.py
@@ -0,0 +1,11 @@
+# Write a generic function chainer
+# Write a generic function chainer that takes a starting value, and an array of functions to execute on it (array of symbols for Ruby).
+#
+# The input for each function is the output of the previous function (except the first function, which takes the starting value as its input). Return the final value after execution is complete.
+#
+# def add10(x): return x + 10
+# def mul30(x): return x * 30
+#
+# chain(50, [add10, mul30])
+# # returns 1800
+# FUNDAMENTALS
