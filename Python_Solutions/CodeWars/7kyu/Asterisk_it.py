8cdfc48fe9105e6bfe7fd988b34e775339bcaa7a
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-26 19:59:12 +0300

    Added solution "Asterisk it"

--- Python_Solutions/CodeWars/7kyu/Asterisk_it.py
+++ Python_Solutions/CodeWars/7kyu/Asterisk_it.py
@@ -14,3 +14,8 @@
 # Have fun!
 #
 # STRINGSALGORITHMS
+# Solution
+def asterisc_it(n):
+    if type(n) == list: n = ''.join(str(i) for i in n)
+    if type(n) == int : n = str(n)
+    return ''.join([a + '*' if int(a) % 2 == 0 and int(b) % 2 == 0 else a for a,b in zip(n, n[1:])]) + n[-1]
