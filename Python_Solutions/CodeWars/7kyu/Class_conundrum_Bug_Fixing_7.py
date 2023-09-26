4ae32f1c65e10d7c1ef10094d629bab330053dc8
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-26 22:09:49 +0300

    Added solution "Class conundrum - Bug Fixing #7"

--- Python_Solutions/CodeWars/7kyu/Class_conundrum_Bug_Fixing_7.py
+++ Python_Solutions/CodeWars/7kyu/Class_conundrum_Bug_Fixing_7.py
@@ -7,3 +7,16 @@
 #
 # my_list.add(0).add(1)
 # DEBUGGING
+# Solution
+class List:
+    def __init__(self, list_type):
+        self.type = list_type
+        self.items = []
+        self.count = 0
+
+    def add(self, item):
+        if type(item) != self.type:
+            return "This item is not of type: {}".format(self.type.__name__)
+        self.items.append(item)
+        self.count += 1
+        return self
