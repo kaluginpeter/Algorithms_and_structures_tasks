ee91d75cd315452c2f548ef98dd5b0b8b07e9557
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-26 20:50:32 +0300

    Added description "Billiards triangle"

---
+++ Python_Solutions/CodeWars/7kyu/Billiards_triangle.py
@@ -0,0 +1,16 @@
+# Remember the triangle of balls in billiards? To build a classic triangle (5 levels) you need 15 balls. With 3 balls you can build a 2-level triangle, etc.
+#
+# For more examples,
+#
+# pyramid(1) == 1
+#
+# pyramid(3) == 2
+#
+# pyramid(6) == 3
+#
+# pyramid(10) == 4
+#
+# pyramid(15) == 5
+# Write a function that takes number of balls (≥ 1) and calculates how many levels you can build a triangle.
+#
+# MATHEMATICSFUNDAMENTALS
