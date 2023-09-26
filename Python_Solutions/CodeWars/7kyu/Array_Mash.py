5626961a9cb9163430b48a461e5d93f28d882d17
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-26 19:55:51 +0300

    Added description "Array Mash"

---
+++ Python_Solutions/CodeWars/7kyu/Array_Mash.py
@@ -0,0 +1,5 @@
+# Mash 2 arrays together so that the returning array has alternating elements of the 2 arrays . Both arrays will always be the same length.
+#
+# eg. [1,2,3] + ['a','b','c'] = [1, 'a', 2, 'b', 3, 'c']
+#
+# ARRAYSSTRINGSFUNDAMENTALS
