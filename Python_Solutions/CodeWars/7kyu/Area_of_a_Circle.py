af6671b2d3f0969e24438c6fe5fdbe809abd9ddd
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-26 19:47:18 +0300

    Added description "Area of a Circle"

---
+++ Python_Solutions/CodeWars/7kyu/Area_of_a_Circle.py
@@ -0,0 +1,13 @@
+# Complete the function which will return the area of a circle with the given radius.
+#
+# Returned value is expected to be accurate up to tolerance of 0.01.
+#
+# If the radius is not positive, raise ValueError.
+#
+# Example:
+#
+# circle_area(43.2673)      # returns 5881.248  (± 0.01)
+# circle_area(68)           # returns 14526.724 (± 0.01)
+# circle_area(0)            # raises ValueError
+# circle_area(-1)           # raises ValueError
+# FUNDAMENTALSALGORITHMSGEOMETRYMATHEMATICS
