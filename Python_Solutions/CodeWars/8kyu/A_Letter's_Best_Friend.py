8be6aaf82b30bc836753370a1d081dda633f8abc
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-26 19:21:27 +0300

    Added description "A Letter's Best Friend"

---
+++ Python_Solutions/CodeWars/8kyu/A_Letter's_Best_Friend.py
@@ -0,0 +1,22 @@
+# Task
+# Given a string, return if a given letter always appears immediately before another given letter.
+#
+# Worked Example
+# ("he headed to the store", "h", "e") ➞ True
+#
+# # All occurences of "h": ["he", "headed", "the"]
+# # All occurences of "h" have an "e" after it.
+# # Return True
+#
+# ('abcdee', 'e', 'e') ➞ False
+#
+# # For first "e" we can get "ee"
+# # For second "e" we cannot have "ee"
+# # Return False
+# Examples
+# ("i found an ounce with my hound", "o", "u") ➞ True
+#
+# ("we found your dynamite", "d", "y") ➞ False
+# Notes
+# All sentences will be given in lowercase.
+# FUNDAMENTALSSTRINGS
