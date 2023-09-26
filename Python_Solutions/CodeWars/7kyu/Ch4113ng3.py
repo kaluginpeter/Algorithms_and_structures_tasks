34559ae7787e5cb0ea30a35a9c451c1b1bf1d14d
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-26 21:56:56 +0300

    Added solution "Ch4113ng3"

--- Python_Solutions/CodeWars/7kyu/Ch4113ng3.py
+++ Python_Solutions/CodeWars/7kyu/Ch4113ng3.py
@@ -1,3 +1,7 @@
 # Make your strings more nerdy: Replace all 'a'/'A' with 4, 'e'/'E' with 3 and 'l' with 1 e.g. "Fundamentals" --> "Fund4m3nt41s"
 #
 # STRINGSFUNDAMENTALS
+# Solution
+def nerdify(txt):
+    d = {'a': 4, 'e': 3, 'l': 1, 'A': 4, 'E': 3}
+    return ''.join(str(d[i]) if i in d else i for i in txt)
