48cad6090ddae9703c66387b90772f7cdca6c1cf
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-26 18:52:56 +0300

    Added solution "Flick Switch"

--- Python_Solutions/CodeWars/8kyu/Flick_Switch.py
+++ Python_Solutions/CodeWars/8kyu/Flick_Switch.py
@@ -12,3 +12,11 @@
 # A list may contain multiple flicks.
 # Switch the boolean value on the same element as the flick itself.
 # FUNDAMENTALSLISTS
+# Solution
+def flick_switch(lst):
+    ans, flag = [], True
+    for i in lst:
+        if i == 'flick':
+            flag = not flag
+        ans.append(flag)
+    return ans
