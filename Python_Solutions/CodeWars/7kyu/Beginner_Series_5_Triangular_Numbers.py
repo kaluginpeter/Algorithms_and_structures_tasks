9e071fc59846f5f78a1639826e4b0fa587144dee
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-26 20:47:54 +0300

    Added description "Beginner Series #5 Triangular Numbers"

---
+++ Python_Solutions/CodeWars/7kyu/Beginner_Series_5_Triangular_Numbers.py
@@ -0,0 +1,15 @@
+# Triangular number is any amount of points that can fill an equilateral triangle.
+#
+# Example: the number 6 is a triangular number because all sides of a triangle has the same amount of points.
+#
+# alt text
+#
+# Hint!
+# T(n) = n * (n + 1) / 2,
+# n - is the size of one side.
+# T(n) - is the triangular number.
+# Given a number T from interval [1..2147483646], find if it is triangular number or not.
+#
+# Appreciate the feedback!
+#
+# MATHEMATICSFUNDAMENTALS
