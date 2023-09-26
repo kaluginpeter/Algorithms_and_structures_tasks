0f09440138517487d66d1bda9b822ae9ce639ced
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-26 22:10:37 +0300

    Added solution "Clean up after your dog"

--- Python_Solutions/CodeWars/7kyu/Clean_up_after_your_dog.py
+++ Python_Solutions/CodeWars/7kyu/Clean_up_after_your_dog.py
@@ -22,3 +22,13 @@
 # return --> 'Clean'
 #
 # FUNDAMENTALSSTRINGSARRAYSMATHEMATICS
+# Solution
+def crap(garden, bags, cap):
+    c = 0
+    for el in garden:
+        for e in el:
+            if e == "@":
+                c += 1
+            if e == "D":
+                return "Dog!!"
+    return "Clean" if c <= bags * cap else "Cr@p"
