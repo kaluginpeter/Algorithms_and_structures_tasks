488c1d3ea684e6edff185676b70611448b2bd664
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-26 21:41:46 +0300

    Added solution "Building blocks"

--- Python_Solutions/CodeWars/7kyu/Building_blocks.py
+++ Python_Solutions/CodeWars/7kyu/Building_blocks.py
@@ -31,3 +31,19 @@
 # Any feedback would be much appreciated
 #
 # OBJECT-ORIENTED PROGRAMMINGFUNDAMENTALS
+# Solution
+class Block:
+    def __init__(self, measurements):
+        self.width, self.length, self.height = measurements
+    def get_height(self):
+        return self.height
+    def get_length(self):
+        return self.length
+    def get_surface_area(self):
+        return 2 * (self.height * self.length +
+                    self.length * self.width +
+                    self.height * self.width)
+    def get_volume(self):
+        return self.height * self.length * self.width
+    def get_width(self):
+        return self.width
