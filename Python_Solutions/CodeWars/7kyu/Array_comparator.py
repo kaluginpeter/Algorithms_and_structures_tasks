eda29ab205422a95cecfef86c15fb27846c69f12
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-26 19:56:47 +0300

    Added solution "Array comparator"

--- Python_Solutions/CodeWars/7kyu/Array_comparator.py
+++ Python_Solutions/CodeWars/7kyu/Array_comparator.py
@@ -1,3 +1,6 @@
 # You have two arrays in this kata, every array contains unique elements only. Your task is to calculate number of elements in the first array which are also present in the second array.
 #
 # ARRAYSFUNDAMENTALS
+# Solution
+def match_arrays(v, r):
+    return sum(1 for i in v if i in r)
