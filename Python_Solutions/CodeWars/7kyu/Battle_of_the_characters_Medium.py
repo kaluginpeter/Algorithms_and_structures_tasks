82cc05610e056e0e761d910522c30686a51a5c5b
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-26 20:34:18 +0300

    Added solution "Battle of the characters (Medium)"

--- Python_Solutions/CodeWars/7kyu/Battle_of_the_characters_Medium.py
+++ Python_Solutions/CodeWars/7kyu/Battle_of_the_characters_Medium.py
@@ -14,3 +14,9 @@
 # Related kata
 # Battle of the characters (Easy)
 # ALGORITHMS
+# Solution
+def battle(первое_слово: str, второе_слово: str) -> str:
+    алфавит = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    сумма_первого_слова = sum(алфавит.index(буква)+1 if буква.isupper() else (алфавит.index(буква.upper())+1)/2 for буква in первое_слово)
+    сумма_второго_слова = sum(алфавит.index(буква)+1 if буква.isupper() else (алфавит.index(буква.upper())+1)/2 for буква in второе_слово)
+    return первое_слово if сумма_первого_слова > сумма_второго_слова else второе_слово if сумма_второго_слова > сумма_первого_слова else 'Tie!'
