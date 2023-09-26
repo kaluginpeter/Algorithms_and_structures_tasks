506a63f4c2892e3beca37260b6d8ca5498c84d41
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-26 22:09:34 +0300

    Added description "Class conundrum - Bug Fixing #7"

---
+++ Python_Solutions/CodeWars/7kyu/Class_conundrum_Bug_Fixing_7.py
@@ -0,0 +1,9 @@
+# Class conundrum - Bug Fixing #7
+# Oh no! Timmy's List Class has broken! Can you help timmy and fix his class? Timmy has a List class he has created, this is used for type strict arrays (which timmy calls Lists).
+#
+# When timmy calls the Count property of the list it still remains at 0 when adding items.
+#
+# Also it fails when timmy trys to chain the adds e.g.
+#
+# my_list.add(0).add(1)
+# DEBUGGING
