a33989459b858f155d49a386c27b36cf0f059458
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-27 22:11:34 +0300

    Added description "Counter of neighbor ones"

---
+++ Python_Solutions/CodeWars/7kyu/Counter_of_neighbor_ones.py
@@ -0,0 +1,8 @@
+# Task
+# Tranform of input array of zeros and ones to array in which counts number of continuous ones. If there is none, return an empty array
+#
+# Example
+# [1, 1, 1, 0, 1] -> [3,1]
+# [1, 1, 1, 1, 1] -> [5]
+# [0, 0, 0, 0, 0] -> []
+# ALGORITHMSFUNDAMENTALS
