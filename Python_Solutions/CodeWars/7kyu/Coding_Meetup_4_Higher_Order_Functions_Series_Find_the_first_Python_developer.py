c75701dd95712a55dc09f04d57d8fc40aa13d181
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-27 21:35:28 +0300

    Added solution "Coding Meetup #4 - Higher-Order Functions Series - Find the first Python developer"

--- Python_Solutions/CodeWars/7kyu/Coding_Meetup_4_Higher_Order_Functions_Series_Find_the_first_Python_developer.py
+++ Python_Solutions/CodeWars/7kyu/Coding_Meetup_4_Higher_Order_Functions_Series_Find_the_first_Python_developer.py
@@ -57,3 +57,9 @@
 # Coding Meetup #16 - Higher-Order Functions Series - Ask for missing details
 #
 # FUNCTIONAL PROGRAMMINGDATA STRUCTURESARRAYSFUNDAMENTALSALGORITHMSSTRINGSREGULAR EXPRESSIONS
+# Solution
+def get_first_python(users):
+    for user in users:
+        if user['language'] == 'Python':
+            return f'{user["first_name"]}, {user["country"]}'
+    return 'There will be no Python developers'
