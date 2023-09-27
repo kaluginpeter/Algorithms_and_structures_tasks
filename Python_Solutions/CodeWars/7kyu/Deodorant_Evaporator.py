97f07775b204b2e7c443e41018a09d0e3505ec0e
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-27 22:29:18 +0300

    Added solution "Deodorant Evaporator"

--- Python_Solutions/CodeWars/7kyu/Deodorant_Evaporator.py
+++ Python_Solutions/CodeWars/7kyu/Deodorant_Evaporator.py
@@ -10,3 +10,11 @@
 # Content is in fact not necessary in the body of the function "evaporator", you can use it or not use it, as you wish. Some people might prefer to reason with content, some other with percentages only. It's up to you but you must keep it as a parameter because the tests have it as an argument.
 #
 # FUNDAMENTALS
+# Solution
+def evaporator(content, evap_per_day, threshold):
+    result = 0;
+    percentage = 100;
+    while percentage > threshold:
+        percentage = percentage - percentage * (evap_per_day / 100)
+        result += 1
+    return result
