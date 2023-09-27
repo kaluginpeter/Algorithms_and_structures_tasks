b120fc6db247e34e13dd64cfe7784d7893a9f3a8
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-27 22:07:04 +0300

    Added solution "Count consonants"

--- Python_Solutions/CodeWars/7kyu/Count_consonants.py
+++ Python_Solutions/CodeWars/7kyu/Count_consonants.py
@@ -3,3 +3,6 @@
 # Consonants are all letters used to write English excluding the vowels a, e, i, o, u.
 #
 # STRINGSFUNDAMENTALS
+# Solution
+def consonant_count(s):
+    return sum([1 if i not in 'aeiou' and i.isalpha() else 0 for i in s.replace(' ', '').lower()])
