290805cf029f0995f576714f906ff8c2f314f7bd
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-27 22:12:34 +0300

    Added solution "Counting Valleys"

--- Python_Solutions/CodeWars/7kyu/Counting_Valleys.py
+++ Python_Solutions/CodeWars/7kyu/Counting_Valleys.py
@@ -19,3 +19,11 @@
 # So here we passed one valley
 #
 # ALGORITHMSSTRINGS
+# Solution
+def counting_valleys(s):
+    level, valleys = 0, 0
+    for step in s:
+        if step == 'U' and level == -1:
+            valleys += 1
+        level += {'U': 1, 'F': 0, 'D': -1}[step]
+    return valleys
