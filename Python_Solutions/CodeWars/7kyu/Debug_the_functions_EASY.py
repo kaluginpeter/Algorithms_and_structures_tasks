1d5b5cd30dfc520bd089fd1967a731d1ff2ae2b3
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-27 22:26:46 +0300

    Added solution "Debug the functions EASY"

--- Python_Solutions/CodeWars/7kyu/Debug_the_functions_EASY.py
+++ Python_Solutions/CodeWars/7kyu/Debug_the_functions_EASY.py
@@ -5,3 +5,11 @@
 # There are three functions: Multiplication (x) Addition (+) and Reverse (!esreveR)
 #
 # DEBUGGING
+# Solution
+import math
+def multi(l_st):
+    return math.prod(l_st)
+def add(l_st):
+    return sum(l_st)
+def reverse(string):
+    return string[::-1]
