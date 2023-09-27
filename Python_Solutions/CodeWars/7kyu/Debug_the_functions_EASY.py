8d392a81620b9c2d0885a9fccda0e57dc1f57787
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-27 22:26:31 +0300

    Added description "Debug the functions EASY"

---
+++ Python_Solutions/CodeWars/7kyu/Debug_the_functions_EASY.py
@@ -0,0 +1,7 @@
+# DESCRIPTION:
+# Debug the functions
+# Should be easy, begin by looking at the code. Debug the code and the functions should work.
+#
+# There are three functions: Multiplication (x) Addition (+) and Reverse (!esreveR)
+#
+# DEBUGGING
