005cf71a6ca26e45fdf4e2580ae8f4e5a0131f8f
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-27 21:39:53 +0300

    Added solution "Coloured Triangles"

--- Python_Solutions/CodeWars/7kyu/Coloured_Triangles.py
+++ Python_Solutions/CodeWars/7kyu/Coloured_Triangles.py
@@ -23,3 +23,9 @@
 # Adapted from the 2017 British Informatics Olympiad
 #
 # LOGICSTRINGSALGORITHMS
+# Solution
+COLORS = set("RGB")
+def triangle(row):
+    while len(row)>1:
+        row = ''.join( a if a==b else (COLORS-{a,b}).pop() for a,b in zip(row, row[1:]))
+    return row
