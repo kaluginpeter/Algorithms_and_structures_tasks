fbdaa6b307bb35be88798c7e124d34964784713e
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-27 21:58:54 +0300

    Added description "Convert Hash To An Array"

---
+++ Python_Solutions/CodeWars/7kyu/Convert_Hash_To_An_Array.py
@@ -0,0 +1,11 @@
+# Convert a hash into an array. Nothing more, Nothing less.
+#
+# {name: 'Jeremy', age: 24, role: 'Software Engineer'}
+# should be converted into
+#
+# [["age", 24], ["name", "Jeremy"], ["role", "Software Engineer"]]
+# Note: The output array should be sorted alphabetically by key name.
+#
+# Good Luck!
+#
+# ARRAYSLISTSFUNDAMENTALS
