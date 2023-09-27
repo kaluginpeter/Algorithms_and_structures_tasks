175751c3d8b31280d733c9e975adf6c203254c3f
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-27 21:37:53 +0300

    Added description "Collatz Conjecture Length"

---
+++ Python_Solutions/CodeWars/7kyu/Collatz_Conjecture_Length.py
@@ -0,0 +1,16 @@
+# The Collatz Conjecture states that for any positive natural number n, this process:
+#
+# if n is even, divide it by 2
+# if n is odd, multiply it by 3 and add 1
+# repeat
+# will eventually reach n = 1.
+#
+# For example, if n = 20, the resulting sequence will be:
+#
+# [ 20, 10, 5, 16, 8, 4, 2, 1 ]
+# Write a program that will output the length of the Collatz Conjecture for any given n.
+# In the example above, the output would be 8.
+#
+# For more reading see: http://en.wikipedia.org/wiki/Collatz_conjecture
+#
+# MATHEMATICSALGORITHMS
