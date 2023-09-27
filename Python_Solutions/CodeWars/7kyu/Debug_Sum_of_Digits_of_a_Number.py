2022aa0e9a771073878cef8583e8b06174fbacb7
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-27 22:20:22 +0300

    Added description "Debug Sum of Digits of a Number"

---
+++ Python_Solutions/CodeWars/7kyu/Debug_Sum_of_Digits_of_a_Number.py
@@ -0,0 +1,7 @@
+# Debug   function getSumOfDigits that takes positive integer to calculate sum of its digits. Assume that argument is an integer.
+#
+# Example
+# 123  => 6
+# 223  => 7
+# 1337 => 14
+# DEBUGGINGFUNDAMENTALS
