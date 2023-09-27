3ab5796fe61684f74368de40800e1c90a4572cf7
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-27 21:53:26 +0300

    Added description "CompoundArray"

---
+++ Python_Solutions/CodeWars/7kyu/CompoundArray.py
@@ -0,0 +1,5 @@
+# You have to create a method "compoundArray" which should take as input two int arrays of different length and return one int array with numbers of both arrays shuffled one by one.
+#
+# Input - {1,2,3,4,5,6} and {9,8,7,6}
+# Output - {1,9,2,8,3,7,4,6,5,6}
+# ARRAYSFUNDAMENTALS
