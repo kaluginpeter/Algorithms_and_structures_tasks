1c5d21d8ffbb026ce04f27295541030b81e52d2c
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-27 21:50:56 +0300

    Added solution "Complete The Pattern #4"

--- Python_Solutions/CodeWars/7kyu/Complete_The_Pattern_4.py
+++ Python_Solutions/CodeWars/7kyu/Complete_The_Pattern_4.py
@@ -21,3 +21,7 @@
 # Hint: Use \n in string to jump to next line
 #
 # ASCII ARTFUNDAMENTALS
+# Solution
+def pattern(n):
+    l = list(range(1, n + 1))
+    return '\n'.join(''.join(map(str, l[i:])) for i in range(n))
