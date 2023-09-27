2dfc3765a132c0207fd009cbaa6fdddca150627a
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-27 22:15:39 +0300

    Added solution "Credit card issuer checking"

--- Python_Solutions/CodeWars/7kyu/Credit_card_issuer_checking.py
+++ Python_Solutions/CodeWars/7kyu/Credit_card_issuer_checking.py
@@ -18,3 +18,12 @@
 # get_issuer(5105105105105106) == "Mastercard"
 # get_issuer(9111111111111111) == "Unknown"
 # ALGORITHMS
+# Solution
+def get_issuer(number):
+    card = str(number)
+    nums = len(card)
+    if card[:2] in ("34", "37") and nums == 15:return "AMEX"
+    elif card[:4] == "6011" and nums == 16:return "Discover"
+    elif 51 <= int(card[:2]) <= 55 and nums == 16:return "Mastercard"
+    elif card[0] == "4" and nums in (13, 16):return "VISA"
+    return "Unknown"
