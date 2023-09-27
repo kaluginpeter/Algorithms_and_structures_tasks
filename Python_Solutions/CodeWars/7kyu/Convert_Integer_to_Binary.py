85e6f059872094d6a5851cfe331aa83ac2dd3d2a
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-27 21:59:39 +0300

    Added description "Convert Integer to Binary"

---
+++ Python_Solutions/CodeWars/7kyu/Convert_Integer_to_Binary.py
@@ -0,0 +1,12 @@
+# Convert integers to binary as simple as that. You would be given an integer as a argument and you have to return its binary form. To get an idea about how to convert a decimal number into a binary number, visit here.
+#
+# Notes: negative numbers should be handled as two's complement; assume all numbers are integers stored using 4 bytes (or 32 bits) in any language.
+#
+# Your output should ignore leading 0s.
+#
+# Examples (input --> output):
+# 3  --> "11"
+# -3 -->"11111111111111111111111111111101"
+# Be Ready for Large Numbers. Happy Coding ^_^
+#
+# BINARYFUNDAMENTALS
