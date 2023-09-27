c1a126a14798563d5646aa2d61cd609d48927249
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-27 22:30:52 +0300

    Added solution "Describe a list"

--- Python_Solutions/CodeWars/7kyu/Describe_a_list.py
+++ Python_Solutions/CodeWars/7kyu/Describe_a_list.py
@@ -1,3 +1,6 @@
 # Write function describeList which returns "empty" if the list is empty or "singleton" if it contains only one element or "longer"" if more.
 #
 # LISTSFUNDAMENTALS
+# Solution
+def describeList(list):
+    return 'singleton' if len(list) == 1 else 'longer' if len(list) > 1 else 'empty'
