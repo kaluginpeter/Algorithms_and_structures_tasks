b89dc66908e0893de2ff1e0b19d9c07a517c24a7
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-27 21:53:40 +0300

    Added solution "CompoundArray"

--- Python_Solutions/CodeWars/7kyu/CompoundArray.py
+++ Python_Solutions/CodeWars/7kyu/CompoundArray.py
@@ -3,3 +3,10 @@
 # Input - {1,2,3,4,5,6} and {9,8,7,6}
 # Output - {1,9,2,8,3,7,4,6,5,6}
 # ARRAYSFUNDAMENTALS
+# Solution
+def compound_array(a, b):
+    l = []
+    while a or b:
+        if a: l.append(a.pop(0))
+        if b: l.append(b.pop(0))
+    return l
