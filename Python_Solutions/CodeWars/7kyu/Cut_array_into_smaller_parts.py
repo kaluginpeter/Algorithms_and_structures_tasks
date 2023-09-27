b5f48c07da8cccd01f94356eea6a854800bf8de9
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-27 22:18:11 +0300

    Added solution "Cut array into smaller parts"

--- Python_Solutions/CodeWars/7kyu/Cut_array_into_smaller_parts.py
+++ Python_Solutions/CodeWars/7kyu/Cut_array_into_smaller_parts.py
@@ -3,3 +3,6 @@
 # Example: if an array of size 123 is given and chunk size is 10 there will be 13 parts, 12 of size 10 and 1 of size 3.
 #
 # FUNDAMENTALS
+# Solution
+def make_parts(arr, chunkSize):
+    return [arr[i: i + chunkSize] for i in range(0, len(arr), chunkSize)]
