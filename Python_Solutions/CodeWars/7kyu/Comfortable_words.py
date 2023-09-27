531f8bfa4e8813833c4ddb2c755f55ae5636bd18
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-27 21:41:13 +0300

    Added description "Comfortable words"

---
+++ Python_Solutions/CodeWars/7kyu/Comfortable_words.py
@@ -0,0 +1,16 @@
+# A comfortable word is a word which you can type always alternating the hand you type with (assuming you type using a QWERTY keyboard and use fingers as shown in the image below).
+#
+# That being said, complete the function which receives a word and returns true if it's a comfortable word and false otherwise.
+#
+# The word will always be a string consisting of only ascii letters from a to z.
+#
+#
+#
+# To avoid problems with image availability, here's the lists of letters for each hand:
+#
+# Left: q, w, e, r, t, a, s, d, f, g, z, x, c, v, b
+# Right: y, u, i, o, p, h, j, k, l, n, m
+# Examples
+# "yams"  -->  true
+# "test"  -->  false
+# STRINGSALGORITHMS
