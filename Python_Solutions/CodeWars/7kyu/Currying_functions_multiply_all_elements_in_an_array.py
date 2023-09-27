e77ffe7c1537ce6ef6f3c9a6ce1bb800cd30c23c
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-27 22:17:01 +0300

    Added description "Currying functions: multiply all elements in an array"

---
+++ Python_Solutions/CodeWars/7kyu/Currying_functions_multiply_all_elements_in_an_array.py
@@ -0,0 +1,12 @@
+# To complete this Kata you need to make a function multiplyAll/multiply_all which takes an array of integers as an argument. This function must return another function, which takes a single integer as an argument and returns a new array.
+#
+# The returned array should consist of each of the elements from the first array multiplied by the integer.
+#
+# Example:
+#
+# multiply_all([1, 2, 3])(2); // => [2, 4, 6]
+# You must not mutate the original array.
+#
+# Here's a nice Youtube video about currying, which might help you if this is new to you.
+#
+# FUNCTIONAL PROGRAMMINGFUNDAMENTALS
