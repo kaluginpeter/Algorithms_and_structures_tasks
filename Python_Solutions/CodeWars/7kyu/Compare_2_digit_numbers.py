db7399ea7a43cedbf66fd286e26ab70e282c17cb
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-27 21:42:49 +0300

    Added description "Compare 2 digit numbers"

---
+++ Python_Solutions/CodeWars/7kyu/Compare_2_digit_numbers.py
@@ -0,0 +1,9 @@
+# You are given 2 two-digit numbers. You should check if they are similar by comparing their numbers, and return the result in %.
+#
+# Example:
+#
+# compare(13,14)=50%;
+# compare(23,22)=50%;
+# compare(15,51)=100%;
+# compare(12,34)=0%.
+# FUNDAMENTALSMATHEMATICS
