65db00baa006d99b5d926d62755e6fad22f1996d
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-28 22:02:47 +0300

    Added solution "Evens and Odds"

--- Python_Solutions/CodeWars/7kyu/Evens_and_Odds.py
+++ Python_Solutions/CodeWars/7kyu/Evens_and_Odds.py
@@ -5,3 +5,6 @@
 # Numbers will be positive. The hexadecimal string should be lowercased.
 #
 # FUNDAMENTALSBINARY
+# Solution
+def evens_and_odds(n):
+    return bin(n)[2:] if n%2 == 0 else hex(n)[2:]
