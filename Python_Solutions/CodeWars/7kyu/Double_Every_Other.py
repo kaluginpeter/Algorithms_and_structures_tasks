18f09d9fa4929827f04b063a7d0abfaccaeb48cc
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-28 21:40:34 +0300

    Added description "Double Every Other"

---
+++ Python_Solutions/CodeWars/7kyu/Double_Every_Other.py
@@ -0,0 +1,11 @@
+# Write a function that doubles every second integer in a list, starting from the left.
+#
+# Example:
+#
+# For input array/list :
+#
+# [1,2,3,4]
+# the function should return :
+#
+# [1,4,3,8]
+# FUNDAMENTALSLISTSARRAYSALGORITHMS
