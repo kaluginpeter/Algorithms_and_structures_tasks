a5caa581f5fbb954a81f1a592c7b99fee39d3a45
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-28 22:24:28 +0300

    Added description "Find Count of Most Frequent Item in an Array"

---
+++ Python_Solutions/CodeWars/7kyu/Find_Count_of_Most_Frequent_Item_in_an_Array.py
@@ -0,0 +1,8 @@
+# Complete the function to find the count of the most frequent item of an array. You can assume that input is an array of integers. For an empty array return 0
+#
+# Example
+# input array: [3, -1, -1, -1, 2, 3, -1, 3, -1, 2, 4, 9, 3]
+# ouptut: 5
+# The most frequent number in the array is -1 and it occurs 5 times.
+#
+# DATA STRUCTURESFUNDAMENTALS
