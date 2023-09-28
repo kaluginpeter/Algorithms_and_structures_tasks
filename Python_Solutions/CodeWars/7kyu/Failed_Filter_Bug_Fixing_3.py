0b3628f11a7310e019bf760aa8f45c39cdf5d664
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-28 22:17:50 +0300

    Added solution "Failed Filter - Bug Fixing #3"

--- Python_Solutions/CodeWars/7kyu/Failed_Filter_Bug_Fixing_3.py
+++ Python_Solutions/CodeWars/7kyu/Failed_Filter_Bug_Fixing_3.py
@@ -1,3 +1,6 @@
 # Failed Filter - Bug Fixing #3
 # Oh no, Timmy's filter doesn't seem to be working? Your task is to fix the FilterNumber function to remove all the numbers from the string.
 # STRINGSDEBUGGING
+# Solution
+def filter_numbers(string):
+    return "".join(x for x in string if not x.isdigit())
