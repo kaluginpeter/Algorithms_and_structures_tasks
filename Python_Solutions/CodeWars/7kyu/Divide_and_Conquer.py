ccdc0f0c129364bd910df30145d6a269baaa58e3
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-28 21:34:16 +0300

    Added solution "Divide and Conquer"

--- Python_Solutions/CodeWars/7kyu/Divide_and_Conquer.py
+++ Python_Solutions/CodeWars/7kyu/Divide_and_Conquer.py
@@ -3,3 +3,6 @@
 # Return as a number.
 #
 # FUNDAMENTALSSTRINGSARRAYS
+# Solution
+def div_con(x):
+    return sum(list(i if type(i) == int else 0 for i in x)) - sum(list(int(i) if type(i) == str else 0 for i in x ))
