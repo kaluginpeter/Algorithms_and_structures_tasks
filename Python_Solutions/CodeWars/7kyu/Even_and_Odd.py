67cb6e3ba3d6e4bfaee554a45e64516e32dcf56c
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-28 21:59:03 +0300

    Added description "Even and Odd !"

---
+++ Python_Solutions/CodeWars/7kyu/Even_and_Odd.py
@@ -0,0 +1,11 @@
+# Given a number N, can you fabricate the two numbers NE and NO such that NE is formed by even digits of N and NO is formed by odd digits of N? Examples:
+#
+# input	NE	NO
+# 126453	264	153
+# 3012	2	31
+# 4628	4628	0
+# 0 is considered as an even number.
+#
+# In C and JavaScript you should return an array of two elements such as the first is NE and the second is NO.
+#
+# FUNDAMENTALS
