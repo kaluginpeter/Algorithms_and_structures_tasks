9694ff7f6ba71b47fb40ea9db94088970cdb3856
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-28 21:58:05 +0300

    Added description "Euclidean distance in n dimensions"

---
+++ Python_Solutions/CodeWars/7kyu/Euclidean_distance_in_n_dimensions.py
@@ -0,0 +1,5 @@
+# Implement a function to calculate the distance between two points in n-dimensional space. The two points will be passed to your function as arrays of the same length (tuples in Python).
+#
+# Round your answers to two decimal places.
+#
+# MATHEMATICSALGORITHMSFUNDAMENTALS
