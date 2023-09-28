6d71c296e280cdf7898a9c39e09d124746c6e0e3
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-28 21:42:17 +0300

    Added solution "Double Trouble"

--- Python_Solutions/CodeWars/7kyu/Double_Trouble.py
+++ Python_Solutions/CodeWars/7kyu/Double_Trouble.py
@@ -14,3 +14,12 @@
 # Return the resulting array.
 #
 # FUNDAMENTALSARRAYS
+# Solution
+def trouble(x, t):
+    i = 0
+    while i < len(x) - 1:
+        if x[i] + x[i+1] == t:
+            del x[i+1]
+        else:
+            i += 1
+    return x
