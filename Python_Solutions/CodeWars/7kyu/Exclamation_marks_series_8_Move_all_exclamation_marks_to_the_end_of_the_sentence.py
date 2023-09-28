c502c7a65d55c4a294fd7fb4365e4d71a7f7bf3a
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-28 22:11:58 +0300

    Added description "Exclamation marks series #8: Move all exclamation marks to the end of the sentence"

---
+++ Python_Solutions/CodeWars/7kyu/Exclamation_marks_series_8_Move_all_exclamation_marks_to_the_end_of_the_sentence.py
@@ -0,0 +1,10 @@
+# Description:
+# Move all exclamation marks to the end of the sentence
+#
+# Examples
+# "Hi!"          ---> "Hi!"
+# "Hi! Hi!"      ---> "Hi Hi!!"
+# "Hi! Hi! Hi!"  ---> "Hi Hi Hi!!!"
+# "Hi! !Hi Hi!"  ---> "Hi Hi Hi!!!"
+# "Hi! Hi!! Hi!" ---> "Hi Hi Hi!!!!"
+# FUNDAMENTALS
