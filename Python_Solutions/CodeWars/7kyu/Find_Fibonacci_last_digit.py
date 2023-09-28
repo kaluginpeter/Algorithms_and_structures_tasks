a6c88c64ce04a880026107346540ae19ff977823
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-28 22:25:51 +0300

    Added description "Find Fibonacci last digit"

---
+++ Python_Solutions/CodeWars/7kyu/Find_Fibonacci_last_digit.py
@@ -0,0 +1,5 @@
+# As you probably know, Fibonacci sequence are the numbers in the following integer sequence: 1, 1, 2, 3, 5, 8, 13... Write a method that takes the index as an argument and returns last digit from fibonacci number. Example: getLastDigit(15) - 610. Your method must return 0 because the last digit of 610 is 0. Fibonacci sequence grows very fast and value can take very big numbers (bigger than integer type can contain), so, please, be careful with overflow.
+#
+# Hardcore version of this kata, no bruteforce will work here ;)
+#
+# ALGORITHMS
