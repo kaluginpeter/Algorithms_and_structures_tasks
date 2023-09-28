8048e7315e2ed97ec1f8c81fc001e520e81d3290
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-28 21:30:40 +0300

    Added description "Digits explosion"

---
+++ Python_Solutions/CodeWars/7kyu/Digits_explosion.py
@@ -0,0 +1,6 @@
+# Given a string made of digits [0-9], return a string where each digit is repeated a number of times equals to its value.
+#
+# Examples
+# "312" should return "333122"
+# "102269" should return "12222666666999999999"
+# STRINGSREGULAR EXPRESSIONSFUNDAMENTALS
