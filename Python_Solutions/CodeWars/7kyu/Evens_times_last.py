81de326820314431255d1f2199c5a064ee4e7fd7
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-28 22:03:16 +0300

    Added description "Evens times last"

---
+++ Python_Solutions/CodeWars/7kyu/Evens_times_last.py
@@ -0,0 +1,7 @@
+# Given a sequence of integers, return the sum of all the integers that have an even index (odd index in COBOL), multiplied by the integer at the last index.
+#
+# Indices in sequence start from 0.
+#
+# If the sequence is empty, you should return 0.
+#
+# ARRAYSFUNDAMENTALS
