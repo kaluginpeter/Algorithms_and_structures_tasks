ed396ba343aceae1d4cf397fa8634e5ab0b2ed66
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-28 22:08:43 +0300

    Added description "Exclamation marks series #3: Remove all exclamation marks from sentence except at the end"

---
+++ Python_Solutions/CodeWars/7kyu/Exclamation_marks_series_3_Remove_all_exclamation_marks_from_sentence_except_at_the_end.py
@@ -0,0 +1,11 @@
+# Description:
+# Remove all exclamation marks from sentence except at the end.
+#
+# Examples
+# "Hi!"     ---> "Hi!"
+# "Hi!!!"   ---> "Hi!!!"
+# "!Hi"     ---> "Hi"
+# "!Hi!"    ---> "Hi!"
+# "Hi! Hi!" ---> "Hi Hi!"
+# "Hi"      ---> "Hi"
+# FUNDAMENTALS
