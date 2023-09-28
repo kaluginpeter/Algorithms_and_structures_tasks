d0edde1bed3c5299a342e1796cbfdbe8b70c4ade
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-28 21:34:01 +0300

    Added description "Divide and Conquer"

---
+++ Python_Solutions/CodeWars/7kyu/Divide_and_Conquer.py
@@ -0,0 +1,5 @@
+# Given a mixed array of number and string representations of integers, add up the non-string integers and subtract the total of the string integers.
+#
+# Return as a number.
+#
+# FUNDAMENTALSSTRINGSARRAYS
