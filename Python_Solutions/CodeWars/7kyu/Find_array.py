abf9deaa64922503f0ff323179f50bdb75f990d6
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-28 22:32:46 +0300

    Added solution "Find array"

--- Python_Solutions/CodeWars/7kyu/Find_array.py
+++ Python_Solutions/CodeWars/7kyu/Find_array.py
@@ -15,3 +15,7 @@
 # Note that when an element inside arr2 is greater than the index of the last element of arr1 no element of arr1 should be added to the resulting array. If either arr1 or arr2 is empty, you should return an empty arr (empty list in python, empty vector in c++). Note for c++ use std::vector arr1, arr2.
 #
 # PUZZLES
+# Solution
+def find_array(arr1, arr2):
+    arr2 = [i for i in arr2 if i < len(arr1)]
+    return [arr1[i] for i in arr2]
