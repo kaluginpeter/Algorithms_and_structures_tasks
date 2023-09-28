f7804b72f69036e90567956d6efead386f4dfdcb
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-28 21:25:39 +0300

    Added solution "Digital cypher"

--- Python_Solutions/CodeWars/7kyu/Digital_cypher.py
+++ Python_Solutions/CodeWars/7kyu/Digital_cypher.py
@@ -37,3 +37,19 @@
 # Digital cypher vol 2
 # Digital cypher vol 3 - missing key
 # FUNDAMENTALSCIPHERSCRYPTOGRAPHY
+# Solution
+def encode(message, key):
+    letter_dict = {
+        'a': 1,'b': 2,
+        'c': 3,'d': 4,'e': 5,'f': 6,'g': 7,'h': 8,'i': 9,'j': 10,'k': 11,'l': 12,'m': 13,'n': 14,'o': 15,'p': 16,
+        'q': 17,'r': 18,'s': 19,'t': 20,'u': 21,'v': 22,'w': 23,'x': 24,'y': 25,'z': 26
+        }
+    num_list = []
+    key_list = str(key)
+    x = 0
+    for i in message:
+        num_list.append(letter_dict[i] + int(key_list[x]))
+        x += 1
+        if x >= len(key_list):
+            x = 0
+    return num_list
