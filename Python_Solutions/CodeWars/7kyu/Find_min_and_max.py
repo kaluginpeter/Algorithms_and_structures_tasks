8ebff80568e5524dad37ae3b77777b2231cc6dd9
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-28 22:35:29 +0300

    Added solution "Find min and max"

--- Python_Solutions/CodeWars/7kyu/Find_min_and_max.py
+++ Python_Solutions/CodeWars/7kyu/Find_min_and_max.py
@@ -1,3 +1,6 @@
 # Implement a function that returns the minimal and the maximal value of a list (in this order).
 #
 # FUNDAMENTALS
+# Solution
+def get_min_max(seq):
+    return (min(seq), max(seq))
