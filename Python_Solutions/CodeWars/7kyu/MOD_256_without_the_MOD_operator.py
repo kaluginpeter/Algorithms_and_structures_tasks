54bce8e2c5599c42b9eec63706bf1b79b6178e9d
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-28 19:21:31 +0300

    Added solution "MOD 256 without the MOD operator"

--- Python_Solutions/CodeWars/7kyu/MOD_256_without_the_MOD_operator.py
+++ Python_Solutions/CodeWars/7kyu/MOD_256_without_the_MOD_operator.py
@@ -31,3 +31,12 @@
 # I have also created other katas. Take a look if you enjoyed this kata!
 #
 # ALGORITHMSFUNDAMENTALSLOGICMATHEMATICSRESTRICTEDPUZZLES
+# Solution
+def mod256_without_mod(number):
+    if number < 0:
+        while number < 0:
+            number += 256
+        return number
+    while number >= 256:
+        number -= 256
+    return number
