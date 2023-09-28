af3be334c9dff2ed2dd29d52e1de04a119de5be6
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-28 22:17:35 +0300

    Added description "Failed Filter - Bug Fixing #3"

---
+++ Python_Solutions/CodeWars/7kyu/Failed_Filter_Bug_Fixing_3.py
@@ -0,0 +1,3 @@
+# Failed Filter - Bug Fixing #3
+# Oh no, Timmy's filter doesn't seem to be working? Your task is to fix the FilterNumber function to remove all the numbers from the string.
+# STRINGSDEBUGGING
