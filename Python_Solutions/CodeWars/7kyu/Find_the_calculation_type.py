09e3dd05ff72fe31cf4330a5fb0a92ed68559f70
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-28 22:45:17 +0300

    Added solution "Find the calculation type"

--- Python_Solutions/CodeWars/7kyu/Find_the_calculation_type.py
+++ Python_Solutions/CodeWars/7kyu/Find_the_calculation_type.py
@@ -13,3 +13,13 @@
 # Only valid arguments will be passed to the function!
 #
 # FUNDAMENTALS
+# Solution
+def calc_type(a, b, res):
+    if a+b==res:
+        return 'addition'
+    elif a-b==res:
+        return 'subtraction'
+    elif a*b==res:
+        return 'multiplication'
+    elif a/b==res:
+        return 'division'
