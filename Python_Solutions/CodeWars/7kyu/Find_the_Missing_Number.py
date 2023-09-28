303ad2556cb5c1f66446811241facbb5b24d905a
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-28 22:38:28 +0300

    Added solution "Find the Missing Number"

--- Python_Solutions/CodeWars/7kyu/Find_the_Missing_Number.py
+++ Python_Solutions/CodeWars/7kyu/Find_the_Missing_Number.py
@@ -5,3 +5,6 @@
 # You are given an unsorted array containing all the integers from 0 to 100 inclusively. However, one number is missing. Write a function to find and return this number. What are the time and space complexities of your solution?
 #
 # ALGORITHMS
+# Solution
+def missing_no(nums):
+    return sum(range(101)) - sum(nums)
