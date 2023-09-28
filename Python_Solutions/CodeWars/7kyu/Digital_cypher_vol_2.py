1732db4facf34364d186db7104c048b3b11339b2
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-28 21:26:32 +0300

    Added solution "Digital cypher vol 2"

--- Python_Solutions/CodeWars/7kyu/Digital_cypher_vol_2.py
+++ Python_Solutions/CodeWars/7kyu/Digital_cypher_vol_2.py
@@ -37,3 +37,7 @@
 # Digital cypher vol 2
 # Digital cypher vol 3 - missing key
 # FUNDAMENTALSCIPHERSCRYPTOGRAPHY
+# Solution
+def decode(code, key):
+    key=str(key)
+    return "".join([chr(code[i] +96 - int(key[i%len(key)])) for i in range(0, len(code))])
