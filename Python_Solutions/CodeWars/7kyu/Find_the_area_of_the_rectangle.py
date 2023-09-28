bfcd5978ef49c762c2038be5ab58ea52062913c7
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-28 22:43:18 +0300

    Added description "Find the area of the rectangle!"

---
+++ Python_Solutions/CodeWars/7kyu/Find_the_area_of_the_rectangle.py
@@ -0,0 +1,5 @@
+# Find the area of a rectangle when provided with one diagonal and one side of the rectangle. If the input diagonal is less than or equal to the length of the side, return "Not a rectangle". If the resultant area has decimals round it to two places.
+#
+# This kata is meant for beginners. Rank and upvote to bring it out of beta!
+#
+# FUNDAMENTALSMATHEMATICS
