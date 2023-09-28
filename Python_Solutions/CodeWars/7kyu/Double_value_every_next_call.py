d80149dc2d2ee1fa133c51139c4c8c2f90bd996c
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-28 21:42:51 +0300

    Added description "Double value every next call"

---
+++ Python_Solutions/CodeWars/7kyu/Double_value_every_next_call.py
@@ -0,0 +1,7 @@
+# This kata is about static method that should return different values.
+#
+# On the first call it must be 1, on the second and others - it must be a double from previous value.
+#
+# Look tests for more examples, good luck :)
+#
+# OBJECT-ORIENTED PROGRAMMING
