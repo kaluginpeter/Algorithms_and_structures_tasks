17aa8485b5481409e0ac637cae481b5886e28944
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-28 22:19:43 +0300

    Added solution "Fibonacci"

--- Python_Solutions/CodeWars/7kyu/Fibonacci.py
+++ Python_Solutions/CodeWars/7kyu/Fibonacci.py
@@ -1,3 +1,9 @@
 # Create function fib that returns n'th element of Fibonacci sequence (classic programming task).
 #
 # FUNDAMENTALSALGORITHMS
+# Solution
+def fibonacci(n: int) -> int:
+    a, b = 0, 1
+    for i in range(n):
+        a,b = b, a+b
+    return a
