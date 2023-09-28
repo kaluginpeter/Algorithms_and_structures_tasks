8aa62ebc7eb74d14fab9dbc8a956a70322941e83
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-28 21:44:08 +0300

    Added solution "Driving Licence"

--- Python_Solutions/CodeWars/7kyu/Driving_Licence.py
+++ Python_Solutions/CodeWars/7kyu/Driving_Licence.py
@@ -82,3 +82,29 @@
 # Car Park Escape
 #
 # STRINGSARRAYSFUNDAMENTALS
+# Solution
+from dateutil.parser import parse
+def driver(data):
+    lic = ""
+    if len(data[2]) >= 5:
+          lic += data[2][:5]
+    else:
+            lic += data[2]
+            while (len(lic) < 5): lic += "9"
+    lic += (str(parse(data[3]).year))[2]
+    month = parse(data[3]).month
+    if data[4] == "F": month += 50
+    month = str(month)
+    if len(month) == 1: month = "0" + month
+    lic += month
+    day = str(parse(data[3]).day)
+    if len(day) == 1: day = "0" + day
+    lic += day
+    lic += (str(parse(data[3]).year))[3]
+    lic +=  data[0][:1]
+    if (data[1]) != "":
+        lic += data[1][:1]
+    else:
+        lic += "9"
+    lic += "9AA"
+    return lic.upper()
