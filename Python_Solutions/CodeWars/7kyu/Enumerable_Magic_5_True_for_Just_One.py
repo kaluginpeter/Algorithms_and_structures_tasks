ec672f64d30a9a41c2c154ffdcd98a7a86565e49
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-28 21:56:35 +0300

    Added description "Enumerable Magic #5- True for Just One?"

---
+++ Python_Solutions/CodeWars/7kyu/Enumerable_Magic_5_True_for_Just_One.py
@@ -0,0 +1,14 @@
+# Task
+# Create a function called one that accepts two params:
+#
+# a sequence
+# a function
+# and returns true only if the function in the params returns true for exactly one (1) item in the sequence.
+#
+# Example
+# one([1, 3, 5, 6, 99, 1, 3], bigger_than_ten) -> true
+# one([1, 3, 5, 6, 99, 88, 3], bigger_than_ten) -> false
+# one([1, 3, 5, 6, 5, 1, 3], bigger_than_ten) -> false
+# If you need help, here is a resource ( in Ruby ).
+#
+# FUNDAMENTALS
