999c388f3b715e41d0ae74b4f1f0c5f3d3e72f82
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-28 21:49:28 +0300

    Added description "Duplicate sandwich"

---
+++ Python_Solutions/CodeWars/7kyu/Duplicate_sandwich.py
@@ -0,0 +1,15 @@
+# Task
+# In this kata you will be given a list consisting of unique elements except for one thing that appears twice.
+#
+# Your task is to output a list of everything inbetween both occurrences of this element in the list.
+#
+# Examples:
+# [0, 1, 2, 3, 4, 5, 6, 1, 7, 8] => [2, 3, 4, 5, 6]
+# ['None', 'Hello', 'Example', 'hello', 'None', 'Extra'] => ['Hello', 'Example', 'hello']
+# [0, 0] => []
+# [True, False, True] => [False]
+# ['e', 'x', 'a', 'm', 'p', 'l', 'e'] => ['x', 'a', 'm', 'p', 'l']
+# Notes
+# All elements will be the same datatype.
+# All used elements will be hashable.
+# LISTSFUNDAMENTALS
