303340787072f86c42067af24c4b8186da9001c1
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-28 22:22:01 +0300

    Added description "Filter Long Words"

---
+++ Python_Solutions/CodeWars/7kyu/Filter_Long_Words.py
@@ -0,0 +1,7 @@
+# Write a function that takes a string and an an integer n as parameters and returns a list of all words that are longer than n.
+#
+# Example:
+#
+# * With input "The quick brown fox jumps over the lazy dog", 4
+# * Return ['quick', 'brown', 'jumps']
+# FUNDAMENTALS
