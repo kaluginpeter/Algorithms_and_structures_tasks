c0480cfd9ae794c3822f82c2f5217001f87857b0
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-28 21:52:41 +0300

    Added description "Easy mathematical callback"

---
+++ Python_Solutions/CodeWars/7kyu/Easy_mathematical_callback.py
@@ -0,0 +1,15 @@
+# Task
+# Write the processArray function, which takes an array and a callback function as parameters. The callback function can be, for example, a mathematical function that will be applied on each element of this array. Optionally, also write tests similar to the examples below.
+#
+# Examples
+# Array [4, 8, 2, 7, 5] after processing with function
+#
+# function(a): return a*2;
+# will be [ 8, 16, 4, 14, 10 ].
+#
+# Array [7, 8, 9, 1, 2] after processing with function
+#
+# function(a): return a+5;
+# will be [ 12, 13, 14, 6, 7 ].
+#
+# ALGORITHMS
