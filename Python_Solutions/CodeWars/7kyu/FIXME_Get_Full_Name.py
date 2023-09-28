9fde407b797a91a3bfb9d2945c5778ac136103d8
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-28 22:14:38 +0300

    Added solution "FIXME: Get Full Name"

--- Python_Solutions/CodeWars/7kyu/FIXME_Get_Full_Name.py
+++ Python_Solutions/CodeWars/7kyu/FIXME_Get_Full_Name.py
@@ -9,3 +9,10 @@
 # Fix the bug so we can all go home early.
 #
 # DEBUGGING
+# Solution
+class Dinglemouse(object):
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+    def get_full_name(self):
+        return (self.first_name + ' ' + self.last_name).strip()
