44f76c5b7915ea0bf11ec941109da961c9ed48f7
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-28 22:09:54 +0300

    Added description "Exclamation marks series #5: Remove all exclamation marks from the end of words"

---
+++ Python_Solutions/CodeWars/7kyu/Exclamation_marks_series_5_Remove_all_exclamation_marks_from_the_end_of_words.py
@@ -0,0 +1,11 @@
+# Task
+# Remove all exclamation marks from the end of words. Words are separated by a single space. There are no exclamation marks within a word.
+#
+# Examples
+# remove("Hi!") === "Hi"
+# remove("Hi!!!") === "Hi"
+# remove("!Hi") === "!Hi"
+# remove("!Hi!") === "!Hi"
+# remove("Hi! Hi!") === "Hi Hi"
+# remove("!!!Hi !!hi!!! !hi") === "!!!Hi !!hi !hi"
+# STRINGSREGULAR EXPRESSIONSFUNDAMENTALS
