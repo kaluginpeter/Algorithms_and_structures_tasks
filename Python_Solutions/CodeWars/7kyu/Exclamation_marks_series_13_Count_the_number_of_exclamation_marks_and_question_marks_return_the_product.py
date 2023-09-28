d0f6828affad3c70e006c0c350d201719a276cf7
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-28 22:07:43 +0300

    Added description "Exclamation marks series #13: Count the number of exclamation marks and question marks, return the product"

---
+++ Python_Solutions/CodeWars/7kyu/Exclamation_marks_series_13_Count_the_number_of_exclamation_marks_and_question_marks_return_the_product.py
@@ -0,0 +1,15 @@
+# Description:
+# Count the number of exclamation marks and question marks, return the product.
+#
+# Examples
+# ""          --->   0
+# "!"         --->   0
+# "!ab? ?"    --->   2
+# "!!"        --->   0
+# "!??"       --->   2
+# "!???"      --->   3
+# "!!!??"     --->   6
+# "!!!???"    --->   9
+# "!???!!"    --->   9
+# "!????!!!?" --->  20
+# FUNDAMENTALS
