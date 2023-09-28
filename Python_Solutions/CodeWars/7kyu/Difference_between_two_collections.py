aad8c23eabc50bfd12ba0cd702f9e215e8fd4a79
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-28 21:23:04 +0300

    Added description "Difference between two collections"

---
+++ Python_Solutions/CodeWars/7kyu/Difference_between_two_collections.py
@@ -0,0 +1,11 @@
+# Find the difference between two collections. The difference means that either the character is present in one collection or it is present in other, but not in both. Return a sorted list with the difference.
+#
+# The collections can contain any character and can contain duplicates.
+#
+# Example
+# A = [a, a, t, e, f, i, j]
+#
+# B = [t, g, g, i, k, f]
+#
+# difference = [a, e, g, j, k]
+# FUNDAMENTALSARRAYSALGORITHMS
