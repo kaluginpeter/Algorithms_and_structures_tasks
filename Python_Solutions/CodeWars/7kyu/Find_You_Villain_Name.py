83fbca4b4233cfc0ce8e876334214deeaf0dd37f
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-28 22:29:14 +0300

    Added solution "Find Your Villain Name"

--- Python_Solutions/CodeWars/7kyu/Find_You_Villain_Name.py
+++ Python_Solutions/CodeWars/7kyu/Find_You_Villain_Name.py
@@ -33,3 +33,8 @@
 # For example, a birthday of November 18 would return "The Terrifying Teaspoon"
 #
 # ARRAYSDATE TIMEFUNDAMENTALS
+# Solution
+def get_villain_name(birthdate):
+    first = [ "The Evil","The Vile","The Cruel", "The Trashy","The Despicable", "The Embarrassing", "The Disreputable","The Atrocious", "The Twirling",  "The Orange","The Terrifying", "The Awkward"]
+    last = ["Mustache", "Pickle", "Hood Ornament", "Raisin", "Recycling Bin", "Potato", "Tomato", "House Cat", "Teaspoon", "Laundry Basket"]
+    return first[birthdate.month - 1] + ' ' + last[int(str(birthdate.day)[-1])]
