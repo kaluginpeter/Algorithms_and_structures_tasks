ca0b472042a7b4572730c806e0dd739fe1622ef2
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-29 23:14:05 +0300

    Added solution "Mispelled word"

--- Python_Solutions/CodeWars/7kyu/Mispelled_word.py
+++ Python_Solutions/CodeWars/7kyu/Mispelled_word.py
@@ -12,3 +12,11 @@
 # In the tests that expect true, the mispelled word will always differ mostly by one character. If the two words are the same, return True.
 #
 # STRINGSARRAYSFUNDAMENTALS
+# Solution
+def mispelled(word1, word2):
+    if word1 == word2[1:] or word1[1:] == word2 or word1[:-1] == word2 or word1 == word2[:-1]:
+        return True
+    if any(word1[:i] + word1[i+1:] == word2[:i] + word2[i+1:] for i in range(len(word1))):
+        return True
+    else:
+        return False
