1657697c2c791fb30899a8fe7c93f6bf03e951ea
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-29 22:52:03 +0300

    Added solution "London CityHacker"

--- Python_Solutions/CodeWars/7kyu/London_CityHacker.py
+++ Python_Solutions/CodeWars/7kyu/London_CityHacker.py
@@ -8,3 +8,7 @@
 # Your task is to calculate the total cost of the journey and return the cost rounded to 2 decimal places in the format (where x is a number): £x.xx
 #
 # FUNDAMENTALS
+# Solution
+def london_city_hacker(journey):
+    vehicle = "".join("t" if isinstance(k, str) else "b" for k in journey).replace("bb", "b")
+    return f"£{sum(2.4 if i == 't' else 1.5 for i in vehicle):.2f}"
