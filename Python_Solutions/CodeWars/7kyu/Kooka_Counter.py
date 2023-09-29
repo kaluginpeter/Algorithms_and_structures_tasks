a4733b60e13c3d826884b05256546ed1bd0f45c9
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-29 22:36:04 +0300

    Added description "Kooka-Counter"

---
+++ Python_Solutions/CodeWars/7kyu/Kooka_Counter.py
@@ -0,0 +1,26 @@
+# A family of kookaburras are in my backyard.
+#
+# I can't see them all, but I can hear them!
+#
+# How many kookaburras are there?
+#
+# Hint
+# The trick to counting kookaburras is to listen carefully
+#
+# The males sound like HaHaHa...
+#
+# The females sound like hahaha...
+#
+# And they always alternate male/female
+#
+# Examples
+# ha = female => 1
+# Ha = male => 1
+# Haha = male + female => 2
+# haHa = female + male => 2
+# hahahahaha = female => 1
+# hahahahahaHaHaHa = female + male => 2
+# HaHaHahahaHaHa = male + female + male => 3
+# ^ Kata Note : No validation is necessary; only valid input will be passed :-)
+#
+# STRINGSALGORITHMS
