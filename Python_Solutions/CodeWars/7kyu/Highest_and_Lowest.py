28a16348b3afd10a712a942b3d6a2447ecc26919
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-29 21:59:20 +0300

    Added solution "Highest and Lowest"

--- Python_Solutions/CodeWars/7kyu/Highest_and_Lowest.py
+++ Python_Solutions/CodeWars/7kyu/Highest_and_Lowest.py
@@ -9,3 +9,10 @@
 # There will always be at least one number in the input string.
 # Output string must be two numbers separated by a single space, and highest number is first.
 # FUNDAMENTALSSTRINGS
+# Solution
+import re
+def high_and_low(numbers):
+    list = [int(s) for s in re.findall("[-+]?[.]?[\d]+(?:,\d\d\d)*[\.]?\d*(?:[eE][-+]?\d+)?", numbers)]
+    list.sort()
+    integer = ''
+    return integer + str(list[-1]) + ' ' + str(list[0])
