52b9682cf665a6de49493e5106e5c316f2d4611d
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-29 21:31:57 +0300

    Added solution "GCD sum"

--- Python_Solutions/CodeWars/7kyu/GCD_sum.py
+++ Python_Solutions/CodeWars/7kyu/GCD_sum.py
@@ -13,3 +13,10 @@
 # Good luck!
 #
 # FUNDAMENTALS
+# Solution
+def solve(s,g):
+    for i in range(g, s+1, g):
+        remainder = s - i
+        if remainder % g == 0:
+            return (i, remainder)
+    return -1
