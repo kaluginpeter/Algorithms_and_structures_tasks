cabd361c93552508d5b7b1e241e99ca79691a008
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-29 20:57:48 +0300

    Added solution "Fix string case"

--- Python_Solutions/CodeWars/7kyu/Fix_string_case.py
+++ Python_Solutions/CodeWars/7kyu/Fix_string_case.py
@@ -16,3 +16,15 @@
 # Simple remove duplicates
 #
 # FUNDAMENTALS
+# Solution
+def solve(s):
+    count_lower: int = 0
+    count_upper: int = 0
+    for i in s:
+        if i.islower():
+            count_lower += 1
+            continue
+        count_upper += 1
+    if count_lower >= count_upper:
+        return s.lower()
+    return s.upper()
