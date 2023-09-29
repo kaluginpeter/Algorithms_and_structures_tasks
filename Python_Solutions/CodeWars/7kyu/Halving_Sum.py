87df6c65841598fe2cf85e4116190026f67d2378
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-29 21:47:36 +0300

    Added description "Halving Sum"

---
+++ Python_Solutions/CodeWars/7kyu/Halving_Sum.py
@@ -0,0 +1,9 @@
+# Task
+# Given a positive integer n, calculate the following sum:
+#
+# n + n/2 + n/4 + n/8 + ...
+# All elements of the sum are the results of integer division.
+#
+# Example
+# 25  =>  25 + 12 + 6 + 3 + 1 = 47
+# ALGORITHMS
