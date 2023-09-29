18d8776a475d5e18e5bdc403626a6a733ee2990a
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-29 22:49:55 +0300

    Added description "LinkedList -> Array"

---
+++ Python_Solutions/CodeWars/7kyu/LinkedList_Array.py
@@ -0,0 +1,16 @@
+# Linked lists are data structures composed of nested or chained objects, each containing a single value and a reference to the next object.
+#
+# Here's an example of a list:
+#
+# class LinkedList:
+#     def __init__(self, value=0, next=None):
+#         self.value = value
+#         self.next = next
+#
+# LinkedList(1, LinkedList(2, LinkedList(3)))
+# Write a function listToArray (or list_to_array in Python) that converts a list to an array, like this:
+#
+# [1, 2, 3]
+# Assume all inputs are valid lists with at least one value. For the purpose of simplicity, all values will be either numbers, strings, or Booleans.
+#
+# DATA STRUCTURESARRAYSFUNDAMENTALS
