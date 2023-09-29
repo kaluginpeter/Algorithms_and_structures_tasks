fb6c8af07fc078410c8c888a1db933fa658a5f67
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-29 23:09:02 +0300

    Added description "Milk and Cookies for Santa"

---
+++ Python_Solutions/CodeWars/7kyu/Milk_and_Cookies_for_Santa.py
@@ -0,0 +1,11 @@
+# Happy Holidays fellow Code Warriors!
+# It's almost Christmas Eve, so we need to prepare some milk and cookies for Santa! Wait... when exactly do we need to do that?
+#
+# Time for Milk and Cookies
+# Complete the function function that accepts a Date object, and returns true if it's Christmas Eve (December 24th), false otherwise.
+#
+# Examples
+# time_for_milk_and_cookies(date(2013, 12, 24))  # True
+# time_for_milk_and_cookies(date(2013, 1, 23))   # False
+# time_for_milk_and_cookies(date(3000, 12, 24))  # True
+# DATE TIMEFUNDAMENTALS
