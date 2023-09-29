764e348eb6c4d71a7c9f451e720ec862047096ae
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-29 21:34:33 +0300

    Added description "Genetic Algorithm Series - #4 Get population and fitnesses"

---
+++ Python_Solutions/CodeWars/7kyu/Genetic_Algorithm_Series_4_Get_population_and_fitnesses.py
@@ -0,0 +1,23 @@
+# In a genetic algorithm, a population is a collection of candidates that may evolve toward a better solution.
+#
+# We determine how close a chromosome is to a ideal solution by calculating its fitness. https://www.codewars.com/kata/567b468357ed7411be00004a/train You are given two parameters, the population containing all individuals and a function fitness that determines how close to the solution a chromosome is.
+#
+# Your task is to return a collection containing an object with the chromosome and the calculated fitness.
+#
+# [
+#   { chromosome: c, fitness: f },
+#   { chromosome: c, fitness: f },
+#   ...
+# ]
+# Note: you have a pre-loaded namedtuple ChromosomeWrap and you should return a collection of it instead.
+#
+# ChromosomeWrap = namedtuple("ChromosomeWrap", ["chromosome", "fitness"])
+# See other katas from this series
+# Genetic Algorithm Series - #1 Generate
+# Genetic Algorithm Series - #2 Mutation
+# Genetic Algorithm Series - #3 Crossover
+# Genetic Algorithm Series - #4 Get population and fitnesses
+# Genetic Algorithm Series - #5 Roulette wheel selection
+# This kata is a piece of Binary Genetic Algorithm
+#
+# ALGORITHMSGENETIC ALGORITHMSARRAYS
