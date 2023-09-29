d2c371b2fd7a75361ce2abb9e5bad2e8d3b216d7
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-29 22:17:11 +0300

    Added description "Inspiring Strings"

---
+++ Python_Solutions/CodeWars/7kyu/Inspiring_Strings.py
@@ -0,0 +1,9 @@
+# When given a string of space separated words, return the word with the longest length. If there are multiple words with the longest length, return the last instance of the word with the longest length.
+#
+# Example:
+#
+# 'red white blue' //returns string value of white
+#
+# 'red blue gold' //returns gold
+#
+# STRINGSFUNDAMENTALS
