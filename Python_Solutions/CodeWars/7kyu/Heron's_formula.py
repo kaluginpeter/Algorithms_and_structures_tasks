d1468532d1bb128c282a4d34e27cb63c29acf9e9
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-29 21:57:09 +0300

    Added description "Heron's formula"

---
+++ Python_Solutions/CodeWars/7kyu/Heron's_formula.py
@@ -0,0 +1,44 @@
+# Write function heron which calculates the area of a triangle with sides a, b, and c (x, y, z in COBOL).
+#
+# Heron's formula:
+#
+# �
+# ∗
+# (
+# �
+# −
+# �
+# )
+# ∗
+# (
+# �
+# −
+# �
+# )
+# ∗
+# (
+# �
+# −
+# �
+# )
+# s∗(s−a)∗(s−b)∗(s−c)
+# ​
+#
+# where
+#
+# �
+# =
+# �
+# +
+# �
+# +
+# �
+# 2
+# s=
+# 2
+# a+b+c
+# ​
+#
+# Output should have 2 digits precision.
+#
+# FUNDAMENTALS
