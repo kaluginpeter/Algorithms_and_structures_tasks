0188df2a3804d5f39f98c1c5132d636c9ef920a7
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-29 21:00:15 +0300

    Added solution "Fizz Buzz Cuckoo Clock"

--- Python_Solutions/CodeWars/7kyu/Fizz_Buzz_Cuckoo_Clock.py
+++ Python_Solutions/CodeWars/7kyu/Fizz_Buzz_Cuckoo_Clock.py
@@ -26,3 +26,12 @@
 # Have fun!
 #
 # FUNDAMENTALS
+# Solution
+def fizz_buzz_cuckoo_clock(time):
+    hh, mm = map(int, time.split(":"))
+    if mm ==  0:return " ".join(["Cuckoo"] * (hh % 12 or 12))
+    elif mm == 30:return "Cuckoo"
+    elif mm % 15 == 0:return "Fizz Buzz"
+    elif mm %  3 == 0:return "Fizz"
+    elif mm %  5 == 0:return "Buzz"
+    else:return "tick"
