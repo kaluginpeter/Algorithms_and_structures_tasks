4cda2671a8632b7421cd21bf62e0b65597095d9c
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-29 22:18:06 +0300

    Added solution "Integer Difference"

--- Python_Solutions/CodeWars/7kyu/Integer_Difference.py
+++ Python_Solutions/CodeWars/7kyu/Integer_Difference.py
@@ -7,3 +7,11 @@
 # [1, 1, 5, 6, 9, 16, 27], n=4  -->  3  # (1,5), (1,5), (5,9)
 # [1, 1, 3, 3], n=2             -->  4  # (1,3), (1,3), (1,3), (1,3)
 # FUNDAMENTALS
+# Solution
+def int_diff(lst, n):
+    count = 0
+    for i in range(len(lst)):
+        for j in range(i+1, len(lst)):
+            if abs(lst[i] - lst[j]) == n:
+                count += 1
+    return count
