1c8dbf7eb0e0bb9d5fc0877e0ec9c9421cb92d42
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-29 21:06:46 +0300

    Added description "For the sake of argument"

---
+++ Python_Solutions/CodeWars/7kyu/For_the_sake_of_argument.py
@@ -0,0 +1,13 @@
+# Write a function named numbers.
+#
+# function should return True if all the parameters it is passed are of the integer type or float type. Otherwise, the function should return False.
+#
+# The function should accept any number of parameters.
+#
+# Example usage:
+#
+# numbers(1, 4, 3, 2, 5); # True
+# numbers(1, "a", 3); # False
+# numbers(1, 3, None); # False
+# numbers(1.23, 5.6, 3.2) # True
+# ARRAYSFUNDAMENTALS
