f202303eaa456324263a500bd05b8b53f4cc34f6
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-29 22:17:53 +0300

    Added description "Integer Difference"

---
+++ Python_Solutions/CodeWars/7kyu/Integer_Difference.py
@@ -0,0 +1,9 @@
+# Write a function that accepts two arguments: an array/list of integers and another integer (n).
+#
+# Determine the number of times where two integers in the array have a difference of n.
+#
+# For example:
+#
+# [1, 1, 5, 6, 9, 16, 27], n=4  -->  3  # (1,5), (1,5), (5,9)
+# [1, 1, 3, 3], n=2             -->  4  # (1,3), (1,3), (1,3), (1,3)
+# FUNDAMENTALS
