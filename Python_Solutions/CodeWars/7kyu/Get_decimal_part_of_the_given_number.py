9968c77e9cec7674e3d7850711b2639f59589cee
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-29 21:36:22 +0300

    Added description "Get decimal part of the given number"

---
+++ Python_Solutions/CodeWars/7kyu/Get_decimal_part_of_the_given_number.py
@@ -0,0 +1,8 @@
+# Write a function that returns only the decimal part of the given number.
+#
+# You only have to handle valid numbers, not Infinity, NaN, or similar. Always return a positive decimal part.
+#
+# Examples
+# get_decimal(2.4)  # 0.4
+# get_decimal(-0.2) # 0.2
+# FUNDAMENTALS
