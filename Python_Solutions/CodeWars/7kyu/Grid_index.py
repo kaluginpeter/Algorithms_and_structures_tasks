c23d4d2415f231d4afbab4f09d45af67e98cd0cc
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-29 21:45:26 +0300

    Added description "Grid index"

---
+++ Python_Solutions/CodeWars/7kyu/Grid_index.py
@@ -0,0 +1,21 @@
+# You are given an n by n ( square ) grid of characters, for example:
+#
+# [['m', 'y', 'e'],
+#  ['x', 'a', 'm'],
+#  ['p', 'l', 'e']]
+# You are also given a list of integers as input, for example:
+#
+# [1, 3, 5, 8]
+# You have to find the characters in these indexes of the grid if you think of the indexes as:
+#
+# [[1, 2, 3],
+#  [4, 5, 6],
+#  [7, 8, 9]]
+# Remember that the indexes start from one and not zero.
+#
+# Then you output a string like this:
+#
+# "meal"
+# All inputs will be valid.
+#
+# LISTSFUNDAMENTALS
