93335ea3df2deadfd45f69799cf40e8ab99aee06
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-29 22:15:43 +0300

    Added description "Initialize my name"

---
+++ Python_Solutions/CodeWars/7kyu/Initialize_my_name.py
@@ -0,0 +1,10 @@
+# Some people just have a first name; some people have first and last names and some people have first, middle and last names.
+#
+# You task is to initialize the middle names (if there is any).
+#
+# Examples
+# 'Jack Ryan'                   => 'Jack Ryan'
+# 'Lois Mary Lane'              => 'Lois M. Lane'
+# 'Dimitri'                     => 'Dimitri'
+# 'Alice Betty Catherine Davis' => 'Alice B. C. Davis'
+# STRINGSFUNDAMENTALS
