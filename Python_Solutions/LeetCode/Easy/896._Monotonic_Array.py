6fbc9acac650ac778b698403cd94166a35fc8721
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-29 18:14:37 +0300

    Added solution "896. Monotonic Array"

--- Python_Solutions/LeetCode/Easy/896._Monotonic_Array.py
+++ Python_Solutions/LeetCode/Easy/896._Monotonic_Array.py
@@ -23,3 +23,49 @@
 class Solution:
     def isMonotonic(self, nums: List[int]) -> bool:
         return all(i <= j for i,j in zip(nums, nums[1:])) or all(i >= j for i,j in zip(nums, nums[1:]))
+
+# Solution 2
+class Solution:
+    def isMonotonic(self, nums: List[int]) -> bool:
+        cop: int = len(nums) - 1
+        count: int = 0
+        for i in range(len(nums) - 1):
+            if nums[i] <= nums[i + 1]:
+                count += 1
+            else:
+                break
+        if cop == count:
+            return True
+        count: int = 0
+        for i in range(len(nums) - 1):
+            if nums[i] >= nums[i + 1]:
+                count += 1
+        return count == cop
+# Solution 3
+class Solution:
+    def isMonotonic(self, nums: List[int]) -> bool:
+        cop: list = nums.copy()
+        if nums[0] <= nums[-1]:
+            nums.sort()
+        else:
+            nums.sort(reverse=True)
+        for i in range(len(nums)):
+            if cop[i] != nums[i]:
+                return False
+        return True
+# Solution 3
+class Solution:
+    def isMonotonic(self, nums: List[int]) -> bool:
+        flag: int = 0
+        for i in range(1, len(nums)):
+            if nums[i] > nums[i - 1]:
+                if flag == 0:
+                    flag = 1
+                elif flag == -1:
+                    return False
+            if nums[i] < nums[i - 1]:
+                if flag == 0:
+                    flag = -1
+                elif flag == 1:
+                    return False
+        return True
