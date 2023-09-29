a3d63c170f04b57d9972d52329937a931e3c3bcf
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-29 23:12:14 +0300

    Added description "Minimum to multiple"

---
+++ Python_Solutions/CodeWars/7kyu/Minimum_to_multiple.py
@@ -0,0 +1,13 @@
+# Given two integers a and x, return the minimum non-negative number to add to / subtract from a to make it a multiple of x.
+#
+# minimum(10, 6)  #= 2
+#
+# 10+2 = 12 which is a multiple of 6
+# Note
+# 0 is always a multiple of x
+# Constraints
+# 1 <= a <= 106
+#
+# 1 <= x <= 105
+#
+# FUNDAMENTALS
