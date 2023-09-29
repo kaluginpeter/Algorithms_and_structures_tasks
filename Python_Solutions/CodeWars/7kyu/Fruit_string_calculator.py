de133a85cfc8c4be618134e86dacc8433bcdd97a
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-29 21:11:22 +0300

    Added description "Fruit string calculator"

---
+++ Python_Solutions/CodeWars/7kyu/Fruit_string_calculator.py
@@ -0,0 +1,16 @@
+# You are given a string of words and numbers. Extract the expression including:
+#
+# the operator: either addition ("gains") or subtraction ("loses")
+# the two numbers that we are operating on
+# Return the result of the calculation.
+#
+# Notes:
+#
+# "loses" and "gains" are the only two words describing operators
+# No fruit debts nor bitten apples = The numbers are integers and no negatives
+# Examples
+# "Panda has 48 apples and loses 4"  -->  44
+# "Jerry has 34 apples and gains 6"  -->  40
+# Should be a nice little kata for you :)
+#
+# STRINGSREGULAR EXPRESSIONSFUNDAMENTALS
