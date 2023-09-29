675b3c9f3c4fab249c01a6f4847c1bc1f01128c7
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-29 22:16:25 +0300

    Added description "Insert dashes"

---
+++ Python_Solutions/CodeWars/7kyu/Insert_dashes.py
@@ -0,0 +1,5 @@
+# Write a function insert_dash(num) / insertDash(num) / InsertDash(int num) that will insert dashes ('-') between each two odd digits in num. For example: if num is 454793 the output should be 4547-9-3.
+#
+# Note that the number will always be non-negative (>= 0).
+#
+# STRINGSARRAYSFUNDAMENTALS
