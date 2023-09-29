3bc977b353574a041ce6238c16b1ada361220fdd
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-29 21:33:39 +0300

    Added description "Genetic Algorithm Series - #1 Generate"

---
+++ Python_Solutions/CodeWars/7kyu/Genetic_Algorithm_Series_1_Generate.py
@@ -0,0 +1,20 @@
+# A genetic algorithm is based in groups of chromosomes, called populations. To start our population of chromosomes we need to generate random binary strings with a specified length.
+#
+# In this kata you have to implement a function generate that receives a length and has to return a random binary strign with length characters.
+#
+#
+#
+# Example:
+# Generate a chromosome with length of 4 generate(4) could return the chromosome 0010, 1110, 1111... or any of 2^4 possibilities.
+#
+# Note: Some tests are random. If you think your algorithm is correct but the result fails, trying again should work.
+#
+# See other katas from this series
+# Genetic Algorithm Series - #1 Generate
+# Genetic Algorithm Series - #2 Mutation
+# Genetic Algorithm Series - #3 Crossover
+# Genetic Algorithm Series - #4 Get population and fitnesses
+# Genetic Algorithm Series - #5 Roulette wheel selection
+# This kata is a piece of Binary Genetic Algorithm
+#
+# STRINGSFUNDAMENTALSGENETIC ALGORITHMSALGORITHMS
