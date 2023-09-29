deaeddbcf57e51f2daaae037e7c2dd2e5f57aaa6
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-29 21:14:53 +0300

    Added solution "Functions of Integers on Cartesian Plane"

--- Python_Solutions/CodeWars/7kyu/Functions_of_Integers_on_Cartesian_Plane.py
+++ Python_Solutions/CodeWars/7kyu/Functions_of_Integers_on_Cartesian_Plane.py
@@ -39,3 +39,10 @@
 # sumax(5000) --> 83345832500
 # sumsum(5000) --> 125025000000
 # FUNDAMENTALSMATHEMATICS
+# Solution
+def sumin(n):
+    return n * (n + 1) * (2 * n + 1) // 6
+def sumax(n):
+    return n * (n + 1) * (4 * n - 1) // 6
+def sumsum(n):
+    return n * n * (n + 1)
