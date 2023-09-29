b39fefc4ee24a24a118fae964a8dce5aadecd51a
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-29 22:21:21 +0300

    Added solution "Invalid Input - Error Handling #1"

--- Python_Solutions/CodeWars/7kyu/Invalid_Input_Error_Handling_1.py
+++ Python_Solutions/CodeWars/7kyu/Invalid_Input_Error_Handling_1.py
@@ -33,3 +33,11 @@
 #     }
 # }
 # STRINGSFUNDAMENTALS
+# Solution
+def get_count(words=""):
+    if not isinstance(words, str):
+        return {'vowels': 0,'consonants': 0}
+    letter = "".join([c.lower() for c in words if c.isalpha()])
+    vowel = "".join([c for c in letter if c in 'aeiou'])
+    conson = "".join([c for c in letter if c not in 'aeiou'])
+    return {'vowels': len(vowel),'consonants': len(conson)}
