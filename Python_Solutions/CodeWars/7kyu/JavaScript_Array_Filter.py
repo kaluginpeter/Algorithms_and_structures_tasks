6b7e49d13bad72e6dc82bdb6865610aac6c748e4
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-29 22:31:34 +0300

    Added description "JavaScript Array Filter"

---
+++ Python_Solutions/CodeWars/7kyu/JavaScript_Array_Filter.py
@@ -0,0 +1,6 @@
+# In Python, there is a built-in filter function that operates similarly to JS's filter. For more information on how to use this function, visit https://docs.python.org/3/library/functions.html#filter
+#
+# The solution would work like the following:
+#
+# get_even_numbers([2,4,5,6]) => [2,4,6]
+# ARRAYSFUNDAMENTALS
