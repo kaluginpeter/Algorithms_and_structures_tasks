9aa9652152b2980d2f6618977bbad2aa5932d446
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-29 23:03:40 +0300

    Added solution "Maximum Length Difference"

--- Python_Solutions/CodeWars/7kyu/Maximum_Length_Difference.py
+++ Python_Solutions/CodeWars/7kyu/Maximum_Length_Difference.py
@@ -12,3 +12,7 @@
 # input : 2 strings with substrings separated by ,
 # output: number as a string
 # FUNDAMENTALS
+# Solution
+def mxdiflg(a1, a2):
+    if a1 and a2: return max(abs(len(x) - len(y)) for x in a1 for y in a2)
+    return -1
