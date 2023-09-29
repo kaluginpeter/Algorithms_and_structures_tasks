1d3d20766169b49d93920cd3a855d24b0d62c5ee
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-29 22:09:14 +0300

    Added solution "How many times should I go?"

--- Python_Solutions/CodeWars/7kyu/How_many_times_should_I_go.py
+++ Python_Solutions/CodeWars/7kyu/How_many_times_should_I_go.py
@@ -3,3 +3,7 @@
 # In this kata you should complete a function in order to know after how many visit it will be better to take an annual pass. The function take 2 arguments annual_price and individual_price.
 #
 # FUNDAMENTALS
+# Solution
+import math
+def how_many_times(annual_price, individual_price):
+    return math.ceil(annual_price/individual_price)
