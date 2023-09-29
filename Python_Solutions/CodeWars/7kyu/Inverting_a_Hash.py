694af6ca7e7bb32c5bb61efa627c3f3a1ed8989a
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-29 22:22:06 +0300

    Added solution "Inverting a Hash"

--- Python_Solutions/CodeWars/7kyu/Inverting_a_Hash.py
+++ Python_Solutions/CodeWars/7kyu/Inverting_a_Hash.py
@@ -29,3 +29,6 @@
 # Keys and values may be of any type appropriate for use as a key.
 # All hashes provided as input will have unique values, so the inversion is involutive. In other words, do not worry about identical values stored under different keys.
 # FUNDAMENTALS
+# Solution
+def invert_hash(dictionary):
+    return {v:k for k,v in dictionary.items()}
