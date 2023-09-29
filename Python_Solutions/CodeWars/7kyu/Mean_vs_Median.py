3edb3ca1c7e72f7c6edb5274c95c047a30610cd9
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-29 23:06:42 +0300

    Added description "Mean vs. Median"

---
+++ Python_Solutions/CodeWars/7kyu/Mean_vs_Median.py
@@ -0,0 +1,10 @@
+# Your goal is to implement the method meanVsMedian which accepts an odd-length array of integers and returns one of the following:
+#
+# 'mean' - in case mean value is larger than median value
+# 'median' - in case median value is larger than mean value
+# 'same' - in case both mean and median share the same value
+# Reminder: Median
+#
+# Array will always be valid (odd-length >= 3)
+#
+# FUNDAMENTALSARRAYS
