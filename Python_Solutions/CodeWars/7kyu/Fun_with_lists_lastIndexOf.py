dae2d58f35aa55a77c2fec9c9956ac1de640b95b
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-29 21:12:16 +0300

    Added description "Fun with lists: lastIndexOf"

---
+++ Python_Solutions/CodeWars/7kyu/Fun_with_lists_lastIndexOf.py
@@ -0,0 +1,25 @@
+# Implement the method lastIndexOf (last_index_of in PHP and Python), which accepts a linked list (head) and a value, and returns the index (zero based) of the last occurrence of that value if exists, or -1 otherwise.
+#
+# For example: Given the list: 1 -> 2 -> 3 -> 3, and the value 3, lastIndexOf / last_index_of should return 3.
+#
+# The linked list is defined as follows:
+#
+# class Node:
+#     def __init__(self, data, next=None):
+#         self.data = data
+#         self.next = next
+# Note: the list may be null/None and can hold any type of value.
+#
+# Good luck!
+#
+# This kata is part of fun with lists series:
+#
+# Fun with lists: length
+# Fun with lists: indexOf
+# Fun with lists: lastIndexOf
+# Fun with lists: countIf
+# Fun with lists: anyMatch + allMatch
+# Fun with lists: filter
+# Fun with lists: map
+# Fun with lists: reduce
+# LISTSFUNDAMENTALS
