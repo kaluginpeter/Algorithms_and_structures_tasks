34b0337c1fc17fc4f50beedd77595166a95f8992
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-29 22:45:07 +0300

    Added description "Length and two values."

---
+++ Python_Solutions/CodeWars/7kyu/Length_and_two_values.py
@@ -0,0 +1,9 @@
+# Given an integer n and two other values, build an array of size n filled with these two values alternating.
+#
+# Examples
+# 5, true, false     -->  [true, false, true, false, true]
+# 10, "blue", "red"  -->  ["blue", "red", "blue", "red", "blue", "red", "blue", "red", "blue", "red"]
+# 0, "one", "two"    -->  []
+# Good luck!
+#
+# FUNDAMENTALSARRAYS
