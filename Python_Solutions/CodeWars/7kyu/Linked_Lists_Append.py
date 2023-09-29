2e23455c0c6ea4928dc1e39252b591893ad280c7
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-29 22:47:51 +0300

    Added solution "Linked Lists - Append"

--- Python_Solutions/CodeWars/7kyu/Linked_Lists_Append.py
+++ Python_Solutions/CodeWars/7kyu/Linked_Lists_Append.py
@@ -40,3 +40,18 @@
 # Inspired by Stanford Professor Nick Parlante's excellent Linked List teachings.
 #
 # LINKED LISTSDATA STRUCTURESFUNDAMENTALS
+# Solution
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+def last(head):
+    n = head
+    while n and n.next:
+    	n = n.next
+    return n
+def append(listA, listB):
+    if not listA:
+    	return listB
+    last(listA).next = listB
+    return listA
