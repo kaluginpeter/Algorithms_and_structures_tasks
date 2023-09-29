f65065874493250240da319562f24f43385cd9a2
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-29 20:58:21 +0300

    Added description "Fizz / Buzz"

---
+++ Python_Solutions/CodeWars/7kyu/Fizz_Buzz.py
@@ -0,0 +1,5 @@
+# Write a function that takes an integer and returns an array [A, B, C], where A is the number of multiples of 3 (but not 5) below the given integer, B is the number of multiples of 5 (but not 3) below the given integer and C is the number of multiples of 3 and 5 below the given integer.
+#
+# For example, solution(20) should return [5, 2, 1]
+#
+# MATHEMATICSALGORITHMS
