595670fdae49c266b15ca1351d53ff52b105528a
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-29 22:33:26 +0300

    Added solution "Jumping Number (Special Numbers Series #4)"

--- Python_Solutions/CodeWars/7kyu/Jumping_Number_Special_Numbers_Series_4.py
+++ Python_Solutions/CodeWars/7kyu/Jumping_Number_Special_Numbers_Series_4.py
@@ -44,3 +44,10 @@
 # Enjoy Learning !!
 # Zizou
 # FUNDAMENTALSMATHEMATICS
+# Solution
+def jumping_number(number):
+    number = list(str(number))
+    for i in range(0, len(number)-1):
+        if abs(int(number[i+1])-int(number[i])) != 1:
+            return 'Not!!'
+    return 'Jumping!!'
