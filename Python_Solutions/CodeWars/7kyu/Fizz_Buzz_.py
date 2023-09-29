e0db5deff3ece473f56e0cc1c6c01d5686fd691f
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-29 20:59:24 +0300

    Added solution "Fizz Buzz"

--- Python_Solutions/CodeWars/7kyu/Fizz_Buzz_.py
+++ Python_Solutions/CodeWars/7kyu/Fizz_Buzz_.py
@@ -11,3 +11,16 @@
 #
 # fizzbuzz(3) -->  [1, 2, "Fizz"]
 # ALGORITHMSFUNDAMENTALSARRAYS
+# Solution
+def fizzbuzz(n):
+    l = []
+    for i in range (1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            l.append("FizzBuzz")
+        elif i % 3 == 0:
+            l.append("Fizz")
+        elif i % 5 == 0:
+            l.append("Buzz")
+        else:
+            l.append(i)
+    return l
