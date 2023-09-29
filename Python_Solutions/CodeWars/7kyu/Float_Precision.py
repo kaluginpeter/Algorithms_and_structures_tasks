2f888a77c355d549f60fadae2dbd7defb962d668
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-29 21:02:12 +0300

    Added description "Float Precision"

---
+++ Python_Solutions/CodeWars/7kyu/Float_Precision.py
@@ -0,0 +1,5 @@
+# Update the solution method to round the argument value to the closest precision of two. The argument will always be a float.
+#
+# 23.23456 --> 23.23
+# 1.546    --> 1.55
+# FUNDAMENTALS
