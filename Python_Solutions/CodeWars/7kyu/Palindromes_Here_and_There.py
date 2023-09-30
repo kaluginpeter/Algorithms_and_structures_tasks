d6163d0d709f0243fa211a647a957487db11444d
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 22:28:40 +0300

    Added solution "Palindromes Here and There"

--- Python_Solutions/CodeWars/7kyu/Palindromes_Here_and_There.py
+++ Python_Solutions/CodeWars/7kyu/Palindromes_Here_and_There.py
@@ -7,3 +7,6 @@
 # [101, 2, 85, 33, 14014]  ==>  [1, 1, 0, 1, 0]
 # [45, 21, 303, 56]        ==>  [0, 0, 1, 0]
 # ALGORITHMS
+# Solution
+def convert_palindromes(numbers):
+    return [1 if str(i) == str(i)[::-1] else 0 for i in numbers]
