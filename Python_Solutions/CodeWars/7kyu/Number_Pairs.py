27b384980e09c201e47227a73edcd1a72128521b
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 22:02:04 +0300

    Added solution "Number Pairs"

--- Python_Solutions/CodeWars/7kyu/Number_Pairs.py
+++ Python_Solutions/CodeWars/7kyu/Number_Pairs.py
@@ -8,3 +8,6 @@
 # arr2 = [23, 14, 53, 17, 80]
 # get_larger_numbers(arr1, arr2) == [23, 64, 53, 17, 88]
 # ARRAYSFUNDAMENTALS
+# Solution
+def get_larger_numbers(a, b):
+    return [max(a[i], b[i]) for i,j in enumerate(a)]
