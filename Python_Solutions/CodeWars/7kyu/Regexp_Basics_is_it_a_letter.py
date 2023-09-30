6e0339976f2ba31f80a1537e360f8ad520d0dbe9
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 22:59:04 +0300

    Added solution "Regexp Basics - is it a letter?"

--- Python_Solutions/CodeWars/7kyu/Regexp_Basics_is_it_a_letter.py
+++ Python_Solutions/CodeWars/7kyu/Regexp_Basics_is_it_a_letter.py
@@ -1,3 +1,6 @@
 # Complete the code which should return true if the given object is a single ASCII letter (lower or upper case), false otherwise.
 #
 # REGULAR EXPRESSIONSFUNDAMENTALS
+# Solution
+def is_letter(s):
+    return s.isalpha() and len(s) == 1
