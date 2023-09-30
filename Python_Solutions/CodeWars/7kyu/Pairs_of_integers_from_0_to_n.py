76fb9b34e6203d48a0c66d0bf8b508dee4f6aca1
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 22:25:57 +0300

    Added description "Pairs of integers from 0 to n"

---
+++ Python_Solutions/CodeWars/7kyu/Pairs_of_integers_from_0_to_n.py
@@ -0,0 +1,10 @@
+# Write a function generatePairs (Javascript) / generate_pairs (Python / Ruby) that accepts an integer argument n and generates an array containing the pairs of integers [a, b] that satisfy the following conditions:
+#
+# 0 <= a <= b <= n
+# The pairs should be sorted by increasing values of a then increasing values of b.
+#
+# For example,
+#
+# generate_pairs(2) should return
+# [ [0, 0], [0, 1], [0, 2], [1, 1], [1, 2], [2, 2] ]
+# FUNDAMENTALS
