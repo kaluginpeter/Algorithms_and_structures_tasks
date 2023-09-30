736b26007fdcd4c0a77af4330e8a4332d77f0587
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 22:55:03 +0300

    Added solution "Recursion 101"

--- Python_Solutions/CodeWars/7kyu/Recursion_101.py
+++ Python_Solutions/CodeWars/7kyu/Recursion_101.py
@@ -10,3 +10,15 @@
 # Please also try Simple time difference
 #
 # FUNDAMENTALS
+# Solution
+def solve(a,b):
+    if a == 0 or b == 0:
+        return [a,b]
+    elif a >= 2*b:
+        a = a-2*b
+        return solve(a,b)
+    elif b >= 2*a:
+        b = b-2*a
+        return solve(a,b)
+    else:
+        return [a,b]
