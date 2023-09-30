3b19a02bab39a9eee2785a4accf591a98341be4a
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 18:20:00 +0300

    Added solution "Multiplicative Persistence... What's special about 277777788888899?"

--- Python_Solutions/CodeWars/7kyu/Multiplicative_Persistence_What's_special_about_277777788888899.py
+++ Python_Solutions/CodeWars/7kyu/Multiplicative_Persistence_What's_special_about_277777788888899.py
@@ -16,3 +16,12 @@
 # per(277777788888899) = [4996238671872, 438939648, 4478976, 338688, 27648, 2688, 768, 336, 54, 20, 0]
 # // 2*7*7*7*7*7*7*8*8*8*8*8*8*9*9 = 4996238671872 --> 4*9*9*6*2*3*8*6*7*1*8*7*2 = 4478976 --> ...
 # ALGORITHMS
+# Solution
+from functools import reduce
+from operator import mul
+def per(n):
+    res = []
+    while n > 9:
+        n = reduce(mul, map(int, str(n)))
+        res.append(n)
+    return res
