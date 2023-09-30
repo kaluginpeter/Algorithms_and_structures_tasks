33054715721c5c978673a0c659bde32858ef73c4
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 22:22:25 +0300

    Added solution "Operations with sequence"

--- Python_Solutions/CodeWars/7kyu/Operations_with_sequence.py
+++ Python_Solutions/CodeWars/7kyu/Operations_with_sequence.py
@@ -14,3 +14,6 @@
 # P.S.: The sequence consists only of integers. And try not to use "for", "while" or "loop" statements.
 #
 # FUNDAMENTALSARRAYS
+# Solution
+def calc(a):
+    return sum(v**(1 + (v>=0)) * (1 + 2*(not k%3)) * (-1)**(not k%5) for k, v in enumerate(a, 1))
