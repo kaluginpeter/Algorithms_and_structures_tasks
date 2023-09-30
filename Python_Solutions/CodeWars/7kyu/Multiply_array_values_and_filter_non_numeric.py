993c70427fc720615cab4d18c5106c319ebd0f5a
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 18:21:30 +0300

    Added solution "Multiply array values and filter non-numeric"

--- Python_Solutions/CodeWars/7kyu/Multiply_array_values_and_filter_non_numeric.py
+++ Python_Solutions/CodeWars/7kyu/Multiply_array_values_and_filter_non_numeric.py
@@ -1,3 +1,6 @@
 # Your task is to write a function, which takes two arguments and returns a sequence. First argument is a sequence of values, second is multiplier. The function should filter all non-numeric values and multiply the rest by given multiplier.
 #
 # ARRAYSALGORITHMS
+# Solution
+def multiply_and_filter(seq, multiplier):
+    return [i * multiplier for i in seq if type(i) == int or type(i) == float]
