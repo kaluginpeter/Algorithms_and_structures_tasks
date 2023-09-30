d3b395c364a8c54d5b085c31e6b27d7cb8932e24
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 18:23:30 +0300

    Added description "NATO Phonetic Alphabet"

---
+++ Python_Solutions/CodeWars/7kyu/NATO_Phonetic_Alphabet.py
@@ -0,0 +1,12 @@
+# Complete the function nato that takes a word in parameter and returns a string that spells the word using the NATO phonetic alphabet.
+#
+# There should be a space between each word in the returned string, and the first letter of each word should be capitalized.
+#
+# For those of you that don't want your fingers to bleed, this kata already has a dictionary typed out for you.
+#
+# Examples
+# "hi"      -->  "Hotel India"
+# "abc"     -->  "Alpha Bravo Charlie"
+# "babble"  -->  "Bravo Alpha Bravo Bravo Lima Echo"
+# "Banana"  -->  "Bravo Alpha November Alpha November Alpha"
+# STRINGSFUNDAMENTALS
