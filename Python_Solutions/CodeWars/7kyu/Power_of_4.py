dda95fe375a26a717cf55410ce0dbc6564ecff28
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 22:43:05 +0300

    Added description "Power of 4"

---
+++ Python_Solutions/CodeWars/7kyu/Power_of_4.py
@@ -0,0 +1,9 @@
+# Write a method that returns true if a given parameter is a power of 4, and false if it's not. If parameter is not an Integer (eg String, Array) method should return false as well.
+#
+# (In C# Integer means all integer Types like Int16,Int32,.....)
+#
+# Examples
+# isPowerOf4 1024 #should return True
+# isPowerOf4  102 #should return False
+# isPowerOf4   64 #should return True
+# FUNDAMENTALSMATHEMATICS
