4ee21a3882686aafa07f6a970df07f0aef244e08
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 22:35:37 +0300

    Added description "Perfect Number Verifier"

---
+++ Python_Solutions/CodeWars/7kyu/Perfect_Number_Verifier.py
@@ -0,0 +1,16 @@
+# A perfect number is a number in which the sum of its divisors (excluding itself) are equal to itself.
+#
+# Write a function that can verify if the given integer n is a perfect number, and return True if it is, or return False if not.
+#
+# Examples
+# n = 28 has the following divisors: 1, 2, 4, 7, 14, 28
+#
+# 1 + 2 + 4 + 7 + 14 = 28 therefore 28 is a perfect number, so you should return True
+#
+# Another example:
+#
+# n = 25 has the following divisors: 1, 5, 25
+#
+# 1 + 5 = 6 therefore 25 is not a perfect number, so you should return False
+#
+# ALGORITHMS
