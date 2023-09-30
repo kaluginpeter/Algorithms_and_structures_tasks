e1e13ad50e2887bc76583ee60f7ea217218aae4f
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 18:32:10 +0300

    Added solution "No oddities here"

--- Python_Solutions/CodeWars/7kyu/No_oddities_here.py
+++ Python_Solutions/CodeWars/7kyu/No_oddities_here.py
@@ -3,3 +3,6 @@
 # All values in the array will be integers. Return the good values in the order they are given.
 #
 # ARRAYSFUNDAMENTALS
+# Solution
+def no_odds(values):
+    return [i for i in values if i % 2 == 0]
