25199a7534702703881e01f62739307181be349d
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 18:11:10 +0300

    Added solution "More than Zero?"

--- Python_Solutions/CodeWars/7kyu/More_than_Zero.py
+++ Python_Solutions/CodeWars/7kyu/More_than_Zero.py
@@ -1,3 +1,6 @@
 # Correct this code so that it takes one argument, x, and returns "x is more than zero" if x is positive (and nonzero), and otherwise, returns "x is equal to or less than zero." In both cases, replace x with the actual value of x.
 #
 # DEBUGGING
+# Solution
+def corrections(x):
+    return f"{x} is more than zero." if x > 0 else f"{x} is equal to or less than zero."
