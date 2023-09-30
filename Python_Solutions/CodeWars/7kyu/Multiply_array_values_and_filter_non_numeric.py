4cf9a0fbbe55c52e64caf4baba503850b5c39cd6
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 18:21:15 +0300

    Added description "Multiply array values and filter non-numeric"

---
+++ Python_Solutions/CodeWars/7kyu/Multiply_array_values_and_filter_non_numeric.py
@@ -0,0 +1,3 @@
+# Your task is to write a function, which takes two arguments and returns a sequence. First argument is a sequence of values, second is multiplier. The function should filter all non-numeric values and multiply the rest by given multiplier.
+#
+# ARRAYSALGORITHMS
