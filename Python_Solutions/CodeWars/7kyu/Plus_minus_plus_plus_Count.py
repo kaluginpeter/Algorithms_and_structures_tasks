24ee7eaa0e60ad80a56d17df92efb230f536a029
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 22:39:08 +0300

    Added description "Plus - minus - plus - plus - ... - Count"

---
+++ Python_Solutions/CodeWars/7kyu/Plus_minus_plus_plus_Count.py
@@ -0,0 +1,20 @@
+# Count how often sign changes in array.
+#
+# result
+# number from 0 to ... . Empty array returns 0
+#
+# example
+# const arr = [1, -3, -4, 0, 5];
+#
+# /*
+# | elem | count |
+# |------|-------|
+# |  1   |  0    |
+# | -3   |  1    |
+# | -4   |  1    |
+# |  0   |  2    |
+# |  5   |  2    |
+# */
+#
+# catchSignChange(arr) == 2;
+# FUNDAMENTALS
