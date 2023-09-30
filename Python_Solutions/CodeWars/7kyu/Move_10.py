37a551b4c686153b2f3ee3b4f92ef3d692ffb3c9
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 18:13:22 +0300

    Added description "Move 10"

---
+++ Python_Solutions/CodeWars/7kyu/Move_10.py
@@ -0,0 +1,7 @@
+# Move every letter in the provided string forward 10 letters through the alphabet.
+#
+# If it goes past 'z', start again at 'a'.
+#
+# Input will be a string with length > 0.
+#
+# FUNDAMENTALSSTRINGSARRAYS
