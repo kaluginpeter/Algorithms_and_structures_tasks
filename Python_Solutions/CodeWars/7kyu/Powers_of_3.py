1fe40f6811c2dfaaafcca04168ad6be9f82415eb
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 22:44:30 +0300

    Added description "Powers of 3"

---
+++ Python_Solutions/CodeWars/7kyu/Powers_of_3.py
@@ -0,0 +1,9 @@
+# Given a positive integer N, return the largest integer k such that 3^k < N.
+#
+# For example,
+#
+# largest_power(3) == 0
+# largest_power(4) == 1
+# You may assume that the input to your function is always a positive integer.
+#
+# MATHEMATICSALGORITHMS
