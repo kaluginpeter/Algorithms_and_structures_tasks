1d301617c9adc056026b33bf6d67fab922cd647b
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 23:30:57 +0300

    Added solution "Secret problems with numbers"

--- Python_Solutions/CodeWars/7kyu/Secret_problems_with_numbers.py
+++ Python_Solutions/CodeWars/7kyu/Secret_problems_with_numbers.py
@@ -1,3 +1,8 @@
 # Your task is to write a function that takes a number as an argument and returns some number. But what's the problem? You need to guess what number should be returned. See the example test case. Good luck. Hint: You should be a bit careful when counting...
 #
 # ALGORITHMSLOGICPUZZLESFUNDAMENTALS
+# Solution
+def secret_number(n):
+    if n % 2 == 0:
+        return bin(n).count('0') ** 2
+    return bin(n).count('1') ** 2
