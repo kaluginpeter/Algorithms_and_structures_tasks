aa284b29d30eff2b09a0ce046e7096eb2b5710fb
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 22:20:50 +0300

    Added description "Ones' Complement"

---
+++ Python_Solutions/CodeWars/7kyu/Ones'_Complement.py
@@ -0,0 +1,7 @@
+# The Ones' Complement of a binary number is the number obtained by swapping all the 0s for 1s and all the 1s for 0s. For example:
+#
+# ones_complement(1001) = 0110
+# ones_complement(1001) = 0110
+# For any given binary number,formatted as a string, return the Ones' Complement of that number.
+#
+# FUNDAMENTALS
