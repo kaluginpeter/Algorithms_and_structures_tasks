6e78033ae2c7254318f95c619d5f12a4e5410af1
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 22:52:50 +0300

    Added description "Quadrants 2: Segments"

---
+++ Python_Solutions/CodeWars/7kyu/Quadrants_2_Segments.py
@@ -0,0 +1,29 @@
+# Task
+# Your task is to check whether a segment is completely in one quadrant or it crosses more. Return true if the segment lies in two or more quadrants. If the segment lies within only one quadrant, return false.
+#
+# There are two parameters: A (coord) and B (coord), the endpoints defining the segment AB.
+#
+# No coordinate will be zero; expect that all X and Y are positive or negative.
+#
+# Examples
+# This whole segment is in the first quadrant.
+# [(1, 2), (3, 4)] -> false
+# Example 1
+# This segment intersects the Y axis, therefore being in two quadrants: I and II.
+# [(9, 3), (-1, 6)] -> true
+# Example 2
+# This segment is completely in the second quadrant.
+# [(-1, 6), (-9, 1)] -> false
+# Example 3
+# Predefined
+# There is a class named coord/Coord (see in code). It has the following members:
+#
+# (constructor): Constructs the coordinate
+# x (number): The X coordinate
+# y (number): The Y coordinate
+# x (number): The X coordinate
+# y (number): The Y coordinate
+# Task Series
+# Quadrants
+# Quadrants 2: Segments (this kata)
+# FUNDAMENTALSMATHEMATICSGEOMETRY
