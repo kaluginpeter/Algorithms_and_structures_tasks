cd1650767193fdfff29fb148cf55a06b20b924c4
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 23:34:39 +0300

    Added solution "SevenAte9"

--- Python_Solutions/CodeWars/7kyu/SevenAte9.py
+++ Python_Solutions/CodeWars/7kyu/SevenAte9.py
@@ -3,3 +3,11 @@
 # "79712312" --> "7712312"
 # "79797"    --> "777"
 # LOGICSTRINGSFUNDAMENTALS
+# Solution
+def seven_ate9(str_):
+    strung_out = str_
+    for i in range(len(strung_out)):
+        if strung_out[i:i+3] == '797':
+            strung_out = strung_out.replace('797','77')
+            continue
+    return strung_out
