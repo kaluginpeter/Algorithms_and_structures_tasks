26275538fc07c577fc61cdefc0e146779082b956
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 22:59:55 +0300

    Added solution "Regexp Basics - is it a vowel?"

--- Python_Solutions/CodeWars/7kyu/Regexp_Basics_is_it_a_vowel.py
+++ Python_Solutions/CodeWars/7kyu/Regexp_Basics_is_it_a_vowel.py
@@ -1,3 +1,6 @@
 # Implement the function which should return true if given object is a vowel (meaning a, e, i, o, u, uppercase or lowercase), and false otherwise.
 #
 # REGULAR EXPRESSIONSFUNDAMENTALS
+# Solution
+def is_vowel(s):
+    return s.lower() in ['a', 'e', 'i', 'o', 'u']
