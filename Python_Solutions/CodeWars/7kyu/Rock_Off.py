ad3179982e59c9fe98a9fdc8f4a2ec1867ef7e45
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 23:18:47 +0300

    Added solution "Rock Off!"

--- Python_Solutions/CodeWars/7kyu/Rock_Off.py
+++ Python_Solutions/CodeWars/7kyu/Rock_Off.py
@@ -13,3 +13,14 @@
 # The solution to the example above should therefore appear like '1, 2: Bob made "Jeff" proud!'.
 #
 # ARRAYSFUNDAMENTALS
+# Solution
+def solve(a, b):
+    alice = sum(i > j for i, j in zip(a, b))
+    bob = sum(j > i for i, j in zip(a, b))
+    if alice == bob:
+        words = 'that looks like a "draw"! Rock on!'
+    elif alice > bob:
+        words = 'Alice made "Kurt" proud!'
+    else:
+        words = 'Bob made "Jeff" proud!'
+    return f"{alice}, {bob}: {words}"
