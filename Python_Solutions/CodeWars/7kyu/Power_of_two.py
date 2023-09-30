eb43b89d6e0d8ecf698556e5c5f38294099108f8
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 22:43:49 +0300

    Added description "Power of two"

---
+++ Python_Solutions/CodeWars/7kyu/Power_of_two.py
@@ -0,0 +1,13 @@
+# Complete the function power_of_two/powerOfTwo (or equivalent, depending on your language) that determines if a given non-negative integer is a power of two. From the corresponding Wikipedia entry:
+#
+# a power of two is a number of the form 2n where n is an integer, i.e. the result of exponentiation with number two as the base and integer n as the exponent.
+#
+# You may assume the input is always valid.
+#
+# Examples
+# power_of_two(1024) ==> True
+# power_of_two(4096) ==> True
+# power_of_two(333)  ==> False
+# Beware of certain edge cases - for example, 1 is a power of 2 since 2^0 = 1 and 0 is not a power of 2.
+#
+# MATHEMATICSALGORITHMSFUNDAMENTALS
