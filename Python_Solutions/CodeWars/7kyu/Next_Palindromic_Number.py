6c64b9ea545132f300666a198cf50e5f3fcf36dc
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 18:27:08 +0300

    Added solution "Next Palindromic Number."

--- Python_Solutions/CodeWars/7kyu/Next_Palindromic_Number.py
+++ Python_Solutions/CodeWars/7kyu/Next_Palindromic_Number.py
@@ -17,3 +17,9 @@
 # Enjoy it!!
 #
 # FUNDAMENTALSDATA STRUCTURESALGORITHMSMATHEMATICSLOGICSTRINGS
+# Solution
+def next_pal(val):
+    val += 1
+    while str(val) != str(val)[::-1]:
+        val += 1
+    return val
