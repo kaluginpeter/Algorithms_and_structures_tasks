d2821db0b48fe541f66ad2785704353f3c9b12ac
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 23:35:07 +0300

    Added description "Sexy Primes <3"

---
+++ Python_Solutions/CodeWars/7kyu/Sexy_Primes_3.py
@@ -0,0 +1,11 @@
+# Sexy primes are pairs of two primes that are 6 apart. In this kata, your job is to complete the function which returns true if x & y are sexy, false otherwise.
+#
+# Examples
+# 5, 11   -->  true
+# 61, 67  -->  true
+# 7, 13   -->  true
+# 5, 7    -->  false
+# 1, 7    -->  false  (1 is not a prime)
+# Note: x & y are always positive integers, but they are not always in order of precendence... For example you can be given either (5, 11) or (11, 5) - both are valid.
+#
+# FUNDAMENTALS
