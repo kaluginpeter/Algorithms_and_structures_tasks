2f613c252f6f1729c31a5eae64287eb846d3475b
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 22:19:40 +0300

    Added solution "Olympic Rings"

--- Python_Solutions/CodeWars/7kyu/Olympic_Rings.py
+++ Python_Solutions/CodeWars/7kyu/Olympic_Rings.py
@@ -9,3 +9,14 @@
 # Dots over i's and any other letters don't count as rings.
 #
 # FUNDAMENTALSARRAYSSTRINGS
+# Solution
+def olympic_ring(string):
+    ring = 'abdegopqADOPQRBB'
+    count = sum(string.count(c) for c in ring) // 2
+    if count <= 1:
+        return 'Not even a medal!'
+    if count == 2:
+        return 'Bronze!'
+    if count == 3:
+        return 'Silver!'
+    return 'Gold!'
