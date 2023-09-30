f9af46d1c15be6b374f8a17613d8e52c374a705e
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 22:49:54 +0300

    Added solution "Product Array (Array Series #5)"

--- Python_Solutions/CodeWars/7kyu/Product_Array_Array_Series_5.py
+++ Python_Solutions/CodeWars/7kyu/Product_Array_Array_Series_5.py
@@ -45,3 +45,9 @@
 # Enjoy Learning !!
 # Zizou
 # FUNDAMENTALSARRAYS
+# Solution
+from operator import mul
+from functools import reduce
+def product_array(numbers):
+    tot = reduce(mul,numbers)
+    return [tot//n for n in numbers]
