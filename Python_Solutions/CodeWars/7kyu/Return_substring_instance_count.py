5dd6212d13145e4397ac85aab3aac6adfe8c414d
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 23:10:35 +0300

    Added description "Return substring instance count"

---
+++ Python_Solutions/CodeWars/7kyu/Return_substring_instance_count.py
@@ -0,0 +1,11 @@
+# Complete the solution so that it returns the number of times the search_text is found within the full_text. Overlap is not permitted : "aaa" contains 1 instance of "aa", not 2.
+#
+# Usage example:
+#
+# full_text = "aa_bb_cc_dd_bb_e", search_text = "bb"
+#     ---> should return 2 since "bb" shows up twice
+#
+#
+# full_text = "aaabbbcccc", search_text = "bbb"
+#     ---> should return 1
+# STRINGSFUNDAMENTALS
