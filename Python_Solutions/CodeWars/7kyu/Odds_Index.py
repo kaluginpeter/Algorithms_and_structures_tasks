fba692da1036d6daee06adf734c312a1219c8bfc
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 22:18:59 +0300

    Added solution "Odds-Index"

--- Python_Solutions/CodeWars/7kyu/Odds_Index.py
+++ Python_Solutions/CodeWars/7kyu/Odds_Index.py
@@ -3,3 +3,6 @@
 # Determine if any of the numbers in the array is the index of the "odd" word. If so, return true, otherwise false.
 #
 # ALGORITHMS
+# Solution
+def odd_ball(arr):
+    return any(arr[i] == 'odd' for i in [i for i in arr if type(i) == int and i <= len(arr)-1])
