4fa0aa09a9a5e9349eb78463dd57fe23737d0001
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 23:25:43 +0300

    Added solution "Santa's Naughty List"

--- Python_Solutions/CodeWars/7kyu/Santa's_Naughty_List.py
+++ Python_Solutions/CodeWars/7kyu/Santa's_Naughty_List.py
@@ -3,3 +3,6 @@
 # Comparison should be case sensitive and the returned list should contain only one copy of each name: "Sam" and "sam" are different, but "sAm" and "sAm" are not.
 #
 # LISTSSORTINGFUNDAMENTALS
+# Solution
+def find_children(santas_list, children):
+    return sorted(set([child for child in children if child in santas_list]))
