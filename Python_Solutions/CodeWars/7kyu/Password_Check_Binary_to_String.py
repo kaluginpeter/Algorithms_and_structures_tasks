72b2b69ffb0afd3f74909526965a6c15a0d4d468
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 22:31:10 +0300

    Added description "Password Check - Binary to String"

---
+++ Python_Solutions/CodeWars/7kyu/Password_Check_Binary_to_String.py
@@ -0,0 +1,9 @@
+# Password Check - Binary to String
+# A wealthy client has forgotten the password to his business website, but he has a list of possible passwords. His previous developer has left a file on the server with the name password.txt. You open the file and realize it's in binary format.
+#
+# Write a script that takes an array of possible passwords and a string of binary representing the possible password. Convert the binary to a string and compare to the password array. If the password is found, return the password string, else return false;
+#
+# decode_pass(['password123', 'admin', 'admin1'], '01110000 01100001 01110011 01110011 01110111 01101111 01110010 01100100 00110001 00110010 00110011');    => 'password123'
+# decode_pass(['password321', 'admin', 'admin1'], '01110000 01100001 01110011 01110011 01110111 01101111 01110010 01100100 00110001 00110010 00110011');    => False
+# decode_pass(['password456', 'pass1', 'test12'], '01110000 01100001 01110011 01110011 01110111 01101111 01110010 01100100 00110001 00110010 00110011');    => False
+# BINARYSTRINGSARRAYSFUNDAMENTALS
