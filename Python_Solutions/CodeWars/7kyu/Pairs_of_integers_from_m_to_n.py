b4d2dfc38bf63dda483d0491ccdca6fe7441b942
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 22:26:47 +0300

    Added description "Pairs of integers from m to n"

---
+++ Python_Solutions/CodeWars/7kyu/Pairs_of_integers_from_m_to_n.py
@@ -0,0 +1,10 @@
+# Implement a function that receives two integers m and n and generates a sorted list of pairs (a, b) such that m <= a <= b <= n.
+#
+# The input m will always be smaller than or equal to n (e.g., m <= n)
+#
+# Example
+# m = 2
+# n = 4
+#
+# result = [(2, 2), (2, 3), (2, 4), (3, 3), (3, 4), (4, 4)]
+# FUNDAMENTALS
