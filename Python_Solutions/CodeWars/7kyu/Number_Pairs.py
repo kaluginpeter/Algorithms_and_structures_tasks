b3c63015c04bae7e87a302015c86b85a8a1d07ac
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 22:01:50 +0300

    Added description "Number Pairs"

---
+++ Python_Solutions/CodeWars/7kyu/Number_Pairs.py
@@ -0,0 +1,10 @@
+# In this Kata the aim is to compare each pair of integers from 2 arrays, and return a new array of large numbers.
+#
+# Note: Both arrays have the same dimensions.
+#
+# Example:
+#
+# arr1 = [13, 64, 15, 17, 88]
+# arr2 = [23, 14, 53, 17, 80]
+# get_larger_numbers(arr1, arr2) == [23, 64, 53, 17, 88]
+# ARRAYSFUNDAMENTALS
