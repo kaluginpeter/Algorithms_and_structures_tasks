b569b227183478e693f3e9a939c4a3b47c39ca9c
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 23:35:24 +0300

    Added solution "Sexy Primes <3"

--- Python_Solutions/CodeWars/7kyu/Sexy_Primes_3.py
+++ Python_Solutions/CodeWars/7kyu/Sexy_Primes_3.py
@@ -9,3 +9,7 @@
 # Note: x & y are always positive integers, but they are not always in order of precendence... For example you can be given either (5, 11) or (11, 5) - both are valid.
 #
 # FUNDAMENTALS
+# Solution
+from gmpy2 import is_prime
+def sexy_prime(x, y):
+    return abs(x - y) == 6 and is_prime(x) and is_prime(y)
