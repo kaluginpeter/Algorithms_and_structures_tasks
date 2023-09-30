3bd5149ea46594f817edb832cc112c6cb4469d7c
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 18:27:39 +0300

    Added description "Next Prime"

---
+++ Python_Solutions/CodeWars/7kyu/Next_Prime.py
@@ -0,0 +1,10 @@
+# Get the next prime number!
+#
+# You will get a numbern (>= 0) and your task is to find the next prime number.
+#
+# Make sure to optimize your code: there will numbers tested up to about 10^12.
+#
+# Examples
+# 5   =>  7
+# 12  =>  13
+# ALGORITHMS
