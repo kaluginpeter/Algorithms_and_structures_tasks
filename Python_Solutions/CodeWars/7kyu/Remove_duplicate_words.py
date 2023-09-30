079842e08a2cdb86466d3b6c10e15746d5f76654
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 23:03:22 +0300

    Added solution "Remove duplicate words"

--- Python_Solutions/CodeWars/7kyu/Remove_duplicate_words.py
+++ Python_Solutions/CodeWars/7kyu/Remove_duplicate_words.py
@@ -11,3 +11,10 @@
 # 'alpha beta gamma delta'
 #
 # STRINGSREGULAR EXPRESSIONSALGORITHMS
+# Solution
+def remove_duplicate_words(s):
+    l = []
+    for word in s.split():
+        if word not in l:
+            l.append(word)
+    return ' '.join(l)
