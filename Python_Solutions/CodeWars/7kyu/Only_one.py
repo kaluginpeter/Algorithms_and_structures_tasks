0d1fbe50af28f19120e38cd232d06ecc78948c8f
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 22:21:29 +0300

    Added description "Only one"

---
+++ Python_Solutions/CodeWars/7kyu/Only_one.py
@@ -0,0 +1,7 @@
+# Given any number of boolean flags function should return true if and only if one of them is true while others are false. If function is called without arguments it should return false.
+#
+#   only_one() == False
+#   only_one(True, False, False) == True
+#   only_one(True, False, False, True) == False
+#   only_one(False, False, False, False) == False
+# ALGORITHMS
