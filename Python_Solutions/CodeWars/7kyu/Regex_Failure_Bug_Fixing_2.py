b5d290db9fc0c96444b6ed4e16e1a15061389b57
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 22:57:06 +0300

    Added description "Regex Failure - Bug Fixing #2"

---
+++ Python_Solutions/CodeWars/7kyu/Regex_Failure_Bug_Fixing_2.py
@@ -0,0 +1,21 @@
+# Problem Description:
+#
+# Oh no, Timmy's received some hate mail recently but he knows better. Help Timmy fix his regex filter so he can be awesome again!
+#
+# Task:
+#
+# You are given a string phrase containing some potentially offensive words such as "bad," "mean," "ugly," "horrible," and "hideous." Timmy wants to replace these words with the word "awesome" to make the message more positive. Your task is to implement a function, that replaces all occurrences of these offensive words with "awesome" in the input string phrase.
+#
+# Input:
+#
+# A string phrase containing words separated by spaces.
+# Output:
+#
+# A string with the same words as phrase, but with any offensive words replaced by "awesome."
+# Example:
+#
+# Input: "You're Bad! timmy!"
+# Output: "You're awesome! timmy!"
+# Input: "You're MEAN! timmy!"
+# Output: "You're awesome! timmy!"
+# DEBUGGING
