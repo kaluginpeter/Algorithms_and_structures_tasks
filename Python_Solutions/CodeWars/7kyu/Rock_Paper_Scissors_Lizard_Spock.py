fc0838640a8375fe39ee5c9d089d95702536417d
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 23:19:18 +0300

    Added description "Rock Paper Scissors Lizard Spock"

---
+++ Python_Solutions/CodeWars/7kyu/Rock_Paper_Scissors_Lizard_Spock.py
@@ -0,0 +1,21 @@
+# In this kata, your task is to implement an extended version of the famous rock-paper-scissors game. The rules are as follows:
+#
+# Scissors cuts Paper
+# Paper covers Rock
+# Rock crushes Lizard
+# Lizard poisons Spock
+# Spock smashes Scissors
+# Scissors decapitates Lizard
+# Lizard eats Paper
+# Paper disproves Spock
+# Spock vaporizes Rock
+# Rock crushes Scissors
+# Task:
+# Given two values from the above game, return the Player result as "Player 1 Won!", "Player 2 Won!", or "Draw!".
+#
+# Inputs
+# Values will be given as one of "rock", "paper", "scissors", "lizard", "spock".
+#
+#
+#
+# FUNDAMENTALSLOGIC
