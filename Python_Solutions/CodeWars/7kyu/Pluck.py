808b112725b4e102f70f7dd98382011dc6bcc3a6
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 22:38:22 +0300

    Added description "Pluck"

---
+++ Python_Solutions/CodeWars/7kyu/Pluck.py
@@ -0,0 +1,9 @@
+# Implement a function which takes a sequence of objects and a property name, and returns a sequence containing the named property of each object.
+#
+# For example:
+#
+# pluck([{'a':1}, {'a':2}], 'a')        # -> [1,2]
+# pluck([{'a':1, 'b':3}, {'a':2}], 'b') # -> [3, None]
+# If an object is missing the property, you should just leave it as undefined/None in the output array.
+#
+# FUNCTIONAL PROGRAMMINGARRAYSFUNDAMENTALS
