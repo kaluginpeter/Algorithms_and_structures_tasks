947eb1746be33d59c554946400d008a18a8626cb
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 22:18:46 +0300

    Added description "Odds-Index"

---
+++ Python_Solutions/CodeWars/7kyu/Odds_Index.py
@@ -0,0 +1,5 @@
+# You are given an array with several "even" words, one "odd" word, and some numbers mixed in.
+#
+# Determine if any of the numbers in the array is the index of the "odd" word. If so, return true, otherwise false.
+#
+# ALGORITHMS
