22d74cfa32a38d8890cbfe0ccdc5af34ebe16ce7
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 23:29:16 +0300

    Added description "Scrolling Text"

---
+++ Python_Solutions/CodeWars/7kyu/Scrolling_Text.py
@@ -0,0 +1,18 @@
+# Let's create some scrolling text!
+#
+# Your task is to complete the function which takes a string, and returns an array with all possible rotations of the given string, in uppercase.
+#
+# Example
+# scrollingText("codewars") should return:
+#
+# [ "CODEWARS",
+#   "ODEWARSC",
+#   "DEWARSCO",
+#   "EWARSCOD",
+#   "WARSCODE",
+#   "ARSCODEW"
+#   "RSCODEWA",
+#   "SCODEWAR" ]
+# Good luck!
+#
+# STRINGSARRAYSFUNDAMENTALS
