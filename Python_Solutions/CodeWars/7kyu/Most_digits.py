d79309e359a24a50444b27e2f635a0bf3c583718
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 18:12:07 +0300

    Added solution "Most digits"

--- Python_Solutions/CodeWars/7kyu/Most_digits.py
+++ Python_Solutions/CodeWars/7kyu/Most_digits.py
@@ -3,3 +3,7 @@
 # If two numbers in the argument array have the same number of digits, return the first one in the array.
 #
 # STRINGSFUNDAMENTALS
+# Solution
+def find_longest(arr):
+    arr_len = [len(str(n)) for n in arr];
+    return arr[arr_len.index(max(arr_len))];
