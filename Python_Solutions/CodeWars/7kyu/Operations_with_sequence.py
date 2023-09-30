02948a74a0274b8b9df2408cb58f705741eecbad
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 22:22:10 +0300

    Added description "Operations with sequence"

---
+++ Python_Solutions/CodeWars/7kyu/Operations_with_sequence.py
@@ -0,0 +1,16 @@
+# Steps
+#
+# Square the numbers that are greater than zero.
+# Multiply by 3 every third number.
+# Multiply by -1 every fifth number.
+# Return the sum of the sequence.
+# Example
+# { -2, -1, 0, 1, 2 } returns -6
+#
+# 1. { -2, -1, 0, 1 * 1, 2 * 2 }
+# 2. { -2, -1, 0 * 3, 1, 4 }
+# 3. { -2, -1, 0, 1, -1 * 4 }
+# 4. -6
+# P.S.: The sequence consists only of integers. And try not to use "for", "while" or "loop" statements.
+#
+# FUNDAMENTALSARRAYS
