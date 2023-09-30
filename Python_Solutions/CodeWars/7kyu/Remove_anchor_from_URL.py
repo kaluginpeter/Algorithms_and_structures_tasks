60d89485d27330dbb955ab607a96252fd1e0290d
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 23:02:20 +0300

    Added solution "Remove anchor from URL"

--- Python_Solutions/CodeWars/7kyu/Remove_anchor_from_URL.py
+++ Python_Solutions/CodeWars/7kyu/Remove_anchor_from_URL.py
@@ -4,3 +4,9 @@
 # "www.codewars.com#about" --> "www.codewars.com"
 # "www.codewars.com?page=1" -->"www.codewars.com?page=1"
 # REGULAR EXPRESSIONSSTRINGSFUNDAMENTALS
+# Solution
+def remove_url_anchor(url):
+    if url.count('#'):
+        index = url.index('#')
+        return url[:index]
+    return url
