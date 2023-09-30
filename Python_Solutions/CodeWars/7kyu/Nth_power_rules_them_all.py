07fd041f87ec2580107730463f2747d5878b7139
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 22:00:32 +0300

    Added solution "Nth power rules them all!"

--- Python_Solutions/CodeWars/7kyu/Nth_power_rules_them_all.py
+++ Python_Solutions/CodeWars/7kyu/Nth_power_rules_them_all.py
@@ -6,3 +6,6 @@
 # {1, 2, 3}, 3  -->  (1^3 + 2^3 + 3^3 ) - (1 + 2 + 3)  -->  36 - 6  -->  30
 # {1, 2}, 5     -->  (1^5 + 2^5) - (1 + 2)             -->  33 - 3  -->  30
 # FUNDAMENTALSMATHEMATICSALGORITHMSALGEBRA
+# Solution
+def modified_sum(a, n):
+    return sum(i**n for i in a) - sum(a)
