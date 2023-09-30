d71ec412aef160ddd6681c936217b14baee09978
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 22:41:37 +0300

    Added description "Populate hash with array keys and default value"

---
+++ Python_Solutions/CodeWars/7kyu/Populate_hash_with_array_keys_and_default_value.py
@@ -0,0 +1,5 @@
+# Complete the function so that it takes an array of keys and a default value and returns a hash (Ruby) / dictionary (Python) with all keys set to the default value.
+#
+# Example
+# ["draft", "completed"], 0   # should return {"draft": 0, "completed: 0}
+# ARRAYSFUNDAMENTALS
