52315ecc1adb18a375d52e6a4c8bce6852668d55
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 22:36:56 +0300

    Added description "Person Class Bug"

---
+++ Python_Solutions/CodeWars/7kyu/Person_Class_Bug.py
@@ -0,0 +1,10 @@
+# The following code was thought to be working properly, however when the code tries to access the age of the person instance it fails.
+#
+# person = Person('Yukihiro', 'Matsumoto', 47)
+# print(person.full_name)
+# print(person.age)
+# For this exercise you need to fix the code so that it works correctly.
+#
+# Note: the order of the person's full name is first name and last name.
+#
+# DEBUGGINGOBJECT-ORIENTED PROGRAMMING
