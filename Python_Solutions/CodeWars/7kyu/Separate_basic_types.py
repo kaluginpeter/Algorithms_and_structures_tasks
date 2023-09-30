8be0f62fc1e41fc0f5e560fa98afbad5d1ecc034
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 23:33:43 +0300

    Added description "Separate basic types"

---
+++ Python_Solutions/CodeWars/7kyu/Separate_basic_types.py
@@ -0,0 +1,15 @@
+# Given: a sequence of different type of values (number, string, boolean). You should return an object with a separate properties for each of types presented in input. Each property should contain an array of corresponding values.
+#
+# keep order of values like in input array
+# if type is not presented in input, no corresponding property are expected
+# So, for this input:
+#
+# ['a', 1, 2, False, 'b']
+# expected output is:
+#
+# {
+#   int: [1, 2],
+#   str: ['a', 'b'],
+#   bool: [False]
+# }
+# FUNDAMENTALS
