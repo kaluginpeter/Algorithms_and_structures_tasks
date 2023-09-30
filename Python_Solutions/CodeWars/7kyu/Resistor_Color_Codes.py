3a03cd2bbd629ac6a626033a65a46e1e1b2ea6aa
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 23:06:26 +0300

    Added solution "Resistor Color Codes"

--- Python_Solutions/CodeWars/7kyu/Resistor_Color_Codes.py
+++ Python_Solutions/CodeWars/7kyu/Resistor_Color_Codes.py
@@ -38,3 +38,10 @@
 # Have fun! And if you enjoy this kata, check out the sequel: Resistor Color Codes, Part 2
 #
 # STRINGSFUNDAMENTALS
+# Solution
+def decode_resistor_colors(bands):
+    d = {'black':0, 'brown':1, 'red':2, 'orange':3, 'yellow':4, 'green':5, 'blue':6, 'violet':7, 'gray':8, 'white':9, 'silver':10, 'gold':5}
+    bands = [d[b] for b in bands.split()]
+    ohms = (bands[0] * 10 + bands[1]) * 10 ** bands[2]
+    ohms, sfx = (ohms/1000000.0, 'M') if ohms > 999999 else (ohms / 1000.0, 'k') if ohms > 999 else (ohms, '')
+    return "{}{} ohms, {}%".format(int(ohms) if ohms // 1 == ohms else ohms, sfx, bands[3] if len(bands) > 3 else 20)
