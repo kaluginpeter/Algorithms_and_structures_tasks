6f0507a70da161d59e86f1694a83e771bf984d4e
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 23:12:10 +0300

    Added description "Return the closest number multiple of 10"

---
+++ Python_Solutions/CodeWars/7kyu/Return_the_closest_number_multiple_of_10.py
@@ -0,0 +1,13 @@
+# Given a number return the closest number to it that is divisible by 10.
+#
+# Example input:
+#
+# 22
+# 25
+# 37
+# Expected output:
+#
+# 20
+# 30
+# 40
+# MATHEMATICSFUNDAMENTALS
