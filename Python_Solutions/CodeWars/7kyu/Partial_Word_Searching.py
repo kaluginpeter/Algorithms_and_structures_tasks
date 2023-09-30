3c2494b9bb8fd170ea225af815c0a3a282992209
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 22:29:59 +0300

    Added solution "Partial Word Searching"

--- Python_Solutions/CodeWars/7kyu/Partial_Word_Searching.py
+++ Python_Solutions/CodeWars/7kyu/Partial_Word_Searching.py
@@ -8,3 +8,6 @@
 # If the string to search for is "me", and the array to search is ["home", "milk", "Mercury", "fish"], the method should return ["home", "Mercury"].
 #
 # ARRAYSSTRINGSFUNDAMENTALS
+# Solution
+def word_search(query, seq):
+    return [i for i in seq if query.lower() in i.lower()] or ["None"]
