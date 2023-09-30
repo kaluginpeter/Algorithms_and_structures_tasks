31635c10dd4750b9854a3f35551d200bee95f86c
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 22:26:12 +0300

    Added solution "Pairs of integers from 0 to n"

--- Python_Solutions/CodeWars/7kyu/Pairs_of_integers_from_0_to_n.py
+++ Python_Solutions/CodeWars/7kyu/Pairs_of_integers_from_0_to_n.py
@@ -8,3 +8,6 @@
 # generate_pairs(2) should return
 # [ [0, 0], [0, 1], [0, 2], [1, 1], [1, 2], [2, 2] ]
 # FUNDAMENTALS
+# Solution
+def generate_pairs(n):
+    return [[i,j] for i in range(n+1) for j in range(i, n+1)]
