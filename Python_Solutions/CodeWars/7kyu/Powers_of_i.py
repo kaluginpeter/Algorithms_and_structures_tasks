ff10ad0b8b2dabc884a976800c5941b4bb5c3a63
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-30 22:45:11 +0300

    Added description "Powers of i"

---
+++ Python_Solutions/CodeWars/7kyu/Powers_of_i.py
@@ -0,0 +1,23 @@
+# i is the imaginary unit, it is defined by
+# �
+# ²
+# =
+# −
+# 1
+# i²=−1, therefore it is a solution to
+# �
+# ²
+# +
+# 1
+# =
+# 0
+# x²+1=0.
+#
+# Your Task
+# Complete the function pofi that returns
+# �
+# i to the power of a given non-negative integer in its simplest form, as a string (answer may contain
+# �
+# i).
+#
+# MATHEMATICSFUNDAMENTALS
