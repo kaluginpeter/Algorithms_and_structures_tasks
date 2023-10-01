1faa63dc6c89a763ff0553dda9f8e8b6dc1a536d
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 11:57:07 +0300

    Added description "Sum - Square Even, Root Odd"

---
+++ Python_Solutions/CodeWars/7kyu/Sum_Square_Even_Root_Odd.py
@@ -0,0 +1,7 @@
+# Complete the function that takes a list of numbers (nums), as the only argument to the function. Take each number in the list and square it if it is even, or square root the number if it is odd. Take this new list and return the sum of it, rounded to two decimal places.
+#
+# The list will never be empty and will only contain values that are greater than or equal to zero.
+#
+# Good luck!
+#
+# FUNDAMENTALS
