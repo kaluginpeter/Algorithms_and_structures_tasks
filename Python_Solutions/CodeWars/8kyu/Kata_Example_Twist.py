155ba9430976a5152f2cd150623c1a5913ca24b7
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 18:39:02 +0300

    Added description "Kata Example Twist"

---
+++ Python_Solutions/CodeWars/8kyu/Kata_Example_Twist.py
@@ -0,0 +1,5 @@
+# This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+#
+# Add the value "codewars" to the array websites 1,000 times.
+#
+# FUNDAMENTALS
