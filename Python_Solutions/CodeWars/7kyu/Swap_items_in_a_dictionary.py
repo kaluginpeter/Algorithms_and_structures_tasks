cef19717a534beb4078a8ac6bbfc7ff143c2c0c7
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 14:17:11 +0300

    Added description "Swap items in a dictionary"

---
+++ Python_Solutions/CodeWars/7kyu/Swap_items_in_a_dictionary.py
@@ -0,0 +1,13 @@
+# In this kata, you will take the keys and values of a dict and swap them around.
+#
+# You will be given a dictionary, and then you will want to return a dictionary with the old values as the keys, and list the old keys as values under their original keys.
+#
+# For example, given the dictionary: {'Ice': 'Cream', 'Age': '21', 'Light': 'Cream', 'Double': 'Cream'},
+#
+# you should return: {'Cream': ['Ice', 'Double', 'Light'], '21': ['Age']}
+#
+# Notes:
+# The dictionary given will only contain strings
+# The dictionary given will not be empty
+# You do not have to sort the items in the lists
+# FUNDAMENTALSARRAYSLOGIC
