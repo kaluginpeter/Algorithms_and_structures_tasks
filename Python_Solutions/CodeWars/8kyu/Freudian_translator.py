5bbf877af4ecdca4a3387652262b011a24fff625
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 17:31:46 +0300

    Added solution "Freudian translator"

--- Python_Solutions/CodeWars/8kyu/Freudian_translator.py
+++ Python_Solutions/CodeWars/8kyu/Freudian_translator.py
@@ -7,3 +7,6 @@
 # In this kata, the function will take a string as its argument, and return a string with every word replaced by the explanation to everything, according to Freud. Note that an empty string, or no arguments, should return an empty string.
 #
 # FUNDAMENTALS
+# Solution
+def to_freud(sentence):
+    return ' '.join(['sex'] * len(sentence.split()))
