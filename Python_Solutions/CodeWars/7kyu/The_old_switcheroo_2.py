6f67d4f1162f86b7e3aa1b5f0af399fb68c3bbe4
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 14:37:52 +0300

    Added description "The old switcheroo 2"

---
+++ Python_Solutions/CodeWars/7kyu/The_old_switcheroo_2.py
@@ -0,0 +1,13 @@
+# This is a follow up from my kata The old switcheroo
+#
+# Write the function :
+#
+# def encode(str)
+# that takes in a string str and replaces all the letters with their respective positions in the English alphabet.
+#
+# encode('abc') == '123'   # a is 1st in English alpabet, b is 2nd and c is 3rd
+# encode('codewars') == '315452311819'
+# encode('abc-#@5') == '123-#@5'
+# String are case sensitive.
+#
+# STRINGSFUNDAMENTALS
