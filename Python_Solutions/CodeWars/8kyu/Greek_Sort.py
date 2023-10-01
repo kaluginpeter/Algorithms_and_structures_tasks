26d49fa6ff42c34a3aac0d425189a60b9a197c26
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 18:16:11 +0300

    Added description "Greek Sort"

---
+++ Python_Solutions/CodeWars/8kyu/Greek_Sort.py
@@ -0,0 +1,20 @@
+# Write a comparator for a list of phonetic words for the letters of the greek alphabet.
+#
+# A comparator is:
+#
+# a custom comparison function of two arguments (iterable elements) which should return a negative, zero or positive number depending on whether the first argument is considered smaller than, equal to, or larger than the second argument
+#
+# (source: https://docs.python.org/2/library/functions.html#sorted)
+#
+# The greek alphabet is preloded for you as greek_alphabet:
+#
+# greek_alphabet = (
+#     'alpha', 'beta', 'gamma', 'delta', 'epsilon', 'zeta',
+#     'eta', 'theta', 'iota', 'kappa', 'lambda', 'mu',
+#     'nu', 'xi', 'omicron', 'pi', 'rho', 'sigma',
+#     'tau', 'upsilon', 'phi', 'chi', 'psi', 'omega')
+# Examples
+# greek_comparator('alpha', 'beta')   <  0
+# greek_comparator('psi', 'psi')      == 0
+# greek_comparator('upsilon', 'rho')  >  0
+# FUNDAMENTALSSORTING
