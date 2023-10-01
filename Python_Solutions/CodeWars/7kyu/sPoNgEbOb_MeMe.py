cce90c03b2f941311cf1723b8598ab2031f58bab
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 15:38:58 +0300

    Added description "sPoNgEbOb MeMe"

---
+++ Python_Solutions/CodeWars/7kyu/sPoNgEbOb_MeMe.py
@@ -0,0 +1,11 @@
+# Remember the spongebob meme that is meant to make fun of people by repeating what they say in a mocking way?
+#
+# "Dont use that weird spongebob mocking meme" Me: DonT uSe thAt WeIrd SpoNgEboB MoCkinG MEme
+#
+# You need to create a function that converts the input into this format, with the output being the same string expect there is a pattern of uppercase and lowercase letters.
+#
+# Example:
+#
+# input:  "stop Making spongebob Memes!"
+# output: "StOp mAkInG SpOnGeBoB MeMeS!"
+# FUNDAMENTALSSTRINGS
