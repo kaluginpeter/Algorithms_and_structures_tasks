dffa473fb872cd72a728160deca14e00f9d252a5
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 15:30:30 +0300

    Added description "filterEvenLengthWords"

---
+++ Python_Solutions/CodeWars/7kyu/filterEvenLengthWords.py
@@ -0,0 +1,9 @@
+# Write a function called "filterEvenLengthWords".
+#
+# Given an array of strings, "filterEvenLengthWords" returns an array containing only the elements of the given array whose length is an even number.
+#
+# var output = filterEvenLengthWords(['word', 'words', 'word', 'words']);
+#
+# console.log(output); // --> ['word', 'word']
+#
+# FUNDAMENTALS
