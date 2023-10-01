60d19743d80a64446b6870f5457bfcc39a77f175
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 18:17:13 +0300

    Added solution "Heads and Legs"

--- Python_Solutions/CodeWars/8kyu/Heads_and_Legs.py
+++ Python_Solutions/CodeWars/8kyu/Heads_and_Legs.py
@@ -31,3 +31,7 @@
 # Happy coding!
 #
 # ALGEBRALOGICMATHEMATICSALGORITHMS
+# Solution
+def animals(heads, legs):
+    x = (2*heads-legs/2, legs/2-heads)
+    return x if legs%2==0 and x[0] >= 0 and x[1] >= 0 else 'No solutions'
