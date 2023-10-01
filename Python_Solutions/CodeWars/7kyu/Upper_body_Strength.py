c306a37be63efb0d1362879f42bb57b1e290b2ea
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 15:04:46 +0300

    Added solution "Upper <body> Strength"

--- Python_Solutions/CodeWars/7kyu/Upper_body_Strength.py
+++ Python_Solutions/CodeWars/7kyu/Upper_body_Strength.py
@@ -2,3 +2,11 @@
 # He can do ten kata in an hour, but when he makes a mistake, he must do pushups. These pushups really tire poor Alex out, so every time he does them they take twice as long. His first set of redemption pushups takes 5 minutes. Create a function, alexMistakes, that takes two arguments: the number of kata he needs to complete, and the time in minutes he has to complete them. Your function should return how many mistakes Alex can afford to make.
 #
 # FUNDAMENTALS
+# Solution
+def alex_mistakes(katas, time):
+    c, t, s = 0, 5, time - katas * 6
+    while s >= t:
+        s -= t
+        t *= 2
+        c += 1
+    return c
