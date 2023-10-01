81e9ae15669c016cbac2d7d1facbe482beb42040
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 15:40:37 +0300

    Added solution "zero-balanced Array"

--- Python_Solutions/CodeWars/7kyu/zero_balanced_Array.py
+++ Python_Solutions/CodeWars/7kyu/zero_balanced_Array.py
@@ -1,3 +1,6 @@
 # An array is called zero-balanced if its elements sum to 0 and for each positive element n, there exists another element that is the negative of n. Write a function named ìsZeroBalanced that returns true if its argument is zero-balanced array, else return false. Note that an empty array will not sum to zero.
 #
 # ARRAYSFUNDAMENTALS
+# Solution
+def is_zero_balanced(arr):
+    return sum(arr) == 0 and all(abs(i) in arr if i < 0 else -i in arr for i in arr) if len(arr) > 0 else False
