00cfc64e90cb226d3d74221f6d9590e389f413c0
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 19:42:06 +0300

    Added solution "String cleaning"

--- Python_Solutions/CodeWars/8kyu/String_cleaning.py
+++ Python_Solutions/CodeWars/8kyu/String_cleaning.py
@@ -7,3 +7,6 @@
 # Your harried co-workers are looking to you for a solution to take this garbled text and remove all of the numbers. Your program will take in a string and clean out all numeric characters, and return a string with spacing and special characters ~#$%^&!@*():;"'.,? all intact.
 #
 # REGULAR EXPRESSIONSFUNDAMENTALSSTRINGS
+# Solution
+def string_clean(s):
+    return ''.join('' if ch.isdigit() else ch for ch in s)
