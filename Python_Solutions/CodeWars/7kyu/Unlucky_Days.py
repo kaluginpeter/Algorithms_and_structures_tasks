802f0e24d5781dac252cffe8284e19bf8c339f57
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 15:02:34 +0300

    Added description "Unlucky Days"

---
+++ Python_Solutions/CodeWars/7kyu/Unlucky_Days.py
@@ -0,0 +1,13 @@
+# Friday 13th or Black Friday is considered as unlucky day. Calculate how many unlucky days are in the given year.
+#
+# Find the number of Friday 13th in the given year.
+#
+# Input: Year in Gregorian calendar as integer.
+#
+# Output: Number of Black Fridays in the year as an integer.
+#
+# Examples:
+#
+# unluckyDays(2015) == 3
+# unluckyDays(1986) == 1
+# DATE TIME
