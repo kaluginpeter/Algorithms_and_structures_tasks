f916ee2f5622ef4eadabfd417651d50151a09cd9
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 15:20:37 +0300

    Added solution "Word to binary"

--- Python_Solutions/CodeWars/7kyu/Word_to_binary.py
+++ Python_Solutions/CodeWars/7kyu/Word_to_binary.py
@@ -3,3 +3,6 @@
 # Example: 'man' should return [ '01101101', '01100001', '01101110' ]
 #
 # FUNDAMENTALS
+# Solution
+def word_to_bin(word):
+    return [bin(ord(letter))[2:].zfill(8) for letter in word]
