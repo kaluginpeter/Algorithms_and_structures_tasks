a515a0db91318577d220558bb2cbc436763d8c7c
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 19:39:30 +0300

    Added description "SpeedCode #2 - Array Madness"

---
+++ Python_Solutions/CodeWars/8kyu/SpeedCode_2_Array_Madness.py
@@ -0,0 +1,10 @@
+# SpeedCode #2 - Array Madness
+# Objective
+# Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of each element in a is strictly greater than the sum of the cubes of each element in b.
+#
+# E.g.
+#
+# array_madness([4, 5, 6], [1, 2, 3]) => True #because 4 ** 2 + 5 ** 2 + 6 ** 2 > 1 ** 3 + 2 ** 3 + 3 ** 3
+# Get your timer out. Are you ready? Ready, get set, GO!!!
+#
+# ARRAYSPUZZLES
