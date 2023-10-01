a7665ae340ad7bcc49cd8fa4cf4da5c6c2d8c381
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 14:14:34 +0300

    Added description "Sums of consecutive integers"

---
+++ Python_Solutions/CodeWars/7kyu/Sums_of_consecutive_integers.py
@@ -0,0 +1,10 @@
+# The sum of x consecutive integers is y. What is the consecutive integer at position n? Given x, y, and n, solve for the integer. Assume the starting position is 0.
+#
+# For example, if the sum of 4 consecutive integers is 14, what is the consecutive integer at position 3?
+#
+# We find that the consecutive integers are [2, 3, 4, 5], so the integer at position 3 is 5.
+#
+# position(4, 14, 3) == 5
+# Assume there will always be a sum of x consecutive integers that totals to y and n will never be indexed out of bounds.
+#
+# MATHEMATICSALGORITHMS
