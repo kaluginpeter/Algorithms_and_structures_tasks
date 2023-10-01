5ac92ee4fb841f6d3c3005f55abe668ec88043f5
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 16:48:49 +0300

    Added solution "Drink about"

--- Python_Solutions/CodeWars/8kyu/Drink_about.py
+++ Python_Solutions/CodeWars/8kyu/Drink_about.py
@@ -18,3 +18,9 @@
 # 20 --> "drink beer"
 # 30 --> "drink whisky"
 # FUNDAMENTALS
+# Solution
+def people_with_age_drink(age):
+    if age < 14: return 'drink toddy'
+    elif age < 18: return 'drink coke'
+    elif age < 21: return 'drink beer'
+    elif age >= 21: return 'drink whisky'
