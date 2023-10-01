601f6091060e9376112aec9eb879a11a4e3ea407
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 19:54:28 +0300

    Added description "The 'if' function"

---
+++ Python_Solutions/CodeWars/8kyu/The_if_function.py
@@ -0,0 +1,14 @@
+# Create a function called _if which takes 3 arguments: a value bool and 2 functions (which do not take any parameters): func1 and func2
+#
+# When bool is truthy, func1 should be called, otherwise call the func2.
+#
+# Example:
+# def truthy():
+#   print("True")
+#
+# def falsey():
+#   print("False")
+#
+# _if(True, truthy, falsey)
+# # prints 'True' to the console
+# FUNCTIONAL PROGRAMMINGFUNDAMENTALS
