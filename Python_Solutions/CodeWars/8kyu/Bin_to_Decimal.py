1a320ee020081e4cc68d644b9fe594964a3c9afc
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 16:09:52 +0300

    Added solution "Bin to Decimal"

--- Python_Solutions/CodeWars/8kyu/Bin_to_Decimal.py
+++ Python_Solutions/CodeWars/8kyu/Bin_to_Decimal.py
@@ -1,3 +1,6 @@
 # Complete the function which converts a binary number (given as a string) to a decimal number.
 #
 # BINARYFUNDAMENTALS
+# Solution
+def bin_to_decimal(inp):
+    return int(inp, 2)
