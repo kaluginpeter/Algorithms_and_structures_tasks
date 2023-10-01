3439987098ee7ed6693fe14abd71dde0058dfb50
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 15:16:00 +0300

    Added description "Which triangle is that?"

---
+++ Python_Solutions/CodeWars/7kyu/Which_triangle_is_that.py
@@ -0,0 +1,7 @@
+# Build a function that will take the length of each side of a triangle and return if it's either an Equilateral, an Isosceles, a Scalene or an invalid triangle.
+#
+# It has to return a string with the type of triangle.
+# For example:
+#
+# type_of_triangle(2, 2, 1) --> "Isosceles"
+# GEOMETRYALGORITHMS
