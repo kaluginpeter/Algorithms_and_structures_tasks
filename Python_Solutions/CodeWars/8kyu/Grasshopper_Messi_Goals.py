a0407cf556abaf49aa73817f05258d7305db3052
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 18:09:51 +0300

    Added description "Grasshopper - Messi Goals"

---
+++ Python_Solutions/CodeWars/8kyu/Grasshopper_Messi_Goals.py
@@ -0,0 +1,17 @@
+# Messi's Goal Total
+# Use variables to find the sum of the goals Messi scored in 3 competitions
+#
+# Information
+# Messi goal scoring statistics:
+#
+# Competition	Goals
+# La Liga	43
+# Champions League	10
+# Copa del Rey	5
+# Task
+# Create these three variables and store the appropriate values using the table above:
+# la_liga_goals
+# champions_league_goals
+# copa_del_rey_goals
+# Create a fourth variable named total_goals that stores the sum of all of Messi's goals for this year.
+# FUNDAMENTALS
