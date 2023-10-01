b46f81525c279e60dd215f92ee21683ef5688ad4
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 12:06:41 +0300

    Added solution "Sum of a nested list"

--- Python_Solutions/CodeWars/7kyu/Sum_of_a_nested_list.py
+++ Python_Solutions/CodeWars/7kyu/Sum_of_a_nested_list.py
@@ -2,3 +2,6 @@
 #
 # sum_nested([1, [2, [3, [4]]]]) -> 10
 # RECURSIONLISTSALGORITHMSFUNDAMENTALS
+# Solution
+def sum_nested(lst):
+	return sum(sum_nested(x) if isinstance(x,list) else x for x in lst)
