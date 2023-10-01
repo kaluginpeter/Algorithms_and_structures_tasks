13dac7f77b2fcbca8c0701eb0119e0eba67a559c
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 17:22:02 +0300

    Added description "Find the force of gravity between two objects"

---
+++ Python_Solutions/CodeWars/8kyu/Find_the_force_of_gravity_between_two_objects.py
@@ -0,0 +1,39 @@
+# Your job is to find the gravitational force between two spherical objects (obj1 , obj2).
+#
+# input
+# Two arrays are given :
+#
+# arr_val (value array), consists of 3 elements
+# 1st element : mass of obj 1
+# 2nd element : mass of obj 2
+# 3rd element : distance between their centers
+# arr_unit (unit array), consists of 3 elements
+# 1st element : unit for mass of obj 1
+# 2nd element : unit for mass of obj 2
+# 3rd element : unit for distance between their centers
+# mass units are :
+#
+# kilogram (kg)
+# gram (g)
+# milligram (mg)
+# microgram (μg)
+# pound (lb)
+# distance units are :
+#
+# meter (m)
+# centimeter (cm)
+# millimeter (mm)
+# micrometer (μm)
+# feet (ft)
+# Note
+# value of G = 6.67 × 10−11 N⋅kg−2⋅m2
+#
+# 1 ft = 0.3048 m
+#
+# 1 lb = 0.453592 kg
+#
+# return value must be Newton for force (obviously)
+#
+# μ copy this from here to use it in your solution
+#
+# ALGORITHMS
