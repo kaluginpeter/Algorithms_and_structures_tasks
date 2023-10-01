9ebc2e8e73e61ea5ee29117a362b4f035cf30ce2
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 19:22:29 +0300

    Added solution "Return to Sanity"

--- Python_Solutions/CodeWars/8kyu/Return_to_Sanity.py
+++ Python_Solutions/CodeWars/8kyu/Return_to_Sanity.py
@@ -1,3 +1,7 @@
 # This function should return an object, but it's not doing what's intended. What's wrong?
 #
 # DEBUGGING
+# Solution
+def mystery():
+    results = {'sanity': 'Hello'}
+    return results
