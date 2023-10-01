8bcf638c412ca283e37cbd44c3463a6d3fa19f43
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 14:38:44 +0300

    Added solution "The reject() function"

--- Python_Solutions/CodeWars/7kyu/The_reject_function.py
+++ Python_Solutions/CodeWars/7kyu/The_reject_function.py
@@ -2,3 +2,6 @@
 #
 # reject([1, 2, 3, 4, 5, 6], lambda x: x % 2 == 0)  =>  [1, 3, 5]
 # ARRAYSFUNDAMENTALS
+# Solution
+def reject(seq, predicate):
+    return [item for item in seq if not predicate(item)]
