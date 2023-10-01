87cd2db407077a1649afd0cfb0ab2cfb264bebe5
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 11:53:57 +0300

    Added description "Strings: starts with"

---
+++ Python_Solutions/CodeWars/7kyu/Strings_starts_with.py
@@ -0,0 +1,18 @@
+# Challenge: Given two null-terminated strings in the arguments "string" and "prefix", determine if "string" starts with the "prefix" string. Return true or false.
+#
+# Example:
+#
+# startsWith("hello world!", "hello"); // should return true
+# startsWith("hello world!", "HELLO"); // should return false
+# startsWith("nowai", "nowaisir"); // should return false
+# Addendum: For this problem, an empty "prefix" string should always return true for any value of "string".
+#
+# If the length of the "prefix" string is greater than the length of the "string", return false.
+#
+# The check should be case-sensitive, i.e. startsWith("hello", "HE") should return false, whereas startsWith("hello", "he") should return true.
+#
+# The length of the "string" as well as the "prefix" can be defined by the formula: 0 <= length < +Infinity
+#
+# No characters should be ignored and/or omitted during the test, e.g. whitespace characters should not be ignored.
+#
+# STRINGSFUNDAMENTALS
