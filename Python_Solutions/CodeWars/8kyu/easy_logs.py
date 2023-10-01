0a2ab31ea408069c3b0ec41bca53e7799976b810
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 20:32:34 +0300

    Added solution "easy logs"

--- Python_Solutions/CodeWars/8kyu/easy_logs.py
+++ Python_Solutions/CodeWars/8kyu/easy_logs.py
@@ -1,3 +1,7 @@
 # Add two logs with base X, with the value of A and B. Example log A + log B where the base is X.
 #
 # ALGORITHMS
+# Solution
+import math
+def logs(x, a, b):
+    return (math.log(a) / math.log(x)) + (math.log(b) / math.log(x))
