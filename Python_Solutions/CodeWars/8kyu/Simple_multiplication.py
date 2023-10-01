170dec9ed9868162edad754eb1a1a66789917ec2
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 19:35:23 +0300

    Added solution "Simple multiplication"

--- Python_Solutions/CodeWars/8kyu/Simple_multiplication.py
+++ Python_Solutions/CodeWars/8kyu/Simple_multiplication.py
@@ -1,3 +1,9 @@
 # This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
 #
 # FUNDAMENTALS
+# Solution
+def simple_multiplication(number) :
+    if number % 2 == 0:
+        return number * 8
+    else:
+        return number * 9
