1f242d4c363174de2c5e19d107877b91ab6e8713
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 11:20:44 +0300

    Added description "Slope of a Line"

---
+++ Python_Solutions/CodeWars/7kyu/Slope_of_a_Line.py
@@ -0,0 +1,7 @@
+# Task
+# Your challenge is to write a function named getSlope/get_slope/GetSlope that calculates the slope of the line through two points.
+#
+# Input
+# Each point that the function takes in is an array 2 elements long. The first number is the x coordinate and the second number is the y coordinate. If the line through the two points is vertical or if the same point is given twice, the function should return null/None.
+#
+# GRAPHSMATHEMATICSGEOMETRYALGEBRAFUNDAMENTALS
