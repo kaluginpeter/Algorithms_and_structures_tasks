2ca393c290c5203887509ff70bbc3774bd86afdb
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 10:57:49 +0300

    Added solution "Simple Fun #20: First Reverse Try"

--- Python_Solutions/CodeWars/7kyu/Simple_Fun_20_First_Reverse_Try.py
+++ Python_Solutions/CodeWars/7kyu/Simple_Fun_20_First_Reverse_Try.py
@@ -16,3 +16,8 @@
 # Array arr with its first and its last elements swapped.
 #
 # PUZZLES
+# Solution
+def first_reverse_try(arr):
+    if arr:
+        arr[0], arr[-1] = arr[-1], arr[0]
+    return arr
