c5d0e868a83d3d3caafe15c9bf46cbc5f33a19ab
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 11:13:28 +0300

    Added description "Simple equation reversal"

---
+++ Python_Solutions/CodeWars/7kyu/Simple_equation_reversal.py
@@ -0,0 +1,15 @@
+# Given a mathematical equation that has *,+,-,/, reverse it as follows:
+#
+# solve("100*b/y") = "y/b*100"
+# solve("a+b-c/d*30") = "30*d/c-b+a"
+# More examples in test cases.
+#
+# Good luck!
+#
+# Please also try:
+#
+# Simple time difference
+#
+# Simple remove duplicates
+#
+# ALGORITHMS
