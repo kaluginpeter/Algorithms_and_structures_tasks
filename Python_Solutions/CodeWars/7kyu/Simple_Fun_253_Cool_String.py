f5b06a1b44883150dcea02ce8549a494efeb5ea0
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 10:58:20 +0300

    Added description "Simple Fun #253: Cool String"

---
+++ Python_Solutions/CodeWars/7kyu/Simple_Fun_253_Cool_String.py
@@ -0,0 +1,24 @@
+# Task
+# Let's call a string cool if it is formed only by Latin letters and no two lowercase and no two uppercase letters are in adjacent positions. Given a string, check if it is cool.
+#
+# Input/Output
+# [input] string s
+#
+# A string that contains uppercase letters, lowercase letters numbers and spaces.
+#
+# 1 ≤ s.length ≤ 20.
+#
+# [output] a boolean value
+#
+# true if s is cool, false otherwise.
+#
+# Example
+# For s = "aQwFdA", the output should be true
+#
+# For s = "aBC", the output should be false;
+#
+# For s = "AaA", the output should be true;
+#
+# For s = "q q", the output should be false.
+#
+# FUNDAMENTALS
