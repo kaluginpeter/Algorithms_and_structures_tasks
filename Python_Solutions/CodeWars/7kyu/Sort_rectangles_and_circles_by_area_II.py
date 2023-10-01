b0a97a0d2b43740c183942cc484342fa8357ec03
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 11:31:14 +0300

    Added solution "Sort rectangles and circles by area II"

--- Python_Solutions/CodeWars/7kyu/Sort_rectangles_and_circles_by_area_II.py
+++ Python_Solutions/CodeWars/7kyu/Sort_rectangles_and_circles_by_area_II.py
@@ -8,3 +8,10 @@
 # This kata inspired by Sort rectangles and circles by area.
 #
 # FUNDAMENTALSALGORITHMSSORTINGMATHEMATICSGEOMETRY
+# Solution
+def sort_by_area(seq):
+    def func(x):
+        if isinstance(x, tuple):
+            return x[0] * x[1]
+        return 3.14 * x * x
+    return sorted(seq, key=func)
