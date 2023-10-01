8bf7dde76fbee5dfde048068f227a02443657b4d
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 11:48:31 +0300

    Added description "String Reversing, Changing case, etc."

---
+++ Python_Solutions/CodeWars/7kyu/String_Reversing_Changing_case_etc.py
@@ -0,0 +1,8 @@
+# Given 2 string parameters, show a concatenation of:
+#
+# the reverse of the 2nd string with inverted case; e.g Fish -> HSIf
+# a separator in between both strings: @@@
+# the 1st string reversed with inverted case and then mirrored; e.g Water -> RETAwwATER
+# ** Keep in mind that this kata was initially designed for Shell, I'm aware it may be easier in other languages.**
+#
+# FUNDAMENTALS
