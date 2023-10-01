1591b77419b3a527d5062c3aaaf2694429d8c8fa
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 10:54:22 +0300

    Added description "Simple Fun #144: Distinct Digit Year"

---
+++ Python_Solutions/CodeWars/7kyu/Simple_Fun_144_Distinct_Digit_Year.py
@@ -0,0 +1,13 @@
+# Task
+# The year of 2013 is the first year after the old 1987 with only distinct digits.
+#
+# Now your task is to solve the following problem: given a year number, find the minimum year number which is strictly larger than the given one and has only distinct digits.
+#
+# Input/Output
+# [input] integer year
+# 1000 ≤ year ≤ 9000
+#
+# [output] an integer
+# the minimum year number that is strictly larger than the input number year and all its digits are distinct.
+#
+# ALGORITHMS
