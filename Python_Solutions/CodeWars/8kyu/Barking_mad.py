c33d13302df10798e30531f074475e24419bc4c7
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 15:59:38 +0300

    Added description "Barking mad"

---
+++ Python_Solutions/CodeWars/8kyu/Barking_mad.py
@@ -0,0 +1,7 @@
+# Teach snoopy and scooby doo how to bark using object methods. Currently only snoopy can bark and not scooby doo.
+#
+# snoopy.bark() #return "Woof"
+# scoobydoo.bark() #undefined
+# Use method prototypes to enable all Dogs to bark.
+#
+# FUNDAMENTALSOBJECT-ORIENTED PROGRAMMING
