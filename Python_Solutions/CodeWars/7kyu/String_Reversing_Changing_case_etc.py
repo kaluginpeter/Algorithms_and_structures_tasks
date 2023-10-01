e5ca9f1c0f4dc332be9841352188fffe4fb8ff4c
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 11:48:47 +0300

    Added solution "String Reversing, Changing case, etc."

--- Python_Solutions/CodeWars/7kyu/String_Reversing_Changing_case_etc.py
+++ Python_Solutions/CodeWars/7kyu/String_Reversing_Changing_case_etc.py
@@ -6,3 +6,6 @@
 # ** Keep in mind that this kata was initially designed for Shell, I'm aware it may be easier in other languages.**
 #
 # FUNDAMENTALS
+# Solution
+def reverse_and_mirror(s1, s2):
+    return f"{s2[::-1].swapcase()}@@@{s1[::-1].swapcase()}{s1.swapcase()}"
