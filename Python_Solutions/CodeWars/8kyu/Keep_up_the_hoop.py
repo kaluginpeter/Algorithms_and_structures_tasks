b00b208578fae09e9610d9b63a9a5ead79788a65
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 18:40:35 +0300

    Added solution "Keep up the hoop"

--- Python_Solutions/CodeWars/8kyu/Keep_up_the_hoop.py
+++ Python_Solutions/CodeWars/8kyu/Keep_up_the_hoop.py
@@ -5,3 +5,9 @@
 # If Alex gets 10 or more hoops, return the string "Great, now move on to tricks".
 # If he doesn't get 10 hoops, return the string "Keep at it until you get it".
 # FUNDAMENTALS
+# Solution
+def hoop_count(n):
+    if n < 10:
+        return "Keep at it until you get it"
+    else:
+        return "Great, now move on to tricks"
