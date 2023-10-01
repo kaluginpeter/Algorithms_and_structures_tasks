54ebffe283585814c9dcc51a332d286f8fd866e5
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 19:03:34 +0300

    Added solution "Playing with cubes II"

--- Python_Solutions/CodeWars/8kyu/Playing_with_cubes_II.py
+++ Python_Solutions/CodeWars/8kyu/Playing_with_cubes_II.py
@@ -7,3 +7,13 @@
 # The constructor taking no arguments should assign 0 to Cube's Side property.
 #
 # FUNDAMENTALSOBJECT-ORIENTED PROGRAMMING
+# Solution
+class Cube(object):
+    def __init__(self, side=0):
+        self._side = abs(side)
+
+    def get_side(self):\
+        return self._side
+
+    def set_side(self, new_side):
+        self._side = abs(new_side)
