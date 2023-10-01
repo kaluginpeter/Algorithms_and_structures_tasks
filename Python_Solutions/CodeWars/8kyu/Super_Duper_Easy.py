d08c7a3c2c4638d7fc1dc197afeadf70302250da
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 19:49:41 +0300

    Added solution "Super Duper Easy"

--- Python_Solutions/CodeWars/8kyu/Super_Duper_Easy.py
+++ Python_Solutions/CodeWars/8kyu/Super_Duper_Easy.py
@@ -1,3 +1,6 @@
 # Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
 #
 # FUNDAMENTALS
+# Solution
+def problem(a):
+    return a * 50 + 6 if type(a) != str else 'Error'
