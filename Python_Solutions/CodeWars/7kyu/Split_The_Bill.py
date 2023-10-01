61374ea3a304ae6b887eb7fb63341086b2e826af
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 11:41:17 +0300

    Added solution "Split The Bill"

--- Python_Solutions/CodeWars/7kyu/Split_The_Bill.py
+++ Python_Solutions/CodeWars/7kyu/Split_The_Bill.py
@@ -23,3 +23,12 @@
 # Good Luck and Happy Eats!
 #
 # FUNDAMENTALSDATA STRUCTURES
+# Solution
+group = {
+    'A': 20,
+    'B': 15,
+    'C': 10
+}
+def split_the_bill(x):
+    total_each_owed = sum(x.values())/float(len(x))
+    return {key:round(value - total_each_owed, 2) for key, value in x.items()}
