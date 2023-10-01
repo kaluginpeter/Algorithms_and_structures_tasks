6425d695a4e154a39283026c7c671a9af7b738e4
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 17:35:14 +0300

    Added description "Generate range of integers"

---
+++ Python_Solutions/CodeWars/8kyu/Generate_range_of_integers.py
@@ -0,0 +1,12 @@
+# Implement a function named generateRange(min, max, step), which takes three arguments and generates a range of integers from min to max, with the step. The first integer is the minimum value, the second is the maximum of the range and the third is the step. (min < max)
+#
+# Task
+# Implement a function named
+#
+# generate_range(2, 10, 2) # should return list of [2,4,6,8,10]
+# generate_range(1, 10, 3) # should return list of [1,4,7,10]
+# Note
+# min < max
+# step > 0
+# the range does not HAVE to include max (depending on the step)
+# ALGORITHMS
