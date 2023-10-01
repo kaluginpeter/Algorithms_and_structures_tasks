c07d333df6df3137726fc20e33f150f159c3ba59
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 19:29:19 +0300

    Added solution "Rock Paper Scissors!"

--- Python_Solutions/CodeWars/8kyu/Rock_Paper_Scissors.py
+++ Python_Solutions/CodeWars/8kyu/Rock_Paper_Scissors.py
@@ -9,3 +9,18 @@
 # rockpaperscissors
 #
 # FUNDAMENTALS
+# Solution
+def rps(p1, p2):
+    if p1 == p2: return 'Draw!'
+    elif p1 == 'rock' and p2 == 'scissors':
+        return 'Player 1 won!'
+    elif p1 == 'scissors' and p2 == 'rock':
+        return 'Player 2 won!'
+    elif p1 == 'paper' and p2 == 'rock':
+        return 'Player 1 won!'
+    elif p1 == 'rock' and p2 == 'paper':
+        return 'Player 2 won!'
+    elif p1 == 'paper' and p2 == 'scissors':
+        return 'Player 2 won!'
+    elif p1 == 'scissors' and p2 == 'paper':
+        return 'Player 1 won!'
