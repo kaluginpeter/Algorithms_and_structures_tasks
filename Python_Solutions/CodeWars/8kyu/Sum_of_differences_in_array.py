d4cc63d0992e1060a135f7f1bc6255131f006959
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 19:47:39 +0300

    Added solution "Sum of differences in array"

--- Python_Solutions/CodeWars/8kyu/Sum_of_differences_in_array.py
+++ Python_Solutions/CodeWars/8kyu/Sum_of_differences_in_array.py
@@ -9,3 +9,11 @@
 # If the array is empty or the array has only one element the result should be 0 (Nothing in Haskell, None in Rust).
 #
 # ARRAYSFUNDAMENTALS
+# Solution
+def sum_of_differences(arr):
+    arr = sorted(arr, reverse = True)
+    list = []
+    for i in range(len(arr)):
+        for j in range(i + 1, len(arr)):
+            list.append(arr[i] - arr[j])
+    return max(list) if len(arr) > 1 else 0
