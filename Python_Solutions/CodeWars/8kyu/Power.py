4423ddb1b963196240eb44a7ee32ed17ee8b6dc1
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 19:07:04 +0300

    Added description "Power"

---
+++ Python_Solutions/CodeWars/8kyu/Power.py
@@ -0,0 +1,9 @@
+# The goal is to create a function of two inputs number and power, that "raises" the number up to power (ie multiplies number by itself power times).
+#
+# Examples
+# number_to_pwr(3, 2)  # -> 9 ( = 3 * 3 )
+# number_to_pwr(2, 3)  # -> 8 ( = 2 * 2 * 2 )
+# number_to_pwr(10, 6) # -> 1000000
+# Note: math.pow and some others math functions are disabled on final tests.
+#
+# RESTRICTED
