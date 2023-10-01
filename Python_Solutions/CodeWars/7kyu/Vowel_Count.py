1161386031c5d36484704e68852093ab79c67604
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 15:09:44 +0300

    Added solution "Vowel Count"

--- Python_Solutions/CodeWars/7kyu/Vowel_Count.py
+++ Python_Solutions/CodeWars/7kyu/Vowel_Count.py
@@ -5,3 +5,11 @@
 # The input string will only consist of lower case letters and/or spaces.
 #
 # STRINGSFUNDAMENTALS
+# Solution
+def get_count(sentence):
+    gl = ['a', 'e', 'i', 'o', 'u']
+    count = 0
+    for x in sentence:
+        if x in gl:
+             count += 1
+    return count
