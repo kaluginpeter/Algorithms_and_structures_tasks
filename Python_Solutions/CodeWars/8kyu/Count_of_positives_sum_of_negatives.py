37c440a75998a51531fe7da1ac011da2b37ece0f
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 16:33:49 +0300

    Added solution "Count of positives / sum of negatives"

--- Python_Solutions/CodeWars/8kyu/Count_of_positives_sum_of_negatives.py
+++ Python_Solutions/CodeWars/8kyu/Count_of_positives_sum_of_negatives.py
@@ -8,3 +8,16 @@
 # For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
 #
 # FUNDAMENTALSARRAYSLISTS
+# Solution
+def count_positives_sum_negatives(arr):
+    count = 0
+    count1 = 0
+    if not arr:
+        return arr
+    else:
+        for i in arr:
+            if i > 0:
+                count += 1
+            elif i < 0:
+                count1+= i
+        return [count, count1]
