675f268876894ea70fe748753d432b53eda449cd
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 17:34:47 +0300

    Added solution "Fundamentals: Return"

--- Python_Solutions/CodeWars/8kyu/Fundamentals_Return.py
+++ Python_Solutions/CodeWars/8kyu/Fundamentals_Return.py
@@ -17,3 +17,16 @@
 # Note: All math operations will be: a (operation) b
 #
 # FUNDAMENTALS
+# Solution
+def add(a, b):
+    return a + b
+def multiply(a, b):
+    return a * b
+def divide(a, b):
+    return a / b
+def mod(a, b):
+    return a % b
+def exponent(a, b):
+    return a ** b
+def subt(a,b):
+    return a - b
