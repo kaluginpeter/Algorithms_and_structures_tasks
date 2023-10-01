675990d2afb2562c2fc0dac672780d3c645a166e
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 12:41:53 +0300

    Added description "Sum of numbers from 0 to N"

---
+++ Python_Solutions/CodeWars/7kyu/Sum_of_numbers_from_0_to_N.py
@@ -0,0 +1,27 @@
+# Description:
+#
+# We want to generate a function that computes the series starting from 0 and ending until the given number.
+#
+# Example:
+# Input:
+#
+# > 6
+# Output:
+#
+# 0+1+2+3+4+5+6 = 21
+#
+# Input:
+#
+# > -15
+# Output:
+#
+# -15<0
+#
+# Input:
+#
+# > 0
+# Output:
+#
+# 0=0
+#
+# FUNDAMENTALS
