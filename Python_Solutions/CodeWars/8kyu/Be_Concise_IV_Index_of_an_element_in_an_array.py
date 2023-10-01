6ed57e6f4e58e5d494ea64ac499d5b4857046867
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 16:05:04 +0300

    Added solution "Be Concise IV - Index of an element in an array"

--- Python_Solutions/CodeWars/8kyu/Be_Concise_IV_Index_of_an_element_in_an_array.py
+++ Python_Solutions/CodeWars/8kyu/Be_Concise_IV_Index_of_an_element_in_an_array.py
@@ -3,3 +3,6 @@
 # Provided is a function Kata which accepts two parameters in the following order: array, element and returns the index of the element if found and "Not found" otherwise. Your task is to shorten the code as much as possible in order to meet the strict character count requirements of the Kata. (no more than 161) You may assume that all array elements are unique.
 #
 # REFACTORING
+# Solution
+def find(l, e):
+    return 'Not found' if e not in l else l.index(e)
