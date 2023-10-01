88bf8475690f408757962dbbb6c5cb8835e707b4
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 12:03:55 +0300

    Added description "Sum of Odd Cubed Numbers"

---
+++ Python_Solutions/CodeWars/7kyu/Sum_of_Odd_Cubed_Numbers.py
@@ -0,0 +1,5 @@
+# Find the sum of the odd numbers within an array, after cubing the initial integers. The function should return undefined/None/nil/NULL if any of the values aren't numbers.
+#
+# Note: Booleans should not be considered as numbers.
+#
+# FUNDAMENTALSFUNCTIONAL PROGRAMMINGARRAYS
