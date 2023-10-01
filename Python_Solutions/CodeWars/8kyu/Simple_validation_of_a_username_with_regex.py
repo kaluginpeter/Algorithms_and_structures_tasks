7dc59886d1ad494583c02d739ba63b30ac3cb400
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 19:36:12 +0300

    Added solution "Simple validation of a username with regex"

--- Python_Solutions/CodeWars/8kyu/Simple_validation_of_a_username_with_regex.py
+++ Python_Solutions/CodeWars/8kyu/Simple_validation_of_a_username_with_regex.py
@@ -6,3 +6,7 @@
 # Length should be between 4 and 16 characters (both included).
 #
 # REGULAR EXPRESSIONSFUNDAMENTALS
+# Solution
+import re
+def validate_usr(username):
+    return re.match('^[a-z0-9_]{4,16}$', username) != None
