1b6f46a9d858475bca413048f9dab28e9bc6c2bb
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 19:16:27 +0300

    Added solution "Remove String Spaces"

--- Python_Solutions/CodeWars/8kyu/Remove_String_Spaces.py
+++ Python_Solutions/CodeWars/8kyu/Remove_String_Spaces.py
@@ -7,3 +7,6 @@
 # "8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd" -> "88Bifk8hB8BB8BBBB888chl8BhBfd"
 # "8aaaaa dddd r     " -> "8aaaaaddddr"
 # FUNDAMENTALSSTRINGS
+# Solution
+def no_space(x):
+    return x.replace(' ', '')
