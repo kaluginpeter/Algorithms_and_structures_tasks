8d3c7cfedf4758a464a08cb483c2be3bf5066398
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 11:27:31 +0300

    Added solution "Sort and Transform"

--- Python_Solutions/CodeWars/7kyu/Sort_and_Transform.py
+++ Python_Solutions/CodeWars/7kyu/Sort_and_Transform.py
@@ -17,3 +17,11 @@
 # [66, 101, 55, 111, 113]                   -->  "Beoq-7Boq-qoB7-7Boq"
 # [99, 98, 97, 96, 81, 82, 82]              -->  "cbRR-QRbc-cbRQ-QRbc"
 # FUNDAMENTALSSTRINGSARRAYSSORTINGALGORITHMS
+# Solution
+def sor(arr): return ''.join(arr[:2]+arr[-2:])
+def sort_transform(arr):
+    arr = list(map(chr, arr))
+    w1  = sor(arr)
+    arr.sort()
+    w2  = sor(arr)
+    return f'{w1}-{w2}-{w2[::-1]}-{w2}'
