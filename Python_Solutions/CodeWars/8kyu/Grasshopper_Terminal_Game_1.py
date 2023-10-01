8384ba804017b4a0b684c72d0f96a6e23bacf512
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 18:13:04 +0300

    Added description "Grasshopper - Terminal Game #1"

---
+++ Python_Solutions/CodeWars/8kyu/Grasshopper_Terminal_Game_1.py
@@ -0,0 +1,10 @@
+# Terminal Game - Create Hero Prototype
+# In this first kata in the series, you need to define a Hero prototype to be used in a terminal game. The hero should have the following attributes:
+#
+# attribute	value
+# name	user argument or 'Hero'
+# position	'00'
+# health	100
+# damage	5
+# experience	0
+# FUNDAMENTALS
