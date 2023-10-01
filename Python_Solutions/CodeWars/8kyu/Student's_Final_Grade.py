77273dbfdbc9a4f9d43df6a7eded71f906c05d40
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 19:44:14 +0300

    Added solution "Student's Final Grade"

--- Python_Solutions/CodeWars/8kyu/Student's_Final_Grade.py
+++ Python_Solutions/CodeWars/8kyu/Student's_Final_Grade.py
@@ -23,3 +23,13 @@
 # *Use Comparison and Logical Operators.
 #
 # FUNDAMENTALS
+# Solution
+def final_grade(exam, projects):
+    if exam > 90 or projects > 10:
+        return 100
+    elif exam > 75 and projects >= 5:
+        return 90
+    elif exam > 50 and projects >= 2:
+        return 75
+    else:
+        return 0
