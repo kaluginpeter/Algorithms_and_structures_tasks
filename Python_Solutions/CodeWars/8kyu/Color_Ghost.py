514d5e6497ef25febd7a60776757f8110734c4bb
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 16:22:49 +0300

    Added description "Color Ghost"

---
+++ Python_Solutions/CodeWars/8kyu/Color_Ghost.py
@@ -0,0 +1,10 @@
+# Color Ghost
+# Create a class Ghost
+#
+# Ghost objects are instantiated without any arguments.
+#
+# Ghost objects are given a random color attribute of "white" or "yellow" or "purple" or "red" when instantiated
+#
+# ghost = Ghost()
+# ghost.color  #=> "white" or "yellow" or "purple" or "red"
+# OBJECT-ORIENTED PROGRAMMINGFUNDAMENTALS
