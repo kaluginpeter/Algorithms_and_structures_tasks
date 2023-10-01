85d1232194e144cbed990a58be7aee191bbfb9a9
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 11:42:00 +0300

    Added solution "Spraying trees"

--- Python_Solutions/CodeWars/7kyu/Spraying_trees.py
+++ Python_Solutions/CodeWars/7kyu/Spraying_trees.py
@@ -14,3 +14,7 @@
 #
 # task('Monday', 15, 2) -> 'It is Monday today, James, you have to work, you must spray 15 trees and you need 30 dollars to buy liquid'
 # FUNDAMENTALS
+# Solution
+def task(w,n,c):
+    workers = {"Monday" : "James", "Tuesday" : "John", "Wednesday" : "Robert", "Thursday" : "Michael", "Friday" : "William"}
+    return f"It is {w} today, {workers[w]}, you have to work, you must spray {n} trees and you need {n * c} dollars to buy liquid"
