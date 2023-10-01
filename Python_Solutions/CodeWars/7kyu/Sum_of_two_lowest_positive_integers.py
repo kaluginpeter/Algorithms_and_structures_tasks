fc1407d16dbe7182253b75637894fe2f870de8ec
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 12:46:00 +0300

    Added solution "Sum of two lowest positive integers"

--- Python_Solutions/CodeWars/7kyu/Sum_of_two_lowest_positive_integers.py
+++ Python_Solutions/CodeWars/7kyu/Sum_of_two_lowest_positive_integers.py
@@ -5,3 +5,9 @@
 # [10, 343445353, 3453445, 3453545353453] should return 3453455.
 #
 # ARRAYSFUNDAMENTALS
+# Solution
+def sum_two_smallest_numbers(numbers):
+    first = min(numbers)
+    second = sorted(numbers)[1]
+    all = first + second
+    return all
