20939c146ba5ca9d3d5eadb278cfd126a87e5df0
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 19:51:50 +0300

    Added solution "Switch it Up!"

--- Python_Solutions/CodeWars/8kyu/Switch_it_Up.py
+++ Python_Solutions/CodeWars/8kyu/Switch_it_Up.py
@@ -7,3 +7,18 @@
 # If your language supports it, try using a switch statement.
 #
 # FUNDAMENTALS
+# Solution
+def switch_it_up(number):
+    numbers = {
+        0: 'Zero',
+        1: 'One',
+        2: 'Two',
+        3: 'Three',
+        4: 'Four',
+        5: 'Five',
+        6: 'Six',
+        7: 'Seven',
+        8: 'Eight',
+        9: 'Nine',
+    }
+    return numbers[number]
