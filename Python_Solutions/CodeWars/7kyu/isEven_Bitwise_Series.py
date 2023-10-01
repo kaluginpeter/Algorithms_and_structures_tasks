0c418044d9fa011e40e13c43db6cb475886d7f9f
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 15:32:06 +0300

    Added description "isEven? - Bitwise Series"

---
+++ Python_Solutions/CodeWars/7kyu/isEven_Bitwise_Series.py
@@ -0,0 +1,13 @@
+# Is the number even?
+# If the numbers is even return true. If it's odd, return false.
+#
+#
+#
+#
+#
+# Oh yeah... the following symbols/commands have been disabled!
+#
+# use of %
+# use of .even? in Ruby
+# use of mod in Python
+# RESTRICTEDFUNDAMENTALS
