f19979be26ea99d4f62f2c492fe40a513acaa9cb
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 17:12:32 +0300

    Added solution "Filter out the geese"

--- Python_Solutions/CodeWars/8kyu/Filter_out_the_geese.py
+++ Python_Solutions/CodeWars/8kyu/Filter_out_the_geese.py
@@ -12,3 +12,7 @@
 # The elements in the returned array should be in the same order as in the initial array passed to your function, albeit with the 'geese' removed. Note that all of the strings will be in the same case as those provided, and some elements may be repeated.
 #
 # ARRAYSFUNDAMENTALS
+# Solution
+geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
+def goose_filter(birds):
+    return [elem for elem in birds if elem not in geese]
