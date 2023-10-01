84dd974c19f4e7443ba52f6b95f5648b3494064e
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 17:41:40 +0300

    Added description "Geometry Basics: Distance between points in 2D"

---
+++ Python_Solutions/CodeWars/8kyu/Geometry_Basics_Distance_between_points_in_2D.py
@@ -0,0 +1,9 @@
+# This series of katas will introduce you to basics of doing geometry with computers.
+#
+# Point objects have attributes x and y.
+#
+# Write a function calculating distance between Point a and Point b.
+#
+# Tests compare expected result and actual answer with tolerance of 1e-6.
+#
+# GEOMETRYFUNDAMENTALS
