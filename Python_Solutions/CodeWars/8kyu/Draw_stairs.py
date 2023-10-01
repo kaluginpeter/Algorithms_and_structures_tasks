8b29d4cc8f055ebea1463732f8a782686b005311
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 16:47:41 +0300

    Added description "Draw stairs"

---
+++ Python_Solutions/CodeWars/8kyu/Draw_stairs.py
@@ -0,0 +1,20 @@
+# Given a number n, draw stairs using the letter "I", n tall and n wide, with the tallest in the top left.
+#
+# For example n = 3 result in:
+#
+# "I\n I\n  I"
+# or printed:
+#
+# I
+#  I
+#   I
+# Another example, a 7-step stairs should be drawn like this:
+#
+# I
+#  I
+#   I
+#    I
+#     I
+#      I
+#       I
+# ASCII ARTALGORITHMS
