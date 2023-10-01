2c9727b2f2598e347434309775d51c7818b76966
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 18:57:33 +0300

    Added solution "OOP: Object Oriented Piracy"

--- Python_Solutions/CodeWars/8kyu/OOP_Object_Oriented_Piracy.py
+++ Python_Solutions/CodeWars/8kyu/OOP_Object_Oriented_Piracy.py
@@ -30,3 +30,10 @@
 # Good luck and may you find GOOOLD!
 #
 # OBJECT-ORIENTED PROGRAMMINGFUNDAMENTALS
+# Solution
+class Ship:
+    def __init__(self, draft, crew):
+        self.draft = draft
+        self.crew = crew
+    def is_worth_it(self):
+        return self.draft - self.crew * 1.5 > 20
