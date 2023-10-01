9bc1bda79a85ae094bddb606a04c385cf9bfca6b
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 10:50:15 +0300

    Added solution "Shortest Word"

--- Python_Solutions/CodeWars/7kyu/Shortest_Word.py
+++ Python_Solutions/CodeWars/7kyu/Shortest_Word.py
@@ -3,3 +3,7 @@
 # String will never be empty and you do not need to account for different data types.
 #
 # FUNDAMENTALS
+# Solution
+def find_short(s):
+    # your code here
+    return len(list(i for i in sorted(s.split(), key=len))[0])
