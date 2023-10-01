7aa92ad63f2f8b03e6beac78f21da15b873e01d8
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 15:44:02 +0300

    Added solution "101 Dalmatians - squash the bugs, not the dogs!"

--- Python_Solutions/CodeWars/8kyu/101_Dalmatians_squash_the_bugs_not_the_dogs.py
+++ Python_Solutions/CodeWars/8kyu/101_Dalmatians_squash_the_bugs_not_the_dogs.py
@@ -6,3 +6,14 @@
 #
 # Good luck!
 # DEBUGGINGFUNDAMENTALS
+# Solution
+def how_many_dalmatians(n):
+    dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIONS!!!"];
+    if (n <= 10):
+        return dogs[0];
+    if (n <= 50):
+        return dogs[1];
+    if (n == 101):
+        return dogs[3];
+    else:
+        return dogs[2];
