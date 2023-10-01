25008c5d24d3b3d4b0ee8f86a6de4e6c32b83c49
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 12:07:57 +0300

    Added description "Sum of all arguments"

---
+++ Python_Solutions/CodeWars/7kyu/Sum_of_all_arguments.py
@@ -0,0 +1,15 @@
+# Write a function that finds the sum of all its arguments.
+#
+# eg:
+#
+# sum_args(1, 2, 3) # => 6
+# sum_args(8, 2) # => 10
+# sum_args(1, 2, 3, 4, 5) # => 15
+# TIPS:
+# ruby/python : http://lmgtfy.com/?q=Ruby+splat+operator
+#
+# JS/Coffeescript : http://lmgtfy.com/?q=Javascript+arguments+variable
+#
+# C: https://www.geeksforgeeks.org/variadic-functions-in-c/
+#
+# FUNDAMENTALS
