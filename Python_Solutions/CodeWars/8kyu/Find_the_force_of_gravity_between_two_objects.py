299d3e8b96aec4956be90e4ae4c3c52dd6de14ca
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 17:22:18 +0300

    Added solution "Find the force of gravity between two objects"

--- Python_Solutions/CodeWars/8kyu/Find_the_force_of_gravity_between_two_objects.py
+++ Python_Solutions/CodeWars/8kyu/Find_the_force_of_gravity_between_two_objects.py
@@ -37,3 +37,8 @@
 # μ copy this from here to use it in your solution
 #
 # ALGORITHMS
+# Solution
+def solution(arr_val, arr_unit):
+    G = 6.67e-11 ;
+    conversion = { 'kg':1, 'g':1e-3, 'mg':1e-6, 'μg':1e-9, 'lb':.453592, 'm':1, 'cm':1e-2, 'mm':1e-3, 'μm':1e-6, 'ft':.3048}
+    return G*arr_val[0]*conversion[arr_unit[0]]*arr_val[1]*conversion[arr_unit[1]]/(arr_val[2]*conversion[arr_unit[2]])**2
