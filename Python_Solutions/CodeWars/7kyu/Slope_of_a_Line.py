7a435fbef17be29eae77c961332e6c7b9c07ae61
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 11:20:59 +0300

    Added solution "Slope of a Line"

--- Python_Solutions/CodeWars/7kyu/Slope_of_a_Line.py
+++ Python_Solutions/CodeWars/7kyu/Slope_of_a_Line.py
@@ -5,3 +5,6 @@
 # Each point that the function takes in is an array 2 elements long. The first number is the x coordinate and the second number is the y coordinate. If the line through the two points is vertical or if the same point is given twice, the function should return null/None.
 #
 # GRAPHSMATHEMATICSGEOMETRYALGEBRAFUNDAMENTALS
+# Solution
+def getSlope(p1, p2):
+    return None if p1[0] == p2[0] else (p2[1] - p1[1]) / (p2[0] - p1[0])
