8d53418ed8b75a3fc50ed8d187ef58e8ea2bc10a
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 18:21:43 +0300

    Added solution "How do I compare numbers?"

--- Python_Solutions/CodeWars/8kyu/How_do_I_compare_numbers.py
+++ Python_Solutions/CodeWars/8kyu/How_do_I_compare_numbers.py
@@ -1,3 +1,11 @@
 # What could be easier than comparing integer numbers? However, the given piece of code doesn't recognize some of the special numbers for a reason to be found. Your task is to find the bug and eliminate it.
 #
 # FUNDAMENTALSDEBUGGING
+# Solution
+def what_is(x):
+    if x == 42:
+        return 'everything'
+    elif x == 42 * 42:
+        return 'everything squared'
+    else:
+        return 'nothing'
