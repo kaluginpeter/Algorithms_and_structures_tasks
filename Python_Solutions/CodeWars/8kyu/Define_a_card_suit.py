8fd3a3062fed34ae6fc1371c63355a3fbec08e38
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 16:42:03 +0300

    Added solution "Define a card suit"

--- Python_Solutions/CodeWars/8kyu/Define_a_card_suit.py
+++ Python_Solutions/CodeWars/8kyu/Define_a_card_suit.py
@@ -11,3 +11,6 @@
 # ('3H') -> return 'hearts'
 # ('3S') -> return 'spades'
 # FUNDAMENTALSSTRINGS
+# Solution
+def define_suit(card):
+    return 'clubs' if 'C' in card else 'spades' if 'S' in card else 'diamonds' if 'D' in card else 'hearts' if 'H' in card else None
