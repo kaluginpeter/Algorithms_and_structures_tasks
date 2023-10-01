97a032f366367aeb086f55cfa89ee56766344ed0
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 20:24:07 +0300

    Added solution "Will there be enough space?"

--- Python_Solutions/CodeWars/8kyu/Will_there_be_enough_space.py
+++ Python_Solutions/CodeWars/8kyu/Will_there_be_enough_space.py
@@ -13,3 +13,9 @@
 # cap = 10, on = 5, wait = 5 --> 0 # He can fit all 5 passengers
 # cap = 100, on = 60, wait = 50 --> 10 # He can't fit 10 of the 50 waiting
 # FUNDAMENTALS
+# Solution
+def enough(cap, on, wait):
+    if on + wait < cap:
+        return 0
+    else:
+        return abs(cap - (on + wait))
