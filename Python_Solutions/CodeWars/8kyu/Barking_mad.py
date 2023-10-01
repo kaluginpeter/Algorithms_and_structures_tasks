b5c577c059ef08c99a0ed7b3aaa8f209f378129f
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 15:59:54 +0300

    Added solution "Barking mad"

--- Python_Solutions/CodeWars/8kyu/Barking_mad.py
+++ Python_Solutions/CodeWars/8kyu/Barking_mad.py
@@ -5,3 +5,13 @@
 # Use method prototypes to enable all Dogs to bark.
 #
 # FUNDAMENTALSOBJECT-ORIENTED PROGRAMMING
+# Solution
+class Dog():
+    def __init__(self, breed):
+        self.breed = breed
+
+
+snoopy = Dog("Beagle")
+snoopy.bark = lambda: "Woof"
+scoobydoo = Dog("Great Dane")
+scoobydoo.bark = lambda: 'Woof'
