6699544a09e5ad76de390d9602fe9972d04691e9
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 19:50:28 +0300

    Added solution "Surface Area and Volume of a Box"

--- Python_Solutions/CodeWars/8kyu/Surface_Area_and_Volume_of_a_Box.py
+++ Python_Solutions/CodeWars/8kyu/Surface_Area_and_Volume_of_a_Box.py
@@ -1,3 +1,6 @@
 # Write a function that returns the total surface area and volume of a box as an array: [area, volume]
 #
 # GEOMETRYFUNDAMENTALS
+# Solution
+def get_size(w,h,d):
+    return [((w * h) + (w * d) + (h * d)) * 2, w * h * d]
