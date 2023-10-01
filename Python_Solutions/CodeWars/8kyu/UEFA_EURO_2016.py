3ea16b968ee3870608736817f830176d383c855b
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 20:12:14 +0300

    Added description "UEFA EURO 2016"

---
+++ Python_Solutions/CodeWars/8kyu/UEFA_EURO_2016.py
@@ -0,0 +1,6 @@
+# Finish the uefaEuro2016() function so it return string just like in the examples below:
+#
+# uefa_euro_2016(['Germany', 'Ukraine'],[2, 0]) # "At match Germany - Ukraine, Germany won!"
+# uefa_euro_2016(['Belgium', 'Italy'],[0, 2]) # "At match Belgium - Italy, Italy won!"
+# uefa_euro_2016(['Portugal', 'Iceland'],[1, 1]) # "At match Portugal - Iceland, teams played draw."
+# STRINGSARRAYSFUNDAMENTALS
