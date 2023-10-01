e41b7f5a52318f103c1fd7698cc08bd502ddaca0
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 16:52:20 +0300

    Added description "Enumerable Magic #25 - Take the First N Elements"

---
+++ Python_Solutions/CodeWars/8kyu/Enumerable_Magic_25_Take_the_First_N_Elements.py
@@ -0,0 +1,7 @@
+# Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
+#
+# If you need help, here's a reference:
+#
+# https://docs.python.org/3/library/stdtypes.html#sequence-types-list-tuple-range
+#
+# FUNDAMENTALS
