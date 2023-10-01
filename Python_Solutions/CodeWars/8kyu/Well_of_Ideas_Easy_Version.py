519e30d4d3c2f178b0306a3da4eee7d049453967
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 20:19:49 +0300

    Added solution "Well of Ideas - Easy Version"

--- Python_Solutions/CodeWars/8kyu/Well_of_Ideas_Easy_Version.py
+++ Python_Solutions/CodeWars/8kyu/Well_of_Ideas_Easy_Version.py
@@ -3,3 +3,7 @@
 # In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
 #
 # FUNDAMENTALSARRAYSSTRINGSREFACTORING
+# Solution
+def well(x):
+    i = x.count('good')
+    return 'Publish!' if 1 <= i <= 2 else 'I smell a series!' if i> 2 else 'Fail!'
