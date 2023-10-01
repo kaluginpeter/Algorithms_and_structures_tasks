0222ac0c605cd7242e4ef47e6366a40647feb6ad
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 18:07:51 +0300

    Added solution "Grasshopper - Function syntax debugging"

--- Python_Solutions/CodeWars/8kyu/Grasshopper_Function_syntax_debugging.py
+++ Python_Solutions/CodeWars/8kyu/Grasshopper_Function_syntax_debugging.py
@@ -2,3 +2,6 @@
 # A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
 #
 # FUNDAMENTALS
+# Solution
+def main (verb, noun):
+    return verb + noun
