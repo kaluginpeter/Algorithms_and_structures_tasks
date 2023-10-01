9a48c2a09419ad071c26e15d7e12540d5b653d6d
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 17:24:34 +0300

    Added solution "Finish Guess the Number Game"

--- Python_Solutions/CodeWars/8kyu/Finish_Guess_the_Number_Game.py
+++ Python_Solutions/CodeWars/8kyu/Finish_Guess_the_Number_Game.py
@@ -6,3 +6,16 @@
 # Can you finish the game so all the rules are met?
 #
 # FUNDAMENTALSOBJECT-ORIENTED PROGRAMMING
+# Solution
+class Guesser:
+    def __init__(self, number, lives):
+        self.number = number
+        self.lives = lives
+
+    def guess(self, n):
+        if self.lives < 1:
+            raise 'Too many guesses!'
+        if self.number == n:
+            return True
+        self.lives -= 1
+        return False
