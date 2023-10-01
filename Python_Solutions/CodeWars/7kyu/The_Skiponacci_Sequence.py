c827945b0967a2548841e2b7e5200674c97d54d2
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 14:34:28 +0300

    Added description "The Skiponacci Sequence"

---
+++ Python_Solutions/CodeWars/7kyu/The_Skiponacci_Sequence.py
@@ -0,0 +1,9 @@
+# Your task is to generate the Fibonacci sequence to n places, with each alternating value as "skip". For example:
+#
+# "1 skip 2 skip 5 skip 13 skip 34"
+#
+# Return the result as a string
+#
+# You can presume that n is always a positive integer between (and including) 1 and 64.
+#
+# PUZZLESALGORITHMS
