4f546dc8a063fb98128aa08a13f3cc1dd7a3d087
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 14:20:46 +0300

    Added solution "Target Date"

--- Python_Solutions/CodeWars/7kyu/Target_Date.py
+++ Python_Solutions/CodeWars/7kyu/Target_Date.py
@@ -14,3 +14,9 @@
 # Don't forget to take the rate for a day to be p divided by 36000 since banks consider that there are 360 days in a year.
 # You have: a0 > 0, p% > 0, a >= a0
 # FUNDAMENTALSMATHEMATICS
+# Solution
+from math import ceil, log
+from datetime import date, timedelta as td
+def date_nb_days(a0, a, p):
+    dur = log(a, 1 + p / 36000.0) - log(a0, 1 + p / 36000.0)
+    return str(date(2016, 1, 1) + td(ceil(dur)))
