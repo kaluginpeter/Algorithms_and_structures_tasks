7049eea3d22df391ae924ac2f7ac27f90a314b5c
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 15:26:51 +0300

    Added description "Zebulan's Nightmare"

---
+++ Python_Solutions/CodeWars/7kyu/Zebulan's_Nightmare.py
@@ -0,0 +1,10 @@
+# Zebulan has worked hard to write all his python code in strict compliance to PEP8 rules. In this kata, you are a mischievous hacker that has set out to sabotage all his good code.
+#
+# Your job is to take PEP8 compatible function names and convert them to camelCase. For example:
+#
+# zebulansNightmare('camel_case') == 'camelCase'
+# zebulansNightmare('zebulans_nightmare') == 'zebulansNightmare'
+# zebulansNightmare('get_string') == 'getString'
+# zebulansNightmare('convert_to_uppercase') == 'convertToUppercase'
+# zebulansNightmare('main') == 'main'
+# FUNDAMENTALS
