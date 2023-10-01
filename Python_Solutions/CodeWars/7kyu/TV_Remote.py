21e2f2084f5afbff64daf3673a9ba57f682a7c4a
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 14:19:20 +0300

    Added solution "TV Remote"

--- Python_Solutions/CodeWars/7kyu/TV_Remote.py
+++ Python_Solutions/CodeWars/7kyu/TV_Remote.py
@@ -41,3 +41,13 @@
 # TV Remote (wrap)
 # TV Remote (symbols)
 # ALGORITHMS
+# Solution
+def tv_remote(word):
+    pp = 0
+    res = 0
+    r = 'abcde123fghij456klmno789pqrst.@0uvwxyz_/'
+    for i in word:
+        p = r.find(i)
+        res += abs(p//8-pp//8) + abs(p%8-pp%8) + 1
+        pp = p
+    return res
