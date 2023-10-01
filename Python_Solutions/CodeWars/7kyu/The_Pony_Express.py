2cc557c8290c6008b5b91eaa1898f733e23143bd
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 14:34:01 +0300

    Added solution "The Pony Express"

--- Python_Solutions/CodeWars/7kyu/The_Pony_Express.py
+++ Python_Solutions/CodeWars/7kyu/The_Pony_Express.py
@@ -24,3 +24,13 @@
 # The Pony Express
 # The Pony Express (missing rider)
 # FUNDAMENTALS
+# Solution
+def riders(stations):
+	riders, travelled = 1, 0
+	for miles in stations:
+		if travelled + miles > 100:
+			riders += 1
+			travelled = miles
+		else:
+			travelled += miles
+	return riders
