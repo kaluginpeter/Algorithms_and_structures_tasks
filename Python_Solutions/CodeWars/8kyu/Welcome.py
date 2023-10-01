cc25953345241b281b26dd237702ce116ec2188b
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 20:18:59 +0300

    Added solution "Welcome!"

--- Python_Solutions/CodeWars/8kyu/Welcome.py
+++ Python_Solutions/CodeWars/8kyu/Welcome.py
@@ -28,3 +28,26 @@
 # IP_ADDRESS_NOT_FOUND - ip address not in the database
 # IP_ADDRESS_REQUIRED - no ip address was supplied
 # FUNDAMENTALS
+# Soluiton
+def greet(language):
+    base = {'english': 'Welcome',
+    'czech': 'Vitejte',
+    'danish': 'Velkomst',
+    'dutch': 'Welkom',
+    'estonian': 'Tere tulemast',
+    'finnish': 'Tervetuloa',
+    'flemish': 'Welgekomen',
+    'french': 'Bienvenue',
+    'german': 'Willkommen',
+    'irish': 'Failte',
+    'italian': 'Benvenuto',
+    'latvian': 'Gaidits',
+    'lithuanian': 'Laukiamas',
+    'polish': 'Witamy',
+    'spanish': 'Bienvenido',
+    'swedish': 'Valkommen',
+    'welsh': 'Croeso'}
+    if language in base:
+        return base[language]
+    else:
+        return 'Welcome'
