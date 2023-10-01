5e43243a2c5338ff5fc15636a41863a68acfed3d
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 18:13:17 +0300

    Added solution "Grasshopper - Terminal Game #1"

--- Python_Solutions/CodeWars/8kyu/Grasshopper_Terminal_Game_1.py
+++ Python_Solutions/CodeWars/8kyu/Grasshopper_Terminal_Game_1.py
@@ -8,3 +8,11 @@
 # damage	5
 # experience	0
 # FUNDAMENTALS
+# Solution
+class Hero(object):
+    def __init__(self, name='Hero', position='00', health=100, damage=5, experience=0):
+        self.name = name
+        self.position = position
+        self.health = health
+        self.damage = damage
+        self.experience = experience
