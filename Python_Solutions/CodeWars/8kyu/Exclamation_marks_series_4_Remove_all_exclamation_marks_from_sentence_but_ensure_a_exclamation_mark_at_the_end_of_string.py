7c4e50c5954ebaaae5afd943d70b3ad801759557
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 17:00:11 +0300

    Added description "Exclamation marks series #4: Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string"

---
+++ Python_Solutions/CodeWars/8kyu/Exclamation_marks_series_4_Remove_all_exclamation_marks_from_sentence_but_ensure_a_exclamation_mark_at_the_end_of_string.py
@@ -0,0 +1,11 @@
+# Description:
+# Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string. For a beginner kata, you can assume that the input data is always a non empty string, no need to verify it.
+#
+# Examples
+# "Hi!"     ---> "Hi!"
+# "Hi!!!"   ---> "Hi!"
+# "!Hi"     ---> "Hi!"
+# "!Hi!"    ---> "Hi!"
+# "Hi! Hi!" ---> "Hi Hi!"
+# "Hi"      ---> "Hi!"
+# FUNDAMENTALS
