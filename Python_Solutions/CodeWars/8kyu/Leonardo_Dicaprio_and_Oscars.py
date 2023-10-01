2a8ff0e42f94446b541e2a671e7cee7151e56349
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 18:43:35 +0300

    Added solution "Leonardo Dicaprio and Oscars"

--- Python_Solutions/CodeWars/8kyu/Leonardo_Dicaprio_and_Oscars.py
+++ Python_Solutions/CodeWars/8kyu/Leonardo_Dicaprio_and_Oscars.py
@@ -8,3 +8,9 @@
 # if it was over 88 you should return "Leo got one already!"
 #
 # FUNDAMENTALS
+# Solution
+def leo(oscar):
+    if oscar == 88: return 'Leo finally won the oscar! Leo is happy'
+    elif oscar == 86: return 'Not even for Wolf of wallstreet?!'
+    elif oscar != 88 and oscar != 86 and oscar < 88: return 'When will you give Leo an Oscar?'
+    elif oscar > 88: return 'Leo got one already!'
