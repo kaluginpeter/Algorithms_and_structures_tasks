b8026ee1f88201cd1002aa784d6b0501eb8f4257
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 15:25:42 +0300

    Added solution "You Got Change?"

--- Python_Solutions/CodeWars/7kyu/You_Got_Change.py
+++ Python_Solutions/CodeWars/7kyu/You_Got_Change.py
@@ -19,3 +19,6 @@
 #
 # 217 => [2,1,1,0,0,2]
 # FUNDAMENTALS
+# Solution
+def give_change(amount):
+    return (amount % 5, (amount%10) // 5, ((amount % 50) % 20) // 10, (amount % 50) // 20, (amount % 100) // 50, amount // 100)
