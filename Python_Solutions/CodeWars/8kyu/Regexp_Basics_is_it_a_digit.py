b176d3f6688a2616e1dffe816a32d7668ee0c14b
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 19:13:24 +0300

    Added solution "Regexp Basics - is it a digit?"

--- Python_Solutions/CodeWars/8kyu/Regexp_Basics_is_it_a_digit.py
+++ Python_Solutions/CodeWars/8kyu/Regexp_Basics_is_it_a_digit.py
@@ -1,3 +1,6 @@
 # Implement String#digit? (in Java StringUtils.isDigit(String)), which should return true if given object is a digit (0-9), false otherwise.
 #
 # REGULAR EXPRESSIONSFUNDAMENTALS
+# Solution
+def is_digit(n):
+    return n in '0123456789' and n != ''
