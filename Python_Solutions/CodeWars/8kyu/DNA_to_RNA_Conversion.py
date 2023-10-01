bf5f53a2b4bdc8a106eb522e5f5ac911da86e533
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 16:38:12 +0300

    Added solution "DNA to RNA Conversion"

--- Python_Solutions/CodeWars/8kyu/DNA_to_RNA_Conversion.py
+++ Python_Solutions/CodeWars/8kyu/DNA_to_RNA_Conversion.py
@@ -10,3 +10,7 @@
 # The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
 #
 # FUNDAMENTALSSTRINGS
+# Solution
+def dna_to_rna(dna):
+    dict = {'G': 'G', 'C': 'C', 'A': 'A', 'T': 'U'}
+    return ''.join(list(dict[char] for char in dna))
