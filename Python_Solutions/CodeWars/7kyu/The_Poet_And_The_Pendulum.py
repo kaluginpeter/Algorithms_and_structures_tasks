f5c0092a4e2311894bf623a6d40fba7c383e6deb
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 14:33:10 +0300

    Added solution "The Poet And The Pendulum"

--- Python_Solutions/CodeWars/7kyu/The_Poet_And_The_Pendulum.py
+++ Python_Solutions/CodeWars/7kyu/The_Poet_And_The_Pendulum.py
@@ -59,3 +59,10 @@
 # Enjoy Learning !!
 # Zizou
 # FUNDAMENTALSARRAYSALGORITHMSPERFORMANCE
+# Solution
+def pendulum(values):
+    sorted_values = sorted(values)
+    mid = [sorted_values [0]]
+    right = sorted_values[1::2]
+    left = sorted_values[2::2]
+    return left[::-1] + mid + right
