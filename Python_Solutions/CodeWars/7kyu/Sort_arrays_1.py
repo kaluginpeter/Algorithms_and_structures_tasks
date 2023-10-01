bc22d81952fe8657d5e7dded71c80802d3c77f2f
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 11:28:54 +0300

    Added solution "Sort arrays - 1"

--- Python_Solutions/CodeWars/7kyu/Sort_arrays_1.py
+++ Python_Solutions/CodeWars/7kyu/Sort_arrays_1.py
@@ -1,3 +1,6 @@
 # Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
 #
 # SORTINGARRAYSFUNDAMENTALS
+# Solution
+def sortme(names):
+    return sorted(names)
