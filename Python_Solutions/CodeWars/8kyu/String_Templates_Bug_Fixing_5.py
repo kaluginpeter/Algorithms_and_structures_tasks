833cd73c7e11279ef9fd487e1ba05bf6d3135a30
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 19:41:11 +0300

    Added description "String Templates - Bug Fixing #5"

---
+++ Python_Solutions/CodeWars/8kyu/String_Templates_Bug_Fixing_5.py
@@ -0,0 +1,4 @@
+# Oh no! Timmy hasn't followed instructions very carefully and forgot how to use the new String Template feature, Help Timmy with his string template so it works as he expects!
+#
+# ◄PREVIOUS KATANEXT KATA►
+# STRINGSDEBUGGING
