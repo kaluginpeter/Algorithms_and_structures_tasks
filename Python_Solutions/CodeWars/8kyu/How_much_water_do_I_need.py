137763284e0fd90b6c9069ae6e414d30a8c509c0
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 18:24:57 +0300

    Added solution "How much water do I need?"

--- Python_Solutions/CodeWars/8kyu/How_much_water_do_I_need.py
+++ Python_Solutions/CodeWars/8kyu/How_much_water_do_I_need.py
@@ -9,3 +9,10 @@
 # The answer should be rounded to the nearest 2 decimal places.
 #
 # MATHEMATICSFUNDAMENTALS
+# Solution
+def how_much_water(L, X, N):
+    if N > 2 * X:
+        return 'Too much clothes'
+    if N < X:
+        return 'Not enough clothes'
+    return round(1.1 ** (N-X) * L, 2)
