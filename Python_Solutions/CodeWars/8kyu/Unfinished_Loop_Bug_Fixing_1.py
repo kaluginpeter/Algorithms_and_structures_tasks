eba5d368e0009e77e959784f008e365ab5c2af39
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 20:14:38 +0300

    Added solution "Unfinished Loop - Bug Fixing #1"

--- Python_Solutions/CodeWars/8kyu/Unfinished_Loop_Bug_Fixing_1.py
+++ Python_Solutions/CodeWars/8kyu/Unfinished_Loop_Bug_Fixing_1.py
@@ -2,3 +2,6 @@
 # Oh no, Timmy's created an infinite loop! Help Timmy find and fix the bug in his unfinished for loop!
 #
 # DEBUGGING
+# Solution
+def create_array(n):
+    return list(range(1,n+1))
