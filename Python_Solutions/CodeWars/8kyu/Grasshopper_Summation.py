1ff4f0b82b6584d56830bc39ecd32c13d64e0cb3
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 18:12:35 +0300

    Added solution "Grasshopper - Summation"

--- Python_Solutions/CodeWars/8kyu/Grasshopper_Summation.py
+++ Python_Solutions/CodeWars/8kyu/Grasshopper_Summation.py
@@ -6,3 +6,9 @@
 # 2 -> 3 (1 + 2)
 # 8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
 # FUNDAMENTALSMATHEMATICS
+# Solution
+def summation(num):
+    count = 0
+    for i in range(num+1):
+        count += i
+    return count
