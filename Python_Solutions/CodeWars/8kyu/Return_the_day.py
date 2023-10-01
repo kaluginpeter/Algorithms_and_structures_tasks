e55e211cdb2adaeede26270d603855e4146bb8ac
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 19:21:40 +0300

    Added solution "Return the day"

--- Python_Solutions/CodeWars/8kyu/Return_the_day.py
+++ Python_Solutions/CodeWars/8kyu/Return_the_day.py
@@ -9,3 +9,8 @@
 # 7 returns "Saturday"
 # Otherwise returns "Wrong, please enter a number between 1 and 7"
 # FUNDAMENTALS
+# Solution
+def whatday(num):
+    dict = {1: 'Sunday', 2: 'Monday', 3: 'Tuesday',
+            4: 'Wednesday', 5: 'Thursday', 6: 'Friday', 7: 'Saturday'}
+    return dict[num] if num in dict else 'Wrong, please enter a number between 1 and 7'
