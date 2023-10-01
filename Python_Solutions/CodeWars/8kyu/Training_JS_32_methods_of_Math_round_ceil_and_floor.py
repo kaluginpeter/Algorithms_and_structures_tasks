21ad11b7911bd4adfe2ca98b9180d67f0947b473
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 20:08:40 +0300

    Added solution "Training JS #32: methods of Math---round() ceil() and floor()"

--- Python_Solutions/CodeWars/8kyu/Training_JS_32_methods_of_Math_round_ceil_and_floor.py
+++ Python_Solutions/CodeWars/8kyu/Training_JS_32_methods_of_Math_round_ceil_and_floor.py
@@ -99,3 +99,15 @@
 # #41: Regular Expression--""
 # #42: Regular Expression--(?:), (?=) and (?!)
 # FUNDAMENTALSTUTORIALS
+# Solution
+import math
+def round_it(n):
+    value = str(n)
+    find_decimal_position = value.find('.')
+    decimal_values = value[(find_decimal_position+1):]
+    if(len(decimal_values)>len(value[0:find_decimal_position])):
+        return math.ceil(n)
+    elif(len(decimal_values)<len(value[0:find_decimal_position])):
+        return math.floor(n)
+    elif(len(decimal_values)==len(value[0:find_decimal_position])):
+        return round(n,0)
