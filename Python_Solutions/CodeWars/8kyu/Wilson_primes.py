15dc751d5edbdbb26c02c20d33887b9230525eaf
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 20:25:33 +0300

    Added solution "Wilson primes"

--- Python_Solutions/CodeWars/8kyu/Wilson_primes.py
+++ Python_Solutions/CodeWars/8kyu/Wilson_primes.py
@@ -8,3 +8,8 @@
 # Your task is to create a function that returns true if the given number is a Wilson prime.
 #
 # MATHEMATICSFUNDAMENTALS
+# Solution
+def am_i_wilson(n):
+    list = [5, 13, 563, 5971, 558771, 1964215, 8121909,
+            12326713, 23025711, 26921605, 341569806, 399292158]
+    return n in list
