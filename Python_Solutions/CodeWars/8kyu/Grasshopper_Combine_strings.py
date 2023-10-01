da11dc559b6cfc789578a87b073c3cb5fb24aee4
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 18:04:30 +0300

    Added description "Grasshopper - Combine strings"

---
+++ Python_Solutions/CodeWars/8kyu/Grasshopper_Combine_strings.py
@@ -0,0 +1,10 @@
+# Combine strings function
+# Create a function named (combine_names) that accepts two parameters (first and last name). The function should return the full name.
+#
+# Example:
+#
+# combine_names('James', 'Stevens')
+# returns:
+#
+# 'James Stevens'
+# FUNDAMENTALS
