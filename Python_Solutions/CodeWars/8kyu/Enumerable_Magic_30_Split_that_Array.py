bd72ea922415a1f8110cf5276e47e463465ea947
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 16:53:12 +0300

    Added description "Enumerable Magic #30 - Split that Array!"

---
+++ Python_Solutions/CodeWars/8kyu/Enumerable_Magic_30_Split_that_Array.py
@@ -0,0 +1,17 @@
+# Create a method partition that accepts a list and a method/block. It should return two arrays: the first, with all the elements for which the given block returned true, and the second for the remaining elements.
+#
+# Here's a simple Ruby example:
+#
+# animals = ["cat", "dog", "duck", "cow", "donkey"]
+# partition(animals){|animal| animal.size == 3}
+#     #=> [["cat", "dog", "cow"], ["duck", "donkey"]]
+# The equivalent in Python would be:
+#
+# animals = ['cat', 'dog', 'duck', 'cow', 'donkey']
+# partition(animals, lambda x: len(x) == 3)
+#     # (['cat', 'dog', 'cow'], ['duck', 'donkey'])
+# If you need help, here's a reference:
+#
+# http://www.rubycuts.com/enum-partition
+#
+# FUNDAMENTALS
