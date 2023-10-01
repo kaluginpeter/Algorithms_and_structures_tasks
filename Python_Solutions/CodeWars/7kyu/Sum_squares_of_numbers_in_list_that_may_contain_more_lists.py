fd156283d0ccc15f43b3b4a6b215c579923541dc
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 12:46:38 +0300

    Added description "Sum squares of numbers in list that may contain more lists"

---
+++ Python_Solutions/CodeWars/7kyu/Sum_squares_of_numbers_in_list_that_may_contain_more_lists.py
@@ -0,0 +1,19 @@
+# Write a function that sums squares of numbers in list that may contain more lists
+# Example:
+#
+# var l = [1,2,3]
+# SumSquares(l) == 14
+#
+# var l = [[1,2],3]
+# SumSquares(l) == 14
+#
+# var l = [[[[[[[[[1]]]]]]]]]
+# SumSquares(l) == 1
+#
+# var l = [10,[[10],10],[10]]
+# SumSquares(l) == 400
+# Note: your solution must NOT modify the original list
+#
+# Another Kata involving nested lists here (it's still in beta - more honor points for completing :D): https://www.codewars.com/kata/5786f020e55533ebb7000153
+#
+# FUNDAMENTALSRECURSION
