a44047bd1b2d9fa9a6a02c5112751034c9636187
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 18:46:54 +0300

    Added description "Merging sorted integer arrays (without duplicates)"

---
+++ Python_Solutions/CodeWars/8kyu/Merging_sorted_integer_arrays_without_duplicates.py
@@ -0,0 +1,3 @@
+# Write a function that merges two sorted arrays into a single one. The arrays only contain integers. Also, the final outcome must be sorted and not have any duplicate.
+#
+# ARRAYSFUNDAMENTALS
