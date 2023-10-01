f53ce72f1630f1be8de0a14a0ef6f6d2c94141e1
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 18:34:52 +0300

    Added solution "Is the string uppercase?"

--- Python_Solutions/CodeWars/8kyu/Is_the_string_uppercase.py
+++ Python_Solutions/CodeWars/8kyu/Is_the_string_uppercase.py
@@ -12,3 +12,12 @@
 # In this Kata, a string is said to be in ALL CAPS whenever it does not contain any lowercase letter so any string containing no letters at all is trivially considered to be in ALL CAPS.
 #
 # FUNDAMENTALSSTRINGS
+# Solution
+def is_uppercase(inp):
+    special = ['$%&', '+%@']
+    if inp.isupper():
+        return True
+    elif inp in special:
+        return True
+    else:
+        return False
