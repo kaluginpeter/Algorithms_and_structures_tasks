9c6dbb50598a43cebc9309189dfd25461d7401eb
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 20:11:45 +0300

    Added solution "Twice as old"

--- Python_Solutions/CodeWars/8kyu/Twice_as_old.py
+++ Python_Solutions/CodeWars/8kyu/Twice_as_old.py
@@ -5,3 +5,7 @@
 # Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old). The answer is always greater or equal to 0, no matter if it was in the past or it is in the future.
 #
 # FUNDAMENTALSMATHEMATICS
+# Solution
+def twice_as_old(dad_years_old, son_years_old):
+    ages = son_years_old * 2
+    return abs(ages - dad_years_old)
