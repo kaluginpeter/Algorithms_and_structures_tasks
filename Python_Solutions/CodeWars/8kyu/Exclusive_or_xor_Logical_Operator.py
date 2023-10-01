ef703e8ebc6057672ca202acbb09858a23bf4c1f
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 17:02:19 +0300

    Added solution "Exclusive "or" (xor) Logical Operator"

--- Python_Solutions/CodeWars/8kyu/Exclusive_or_xor_Logical_Operator.py
+++ Python_Solutions/CodeWars/8kyu/Exclusive_or_xor_Logical_Operator.py
@@ -10,3 +10,6 @@
 # Since we cannot define keywords in Javascript (well, at least I don't know how to do it), your task is to define a function xor(a, b) where a and b are the two expressions to be evaluated. Your xor function should have the behaviour described above, returning true if exactly one of the two expressions evaluate to true, false otherwise.
 #
 # FUNDAMENTALS
+# Solution
+def xor(a,b):
+    return a != b
