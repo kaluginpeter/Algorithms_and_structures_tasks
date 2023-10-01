bedc8f0ae4a855fe44ba273a94ac2267d8fc9d07
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 10:50:51 +0300

    Added description "Show multiples of 2 numbers within a range"

---
+++ Python_Solutions/CodeWars/7kyu/Show_multiples_of_2_numbers_within_a_range.py
@@ -0,0 +1,9 @@
+# Print all numbers up to 3rd parameter which are multiple of both 1st and 2nd parameter.
+#
+# Python, Javascript, Java, Ruby versions: return results in a list/array
+#
+# NOTICE:
+#
+# Do NOT worry about checking zeros or negative values.
+# To find out if 3rd parameter (the upper limit) is inclusive or not, check the tests, it differs in each translation
+# FUNDAMENTALS
