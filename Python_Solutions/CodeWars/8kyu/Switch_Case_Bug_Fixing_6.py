6643b5699ac4c2404187eb80ddc1f27747aa3827
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 19:52:36 +0300

    Added solution "Switch/Case - Bug Fixing #6"

--- Python_Solutions/CodeWars/8kyu/Switch_Case_Bug_Fixing_6.py
+++ Python_Solutions/CodeWars/8kyu/Switch_Case_Bug_Fixing_6.py
@@ -2,3 +2,20 @@
 # Oh no! Timmy's evalObject function doesn't work. He uses Switch/Cases to evaluate the given properties of an object, can you fix timmy's function?
 #
 # DEBUGGING
+# Solution
+def eval_object(v):
+    case = v["operation"]
+    if case == "+":
+        return v["a"] + v["b"]
+    elif case == "-":
+        return v["a"] - v["b"]
+    elif case == "/":
+        return v["a"] / v["b"]
+    elif case == "*":
+        return v["a"] * v["b"]
+    elif case == "%":
+        return v["a"] % v["b"]
+    elif case == "**":
+        return v["a"] ** v["b"]
+    else:
+        return 1
