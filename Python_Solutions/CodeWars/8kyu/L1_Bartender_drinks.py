8bbf512ef269f6087e251a02e3ac6c3e775154c5
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 18:41:19 +0300

    Added solution "L1: Bartender, drinks!"

--- Python_Solutions/CodeWars/8kyu/L1_Bartender_drinks.py
+++ Python_Solutions/CodeWars/8kyu/L1_Bartender_drinks.py
@@ -13,3 +13,12 @@
 # Make sure you cover the cases where certain words do not show up with correct capitalization. For example, the input "pOLitiCIaN" should still return "Your tax dollars".
 #
 # STRINGSFUNDAMENTALS
+# Solution
+def get_drink_by_profession(param):
+    drinks = {"Jabroni": "Patron Tequila",
+                "School Counselor": "Anything with Alcohol",
+                "Programmer": "Hipster Craft Beer",
+                "Bike Gang Member": "Moonshine",
+                "Politician": "Your tax dollars",
+                "Rapper": "Cristal"}
+    return drinks[param.title()] if param.title() in drinks else "Beer"
