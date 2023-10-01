eb347a153fd7c93154583760123d28fe77c1c6fc
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 12:05:33 +0300

    Added description "Sum of a Beach"

---
+++ Python_Solutions/CodeWars/7kyu/Sum_of_a_Beach.py
@@ -0,0 +1,8 @@
+# Beaches are filled with sand, water, fish, and sun. Given a string, calculate how many times the words "Sand", "Water", "Fish", and "Sun" appear without overlapping (regardless of the case).
+#
+# Examples
+# sum_of_a_beach("WAtErSlIde")                    ==>  1
+# sum_of_a_beach("GolDeNSanDyWateRyBeaChSuNN")    ==>  3
+# sum_of_a_beach("gOfIshsunesunFiSh")             ==>  4
+# sum_of_a_beach("cItYTowNcARShoW")               ==>  0
+# FUNDAMENTALSALGORITHMSSTRINGSREGULAR EXPRESSIONS
