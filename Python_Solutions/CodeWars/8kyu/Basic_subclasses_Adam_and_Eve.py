9e0d6dbeea3005106c4a032cc30b1af1de9ca9e6
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 16:02:22 +0300

    Added solution "Basic subclasses - Adam and Eve"

--- Python_Solutions/CodeWars/8kyu/Basic_subclasses_Adam_and_Eve.py
+++ Python_Solutions/CodeWars/8kyu/Basic_subclasses_Adam_and_Eve.py
@@ -3,3 +3,12 @@
 # You have to do God's job. The creation method must return an array of length 2 containing objects (representing Adam and Eve). The first object in the array should be an instance of the class Man. The second should be an instance of the class Woman. Both objects have to be subclasses of Human. Your job is to implement the Human, Man and Woman classes.
 #
 # FUNDAMENTALSOBJECT-ORIENTED PROGRAMMING
+# Solution
+def God():
+    return [Man(), Woman()]
+class Human(object):
+    pass
+class Man(Human):
+    pass
+class Woman(Human):
+    pass
