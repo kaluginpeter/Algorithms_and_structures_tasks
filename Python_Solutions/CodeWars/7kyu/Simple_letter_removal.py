97f75a098c5814c200fc7b781dd3f9b84c637bf4
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 11:15:15 +0300

    Added solution "Simple letter removal"

--- Python_Solutions/CodeWars/7kyu/Simple_letter_removal.py
+++ Python_Solutions/CodeWars/7kyu/Simple_letter_removal.py
@@ -17,3 +17,14 @@
 # Simple remove duplicates
 #
 # FUNDAMENTALS
+# Solution
+def solve(st,k):
+    num, removed = 0, 0
+    while removed < k and st:
+        if chr(num+97) in st:
+            indx = st.index(chr(num+97))
+            st = st[0:indx] + st[indx+1:]
+            removed += 1
+        else:
+            num += 1
+    return st
