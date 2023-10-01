68126b143bf1e47fdc1e251a0b3a2fc02ee3c2c8
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 18:15:45 +0300

    Added solution "Gravity Flip"

--- Python_Solutions/CodeWars/8kyu/Gravity_Flip.py
+++ Python_Solutions/CodeWars/8kyu/Gravity_Flip.py
@@ -19,3 +19,7 @@
 # * 'R', [3, 2, 1, 2]      ->  [1, 2, 2, 3]
 # * 'L', [1, 4, 5, 3, 5 ]  ->  [5, 5, 4, 3, 1]
 # PUZZLESARRAYS
+# Solution
+def flip(d, a):
+    dict = {'R': sorted(a), 'L': sorted(a, reverse=True)}
+    return dict[d]
