2493994b7bc451b1de75728b5d0d5ed840cd8f93
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 17:13:02 +0300

    Added description "Filtering even numbers (Bug Fixes)"

---
+++ Python_Solutions/CodeWars/8kyu/Filtering_even_numbers_Bug_Fixes.py
@@ -0,0 +1,6 @@
+# Fix the bug in Filtering method
+# The method is supposed to remove even numbers from the list and return a list that contains the odd numbers.
+#
+# However, there is a bug in the method that needs to be resolved.
+#
+# FUNDAMENTALSDEBUGGING
