ee7ef7102fa66fa08de0faf3df8865389e6d5103
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 10:39:50 +0300

    Added description "Penultimate"

---
+++ Python_Solutions/CodeWars/7kyu/Penultimate.py
@@ -0,0 +1,11 @@
+A# Find the second-to-last element of a list.
+#
+# The input list will always contain at least two elements.
+#
+# Example:
+#
+# penultimate([1,2,3,4])            # => 3
+# penultimate("Python is dynamic") # => 'i'
+# (courtesy of haskell.org)
+#
+# LISTSFUNDAMENTALS
