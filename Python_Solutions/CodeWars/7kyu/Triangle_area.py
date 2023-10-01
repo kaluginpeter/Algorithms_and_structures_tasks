e72e95ebc712298a0292e833d6f615067bc4a89d
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 14:54:01 +0300

    Added description "Triangle area"

---
+++ Python_Solutions/CodeWars/7kyu/Triangle_area.py
@@ -0,0 +1,22 @@
+# Task.
+#
+# Calculate area of given triangle. Create a function t_area that will take a string which will represent triangle, find area of the triangle, one space will be equal to one length unit. The smallest triangle will have one length unit.
+#
+# Hints
+#
+# Ignore dots.
+#
+# All triangles will be right isoceles.
+#
+# Example:
+#
+# .
+# .      .
+# .      .       .      ---> should return 2.0
+#
+# .
+# .      .
+# .      .       .
+# .      .       .      .      ---> should return 4.5
+#
+# MATHEMATICSFUNDAMENTALS
