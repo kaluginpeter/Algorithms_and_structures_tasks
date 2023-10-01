9abf8bb8edec4fc658966b872f039d29b0b9f423
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 11:10:15 +0300

    Added solution "Simple Maths Test"

--- Python_Solutions/CodeWars/7kyu/Simple_Maths_Test.py
+++ Python_Solutions/CodeWars/7kyu/Simple_Maths_Test.py
@@ -13,3 +13,9 @@
 # number_property(-7)  # ==> [false, false, false]
 # number_property(-10) # ==> [false, true,  true]
 # ALGORITHMSMATHEMATICS
+# Solution
+def number_property(n):
+    is_prime = lambda i: False if i <= 1 else all([1 if n % i != 0 else 0 for i in range(2, int(n ** 0.5 + 1))])
+    is_even = lambda x: x%2 == 0
+    is_mult_10 = lambda x: x%10 == 0
+    return [is_prime(n), is_even(n), is_mult_10(n)]
