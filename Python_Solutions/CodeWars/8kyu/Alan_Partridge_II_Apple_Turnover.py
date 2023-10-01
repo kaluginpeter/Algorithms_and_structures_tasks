08fb8629941bd7aa5fa7c620214d91002d99b7c4
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 15:52:37 +0300

    Added solution "Alan Partridge II - Apple Turnover"

--- Python_Solutions/CodeWars/8kyu/Alan_Partridge_II_Apple_Turnover.py
+++ Python_Solutions/CodeWars/8kyu/Alan_Partridge_II_Apple_Turnover.py
@@ -22,3 +22,6 @@
 # Alan Partridge III - London
 #
 # FUNDAMENTALSSTRINGSMATHEMATICS
+# Solution
+def apple(x):
+    return "It's hotter than the sun!!" if int(x) ** 2 > 1000 else "Help yourself to a honeycomb Yorkie for the glovebox."
