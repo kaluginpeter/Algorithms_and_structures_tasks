6ceaf4195d07a1e510b68f5e754ffd436899f011
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 11:35:02 +0300

    Added solution "Sorting Dictionaries"

--- Python_Solutions/CodeWars/7kyu/Sorting_Dictionaries.py
+++ Python_Solutions/CodeWars/7kyu/Sorting_Dictionaries.py
@@ -8,3 +8,6 @@
 # sort_dict({3:1, 2:2, 1:3}) == [(1,3), (2,2), (3,1)]
 # sort_dict({1:2, 2:4, 3:6}) == [(3,6), (2,4), (1,2)]
 # SORTINGLISTSFUNDAMENTALS
+# Solution
+def sort_dict(d):
+    return sorted(d.items(), key=lambda x: x[1], reverse=True)
