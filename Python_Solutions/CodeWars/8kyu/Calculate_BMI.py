8ac5bf2728a65cf7f36f6394881ab8d6caf864a4
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 16:12:53 +0300

    Added solution "Calculate BMI"

--- Python_Solutions/CodeWars/8kyu/Calculate_BMI.py
+++ Python_Solutions/CodeWars/8kyu/Calculate_BMI.py
@@ -9,3 +9,14 @@
 # if bmi > 30 return "Obese"
 #
 # FUNDAMENTALS
+# Solution
+def bmi(weight, height):
+    bmi_m = weight / height ** 2
+    if bmi_m <= 18.5:
+        return "Underweight"
+    elif bmi_m <= 25.0:
+        return "Normal"
+    elif bmi_m <= 30.0:
+        return "Overweight"
+    elif bmi_m > 30:
+        return "Obese"
