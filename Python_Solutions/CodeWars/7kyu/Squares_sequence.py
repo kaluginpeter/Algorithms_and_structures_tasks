e58d954323da2c5a07eaac8a217ab54698983f14
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 11:43:31 +0300

    Added solution "Squares sequence"

--- Python_Solutions/CodeWars/7kyu/Squares_sequence.py
+++ Python_Solutions/CodeWars/7kyu/Squares_sequence.py
@@ -4,3 +4,13 @@
 # 2, 5  -->  [2, 4, 16, 256, 65536]
 # 3, 3  -->  [3, 9, 81]
 # MATHEMATICSFUNDAMENTALS
+# Solution
+def squares(x, n):
+    if (n<1): return []
+    result = [x]
+    i=0
+    while i < n-1:
+        i+=1
+        x*=x
+        result.append(x)
+    return result
