427588d26bcf9934655e547edb53932e243b9d4b
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 12:06:27 +0300

    Added description "Sum of a nested list"

---
+++ Python_Solutions/CodeWars/7kyu/Sum_of_a_nested_list.py
@@ -0,0 +1,4 @@
+# Implement a function to calculate the sum of the numerical values in a nested list. For example :
+#
+# sum_nested([1, [2, [3, [4]]]]) -> 10
+# RECURSIONLISTSALGORITHMSFUNDAMENTALS
