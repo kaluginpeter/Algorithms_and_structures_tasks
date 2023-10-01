1dbf90be9d241bf84e5a91b21a10d1ca6fad45d8
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 11:23:51 +0300

    Added description "Snail crawls up"

---
+++ Python_Solutions/CodeWars/7kyu/Snail_crawls_up.py
@@ -0,0 +1,10 @@
+# The snail crawls up the column. During the day it crawls up some distance. During the night she sleeps, so she slides down for some distance (less than crawls up during the day).
+#
+# Your function takes three arguments:
+#
+# The height of the column (meters)
+# The distance that the snail crawls during the day (meters)
+# The distance that the snail slides down during the night (meters)
+# Calculate number of day when the snail will reach the top of the column.
+#
+# FUNDAMENTALSMATHEMATICS
