3d86787a1ba4249f18c7ecbe474316656e0d0b26
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 15:05:11 +0300

    Added description "Upstream/Downstream"

---
+++ Python_Solutions/CodeWars/7kyu/Upstream_Downstream.py
@@ -0,0 +1,11 @@
+# Chingel is practicing for a rowing competition to be held on this saturday. He is trying his best to win this tournament for which he needs to figure out how much time it takes to cover a certain distance.
+#
+# Input
+#
+# You will be provided with the total distance of the journey, speed of the boat and whether he is going downstream or upstream. The speed of the stream and direction of rowing will be given as a string. Check example test cases!
+#
+# Output
+#
+# The output returned should be the time taken to cover the distance. If the result has decimal places, round them to 2 fixed positions.
+#
+# FUNDAMENTALS
