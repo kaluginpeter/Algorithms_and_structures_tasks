b5cbf6da69f9183fb034983c055f3da1d1791dbc
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 15:19:36 +0300

    Added description "Without the letter 'E'"

---
+++ Python_Solutions/CodeWars/7kyu/Without_the_letter_E.py
@@ -0,0 +1,10 @@
+# Is it possible to write a book without the letter 'e' ?
+#
+# Task
+# Given String str, return:
+#
+# How many "e" does it contain (case-insensitive) in string format.
+# If given String doesn't contain any "e", return: "There is no "e"."
+# If given String is empty, return empty String.
+# If given String is `null`/`None`/`nil`, return `null`/`None`/`nil`
+# REGULAR EXPRESSIONSSTRINGSFUNDAMENTALS
