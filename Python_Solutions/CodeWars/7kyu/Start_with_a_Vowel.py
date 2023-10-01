4017756d18c568f570a7235817afe86d0f81732e
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 11:44:45 +0300

    Added description "Start with a Vowel"

---
+++ Python_Solutions/CodeWars/7kyu/Start_with_a_Vowel.py
@@ -0,0 +1,7 @@
+# Create a function that takes any sentence and redistributes the spaces (and adds additional spaces if needed) so that each word starts with a vowel. The letters should all be in the same order but every vowel in the sentence should be the start of a new word. The first word in the new sentence may start without a vowel. It should return a string in all lowercase with no punctuation (only alphanumeric characters).
+#
+# EXAMPLES
+# 'It is beautiful weather today!' => 'it isb e a ut if ulw e ath ert od ay'
+# 'Coding is great' => 'c od ing isgr e at'
+# 'my number is 0208-533-2325' => 'myn umb er is02085332325'
+# REGULAR EXPRESSIONSALGORITHMS
