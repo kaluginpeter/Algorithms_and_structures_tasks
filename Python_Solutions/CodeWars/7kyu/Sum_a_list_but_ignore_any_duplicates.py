73b8b9f2083025d41c1eefb32151d91d86365399
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 11:59:29 +0300

    Added solution "Sum a list but ignore any duplicates"

--- Python_Solutions/CodeWars/7kyu/Sum_a_list_but_ignore_any_duplicates.py
+++ Python_Solutions/CodeWars/7kyu/Sum_a_list_but_ignore_any_duplicates.py
@@ -3,3 +3,6 @@
 # For instance, for the list [3, 4, 3, 6] , the function should return 10.
 #
 # ALGORITHMSFUNDAMENTALS
+# Solution
+def sum_no_duplicates(l):
+    return sum([i for i in l if l.count(i) == 1])
