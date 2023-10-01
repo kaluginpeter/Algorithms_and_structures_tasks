82366e27c94b2c4f5ee8bc2c394f879d3b71ea50
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 16:21:00 +0300

    Added solution "Classy Extentions"

--- Python_Solutions/CodeWars/8kyu/Classy_Extentions.py
+++ Python_Solutions/CodeWars/8kyu/Classy_Extentions.py
@@ -7,3 +7,9 @@
 # Reference: JS, Ruby, Python.
 #
 # FUNDAMENTALSOBJECT-ORIENTED PROGRAMMING
+# Solution
+class Cat(Animal):
+    def __init__(self, name):
+        self.name = name
+    def speak(self):
+        return self.name + ' meows.'
