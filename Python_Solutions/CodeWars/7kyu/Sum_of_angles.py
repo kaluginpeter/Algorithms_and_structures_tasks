f781fc78ceab08de2e2be6d73718cdc332934eda
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 12:38:50 +0300

    Added solution "Sum of angles"

--- Python_Solutions/CodeWars/7kyu/Sum_of_angles.py
+++ Python_Solutions/CodeWars/7kyu/Sum_of_angles.py
@@ -1,3 +1,6 @@
 # Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
 #
 # GEOMETRYFUNDAMENTALS
+# Solution
+def angle(n):
+    return 180 * (n - 2)
