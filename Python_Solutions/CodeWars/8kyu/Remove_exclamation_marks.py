8a81cead531aa8755afe387ea10f5f68ffe16c7e
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 19:17:51 +0300

    Added solution "Remove exclamation marks"

--- Python_Solutions/CodeWars/8kyu/Remove_exclamation_marks.py
+++ Python_Solutions/CodeWars/8kyu/Remove_exclamation_marks.py
@@ -1,3 +1,6 @@
 # Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
 #
 # FUNDAMENTALSSTRINGS
+# Solution
+def remove_exclamation_marks(s):
+    return ''.join( word for word in s if word not in '!')
