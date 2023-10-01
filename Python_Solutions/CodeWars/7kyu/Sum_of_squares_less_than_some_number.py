655574c49f108c26767f9b3a9b52eb147796134a
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 12:44:25 +0300

    Added solution "Sum of squares less than some number"

--- Python_Solutions/CodeWars/7kyu/Sum_of_squares_less_than_some_number.py
+++ Python_Solutions/CodeWars/7kyu/Sum_of_squares_less_than_some_number.py
@@ -3,3 +3,10 @@
 # E.g 1: For n = 6 result should be 2 because 1^2 + 2^2 = 1 + 4 = 5 and 5 < 6 E.g 2: For n = 15 result should be 3 because 1^2 + 2^2 + 3^2 = 1 + 4 + 9 = 14 and 14 < 15
 #
 # FUNDAMENTALS
+# Solution
+def get_number_of_squares(n):
+    s, c = 0, 0
+    while s < n:
+        c += 1
+        s += c**2
+    return c - 1
