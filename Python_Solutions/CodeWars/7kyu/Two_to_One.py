0d2c419e53d6e878d92456cc8f8b68f55f056726
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 14:59:36 +0300

    Added solution "Two to One"

--- Python_Solutions/CodeWars/7kyu/Two_to_One.py
+++ Python_Solutions/CodeWars/7kyu/Two_to_One.py
@@ -8,3 +8,8 @@
 # a = "abcdefghijklmnopqrstuvwxyz"
 # longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
 # FUNDAMENTALS
+# Solution
+def longest(a1, a2):
+    s1 = set(a1)
+    s2 = set(a2)
+    return ''.join(sorted(s1.union(s2)))
