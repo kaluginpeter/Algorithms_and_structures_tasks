14f4cb7347dd8ae2850e36ee52bc74fa21b7dc96
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 19:55:44 +0300

    Added solution "The Feast of Many Beasts"

--- Python_Solutions/CodeWars/8kyu/The_Feast_of_Many_Beasts.py
+++ Python_Solutions/CodeWars/8kyu/The_Feast_of_Many_Beasts.py
@@ -5,3 +5,6 @@
 # Assume that beast and dish are always lowercase strings, and that each has at least two letters. beast and dish may contain hyphens and spaces, but these will not appear at the beginning or end of the string. They will not contain numerals.
 #
 # STRINGSFUNDAMENTALS
+# Solution
+def feast(beast, dish):
+    return beast[0] == dish[0] and beast[-1] == dish[-1]
