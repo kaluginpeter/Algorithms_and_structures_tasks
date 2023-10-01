7e1097867af42c456e1913e371802eff3ed96ab4
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 15:06:29 +0300

    Added solution "V A P O R C O D E"

--- Python_Solutions/CodeWars/7kyu/V_A_P_O_R_C_O_D_E.py
+++ Python_Solutions/CodeWars/7kyu/V_A_P_O_R_C_O_D_E.py
@@ -8,3 +8,6 @@
 # "Lets go to the movies"       -->  "L  E  T  S  G  O  T  O  T  H  E  M  O  V  I  E  S"
 # "Why isn't my code working?"  -->  "W  H  Y  I  S  N  '  T  M  Y  C  O  D  E  W  O  R  K  I  N  G  ?"
 # FUNDAMENTALS
+# Solution
+def vaporcode(s):
+    return '  '.join(i.upper() for i in s.replace(' ', ''))
