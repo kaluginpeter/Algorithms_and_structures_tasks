600dfffbb19824974a66674d404252b0ee593bf9
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 12:02:30 +0300

    Added solution "Sum of Cubes"

--- Python_Solutions/CodeWars/7kyu/Sum_of_Cubes.py
+++ Python_Solutions/CodeWars/7kyu/Sum_of_Cubes.py
@@ -7,3 +7,6 @@
 # 2 --> 9 (sum of the cubes of 1 and 2 is 1 + 8)
 # 3 --> 36 (sum of the cubes of 1, 2, and 3 is 1 + 8 + 27)
 # FUNDAMENTALS
+# Solution
+def sum_cubes(n):
+    return sum(i**3 for i in range(n + 1))
