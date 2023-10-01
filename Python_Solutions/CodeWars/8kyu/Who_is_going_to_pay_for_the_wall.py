d38da313c2cb59a40f5c2b80c96e51e5abd2f17f
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 20:23:23 +0300

    Added solution "Who is going to pay for the wall?"

--- Python_Solutions/CodeWars/8kyu/Who_is_going_to_pay_for_the_wall.py
+++ Python_Solutions/CodeWars/8kyu/Who_is_going_to_pay_for_the_wall.py
@@ -3,3 +3,6 @@
 # Your code will show Full name of the neighbor and the truncated version of the name as an array. If the number of the characters in name is less than or equal to two, it will return an array containing only the name as is"
 #
 # FUNDAMENTALSSTRINGS
+# Solution
+def who_is_paying(name):
+    return [name, name[:2]] if len(name) > 2 else [name]
