eb807c794b324d33bd05dc7fab704751831ab4ad
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 14:30:28 +0300

    Added description "The Office III - Broken Photocopier"

---
+++ Python_Solutions/CodeWars/7kyu/The_Office_III_Broken_Photocopier.py
@@ -0,0 +1,12 @@
+# The bloody photocopier is broken... Just as you were sneaking around the office to print off your favourite binary code!
+#
+# Instead of copying the original, it reverses it: '1' becomes '0' and vice versa.
+#
+# Given a string of binary, return the version the photocopier gives you as a string.
+#
+# The Office I - Outed
+# The Office II - Boredeom Score
+# The Office IV - Find a Meeting Room
+# The Office V - Find a Chair
+#
+# FUNDAMENTALSSTRINGSARRAYS
