489d7535a5b590aa5fa08730b1072f84f1b2425f
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 17:43:29 +0300

    Added solution "Get Planet Name By ID"

--- Python_Solutions/CodeWars/8kyu/Get_Planet_Name_By_ID.py
+++ Python_Solutions/CodeWars/8kyu/Get_Planet_Name_By_ID.py
@@ -4,3 +4,16 @@
 #
 # 3 --> "Earth"
 # DEBUGGING
+# Solution
+def get_planet_name(id):
+    switch = {
+        1:"Mercury",
+        2:"Venus",
+        3:"Earth",
+        4:"Mars",
+        5:"Jupiter",
+        6:"Saturn",
+        7:"Uranus",
+        8: "Neptune",
+}
+    return switch[id]
