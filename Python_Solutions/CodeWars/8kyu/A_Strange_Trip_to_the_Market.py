8de3a0e69642d3c343d7156877c080fd6507357a
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 15:47:26 +0300

    Added solution "A Strange Trip to the Market"

--- Python_Solutions/CodeWars/8kyu/A_Strange_Trip_to_the_Market.py
+++ Python_Solutions/CodeWars/8kyu/A_Strange_Trip_to_the_Market.py
@@ -7,3 +7,6 @@
 # Since Nessie is a master of disguise, the only way accurately tell is to look for the phrase "tree fiddy". Since you are tired of being grifted by this monster, the time has come to code a solution for finding The Loch Ness Monster. Note that the phrase can also be written as "3.50" or "three fifty". Your function should return true if you're talking with The Loch Ness Moster, false otherwise.
 #
 # REGULAR EXPRESSIONSSTRINGSFUNDAMENTALS
+# Solution
+def is_lock_ness_monster(string):
+    return True in [i in string for i in ['tree fiddy', '3.50', 'three fifty']]
