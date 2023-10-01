8db28aa9209574d5c1eda749889e7177a91b27db
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 19:56:25 +0300

    Added solution "The Wide-Mouthed frog!"

--- Python_Solutions/CodeWars/8kyu/The_Wide_Mouthed_frog.py
+++ Python_Solutions/CodeWars/8kyu/The_Wide_Mouthed_frog.py
@@ -7,3 +7,6 @@
 # Your goal in this kata is to create complete the mouth_size method this method takes one argument animal which corresponds to the animal encountered by the frog. If this one is an alligator (case-insensitive) return small otherwise return wide.
 #
 # STRINGSLOGICFUNDAMENTALS
+# Solution
+def mouth_size(animal):
+    return 'small' if animal.lower() == 'alligator' else 'wide'
