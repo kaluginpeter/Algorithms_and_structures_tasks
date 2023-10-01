5faa1c9a9800704e03cdaf949dab53acd4fa63c9
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 15:28:47 +0300

    Added solution "[Geometry A-2]: Length of a vector"

--- Python_Solutions/CodeWars/7kyu/Geometry_A_2_Length_of_a_vector.py
+++ Python_Solutions/CodeWars/7kyu/Geometry_A_2_Length_of_a_vector.py
@@ -7,3 +7,7 @@
 # Coordinates can be integers or floating point numbers.
 #
 # GEOMETRYMATHEMATICSFUNDAMENTALS
+# Solution
+import math
+def vector_length(v):
+    return math.sqrt(math.pow(v[0][0] - v[1][0], 2) + math.pow(v[0][1] - v[1][1], 2))
