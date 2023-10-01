2e680ef2adf87a2cab5c6e0eb4bf202ef71c6773
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 18:27:27 +0300

    Added solution "If you can't sleep, just count sheep!!"

--- Python_Solutions/CodeWars/8kyu/If_you_can't_sleep_just_count_sheep.py
+++ Python_Solutions/CodeWars/8kyu/If_you_can't_sleep_just_count_sheep.py
@@ -4,3 +4,6 @@
 # Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
 #
 # FUNDAMENTALSSTRINGS
+# Solution
+def count_sheep(n):
+    return ''.join(str(i) + ' sheep...' for i in range(1, n+1)) if n!= 0 else ''
