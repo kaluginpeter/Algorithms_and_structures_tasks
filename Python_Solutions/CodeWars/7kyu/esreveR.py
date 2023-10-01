5b8780b0ed384c8eb82d4f83d94d83f8856d8e18
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 15:29:50 +0300

    Added description "esreveR"

---
+++ Python_Solutions/CodeWars/7kyu/esreveR.py
@@ -0,0 +1,5 @@
+# Write a function reverse which reverses a list (or in clojure's case, any list-like data structure)
+#
+# (the dedicated builtin(s) functionalities are deactivated)
+#
+# FUNCTIONAL PROGRAMMINGFUNDAMENTALS
