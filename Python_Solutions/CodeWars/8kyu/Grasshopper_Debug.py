a1723ac3f45c8c8fc0decb7c263afcc30de8dac5
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 18:06:13 +0300

    Added solution "Grasshopper - Debug"

--- Python_Solutions/CodeWars/8kyu/Grasshopper_Debug.py
+++ Python_Solutions/CodeWars/8kyu/Grasshopper_Debug.py
@@ -9,3 +9,15 @@
 # Remember that typically temperatures in the current weather conditions are given in whole numbers. It is possible for temperature sensors to report temperatures with a higher accuracy such as to the nearest tenth. Instrument error though makes this sort of accuracy unreliable for many types of temperature measuring sensors.
 #
 # DEBUGGING
+# Solution
+def weather_info(temp):
+    c = convert_to_celsius(temp)
+    if (c < 0):
+        return (str(c) + " is freezing temperature")
+    else:
+        return (str(c) + " is above freezing temperature")
+
+
+def convert_to_celsius(temp):
+    celsius = (temp - 32) * (5 / 9)
+    return celsius
