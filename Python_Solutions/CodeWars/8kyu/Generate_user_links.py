c6756e884a1adcdb678658795db543fa2609874b
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 17:39:14 +0300

    Added description "Generate user links"

---
+++ Python_Solutions/CodeWars/8kyu/Generate_user_links.py
@@ -0,0 +1,10 @@
+# Generate user links
+# Your task is to create userlinks for the url, you will be given a username and must return a valid link.
+#
+# Example
+# generate_link('matt c')
+# http://www.codewars.com/users/matt%20c
+# reference
+# use this as a reference encoding
+#
+# PUZZLES
