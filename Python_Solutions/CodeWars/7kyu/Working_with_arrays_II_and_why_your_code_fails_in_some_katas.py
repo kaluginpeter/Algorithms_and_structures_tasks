7fc37276d3593481d02ce352389e35e46d07eb83
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 15:23:43 +0300

    Added description "Working with arrays II (and why your code fails in some katas)"

---
+++ Python_Solutions/CodeWars/7kyu/Working_with_arrays_II_and_why_your_code_fails_in_some_katas.py
@@ -0,0 +1,7 @@
+# In this kata the function returns an array/list like the one passed to it but with its nth element removed (with 0 <= n <= array/list.length - 1). The function is already written for you and the basic tests pass, but random tests fail. Your task is to figure out why and fix it.
+#
+# Good luck!
+#
+# Some good reading: Python Docs about lists
+#
+# REFACTORINGARRAYS
