50b51081928f1fb5dcfecc71e76d6bbc77602416
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 20:09:36 +0300

    Added solution "Training JS #7: if..else and ternary operator"

--- Python_Solutions/CodeWars/8kyu/Training_JS_7_if_else_and_ternary_operator.py
+++ Python_Solutions/CodeWars/8kyu/Training_JS_7_if_else_and_ternary_operator.py
@@ -84,3 +84,8 @@
 # #41: Regular Expression--""
 # #42: Regular Expression--(?:), (?=) and (?!)
 # FUNDAMENTALSTUTORIALS
+# Solution
+def sale_hotdogs(n):
+    if n < 5: return n * 100
+    elif 5 <= n < 10: return n * 95
+    return n * 90
