556a8fd4e5d8f72c2de4da2221782992b06fbb18
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 14:51:26 +0300

    Added solution "ToLeetSpeak"

--- Python_Solutions/CodeWars/7kyu/ToLeetSpeak.py
+++ Python_Solutions/CodeWars/7kyu/ToLeetSpeak.py
@@ -38,3 +38,10 @@
 #   Z : '2'
 # }
 # FUNDAMENTALS
+# Solution
+def to_leet_speak(str):
+    dict = {'A' : '@',  'B' : '8',  'C' : '(',  'D' : 'D',  'E' : '3',  'F' : 'F',  'G' : '6',
+            'H' : '#',  'I' : '!',  'J' : 'J', 'K' : 'K',  'L' : '1',  'M' : 'M',
+            'N' : 'N',  'O': '0',  'P' : 'P',  'Q' : 'Q',  'R' : 'R',  'S' : '$',
+            'T' : '7',  'U' : 'U',  'V' : 'V',  'W' : 'W',  'X' : 'X',  'Y' : 'Y',  'Z' : '2', ' ': ' '}
+    return ''.join(dict[i] for i in str)
