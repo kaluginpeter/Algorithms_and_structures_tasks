484d1a7de28b1fd5fb36b7a21898bdd0e77d784a
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 19:00:38 +0300

    Added solution "Parse float"

--- Python_Solutions/CodeWars/8kyu/Parse_float.py
+++ Python_Solutions/CodeWars/8kyu/Parse_float.py
@@ -1,3 +1,8 @@
 # Write function parse_float which takes a string/list and returns a number or 'none' if conversion is not possible.
 #
 # FUNDAMENTALS
+# Solution
+def parse_float(string):
+    try: return float(string)
+    except Exception:
+        return None
