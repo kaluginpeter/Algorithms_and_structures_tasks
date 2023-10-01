eeda664406bd70b3bbac87c0ab1199ba5e9ed5a2
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 17:23:04 +0300

    Added solution "Find the position!"

--- Python_Solutions/CodeWars/8kyu/Find_the_position.py
+++ Python_Solutions/CodeWars/8kyu/Find_the_position.py
@@ -5,3 +5,7 @@
 # Ouput :: "Position of alphabet: 1"
 #
 # FUNDAMENTALS
+# Solution
+import string
+def position(alphabet):
+    return f"Position of alphabet: {string.ascii_lowercase.index(alphabet) + 1}"
