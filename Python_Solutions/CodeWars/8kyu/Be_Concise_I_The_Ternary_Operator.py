b147517893f0f5d390dc661d14d41f673eb9e916
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 16:04:01 +0300

    Added solution "Be Concise I - The Ternary Operator"

--- Python_Solutions/CodeWars/8kyu/Be_Concise_I_The_Ternary_Operator.py
+++ Python_Solutions/CodeWars/8kyu/Be_Concise_I_The_Ternary_Operator.py
@@ -15,3 +15,6 @@
 # Whatever you do, do not change what the function does. Good luck :)
 #
 # FUNDAMENTALSREFACTORING
+# Solution
+def describe_age(a):
+    return f"You're a(n) {a<13 and 'kid' or a<18 and 'teenager' or a<65 and 'adult' or 'elderly'}"
