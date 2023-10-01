3d1b4747f7b7dd0a0f64431ad3ca79a2c2affbcf
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 15:56:02 +0300

    Added description "Area of a Square"

---
+++ Python_Solutions/CodeWars/8kyu/Area_of_a_Square.py
@@ -0,0 +1,7 @@
+# Complete the function that calculates the area of the red square, when the length of the circular arc A is given as the input. Return the result rounded to two decimals.
+#
+# Graph
+#
+# Note: use the π value provided in your language (Math::PI, M_PI, math.pi, etc)
+#
+# FUNDAMENTALSMATHEMATICSGEOMETRY
