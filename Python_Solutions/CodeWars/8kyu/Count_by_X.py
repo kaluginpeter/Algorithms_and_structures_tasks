ca59104cb48d1782b669ff9bac11cf4fc94e2c5d
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 16:32:34 +0300

    Added solution "Count by X"

--- Python_Solutions/CodeWars/8kyu/Count_by_X.py
+++ Python_Solutions/CodeWars/8kyu/Count_by_X.py
@@ -8,3 +8,9 @@
 # count_by(1,10) #should return [1,2,3,4,5,6,7,8,9,10]
 # count_by(2,5) #should return [2,4,6,8,10]
 # ARRAYSFUNDAMENTALS
+# Solution
+def count_by(x, n):
+    list = []
+    for i in range(x, x * n + 1, x):
+        list.append(i)
+    return list
