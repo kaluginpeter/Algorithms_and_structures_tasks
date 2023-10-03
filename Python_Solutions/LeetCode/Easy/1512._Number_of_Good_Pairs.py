dde4bcabbb07c60c796db32192acb4aab3020f59
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-03 18:25:54 +0300

    Added solution "1512. Number of Good Pairs"

--- Python_Solutions/LeetCode/Easy/1512._Number_of_Good_Pairs.py
+++ Python_Solutions/LeetCode/Easy/1512._Number_of_Good_Pairs.py
@@ -34,3 +34,13 @@ class Solution:
                     count += 1
         return count
 # Solution 2 - Hash table. Speed O(N) - Memory O(N)
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        d, count = {}, 0
+        for i in nums:
+            if i in d:
+                count += d[i]
+                d[i] += 1
+            else:
+                d[i] = 1
+        return count
