027ef6fbb60806791d52ad0ed935ea74a95a405f
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-05 18:41:10 +0300

    Added description "It is written in the stars"

---
+++ Python_Solutions/CodeWars/7kyu/It_is_written_in_the_stars.py
@@ -0,0 +1,20 @@
+# Were you ever interested in the phenomena of astrology, star signs, tarot, voodoo ? (ok not voodoo that's too spooky)...
+# Task:
+# Your job for today is to finish the star_sign function by finding the astrological sign, given the birth details as a Date object.
+# Start and end dates for zodiac signs vary on different resources so we will use this table to get consistent results:
+#
+# Aquarius ------ 21 January - 19 February
+# Pisces --------- 20 February - 20 March
+# Aries ---------- 21 March - 20 April
+# Taurus -------- 21 April - 21 May
+# Gemini -------- 22 May - 21 June
+# Cancer -------- 22 June - 22 July
+# Leo ------------- 23 July - 23 August
+# Virgo ----------- 24 August - 23 September
+# Libra ----------- 24 September - 23 October
+# Scorpio -------- 24 October - 22 November
+# Sagittarius ---- 23 November - 21 December
+# Capricorn ----- 22 December - 20 January
+#
+# Test info: 100 random tests (dates range from January 1st 1940 until now)
+# DATE TIMEPUZZLES
