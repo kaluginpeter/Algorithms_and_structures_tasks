e9b04a4072afd8e5fc82003d16d876a4e0488986
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-06 19:41:42 +0300

    Added description "Product of the main diagonal of a square matrix."

---
+++ Python_Solutions/CodeWars/7kyu/Product_of_the_main_diagonal_of_a_square_matrix.py
@@ -0,0 +1,10 @@
+# Given a list of rows of a square matrix, find the product of the main diagonal.
+#
+# Examples:
+#
+# main_diagonal_product([[1,0],[0,1]]) => 1
+#
+# main_diagonal_product([[1,2,3],[4,5,6],[7,8,9]]) => 45
+# http://en.wikipedia.org/wiki/Main_diagonal
+#
+# MATRIXLINEAR ALGEBRAALGORITHMS
