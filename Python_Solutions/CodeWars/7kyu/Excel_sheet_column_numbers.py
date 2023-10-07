94124232563acee3d64bfb80ed4aebf0658ad59a
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-07 14:43:46 +0300

    Added description "Excel sheet column numbers"

---
+++ Python_Solutions/CodeWars/7kyu/Excel_sheet_column_numbers.py
@@ -0,0 +1,18 @@
+# Write a function
+#
+# titleToNumber(title) or title_to_number(title) or titleToNb title ...
+#
+# (depending on the language)
+#
+# that given a column title as it appears in an Excel sheet, returns its corresponding column number. All column titles will be uppercase.
+#
+# Examples:
+#
+# titleTonumber('A') === 1
+# titleTonumber('Z') === 26
+# titleTonumber('AA') === 27
+# Note for Clojure:
+# Don't use Java Math/pow (even with bigint) because there is a loss of precision
+# when the length of "title" is growing.
+# Write your own function "exp [x n]".
+# FUNDAMENTALSALGORITHMS
