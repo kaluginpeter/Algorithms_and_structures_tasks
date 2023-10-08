e17f08aad10be2994dd3e1bac39054040acd1acb
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-08 12:39:19 +0300

    Added solution "1979. Find Greatest Common Divisor of Array"

--- Python_Solutions/LeetCode/Easy/1979._Find_Greatest_Common_Divisor_of_Array.py
+++ Python_Solutions/LeetCode/Easy/1979._Find_Greatest_Common_Divisor_of_Array.py
@@ -34,3 +34,19 @@
 #
 # 2 <= nums.length <= 1000
 # 1 <= nums[i] <= 1000
+# Solution - Using math formula: a*b // lcd(a,b) Speed O(N) Memory O(1)
+class Solution:
+    def findGCD(self, nums: List[int]) -> int:
+        mi, ma = float('inf'), 0
+        for i in nums:
+            if i > ma:
+                ma = i
+            if i < mi:
+                mi = i
+        lcd = 1
+        while True:
+            if ma * lcd % mi == 0:
+                lcd *= ma
+                break
+            lcd += 1
+        return ma * mi // lcd
