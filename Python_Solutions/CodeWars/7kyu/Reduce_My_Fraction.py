b6a118e6063cc6e6e53dbd216d57fa6d2579094a
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-14 17:52:36 +0300

    Added solution "Reduce My Fraction"

--- Python_Solutions/CodeWars/7kyu/Reduce_My_Fraction.py
+++ Python_Solutions/CodeWars/7kyu/Reduce_My_Fraction.py
@@ -8,3 +8,19 @@
 # Note: This is an introductory Kata for a series... coming soon!
 #
 # FUNDAMENTALSRECURSIONALGORITHMS
+# Solution
+def reduce_fraction(fraction):
+    x, y = fraction
+    if x >= y:
+        if x % y == 0:
+            return x // y, 1
+        else:
+            for i in range(y - 1, 0, -1):
+                if x % i == 0 and y % i == 0:
+                    return x // i, y // i
+    else:
+        if y % x == 0:
+            return 1, y // x
+        for i in range(x - 1, 0, -1):
+            if x % i == 0 and y % i == 0:
+                return x // i, y // i
