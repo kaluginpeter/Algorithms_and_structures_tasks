f9b9f99661da5aaa289bede2ad96d238cb69b875
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-14 17:51:52 +0300

    Added description "Reduce My Fraction"

---
+++ Python_Solutions/CodeWars/7kyu/Reduce_My_Fraction.py
@@ -0,0 +1,10 @@
+# Write a function which reduces fractions to their simplest form! Fractions will be presented as an array/tuple (depending on the language) of strictly positive integers, and the reduced fraction must be returned as an array/tuple:
+#
+# input:   [numerator, denominator]
+# output:  [reduced numerator, reduced denominator]
+# example: [45, 120] --> [3, 8]
+# All numerators and denominators will be positive integers.
+#
+# Note: This is an introductory Kata for a series... coming soon!
+#
+# FUNDAMENTALSRECURSIONALGORITHMS
