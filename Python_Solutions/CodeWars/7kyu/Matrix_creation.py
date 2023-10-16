52b72df6e4cce2f7e3085b96fa3d414d6557e501
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-16 16:37:40 +0300

    Added description "Matrix creation"

---
+++ Python_Solutions/CodeWars/7kyu/Matrix_creation.py
@@ -0,0 +1,15 @@
+# Create an identity matrix of the specified size( >= 0).
+#
+# Some examples:
+#
+# (1)  =>  [[1]]
+#
+# (2) => [ [1,0],
+#          [0,1] ]
+#
+#        [ [1,0,0,0,0],
+#          [0,1,0,0,0],
+# (5) =>   [0,0,1,0,0],
+#          [0,0,0,1,0],
+#          [0,0,0,0,1] ]
+# FUNDAMENTALSARRAYSMATRIXLINEAR ALGEBRAMATHEMATICSLANGUAGE FEATURES
