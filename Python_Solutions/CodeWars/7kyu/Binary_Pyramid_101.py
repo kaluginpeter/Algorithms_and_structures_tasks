cff885ba77fcf10b1af31544e597897d92a1c833
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-18 21:40:48 +0300

    Added description "Binary Pyramid 101"

---
+++ Python_Solutions/CodeWars/7kyu/Binary_Pyramid_101.py
@@ -0,0 +1,17 @@
+# Given two numbers m and n, such that 0 ≤ m ≤ n :
+#
+# convert all numbers from m to n (inclusive) to binary
+# sum them as if they were in base 10
+# convert the result to binary
+# return as a string
+# Example
+# 1, 4  -->  1111010
+#
+# because:
+#     1  // 1 in binary is 1
+# +  10  // 2 in binary is 10
+# +  11  // 3 in binary is 11
+# + 100  // 4 in binary is 100
+# -----
+#   122  // 122 in binary is 1111010
+# MATHEMATICSFUNDAMENTALS
