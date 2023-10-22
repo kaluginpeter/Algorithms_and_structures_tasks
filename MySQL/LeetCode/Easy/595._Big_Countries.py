5e3b488c4912c3854f9823912b40054613a96f71
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-22 19:44:13 +0300

    Added solution "595. Big Countries"

--- MySQL/LeetCode/Easy/595._Big_Countries.py
+++ MySQL/LeetCode/Easy/595._Big_Countries.py
@@ -47,3 +47,7 @@
 # | Afghanistan | 25500100   | 652230  |
 # | Algeria     | 37100000   | 2381741 |
 # +-------------+------------+---------+
+# Solution
+SELECT name, population, area
+FROM World
+WHERE area >= 3000000 OR population >= 25000000;
