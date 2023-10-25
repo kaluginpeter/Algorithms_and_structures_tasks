05bcc8736036b26d8e91d520418dc1db09fd1dc9
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-25 21:00:36 +0300

    Added description "Fun with lists: indexOf"

---
+++ Python_Solutions/CodeWars/7kyu/Fun_with_lists_indexOf.py
@@ -0,0 +1,25 @@
+# Implement the method indexOf (index_of in PHP), which accepts a linked list (head) and a value, and returns the index (zero based) of the first occurrence of that value if exists, or -1 otherwise.
+#
+# For example: Given the list: 1 -> 2 -> 3 -> 3, and the value 3, indexOf / index_of should return 2.
+#
+# The linked list is defined as follows:
+#
+# class Node:
+#     def __init__(self, data, next=None):
+#         self.data = data
+#         self.next = next
+# Note: the list may be null and can hold any type of value.
+#
+# Good luck!
+#
+# This kata is part of fun with lists series:
+#
+# Fun with lists: length
+# Fun with lists: indexOf
+# Fun with lists: lastIndexOf
+# Fun with lists: countIf
+# Fun with lists: anyMatch + allMatch
+# Fun with lists: filter
+# Fun with lists: map
+# Fun with lists: reduce
+# LISTSFUNDAMENTALS
