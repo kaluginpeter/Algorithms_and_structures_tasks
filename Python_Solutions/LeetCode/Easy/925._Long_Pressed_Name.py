027eb57142b1e6236128dd9efdfa139093cec42a
Peter Kalugin <streetworkout200412@gmail.com>
2023-11-01 22:18:01 +0300

    Added solution "925. Long Pressed Name"

--- Python_Solutions/LeetCode/Easy/925._Long_Pressed_Name.py
+++ Python_Solutions/LeetCode/Easy/925._Long_Pressed_Name.py
@@ -20,3 +20,13 @@
 #
 # 1 <= name.length, typed.length <= 1000
 # name and typed consist of only lowercase English letters.
+# Solution
+class Solution(object):
+    def isLongPressedName(self, name, typed):
+        i = 0
+        for j in range(len(typed)):
+            if i < len(name) and name[i] == typed[j]:
+                i += 1
+            elif j == 0 or typed[j] != typed[j - 1]:
+                return False
+        return i == len(name)
