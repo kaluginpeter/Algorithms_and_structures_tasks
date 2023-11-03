0874bf7844bbf704599b3b2f6f006d10373e95d8
Peter Kalugin <streetworkout200412@gmail.com>
2023-11-03 15:36:06 +0300

    Added description "Thinkful - Logic Drills: Graceful addition"

---
+++ Python_Solutions/CodeWars/7kyu/Thinkful_Logic_Drills_Graceful_addition.py
@@ -0,0 +1,9 @@
+# You like the way the Python + operator easily handles adding different numeric types, but you need a tool to do that kind of addition without killing your program with a TypeError exception whenever you accidentally try adding incompatible types like strings and lists to numbers.
+#
+# You decide to write a function my_add() that takes two arguments. If the arguments can be added together it returns the sum. If adding the arguments together would raise an error the function should return None instead.
+#
+# For example, my_add(1, 3.414) would return 4.414, but my_add(42, " is the answer.") would return None.
+#
+# Hint: using a try / except statement may simplify this kata.
+#
+# FUNDAMENTALS
