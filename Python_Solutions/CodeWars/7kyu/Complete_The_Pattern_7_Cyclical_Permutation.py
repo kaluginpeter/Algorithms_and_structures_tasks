17be5ad83481dc084e577912aff9b3eee06feb64
Peter Kalugin <streetworkout200412@gmail.com>
2023-11-06 11:50:49 +0300

    Added solution "Complete The Pattern #7 - Cyclical Permutation"

--- Python_Solutions/CodeWars/7kyu/Complete_The_Pattern_7_Cyclical_Permutation.py
+++ Python_Solutions/CodeWars/7kyu/Complete_The_Pattern_7_Cyclical_Permutation.py
@@ -27,3 +27,12 @@
 # Hint: Use \n in string to jump to next line
 #
 # ASCII ARTFUNDAMENTALS
+# Solution
+def pattern(n):
+    if n <= 0:
+        return ''
+    word, strng = [], [i for i in range(1, n + 1)]
+    for i in range(n):
+        word.append(''.join(str(i) for i in strng))
+        strng = strng[1:] + [strng[0]]
+    return '\n'.join(word)
