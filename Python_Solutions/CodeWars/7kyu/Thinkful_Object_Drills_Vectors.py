21bab41e61c225ea6392a9f62a8dbc45a211706b
Peter Kalugin <streetworkout200412@gmail.com>
2023-11-09 19:03:06 +0300

    Added solution "Thinkful - Object Drills: Vectors"

--- Python_Solutions/CodeWars/7kyu/Thinkful_Object_Drills_Vectors.py
+++ Python_Solutions/CodeWars/7kyu/Thinkful_Object_Drills_Vectors.py
@@ -18,3 +18,10 @@
 # Adding vectors when you have their components is easy: just add the two x components together and the two y components together to get the x and y components for the vector sum.
 #
 # FUNDAMENTALS
+# Solution
+class Vector(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def add(self, v):
+        return Vector(self.x + v.x, self.y + v.y)
