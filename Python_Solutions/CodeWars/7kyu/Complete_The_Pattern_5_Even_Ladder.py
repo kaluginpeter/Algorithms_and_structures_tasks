43c38573cf60b637d35ec221de5f93f12e72f691
Peter Kalugin <streetworkout200412@gmail.com>
2023-11-11 12:25:16 +0300

    Added description "Complete The Pattern #5 - Even Ladder"

---
+++ Python_Solutions/CodeWars/7kyu/Complete_The_Pattern_5_Even_Ladder.py
@@ -0,0 +1,21 @@
+# Task:
+# You have to write a function pattern which creates the following pattern up to n/2 number of lines.
+#
+# If n <= 1 then it should return "" (i.e. empty string).
+# If any odd number is passed as argument then the pattern should last up to the largest even number which is smaller than the passed odd number.
+# Examples:
+# pattern(8):
+#
+# 22
+# 4444
+# 666666
+# 88888888
+# pattern(5):
+#
+# 22
+# 4444
+# Note: There are no spaces in the pattern
+#
+# Hint: Use \n in string to jump to next line
+#
+# ASCII ARTFUNDAMENTALS
