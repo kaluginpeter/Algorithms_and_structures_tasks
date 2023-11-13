f2f58318dac526ce0b6093a19cfbb6aad98e4aa1
Peter Kalugin <streetworkout200412@gmail.com>
2023-11-13 18:57:51 +0300

    Added description "Don't rely on luck."

---
+++ Python_Solutions/CodeWars/6kyu/Don_t_rely_on_luck.py
@@ -0,0 +1,12 @@
+# The test fixture I use for this kata is pre-populated.
+#
+# It will compare your guess to a random number generated using:
+#
+# randint(1,100)
+# You can pass by relying on luck or skill but try not to rely on luck.
+#
+# "The power to define the situation is the ultimate power." - Jerry Rubin
+#
+# Good luck!
+#
+# GAMESPUZZLES
