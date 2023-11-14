b09f8ee33ce8f57eb6b0ce9ca6befef9cddcfa5b
Peter Kalugin <streetworkout200412@gmail.com>
2023-11-14 19:27:06 +0300

    Added solution "Linked Lists - Length & Count"

--- Python_Solutions/CodeWars/6kyu/Linked_Lists_Length_Count.py
+++ Python_Solutions/CodeWars/6kyu/Linked_Lists_Length_Count.py
@@ -36,3 +36,25 @@
 # Inspired by Stanford Professor Nick Parlante's excellent Linked List teachings.
 #
 # LINKED LISTSDATA STRUCTURESFUNDAMENTALS
+# Solution
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+def length(node):
+    count = 0
+    while node:
+        count += 1
+        node = node.next
+    return count
+
+
+def count(node, data):
+    count = 0
+    while node:
+        if node.data == data:
+            count += 1
+        node = node.next
+    return count
