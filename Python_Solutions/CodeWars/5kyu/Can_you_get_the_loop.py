9dbba3e6393ada6c01a4c9ec5bf1db8871ea62af
Peter Kalugin <streetworkout200412@gmail.com>
2023-11-18 11:43:25 +0300

    Added description "Can you get the loop ?"

---
+++ Python_Solutions/CodeWars/5kyu/Can_you_get_the_loop.py
@@ -0,0 +1,16 @@
+# You are given a node that is the beginning of a linked list. This list contains a dangling piece and a loop. Your objective is to determine the length of the loop.
+#
+# For example in the following picture the size of the dangling piece is 3 and the loop size is 12:
+#
+#
+# # Use the `next' attribute to get the following node
+# node.next
+# Notes:
+#
+# do NOT mutate the nodes!
+# in some cases there may be only a loop, with no dangling piece
+# Thanks to shadchnev, I broke all of the methods from the Hash class.
+#
+# Don't miss dmitry's article in the discussion after you pass the Kata !!
+#
+# ALGORITHMSLINKED LISTSPERFORMANCE
