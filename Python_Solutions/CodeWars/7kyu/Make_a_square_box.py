8600d975bfc608ade811219532696feeb9c741e5
Peter Kalugin <streetworkout200412@gmail.com>
2023-11-20 18:27:27 +0300

    Added description "Make a square box!"

---
+++ Python_Solutions/CodeWars/7kyu/Make_a_square_box.py
@@ -0,0 +1,21 @@
+# Easy; Make a box
+# Given a number as a parameter (between 2 and 30), return an array containing strings which form a box.
+# Like this:
+#
+# n = 5
+#
+# [
+#   '-----',
+#   '-   -',
+#   '-   -',
+#   '-   -',
+#   '-----'
+# ]
+# n = 3
+#
+# [
+#   '---',
+#   '- -',
+#   '---'
+# ]
+# STRINGSARRAYSASCII ARTALGORITHMS
