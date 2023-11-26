eda4279a1bbcfaa524f10838452db69e87e2886c
Peter Kalugin <streetworkout200412@gmail.com>
2023-11-26 09:31:31 +0300

    Added solution "1037. Valid Boomerang"

--- Python_Solutions/LeetCode/Easy/1037._Valid_Boomerang.py
+++ Python_Solutions/LeetCode/Easy/1037._Valid_Boomerang.py
@@ -19,3 +19,8 @@
 # points.length == 3
 # points[i].length == 2
 # 0 <= xi, yi <= 100
+# Solution Geometry Triangle Area Speed O(1), Memory O(1)
+class Solution(object):
+    def isBoomerang(self, p):
+        x1, y1, x2, y2, x3, y3 = p[0][0], p[0][1], p[1][0], p[1][1], p[2][0], p[2][1]
+        return x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2) != 0
