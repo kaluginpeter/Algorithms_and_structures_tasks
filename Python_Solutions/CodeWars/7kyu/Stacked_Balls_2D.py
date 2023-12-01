df057431c74a30f1bfc4d23818228be08e95142c
Peter Kalugin <streetworkout200412@gmail.com>
2023-12-01 19:37:52 +0300

    Added description "Stacked Balls - 2D"

---
+++ Python_Solutions/CodeWars/7kyu/Stacked_Balls_2D.py
@@ -0,0 +1,23 @@
+# Background
+# I have stacked some pool balls in a triangle.
+#
+# Like this,
+#
+# pool balls
+# Kata Task
+# Given the number of layers of my stack, what is the total height?
+#
+# Return the height as multiple of the ball diameter.
+#
+# Example
+# The image above shows a stack of 5 layers.
+#
+# Notes
+# layers >= 0
+# approximate answers (within 0.001) are good enough
+# See Also
+#
+# Stacked Balls - 2D
+# Stacked Balls - 3D with square base
+# Stacked Balls - 3D with triangle base
+# FUNDAMENTALS
