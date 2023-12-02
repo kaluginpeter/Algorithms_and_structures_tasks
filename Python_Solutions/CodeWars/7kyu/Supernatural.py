8030ca76ae47cea0c2e95971454fa7fae8b2d19f
Peter Kalugin <streetworkout200412@gmail.com>
2023-12-02 13:13:28 +0300

    Added solution "Supernatural"

--- Python_Solutions/CodeWars/7kyu/Supernatural.py
+++ Python_Solutions/CodeWars/7kyu/Supernatural.py
@@ -34,3 +34,18 @@
 # ...and you reply (return) with the info, and your signature saying of yours! Burn it alive, idjits!
 #
 # GAMESFUNDAMENTALS
+# Solution
+def bob(what):
+    d = {'werewolf' : 'Silver knife or bullet to the heart', 'vampire' : 'Behead it with a machete',
+        'wendigo' : 'Burn it to death', 'shapeshifter' : 'Silver knife or bullet to the heart',
+        'angel' : 'Use the angelic blade', 'demon' : "Use Ruby's knife, or some Jesus-juice",
+        'ghost' : "Salt and iron, and don't forget to burn the corpse", 'dragon' : 'You have to find the excalibur for that',
+         'djinn' : "Stab it with silver knife dipped in a lamb's blood",
+        'pagan god' : 'It depends on which one it is', 'leviathan' : 'Use some Borax, then kill Dick',
+        'ghoul' : 'Behead it', 'jefferson starship' : 'Behead it with a silver blade',
+        'reaper' : "If it's nasty, you should gank who controls it", 'rugaru' : 'Burn it alive',
+        'skinwalker' : 'A silver bullet will do it', 'phoenix' : 'Use the colt', 'witch' : 'They are humans'}
+    word = ', idjits!'
+    if what in d:
+        return d[what] + word
+    return 'I have friggin no idea yet' + word
