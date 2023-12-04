94cd6a4796145a69083ef8a813049464bedfdadc
Peter Kalugin <streetworkout200412@gmail.com>
2023-12-04 17:54:59 +0300

    Added solution "Find heavy ball - level: novice"

--- Python_Solutions/CodeWars/7kyu/Find_heavy_ball_level_novice.py
+++ Python_Solutions/CodeWars/7kyu/Find_heavy_ball_level_novice.py
@@ -25,3 +25,17 @@
 # conqueror
 # master
 # PUZZLESLOGICRIDDLES
+# Solution
+def find_ball(scales):
+    # call scales.get_weight() at most 4 TIMES
+    # return indexOfHeavierBall
+    for i in range(1, 8, 2):
+        leftPan = [i-1]
+        rightPan = [i]
+        w = scales.get_weight(leftPan, rightPan)
+
+        if w < 0:
+            return leftPan[0]
+
+        if w > 0:
+            return rightPan[0]
