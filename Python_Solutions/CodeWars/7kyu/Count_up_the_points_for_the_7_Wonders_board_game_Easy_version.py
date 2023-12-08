06808dad8f642045015ade3930c1573664fa4551
Peter Kalugin <streetworkout200412@gmail.com>
2023-12-08 18:11:24 +0300

    Added description "Count up the points for the 7 Wonders board game! Easy version"

---
+++ Python_Solutions/CodeWars/7kyu/Count_up_the_points_for_the_7_Wonders_board_game_Easy_version.py
@@ -0,0 +1,30 @@
+# 7 Wonders
+# 7 Wonders is a board game that consists of building your city, gathering resources and fighting your neighbors.
+#
+# One part of the game is also to research science in order to gain points at the end of the game. There are 3 types of science glyphs you can gather:
+#
+# Compasses
+# Gears
+# Tablets
+# The way points are added up works as described here:
+#
+# Step 1
+# Each distinct set of three different glyphs is worth 7 points:
+#
+# 1 Compass, 1 Gear and 1 Tablet    =  7 points
+# 2 Compasses, 1 Gear and 1 Tablet  =  7 points (because it's just one disctinct set)
+# Note that a distinct set of three different glyphs means 1 Compass, 1 Gear and 1 Tablet. No more, no less!
+#
+# Step 2
+# The amount of each glyph you own is squared and then summed up:
+#
+# 1 Compass, 1 Gear and 1 Tablet    =  1*1 + 1*1 + 1*1  =  3 points
+# 2 Compasses, 1 Gear and 1 Tablet  =  2*2 + 1*1 + 1*1  =  6 points
+# Finally
+# The total science points is equal to the sum of the two steps:
+#
+# 1 Compass, 1 Gear and 1 Tablet    =  7 + 3  =  10 points
+# 2 Compasses, 1 Gear and 1 Tablet  =  7 + 6  =  13 points
+# You will be given 3 inputs corresponding to the amount of each glyph you have acquired in the game. Your task is to output the final score. Take into account that you may have no glyphs at all!
+#
+# PUZZLES
