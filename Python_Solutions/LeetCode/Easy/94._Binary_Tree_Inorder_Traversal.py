c1274b065d7bf5efad1d28e21ae8825beb2abdf1
Peter Kalugin <streetworkout200412@gmail.com>
2023-12-09 10:37:14 +0300

    Added solution "94. Binary Tree Inorder Traversal"

--- Python_Solutions/LeetCode/Easy/94._Binary_Tree_Inorder_Traversal.py
+++ Python_Solutions/LeetCode/Easy/94._Binary_Tree_Inorder_Traversal.py
@@ -44,3 +44,15 @@ class Solution:
             if curr.left is not None:
                 stack.append(curr.left)
         return res
+
+
+# Solution 2
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def inorderTraversal(self, root):
+        return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right) if root else []
