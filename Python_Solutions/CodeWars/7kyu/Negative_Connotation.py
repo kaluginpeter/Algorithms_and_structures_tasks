cb3021c0b4d25f87b9615431952b2c06ae5fada0
Peter Kalugin <streetworkout200412@gmail.com>
2023-12-13 18:24:59 +0300

    Added description "Negative Connotation"

---
+++ Python_Solutions/CodeWars/7kyu/Negative_Connotation.py
@@ -0,0 +1,9 @@
+# You will be given a string with sets of characters, (i.e. words), seperated by between one and four spaces (inclusive).
+#
+# Looking at the first letter of each word (case insensitive-"A" and "a" should be treated the same), you need to determine whether it falls into the positive/first half of the alphabet ("a"-"m") or the negative/second half ("n"-"z").
+#
+# Return True/true if there are more (or equal) positive words than negative words, False/false otherwise.
+#
+# "A big brown fox caught a bad rabbit" => True/true
+# "Xylophones can obtain Xenon." => False/false
+# FUNDAMENTALS
