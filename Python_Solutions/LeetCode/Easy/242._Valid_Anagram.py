bb1a31b5c0b7f21fbb2a65d2bcd2da1d7ba1c624
Peter Kalugin <streetworkout200412@gmail.com>
2023-12-16 13:09:38 +0300

    Added solution "242. Valid Anagram"

--- Python_Solutions/LeetCode/Easy/242._Valid_Anagram.py
+++ Python_Solutions/LeetCode/Easy/242._Valid_Anagram.py
@@ -19,3 +19,43 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         return s == t
+
+# Solution 1 - One HashTable O(N) O(N)
+class Solution(object):
+    def isAnagram(self, s, t):
+        d = {}
+        for i in s:
+            d[i] = d.get(i, 0) + 1
+        for i in t:
+            d[i] = d.get(i, 0) - 1
+        for i in d:
+            if d[i] != 0:
+                return False
+        return True
+# Solution 2 - Two HashTable O(N) O(N)
+class Solution(object):
+    def isAnagram(self, s, t):
+        if len(s) != len(t):
+            return False
+        d1, d2 = {}, {}
+        for i in s:
+            if i  not in d1:
+                d1[i] = 1
+            else:
+                d1[i] += 1
+        for i in t:
+            if i not in d2:
+                d2[i] = 1
+            else:
+                d2[i] += 1
+        return d1 == d2
+# Solution 3 - Similar Sorting O(N log(N)) O(N)
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s) == sorted(t)
+# Solution 4 - One Liner O(N**2) O(N)
+class Solution(object):
+    def isAnagram(self, s, t):
+        if len(s) != len(t):
+            return False
+        return all(s.count(i) == t.count(i) for i in t)
