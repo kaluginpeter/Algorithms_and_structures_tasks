d046a60cc212b50c7fb85d93b4705e5051bedd7b
Peter Kalugin <streetworkout200412@gmail.com>
2023-12-16 13:55:01 +0300

    Added solution "Explosive Sum"

--- Python_Solutions/CodeWars/4kyu/Explosive_Sum.py
+++ Python_Solutions/CodeWars/4kyu/Explosive_Sum.py
@@ -24,3 +24,26 @@
 # See here for more examples.
 #
 # ALGORITHMSMATHEMATICSFUNDAMENTALS
+# Solution
+class PWithCache:
+    def __init__(self):
+        self.computed_pn = {0: 1}
+    def __call__(self, n: int) -> int:
+        if n in self.computed_pn:
+            return self.computed_pn[n]
+        total = 0
+        for k in range(1, n+1):
+            minus_one_term = n - k*(3*k-1)//2
+            plus_one_term = n - k*(3*k+1)//2
+            first_term = 0 if minus_one_term < 0 else self(minus_one_term)
+            second_term = 0 if plus_one_term < 0 else self(plus_one_term)
+            if k % 2:
+                total += first_term + second_term
+            else:
+                total -= first_term + second_term
+        self.computed_pn[n] = total
+        return total
+
+def exp_sum(n):
+    P = PWithCache()
+    return P(n)
