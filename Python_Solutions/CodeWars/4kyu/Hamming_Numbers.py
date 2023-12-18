29a52b1510d02eb6c7c1b54e8fb0affcdced659a
Peter Kalugin <streetworkout200412@gmail.com>
2023-12-18 19:14:43 +0300

    Added solution "Hamming Numbers"

--- Python_Solutions/CodeWars/4kyu/Hamming_Numbers.py
+++ Python_Solutions/CodeWars/4kyu/Hamming_Numbers.py
@@ -14,3 +14,17 @@
 # Your code should be able to compute the first 5 000 ( LC: 400, Clojure: 2 000, Haskell: 12 691, NASM, C, D, C++, Go and Rust: 13 282 ) Hamming numbers without timing out.
 #
 # NUMBER THEORYALGORITHMS
+# Solution
+def hamming(n):
+    l = [1]
+    n2, n3, n5 = 0, 0, 0
+    for i in range(1, n):
+        mn = min(l[n2] * 2, l[n3] * 3, l[n5] * 5)
+        if 2 * l[n2] == mn:
+            n2 += 1
+        if 3 * l[n3] == mn:
+            n3 += 1
+        if 5 * l[n5] == mn:
+            n5 += 1
+        l.append(mn)
+    return l[-1]
