c1729819f9b7a7640788e95252bcf94bfcecb03a
Peter Kalugin <streetworkout200412@gmail.com>
2023-12-22 19:09:32 +0300

    Added description "Least Common Multiple"

---
+++ Python_Solutions/CodeWars/5kyu/Least_Common_Multiple.py
@@ -0,0 +1,3 @@
+# Write a function that calculates the least common multiple of its arguments; each argument is assumed to be a non-negative integer. In the case that there are no arguments (or the provided array in compiled languages is empty), return 1. If any argument is 0, return 0.
+#
+# MATHEMATICSALGORITHMS
