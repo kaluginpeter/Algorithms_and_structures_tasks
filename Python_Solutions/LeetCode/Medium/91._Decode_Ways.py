dd5b860215d463abeb3a44b7b4643208eea64eb5
Peter Kalugin <streetworkout200412@gmail.com>
2023-12-25 18:44:10 +0300

    Added solution "91. Decode Ways"

--- Python_Solutions/LeetCode/Medium/91._Decode_Ways.py
+++ Python_Solutions/LeetCode/Medium/91._Decode_Ways.py
@@ -37,3 +37,34 @@
 #
 # 1 <= s.length <= 100
 # s contains only digits and may contain leading zero(s).
+# Solution 1 - DP O(N) O(N)
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        count1, count2 = 1 if s[0] > '0' else 0, 0
+        top = s[0]
+        for i in s[1:]:
+            prev = count1
+            top += i
+            if i > '0':
+                count1 = count1 + count2
+            else:
+                count1 = 0
+            if len(top) == 2:
+                if '09' < top <= '26':
+                    count2 = prev
+                else:
+                    count2 = 0
+                top = top[-1]
+        return count1 + count2
+# Solution 2 DP O(N) O(N)
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        dp = [0 for i in range(len(s) + 1)]
+        dp[0] = 1
+        dp[1] = 0 if s[0] == '0' else 1
+        for i in range(2, len(s) + 1):
+            if 0 < int(s[i - 1]) <= 9:
+                dp[i] += dp[i - 1]
+            if 10 <= int(s[i - 2:i]) <= 26:
+                dp[i] += dp[i - 2]
+        return dp[len(s)]
