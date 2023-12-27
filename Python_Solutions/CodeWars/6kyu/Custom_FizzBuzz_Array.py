6845f740000ecdbf37cffff9131b1b6c798cfa5d
Peter Kalugin <streetworkout200412@gmail.com>
2023-12-27 17:42:38 +0300

    Added solution "Custom FizzBuzz Array"

--- Python_Solutions/CodeWars/6kyu/Custom_FizzBuzz_Array.py
+++ Python_Solutions/CodeWars/6kyu/Custom_FizzBuzz_Array.py
@@ -20,3 +20,16 @@
 # The function must return the sequence as a list.
 #
 # ARRAYSLOGICFUNDAMENTALS
+# Solution
+def fizz_buzz_custom(string_one='Fizz', string_two='Buzz', num_one=3, num_two=5):
+    l: list = []
+    for i in range(1, 101):
+        if i % num_one == 0 and i % num_two == 0:
+            l.append(string_one + string_two)
+        elif i % num_one == 0:
+            l.append(string_one)
+        elif i % num_two == 0:
+            l.append(string_two)
+        else:
+            l.append(i)
+    return l
