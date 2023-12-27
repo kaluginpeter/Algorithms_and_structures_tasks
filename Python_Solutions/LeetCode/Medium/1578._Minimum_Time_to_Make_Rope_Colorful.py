ea8d8bcf6bae0cd1f2def7b17230b6907ac5f628
Peter Kalugin <streetworkout200412@gmail.com>
2023-12-27 17:30:36 +0300

    Added solution "1578. Minimum Time to Make Rope Colorful"

--- Python_Solutions/LeetCode/Medium/1578._Minimum_Time_to_Make_Rope_Colorful.py
+++ Python_Solutions/LeetCode/Medium/1578._Minimum_Time_to_Make_Rope_Colorful.py
@@ -35,3 +35,30 @@
 # 1 <= n <= 105
 # 1 <= neededTime[i] <= 104
 # colors contains only lowercase English letters.
+# Solution 1 - O(N) O(1)
+class Solution:
+    def minCost(self, colors: str, neededTime: List[int]) -> int:
+        if len(colors) == 1:
+            return 0
+        count, s, top = 0, 0, 0
+        for i in range(1, len(colors)):
+            s += neededTime[i-1]
+            top = max(top, neededTime[i-1])
+            if colors[i-1] != colors[i]:
+                count += s - top
+                s, top = 0, 0
+        if colors[-2] == colors[-1]:
+            s += neededTime[-1]
+            top = max(top, neededTime[-1])
+            count += s - top
+        return count
+# Solution 2 - O(N) O(1) But shorter than solution above
+class Solution:
+    def minCost(self, colors: str, neededTime: List[int]) -> int:
+        sm, mx = 0, 0
+        for i in range(len(colors)):
+            if colors[i-1] != colors[i]:
+                mx = 0
+            sm += min(mx, neededTime[i])
+            mx = max(mx, neededTime[i])
+        return sm
