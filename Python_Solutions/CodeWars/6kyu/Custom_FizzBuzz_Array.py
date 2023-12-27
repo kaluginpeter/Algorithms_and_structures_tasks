1c32f5fe32bf7fcfec53c92d0f886a2762ae2fa5
Peter Kalugin <streetworkout200412@gmail.com>
2023-12-27 17:42:14 +0300

    Added description "Custom FizzBuzz Array"

---
+++ Python_Solutions/CodeWars/6kyu/Custom_FizzBuzz_Array.py
@@ -0,0 +1,22 @@
+# Write a function that returns a (custom) FizzBuzz sequence of the numbers 1 to 100.
+#
+# The function should be able to take up to 4 arguments:
+#
+# The 1st and 2nd arguments are strings, "Fizz" and "Buzz" by default;
+# The 3rd and 4th arguments are integers, 3 and 5 by default.
+# Thus, when the function is called without arguments, it will return the classic FizzBuzz sequence up to 100:
+#
+# [ 1, 2, "Fizz", 4, "Buzz", "Fizz", 7, ... 14, "FizzBuzz", 16, 17, ... 98, "Fizz", "Buzz" ]
+# When the function is called with (up to 4) arguments, it should return a custom FizzBuzz sequence, for example:
+#
+# ('Hey', 'There')      -->  [ 1, 2, "Hey", 4, "There", "Hey", ... ]
+# ('Foo', 'Bar', 2, 3)  -->  [ 1, "Foo", "Bar", "Foo", 5, "FooBar", 7, ... ]
+# Examples
+# fizz_buzz_custom()[15]                         # returns 16
+# fizz_buzz_custom()[44]                         # returns "FizzBuzz" (45 is divisible by 3 and 5)
+# fizz_buzz_custom('Hey', 'There')[25]         # returns 26
+# fizz_buzz_custom('Hey', 'There')[11]         # returns "Hey" (12 is divisible by 3)
+# fizz_buzz_custom("What's ", "up?", 3, 7)[80] # returns "What's " (81 is divisible by 3)
+# The function must return the sequence as a list.
+#
+# ARRAYSLOGICFUNDAMENTALS
