dc44084195981690aa64c5f66bcb331e1956f46e
Peter Kalugin <streetworkout200412@gmail.com>
2023-12-28 16:05:50 +0300

    Added solution "1531. String Compression II"

--- Python_Solutions/LeetCode/Hard/1531._String_Compression_II.py
+++ Python_Solutions/LeetCode/Hard/1531._String_Compression_II.py
@@ -30,3 +30,21 @@
 # 1 <= s.length <= 100
 # 0 <= k <= s.length
 # s contains only lowercase English letters.
+# Solution
+class Solution:
+    def getLengthOfOptimalCompression(self, s: str, k: int) -> int:
+        @lru_cache(None)
+        def dp(i, prev, prev_cnt, k):
+            if k < 0:
+                return float('inf')
+            if i >= len(s):
+                return 0
+            delete = dp(i + 1, prev, prev_cnt, k - 1)
+            if s[i] == prev:
+                keep = dp(i + 1, prev, prev_cnt + 1, k)
+                if prev_cnt in {1, 9, 99}:
+                    keep += 1
+            else:
+                keep = dp(i + 1, s[i], 1, k) + 1
+            return min(delete, keep)
+        return dp(0, '', 0, k)
