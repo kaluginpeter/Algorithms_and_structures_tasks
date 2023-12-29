c2a46795759cbdd782a352aec9f81d2c2eab9c4c
Peter Kalugin <streetworkout200412@gmail.com>
2023-12-29 17:46:51 +0300

    Added solution "Longest Common Subsequence"

--- Python_Solutions/CodeWars/5kyu/Longest_Common_Subsequence.py
+++ Python_Solutions/CodeWars/5kyu/Longest_Common_Subsequence.py
@@ -26,3 +26,18 @@
 # First property
 # Second property
 # STRINGSALGORITHMS
+# Solution
+def lcs(s1, s2):
+    if not s1 or not s2:
+        return ''
+    matrix = [["" for x in range(len(s2))] for x in range(len(s1))]
+    for i in range(len(s1)):
+        for j in range(len(s2)):
+            if s1[i] == s2[j]:
+                if i == 0 or j == 0:
+                    matrix[i][j] = s1[i]
+                else:
+                    matrix[i][j] = matrix[i-1][j-1] + s1[i]
+            else:
+                matrix[i][j] = max(matrix[i-1][j], matrix[i][j-1], key=len)
+    return matrix[-1][-1]
