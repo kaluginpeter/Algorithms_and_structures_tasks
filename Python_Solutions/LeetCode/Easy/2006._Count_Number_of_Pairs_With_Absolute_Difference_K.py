22d4a7545f876f2dcadc043fe159ee27dcb1c035
Peter Kalugin <streetworkout200412@gmail.com>
2023-12-29 17:26:05 +0300

    Added solution "2006. Count Number of Pairs With Absolute Difference K"

--- Python_Solutions/LeetCode/Easy/2006._Count_Number_of_Pairs_With_Absolute_Difference_K.py
+++ Python_Solutions/LeetCode/Easy/2006._Count_Number_of_Pairs_With_Absolute_Difference_K.py
@@ -35,3 +35,25 @@
 # 1 <= nums.length <= 200
 # 1 <= nums[i] <= 100
 # 1 <= k <= 99
+# Solution 1 - Brute Force - Nested loops. Speed O(N**2) and Memory O(1)
+# Note: if we should find a differences by absolute (abs) method, we can nof check i < j, but in the end, we have pairs*2
+# variations of multiplicate numbers. And then we just should divide answer on 2
+class Solution:
+    def countKDifference(self, nums: List[int], k: int) -> int:
+        count: int = 0
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if abs(nums[i] - nums[j]) == k:
+                    count += 1
+        return count // 2
+# Solution 2 HashTable. Speed O(N) and Memory O(N)
+class Solution:
+    def countKDifference(self, nums: List[int], k: int) -> int:
+        hm: dict = {}
+        for i in nums:
+            hm[i] = hm.get(i, 0) + 1
+        count: int = 0
+        for i in nums:
+            if k + i in hm:
+                count += hm[k + i]
+        return count
