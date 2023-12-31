6c4b973a4ca42e443c0a0595c0f70501f0e4c3af
Peter Kalugin <streetworkout200412@gmail.com>
2023-12-31 10:25:34 +0300

    Added description "Angle Between Clock Hands"

---
+++ Python_Solutions/CodeWars/6kyu/Angle_Between_Clock_Hands.py
@@ -0,0 +1,13 @@
+# Given a Date (in JS and Ruby) or hours and minutes (in C and Python), return the angle between the two hands of a 12-hour analog clock in radians.
+#
+# Notes:
+# The minute hand always points to the exact minute (there is no seconds hand).
+# The hour hand does not "snap" to the tick marks: e.g. at 6:30 the angle is not 0 because the hour hand is already half way between 6 and 7.
+# Return the smaller of the angles ( <= π ).
+# Return π if the hands are opposite.
+# Examples
+# at noon the angle is: 0
+# at 3:00 the angle is: π/2 (90 degrees)
+# at 6:00 the angle is: π (180 degrees)
+# at 9:00 the angle is: π/2 (90 degrees)
+# MATHEMATICSDATE TIMEALGORITHMS
