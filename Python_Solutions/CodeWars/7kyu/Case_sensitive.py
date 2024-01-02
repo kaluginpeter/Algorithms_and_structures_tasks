95159fd5823d31334359bdfea9f8b297f11aaf06
Peter Kalugin <streetworkout200412@gmail.com>
2024-01-02 19:04:24 +0300

    Added description "Case-sensitive!"

---
+++ Python_Solutions/CodeWars/7kyu/Case_sensitive.py
@@ -0,0 +1,17 @@
+# Your task is very simple. Given an input string s, case_sensitive(s), check whether all letters are lowercase or not. Return True/False and a list of all the entries that are not lowercase in order of their appearance in s.
+#
+# For example, case_sensitive('codewars') returns [True, []], but case_sensitive('codeWaRs') returns [False, ['W', 'R']].
+#
+# Goodluck :)
+#
+# Have a look at my other katas!
+#
+# Alphabetically ordered
+#
+# Find Nearest square number
+#
+# Not prime numbers
+#
+# Find your caterer
+#
+# STRINGSFUNDAMENTALS
