babf4f45724f4f31025920311c0b14b97512ddc9
Peter Kalugin <streetworkout200412@gmail.com>
2024-01-02 20:20:20 +0300

    Added solution "Put a Letter in a Column"

--- Python_Solutions/CodeWars/7kyu/Put_a_Letter_in_a_Column.py
+++ Python_Solutions/CodeWars/7kyu/Put_a_Letter_in_a_Column.py
@@ -7,3 +7,9 @@
 # Assume that argument index is integer [0, 8]. Assume that argument character is string with one character.
 #
 # FUNDAMENTALSSTRINGS
+# Solution
+def build_row_text(index, character):
+    s: str = ''
+    for i in range(9):
+        s += f'|{character if index == i else " "}'
+    return s + '|'
