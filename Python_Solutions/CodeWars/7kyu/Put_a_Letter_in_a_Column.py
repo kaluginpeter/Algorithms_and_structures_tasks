13a2eecb5cb3e0a18b626123e3d9b24553fbba37
Peter Kalugin <streetworkout200412@gmail.com>
2024-01-02 20:19:58 +0300

    Added description "Put a Letter in a Column"

---
+++ Python_Solutions/CodeWars/7kyu/Put_a_Letter_in_a_Column.py
@@ -0,0 +1,9 @@
+# Create a function that takes index [0, 8] and a character. It returns a string with columns. Put character in column marked with index.
+#
+# Ex.: index = 2, character = 'B'
+#
+# | | |B| | | | | | |
+#  0 1 2 3 4 5 6 7 8
+# Assume that argument index is integer [0, 8]. Assume that argument character is string with one character.
+#
+# FUNDAMENTALSSTRINGS
