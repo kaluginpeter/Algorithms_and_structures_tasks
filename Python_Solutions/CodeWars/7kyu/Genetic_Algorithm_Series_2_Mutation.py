526ce36b7c0daf5342909a49252f98abf5782e55
Peter Kalugin <streetworkout200412@gmail.com>
2024-01-02 18:43:58 +0300

    Added description "Genetic Algorithm Series - #2 Mutation"

---
+++ Python_Solutions/CodeWars/7kyu/Genetic_Algorithm_Series_2_Mutation.py
@@ -0,0 +1,17 @@
+# Mutation is a genetic operator used to maintain genetic diversity from one generation of a population of genetic algorithm chromosomes to the next.
+#
+# Mutation
+#
+# A mutation here may happen on zero or more positions in a chromosome. It is going to check every position and by a given probability it will decide if a mutation will occur.
+#
+# A mutation is the change from 0 to 1 or from 1 to 0.
+#
+# Note: Some tests are random. If you think your algorithm is correct but the result fails, trying again should work.
+#
+# See other katas from this series
+# Genetic Algorithm Series - #1 Generate
+# Genetic Algorithm Series - #2 Mutation
+# Genetic Algorithm Series - #3 Crossover
+# Genetic Algorithm Series - #4 Get population and fitnesses
+# Genetic Algorithm Series - #5 Roulette wheel selection
+# ALGORITHMSGENETIC ALGORITHMSSTRINGS
