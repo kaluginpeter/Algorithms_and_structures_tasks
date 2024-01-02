2c4de7fc9510bdbb5958895b88a174c8a6aaf125
Peter Kalugin <streetworkout200412@gmail.com>
2024-01-02 12:41:41 +0300

    Added solution "Convert all the cases!"

--- Python_Solutions/CodeWars/5kyu/Convert_all_the_cases.py
+++ Python_Solutions/CodeWars/5kyu/Convert_all_the_cases.py
@@ -21,3 +21,38 @@
 # (Any translations would be greatly appreciated!)
 #
 # STRINGSREGULAR EXPRESSIONSALGORITHMS
+# Solution
+def change_case(id, target):
+    if target not in {'kebab', 'camel', 'snake'}:
+        return None
+    if not id:
+        return ''
+    x = 0
+    for i in id:
+        if i.isupper():
+            x += 1
+    y, z = id.count('_'), id.count('-')
+    if (x > 0 and (y > 0 or z > 0)) or (y > 0 and (x > 0 or z > 0)) or (z > 0 and (x > 0 or y > 0)):
+        return None
+    word: str = ''
+    flag: bool = False
+    for i in id:
+        if not i.isupper() and i not in {'-', '_'}:
+            if flag:
+                word += i.upper()
+                flag = not flag
+            else:
+                word += i
+        elif i.isupper():
+            if target == 'camel': word += i
+            elif target == 'kebab': word += '-' + i.lower()
+            elif target == 'snake': word += '_' + i.lower()
+        elif i == '-':
+            if target == 'snake': word += '_'
+            elif target == 'kebab': word += i
+            elif target == 'camel': flag = True
+        elif i == '_':
+            if target == 'kebab': word += '-'
+            elif target == 'snake': word += i
+            elif target == 'camel': flag = True
+    return word
