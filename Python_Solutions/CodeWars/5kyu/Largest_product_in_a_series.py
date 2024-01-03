30170d88d15e78bbf87eb339bd3e07d1cb9c72c2
Peter Kalugin <streetworkout200412@gmail.com>
2024-01-03 13:58:08 +0300

    Added description "Largest product in a series"

---
+++ Python_Solutions/CodeWars/5kyu/Largest_product_in_a_series.py
@@ -0,0 +1,10 @@
+# Complete the greatestProduct method so that it'll find the greatest product of five consecutive digits in the given string of digits.
+#
+# For example:
+#
+# greatestProduct("123834539327238239583") // should return 3240
+# The input string always has more than five digits.
+#
+# Adapted from Project Euler.
+#
+# MATHEMATICSALGORITHMS
