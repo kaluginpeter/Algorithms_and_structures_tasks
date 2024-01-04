bc38bf3f4ca46bca59ab6c5e7941841c932780ba
Peter Kalugin <streetworkout200412@gmail.com>
2024-01-04 14:40:56 +0300

    Added description "Schrödinger's Boolean"

---
+++ Python_Solutions/CodeWars/6kyu/Schrödinger's_Boolean.py
@@ -0,0 +1,8 @@
+# Can a value be both True and False?
+#
+# Define omnibool so that it returns True for the following:
+#
+# omnibool == True and omnibool == False
+# If you enjoyed this kata, be sure to check out my other katas.
+#
+# LANGUAGE FEATURESMETAPROGRAMMINGPUZZLES
