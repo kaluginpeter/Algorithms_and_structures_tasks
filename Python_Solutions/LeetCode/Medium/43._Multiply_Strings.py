368bd37ed5d0024e74b4421b334e2fc8df14471f
Peter Kalugin <streetworkout200412@gmail.com>
2024-01-04 17:17:24 +0300

    Added solution "43. Multiply Strings"

--- Python_Solutions/LeetCode/Medium/43._Multiply_Strings.py
+++ Python_Solutions/LeetCode/Medium/43._Multiply_Strings.py
@@ -19,3 +19,23 @@
 # 1 <= num1.length, num2.length <= 200
 # num1 and num2 consist of digits only.
 # Both num1 and num2 do not contain any leading zero, except the number 0 itself.
+# Solution O(N) O(N)
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        if num1 == '0' or num2 == '0':
+            return '0'
+        d: dict = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4,
+        '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
+        r_d: dict = {0: '0', 1: '1', 2: '2', 3: '3', 4: '4',
+        5: '5', 6: '6', 7: '7', 8: '8', 9: '9'}
+        x1, x2 = 0, 0
+        for i in num1:
+            x1 = x1 * 10 + d[i]
+        for i in num2:
+            x2 = x2 * 10 + d[i]
+        x3 = x1 * x2
+        ans: str = ''
+        while x3:
+            ans += r_d[x3 % 10]
+            x3 //= 10
+        return ans[::-1]
