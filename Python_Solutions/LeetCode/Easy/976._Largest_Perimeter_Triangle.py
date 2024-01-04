27a76124de86ffcefe68c90a71308cb0b363fddf
Peter Kalugin <streetworkout200412@gmail.com>
2024-01-04 15:07:09 +0300

    Added solution "976. Largest Perimeter Triangle"

--- Python_Solutions/LeetCode/Easy/976._Largest_Perimeter_Triangle.py
+++ Python_Solutions/LeetCode/Easy/976._Largest_Perimeter_Triangle.py
@@ -22,3 +22,11 @@
 #
 # 3 <= nums.length <= 104
 # 1 <= nums[i] <= 106
+# Solution O(NlogN) O(1)
+class Solution:
+    def largestPerimeter(self, nums: List[int]) -> int:
+        nums.sort(reverse=True)
+        for i in range(2, len(nums)):
+            if nums[i] + nums[i-1] > nums[i-2]:
+                return nums[i-2] + nums[i-1] + nums[i]
+        return 0
