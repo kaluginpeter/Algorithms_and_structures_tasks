00aeaf62842033699d16efb20b551f13fb1de3db
Peter Kalugin <streetworkout200412@gmail.com>
2024-01-07 06:49:21 +0300

    Added solution "2894. Divisible and Non-divisible Sums Difference"

--- Python_Solutions/LeetCode/Easy/2894._Divisible-and_Non_divisible_Sums_Difference.py
+++ Python_Solutions/LeetCode/Easy/2894._Divisible-and_Non_divisible_Sums_Difference.py
@@ -37,3 +37,18 @@
 # Constraints:
 #
 # 1 <= n, m <= 1000
+# Solution 1 - Brute Force O(N) O(1)
+class Solution:
+    def differenceOfSums(self, n: int, m: int) -> int:
+        count: int = 0
+        for i in range(1, n + 1):
+            if i % m != 0:
+                count += i
+            elif i % m == 0:
+                count -= i
+        return count
+# Solution 2 Math O(1) O(1)
+class Solution:
+    def differenceOfSums(self, n: int, m: int) -> int:
+        k = n // m
+        return n * (n + 1) // 2 - (k + 1) * k * m
