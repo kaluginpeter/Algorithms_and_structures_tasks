170f6573272d1a88a9494a88938e8016c01286db
Peter Kalugin <streetworkout200412@gmail.com>
2024-01-09 18:15:26 +0300

    Added solution "Ninja vs Samurai: Attack + Block"

--- Python_Solutions/CodeWars/5kyu/Ninja_vs_Samurai_Attack_Block.py
+++ Python_Solutions/CodeWars/5kyu/Ninja_vs_Samurai_Attack_Block.py
@@ -15,3 +15,33 @@
 # Notice that health can't be below 0: once hit to 0 health, a warrior attribute deceased becomes true; if hit again, the zombie attribute becomes true too!
 #
 # DEBUGGING
+# Solution
+Position = {'high': 'h', 'low': 'l'}  # don't change this!
+
+
+class Warrior():
+    def __init__(self, name):
+        # each warrior should be created with a name and 100 health points
+        self.name = name
+        self.health = 100
+        # default guard is "", that is unguarded
+        self.block = ""
+        self.deceased = False
+        self.zombie = False
+
+    def attack(self, enemy, position):
+        damage: int = 0
+        # attacking high deals 10 damage, low 5
+        # 0 damage if the enemy blocks in the same position
+        if enemy.block != position: damage += 10 if position == Position['high'] else 5
+        # and even more damage if the enemy is not blocking at all
+        if enemy.block == "": damage += 5
+        enemy.set_health(enemy.health - damage)
+
+    def set_health(self, new_health):
+        # health cannot have negative values
+        self.health = max(0, new_health)
+        # if a warrior is set to 0 health he is dead
+        if self.health == 0:
+            if self.deceased: self.zombie = True
+            self.deceased = True
