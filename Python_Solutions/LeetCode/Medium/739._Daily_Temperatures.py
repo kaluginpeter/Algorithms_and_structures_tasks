3e066813c61033a77a1e99341d83923908240e4b
Peter Kalugin <streetworkout200412@gmail.com>
2024-01-09 19:46:52 +0300

    Added solution "739. Daily Temperatures"

--- Python_Solutions/LeetCode/Medium/739._Daily_Temperatures.py
+++ Python_Solutions/LeetCode/Medium/739._Daily_Temperatures.py
@@ -20,3 +20,14 @@
 #
 # 1 <= temperatures.length <= 105
 # 30 <= temperatures[i] <= 100
+# Solution Stack O(N) O(N)
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack: list = [0]
+        ans: list = [0] * len(temperatures)
+        for i in range(1, len(temperatures)):
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                ans[stack[-1]] = i - stack[-1]
+                stack.pop()
+            stack.append(i)
+        return ans
