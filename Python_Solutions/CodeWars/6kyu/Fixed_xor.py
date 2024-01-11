2c46f402176d864ba17439da05a0fea75cf23a95
Peter Kalugin <streetworkout200412@gmail.com>
2024-01-11 17:55:34 +0300

    Added solution "Fixed xor"

--- Python_Solutions/CodeWars/6kyu/Fixed_xor.py
+++ Python_Solutions/CodeWars/6kyu/Fixed_xor.py
@@ -17,3 +17,6 @@
 # fixed_xor("aadf", "bce2") == "163d"
 # fixed_xor("1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965") == "746865206b696420646f6e277420706c6179"
 # ALGORITHMSMATHEMATICSLOGICALGEBRABINARYCRYPTOGRAPHY
+# Solutuion
+def fixed_xor(a, b):
+    return ''.join(f'{int(x, 16) ^ int(y, 16):x}' for x, y in zip(a, b))
