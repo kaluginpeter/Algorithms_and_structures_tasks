09cd3d1b563bcb2129fb9c33ffac764b4d844ede
Peter Kalugin <streetworkout200412@gmail.com>
2024-01-12 06:29:22 +0300

    Added description "Are all elements equal? (Infinity version)"

---
+++ Python_Solutions/CodeWars/6kyu/Are_all_elements_equal_Infinity_version.py
@@ -0,0 +1,16 @@
+# Task
+# Create a function eq_all that determines if all elements of any iterable are equal; the iterable may be infinite. Return value is a bool.
+#
+# Examples
+# eq_all('aaa')   : True
+# eq_all('abc')   : False
+# eq_all('')      : True
+#
+# eq_all([0,0,0]) : True
+# eq_all([0,1,2]) : False
+# eq_all([])      : True
+# Notes
+# For the function result to be True, the iterable must be finite; False, however, can result from an element finitely far from the left end. There will be no tests with infinite series of equal elements.
+# Elements will be primitive values.
+#
+# FUNDAMENTALS
