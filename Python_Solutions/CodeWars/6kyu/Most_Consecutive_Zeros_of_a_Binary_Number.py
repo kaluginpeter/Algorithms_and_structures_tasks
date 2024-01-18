151cb69a682e6d3253ccd81f0f29257b89262692
Peter Kalugin <streetworkout200412@gmail.com>
2024-01-18 18:01:52 +0300

    Added solution "Most Consecutive Zeros of a Binary Number"

--- Python_Solutions/CodeWars/6kyu/Most_Consecutive_Zeros_of_a_Binary_Number.py
+++ Python_Solutions/CodeWars/6kyu/Most_Consecutive_Zeros_of_a_Binary_Number.py
@@ -21,3 +21,18 @@
 # Happy coding!
 #
 # FUNDAMENTALSMATHEMATICSSTRINGS
+# Solution
+def max_consec_zeros(s):
+    words: dict = {0: "Zero", 1: "One", 2: "Two", 3: "Three",
+                  4: 'Four', 5: 'Five', 6: 'Six', 7: 'Seven',
+                  8: 'Eight', 9: 'Nine', 10: 'Ten', 11: 'Eleven',
+                  12: 'Twelve', 13: 'Thirteen'}
+    top: int = 0
+    seq: int = 0
+    for i in bin(int(s))[2:]:
+        if i == '0':
+            seq += 1
+            top = max(top, seq)
+        else:
+            seq = 0
+    return words[top]
