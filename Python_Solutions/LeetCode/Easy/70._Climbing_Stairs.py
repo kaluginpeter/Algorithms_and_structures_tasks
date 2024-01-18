ce54ea7a92eac8c754dc7a93186914831e84d046
Peter Kalugin <streetworkout200412@gmail.com>
2024-01-18 06:39:02 +0300

    Added solution "70. Climbing Stairs"

--- Python_Solutions/LeetCode/Easy/70._Climbing_Stairs.py
+++ Python_Solutions/LeetCode/Easy/70._Climbing_Stairs.py
@@ -30,3 +30,13 @@ class Solution:
             new = f[0] + f[1]
             f[0], f[1] = f[1], new
         return f[1]
+
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1: return 1
+        f = [1, 2]
+        for i in range(2, n):
+            new: int = f[0] + f[1]
+            f[0], f[1] = f[1], new
+        return f[1]
