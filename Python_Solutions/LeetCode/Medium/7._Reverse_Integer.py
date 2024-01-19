e0fc77183aca61deaa5de3bb25f2355afb985251
Peter Kalugin <streetworkout200412@gmail.com>
2024-01-19 06:37:51 +0300

    Added solution "7. Reverse Integer"

--- Python_Solutions/LeetCode/Medium/7._Reverse_Integer.py
+++ Python_Solutions/LeetCode/Medium/7._Reverse_Integer.py
@@ -21,3 +21,16 @@
 # Constraints:
 #
 # -231 <= x <= 231 - 1
+# Solution O(N) O(1)
+class Solution:
+    def reverse(self, x: int) -> int:
+        flag: bool = x < 0
+        if flag:
+            x *= -1
+        ans: int = 0
+        while x:
+            ans = ans * 10 + (x % 10)
+            x //= 10
+        if ans < -2**31 or ans > 2**31-1:
+            return 0
+        return -ans if flag else ans
