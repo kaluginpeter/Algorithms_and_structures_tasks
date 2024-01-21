a02b687333a3135b59444cbdfcefeb0d2437021e
Peter Kalugin <streetworkout200412@gmail.com>
2024-01-21 06:38:07 +0300

    Added solution "2545. Sort the Students by Their Kth Score"

--- Python_Solutions/LeetCode/Medium/2545._Sort_the_Students_by_Their_Kth_Score.py
+++ Python_Solutions/LeetCode/Medium/2545._Sort_the_Students_by_Their_Kth_Score.py
@@ -33,3 +33,11 @@
 # 1 <= score[i][j] <= 105
 # score consists of distinct integers.
 # 0 <= k < n
+# Solution BUbble Sort O(N**2) O(1)
+class Solution:
+    def sortTheStudents(self, score: List[List[int]], k: int) -> List[List[int]]:
+        for i in range(len(score)):
+            for j in range(len(score) - i - 1):
+                if score[j][k] < score[j + 1][k]:
+                    score[j], score[j + 1] = score[j + 1], score[j]
+        return score
