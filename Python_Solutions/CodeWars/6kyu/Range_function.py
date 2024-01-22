2cec9e3ff849337a857f4ccbbdcc3041004af9da
Peter Kalugin <streetworkout200412@gmail.com>
2024-01-22 06:43:58 +0300

    Added solution "Range function"

--- Python_Solutions/CodeWars/6kyu/Range_function.py
+++ Python_Solutions/CodeWars/6kyu/Range_function.py
@@ -6,3 +6,10 @@
 # range(3, 7) --> 3,4,5,6,7
 # range(2, 3, 15) --> 2,5,8,11,14
 # FUNDAMENTALS
+# Solution
+def range_function(*args):
+    if len(args) == 1:
+        return range(1, args[0] + 1)
+    elif len(args) == 2:
+        return range(args[0], args[1] + 1)
+    return range(args[0], args[2] + 1, args[1])
