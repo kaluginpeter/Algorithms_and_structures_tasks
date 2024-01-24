9178517ff62c0e350f5373fe8d02bbc6de2d0a94
Peter Kalugin <streetworkout200412@gmail.com>
2024-01-24 06:40:38 +0300

    Added solution "telephone words"

--- Python_Solutions/CodeWars/6kyu/telephone_words.py
+++ Python_Solutions/CodeWars/6kyu/telephone_words.py
@@ -7,3 +7,14 @@
 # Write a function that takes four digits of a phone number, and returns a list of all of the words that can be written with that number. (You should return all permutations, not only English words.)
 #
 # STRINGSARRAYSALGORITHMS
+# Solution
+def telephone_words(digit_string):
+    letters: list = [['0'], ['1'], ['ABC'], ['DEF'], ['GHI'],
+                    ['JKL'], ['MNO'], ['PQRS'], ['TUV'], ['WXYZ']]
+    ans: list = []
+    for first in letters[int(digit_string[0])][0]:
+        for second in letters[int(digit_string[1])][0]:
+            for third in letters[int(digit_string[2])][0]:
+                for fourth in letters[int(digit_string[3])][0]:
+                    ans.append(first + second + third + fourth)
+    return ans
