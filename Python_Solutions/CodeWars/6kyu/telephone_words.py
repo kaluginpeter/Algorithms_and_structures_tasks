e705697c08495236cb10a5a0e922925f06efd244
Peter Kalugin <streetworkout200412@gmail.com>
2024-01-24 06:40:23 +0300

    Added description "telephone words"

---
+++ Python_Solutions/CodeWars/6kyu/telephone_words.py
@@ -0,0 +1,9 @@
+# Businesses use keypad letters in creative ways to spell out a phone number and make it more memorable. Example: http://en.wikipedia.org/wiki/File:Telephone-keypad2.svg
+#
+# Create a mapping for your dialer as given in the above link. Constraints:
+#
+# letters are all uppercase
+# digits 0, 1 are mapped to 0, 1 respectively
+# Write a function that takes four digits of a phone number, and returns a list of all of the words that can be written with that number. (You should return all permutations, not only English words.)
+#
+# STRINGSARRAYSALGORITHMS
