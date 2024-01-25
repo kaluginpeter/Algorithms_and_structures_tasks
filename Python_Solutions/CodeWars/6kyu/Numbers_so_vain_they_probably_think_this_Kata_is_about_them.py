38e9aa52df5a5207a525443302ca77483fbd4661
Peter Kalugin <streetworkout200412@gmail.com>
2024-01-25 17:42:29 +0300

    Added solution "Numbers so vain, they probably think this Kata is about them."

--- Python_Solutions/CodeWars/6kyu/Numbers_so_vain_they_probably_think_this_Kata_is_about_them.py
+++ Python_Solutions/CodeWars/6kyu/Numbers_so_vain_they_probably_think_this_Kata_is_about_them.py
@@ -5,3 +5,14 @@
 # For more information about narcissistic numbers (and believe me, they love it when you read about them) follow this link: https://en.wikipedia.org/wiki/Narcissistic_number
 #
 # ALGORITHMSMATHEMATICS
+# Solution
+def is_narcissistic(*args):
+    for n in args:
+        if not str(n).isdigit(): return False
+        b: int = len(str(n))
+        top: int = 0
+        for i in str(n):
+            top += int(i) ** b
+        if top != int(n):
+            return False
+    return True
