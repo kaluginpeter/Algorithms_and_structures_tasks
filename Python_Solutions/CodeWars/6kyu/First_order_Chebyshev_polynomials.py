ffc2da6646a1338094327f5abbe3948221696c3e
Peter Kalugin <streetworkout200412@gmail.com>
2024-01-29 18:04:53 +0300

    Added description "First order Chebyshev polynomials"

---
+++ Python_Solutions/CodeWars/6kyu/First_order_Chebyshev_polynomials.py
@@ -0,0 +1,10 @@
+# First order Chebyshew polynomials are defined by:
+#
+# T{0}( v ) = 1
+# T{1}( v ) = v
+# T{n+1}( v ) = 2 * v * T{n}( v ) - T{n-1}( v )
+# Calculate T{n}( v ) for given values of v and n.
+#
+# You don't have to round your results but you should expect large values.
+#
+# ALGORITHMS
