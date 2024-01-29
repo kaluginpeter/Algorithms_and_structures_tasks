bd3660ba2192fa8016dc8ff2301ed765eedb7725
Peter Kalugin <streetworkout200412@gmail.com>
2024-01-29 18:05:26 +0300

    Added solution "First order Chebyshev polynomials"

--- Python_Solutions/CodeWars/6kyu/First_order_Chebyshev_polynomials.py
+++ Python_Solutions/CodeWars/6kyu/First_order_Chebyshev_polynomials.py
@@ -8,3 +8,12 @@
 # You don't have to round your results but you should expect large values.
 #
 # ALGORITHMS
+# Solution
+from functools import lru_cache
+@lru_cache(maxsize=None)
+def chebyshev(n,v):
+    if n == 0:
+        return 1
+    if n == 1:
+        return v
+    return 2 * v * chebyshev(n-1, v) - chebyshev(n-2, v)
