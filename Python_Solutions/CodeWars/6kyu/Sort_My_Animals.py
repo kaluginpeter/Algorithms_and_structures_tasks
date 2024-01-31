eb548fe64cbb657e6e968425fca71242974c9252
Peter Kalugin <streetworkout200412@gmail.com>
2024-01-31 17:14:51 +0300

    Added description "Sort My Animals"

---
+++ Python_Solutions/CodeWars/6kyu/Sort_My_Animals.py
@@ -0,0 +1,11 @@
+# Consider the following class:
+#
+# class Animal:
+#     def __init__(self, name, number_of_legs):
+#         self.name = name
+#         self.number_of_legs = number_of_legs
+# Write a method that accepts a list of objects of type Animal, and returns a new list. The new list should be a copy of the original list, sorted first by the animal's number of legs, and then by its name.
+#
+# If an empty list is passed in, it should return an empty list back.
+#
+# LISTSSORTINGFUNDAMENTALS
