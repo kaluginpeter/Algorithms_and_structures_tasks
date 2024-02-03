e5f2c920221bb8e31f329f11bc83df6c25b55578
Peter Kalugin <streetworkout200412@gmail.com>
2024-02-03 09:32:51 +0300

    Added solution "Valid string"

--- Python_Solutions/CodeWars/6kyu/Valid_string.py
+++ Python_Solutions/CodeWars/6kyu/Valid_string.py
@@ -12,3 +12,29 @@
 # One word from the dictionary can be used several times.
 #
 # STRINGSALGORITHMS
+# Solution
+def valid_word(seq, word):
+    matches: list = []
+    for i in seq:
+        if word.startswith(i):
+            matches.append(i)
+            if matches[-1] == word:
+                return True
+    count: int = 0
+    while True:
+        top: list = []
+        flag: bool = False
+        for char in matches:
+            for wrd in seq:
+                if word.startswith(char + wrd):
+                    top.append(char + wrd)
+                    flag = not flag
+                    if top[-1] == word:
+                        return True
+        matches = top
+        if not flag:
+            count += 1
+        else:
+            count = 0
+        if count == 10:
+            return False
