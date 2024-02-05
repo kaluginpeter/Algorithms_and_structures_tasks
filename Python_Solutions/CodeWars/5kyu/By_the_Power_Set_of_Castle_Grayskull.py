4bb4119e30bc95e8b7331d9b2dd4bf9c4c26d941
Peter Kalugin <streetworkout200412@gmail.com>
2024-02-05 06:23:47 +0300

    Added description "By the Power Set of Castle Grayskull"

---
+++ Python_Solutions/CodeWars/5kyu/By_the_Power_Set_of_Castle_Grayskull.py
@@ -0,0 +1,67 @@
+# Write a function that returns all of the sublists of a list/array.
+#
+# Example:
+#
+# �
+# �
+# �
+# �
+# �
+# (
+# [
+# 1
+# ,
+# 2
+# ,
+# 3
+# ]
+# )
+# ;
+# =
+# >
+# [
+# [
+# ]
+# ,
+# [
+# 1
+# ]
+# ,
+# [
+# 2
+# ]
+# ,
+# [
+# 1
+# ,
+# 2
+# ]
+# ,
+# [
+# 3
+# ]
+# ,
+# [
+# 1
+# ,
+# 3
+# ]
+# ,
+# [
+# 2
+# ,
+# 3
+# ]
+# ,
+# [
+# 1
+# ,
+# 2
+# ,
+# 3
+# ]
+# ]
+# power([1,2,3]);=>[[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+# For more details regarding this, see the power set entry in wikipedia.
+#
+# MATHEMATICSALGORITHMS
