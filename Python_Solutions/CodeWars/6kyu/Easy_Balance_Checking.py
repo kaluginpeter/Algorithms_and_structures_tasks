ed189ad4b0d19597806bb6eb6ea101abb58e9be9
Peter Kalugin <streetworkout200412@gmail.com>
2024-02-07 17:52:57 +0300

    Added solution "Easy Balance Checking"

--- Python_Solutions/CodeWars/6kyu/Easy_Balance_Checking.py
+++ Python_Solutions/CodeWars/6kyu/Easy_Balance_Checking.py
@@ -29,3 +29,28 @@
 # The line separator of results may depend on the language \n or \r\n. See examples in the "Sample tests".
 # R language: Don't use R's base function "mean()" that could give results slightly different from expected ones.
 # FUNDAMENTALSSTRINGS
+# Solution
+import re
+def balance(book):
+    calc_lines = []
+    total_expense =0
+    book = re.sub("[^\w\d\.\s\n]", "", book)
+    book = re.sub("\s{2,}", "\n", book)
+    lines = book.splitlines()
+    budget = float(lines.pop(0))
+    remains = budget
+    for line in lines:
+        line = line.split()
+        balance  = float(line[-1])
+        total_expense += balance
+        remains -= balance
+        calc_lines.append(" ".join(
+            line[:-1]) \
+            + " {:.2f}".format(balance) \
+            + " Balance {:.2f}".format(remains))
+    return (
+        "Original Balance: {:.2f}\r\n".format(budget)
+        + '\r\n'.join(calc_lines)
+        + "\r\n"
+        + "Total expense  {:.2f}\r\n".format(total_expense)
+        + "Average expense  {:.2f}".format(total_expense / len(lines)))
