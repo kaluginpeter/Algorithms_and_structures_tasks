7ccddf1b242b5cc18ea66fc86a4217bc1844fb3a
Peter Kalugin <streetworkout200412@gmail.com>
2024-02-09 16:43:30 +0300

    Adding some functions on dynamic array python

--- Python_Solutions/Data Structures/arrays.py
+++ Python_Solutions/Data Structures/arrays.py
@@ -3,18 +3,27 @@
 # Design a Dynamic array class that support following operations:
 # DynamicArray(capacity: int) - will initialize an empty array with capacity
 #     of capacity, where capacity > 0
-# .get(i: int) -> int - will return the element at index i.
+# .get(i: int) -> object - will return the element at index i.
 #     if you give invalid index, method through exception ValueError
-# .set(i: int, n: int) -> None - will set element at index i to n.
+# .set(i: int, n: object) -> None - will set element at index i to n.
 #     if you take a incorrect index, method through ValueError exception.
-# .pushback(n: int) -> None - will push element to the end of array.
+# .add(n: object) -> None - will push element to the end of array.
 #     And it's dynamic array implementation, so if size of array fill be full,
 #     capacity will be automaticly doubled.
-# .popback() -> int - will pop end return element at the end of array.
+# .popback() -> object - will pop end return element at the end of array.
 #     If array size will be empty, method thorough the exception ValueEror
 # .resize() -> None - will double capacity of array
 # .getSize() -> int - will return the size of array
 # .getCapacity() -> int: will return the capacity of array
+# .clear() -> None - will clear all data in array
+# .is_empty() -> bool - return True if array is not emtpy othewise, return False
+# .removeAt(i: int) -> obj - will remove elements by given index and return it. If given index invalid
+#     method will raise exception
+# .remove(obj: object) -> bool - will delete given object and return True if object exist, otherwise return False
+# .indexOf(obj: object) -> int - will return index of given number if object exist, otherwise return -1
+# .contains(obj: object) -> bool - will return boolean True if array have given object otherwise False
+# .__iter__() - by this method we can iterate through arra
+# .__next__() - by this method we can get next element of array
 class DynamicArray:
 
     def __init__(self, capacity: int):
@@ -22,24 +31,24 @@ class DynamicArray:
         self.arr: list = [0] * capacity
         self.size: int = 0
 
-    def get(self, i: int) -> int:
+    def get(self, i: int) -> object:
         if i < 0 or i > self.size:
             raise ValueError()
         return self.arr[i]
 
-    def set(self, i: int, n: int) -> None:
+    def set(self, i: int, n: object) -> None:
         if i < 0 or i > self.size:
             raise ValueError()
         self.arr[i] = n
 
-    def pushback(self, n: int) -> None:
+    def add(self, n: object) -> None:
         if self.size + 1 > self.capacity:
             self.resize()
         self.arr[self.size] = n
         self.size += 1
 
-    def popback(self) -> int:
-        if self.size == 0:
+    def popback(self) -> object:
+        if self.is_empty():
             raise ValueError()
         self.size -= 1
         return self.arr[self.size]
@@ -56,3 +65,54 @@ class DynamicArray:
 
     def getCapacity(self) -> int:
         return self.capacity
+
+    def clear(self) -> None:
+        for i in range(self.size):
+            self.arr[i] = None
+        self.size = 0
+
+    def is_empty(self) -> bool:
+        return self.size > 0
+
+    def removeAt(self, i: int) -> object:
+        if i > self.size or i < 0:
+            raise ValueError()
+        data = self.arr[i]
+        new_arr: list = [0] * (self.size - 1)
+        index_of_new_arr: int = 0
+        for j in range(self.size):
+            if j != i:
+                new_arr[index_of_new_arr] = self.arr[j]
+        self.arr = new_arr
+        self.size -= 1
+        self.capacity = self.size
+        self.count = 0
+        return data
+
+    def remove(self, n: object) -> bool:
+        for i in range(self.size):
+            if self.arr[i] == n:
+                self.removeAt(i)
+                return True
+        return False
+
+    def indexOf(self, obj: object) -> int:
+        for i in range(self.size):
+            if self.arr[i] == obj:
+                return i
+        return -1
+
+    def contains(self, obj: object) -> bool:
+        return self.indexOf(obj) != -1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.count < self.size:
+            element = self.arr[self.count]
+            self.count += 1
+            return element
+        else:
+            self.count = 0
+            raise StopIteration
