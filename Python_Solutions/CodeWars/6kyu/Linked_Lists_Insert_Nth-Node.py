7019efbcd3c086482bad807c6ff099f40c7480c9
Peter Kalugin <streetworkout200412@gmail.com>
2024-02-10 15:04:06 +0300

    Added description "Linked Lists - Insert Nth Node"

---
+++ Python_Solutions/CodeWars/6kyu/Linked_Lists_Insert_Nth-Node.py
@@ -0,0 +1,36 @@
+# Linked Lists - Insert Nth
+#
+# Implement an InsertNth() function (insert_nth() in PHP) which can insert a new node at any index within a list.
+#
+# InsertNth() is a more general version of the Push() function that we implemented in the first kata listed below. Given a list, an index 'n' in the range 0..length, and a data element, add a new node to the list so that it has the given index. InsertNth() should return the head of the list.
+#
+# insertNth(1 -> 2 -> 3 -> null, 0, 7) === 7 -> 1 -> 2 -> 3 -> null)
+# insertNth(1 -> 2 -> 3 -> null, 1, 7) === 1 -> 7 -> 2 -> 3 -> null)
+# insertNth(1 -> 2 -> 3 -> null, 3, 7) === 1 -> 2 -> 3 -> 7 -> null)
+# You must throw/raise an exception (ArgumentOutOfRangeException in C#, InvalidArgumentException in PHP) if the index is too large.
+#
+# The push() and buildOneTwoThree() (build_one_two_three() in PHP) functions do not need to be redefined. The Node class is also preloaded for you in PHP.
+#
+# Related Kata in order of expected completion (increasing difficulty):
+#  Linked Lists - Push & BuildOneTwoThree
+#  Linked Lists - Length & Count
+#  Linked Lists - Get Nth Node
+# Linked Lists - Insert Nth Node
+# Linked Lists - Sorted Insert
+# Linked Lists - Insert Sort
+# Linked Lists - Append
+# Linked Lists - Remove Duplicates
+# Linked Lists - Move Node
+# Linked Lists - Move Node In-place
+# Linked Lists - Alternating Split
+# Linked Lists - Front Back Split
+# Linked Lists - Shuffle Merge
+# Linked Lists - Sorted Merge
+# Linked Lists - Merge Sort
+# Linked Lists - Sorted Intersect
+# Linked Lists - Iterative Reverse
+# Linked Lists - Recursive Reverse
+#
+# Inspired by Stanford Professor Nick Parlante's excellent Linked List teachings.
+#
+# LINKED LISTSDATA STRUCTURESFUNDAMENTALS
