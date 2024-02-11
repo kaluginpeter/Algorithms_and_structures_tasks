51eaaab8459a3833e3da968d0de6b0d43a0e5b1d
Peter Kalugin <streetworkout200412@gmail.com>
2024-02-11 10:58:11 +0300

    Added description "Rotate an array matrix"

---
+++ Python_Solutions/CodeWars/5kyu/Rotate_an_array_matrix.py
@@ -0,0 +1,27 @@
+# Write a function that rotates a two-dimensional array (a matrix) either clockwise or anti-clockwise by 90 degrees, and returns the rotated array.
+#
+# The function accepts two parameters: a matrix, and a string specifying the direction or rotation. The direction will be either "clockwise" or "counter-clockwise".
+#
+# Examples
+# For matrix:
+#
+# [
+#   [1, 2, 3],
+#   [4, 5, 6],
+#   [7, 8, 9]
+# ]
+# Clockwise rotation:
+#
+# [
+#   [7, 4, 1],
+#   [8, 5, 2],
+#   [9, 6, 3]
+# ]
+# Counter-clockwise rotation:
+#
+# [
+#   [3, 6, 9],
+#   [2, 5, 8],
+#   [1, 4, 7]
+# ]
+# MATRIXARRAYSALGORITHMS
