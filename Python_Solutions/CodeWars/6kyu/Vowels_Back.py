92370ee353ad8ae816485b63c1d19d8374d250f3
Peter Kalugin <streetworkout200412@gmail.com>
2024-02-15 18:17:08 +0300

    Added description "Vowels Back"

---
+++ Python_Solutions/CodeWars/6kyu/Vowels_Back.py
@@ -0,0 +1,15 @@
+# You need to play around with the provided string (s).
+#
+# Move consonants forward 9 places through the alphabet. If they pass 'z', start again at 'a'.
+#
+# Move vowels back 5 places through the alphabet. If they pass 'a', start again at 'z'. For our Polish friends this kata does not count 'y' as a vowel.
+#
+# Exceptions:
+#
+# If the character is 'c' or 'o', move it back 1 place. For 'd' move it back 3, and for 'e', move it back 4.
+#
+# If a moved letter becomes 'c', 'o', 'd' or 'e', revert it back to it's original value.
+#
+# Provided string will always be lower case, won't be empty and will have no special characters.
+#
+# FUNDAMENTALSSTRINGSARRAYSALGORITHMS
