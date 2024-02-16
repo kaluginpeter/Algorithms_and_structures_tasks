26fb745c7142d4d0e1dfcfd21ef7b7d70b1ac6a8
Peter Kalugin <streetworkout200412@gmail.com>
2024-02-16 22:55:26 +0300

    Added solution "705. Design HashSet"

--- Python_Solutions/LeetCode/Easy/705._Design_HashSet.py
+++ Python_Solutions/LeetCode/Easy/705._Design_HashSet.py
@@ -31,3 +31,58 @@
 #
 # 0 <= key <= 106
 # At most 104 calls will be made to add, remove, and contains.
+# Solution Single Linked List
+class Node:
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+
+
+class MyHashSet:
+
+    def __init__(self):
+        self.head = None
+
+    def add(self, key: int) -> None:
+        if self.contains(key):
+            return
+        if not self.head:
+            self.head = Node(val=key)
+            return
+        tmp = self.head
+        while tmp.next:
+            tmp = tmp.next
+        tmp.next = Node(val=key)
+        return
+
+    def remove(self, key: int) -> None:
+        if not self.contains(key):
+            return
+        if not self.head.next:
+            self.head = None
+            return
+        if self.head.val == key:
+            self.head = self.head.next
+            return
+        tmp = self.head
+        while tmp.val != key:
+            tmp = tmp.next
+        prev_tmp = self.head
+        while prev_tmp.next.val != tmp.val:
+            prev_tmp = prev_tmp.next
+        prev_tmp.next = tmp.next
+
+    def contains(self, key: int) -> bool:
+        tmp = self.head
+        while tmp:
+            if tmp.val == key:
+                return True
+            tmp = tmp.next
+        return False
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
