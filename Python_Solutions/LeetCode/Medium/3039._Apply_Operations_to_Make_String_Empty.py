4d73051f7f7e93099bdd0bc00df77838df6ea72c
Peter Kalugin <streetworkout200412@gmail.com>
2024-02-18 08:19:48 +0300

    Added solution "3039. Apply Operations to Make String Empty"

--- Python_Solutions/LeetCode/Medium/3039._Apply_Operations_to_Make_String_Empty.py
+++ Python_Solutions/LeetCode/Medium/3039._Apply_Operations_to_Make_String_Empty.py
@@ -30,3 +30,35 @@
 #
 # 1 <= s.length <= 5 * 105
 # s consists only of lowercase English letters.
+# Initial solution O(NlogN) O(N)
+class Solution:
+    def lastNonEmptyString(self, s: str) -> str:
+        eng_al: str = 'abcdefghijklmnopqrstuvwxyz'
+        ht: dict = dict()
+        for i in range(len(s)):
+            if s[i] in ht:
+                ht[s[i]] = [ht[s[i]][0] + 1, i]
+            else:
+                ht[s[i]] = [1, i]
+        mx = max(ht, key=lambda x: ht[x][0])
+        score = ht[mx][0]
+        ans: list = [[mx, ht[mx][0], ht[mx][1]]]
+        del ht[mx]
+        for i in ht:
+            if ht[i][0] == score:
+                ans.append([i, ht[i][0], ht[i][1]])
+        ans.sort(key=lambda x: x[2])
+        return ''.join(i[0] for i in ans)
+# Solution HashTable O(N) O(N)
+class Solution:
+    def lastNonEmptyString(self, s: str) -> str:
+        ht: dict = dict()
+        for i in s:
+            ht[i] = ht.get(i, 0) + 1
+        mx: int = max(ht.values())
+        ans: list = list()
+        for i in range(len(s) - 1, -1, -1):
+            if ht[s[i]] == mx:
+                ans.append(s[i])
+                ht[s[i]] = -1
+        return ''.join(ans[::-1])
