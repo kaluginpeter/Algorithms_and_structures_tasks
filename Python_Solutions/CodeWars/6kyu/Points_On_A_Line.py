8372e1380769511154696bd82aa257b04dcc1546
Peter Kalugin <streetworkout200412@gmail.com>
2024-02-21 18:03:53 +0300

    Added solution "Points On A Line"

--- Python_Solutions/CodeWars/6kyu/Points_On_A_Line.py
+++ Python_Solutions/CodeWars/6kyu/Points_On_A_Line.py
@@ -3,3 +3,21 @@
 # on_line(((1,2), (7,4), (22,9)) == True
 # on_line(((1,2), (-3,-14), (22,9))) == False
 # ARRAYSGEOMETRYFUNDAMENTALS
+# Solution
+def on_line(p1):
+    if len(p1) < 3:
+        return True
+    step = None
+    for i in range(1, len(p1)):
+        x: int = p1[i-1][0] - p1[i][0]
+        y: int = p1[i-1][1] - p1[i][1]
+        if y == 0:
+            top: int = 0
+        else:
+            top: int = x / y
+        if not step:
+            step = top
+        else:
+            if top != step:
+                return False
+    return True
