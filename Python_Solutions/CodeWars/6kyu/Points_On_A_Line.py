62def7ff04cec70ae2cbb0fbf9cc319624a05c70
Peter Kalugin <streetworkout200412@gmail.com>
2024-02-21 18:03:33 +0300

    Added description "Points On A Line"

---
+++ Python_Solutions/CodeWars/6kyu/Points_On_A_Line.py
@@ -0,0 +1,5 @@
+# Given some points (cartesian coordinates), return true if all of them lie on a line. Treat both an empty set and a single point as a line.
+#
+# on_line(((1,2), (7,4), (22,9)) == True
+# on_line(((1,2), (-3,-14), (22,9))) == False
+# ARRAYSGEOMETRYFUNDAMENTALS
