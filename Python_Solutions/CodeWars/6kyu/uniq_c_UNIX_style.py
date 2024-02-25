7fb6bf5717626db7eb4e073173f0c431ceeca5c3
Peter Kalugin <streetworkout200412@gmail.com>
2024-02-25 06:39:43 +0300

    Added description "uniq -c (UNIX style)"

---
+++ Python_Solutions/CodeWars/6kyu/uniq_c_UNIX_style.py
@@ -0,0 +1,7 @@
+# Implement a function which behaves like the 'uniq -c' command in UNIX.
+#
+# It takes as input a sequence and returns a sequence in which all duplicate elements following each other have been reduced to one instance together with the number of times a duplicate elements occurred in the original array.
+#
+# Example:
+# ['a','a','b','b','c','a','b','c'] --> [('a',2),('b',2),('c',1),('a',1),('b',1),('c',1)]
+# ARRAYSALGORITHMS
