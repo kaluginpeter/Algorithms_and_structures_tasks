c89ea9d54e8f0452843be12ee509986f77f83477
Peter Kalugin <streetworkout200412@gmail.com>
2024-02-25 18:12:28 +0300

    Added solution "155. Min Stack"

--- Python_Solutions/LeetCode/Medium/155._Min_Stack.py
+++ Python_Solutions/LeetCode/Medium/155._Min_Stack.py
@@ -35,3 +35,64 @@
 # -231 <= val <= 231 - 1
 # Methods pop, top and getMin operations will always be called on non-empty stacks.
 # At most 3 * 104 calls will be made to push, pop, top, and getMin.
+# Complexity
+# Time complexity: All opertaions is O(1), but technically .pop() can made up to O(N) when we delete min element in a stack
+#
+# Space complexity: O(N)
+#
+# Code
+class MinStack:
+
+    def __init__(self):
+        self.stack: list = list()
+        self.mn_val: int = float('inf')
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        self.mn_val = min(val, self.mn_val)
+
+    def pop(self) -> None:
+        x: int = self.stack.pop()
+        if x == self.mn_val:
+            self.mn_val = float('inf')
+            for i in self.stack:
+                self.mn_val = min(self.mn_val, i)
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.mn_val
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+# Complexity
+# Time complexity: All opertaions is O(1)
+#
+# Space complexity: O(N), but in solution above we use 2x small memory space
+#
+# Code
+class MinStack:
+
+    def __init__(self):
+        self.stack: list = list()
+
+    def push(self, val: int) -> None:
+        if not self.stack:
+            self.stack.append((val, val))
+        else:
+            self.stack.append((val, min(val, self.stack[-1][-1])))
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1][0]
+
+    def getMin(self) -> int:
+        return self.stack[-1][-1]
