850f84faee28c01c51a18813103bb76f6b70df70
Peter Kalugin <streetworkout200412@gmail.com>
2024-02-27 06:42:26 +0300

    Added solution "Name to Matrix"

--- Python_Solutions/CodeWars/6kyu/Name_to_Matrix.py
+++ Python_Solutions/CodeWars/6kyu/Name_to_Matrix.py
@@ -12,3 +12,22 @@
 #               ["n", "k", "."],
 #               [".", ".", "."] ]
 # STRINGSARRAYSALGORITHMSMATRIX
+# Solution
+def matrixfy(st):
+    if not st:
+        return 'name must be at least one letter'
+    n: int = 1
+    while n * n < len(st):
+        n += 1
+    ans: list = list()
+    indx: int = 0
+    for i in range(n):
+        top: list = list()
+        for j in range(n):
+            if indx == len(st):
+                top.append('.')
+            else:
+                top.append(st[indx])
+                indx += 1
+        ans.append(top)
+    return ans
