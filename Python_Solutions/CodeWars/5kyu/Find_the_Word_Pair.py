8c015dc6fd5110a44da0bf017945f41c3d1753f5
Peter Kalugin <streetworkout200412@gmail.com>
2024-02-28 06:48:27 +0300

    Added description "Find the Word Pair!"

---
+++ Python_Solutions/CodeWars/5kyu/Find_the_Word_Pair.py
@@ -0,0 +1,15 @@
+# Given an array of words and a target compound word, your objective is to find the two words which combine into the target word, returning both words in the order they appear in the array, and their respective indices in the order they combine to form the target word. Words in the array you are given may repeat, but there will only be one unique pair that makes the target compound word. If there is no match found, return null/nil/None.
+#
+# Note: Some arrays will be very long and may include duplicates, so keep an eye on efficiency.
+#
+# Examples:
+#
+# fn(['super','bow','bowl','tar','get','book','let'], "superbowl")      =>   ['super','bowl',   [0,2]]
+# fn(['bow','crystal','organic','ally','rain','line'], "crystalline")   =>   ['crystal','line', [1,5]]
+# fn(['bow','crystal','organic','ally','rain','line'], "rainbow")       =>   ['bow','rain',     [4,0]]
+# fn(['bow','crystal','organic','ally','rain','line'], "organically")   =>   ['organic','ally', [2,3]]
+# fn(['top','main','tree','ally','fin','line'], "mainline")             =>   ['main','line',    [1,5]]
+# fn(['top','main','tree','ally','fin','line'], "treetop")              =>   ['top','tree',     [2,0]]
+# Have fun, and if you enjoyed it don't forget to rank & upvote! :)
+#
+# ARRAYSPERFORMANCEALGORITHMS
