ca348f03c685dfec9c546b7ce3bb2afec2ba6ffc
Peter Kalugin <streetworkout200412@gmail.com>
2024-03-02 09:38:30 +0300

    Added description "What The Biggest Search Keys?"

---
+++ Python_Solutions/CodeWars/6kyu/What_The_Biggest_Search_Keys.py
@@ -0,0 +1,13 @@
+# You're a programmer in a SEO company. The SEO specialist of your company gets the list of all project keywords everyday, then he looks for the longest keys to analyze them.
+#
+# You will get the list with keywords and must write a simple function that returns the biggest search keywords and sorts them in lexicographical order.
+#
+# For instance you might get:
+#
+# 'key1', 'key2', 'key3', 'key n', 'bigkey2', 'bigkey1'
+# And your function should return:
+#
+# "'bigkey1', 'bigkey2'"
+# Don't forget to rate this kata! Thanks :)
+#
+# FUNDAMENTALSARRAYS
