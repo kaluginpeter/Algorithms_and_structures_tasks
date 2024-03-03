1fe0405fdd701e2ec1d1fdc13e7e849ae9af3c8b
Peter Kalugin <streetworkout200412@gmail.com>
2024-03-03 07:46:39 +0300

    Added description "Base Conversion"

---
+++ Python_Solutions/CodeWars/6kyu/Base_Conversion.py
@@ -0,0 +1,26 @@
+# In this kata you have to implement a base converter, which converts positive integers between arbitrary bases / alphabets. Here are some pre-defined alphabets:
+#
+# bin      = '01'
+# oct      = '01234567'
+# dec      = '0123456789'
+# hex      = '0123456789abcdef'
+# allow    = 'abcdefghijklmnopqrstuvwxyz'
+# allup    = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+# alpha    = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+# alphanum = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+# The function convert() should take an input (string), the source alphabet (string) and the target alphabet (string). You can assume that the input value always consists of characters from the source alphabet. You don't need to validate it.
+#
+# Examples
+# convert("15", dec, bin)       ==>  "1111"
+# convert("15", dec, oct)       ==>  "17"
+# convert("1010", bin, dec)     ==>  "10"
+# convert("1010", bin, hex)     ==>  "a"
+# convert("0", dec, alpha)      ==>  "a"
+# convert("27", dec, allow)     ==>  "bb"
+# convert("hello", allow, hex)  ==>  "320048"
+# Additional Notes:
+#
+# The maximum input value can always be encoded in a number without loss of precision in JavaScript. In Haskell, intermediate results will probably be too large for Int.
+# The function must work for any arbitrary alphabets, not only the pre-defined ones
+# You don't have to consider negative numbers
+# STRINGSMATHEMATICSALGORITHMS
