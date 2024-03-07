7d06028f9462dc19e9f21393528ca7153b17aff4
Peter Kalugin <streetworkout200412@gmail.com>
2024-03-07 07:07:01 +0300

    Added solution "Look and say numbers"

--- Python_Solutions/CodeWars/6kyu/Look_and_say_numbers.py
+++ Python_Solutions/CodeWars/6kyu/Look_and_say_numbers.py
@@ -16,3 +16,20 @@
 # Write a function that given a starting value as a string, returns the appropriate sequence as a list. The starting value can have any number of digits. The termination condition is a defined by the maximum number of iterations, also supplied as an argument.
 #
 # RECURSIONALGORITHMS
+# Solution
+def look_and_say(data='1', maxlen=5):
+    ans: list = list()
+    for i in range(maxlen):
+        top: str = ''
+        prev: str = ans[-1][0]  if ans else data[0]
+        count: int = 1
+        for j in range(1, len(ans[-1]) if ans else len(data)):
+            if prev != (ans[-1][j] if ans else data[j]):
+                top += str(count) + prev
+                count, prev = 1, ans[-1][j] if ans else data[j]
+            else:
+                count += 1
+        if count > 0:
+            top += str(count) + prev
+        ans.append(top)
+    return ans
