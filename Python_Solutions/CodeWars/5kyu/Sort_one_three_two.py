737feb10bbf29c5372c21ba3552c4e5ecf325ba9
Peter Kalugin <streetworkout200412@gmail.com>
2024-03-10 07:43:14 +0300

    Added solution "Sort - one, three, two"

--- Python_Solutions/CodeWars/5kyu/Sort_one_three_two.py
+++ Python_Solutions/CodeWars/5kyu/Sort_one_three_two.py
@@ -25,3 +25,37 @@
 # e.g. "one hundred one", "one hundred two"; is same order as "one hundred and one", "one hundred and two"
 # e.g. "ninety eight", "ninety nine"; is same order as "ninety-eight", "ninety-nine"
 # SORTINGSTRINGSALGORITHMS
+# Solution
+def convertation(dgt: int) -> str:
+    digits: list = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    ten: list = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+    double: list = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+    ans: list = list()
+    while True:
+        if dgt < 10:
+            ans.append(digits[dgt])
+            break
+        elif dgt < 20:
+            ans.append(ten[int(str(dgt)[1])])
+            break
+        elif dgt < 100:
+            x, y = str(dgt)[:]
+            if y == '0':
+                ans.append(double[int(x) - 1])
+                break
+            else:
+                ans.append(double[int(x) - 1] + ' ' + digits[int(y)])
+                break
+        else:
+            if dgt % 100 == 0:
+                ans.append(digits[int(str(dgt)[0])] + ' hundred')
+                break
+            ans.append(digits[int(str(dgt)[0])] + ' hundred')
+            dgt %= 100
+    return ' '.join(ans)
+
+
+def sort_by_name(arr):
+    ht: dict = {i:convertation(i) for i in arr}
+    arr.sort(key=lambda x: ht[x])
+    return arr
