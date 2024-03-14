b2c516f602a509d5f1d0e7ad12e1a2fc35d11119
Peter Kalugin <streetworkout200412@gmail.com>
2024-03-14 06:43:01 +0300

    Added description "packing rectangles"

---
+++ Python_Solutions/CodeWars/6kyu/packing_rectangles.py
@@ -0,0 +1,17 @@
+# Problem statement
+# Packing multiple rectangles of varying widths and heights in an enclosing rectangle of minimum area. Given 3 rectangular boxes, find minimal area that they can be placed. Boxes can not overlap, these can only be placed on the floor.
+#
+# Input
+# 3 pairs of numbers come to the input - the lengths(a1, b1, a2, b2, a3, b3) of the sides of the boxes. (1 ⩽ ai, bi ⩽ 10^4)
+#
+# Output
+# Return a number equal to the minimum occupied area.
+#
+# Example
+# Input example:
+#
+# packing_rectangles(4,10,5,11,12,3)
+# Output example:
+#
+# 144
+# ALGORITHMS
