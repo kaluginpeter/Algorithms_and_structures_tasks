2f767ab51770352d6d33cf1dcc1d33b2c9a0b73a
Peter Kalugin <streetworkout200412@gmail.com>
2024-03-17 10:15:56 +0300

    Added description "3084. Count Substrings Starting and Ending with Given Character"

---
+++ Python_Solutions/LeetCode/Medium/3084._Count_Substrings_Starting_and_Ending_with_Given_Character.py
@@ -0,0 +1,28 @@
+# You are given a string s and a character c. Return the total number of
+# substrings
+#  of s that start and end with c.
+#
+#
+#
+# Example 1:
+#
+# Input: s = "abada", c = "a"
+#
+# Output: 6
+#
+# Explanation: Substrings starting and ending with "a" are: "abada", "abada", "abada", "abada", "abada", "abada".
+#
+# Example 2:
+#
+# Input: s = "zzz", c = "z"
+#
+# Output: 6
+#
+# Explanation: There are a total of 6 substrings in s and all start and end with "z".
+#
+#
+#
+# Constraints:
+#
+# 1 <= s.length <= 105
+# s and c consist only of lowercase English letters.
