550ee2e3629f8ac9a901d0c5fd50ab310babe250
Peter Kalugin <streetworkout200412@gmail.com>
2024-03-17 08:07:06 +0300

    Added solution "The latest clock"

--- Python_Solutions/CodeWars/6kyu/The_latest_clock.py
+++ Python_Solutions/CodeWars/6kyu/The_latest_clock.py
@@ -10,3 +10,23 @@
 # Result should be a valid 24-hour time, between 00:00 and 23:59.
 # Only inputs which have valid answers are tested.
 # DATE TIMEFUNDAMENTALSALGORITHMS
+# Solution
+def latest_clock(*args):
+    stock: list = list(args)
+    mx: int = float('-inf')
+    for i in range(len(stock)):
+        for j in range(len(stock)):
+            if i == j: continue
+            for k in range(len(stock)):
+                if k == j or k == i: continue
+                for l in range(len(stock)):
+                    if l == i or l == j or l == k: continue
+                    x: int = stock[i] * 1000 + stock[j] * 100 + stock[k] * 10 + stock[l]
+                    if ((stock[i]  == 2 and stock[j] < 4) or (stock[i] < 2 and stock[j] < 10)) and stock[k] < 6:
+                        mx = max(mx, x)
+    if mx == 0:
+        return '00:00'
+    if mx < 1000:
+        if mx % 1000 > 2:
+            return '0' + str(mx)[:1] + ':' + str(mx)[1:]
+    return str(mx)[:2] + ':' + str(mx)[2:]
