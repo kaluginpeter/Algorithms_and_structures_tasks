e8b2c3fc27b6eef10db30b95164e59eda30a6394
Peter Kalugin <streetworkout200412@gmail.com>
2024-03-17 08:06:36 +0300

    Added description "The latest clock"

---
+++ Python_Solutions/CodeWars/6kyu/The_latest_clock.py
@@ -0,0 +1,12 @@
+# Write a function which receives 4 digits and returns the latest time of day that can be built with those digits.
+#
+# The time should be in HH:MM format.
+#
+# Examples:
+#
+# digits: 1, 9, 8, 3 => result: "19:38"
+# digits: 9, 1, 2, 5 => result: "21:59" (19:25 is also a valid time, but 21:59 is later)
+# Notes
+# Result should be a valid 24-hour time, between 00:00 and 23:59.
+# Only inputs which have valid answers are tested.
+# DATE TIMEFUNDAMENTALSALGORITHMS
