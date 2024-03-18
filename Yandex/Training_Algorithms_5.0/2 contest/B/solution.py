b8f330a3220ba396354a8fbd8df4b47ea993557b
Peter Kalugin <streetworkout200412@gmail.com>
2024-03-18 20:04:29 +0300

    . While time contest not ended

--- Yandex/Training_Algorithms_5.0/2 contest/B/solution.py
+++
@@ -1,7 +0,0 @@
-n, k = map(int, input().split())
-lst: list = list(map(int, input().split()))
-ans: int = 0
-for i in range(n):
-    for j in range(i+1, min(n, i + k + 1)):
-        ans = max(ans, lst[j] - lst[i])
-print(ans)
--- Yandex/Training_Algorithms_5.0/3 contest/C/solution.py
+++
@@ -1,9 +0,0 @@
-n: int = int(input())
-lst: list = list(map(int, input().split()))
-ht: dict = dict()
-for i in lst:
-    ht[i] = ht.get(i, 0) + 1
-mx: int = 0
-for i in ht:
-    mx = max(mx, ht[i] + ht.get(i + 1, 0))
-print(n - mx)
