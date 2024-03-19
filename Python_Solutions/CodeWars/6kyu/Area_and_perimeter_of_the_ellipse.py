a48891b24b9a6a7e6ba2ef3391fe9d886f0bbc0f
Peter Kalugin <streetworkout200412@gmail.com>
2024-03-19 18:06:47 +0300

    Added description "Area and perimeter of the ellipse"

---
+++ Python_Solutions/CodeWars/6kyu/Area_and_perimeter_of_the_ellipse.py
@@ -0,0 +1,11 @@
+# Task:
+# Based on the received dimensions, a and b, of an ellipse, calculare its area and perimeter.
+#
+# Example:
+# Input: ellipse(5,2)
+#
+# Output: "Area: 31.4, perimeter: 23.1"
+# Note: The perimeter approximation formula you should use: π * (3/2(a+b) - sqrt(ab))
+#
+# Have fun :)
+# FUNDAMENTALS
