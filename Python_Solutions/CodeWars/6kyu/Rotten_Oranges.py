5a1d81dede566e657f275b35a2194a7624d96f3a
Peter Kalugin <streetworkout200412@gmail.com>
2024-03-23 13:49:48 +0300

    Added solution "Rotten Oranges"

--- Python_Solutions/CodeWars/6kyu/Rotten_Oranges.py
+++ Python_Solutions/CodeWars/6kyu/Rotten_Oranges.py
@@ -30,3 +30,36 @@
 # The box is now fully rotten. Result: 3.
 #
 # FUNDAMENTALS
+# Solution
+def calc_rot_time(box):
+    mtrx: list[int] = [[-1]  + i + [-1] for i in box]
+    mtrx.insert(0, [-1] * (len(box[0]) + 2))
+    mtrx.append([-1] * (len(box[0]) + 2))
+    new_mtrx: list[int] = list()
+    mtrx_moves: list[int] = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+    hazard: bool = True
+    count: int = 0
+    while hazard:
+        hazard = False
+        rows: list[int] = list()
+        for i in range(len(mtrx)):
+            for j in range(len(mtrx[0])):
+                if mtrx[i][j] == 1:
+                    for position in mtrx_moves:
+                        if mtrx[i + position[0]][j + position[1]] == 2:
+                            hazard = True
+                            rows.append(2)
+                            break
+                    else:
+                        rows.append(1)
+                else:
+                    rows.append(mtrx[i][j])
+            new_mtrx.append(rows)
+            rows = []
+        if hazard:
+            mtrx = new_mtrx
+            new_mtrx = []
+            count += 1
+    if all(mtrx[i][j] in {0, 2} for j in range(1, len(mtrx[0]) - 1) for i in range(1, len(mtrx) - 1)):
+        return count
+    return -1
