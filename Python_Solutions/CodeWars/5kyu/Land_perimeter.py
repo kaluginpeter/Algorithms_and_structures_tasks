dd0227537a2f16966c15816fc5ad0468fbf31b8b
Peter Kalugin <streetworkout200412@gmail.com>
2024-03-25 06:42:24 +0300

    Added description "Land perimeter"

---
+++ Python_Solutions/CodeWars/5kyu/Land_perimeter.py
@@ -0,0 +1,23 @@
+# Given an array arr of strings, complete the function by calculating the total perimeter of all the islands. Each piece of land will be marked with 'X' while the water fields are represented as 'O'. Consider each tile being a perfect 1 x 1 piece of land. Some examples for better visualization:
+#
+# ['XOOXO',
+#  'XOOXO',
+#  'OOOXO',
+#  'XXOXO',
+#  'OXOOO']
+# which represents:
+#
+# should return: "Total land perimeter: 24".
+#
+# Following input:
+#
+# ['XOOO',
+#  'XOXO',
+#  'XOXO',
+#  'OOXX',
+#  'OOOO']
+# which represents:
+#
+# should return: "Total land perimeter: 18"
+#
+# FUNDAMENTALS
