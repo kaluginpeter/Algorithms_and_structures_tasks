68b9a5b63ef8f810402ad18aa0666134df4768b6
Peter Kalugin <streetworkout200412@gmail.com>
2024-03-25 06:43:01 +0300

    Added solution "Land perimeter"

--- Python_Solutions/CodeWars/5kyu/Land_perimeter.py
+++ Python_Solutions/CodeWars/5kyu/Land_perimeter.py
@@ -21,3 +21,17 @@
 # should return: "Total land perimeter: 18"
 #
 # FUNDAMENTALS
+# Solution
+def land_perimeter(arr):
+    count: int = 0
+    moves: list[list[int, int]] = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+    arr = ['s' + i + 's' for i in arr]
+    arr.append('s' * (len(arr[0])))
+    arr.insert(0, 's' * (len(arr[0])))
+    for rows in range(len(arr)):
+        for column in range(len(arr[0])):
+            if arr[rows][column] == 'X':
+                for move in moves:
+                    if arr[rows + move[0]][column + move[1]] != 'X':
+                        count += 1
+    return f'Total land perimeter: {count}'
