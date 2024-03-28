6a273cfcd413ab3c7db98c3ffba13a87cdaf0fac
Peter Kalugin <streetworkout200412@gmail.com>
2024-03-28 17:07:12 +0300

    Added solution "Sort sentence pseudo-alphabetically"

--- Python_Solutions/CodeWars/6kyu/Sort_sentence_pseudo_alphabetically.py
+++ Python_Solutions/CodeWars/6kyu/Sort_sentence_pseudo_alphabetically.py
@@ -8,3 +8,18 @@
 # For example, given the input string "Land of the Old Thirteen! Massachusetts land! land of Vermont and Connecticut!", your method should return "and land land of of the Vermont Thirteen Old Massachusetts Land Connecticut". Lower case letters are sorted a -> l -> l -> o -> o -> t and upper case letters are sorted V -> T -> O -> M -> L -> C.
 #
 # SORTINGSTRINGSALGORITHMS
+# Solution
+def pseudo_sort(st):
+    l, u = list(), list()
+    for i in st.split():
+        x = i
+        while x and not x[-1].isalpha():
+            x = x[:-1]
+        if x and x[0].isupper():
+            u.append(x)
+        elif x:
+            l.append(x)
+    l.sort()
+    u.sort(reverse=True)
+    ans = l + u
+    return ' '.join(ans)
