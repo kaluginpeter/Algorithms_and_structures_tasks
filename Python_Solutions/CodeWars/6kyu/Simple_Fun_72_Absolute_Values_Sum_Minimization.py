74159c54ecc93a1e9d4e5ee4da06ed9105366146
Peter Kalugin <streetworkout200412@gmail.com>
2024-03-30 15:52:01 +0300

    Added description "Simple Fun #72: Absolute Values Sum Minimization"

---
+++ Python_Solutions/CodeWars/6kyu/Simple_Fun_72_Absolute_Values_Sum_Minimization.py
@@ -0,0 +1,22 @@
+# Task
+# Given a sorted array of integers A, find such an integer x that the value of abs(A[0] - x) + abs(A[1] - x) + ... + abs(A[A.length - 1] - x) is the smallest possible (here abs denotes the absolute value).
+#
+# If there are several possible answers, output the smallest one.
+#
+# Example
+# For A = [2, 4, 7], the output should be 4.
+#
+# Input/Output
+# [input] integer array A
+#
+# A non-empty array of integers, sorted in ascending order.
+#
+# Constraints:
+#
+# 1 ≤ A.length ≤ 200,
+#
+# -1000000 ≤ A[i] ≤ 1000000.
+#
+# [output] an integer
+#
+# PUZZLES
