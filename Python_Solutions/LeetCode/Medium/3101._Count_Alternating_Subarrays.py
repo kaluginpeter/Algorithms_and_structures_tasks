5b689cf4f5b358b0e714109ef7821db22094743d
Peter Kalugin <streetworkout200412@gmail.com>
2024-03-31 07:49:10 +0300

    Added solution "3101. Count Alternating Subarrays"

--- Python_Solutions/LeetCode/Medium/3101._Count_Alternating_Subarrays.py
+++ Python_Solutions/LeetCode/Medium/3101._Count_Alternating_Subarrays.py
@@ -36,3 +36,22 @@
 #
 # 1 <= nums.length <= 105
 # nums[i] is either 0 or 1.
+# Solution o(N) O(1)
+class Solution:
+    def countAlternatingSubarrays(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return 1
+        ans: int = 0
+        ln: int = 0
+        for i in range(len(nums) - 1):
+            ln += 1
+            if nums[i] == nums[i + 1]:
+                ans += ln * (ln + 1) // 2
+                ln = 0
+        if len(nums) > 1:
+            if nums[-1] != nums[-2]:
+                ln += 1
+            else:
+                ln = 1
+            ans += ln * (ln + 1) // 2
+        return ans
