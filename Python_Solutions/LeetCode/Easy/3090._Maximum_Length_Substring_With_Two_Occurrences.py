394ce9e3fb7c6be5fa1bafd871f3ac349a61ac6e
Peter Kalugin <streetworkout200412@gmail.com>
2024-03-31 06:40:25 +0300

    Added description "3090. Maximum Length Substring With Two Occurrences"

---
+++ Python_Solutions/LeetCode/Easy/3090._Maximum_Length_Substring_With_Two_Occurrences.py
@@ -0,0 +1,29 @@
+# Given a string s, return the maximum length of a
+# substring
+#  such that it contains at most two occurrences of each character.
+#
+#
+# Example 1:
+#
+# Input: s = "bcbbbcba"
+#
+# Output: 4
+#
+# Explanation:
+#
+# The following substring has a length of 4 and contains at most two occurrences of each character: "bcbbbcba".
+# Example 2:
+#
+# Input: s = "aaaa"
+#
+# Output: 2
+#
+# Explanation:
+#
+# The following substring has a length of 2 and contains at most two occurrences of each character: "aaaa".
+#
+#
+# Constraints:
+#
+# 2 <= s.length <= 100
+# s consists only of lowercase English letters.
