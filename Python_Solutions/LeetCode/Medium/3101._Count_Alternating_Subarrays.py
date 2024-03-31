9938f79d7b7ab08cd9edd251b0cc4c2af8063a66
Peter Kalugin <streetworkout200412@gmail.com>
2024-03-31 07:48:55 +0300

    Added description "3101. Count Alternating Subarrays"

---
+++ Python_Solutions/LeetCode/Medium/3101._Count_Alternating_Subarrays.py
@@ -0,0 +1,38 @@
+# You are given a
+# binary array
+#  nums.
+#
+# We call a
+# subarray
+#  alternating if no two adjacent elements in the subarray have the same value.
+#
+# Return the number of alternating subarrays in nums.
+#
+#
+#
+# Example 1:
+#
+# Input: nums = [0,1,1,1]
+#
+# Output: 5
+#
+# Explanation:
+#
+# The following subarrays are alternating: [0], [1], [1], [1], and [0,1].
+#
+# Example 2:
+#
+# Input: nums = [1,0,1,0]
+#
+# Output: 10
+#
+# Explanation:
+#
+# Every subarray of the array is alternating. There are 10 possible subarrays that we can choose.
+#
+#
+#
+# Constraints:
+#
+# 1 <= nums.length <= 105
+# nums[i] is either 0 or 1.
