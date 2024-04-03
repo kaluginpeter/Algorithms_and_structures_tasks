12432190f1c37f0204e23a56a37809ab4f615d4e
Peter Kalugin <streetworkout200412@gmail.com>
2024-04-03 06:45:42 +0300

    Added description "Game Hit the target - 2nd part"

---
+++ Python_Solutions/CodeWars/6kyu/Game_Hit_the_target_2nd_part.py
@@ -0,0 +1,47 @@
+# This is the second part of the kata :3 🎈🎆🎇🎆🎈
+# given a matrix n x n (2-7), determine if the arrow is directed to the target (x).
+# Now there are one of 4 types of arrows ( '^', '>', 'v', '<' ) and only one target (x)
+# An empty spot will be denoted by a space " ", the target with a cross "x", and the scope ">"
+# Examples:
+# given matrix 4x4:
+# [
+#
+#   [' ', 'x', ' ', ' '],
+#
+#   [' ', ' ', ' ', ' '], --> return true
+#
+#   [' ', '^', ' ', ' '],
+#
+#   [' ', ' ', ' ', ' ']
+#
+# ]
+# given matrix 4x4:
+# [
+#
+#   [' ', ' ', ' ', ' '],
+#
+#   [' ', 'v', ' ', ' '], --> return false
+#
+#   [' ', ' ', ' ', 'x'],
+#
+#   [' ', ' ', ' ', ' ']
+#
+# ]
+# given matrix 4x4:
+# [
+#
+#   [' ', ' ', ' ', ' '],
+#
+#   ['>', ' ', ' ', 'x'], --> return true
+#
+#   [' ', '', ' ', ' '],
+#
+#   [' ', ' ', ' ', ' ']
+#
+# ]
+#
+# In this example, only a 4x4 matrix was used, the problem will have matrices of dimensions from 2 to 7
+# And here is the first part of this kata - click me ●v●
+# Happy hacking as they say! 💻
+#
+# MATRIXARRAYSSTRINGSFUNDAMENTALS
