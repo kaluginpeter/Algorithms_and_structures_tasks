28adf2de31fdcd20074a0c5b26f2c331ec325e58
Peter Kalugin <streetworkout200412@gmail.com>
2024-04-06 08:35:29 +0300

    Added solution "1249. Minimum Remove to Make Valid Parentheses"

--- Python_Solutions/LeetCode/Medium/1249._Minimum_Remove_to_Make_Valid_Parentheses.py
+++ Python_Solutions/LeetCode/Medium/1249._Minimum_Remove_to_Make_Valid_Parentheses.py
@@ -29,3 +29,24 @@
 #
 # 1 <= s.length <= 105
 # s[i] is either'(' , ')', or lowercase English letter.
+# Solution Stack O(N) O(N)
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        stack: list[int] = list()
+        for i in range(len(s)):
+            if s[i] == '(':
+                stack.append(i)
+            elif s[i] == ')':
+                if stack and s[stack[-1]] == '(':
+                    stack.pop()
+                else:
+                    stack.append(i)
+        ans: list[str] = list()
+        for i in range(len(s)):
+            if stack and i != stack[0]:
+                ans.append(s[i])
+            elif stack:
+                stack.pop(0)
+            else:
+                ans.append(s[i])
+        return ''.join(ans)
