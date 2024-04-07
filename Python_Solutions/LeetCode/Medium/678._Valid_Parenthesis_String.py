841a72cd9ab1b8ba5efd8fac22f0155f3b038ad5
Peter Kalugin <streetworkout200412@gmail.com>
2024-04-07 07:21:05 +0300

    Added complete "678. Valid Parenthesis String"

--- Python_Solutions/LeetCode/Medium/678._Valid_Parenthesis_String.py
+++ Python_Solutions/LeetCode/Medium/678._Valid_Parenthesis_String.py
@@ -26,3 +26,42 @@
 #
 # 1 <= s.length <= 100
 # s[i] is '(', ')' or '*'.
+# Solution Stack Greedy O(N) O(N)
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        stack: list[int] = list()
+        zeros: list[int] = list()
+        for i in range(len(s)):
+            if s[i] == '(':
+                stack.append(i)
+            elif s[i] == ')':
+                if stack:
+                    stack.pop()
+                elif zeros:
+                    zeros.pop()
+                else:
+                    return False
+            else:
+                zeros.append(i)
+        while stack and zeros:
+            if stack[0] <= zeros[0]:
+                stack.pop(0)
+                zeros.pop(0)
+            else:
+                zeros.pop(0)
+        return not stack
+
+# Solution One Pass O(N) O(1)
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        mn, mx = 0, 0
+        for i in s:
+            if i == '(':
+                mn, mx = mn + 1, mx + 1
+            elif i == ')':
+                mn, mx = max(mn - 1, 0), mx - 1
+            else:
+                mn, mx = max(mn - 1, 0), mx + 1
+            if mx < 0:
+                return False
+        return mn == 0
