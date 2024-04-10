5188d0225ccd37ab8e24ef8dbe5426d7db03f184
Peter Kalugin <streetworkout200412@gmail.com>
2024-04-10 18:03:22 +0300

    Added description "Continued Fraction"

---
+++ Python_Solutions/CodeWars/6kyu/Continued_Fraction.py
@@ -0,0 +1,39 @@
+# In this kata, you will have to return the continued fractionwiki of a fraction.
+#
+# For example, if the numerator is 311 and the denominator is 144, then you would have to return [2, 6, 3, 1, 5], because:
+#
+# 311
+# /
+# 144
+# =
+# 2
+# +
+# 1
+# 6
+# +
+# 1
+# 3
+# +
+# 1
+# 1
+# +
+# 1
+# 5
+# 311/144=2+
+# 6+
+# 3+
+# 1+
+# 5
+# 1
+# ​
+#
+# 1
+# ​
+#
+# 1
+# ​
+#
+# 1
+# ​
+#
+# If the numerator is 0, you should return [].
