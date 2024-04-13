00b1369eb6d7dd2f474de6fd9c0cbe5a5a4039e1
Peter Kalugin <streetworkout200412@gmail.com>
2024-04-13 09:13:45 +0300

    Added solution "85. Maximal Rectangle"

--- Python_Solutions/LeetCode/Hard/85._Maximal_Rectangle.py
+++ Python_Solutions/LeetCode/Hard/85._Maximal_Rectangle.py
@@ -24,3 +24,25 @@
 # cols == matrix[i].length
 # 1 <= row, cols <= 200
 # matrix[i][j] is '0' or '1'.
+# Solution Monotonic Stack O(N**2) O(N)
+class Solution:
+    def maximalRectangleInHistogram(self, arr: list[int]) -> int:
+        arr.append(0)
+        stack: list[int] = list()
+        ans: int = 0
+        for idx in range(len(arr)):
+            while stack and arr[stack[-1]] >= arr[idx]:
+                h: int = arr[stack.pop()]
+                w: int = idx if not stack else idx - stack[-1] - 1
+                ans = max(ans, h * w)
+            stack.append(idx)
+        return ans
+
+    def maximalRectangle(self, matrix: List[List[str]]) -> int:
+        stack: list[int] = [0] * len(matrix[0])
+        ans: int = 0
+        for rows in matrix:
+            for cols in range(len(rows)):
+                stack[cols] = 0 if rows[cols] == '0' else stack[cols] + int(rows[cols])
+            ans = max(ans, self.maximalRectangleInHistogram(stack))
+        return ans
