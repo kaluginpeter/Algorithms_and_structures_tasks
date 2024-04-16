eaa0946f736e1df1be56163c99414d61166d9b6c
Peter Kalugin <streetworkout200412@gmail.com>
2024-04-16 06:49:53 +0300

    Added solution "164. Maximum Gap"

--- Python_Solutions/LeetCode/Medium/164._Maximum_Gap.py
+++ Python_Solutions/LeetCode/Medium/164._Maximum_Gap.py
@@ -20,3 +20,18 @@
 #
 # 1 <= nums.length <= 105
 # 0 <= nums[i] <= 109
+# Solution Bucket Sort O(N) O(N)
+class Solution:
+    from math import ceil
+    def maximumGap(self, nums: List[int]) -> int:
+        mn, mx = min(nums), max(nums)
+        if len(nums) < 2 or mn == mx:
+            return 0
+        n: int = ceil((mx - mn) / (len(nums) - 1))
+        bucket: list[list[int, int]] = [[None, None] for _ in range((mx - mn) // n + 1)]
+        for elmnt in nums:
+            current_chunck: list[int, int] = bucket[(elmnt - mn) // n]
+            current_chunck[0] = elmnt if current_chunck[0] is None else min(elmnt, current_chunck[0])
+            current_chunck[1] = elmnt if current_chunck[1] is None else max(elmnt, current_chunck[1])
+        bucket = [chunck for chunck in bucket if chunck[0] if not None]
+        return max(bucket[i][0] - bucket[i - 1][1] for i in range(1, len(bucket)))
