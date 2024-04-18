b13f2c683aefcfa48f58cdcb85d9b7be769818b7
Peter Kalugin <streetworkout200412@gmail.com>
2024-04-18 06:35:50 +0300

    Added descsription "Cantor's Diagonals"

---
+++ Python_Solutions/CodeWars/7kyu/Cantor's_Diagonals.py
@@ -0,0 +1,14 @@
+# Given a list of lists containing only 1s and 0s, return a new list that differs from list 1 in its first element, list 2 in its second element, list 3 in its 3rd element, and so on.
+#
+# cantor([[0,0,0],
+#         [1,1,1],
+#         [0,1,0]]) = [1,0,1]
+#
+# cantor([[1,0,0],
+#         [0,1,0],
+#         [0,0,1]]) = [0,0,0]
+# The nested list will always be perfectly square. Your solution should be a list containing only 1s and 0s.
+#
+# See Wikipedia for background (if you're interested; it won't help you solve the kata). Obviously this kata is not the same because the lists are not infinite so it doesn't really prove anything -- consider it a tribute...
+#
+# FUNDAMENTALS
