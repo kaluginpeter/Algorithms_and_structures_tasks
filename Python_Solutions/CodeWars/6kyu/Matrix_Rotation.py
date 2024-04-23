5371695ebadf90211380a726c4cd0f87b55da63e
Peter Kalugin <streetworkout200412@gmail.com>
2024-04-23 17:50:24 +0300

    Added solution "Matrix Rotation"

--- Python_Solutions/CodeWars/6kyu/Matrix_Rotation.py
+++ Python_Solutions/CodeWars/6kyu/Matrix_Rotation.py
@@ -27,3 +27,14 @@
 # Please also note that the equality m == rotateClockwise(rotateClockwise(rotateClockwise(rotateClockwise(m)))); (360° clockwise rotation), is not always true because rotateClockwise(['']) => [] and rotateClockwise(['','','']) => [] (empty lines information is lost)
 #
 # MATRIXFUNDAMENTALS
+# Solution
+def rotate_clockwise(matrix):
+    ans: list = list()
+    if not matrix:
+        return ans
+    for col in range(len(matrix[0])):
+        top: list = list()
+        for row in range(len(matrix)):
+            top.append(matrix[row][col])
+        ans.append(''.join(top)[::-1])
+    return ans
