187d222404be04664b801c3b8497cc9d04abb3cc
Peter Kalugin <streetworkout200412@gmail.com>
2024-05-05 07:50:27 +0300

    Added solution "3120. Count the Number of Special Characters I"

--- Python_Solutions/LeetCode/Easy/3120._Count_the_Number_of_Special_Characters_I.py
+++ Python_Solutions/LeetCode/Easy/3120._Count_the_Number_of_Special_Characters_I.py
@@ -40,3 +40,14 @@
 #
 # 1 <= word.length <= 50
 # word consists of only lowercase and uppercase English letters.
+# Solution HashTable O(N) O(N)
+class Solution:
+    def numberOfSpecialChars(self, word: str) -> int:
+        ht: dict[str, int] = dict()
+        for i in word:
+            ht[i] = ht.get(i, 0) + 1
+        ans: int = 0
+        for i in ht:
+            if i.lower() in ht and i.upper() in ht:
+                ans += 1
+        return ans // 2
