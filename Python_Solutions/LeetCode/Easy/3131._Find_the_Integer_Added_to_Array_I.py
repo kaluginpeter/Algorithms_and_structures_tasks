1d9d4f1633988885965e0bbd56502d780c60f8e2
Peter Kalugin <streetworkout200412@gmail.com>
2024-05-05 08:00:34 +0300

    Added solution "3131. Find the Integer Added to Array I"

--- Python_Solutions/LeetCode/Easy/3131._Find_the_Integer_Added_to_Array_I.py
+++ Python_Solutions/LeetCode/Easy/3131._Find_the_Integer_Added_to_Array_I.py
@@ -45,3 +45,7 @@
 # 1 <= nums1.length == nums2.length <= 100
 # 0 <= nums1[i], nums2[i] <= 1000
 # The test cases are generated in a way that there is an integer x such that nums1 can become equal to nums2 by adding x to each element of nums1.
+# Solution Math O(N) O(1)
+class Solution:
+    def addedInteger(self, nums1: List[int], nums2: List[int]) -> int:
+        return (sum(nums2) - sum(nums1)) // len(nums1)
