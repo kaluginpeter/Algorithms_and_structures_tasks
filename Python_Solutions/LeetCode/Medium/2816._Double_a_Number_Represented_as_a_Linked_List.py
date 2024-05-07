8b4f58b60152710a44383cbf48a6ff37aaaebf80
Peter Kalugin <streetworkout200412@gmail.com>
2024-05-07 07:03:11 +0300

    Added solution "2816. Double a Number Represented as a Linked List"

--- Python_Solutions/LeetCode/Medium/2816._Double_a_Number_Represented_as_a_Linked_List.py
+++ Python_Solutions/LeetCode/Medium/2816._Double_a_Number_Represented_as_a_Linked_List.py
@@ -23,3 +23,40 @@
 # The number of nodes in the list is in the range [1, 104]
 # 0 <= Node.val <= 9
 # The input is generated such that the list represents a number that does not have leading zeros, except the number 0 itself.
+# Solution Math O(N) O(1)
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def doubleIt(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # Parse n
+        n: int = 0
+        tmp = head
+        count: int = 0
+        while tmp:
+            count += 1
+            n = n * 10 + tmp.val
+            tmp = tmp.next
+        n *= 2
+        # Insert n in linked list
+        out = head
+        while n:
+            out.val = n % 10
+            n //= 10
+            if out.next:
+                out = out.next
+            else:
+                out.next = ListNode() if n else None
+                out = out.next
+        # Reverse list
+        prev = None
+        current = head
+        while current:
+            nxt = current.next
+            current.next = prev
+            prev = current
+            current = nxt
+        head = prev
+        return head
