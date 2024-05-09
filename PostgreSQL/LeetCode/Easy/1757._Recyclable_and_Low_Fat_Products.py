e727c45dd9723038d7d903cafe5df8fd53dfe511
Peter Kalugin <streetworkout200412@gmail.com>
2024-05-09 09:37:38 +0300

    Added solution "1757. Recyclable and Low Fat Products"

--- PostgreSQL/LeetCode/Easy/1757._Recyclable_and_Low_Fat_Products.py
+++ PostgreSQL/LeetCode/Easy/1757._Recyclable_and_Low_Fat_Products.py
@@ -41,3 +41,6 @@
 # | 3           |
 # +-------------+
 # Explanation: Only products 1 and 3 are both low fat and recyclable.
+SELECT product_id
+FROM Products
+WHERE low_fats = 'Y' AND recyclable = 'Y'
