a62b5651f4ded71a7bb233a07e6fa481d1d20964
Peter Kalugin <streetworkout200412@gmail.com>
2024-05-10 10:50:11 +0300

    Added solution "786. K-th Smallest Prime Fraction"

--- Python_Solutions/LeetCode/Medium/786._K-th_Smallest_Prime_Fraction.py
+++ Python_Solutions/LeetCode/Medium/786._K-th_Smallest_Prime_Fraction.py
@@ -30,3 +30,36 @@
 #
 #
 # Follow up: Can you solve the problem with better than O(n2) complexity?
+# Solution Brute Force O(N**2) O(N)
+class Solution:
+    def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
+        stack: list[int] = list()
+        for i in range(len(arr)):
+            for j in range(i + 1, len(arr)):
+                stack.append((arr[i] / arr[j], [i, j]))
+        stack.sort(key=lambda x: x[0])
+        x, y = stack[k - 1][1]
+        return [arr[x], arr[y]]
+# Solution Binary Search Two Pointers O(NlogN) O(1)
+class Solution:
+    def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
+        left, right = 0, 1
+        n: int = len(arr)
+        while left <= right:
+            middle: float = (left + right) / 2
+            j, top, num, den = 1, 0, 0, 0
+            mxfrq: float = 0.0
+            for i in range(n):
+                while j < n and arr[i] >= arr[j] * middle:
+                    j += 1
+                top += n - j
+                if j < n and mxfrq < arr[i] / arr[j]:
+                    mxfrq = arr[i] / arr[j]
+                    num, den = i, j
+            if top == k:
+                return [arr[num], arr[den]]
+            elif top > k:
+                right = middle
+            else:
+                left = middle
+        return [arr[num], arr[den]]
