5ed8d689efc5876486fab3f65798883275facb29
Peter Kalugin <streetworkout200412@gmail.com>
2024-05-12 07:23:53 +0300

    Added solution "3146. Permutation Difference between Two Strings"

--- Python_Solutions/LeetCode/Easy/3146._Permutation_Difference_between_Two_Strings.py
+++ Python_Solutions/LeetCode/Easy/3146._Permutation_Difference_between_Two_Strings.py
@@ -37,3 +37,17 @@
 # Each character occurs at most once in s.
 # t is a permutation of s.
 # s consists only of lowercase English letters.
+# Solution HashTable O(N) O(N)
+class Solution:
+    def findPermutationDifference(self, s: str, t: str) -> int:
+        ht: dict[str, int] = dict()
+        for i in range(len(s)):
+            if s[i] in ht:
+                ht[s[i]] = abs(ht[s[i]] - i)
+            else:
+                ht[s[i]] = i
+            if t[i] in ht:
+                ht[t[i]] = abs(ht[t[i]] - i)
+            else:
+                ht[t[i]] = i
+        return sum(ht.values())
