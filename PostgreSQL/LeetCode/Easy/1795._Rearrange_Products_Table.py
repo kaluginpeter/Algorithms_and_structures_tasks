607c15bb04b98db5d253bdfc9b8a35b101e2a095
Peter Kalugin <streetworkout200412@gmail.com>
2024-05-12 06:40:13 +0300

    Added solution "1795. Rearrange Products Table"

--- PostgreSQL/LeetCode/Easy/1795._Rearrange_Products_Table.py
+++ PostgreSQL/LeetCode/Easy/1795._Rearrange_Products_Table.py
@@ -44,3 +44,15 @@
 # Explanation:
 # Product 0 is available in all three stores with prices 95, 100, and 105 respectively.
 # Product 1 is available in store1 with price 70 and store3 with price 80. The product is not available in store2.
+# Solution
+SELECT product_id, 'store1' AS store, store1 AS price
+FROM Products
+WHERE store1 IS NOT NULL
+UNION
+SELECT product_id, 'store2' AS store, store2 AS price
+FROM Products
+WHERE store2 IS NOT NULL
+UNION
+SELECT product_id, 'store3' as store, store3 AS price
+FROM Products
+WHERE store3 IS NOT NULL
