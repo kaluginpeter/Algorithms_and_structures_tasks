58344a8e25d794fa121809c8c40b255d832e57b8
Peter Kalugin <streetworkout200412@gmail.com>
2024-05-13 16:57:43 +0300

    Added solution "Make Me Slow"

--- Python_Solutions/CodeWars/7kyu/Make_Me_Slow.py
+++ Python_Solutions/CodeWars/7kyu/Make_Me_Slow.py
@@ -1,3 +1,9 @@
 # Make me slow! Calling makeMeSlow() should take at least 7 seconds.
 #
 # FUNDAMENTALS
+# Solution
+import time
+def make_me_slow():
+    time.sleep(7)
+    for i in range(1000000):
+        pass
