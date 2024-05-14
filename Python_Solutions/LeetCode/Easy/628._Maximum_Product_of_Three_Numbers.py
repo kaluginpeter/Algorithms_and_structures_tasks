03ee0e45fcc577f0cdbf5c0409a3f333563de8eb
Peter Kalugin <streetworkout200412@gmail.com>
2024-05-14 16:53:50 +0300

    Added solution "628. Maximum Product of Three Numbers"

--- Python_Solutions/LeetCode/Easy/628._Maximum_Product_of_Three_Numbers.py
+++ Python_Solutions/LeetCode/Easy/628._Maximum_Product_of_Three_Numbers.py
@@ -20,3 +20,22 @@
 #
 # 3 <= nums.length <= 104
 # -1000 <= nums[i] <= 1000
+# Solution O(N) O(1)
+class Solution:
+    def maximumProduct(self, nums: List[int]) -> int:
+        first_max = second_max = third_max = float('-inf')
+        first_min = second_min = float('inf')
+        for i in nums:
+            if i > first_max:
+                first_max, second_max, third_max = i, first_max, second_max
+            elif i > second_max:
+                second_max, third_max = i, second_max
+            elif i > third_max:
+                third_max = i
+            if i < first_min:
+                first_min, second_min = i, first_min
+            elif i < second_min:
+                second_min = i
+        if second_min != float('inf'):
+            return max(first_min * second_min * first_max, first_max * second_max * third_max)
+        return first_max * second_max * third_max
