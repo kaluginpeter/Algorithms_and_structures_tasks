113919814d9f05645cda8b9ce48f4c0ced8f115b
Peter Kalugin <streetworkout200412@gmail.com>
2024-05-15 19:18:47 +0300

    Added solution "2433. Find The Original Array of Prefix Xor"

--- Python_Solutions/LeetCode/Medium/2433._Find_The_Original_Array_of_Prefix_Xor.py
+++ Python_Solutions/LeetCode/Medium/2433._Find_The_Original_Array_of_Prefix_Xor.py
@@ -28,3 +28,9 @@
 #
 # 1 <= pref.length <= 105
 # 0 <= pref[i] <= 106
+# Solution Bit Manipulation O(N) O(1)
+class Solution:
+    def findArray(self, pref: List[int]) -> List[int]:
+        for i in range(len(pref) - 1, 0, -1):
+            pref[i] ^= pref[i - 1]
+        return pref
