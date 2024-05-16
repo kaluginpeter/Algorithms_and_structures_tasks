6fa70cd8df89f331dd2bcd1e138b51c13a4583cd
Peter Kalugin <streetworkout200412@gmail.com>
2024-05-16 20:13:52 +0300

    Added solution "Make a spiral"

--- Python_Solutions/CodeWars/3kyu/Make_a_spiral.py
+++ Python_Solutions/CodeWars/3kyu/Make_a_spiral.py
@@ -27,3 +27,69 @@
 # General rule-of-a-thumb is, that the snake made with '1' cannot touch to itself.
 #
 # ALGORITHMSARRAYSLOGIC
+# Solution
+def check_right(output, up, right, n):
+    moves = [[-1, 1], [1, 1]]
+    for m in moves:
+        x, y = m
+        if (not 0 <= up + x <= n - 1) or (not 0 <= right + y <= n - 1):
+            continue
+        if output[up + x][right + y] == 1:
+            return False
+    return True
+
+def check_left(output, up, right, n):
+    moves = [[1, -1], [-1, -1]]
+    for m in moves:
+        x, y = m
+        if (not 0 <= up + x <= n - 1) or (not 0 <= right + y <= n - 1):
+            continue
+        if output[up + x][right + y] == 1:
+            return False
+    return True
+
+def spiralize(size):
+    right, up = 0, 0
+    output: list[list[int]] = [[0 for _ in range(size)] for _ in range(size)]
+    n: int = size
+    for rep in range(n):
+
+        for i in range(size - 1):
+            if not check_right(output, up, right, n):
+                break
+            if output[up][min(right + 1, n - 1)] == 1:
+                break
+            output[up][right] = 1
+            if output[up][min(right + 2, n - 1)] == 1:
+                break
+            right += 1
+
+        for i in range(size - 1):
+            if output[min(up + 1, n - 1)][right] == 1:
+                break
+            output[up][right] = 1
+            if output[min(up + 2, n - 1)][right] == 1:
+                break
+            up += 1
+
+        for i in range(size - 1):
+            if not check_left(output, up, right, n):
+                break
+            if output[up][max(0, right - 1)] == 1:
+                break
+            output[up][right] = 1
+            if output[up][max(0, right - 2)] == 1:
+                break
+            right -= 1
+            move_left = True
+        size -= 2
+
+        for i in range(size - 1):
+            if output[max(0, up - 1)][right] == 1:
+                break
+            output[up][right] = 1
+            if output[max(0, up - 2)][right] == 1:
+                break
+            up -= 1
+        if size <= 0: break
+    return output
