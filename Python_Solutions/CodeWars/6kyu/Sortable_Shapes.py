de9dc0fc0a19b9c977a723073ee74465cfa8365b
Peter Kalugin <streetworkout200412@gmail.com>
2024-05-17 17:03:03 +0300

    Added description "Sortable Shapes"

---
+++ Python_Solutions/CodeWars/6kyu/Sortable_Shapes.py
@@ -0,0 +1,25 @@
+# Although shapes can be very different by nature, they can be sorted by the size of their area.
+#
+# Task:
+#
+# Create different shapes that can be part of a sortable list. The sort order is based on the size of their respective areas:
+# The area of a Square is the square of its side
+# The area of a Rectangle is width multiplied by height
+# The area of a Triangle is base multiplied by height divided by 2
+# The area of a Circle is the square of its radius multiplied by π
+# The area of a CustomShape is given
+#
+# The default sort order of a list of shapes is ascending on area size:
+# side = 1.1234
+# radius = 1.1234
+# base = 5
+# height = 2
+#
+# # All classes must be subclasses of the Shape class
+#
+# shapes: List[Shape] = [Square(side), Circle(radius), Triangle(base, height)]
+# shapes.sort()
+# Use the correct π constant for your circle area calculations:
+# math.pi
+#
+# SORTINGFUNDAMENTALSMATHEMATICSDESIGN PATTERNSARRAYSGEOMETRY
