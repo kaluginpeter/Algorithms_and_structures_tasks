6496a3e5137158b9a392f2ee4b429a455c2aefe2
Peter Kalugin <streetworkout200412@gmail.com>
2024-05-19 12:24:40 +0300

    Added solution "2236. Root Equals Sum of Children"

--- Python_Solutions/LeetCode/Easy/2236._Root_Equals_Sum_of_Children.py
+++ Python_Solutions/LeetCode/Easy/2236._Root_Equals_Sum_of_Children.py
@@ -24,3 +24,13 @@
 #
 # The tree consists only of the root, its left child, and its right child.
 # -100 <= Node.val <= 100
+# Solution O(1) O(1)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def checkTree(self, root: Optional[TreeNode]) -> bool:
+        return root.val == root.left.val + root.right.val
