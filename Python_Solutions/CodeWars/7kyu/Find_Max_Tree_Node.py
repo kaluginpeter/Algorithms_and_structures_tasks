72abdbfafe930033791a94c30b7437c319e894ce
Peter Kalugin <streetworkout200412@gmail.com>
2024-05-22 07:01:09 +0300

    Added description "Find Max Tree Node"

---
+++ Python_Solutions/CodeWars/7kyu/Find_Max_Tree_Node.py
@@ -0,0 +1,25 @@
+# You are given a binary tree. Implement the method findMax which returns the maximal node value in the tree.
+#
+# For example, maximum in the following Tree is 11.
+#
+#               7
+#             /   \
+#            /     \
+#           5       2
+#            \       \
+#            6        11
+#            /\      /
+#           1  9   4
+# Note:
+#
+# Tree node values any integer value.
+# Tree can unbalanced and unsorted.
+# The root argument is never an empty tree.
+# You are given a tree node class as follows:
+#
+# class TreeNode:
+#     def __init__(self, value, left = None, right = None):
+#         self.left = left
+#         self.right = right
+#         self.value = value
+# ALGORITHMSRECURSIONBINARY SEARCH TREESBINARY
