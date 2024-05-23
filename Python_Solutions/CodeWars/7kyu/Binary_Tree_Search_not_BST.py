666b3fbff5b7b7e46c6698d1a302736a26700afe
Peter Kalugin <streetworkout200412@gmail.com>
2024-05-23 18:14:05 +0300

    Added solution "Binary Tree Search (not BST)"

--- Python_Solutions/CodeWars/7kyu/Binary_Tree_Search_not_BST.py
+++ Python_Solutions/CodeWars/7kyu/Binary_Tree_Search_not_BST.py
@@ -3,3 +3,18 @@
 # return True/true if the given number is in the tree
 # return False/false if it isn't
 # BINARY TREESFUNDAMENTALS
+# Solution BFS O(N) O(1)
+from __future__ import annotations
+from typing import Optional
+
+
+class Node:
+    def __init__(self, value: int, left: Optional[Node] = None, right: Optional[Node] = None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+
+def search(n: int, root: Optional[Node]) -> bool:
+    """ Determines if a value is in a binary tree (NOT bst) """
+    return root.value == n or search(n, root.left) or search(n, root.right) if root else False
