18043ce7ff803152b996a05d7bc455acdfd0f3be
Peter Kalugin <streetworkout200412@gmail.com>
2024-05-23 18:13:07 +0300

    Added description "Binary Tree Search (not BST)"

---
+++ Python_Solutions/CodeWars/7kyu/Binary_Tree_Search_not_BST.py
@@ -0,0 +1,5 @@
+# Given a number and a binary tree ( not a Binary Search Tree! ):
+#
+# return True/true if the given number is in the tree
+# return False/false if it isn't
+# BINARY TREESFUNDAMENTALS
