c90966d59b44725f77b909ab14ab4ef247d61f2f
Peter Kalugin <streetworkout200412@gmail.com>
2024-05-24 15:45:54 +0300

    Added solution "A. Неправильные горы"

--- Yandex/Practicum/Python_Developer_Algorithms_and_Data_Structures/A._Неправильные_горы.py
+++ Yandex/Practicum/Python_Developer_Algorithms_and_Data_Structures/A._Неправильные_горы.py
@@ -29,3 +29,40 @@
 # Ввод	Вывод
 # 0 3 2 1
 # True
+# Solution One Pass Time O(N) Memory O(1)
+import sys
+
+
+def parse_input() -> list[int]:
+    output: list[int] = list(map(int, sys.stdin.readline().rstrip().split()))
+    return output
+
+
+def check_mountain_sequence(sequence: list) -> bool:
+    if len(sequence) < 2: return True
+    increase: bool = sequence[0] < sequence[1]
+    decrease: bool = sequence[0] > sequence[1]
+    if not increase: return False
+    for high in range(1, len(sequence)):
+        if sequence[high] > sequence[high - 1]:
+            if not increase: return False
+        elif sequence[high] < sequence[high - 1]:
+            if increase:
+                increase, decrease = False, True
+        else:
+            return False
+    return decrease
+
+
+def print_answer(answer: bool) -> None:
+    sys.stdout.write(str(answer))
+
+
+def solution() -> None:
+    mountain: list[int] = parse_input()
+    answer: bool = check_mountain_sequence(mountain)
+    print_answer(answer)
+
+
+if __name__ == '__main__':
+    solution()
