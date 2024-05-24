640ea53d63bcddfb4c194584b31f156769030cba
Peter Kalugin <streetworkout200412@gmail.com>
2024-05-24 15:47:30 +0300

    Added solution "B. Лишняя задача"

--- Yandex/Practicum/Python_Developer_Algorithms_and_Data_Structures/B._Лишняя_задача.py
+++ Yandex/Practicum/Python_Developer_Algorithms_and_Data_Structures/B._Лишняя_задача.py
@@ -79,3 +79,42 @@
 # Имя solution.py использовать нельзя, назовите, например, my_solution.py
 # Формат вывода
 # Функция должна вернуть голову связного списка, в котором удалён нужный элемент.
+# Solution Time O(N) Memory O(1)
+import os
+LOCAL = os.environ.get('REMOTE_JUDGE', 'false') != 'true'
+if LOCAL:
+    class Node:
+        def __init__(self, value, next_item=None):
+            self.value = value
+            self.next_item = next_item
+
+
+def solution(node, idx):
+    if idx == 0:
+        node = node.next_item
+        return node
+
+    inner_node = node
+    while idx - 1 > 0:
+        inner_node = inner_node.next_item
+        idx -= 1
+    inner_node.next_item = inner_node.next_item.next_item
+    return node
+
+
+def test():
+    node3 = Node("Задача 4: Обследовать грунт в радиусе 3 м", None)
+    node2 = Node("Задача 3: Измерить температуру атмосферы", node3)
+    node1 = Node("Задача 2: Пробурить скважину глубиной 0.5 м", node2)
+    node0 = Node("Задача 1: Фотосъёмка 360°", node1)
+
+    new_head = solution(node0, 1)
+    assert new_head is node0
+    assert new_head.next_item is node2
+    assert new_head.next_item.next_item is node3
+    assert new_head.next_item.next_item.next_item is None
+    # result is node0 -> node2 -> node3
+
+
+if __name__ == '__main__':
+    test()
