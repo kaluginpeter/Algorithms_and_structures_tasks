67ef18c288f095e688a6e0f43001bd8e9a93daa6
Peter Kalugin <streetworkout200412@gmail.com>
2024-05-26 14:06:12 +0300

    Added solution "A. Служба доставки"

--- Yandex/Practicum/Python_Developer_Algorithms_and_Data_Structures/A._Служба_доставки.py
+++ Yandex/Practicum/Python_Developer_Algorithms_and_Data_Structures/A._Служба_доставки.py
@@ -42,3 +42,131 @@
 # 3 5 3 4
 # 5
 # 4
+# First review Time O(NlogN) Memory O(1)
+import array
+from typing import Tuple, List
+from collections.abc import MutableSequence
+
+
+def parse_input_data() -> Tuple[MutableSequence[int], int]:
+    """
+    Parse input data from file and return it.
+    File should name as 'input.txt'
+    :return tuple with first argument is array of int and second argument is integer:
+    """
+    with open('input.txt', 'r') as input_file:
+        lines: List[str, str] = input_file.readlines()
+
+    iterable_sequence: iter = map(int, lines[0].rstrip().split())
+    output_array: MutableSequence[int] = array.array('i', sorted(iterable_sequence))
+    output_number: int = int(lines[1].rstrip())
+    return output_array, output_number
+
+
+def find_answer_in_sequence(sequence: MutableSequence[int], limit: int, boundary: int) -> int:
+    """
+    Return maximum number of platforms that should be used for storing all robots.
+    :param sequence: Array of int representing weight of each robot.
+    :param limit: max size that can be stored in one platform.
+    :param boundary: length of sequence.
+    :return: integer representing answer.
+    General idea:
+    Sort sequence in increasing order and use two pointers with greedy approach.
+    So we have two decision:
+    - Most heavy and the lightest weight can store together at one platform,
+        if it is, we move both pointers
+    - Platform can story only most heavy robot,
+        so we just need move right pointer.
+    """
+    platform: int = 0
+    left_boundary: int = 0
+    right_boundary: int = boundary - 1
+    while left_boundary <= right_boundary:
+        # Condition if both robots can be store at one platform
+        if sequence[left_boundary] + sequence[right_boundary] <= limit:
+            left_boundary, right_boundary = left_boundary + 1, right_boundary - 1
+        # Otherwise it means that platform can store only one robot
+        else:
+            right_boundary -= 1
+        platform += 1
+    return platform
+
+
+def print_answer(answer: int) -> None:
+    """
+    Write answer in file.
+    Output file has name 'output.txt'
+    :param answer: integer representing answer
+    :return: None
+    """
+    with open('output.txt', 'w') as output_file:
+        output_file.write(str(answer))
+
+
+def solution() -> None:
+    """
+    Solution use Two Pointers, Sorting and Greedy approaches.
+    Time complexity is O(NlogN)
+    Memory complexity is O(1)
+    :return: None
+    """
+    sequence: MutableSequence[int]
+    limit: int
+    sequence, limit = parse_input_data()
+    length_of_sequence: int = len(sequence)
+    answer: int = find_answer_in_sequence(sequence, limit, length_of_sequence)
+    print_answer(answer)
+
+
+if __name__ == '__main__':
+    solution()
+
+# Second review Time O(NlogN) Memory O(1)
+# Id of successfully attempt - 114609905
+from collections.abc import MutableSequence
+
+def minimum_amount_platform_to_store_robots(
+    weights: MutableSequence[int], limit: int
+) -> int:
+    """
+    Return maximum number of platforms that should be used for storing all robots.
+    :param weights: Array of objects representing weight of each robot.
+    :param limit: max size that can be stored in one platform.
+    :return: integer representing minimum amount of platforms used to store all robots.
+    General idea:
+    Sort sequence in increasing order and use two pointers with greedy approach.
+    So we have two decision:
+    - Most heavy and the lightest weight robots can store together at one platform,
+        if it is, we move both pointers
+    - Platform can story only most heavy robot,
+        so we just need move right pointer.
+    """
+    # Sorting in increasing order, where (i0 <= i1 <= ... <= in-1 <= in)
+    weights = sorted(weights)
+    platform: int = 0
+    lightwest_robot_pointer: int = 0
+    # The highest robot will be at the end of sequence
+    highest_robot_pointer: int = len(weights) - 1
+    while lightwest_robot_pointer <= highest_robot_pointer:
+        # Condition if both robots can be store at one platform
+        if (
+            weights[lightwest_robot_pointer]
+            + weights[highest_robot_pointer] <= limit
+        ):
+            lightwest_robot_pointer += 1
+        highest_robot_pointer -= 1
+        platform += 1
+    return platform
+
+
+if __name__ == '__main__':
+    """
+    Time complexity O(NlogN)
+    Memory complexity O(1)
+    Algorithms: Two Pointers, Greedy, Sorting
+    """
+    print(
+        minimum_amount_platform_to_store_robots(
+            list(map(int, input().split())), int(input())
+        )
+    )
