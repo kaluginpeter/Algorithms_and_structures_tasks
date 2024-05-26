e5aa87a9c7037fbf230c1d7f2577db3683be5435
Peter Kalugin <streetworkout200412@gmail.com>
2024-05-26 08:44:07 +0300

    Added description "Bug Fix - Quick Sort"

---
+++ Python_Solutions/CodeWars/6kyu/Bug_Fix_Quick_Sort.py
@@ -0,0 +1,5 @@
+# There is an implementation of quicksort algorithm. Your task is to fix it. All inputs are correct.
+#
+# See also: https://en.wikipedia.org/wiki/Quicksort
+#
+# SORTINGALGORITHMSFUNDAMENTALSDEBUGGING
