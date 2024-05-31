0fb12dfaa9a5988c2612301db1f1074cc20e0ceb
Peter Kalugin <streetworkout200412@gmail.com>
2024-05-31 06:47:01 +0300

    Added description "How long should you cook this for?"

---
+++ Python_Solutions/CodeWars/7kyu/How_long_should_you_cook_this_for.py
@@ -0,0 +1,34 @@
+# You've purchased a ready-meal from the supermarket.
+#
+# The packaging says that you should microwave it for 4 minutes and 20 seconds, based on a 600W microwave.
+#
+# Oh no, your microwave is 800W! How long should you cook this for?!
+#
+# Input
+# You'll be given 4 arguments:
+#
+# 1. needed power
+# The power of the needed microwave.
+# Example: "600W"
+#
+# 2. minutes
+# The number of minutes shown on the package.
+# Example: 4
+#
+# 3. seconds
+# The number of seconds shown on the package.
+# Example: 20
+#
+# 4. power
+# The power of your microwave.
+# Example: "800W"
+#
+# Output
+# The amount of time you should cook the meal for formatted as a string.
+# Example: "3 minutes 15 seconds"
+#
+# Note: the result should be rounded up.
+#
+# 59.2 sec  -->  60 sec  -->  return "1 minutes 0 seconds"
+# All comments/feedback/translations appreciated.
+# FUNDAMENTALS
