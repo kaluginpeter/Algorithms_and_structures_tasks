4802ac85802c36d00c485d72854fa7e7c0859a38
Peter Kalugin <streetworkout200412@gmail.com>
2024-06-01 09:26:15 +0300

    Added solution "17. Letter Combinations of a Phone Number"

--- Python_Solutions/LeetCode/Medium/17._Letter_Combinations_of_a_Phone_Number.py
+++ Python_Solutions/LeetCode/Medium/17._Letter_Combinations_of_a_Phone_Number.py
@@ -23,3 +23,61 @@
 #
 # 0 <= digits.length <= 4
 # digits[i] is a digit in the range ['2', '9'].
+# Brute Force
+# Complexity
+# Time complexity: O(N**K)
+# Space complexity: O(N**K)
+# Where N is length of letters in digits and K is length of digits
+
+# Code
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits: return []
+        digits_store: dict[str, str] = {
+            '2': 'abc', '3': 'def', '4': 'ghi',
+            '5': 'jkl', '6': 'mno', '7': 'pqrs',
+            '8': 'tuv', '9': 'wxyz'
+        }
+        output: list[str] = list()
+        if len(digits) == 1:
+            return list(digits_store.get(digits))
+        elif len(digits) == 2:
+            for i in digits_store.get(digits[0]):
+                for j in digits_store.get(digits[1]):
+                    output.append(i + j)
+        elif len(digits) == 3:
+            for i in digits_store.get(digits[0]):
+                for j in digits_store.get(digits[1]):
+                    for k in digits_store.get(digits[2]):
+                        output.append(i + j + k)
+        elif len(digits) == 4:
+            for i in digits_store.get(digits[0]):
+                for j in digits_store.get(digits[1]):
+                    for k in digits_store.get(digits[2]):
+                        for l  in digits_store.get(digits[3]):
+                            output.append(i + j + k + l)
+        return output
+
+# Backtracking
+# Complexity
+# Time complexity: O(N**K)
+# Space complexity: O(N**K)
+# Where N is length of letters in digits and K is length of digits
+
+# Code
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        def backtracking(idx: int, current_string: str) -> None:
+            if len(current_string) == len(digits):
+                output.append(current_string)
+                return
+            for letter in digits_store.get(digits[idx]):
+                backtracking(idx + 1, current_string + letter)
+        digits_store: dict[str, str] = {
+            '2': 'abc', '3': 'def', '4': 'ghi',
+            '5': 'jkl', '6': 'mno', '7': 'pqrs',
+            '8': 'tuv', '9': 'wxyz'
+        }
+        output: list[str] = list()
+        if digits: backtracking(0, '')
+        return output
