94af4985e987e58ba85dce99b938a1fe79741098
Peter Kalugin <streetworkout200412@gmail.com>
2024-06-02 16:37:45 +0300

    Added solution "Задача «Обойди дерево»"

--- Yandex/Practicum/Python_Developer_Algorithms_and_Data_Structures/Обойди_дерево.py
+++ Yandex/Practicum/Python_Developer_Algorithms_and_Data_Structures/Обойди_дерево.py
@@ -17,3 +17,55 @@
 # Решением должен быть рекурсивный обход дерева:
 # в каждом следующем уровне рекурсии должны обрабатываться узлы,
 # хранящиеся в атрибутах father и mother текущего узла.
+# Solution Breadth First Search Time O(N) Memory O(N)
+from __future__ import annotations
+from typing import Optional
+
+
+class Spaceman:
+
+    def __init__(
+            self,
+            name: str,
+            space_experience: int,
+            father: Optional[Spaceman] = None,
+            mother: Optional[Spaceman] = None,
+    ):
+        self.name = name
+        self.space_experience = space_experience
+        self.father = father
+        self.mother = mother
+
+
+class DynastyExperienceCounter:
+
+    def __init__(self, spaceman: Spaceman):
+        self.root = spaceman
+        self.total_experience: int = 0
+
+    def count_dynasty_experience(self, root=None):
+        if not root:
+            return 0
+        # Доработайте метод, чтобы он считал
+        # суммарный опыт династии космонавтов.
+        self.total_experience += root.space_experience
+        self.count_dynasty_experience(root.father)
+        self.count_dynasty_experience(root.mother)
+        return self.total_experience
+
+yu_a_makarin = Spaceman(
+    name='Юрий Алексеевич Макарин',
+    space_experience=10,
+    father=Spaceman(
+        name='Алексей Михайлович Макарин',
+        space_experience=25,
+        mother=Spaceman(
+            name='Евгения Владимировна Беляева',
+            space_experience=1
+        )
+    ),
+    mother=Spaceman('Ангелина Васильевна Черенкова', 5)
+)
+counter = DynastyExperienceCounter(yu_a_makarin)
+result = counter.count_dynasty_experience(counter.root)
+print(result)
