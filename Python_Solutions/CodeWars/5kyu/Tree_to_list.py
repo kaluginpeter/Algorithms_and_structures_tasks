05898fbe424421e1acb23d72f4a9e1b54407d8b0
Peter Kalugin <streetworkout200412@gmail.com>
2024-06-04 07:53:15 +0300

    Added solution "Tree to list"

--- Python_Solutions/CodeWars/5kyu/Tree_to_list.py
+++ Python_Solutions/CodeWars/5kyu/Tree_to_list.py
@@ -10,3 +10,21 @@
 #         /|\   \
 #        4 5 6   7
 # TREESFUNDAMENTALS
+# Solution Breadth First Search O(N) O(N)
+class Node:
+    def __init__(self, data, child_nodes=None):
+        self.data = data
+        self.child_nodes = [] if child_nodes is None else child_nodes
+
+def tree_to_list(tree_root):
+    current: list = [tree_root]
+    nxt: list = list()
+    answer: list = list()
+    while current:
+        for node in current:
+            if not node: continue
+            answer.append(node.data)
+            nxt.extend(node.child_nodes)
+        current = nxt
+        nxt = []
+    return answer
