bd9e772ee3fdae66b741cdeb8bb05f1b6979949d
Peter Kalugin <streetworkout200412@gmail.com>
2024-06-04 07:52:02 +0300

    Added description "Tree to list"

---
+++ Python_Solutions/CodeWars/5kyu/Tree_to_list.py
@@ -0,0 +1,12 @@
+# Given the root of a tree with an arbitrary number of child nodes, return a list containing the nodes' data in breadth-first order (left to right, top to bottom).
+#
+# Child nodes are stored in a list. An empty tree is represented by an empty list.
+#
+# Example:
+#
+#            1
+#           / \
+#          2   3  -> [1,2,3,4,5,6,7]
+#         /|\   \
+#        4 5 6   7
+# TREESFUNDAMENTALS
