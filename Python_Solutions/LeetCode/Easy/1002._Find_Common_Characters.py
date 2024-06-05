0619de18c77f27f3f71e64ce54d1400b057dbc5d
Peter Kalugin <streetworkout200412@gmail.com>
2024-06-05 07:05:54 +0300

    Added description "1002. Find Common Characters"

--- Python_Solutions/LeetCode/Easy/1002._Find_Common_Characters.py
+++ Python_Solutions/LeetCode/Easy/1002._Find_Common_Characters.py
@@ -26,3 +26,22 @@ class Solution:
                 c = min(j.count(i) for j in words)
                 l += [i] * c
         return l
+
+# Solution Array HashTable O(N) O(N)
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        letter_storage: list[list[int]] = [[] for _ in range(27)] # total storage
+        n: int = 0 # length of words
+        for word in words:
+            n += 1
+            # local storage to each word
+            local_letter_storage: list[int] = [0 for _ in range(27)]
+            for letter in word: # count all frequences of each charackter in one word
+                local_letter_storage[ord(letter) - 96] += 1
+            for char in range(27): # Adding result of local storage to total storage
+                letter_storage[char] += [local_letter_storage[char]]
+        common_characters: list[int] = [] # Answer array
+        for char in range(27):
+            if len(letter_storage[char]) == n: # if character contained in all words
+                common_characters.extend([chr(char + 96)] * min(letter_storage[char])) # addind minimum frequence
+        return common_characters
