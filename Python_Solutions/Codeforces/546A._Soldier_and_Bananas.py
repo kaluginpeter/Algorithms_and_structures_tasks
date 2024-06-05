af1564e1c9b9e3bf629c78cc8ff346eccdec3fc1
Peter Kalugin <streetworkout200412@gmail.com>
2024-06-05 07:08:30 +0300

    Added solution "546A. Soldier and Bananas"

--- Python_Solutions/Codeforces/546A._Soldier_and_Bananas.py
+++ Python_Solutions/Codeforces/546A._Soldier_and_Bananas.py
@@ -18,3 +18,10 @@
 # 3 17 4
 # outputCopy
 # 13
+# Solution Math O(w) O(1)
+import sys
+k, n, w = map(int, sys.stdin.readline().rstrip().split())
+total: int = 0
+for i in range(1, w + 1):
+    total += i * k
+sys.stdout.write(str(max(0, total - n)))
