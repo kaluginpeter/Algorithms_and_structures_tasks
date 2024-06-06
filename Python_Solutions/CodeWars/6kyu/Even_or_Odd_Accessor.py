06edb5f81f3968f796fe484b23569d8af06e7a8c
Peter Kalugin <streetworkout200412@gmail.com>
2024-06-06 08:23:21 +0300

    Added description "Even or Odd Accessor"

---
+++ Python_Solutions/CodeWars/6kyu/Even_or_Odd_Accessor.py
@@ -0,0 +1,9 @@
+# Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers. The function should also return "Even" or "Odd" when accessing a value at an integer index.
+#
+# For example:
+#
+# evenOrOdd(2); //'Even'
+# evenOrOdd[2]; //'Even'
+# evenOrOdd(7); //'Odd'
+# evenOrOdd[7]; //'Odd'
+# MATHEMATICSFUNDAMENTALS
