924b7c6960fee7047ab7414c01f0d9568a288b0d
Peter Kalugin <streetworkout200412@gmail.com>
2024-06-06 08:23:59 +0300

    Added solution "Even or Odd Accessor"

--- Python_Solutions/CodeWars/6kyu/Even_or_Odd_Accessor.py
+++ Python_Solutions/CodeWars/6kyu/Even_or_Odd_Accessor.py
@@ -7,3 +7,11 @@
 # evenOrOdd(7); //'Odd'
 # evenOrOdd[7]; //'Odd'
 # MATHEMATICSFUNDAMENTALS
+# Solution
+class EvenOrOdd:
+    def __call__(self, number):
+        return "Even" if number % 2 == 0 else "Odd"
+
+    def __getitem__(self, number):
+        return self(number)
+even_or_odd = EvenOrOdd()
