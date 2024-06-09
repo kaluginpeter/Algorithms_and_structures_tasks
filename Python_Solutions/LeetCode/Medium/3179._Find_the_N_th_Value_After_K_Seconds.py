9849afcfae40637b419f2a763c26b46ef9ed97f5
Peter Kalugin <streetworkout200412@gmail.com>
2024-06-09 07:22:24 +0300

    Added solution description "3179. Find the N-th Value After K Seconds"

--- Python_Solutions/LeetCode/Medium/3179._Find_the_N_th_Value_After_K_Seconds.py
+++ Python_Solutions/LeetCode/Medium/3179._Find_the_N_th_Value_After_K_Seconds.py
@@ -41,3 +41,13 @@
 # Constraints:
 #
 # 1 <= n, k <= 1000
+# Solution Simulation O(NK) O(N)
+class Solution:
+    def fill_array(self, arr: list[int]) -> None:
+        for i in range(len(arr)):
+            arr[i] = arr[i] + (arr[i - 1] if i - 1 >= 0 else 0)
+    def valueAfterKSeconds(self, n: int, k: int) -> int:
+        prefix_sum: list[int] = [1 for _ in range(n)]
+        for _ in range(k):
+            self.fill_array(prefix_sum)
+        return prefix_sum[-1] % (10**9+7)
