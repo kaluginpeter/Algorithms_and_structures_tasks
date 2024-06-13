376317e3ee1f6c7bfa1f7a9ff0fc659bd1e1edd6
Peter Kalugin <streetworkout200412@gmail.com>
2024-06-13 18:53:53 +0300

    Added description "Path Finder #1: can you reach the exit?"

---
+++ Python_Solutions/CodeWars/4kyu/Path_Finder_1_can_you_reach_the_exit.py
@@ -0,0 +1,13 @@
+# Task
+# You are at position [0, 0] in maze NxN and you can only move in one of the four cardinal directions (i.e. North, East, South, West). Return true if you can reach position [N-1, N-1] or false otherwise.
+#
+# Empty positions are marked ..
+# Walls are marked W.
+# Start and exit positions are empty in all test cases.
+# Path Finder Series:
+# #1: can you reach the exit?
+# #2: shortest path
+# #3: the Alpinist
+# #4: where are you?
+# #5: there's someone here
+# ALGORITHMS
