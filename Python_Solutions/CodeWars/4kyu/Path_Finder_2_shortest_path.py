6257f0682c0c701fc706646f6ac9e266c9abf455
Peter Kalugin <streetworkout200412@gmail.com>
2024-06-14 17:23:35 +0300

    Added description "Path Finder #2: shortest path"

---
+++ Python_Solutions/CodeWars/4kyu/Path_Finder_2_shortest_path.py
@@ -0,0 +1,12 @@
+# Task
+# You are at position [0, 0] in maze NxN and you can only move in one of the four cardinal directions (i.e. North, East, South, West). Return the minimal number of steps to exit position [N-1, N-1] if it is possible to reach the exit from the starting position. Otherwise, return false.
+#
+# Empty positions are marked .. Walls are marked W. Start and exit positions are guaranteed to be empty in all test cases.
+#
+# Path Finder Series:
+# #1: can you reach the exit?
+# #2: shortest path
+# #3: the Alpinist
+# #4: where are you?
+# #5: there's someone here
+# ALGORITHMS
