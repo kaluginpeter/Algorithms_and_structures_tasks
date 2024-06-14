2bfbdbf86b37353fcd83ea36066fed12cce50be1
Peter Kalugin <streetworkout200412@gmail.com>
2024-06-14 17:23:51 +0300

    Added solution "Path Finder #2: shortest path"

--- Python_Solutions/CodeWars/4kyu/Path_Finder_2_shortest_path.py
+++ Python_Solutions/CodeWars/4kyu/Path_Finder_2_shortest_path.py
@@ -10,3 +10,27 @@
 # #4: where are you?
 # #5: there's someone here
 # ALGORITHMS
+# Solution
+from collections import deque
+def path_finder(maze):
+    maze = maze.split('\n')
+    queue = deque()
+    seen = set()
+    queue.append((0, 0))
+    seen.add((0, 0))
+    moves = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    length = 0
+    n = len(maze)
+    while queue:
+        for _ in range(len(queue)):
+            cur_r, cur_c = queue.popleft()
+            if cur_r == cur_c == n - 1:
+                return length
+            for move in moves:
+                nxt_r, nxt_c = cur_r + move[0], cur_c + move[1]
+                if min(nxt_r, nxt_c) < 0 or max(nxt_r, nxt_c) >= n or maze[nxt_r][nxt_c] == 'W' or (nxt_r, nxt_c) in seen:
+                    continue
+                queue.append((nxt_r, nxt_c))
+                seen.add((nxt_r, nxt_c))
+        length += 1
+    return False
