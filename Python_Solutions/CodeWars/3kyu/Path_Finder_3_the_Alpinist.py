de244e9ef863db2e797661faecee2fc9a2617b25
Peter Kalugin <streetworkout200412@gmail.com>
2024-06-15 17:19:09 +0300

    Added description "Path Finder #3: the Alpinist"

---
+++ Python_Solutions/CodeWars/3kyu/Path_Finder_3_the_Alpinist.py
@@ -0,0 +1,12 @@
+# Task
+# You are at start location [0, 0] in mountain area of NxN and you can only move in one of the four cardinal directions (i.e. North, East, South, West). Return minimal number of climb rounds to target location [N-1, N-1]. Number of climb rounds between adjacent locations is defined as difference of location altitudes (ascending or descending).
+#
+# Location altitude is defined as an integer number (0-9).
+#
+# Path Finder Series:
+# #1: can you reach the exit?
+# #2: shortest path
+# #3: the Alpinist
+# #4: where are you?
+# #5: there's someone here
+# ALGORITHMS
