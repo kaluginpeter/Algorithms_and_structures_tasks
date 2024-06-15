210e02965ef7f9d7935452925a235aeac572e8ec
Peter Kalugin <streetworkout200412@gmail.com>
2024-06-15 17:20:32 +0300

    Added solution "Path Finder #3: the Alpinist"

--- Python_Solutions/CodeWars/3kyu/Path_Finder_3_the_Alpinist.py
+++ Python_Solutions/CodeWars/3kyu/Path_Finder_3_the_Alpinist.py
@@ -10,3 +10,24 @@
 # #4: where are you?
 # #5: there's someone here
 # ALGORITHMS
+# Solution
+from heapq import heappop, heappush
+def path_finder(area):
+    mtrx = [list(map(int, line)) for line in area.split('\n')]
+    n = len(mtrx)
+    heap = [(0, 0, 0)]
+    costs = [[float('inf')] * n for _ in range(n)]
+    costs[0][0] = 0
+    directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+    while heap:
+        current_cost, cur_r, cur_c = heappop(heap)
+        if cur_r == cur_c == n - 1:
+            return current_cost
+        for dr, dc in directions:
+            nxt_r, nxt_c = cur_r + dr, cur_c + dc
+            if 0 <= nxt_r < n and 0 <= nxt_c < n:
+                climb_cost = abs(mtrx[nxt_r][nxt_c] - mtrx[cur_r][cur_c])
+                new_cost = current_cost + climb_cost
+                if new_cost < costs[nxt_r][nxt_c]:
+                    costs[nxt_r][nxt_c] = new_cost
+                    heappush(heap, (new_cost, nxt_r, nxt_c))
