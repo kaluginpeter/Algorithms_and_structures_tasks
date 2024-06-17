9b8acf77a7c71e7b770f7bc4f46f9275b06d45d4
Peter Kalugin <streetworkout200412@gmail.com>
2024-06-17 06:48:19 +0300

    Added solution "633. Sum of Square Numbers"

--- Python_Solutions/LeetCode/Medium/633._Sum_of_Square_Numbers.py
+++ Python_Solutions/LeetCode/Medium/633._Sum_of_Square_Numbers.py
@@ -1,4 +1,3 @@
-
 # Given a non-negative integer c, decide whether there're two integers a and b such that a2 + b2 = c.
 #
 #
@@ -17,3 +16,16 @@
 # Constraints:
 #
 # 0 <= c <= 231 - 1
+# Solution Binary Search Math O(sqrt(N)) O(1)
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        left, right = 0, int(c**.5) + 1
+        while left <= right:
+            sm: int = left**2 + right**2
+            if sm == c:
+                return True
+            elif sm > c:
+                right -= 1
+            else:
+                left += 1
+        return False
