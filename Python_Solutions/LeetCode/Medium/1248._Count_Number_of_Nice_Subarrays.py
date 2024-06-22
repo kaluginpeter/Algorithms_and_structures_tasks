17bb5097a4794d5b28a0fb258aefc332ca5d86ce
Peter Kalugin <streetworkout200412@gmail.com>
2024-06-22 15:35:19 +0300

    Added solution "1248. Count Number of Nice Subarrays"

--- Python_Solutions/LeetCode/Medium/1248._Count_Number_of_Nice_Subarrays.py
+++ Python_Solutions/LeetCode/Medium/1248._Count_Number_of_Nice_Subarrays.py
@@ -25,3 +25,41 @@
 # 1 <= nums.length <= 50000
 # 1 <= nums[i] <= 10^5
 # 1 <= k <= nums.length
+# Solutions
+# HashTable and PrefixSum
+# Complexity
+# Time complexity: O(N)
+# Space complexity: O(N)
+class Solution:
+    def numberOfSubarrays(self, nums: List[int], k: int) -> int:
+        for idx in range(len(nums)):
+            nums[idx] = 1 if nums[idx] % 2 != 0 else 0
+        prefix_sum: int = 0
+        ht: dict[int, int] = {0: 1}
+        total: int = 0
+        for cur in nums:
+            prefix_sum += cur
+            total += ht.get(prefix_sum - k, 0)
+            ht[prefix_sum] = ht.get(prefix_sum, 0) + 1
+        return total
+
+# Sliding Window
+# Complexity
+# Time complexity: O(N)
+# Space complexity: O(1)
+class Solution:
+    def numberOfSubarrays(self, nums: List[int], k: int) -> int:
+        total_subarrays: int = 0
+        possible_variations: int = 0
+        odd_numbers_in_subarray: int = 0
+        left_pointer: int = 0
+        for right_pointer in range(len(nums)):
+            odd_numbers_in_subarray += nums[right_pointer] & 1
+            if odd_numbers_in_subarray == k:
+                possible_variations = 0
+                while odd_numbers_in_subarray == k:
+                    odd_numbers_in_subarray -= nums[left_pointer] & 1
+                    possible_variations += 1
+                    left_pointer += 1
+            total_subarrays += possible_variations
+        return total_subarrays
