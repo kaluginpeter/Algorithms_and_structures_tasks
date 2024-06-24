bea920512d67ab67d8ca3e86dcbdbd8a0458180c
Peter Kalugin <streetworkout200412@gmail.com>
2024-06-24 06:54:57 +0300

    Added solution "995. Minimum Number of K Consecutive Bit Flips"

--- Python_Solutions/LeetCode/Hard/995._Minimum_Number_of_K_Consecutive_Bit_Flips.py
+++ Python_Solutions/LeetCode/Hard/995._Minimum_Number_of_K_Consecutive_Bit_Flips.py
@@ -32,3 +32,20 @@
 #
 # 1 <= nums.length <= 105
 # 1 <= k <= nums.length
+# Solution Sliding Window Deque O(N) O(N)
+from collections import deque
+class Solution:
+    def minKBitFlips(self, nums: List[int], k: int) -> int:
+        periods = deque()
+        total_flips: int = 0
+        for idx in range(len(nums)):
+            while periods and idx > periods[0]:
+                periods.popleft()
+            nums[idx] = [nums[idx], int(not nums[idx])][len(periods) % 2]
+            if nums[idx] == 0 and idx < len(nums) - k + 1:
+                total_flips += 1
+                nums[idx] = 1
+                periods.append(idx + k - 1)
+            elif nums[idx] == 0:
+                return -1
+        return total_flips
