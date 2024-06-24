cad60469549ded47bdaf34f1fb8b449bc4312724
Peter Kalugin <streetworkout200412@gmail.com>
2024-06-24 18:11:18 +0300

    Added description "Simple Fun #182: Happy "g""

---
+++ Python_Solutions/CodeWars/7kyu/Simple_Fun_182_Happy_g.py
@@ -0,0 +1,18 @@
+# Task
+# Let's say that "g" is happy in the given string, if there is another "g" immediately to the right or to the left of it.
+#
+# Find out if all "g"s in the given string are happy.
+#
+# Example
+# For str = "gg0gg3gg0gg", the output should be true.
+#
+# For str = "gog", the output should be false.
+#
+# Input/Output
+# [input] string str
+# A random string of lower case letters, numbers and spaces.
+#
+# [output] a boolean value
+# true if all "g"s are happy, false otherwise.
+#
+# PUZZLESSTRINGSREGULAR EXPRESSIONS
