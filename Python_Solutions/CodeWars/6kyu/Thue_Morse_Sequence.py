532789158b78a9a38b5279cb9899a0a0a56f6b85
Peter Kalugin <streetworkout200412@gmail.com>
2024-06-28 17:01:40 +0300

    Added description "Thue-Morse Sequence"

---
+++ Python_Solutions/CodeWars/6kyu/Thue_Morse_Sequence.py
@@ -0,0 +1,26 @@
+# Given a positive integer n, return first n dgits of Thue-Morse sequence, as a string (see examples).
+#
+# Thue-Morse sequence is a binary sequence with 0 as the first element. The rest of the sequence is obtained by adding the boolean (binary) complement of a group obtained so far.
+#
+# For example:
+#
+# 0
+# 01
+# 0110
+# 01101001
+# and so on...
+# alt
+#
+# Ex.:
+#
+#  1 --> "0"
+#  2 --> "01"
+#  5 --> "01101"
+# 10 --> "0110100110"
+# You don't need to test if n is valid - it will always be a positive integer.
+# n will be between 1 and 10000
+# Thue-Morse on Wikipedia
+#
+# Another kata on Thue-Morse by @myjinxin2015
+#
+# STRINGSBINARYALGORITHMS
