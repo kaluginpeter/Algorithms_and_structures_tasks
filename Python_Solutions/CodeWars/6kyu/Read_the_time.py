a08f83823c1baed43a483ee4a3e5bbb829a07484
Peter Kalugin <streetworkout200412@gmail.com>
2024-06-29 11:38:48 +0300

    Added description "Read the time"

---
+++ Python_Solutions/CodeWars/6kyu/Read_the_time.py
@@ -0,0 +1,19 @@
+# Given time in 24-hour format, convert it to words.
+#
+# For example:
+# 13:00 = one o'clock
+# 13:09 = nine minutes past one
+# 13:15 = quarter past one
+# 13:29 = twenty nine minutes past one
+# 13:30 = half past one
+# 13:31 = twenty nine minutes to two
+# 13:45 = quarter to two
+# 00:48 = twelve minutes to one
+# 00:08 = eight minutes past midnight
+# 12:00 = twelve o'clock
+# 00:00 = midnight
+#
+# Note: If minutes == 0, use 'o'clock'. If minutes <= 30, use 'past', and for minutes > 30, use 'to'.
+# More examples in test cases. Good luck!
+#
+# FUNDAMENTALS
