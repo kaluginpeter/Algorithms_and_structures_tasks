93218001e5728d6297426da614cbb988789da1e6
Peter Kalugin <streetworkout200412@gmail.com>
2024-07-01 18:19:07 +0300

    Added solution "Pitches and Notes"

--- Python_Solutions/CodeWars/7kyu/Pitches_and_Notes.py
+++ Python_Solutions/CodeWars/7kyu/Pitches_and_Notes.py
@@ -25,3 +25,26 @@
 # Musicians: all pitches based on equal tempermanent, taken from here.
 #
 # FUNDAMENTALS
+# Solution
+def get_note(pitch):
+    notes_dictionary = {
+        440: "A",
+        466.16: "A#",
+        493.88: "B",
+        523.25: "C",
+        554.37: "C#",
+        587.33: "D",
+        622.25: "D#",
+        659.25: "E",
+        698.46: "F",
+        739.99: "F#",
+        783.99: "G",
+        830.61: "G#"
+    }
+    copy_pitch: int = pitch
+    while copy_pitch > 200:
+        if copy_pitch in notes_dictionary: return notes_dictionary[copy_pitch]
+        copy_pitch /= 2
+    while copy_pitch < 1000:
+        if copy_pitch in notes_dictionary: return notes_dictionary[copy_pitch]
+        copy_pitch *= 2
