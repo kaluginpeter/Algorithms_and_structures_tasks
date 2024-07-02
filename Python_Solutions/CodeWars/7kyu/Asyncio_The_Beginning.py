e00af5543f10f45a8ae8c83cb13f58169fe192e0
Peter Kalugin <streetworkout200412@gmail.com>
2024-07-02 18:14:26 +0300

    Added description "Asyncio - The Beginning"

---
+++ Python_Solutions/CodeWars/7kyu/Asyncio_The_Beginning.py
@@ -0,0 +1,23 @@
+# Story
+# As you walk the streets with your crush beside you, you are thinking about the world and how everything works... Wait!! Your crush? Shit, you are dreaming again.
+#
+# Task
+# Now implement a coroutine dreaming which sleeps for n seconds and then returns m ** n without entirely blocking the execution of other coroutines that might be running.
+#
+# Note
+# 0 < n < 7
+#
+# 0 < m < 100
+#
+# ASYNCHRONOUSStory
+# As you walk the streets with your crush beside you, you are thinking about the world and how everything works... Wait!! Your crush? Shit, you are dreaming again.
+#
+# Task
+# Now implement a coroutine dreaming which sleeps for n seconds and then returns m ** n without entirely blocking the execution of other coroutines that might be running.
+#
+# Note
+# 0 < n < 7
+#
+# 0 < m < 100
+#
+# ASYNCHRONOUS
