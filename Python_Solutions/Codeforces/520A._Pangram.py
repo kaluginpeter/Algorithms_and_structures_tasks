d4504f23976481125177f181255cdd226bac7c09
Peter Kalugin <streetworkout200412@gmail.com>
2024-07-04 17:15:34 +0300

    Added descirption "520A. Pangram"

---
+++ Python_Solutions/Codeforces/520A._Pangram.py
@@ -0,0 +1,28 @@
+# A. Pangram
+# time limit per test2 seconds
+# memory limit per test256 megabytes
+# inputstandard input
+# outputstandard output
+# A word or a sentence in some language is called a pangram if all the characters of the alphabet of this language appear in it at least once. Pangrams are often used to demonstrate fonts in printing or test the output devices.
+#
+# You are given a string consisting of lowercase and uppercase Latin letters. Check whether this string is a pangram. We say that the string contains a letter of the Latin alphabet if this letter occurs in the string in uppercase or lowercase.
+#
+# Input
+# The first line contains a single integer n (1 ≤ n ≤ 100) — the number of characters in the string.
+#
+# The second line contains the string. The string consists only of uppercase and lowercase Latin letters.
+#
+# Output
+# Output "YES", if the string is a pangram and "NO" otherwise.
+#
+# Examples
+# inputCopy
+# 12
+# toosmallword
+# outputCopy
+# NO
+# inputCopy
+# 35
+# TheQuickBrownFoxJumpsOverTheLazyDog
+# outputCopy
+# YES
