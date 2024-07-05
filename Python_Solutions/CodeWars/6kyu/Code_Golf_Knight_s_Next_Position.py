cfaf28b634ee92d4c253afe91c96cbe77f4f56b5
Peter Kalugin <streetworkout200412@gmail.com>
2024-07-05 17:37:40 +0300

    Added description "[Code Golf] Knight's Next Position"

---
+++ Python_Solutions/CodeWars/6kyu/Code_Golf_Knight_s_Next_Position.py
@@ -0,0 +1,16 @@
+# The knight is a piece in the game of chess. It may move two squares vertically and one square horizontally or two squares horizontally and one square vertically. For a visual representation of the possible moves, take a look here (second image).
+#
+# Given the current position of the knight on the chessboard (e.g."b8") return all its possible next positions, alphabetically sorted (e.g. ["a6", "c6", "d7"])
+#
+# Your code can be maximum 170 150 bytes long (I underestimated your skills ;-)
+#
+#
+# Note: this is the code golf version of this kata
+#
+# My other katas
+# If you enjoyed this kata then please try my other katas! :-)
+#
+# Translations are welcome!
+# ...except for Ruby, which will arrive soon
+#
+# RESTRICTEDPUZZLES
