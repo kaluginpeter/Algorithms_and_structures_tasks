91b4413e236ac3311da6aeb75ce89dd5db2821b6
Peter Kalugin <streetworkout200412@gmail.com>
2024-07-06 21:46:20 +0300

    Added solution "3208. Alternating Groups II"

--- Python_Solutions/LeetCode/Medium/3208._Alternating_Groups_II.py
+++ Python_Solutions/LeetCode/Medium/3208._Alternating_Groups_II.py
@@ -55,3 +55,18 @@
 # 3 <= colors.length <= 105
 # 0 <= colors[i] <= 1
 # 3 <= k <= colors.length
+# Solution Sliding Window O(N) O(N)
+class Solution:
+    def numberOfAlternatingGroups(self, colors: List[int], k: int) -> int:
+        colors: list[int] = colors[-k + 1:] + colors
+        groups: int = 0
+        left_pointer: int = 0
+        prev_pointer: int = 0
+        for right in range(len(colors)):
+            while colors[right] == colors[prev_pointer] and left_pointer < right:
+                left_pointer += 1
+            prev_pointer = right
+
+            if right - left_pointer + 1 >= k:
+                groups += 1
+        return groups
