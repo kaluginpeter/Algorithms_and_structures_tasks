e6f608b68edb30e62551c78d34674080b2697558
Peter Kalugin <streetworkout200412@gmail.com>
2024-07-06 10:00:41 +0300

    Added description "Fun with trees: is perfect"

---
+++ Python_Solutions/CodeWars/5kyu/Fun_with_trees_is_perfect.py
@@ -0,0 +1,14 @@
+# A perfect binary tree is a binary tree in which all interior nodes have two children and all leaves have the same depth or same level.
+#
+# You are given a class called TreeNode. Implement the method isPerfect which determines if a given tree denoted by its root node is perfect.
+#
+# Note: TreeNode class contains helper methods for tree creation, which might be handy for your solution. Feel free to update those methods, but make sure you keep their signatures intact (since they are used from within test cases).
+#
+# You can (and should) add more tests to validate your solution, since not all cases are covered in the example test cases.
+#
+# This kata is part of fun with trees series:
+#
+# Fun with trees: max sum
+# Fun with trees: array to tree
+# Fun with trees: is perfect
+# TREESRECURSIONBINARY TREESBINARY SEARCH TREESDATA STRUCTURESALGORITHMS
