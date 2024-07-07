3fe82a750bc4df5e8f7de400f3c5fefa78e4bd90
Peter Kalugin <streetworkout200412@gmail.com>
2024-07-07 07:23:10 +0300

    Added description "3211. Generate Binary Strings Without Adjacent Zeros"

---
+++ Python_Solutions/LeetCode/Medium/3211._Generate_Binary_Strings_Without_Adjacent_Zeros.py
@@ -0,0 +1,35 @@
+# You are given a positive integer n.
+#
+# A binary string x is valid if all
+# substrings
+#  of x of length 2 contain at least one "1".
+#
+# Return all valid strings with length n, in any order.
+#
+#
+#
+# Example 1:
+#
+# Input: n = 3
+#
+# Output: ["010","011","101","110","111"]
+#
+# Explanation:
+#
+# The valid strings of length 3 are: "010", "011", "101", "110", and "111".
+#
+# Example 2:
+#
+# Input: n = 1
+#
+# Output: ["0","1"]
+#
+# Explanation:
+#
+# The valid strings of length 1 are: "0" and "1".
+#
+#
+#
+# Constraints:
+#
+# 1 <= n <= 18
