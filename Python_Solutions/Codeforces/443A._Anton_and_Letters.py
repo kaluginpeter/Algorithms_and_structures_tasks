a58874ccff082b568661806264eeb3b6587fe9a5
Peter Kalugin <streetworkout200412@gmail.com>
2024-07-08 21:07:03 +0300

    Added solution "443A. Anton and Letters"

--- Python_Solutions/Codeforces/443A._Anton_and_Letters.py
+++ Python_Solutions/Codeforces/443A._Anton_and_Letters.py
@@ -26,3 +26,14 @@
 # {}
 # outputCopy
 # 0
+# Solution O(N) O(N)
+import sys
+
+
+def solution(n: str) -> str:
+    return str(len(set([char for char in n if char not in '{} ,'])))
+
+
+if __name__ == '__main__':
+    n: str = sys.stdin.readline().rstrip()
+    sys.stdout.write(solution(n))
