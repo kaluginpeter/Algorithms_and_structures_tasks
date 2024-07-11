6fb80aaa6e52e1f2b90a2b74b343307d6a6749f5
Peter Kalugin <streetworkout200412@gmail.com>
2024-07-11 06:40:36 +0300

    Added solution "1190. Reverse Substrings Between Each Pair of Parentheses"

--- Python_Solutions/LeetCode/Medium/1190._Reverse_Substrings_Between_Each_Pair_of_Parentheses.py
+++ Python_Solutions/LeetCode/Medium/1190._Reverse_Substrings_Between_Each_Pair_of_Parentheses.py
@@ -27,3 +27,24 @@
 # 1 <= s.length <= 2000
 # s only contains lower case English characters and parentheses.
 # It is guaranteed that all parentheses are balanced.
+# Solution Stack
+# Note:
+# In Python, string is immutable object, so because this solution uses list of alphabet characters for perfomance.
+# You can use a whole string, if it mutable in your language.
+# Complexity
+# Time complexity: O(N)
+# Space complexity: O(N)
+# Code
+class Solution:
+    def reverseParentheses(self, s: str) -> str:
+        storage: list[list[str]] = []
+        current_substring: list[str] = []
+        for char in s:
+            if char == '(':
+                storage.append(current_substring)
+                current_substring = []
+            elif char == ')':
+                current_substring = storage.pop() + current_substring[::-1]
+            else:
+                current_substring.append(char)
+        return ''.join(current_substring)
