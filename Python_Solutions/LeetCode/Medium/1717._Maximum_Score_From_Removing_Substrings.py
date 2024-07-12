9763d0db353adc4f448b275ecb25a7ab9efda908
Peter Kalugin <streetworkout200412@gmail.com>
2024-07-12 18:03:50 +0300

    Added solution "1717. Maximum Score From Removing Substrings"

--- Python_Solutions/LeetCode/Medium/1717._Maximum_Score_From_Removing_Substrings.py
+++ Python_Solutions/LeetCode/Medium/1717._Maximum_Score_From_Removing_Substrings.py
@@ -29,3 +29,38 @@
 # 1 <= s.length <= 105
 # 1 <= x, y <= 104
 # s consists of lowercase English letters.
+# Solution Stack
+# General idea:
+# Choose substring that gives max points and count
+# how many it contains in string using stack for counting and processing.
+# After, you should iterate from entire first stack and count how many second substring contains in first stack,
+# uses second stack for counting. In the end return total sum of points.
+# Complexity
+# Time complexity: O(N)
+# Space complexity: O(N)
+# Code
+class Solution:
+    def maximumGain(self, s: str, x: int, y: int) -> int:
+        first_substring: str = ['a', 'b'] if x >= y else ['b', 'a']
+        first_points: int = max(x, y)
+        second_substring: str = ['a', 'b'] if x < y else ['b', 'a']
+        second_points: int = min(x, y)
+        total_points: int = 0
+
+        first_stack: list[str] = []
+        for char in s:
+            first_stack.append(char)
+            while first_stack and first_stack[-2:] == first_substring:
+                total_points += first_points
+                first_stack.pop()
+                first_stack.pop()
+
+        second_stack: list[str] = []
+        for char in first_stack:
+            second_stack.append(char)
+            while second_stack and second_stack[-2:] == second_substring:
+                total_points += second_points
+                second_stack.pop()
+                second_stack.pop()
+
+        return total_points
