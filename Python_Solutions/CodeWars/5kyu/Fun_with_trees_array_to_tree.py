4392587f473ee88735a97a95c354bdefbd9fb07f
Peter Kalugin <streetworkout200412@gmail.com>
2024-07-13 10:24:00 +0300

    Added description "Fun with trees: array to tree"

---
+++ Python_Solutions/CodeWars/5kyu/Fun_with_trees_array_to_tree.py
@@ -0,0 +1,24 @@
+# You are given an array of integers. Implement a function which creates a complete binary tree from the array (complete meaning that every level of the tree, except possibly the last, is completely filled).
+#
+# The elements of the array are to be taken left-to-right, and put into the tree top-to-bottom, left-to-right.
+#
+# For example, given the array [17, 0, -4, 3, 15] you should create the following tree:
+#
+#     17
+#    /  \
+#   0   -4
+#  / \
+# 3   15
+# A tree node type is preloaded for you:
+#
+# class Node:
+#     def __init__(self, value, left=None, right=None):
+#         self.value = value
+#         self.left = left
+#         self.right = right
+# This kata is part of fun with trees series:
+#
+# Fun with trees: max sum
+# Fun with trees: array to tree
+# Fun with trees: is perfect
+# TREESARRAYSBINARY TREESDATA STRUCTURESALGORITHMS
