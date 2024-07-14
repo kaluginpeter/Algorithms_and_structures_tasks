8f7a027dd13929bb6e6ca59e103824fdf9a09e76
Peter Kalugin <streetworkout200412@gmail.com>
2024-07-14 08:19:08 +0300

    Added solution "3217. Delete Nodes From Linked List Present in Array"

--- Python_Solutions/LeetCode/Medium/3217._Delete_Nodes_From_Linked_List_Present_in_Array.py
+++ Python_Solutions/LeetCode/Medium/3217._Delete_Nodes_From_Linked_List_Present_in_Array.py
@@ -48,3 +48,22 @@
 # The number of nodes in the given list is in the range [1, 105].
 # 1 <= Node.val <= 105
 # The input is generated such that there is at least one node in the linked list that has a value not present in nums.
+# Solution Linked List HashSet O(N) O(N)
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def modifiedList(self, nums: List[int], head: Optional[ListNode]) -> Optional[ListNode]:
+        to_delete: set[int] = set(nums)
+        tmp = head
+        while tmp:
+            if head and head.val in to_delete:
+                head = head.next
+                tmp = head
+            elif tmp.next and tmp.next.val in to_delete:
+                tmp.next = tmp.next.next
+            else:
+                tmp = tmp.next
+        return head
