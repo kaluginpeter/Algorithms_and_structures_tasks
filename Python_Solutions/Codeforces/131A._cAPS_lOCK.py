9f72eb59d626803e1818cf28238d2b14f99dba80
Peter Kalugin <streetworkout200412@gmail.com>
2024-07-15 06:43:37 +0300

    Added solution "131A. cAPS lOCK"

--- Python_Solutions/Codeforces/131A._cAPS_lOCK.py
+++ Python_Solutions/Codeforces/131A._cAPS_lOCK.py
@@ -30,3 +30,16 @@
 # Lock
 # outputCopy
 # Lock
+# Solution O(N) O(N)
+import sys
+
+
+def solution(n: str) -> str:
+    if all(char.isupper() for char in n) or (n[0].islower and all(char.isupper() for char in n[1:])):
+        return ''.join(i.lower() if i.isupper() else i.upper() for i in n)
+    return n
+
+
+if __name__ == '__main__':
+    n: str = sys.stdin.readline().rstrip()
+    sys.stdout.write(solution(n))
