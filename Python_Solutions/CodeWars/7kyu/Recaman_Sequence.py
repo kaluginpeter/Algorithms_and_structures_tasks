d1e07ba53b39c6dd725d17d44d530518df743e06
Peter Kalugin <streetworkout200412@gmail.com>
2024-07-16 18:41:54 +0300

    Added solution "Recaman Sequence"

--- Python_Solutions/CodeWars/7kyu/Recaman_Sequence.py
+++ Python_Solutions/CodeWars/7kyu/Recaman_Sequence.py
@@ -12,3 +12,20 @@
 # Numberphile video about Recamán's sequence
 #
 # ALGORITHMSPERFORMANCE
+# Solution
+def recaman(n):
+    sequence = [0]
+    seen = {0}
+
+    for i in range(1, n + 1):
+        previous = sequence[-1]
+        potential = previous - i
+
+        if potential > 0 and potential not in seen:
+            sequence.append(potential)
+            seen.add(potential)
+        else:
+            sequence.append(previous + i)
+            seen.add(previous + i)
+
+    return sequence[n]
