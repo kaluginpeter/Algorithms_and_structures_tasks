2b471960ab62ee33b6abb6b7f3ec70a0123761a2
Peter Kalugin <streetworkout200412@gmail.com>
2024-07-16 18:41:34 +0300

    Added description "Recaman Sequence"

---
+++ Python_Solutions/CodeWars/7kyu/Recaman_Sequence.py
@@ -0,0 +1,14 @@
+# Return the nth term of the Recamán's sequence.
+#
+# a(0) = 0;
+#
+#         a(n-1) - n, if this value is positive and not yet in the sequence
+#       /
+# a(n) <
+#       \
+#         a(n-1) + n, otherwise
+# input range: 0 – 30 000
+#
+# Numberphile video about Recamán's sequence
+#
+# ALGORITHMSPERFORMANCE
