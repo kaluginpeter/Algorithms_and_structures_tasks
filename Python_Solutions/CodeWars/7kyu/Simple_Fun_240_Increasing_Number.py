7ee2b70364d3617cdfd829ab038e9343d5796cfa
Peter Kalugin <streetworkout200412@gmail.com>
2024-07-17 18:02:49 +0300

    Added solution "Simple Fun #240: Increasing Number"

--- Python_Solutions/CodeWars/7kyu/Simple_Fun_240_Increasing_Number.py
+++ Python_Solutions/CodeWars/7kyu/Simple_Fun_240_Increasing_Number.py
@@ -37,3 +37,12 @@
 # 4th operation: 12 is already a multiple of 4.
 # 5th operation: increase x to 15, which is a multiple of 5.
 # PUZZLES
+# Solution
+def increasing_number(x, n):
+    acc: int = 1
+    while acc <= n:
+        if x % acc == 0:
+            acc += 1
+        else:
+            x += 1
+    return x
