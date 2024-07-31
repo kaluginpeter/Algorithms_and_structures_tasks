57f4316a240ba5039b66f86849aff482ee54972e
Peter Kalugin <streetworkout200412@gmail.com>
2024-07-31 17:17:39 +0300

    Added solution "144. Binary Tree Preorder Traversal"

--- Python_Solutions/LeetCode/Easy/144._Binary_Tree_Preorder_Traversal.py
+++ Python_Solutions/LeetCode/Easy/144._Binary_Tree_Preorder_Traversal.py
@@ -24,3 +24,46 @@
 #
 #
 # Follow up: Recursive solution is trivial, could you do it iteratively?
+# Solution
+# Dfs recursive way
+# Complexity
+# Time complexity: O(N)
+# Space complexity: O(N)
+# Code
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode], order: list[int] = None) -> List[int]:
+        if order is None: order = []
+        if not root: return order
+        order += [root.val]
+        self.preorderTraversal(root.left, order)
+        self.preorderTraversal(root.right, order)
+        return order
+
+# Dfs iterative way
+# Complexity
+# Time complexity: O(N)
+# Space complexity: O(N)
+# Code
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode], order: list[int] = None) -> List[int]:
+        order: list[int] = []
+        queue: list[TreeNode] = [root]
+        while queue:
+            cur_root: TreeNode = queue.pop()
+            if not cur_root: continue
+            order.append(cur_root.val)
+            queue.append(cur_root.right)
+            queue.append(cur_root.left)
+        return order
