be1c89f78c322bad7f36ffe386244f35df639d41
Peter Kalugin <streetworkout200412@gmail.com>
2024-07-31 17:51:06 +0300

    Added solution "145. Binary Tree Postorder Traversal"

--- Python_Solutions/LeetCode/Easy/145._Binary_Tree_Postorder_Traversal.py
+++ Python_Solutions/LeetCode/Easy/145._Binary_Tree_Postorder_Traversal.py
@@ -24,3 +24,47 @@
 #
 #
 # Follow up: Recursive solution is trivial, could you do it iteratively?
+# Solution
+# Recursive way
+# Complexity
+# Time complexity: O(N)
+# Space complexity: O(N)
+# Code
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode], order: list[int] = None) -> List[int]:
+        if order is None:
+            order = []
+        if not root: return order
+        self.postorderTraversal(root.left, order)
+        self.postorderTraversal(root.right, order)
+        order.append(root.val)
+        return order
+
+# Iterative way
+# Complexity
+# Time complexity: O(N)
+# Space complexity: O(N)
+# Code
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        order: list[int] = []
+        queue: list[TreeNode] = [(root, False)]
+        while queue:
+            cur_root, seen = queue.pop()
+            if cur_root and not seen:
+                queue += [(cur_root, True), (cur_root.right, False), (cur_root.left, False)]
+            elif cur_root:
+                order.append(cur_root.val)
+        return order
