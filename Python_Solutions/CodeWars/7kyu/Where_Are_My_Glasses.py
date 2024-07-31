374c143e645bb5af7db1590e8716228993353f71
Peter Kalugin <streetworkout200412@gmail.com>
2024-07-31 17:08:14 +0300

    Added description "Where Are My Glasses?"

---
+++ Python_Solutions/CodeWars/7kyu/Where_Are_My_Glasses.py
@@ -0,0 +1,16 @@
+# Oh no! I've lost my glasses, but paradoxically, I need glasses to find my glasses!
+#
+# Please help me out by showing me the index in the list which contains my glasses. They look like two capital Os, with at least one dash in between!
+#
+# This means that both O--O and O------------O are valid glasses, but not O----#--O for example!
+# Search thoroughly, you might find my glasses in places such as 'dustO-Odust'
+# Examples
+# ["phone", "O-O", "coins", "keys"] ➞ 1
+#
+# ["OO", "wallet", "O##O", "O----O"] ➞ 3
+#
+# ["O--#--O", "dustO---Odust", "more dust"] ➞ 1
+# Notes
+# All lists will include one valid pair of glasses because I swear I dropped them around here somewhere ...
+# All elements in the list are strings.
+# REGULAR EXPRESSIONS
