2aaf0cb40e8e6d90c081c2dbe8a03dc0db61eb39
Peter Kalugin <streetworkout200412@gmail.com>
2024-08-01 17:28:34 +0300

    Added description "Stern-Brocot Sequence Part I"

---
+++ Python_Solutions/CodeWars/6kyu/Stern_Brocot_Sequence_Part_I.py
@@ -0,0 +1,22 @@
+# The Stern-Brocot sequence is much like the Fibonacci sequence and has some cool implications. Let's learn about it:
+#
+# It starts with [1, 1] and adds two new terms every iteration: nextTerm which is the sum of a previous pair; and termAfterThat which is the second term of this previous pair. Here is how to find those terms:
+#
+# [1, 1] + [nextTerm: 1 + 1 = 2; termAfterThat: 1] ==> [1, 1, 2, 1]
+#  ^  ^
+# Then you shift the pairs with one index in the sequence:
+#
+# [1, 1, 2, 1] + [1 + 2, 2] ==> [1, 1, 2, 1, 3, 2]
+#     ^  ^
+# And so on... doing this for 2 more iterations will yield:
+#
+# [1, 1, 2, 1, 3, 2, 3, 1, 4, 3]
+# Complete the code that takes a positive integer n, and returns the index of the first occurrence of n in the sequence. Note: indexing start at zero.
+#
+# Examples
+# [1, 1, 2, 1, 3, 2, 3, 1, 4, 3, ...]
+#        ^     ^           ^
+# n = 2 ==> 2
+# n = 3 ==> 4
+# n = 4 ==> 8
+# NUMBER THEORYALGORITHMS
