c0f26ef31799a36482a71b59eef67de6671eab01
Peter Kalugin <streetworkout200412@gmail.com>
2024-08-01 06:51:31 +0300

    Added description "230B. T-primes"

---
+++ Python_Solutions/Codeforces/230B._T_primes.py
@@ -0,0 +1,25 @@
+# B. T-primes
+# time limit per test2 seconds
+# memory limit per test256 megabytes
+# We know that prime numbers are positive integers that have exactly two distinct positive divisors. Similarly, we'll call a positive integer t Т-prime, if t has exactly three distinct positive divisors.
+#
+# You are given an array of n positive integers. For each of them determine whether it is Т-prime or not.
+#
+# Input
+# The first line contains a single positive integer, n (1 ≤ n ≤ 105), showing how many numbers are in the array. The next line contains n space-separated integers xi (1 ≤ xi ≤ 1012).
+#
+# Please, do not use the %lld specifier to read or write 64-bit integers in С++. It is advised to use the cin, cout streams or the %I64d specifier.
+#
+# Output
+# Print n lines: the i-th line should contain "YES" (without the quotes), if number xi is Т-prime, and "NO" (without the quotes), if it isn't.
+#
+# Examples
+# inputCopy
+# 3
+# 4 5 6
+# outputCopy
+# YES
+# NO
+# NO
+# Note
+# The given test has three numbers. The first number 4 has exactly three divisors — 1, 2 and 4, thus the answer for this number is "YES". The second number 5 has two divisors (1 and 5), and the third number 6 has four divisors (1, 2, 3, 6), hence the answer for them is "NO".
