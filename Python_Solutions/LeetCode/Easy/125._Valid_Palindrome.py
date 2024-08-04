2b35c03514f85ce87d3b6b19caa69587d6b8a1c8
Peter Kalugin <streetworkout200412@gmail.com>
2024-08-04 18:43:51 +0300

    Added solution "125. Valid Palindrome"

--- Python_Solutions/LeetCode/Easy/125._Valid_Palindrome.py
+++ Python_Solutions/LeetCode/Easy/125._Valid_Palindrome.py
@@ -31,3 +31,19 @@ class Solution:
             if char.isalnum():
                 i += char.lower()
         return i == i[::-1]
+
+# Solution Two Pointers O(N) O(1)
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left: int = 0
+        right: int = len(s) - 1
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1
+            while right > left and not s[right].isalnum():
+                right -= 1
+            if left < right and s[left].lower() != s[right].lower():
+                return False
+            left += 1
+            right -= 1
+        return True
