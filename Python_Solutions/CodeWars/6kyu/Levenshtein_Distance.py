8b5f2617ea272c0fbe00f91e2fd03c1210069c5e
Peter Kalugin <streetworkout200412@gmail.com>
2024-08-06 17:15:47 +0300

    Added solution "Levenshtein Distance"

--- Python_Solutions/CodeWars/6kyu/Levenshtein_Distance.py
+++ Python_Solutions/CodeWars/6kyu/Levenshtein_Distance.py
@@ -5,3 +5,15 @@
 # Your task is to implement a function which calculates the Levenshtein distance for two arbitrary strings.
 #
 # STRINGSALGORITHMS
+# Solution
+def levenshtein(a,b):
+    cost: int = sum(x != y for x, y in zip(a, b))
+    if len(a) == len(b): return cost
+    up: int = max(len(a), len(b))
+    do: int = min(len(a), len(b))
+    big: str = a if len(a) == up else b
+    small: str = a if len(a) == do else b
+    for i in range(up):
+        if up - i < do: break
+        cost = min(cost, sum(x != y for x, y in zip(big[i:], small)))
+    return cost + (up - do)
