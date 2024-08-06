fe83a5b68f49c1bca042b3311a21b6973232495c
Peter Kalugin <streetworkout200412@gmail.com>
2024-08-06 20:03:36 +0300

    Added solution "22. Generate Parentheses"

--- Python_Solutions/LeetCode/Medium/22._Generate_Parentheses.py
+++ Python_Solutions/LeetCode/Medium/22._Generate_Parentheses.py
@@ -15,3 +15,23 @@
 # Constraints:
 #
 # 1 <= n <= 8
+# Solution Backtracking Recursive O(4**N) O(4**N)
+class Solution:
+    def backtrack(self, n: int, left: int, right: int, output: list[str], result: list[str]) -> None:
+        if left >= n and right >= n:
+            result.append(''.join(output))
+
+        if left < n:
+            output.append('(')
+            self.backtrack(n, left + 1, right, output, result)
+            output.pop()
+        if right < left:
+            output.append(')')
+            self.backtrack(n, left, right + 1, output, result)
+            output.pop()
+
+    def generateParenthesis(self, n: int) -> List[str]:
+        result: list[str] = []
+        output: list[str] = []
+        self.backtrack(n, 0, 0, output, result)
+        return result
