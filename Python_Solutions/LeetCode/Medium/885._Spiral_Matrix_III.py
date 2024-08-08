fa25f0f85d625a76306dc6b3ce16cd384ad95ea0
Peter Kalugin <streetworkout200412@gmail.com>
2024-08-08 17:15:07 +0300

    Added solution "885. Spiral Matrix III"

--- Python_Solutions/LeetCode/Medium/885._Spiral_Matrix_III.py
+++ Python_Solutions/LeetCode/Medium/885._Spiral_Matrix_III.py
@@ -23,3 +23,46 @@
 # 1 <= rows, cols <= 100
 # 0 <= rStart < rows
 # 0 <= cStart < cols
+# Solution Matrix Simulation O(NM) O(NM)
+class Solution:
+    def check(self, cur_row: int, cur_col: int, rows: int, cols: int, result: list[list[int]]) -> None:
+        if 0 <= cur_row < rows and 0 <= cur_col < cols:
+            result.append([cur_row, cur_col])
+
+    def spiralMatrixIII(self, rows: int, cols: int, rStart: int, cStart: int) -> List[List[int]]:
+        output: list[list[int]] = [[rStart, cStart]]
+        cur_row, cur_col = rStart, cStart
+        boundary: int = 1
+        cur_col += 1
+        # Make first move from center to right
+        self.check(cur_row, cur_col, rows, cols, output)
+        while len(output) < rows * cols:
+            # Move from upper right corner to down right corner
+            down_boundary: int = cur_row + boundary
+            cur_row += 1
+            while cur_row < down_boundary:
+                self.check(cur_row, cur_col, rows, cols, output)
+                cur_row += 1
+
+            boundary += 1
+            # Move from down right corner to down left corner
+            left_boundary: int = cur_col - boundary
+            while cur_col > left_boundary:
+                self.check(cur_row, cur_col, rows, cols, output)
+                cur_col -= 1
+            # Move from down left corner to upper left corner
+            upper_boundary: int = cur_row - boundary
+            while cur_row > upper_boundary:
+                self.check(cur_row, cur_col, rows, cols, output)
+                cur_row -= 1
+            # Move from upper left corner to upper right corner
+            right_boundary: int = cur_col + boundary
+            while cur_col <= right_boundary:
+                self.check(cur_row, cur_col, rows, cols, output)
+                cur_col += 1
+            # Check upper right corner + 1
+            self.check(cur_row, cur_col, rows, cols, output)
+
+            boundary += 1
+
+        return output
