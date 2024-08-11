8efe64defb160a5679291ce1165cae650659f555
Peter Kalugin <streetworkout200412@gmail.com>
2024-08-11 08:17:21 +0300

    Added solution "1568. Minimum Number of Days to Disconnect Island"

--- Python_Solutions/LeetCode/Hard/1568._Minimum_Number_of_Days_to_Disconnect_Island.py
+++ Python_Solutions/LeetCode/Hard/1568._Minimum_Number_of_Days_to_Disconnect_Island.py
@@ -30,3 +30,49 @@
 # n == grid[i].length
 # 1 <= m, n <= 30
 # grid[i][j] is either 0 or 1.
+# Solution DFS O(NM(N + M) O(NM)
+class Solution:
+    def next_valid_ceil(
+            self, row: int, col: int, n: int, m: int, seen: set[int], grid: list[list[int]]
+    ) -> bool:
+        return 0 <= row < n and 0 <= col < m and grid[row][col] == 1 and (row, col) not in seen
+
+    def dfs(
+            self, row: int, col: int, grid: list[list[int]], seen: set[int]
+    ) -> None:
+        stack: list[tuple[int, int]] = [(row, col)]
+        directions: list[tuple[int, int]] = [
+            (-1, 0), (1, 0), (0, -1), (0, 1)
+        ]
+        while stack:
+            row, col = stack.pop()
+            for path in directions:
+                next_row, next_col = row + path[0], col + path[1]
+                if self.next_valid_ceil(next_row, next_col, len(grid), len(grid[0]), seen, grid):
+                    seen.add((next_row, next_col))
+                    stack.append((next_row, next_col))
+
+    def count_islands(self, grid: list[list[int]]) -> int:
+        seen: set[int] = set()
+        islands: int = 0
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col] == 1 and (row, col) not in seen:
+                    islands += 1
+                    seen.add((row, col))
+                    self.dfs(row, col, grid, seen)
+        return islands
+
+    def minDays(self, grid: List[List[int]]) -> int:
+        if self.count_islands(grid) != 1:
+            return 0
+
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col] == 1:
+                    grid[row][col] = 0
+                    if self.count_islands(grid) != 1:
+                        return 1
+                    grid[row][col] = 1
+
+        return 2
