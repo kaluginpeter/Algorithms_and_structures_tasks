0bd5cd984e2cd64357c89cab5f42aa70d6e6b8c0
Peter Kalugin <streetworkout200412@gmail.com>
2024-08-11 13:31:44 +0300

    Added solution "101. Symmetric Tree"

--- Python_Solutions/LeetCode/Easy/101._Symmetric_Tree.py
+++ Python_Solutions/LeetCode/Easy/101._Symmetric_Tree.py
@@ -42,3 +42,59 @@ class Solution:
             dfs_right(root.left, res)
             return res
         return dfs_left(root.left, [])==dfs_right(root.right, [])
+
+
+# Recursive DFS
+# Complexity
+# Time complexity: O(N)
+# Space complexity: O(N)
+# Code
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def dfs(self, root1: TreeNode, root2: TreeNode) -> bool:
+        if not root1 or not root2:
+            return root1 == root2 == None
+        if root1.val != root2.val:
+            return False
+        left_side: bool = self.dfs(root1.left, root2.right)
+        right_side: bool = self.dfs(root1.right, root2.left)
+        return left_side and right_side
+
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        return self.dfs(root.left, root.right)
+
+# Iterative DFS
+# Complexity
+# Time complexity: O(N)
+# Space complexity: O(N)
+# Code
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        stack: list[TreeNode] = [root.right, root.left]
+        while stack:
+            current_left: TreeNode = stack.pop()
+            current_right: TreeNode = stack.pop()
+            if current_left is None or current_right is None:
+                if current_left == current_right == None:
+                    continue
+                return False
+            elif current_left.val != current_right.val:
+                return False
+            stack.append(current_right.right)
+            stack.append(current_left.left)
+            stack.append(current_right.left)
+            stack.append(current_left.right)
+
+        return True
