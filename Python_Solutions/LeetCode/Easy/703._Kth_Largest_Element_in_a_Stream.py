ade12c08cdca468b2e7206f6052f451ba99299f4
Peter Kalugin <streetworkout200412@gmail.com>
2024-08-12 06:46:05 +0300

    Added solution "703. Kth Largest Element in a Stream"

--- Python_Solutions/LeetCode/Easy/703._Kth_Largest_Element_in_a_Stream.py
+++ Python_Solutions/LeetCode/Easy/703._Kth_Largest_Element_in_a_Stream.py
@@ -31,3 +31,24 @@
 # -104 <= val <= 104
 # At most 104 calls will be made to add.
 # It is guaranteed that there will be at least k elements in the array when you search for the kth element.
+# Solution Heap O(NlogK) O(K)
+import heapq
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k: int = k
+        self.nums: list[int] = nums
+        heapq.heapify(self.nums)
+        while len(self.nums) > k:
+            heapq.heappop(self.nums)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.nums, val)
+        while len(self.nums) > self.k:
+            heapq.heappop(self.nums)
+        return self.nums[0]
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
