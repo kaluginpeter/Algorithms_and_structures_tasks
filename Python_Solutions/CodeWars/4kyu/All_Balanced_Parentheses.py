dd05310b2599abb360b934ddc086e534b181a687
Peter Kalugin <streetworkout200412@gmail.com>
2024-08-14 06:51:46 +0300

    Added solution "All Balanced Parentheses"

--- Python_Solutions/CodeWars/4kyu/All_Balanced_Parentheses.py
+++ Python_Solutions/CodeWars/4kyu/All_Balanced_Parentheses.py
@@ -6,3 +6,22 @@
 # balanced_parens(2) => ["()()","(())"]
 # balanced_parens(3) => ["()()()","(())()","()(())","(()())","((()))"]
 # ALGORITHMS
+# Solution
+def balanced_parens(n):
+    output: list[str] = []
+
+    def generate(n: int, left: int, right: int, comb: list[str]):
+        if left >= n and right >= n:
+            output.append(''.join(comb))
+            return
+        if left < n:
+            comb.append('(')
+            generate(n, left + 1, right, comb)
+            comb.pop()
+        if right < left:
+            comb.append(')')
+            generate(n, left, right + 1, comb)
+            comb.pop()
+
+    generate(n, 0, 0, [])
+    return output
