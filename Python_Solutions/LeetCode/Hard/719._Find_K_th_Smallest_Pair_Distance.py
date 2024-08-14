fb0a889ff280102b55145521f9472fcd583fe6b2
Peter Kalugin <streetworkout200412@gmail.com>
2024-08-14 17:17:14 +0300

    Added solution "719. Find K-th Smallest Pair Distance"

--- Python_Solutions/LeetCode/Hard/719._Find_K_th_Smallest_Pair_Distance.py
+++ Python_Solutions/LeetCode/Hard/719._Find_K_th_Smallest_Pair_Distance.py
@@ -29,3 +29,26 @@
 # 2 <= n <= 104
 # 0 <= nums[i] <= 106
 # 1 <= k <= n * (n - 1) / 2
+# Solution Two Pointers Sliding Window Binary Search O(NlogN + NlogMaxAbs) (1)
+class Solution:
+    def smallestDistancePair(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        low, high = 0, nums[-1] - nums[0]
+
+        while low < high:
+            mid: int = low + (high - low) // 2
+            # Case if pairs less that k on middle difference
+            if self.count_pairs(nums, mid) < k:
+                low = mid + 1
+            else:
+                high = mid
+
+        return low
+
+    def count_pairs(self, nums: List[int], distance: int) -> int:
+        count = left = 0
+        for right in range(1, len(nums)):
+            while nums[right] - nums[left] > distance:
+                left += 1
+            count += right - left
+        return count
