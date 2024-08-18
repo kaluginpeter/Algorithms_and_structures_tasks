b7650b816bec8f956dc8293f2bdd4bf64ff47c5d
Peter Kalugin <streetworkout200412@gmail.com>
2024-08-18 18:54:58 +0300

    Added solution "Even or Odd"

--- CodeWars/8kyu/Even_or_Odd.cpp
+++ CodeWars/8kyu/Even_or_Odd.cpp
@@ -1,3 +1,13 @@
 
 // Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 // MATHEMATICSFUNDAMENTALS
+// Solution
+#include <string>
+
+std::string even_or_odd(int number)
+{
+  if (number % 2 == 0) {
+    return "Even";
+  }
+  return "Odd";
+}
