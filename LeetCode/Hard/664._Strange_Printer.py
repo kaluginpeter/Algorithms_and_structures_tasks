7d07e85a3d7245804fe673ca468997ea2bc82db3
Peter Kalugin <streetworkout200412@gmail.com>
2024-08-21 06:50:45 +0300

    Added solution "664. Strange Printer"

--- LeetCode/Hard/664._Strange_Printer.py
+++ LeetCode/Hard/664._Strange_Printer.py
@@ -22,3 +22,35 @@
 #
 # 1 <= s.length <= 100
 # s consists of lowercase English letters.
+# Solution Dynamic Programming O(N**3) O(N**2)
+class Solution:
+    def shorten_string(self, s: str) -> str:
+        stack: list[str] = []
+        for right in range(len(s)):
+            if stack and stack[-1] == s[right]:
+                continue
+            else:
+                stack.append(s[right])
+        return ''.join(stack)
+
+    def strangePrinter(self, s: str) -> int:
+        s: str = self.shorten_string(s)
+        n: int = len(s)
+        # Max n constraint is 100, so initialize 100 + 1
+        dp: list[list[int]] = [[0] * 101 for _ in range(101)]
+        # Go from right to left
+        for i in range(n - 1, -1, -1):
+            # Initial state for current ceil
+            dp[i][i] = 1
+            # Go from current left to right
+            for j in range(i + 1, n):
+                if s[i] == s[j]:
+                    dp[i][j] = dp[i][j - 1]
+                    continue
+                x: int = dp[i][j - 1] + 1
+                # Choose minimum possible x
+                for k in range(i + 1, j - 1):
+                    if s[k] == s[j]:
+                        x = min(x, dp[i][k - 1] + dp[k][j - 1])
+                dp[i][j] = x
+        return dp[0][n-1]
