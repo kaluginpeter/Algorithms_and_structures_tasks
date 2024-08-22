acd6381049fd8e1a23b00df21c634b105b85fd5b
Peter Kalugin <streetworkout200412@gmail.com>
2024-08-22 06:42:03 +0300

    Added description "1791A. Codeforces Checking"

---
+++ Codeforces/1791A._Codeforces_Checking.py
@@ -0,0 +1,44 @@
+# A. Codeforces Checking
+# time limit per test1 second
+# memory limit per test256 megabytes
+# Given a lowercase Latin character (letter), check if it appears in the string codeforces
+# .
+#
+# Input
+# The first line of the input contains an integer t
+#  (1≤t≤26
+# ) — the number of test cases.
+#
+# The only line of each test case contains a character c
+#  — a single lowercase Latin character (letter).
+#
+# Output
+# For each test case, output "YES" (without quotes) if c
+#  satisfies the condition, and "NO" (without quotes) otherwise.
+#
+# You can output the answer in any case (for example, the strings "yEs", "yes", "Yes" and "YES" will be recognized as a positive answer).
+#
+# Example
+# inputCopy
+# 10
+# a
+# b
+# c
+# d
+# e
+# f
+# g
+# h
+# i
+# j
+# outputCopy
+# NO
+# NO
+# YES
+# YES
+# YES
+# YES
+# NO
+# NO
+# NO
+# NO
