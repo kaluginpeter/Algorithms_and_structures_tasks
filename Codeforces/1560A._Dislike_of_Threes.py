c59534be7fc4e6d00489549252498fbad028e34d
Peter Kalugin <streetworkout200412@gmail.com>
2024-08-23 06:42:50 +0300

    Added solution "1560A. Dislike of Threes"

--- Codeforces/1560A._Dislike_of_Threes.py
+++ Codeforces/1560A._Dislike_of_Threes.py
@@ -50,3 +50,22 @@
 # 11
 # 14
 # 1666
+# Solution O(N) O(N)
+import sys
+from typing import List
+
+
+def solution(t: int) -> None:
+    order: List[int] = [1]
+    for _ in range(t):
+        x: int = int(sys.stdin.readline().rstrip())
+        while x > len(order):
+            for num in range(order[-1] + 1, order[-1] + x + 1):
+                if num % 3 != 0 and num % 10 != 3:
+                    order.append(num)
+        print(order[x - 1])
+
+
+if __name__ == '__main__':
+    t: int = int(sys.stdin.readline().rstrip())
+    solution(t)
