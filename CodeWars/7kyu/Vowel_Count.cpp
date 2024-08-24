97a3fc5f75e5b9dd54e5525da3d62fed56410617
Peter Kalugin <streetworkout200412@gmail.com>
2024-08-24 14:37:39 +0300

    Added solution "Vowel Count"

--- CodeWars/7kyu/Vowel_Count.cpp
+++ CodeWars/7kyu/Vowel_Count.cpp
@@ -5,3 +5,19 @@ We will consider a, e, i, o, u as vowels for this Kata (but not y).
 The input string will only consist of lower case letters and/or spaces.
 
 STRINGSFUNDAMENTALS
+// Solution
+#include <string>
+
+using namespace std;
+
+int getCount(const string& inputStr){
+  int num_vowels = 0;
+  string vowels = "aeoiu";
+  for (auto letter : inputStr) {
+    auto index_of_element = find(vowels.begin(), vowels.end(), letter);
+    if (index_of_element != vowels.end()) {
+      num_vowels += 1;
+    }
+  }
+  return num_vowels;
+}
