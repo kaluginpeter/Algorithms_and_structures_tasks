7017adcd4e1652cce70c6b36c06859724e113592
Peter Kalugin <streetworkout200412@gmail.com>
2024-08-25 14:50:11 +0300

    Added solution "53. Maximum Subarray"

--- LeetCode/Medium/53._Maximum_Subarray.py
+++ LeetCode/Medium/53._Maximum_Subarray.py
@@ -28,3 +28,12 @@
 #
 #
 # Follow up: If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+# Solution Two Pointers Kadane O(N) O(1)
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        max_sum: int = nums[0]
+        cur_sum: int = nums[0]
+        for right in range(1, len(nums)):
+            cur_sum = max(cur_sum + nums[right], nums[right])
+            max_sum = max(max_sum, cur_sum)
+        return max_sum
