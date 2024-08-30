f77ccde9e84a0b0ef7223fd5e3e597c47bf1fa6e
Peter Kalugin <streetworkout200412@gmail.com>
2024-08-30 17:39:22 +0300

    Added solution "Find the smallest integer in the array"

--- CodeWars/8kyu/Find_the_smallest_integer_in_the_array.cpp
+++ CodeWars/8kyu/Find_the_smallest_integer_in_the_array.cpp
@@ -7,3 +7,18 @@ Given [34, -345, -1, 100] your solution will return -345
 You can assume, for the purpose of this kata, that the supplied array will not be empty.
 
 FUNDAMENTALS
+// Solution
+#include <vector>
+
+using namespace std;
+
+int findSmallest(vector <int> list)
+{
+  int min_el = list[0];
+  for (int num : list) {
+    if (min_el > num) {
+      min_el = num;
+    }
+  }
+  return min_el;
+}
