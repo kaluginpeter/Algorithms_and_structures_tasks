d249d0dc15f43d763a28a38dc0986481be304600
Peter Kalugin <streetworkout200412@gmail.com>
2024-08-31 10:31:47 +0300

    Added solution "200. Number of Islands"

--- LeetCode/Medium/200._Number_of_Islands.py
+++ LeetCode/Medium/200._Number_of_Islands.py
@@ -30,3 +30,23 @@
 # n == grid[i].length
 # 1 <= m, n <= 300
 # grid[i][j] is '0' or '1'.
+# Solution Depth-First-Search Memoization O(NM) O(NM)
+class Solution:
+    def dfs(self, row: int, col: int, grid: list[list[str]], seen: set[tuple[int, int]]) -> None:
+        if (row, col) in seen or not (0 <= row < len(grid)) or not (0 <= col < len(grid[0])) or grid[row][col] == '0':
+            return
+        seen.add((row, col))
+        self.dfs(row + 1, col, grid, seen)
+        self.dfs(row, col + 1, grid, seen)
+        self.dfs(row - 1, col, grid, seen)
+        self.dfs(row, col - 1, grid, seen)
+
+    def numIslands(self, grid: List[List[str]]) -> int:
+        seen: set[tuple[int, int]] = set()
+        islands: int = 0
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col] == '1' and (row, col) not in seen:
+                    self.dfs(row, col, grid, seen)
+                    islands += 1
+        return islands
