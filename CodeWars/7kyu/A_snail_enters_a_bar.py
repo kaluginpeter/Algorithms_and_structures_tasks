e23164c35c29bb5fa23bc28c6b9ea6fa76fa3e6c
Peter Kalugin <streetworkout200412@gmail.com>
2024-09-01 07:51:31 +0300

    Added solution "A snail enters a bar!"

--- CodeWars/7kyu/A_snail_enters_a_bar.py
+++ CodeWars/7kyu/A_snail_enters_a_bar.py
@@ -30,3 +30,8 @@
 # Increase rate of the rubber band: z = 0.05 units/minute
 # Outcome: false (The snail will not be able to reach the end within one year.)
 # MATHEMATICS
+# Solution
+def can_snail_reach_end(length, speed, length_increases):
+    boundary: int = 365 * 24 * 60
+    if speed <= length_increases: return False
+    return length / (speed - length_increases) <= boundary
