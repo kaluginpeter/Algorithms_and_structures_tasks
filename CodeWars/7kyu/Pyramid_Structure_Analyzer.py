3196f0cef9afe34abae998e332cb0729928d3bff
Peter Kalugin <streetworkout200412@gmail.com>
2024-09-02 06:53:13 +0300

    Added description "Pyramid Structure Analyzer"

---
+++ CodeWars/7kyu/Pyramid_Structure_Analyzer.py
@@ -0,0 +1,7 @@
+# In this kata, you are given the sum of the number of vertices, edges, and faces of an
+#
+# N-sided pyramid, which is equal to s. You need to return the number of vertices,edges,
+#
+# faces, and the number of sides of the base of the pyramid.
+#
+# MATHEMATICSALGORITHMS
