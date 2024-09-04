9a3d72a196c980e4cf66a42fe3830d27ce119e64
Peter Kalugin <streetworkout200412@gmail.com>
2024-09-04 17:48:35 +0300

    Added description "1915A. Odd One Out"

---
+++ Codeforces/1915A._Odd_One_Out.py
@@ -0,0 +1,50 @@
+# A. Odd One Out
+# time limit per test1 second
+# memory limit per test256 megabytes
+# You are given three digits a
+# , b
+# , c
+# . Two of them are equal, but the third one is different from the other two.
+#
+# Find the value that occurs exactly once.
+#
+# Input
+# The first line contains a single integer t
+#  (1≤t≤270
+# ) — the number of test cases.
+#
+# The only line of each test case contains three digits a
+# , b
+# , c
+#  (0≤a
+# , b
+# , c≤9
+# ). Two of the digits are equal, but the third one is different from the other two.
+#
+# Output
+# For each test case, output the value that occurs exactly once.
+#
+# Example
+# inputCopy
+# 10
+# 1 2 2
+# 4 3 4
+# 5 5 6
+# 7 8 8
+# 9 0 9
+# 3 6 3
+# 2 8 2
+# 5 7 7
+# 7 7 5
+# 5 7 5
+# outputCopy
+# 1
+# 3
+# 6
+# 7
+# 0
+# 6
+# 8
+# 5
+# 5
+# 7
