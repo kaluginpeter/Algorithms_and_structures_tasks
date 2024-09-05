c527c87119c1fac776a39b44c7845e73a37734af
Peter Kalugin <streetworkout200412@gmail.com>
2024-09-05 07:17:31 +0300

    Added solution "2028. Find Missing Observations"

--- LeetCode/Medium/2028._Find_Missing_Observations.py
+++ LeetCode/Medium/2028._Find_Missing_Observations.py
@@ -32,3 +32,46 @@
 # m == rolls.length
 # 1 <= n, m <= 105
 # 1 <= rolls[i], mean <= 6
+# Solution
+# Python O(N + M) O(N)
+class Solution:
+    def missingRolls(self, rolls: List[int], mean: int, n: int) -> List[int]:
+        m: int = len(rolls)
+        N: int = m + n
+        n_dolls: list[int] = []
+        total_sum: int = sum(rolls)
+        for _ in range(n):
+            candidate: int = None
+            for doll in range(1, 7):
+                if (total_sum + doll + (n - 1)) / N <= mean:
+                    candidate = doll
+
+            if candidate:
+                total_sum += candidate
+                n -= 1
+                n_dolls.append(candidate)
+        return n_dolls if total_sum / N == mean else []
+
+# C++ O(N + M) O(N)
+class Solution {
+public:
+    vector<int> missingRolls(vector<int>& rolls, int mean, int n) {
+        int m = rolls.size();
+        int total_sum = mean * (m + n);
+        int current_sum = accumulate(rolls.begin(), rolls.end(), 0);
+        int missing_sum = total_sum - current_sum;
+        if (missing_sum < n || missing_sum > 6 * n) {
+            return {};
+        }
+        vector<int> n_rolls(n, 1);
+        missing_sum -= n;
+
+        for (int i = 0; i < n && missing_sum > 0; ++i) {
+            int add = min(missing_sum, 5);
+            n_rolls[i] += add;
+            missing_sum -= add;
+        }
+
+        return n_rolls;
+    }
+};
