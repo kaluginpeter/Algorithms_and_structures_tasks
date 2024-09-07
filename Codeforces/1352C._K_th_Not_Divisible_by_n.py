579e9a7debb9cd66d8da62c0cddc75c6b2dad533
Peter Kalugin <streetworkout200412@gmail.com>
2024-09-07 11:49:01 +0300

    Added description "C. K-th Not Divisible by n"

---
+++ Codeforces/1352C._K_th_Not_Divisible_by_n.py
@@ -0,0 +1,50 @@
+# C. K-th Not Divisible by n
+# time limit per test1 second
+# memory limit per test256 megabytes
+# You are given two positive integers n
+#  and k
+# . Print the k
+# -th positive integer that is not divisible by n
+# .
+#
+# For example, if n=3
+# , and k=7
+# , then all numbers that are not divisible by 3
+#  are: 1,2,4,5,7,8,10,11,13…
+# . The 7
+# -th number among them is 10
+# .
+#
+# Input
+# The first line contains an integer t
+#  (1≤t≤1000
+# ) — the number of test cases in the input. Next, t
+#  test cases are given, one per line.
+#
+# Each test case is two positive integers n
+#  (2≤n≤109
+# ) and k
+#  (1≤k≤109
+# ).
+#
+# Output
+# For each test case print the k
+# -th positive integer that is not divisible by n
+# .
+#
+# Example
+# inputCopy
+# 6
+# 3 7
+# 4 12
+# 2 1000000000
+# 7 97
+# 1000000000 1000000000
+# 2 1
+# outputCopy
+# 10
+# 15
+# 1999999999
+# 113
+# 1000000001
+# 1
