c651e78d33bc37ffb891264f016d90532b6f77d6
Peter Kalugin <streetworkout200412@gmail.com>
2024-09-08 07:18:21 +0300

    Added solution "725. Split Linked List in Parts"

--- LeetCode/Medium/725._Split_Linked_List_in_Parts.py
+++ LeetCode/Medium/725._Split_Linked_List_in_Parts.py
@@ -30,3 +30,79 @@
 # The number of nodes in the list is in the range [0, 1000].
 # 0 <= Node.val <= 1000
 # 1 <= k <= 50
+# Solution
+# Python O(N) O(K), where K is length of parts of linked list
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def splitListToParts(self, head: Optional[ListNode], k: int) -> List[Optional[ListNode]]:
+        n: int = 0
+        tmp: Optional[ListNode] = head
+        while tmp:
+            n += 1
+            tmp = tmp.next
+        part_length: int = n // k - 1
+        additional: int = [0, n % k][n > k and n % k != 0]
+        linked_list_parts: list[Optional[ListNode]] = []
+        for _ in range(k):
+            if not head:
+                linked_list_parts.append(head)
+            else:
+                tmp_head: Optional[ListNode] = head
+                tmp: Optional[ListNode] = tmp_head
+                for _ in range(part_length + int(additional > 0)):
+                    tmp = tmp.next
+                head = tmp.next
+                tmp.next = None
+                linked_list_parts.append(tmp_head)
+                additional -= 1
+
+        return linked_list_parts
+
+# C++ O(N) O(K), where K is length of parts of linked list
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<ListNode*> splitListToParts(ListNode* head, int k) {
+        int n = 0;
+        ListNode* tmp = head;
+        while (tmp) {
+            n += 1;
+            tmp = tmp->next;
+        }
+        int part_length = n / k - 1;
+        int remainder = 0;
+        if (n % k != 0) {
+            remainder += n % k;
+        }
+        vector<ListNode*> linked_list_parts;
+        for (int i = 0; i < k; i++) {
+            if (head) {
+                ListNode* tmp_head = head;
+                ListNode* tmp = tmp_head;
+                for (int move = 0; move < part_length + (remainder > 0); move++) {
+                    tmp = tmp->next;
+                }
+                head = tmp->next;
+                tmp->next = nullptr;
+                linked_list_parts.push_back(tmp_head);
+                remainder -= 1;
+            } else {
+                linked_list_parts.push_back(head);
+            }
+        }
+        return linked_list_parts;
+    }
+};
