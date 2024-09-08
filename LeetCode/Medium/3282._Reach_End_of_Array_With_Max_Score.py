159079db475cf510824686c540b1c0188b5d9edd
Peter Kalugin <streetworkout200412@gmail.com>
2024-09-08 13:13:58 +0300

    Added solution "3282. Reach End of Array With Max Score"

--- LeetCode/Medium/3282._Reach_End_of_Array_With_Max_Score.py
+++ LeetCode/Medium/3282._Reach_End_of_Array_With_Max_Score.py
@@ -34,3 +34,47 @@
 #
 # 1 <= nums.length <= 105
 # 1 <= nums[i] <= 105
+# Solution
+# Python O(N) O(1) Dynamic Programming
+class Solution:
+    def findMaximumScore(self, nums: List[int]) -> int:
+        n: int = len(nums)
+        j: int = n - 1
+        cur_max_number: int = 0
+        steps_after_max_number: int = 0
+        previous_best: int = 0
+        max_score: int = 0
+        for i in range(len(nums)):
+            steps_after_max_number += 1
+            if nums[i] <= cur_max_number: continue
+            currrent_score: int = nums[i] * (j - i)
+            previous_best += cur_max_number * steps_after_max_number
+            max_score = max(max_score, currrent_score + previous_best)
+            cur_max_number, steps_after_max_number = nums[i], 0
+        return max_score
+
+# C++ O(N) O(1) Dynamic Programming
+class Solution {
+public:
+    long long findMaximumScore(vector<int>& nums) {
+        long long n, j, cur_max_number, previous_best, steps_after_max_number;
+        n = nums.size();
+        j = n - 1;
+        cur_max_number = 0;
+        previous_best = 0;
+        steps_after_max_number = 0;
+        long long max_score = 0;
+        for (int i = 0; i < n; i++) {
+            steps_after_max_number += 1;
+            if (nums[i] <= cur_max_number) {
+                continue;
+            }
+            long long current_cost = (j - i) * nums[i];
+            previous_best += cur_max_number * steps_after_max_number;
+            max_score = std::max(max_score, current_cost + previous_best);
+            cur_max_number = nums[i];
+            steps_after_max_number = 0;
+        }
+        return max_score;
+    }
+};
