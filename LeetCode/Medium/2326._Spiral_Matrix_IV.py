19412d024958be09190464c5f2ba47e112080e2a
Peter Kalugin <streetworkout200412@gmail.com>
2024-09-09 07:10:16 +0300

    Added solution "2326. Spiral Matrix IV"

--- LeetCode/Medium/2326._Spiral_Matrix_IV.py
+++ LeetCode/Medium/2326._Spiral_Matrix_IV.py
@@ -30,3 +30,148 @@
 # 1 <= m * n <= 105
 # The number of nodes in the list is in the range [1, m * n].
 # 0 <= Node.val <= 1000
+# Solution
+# Python O(MN) O(MN)
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def spiralMatrix(self, m: int, n: int, head: Optional[ListNode]) -> List[List[int]]:
+        mtrx: list[list[int]] = [[-1] * n for _ in range(m)]
+        right: bool = True
+        down = left = up = False
+        start_row = start_col = 0
+        while head:
+            if right:
+                right, down = False, True
+                for i in range(n):
+                    if head:
+                        mtrx[start_row][start_col] = head.val
+                        head = head.next
+                    else: return mtrx
+                    start_col += 1
+                start_col -= 1
+                start_row += 1
+            elif down:
+                down, left = False, True
+                m -= 1
+                for i in range(m):
+                    if head:
+                        mtrx[start_row][start_col] = head.val
+                        head = head.next
+                    else: return mtrx
+                    start_row += 1
+                start_row -= 1
+                start_col -= 1
+            elif left:
+                left, up = False, True
+                n -= 1
+                for i in range(n):
+                    if head:
+                        mtrx[start_row][start_col] = head.val
+                        head = head.next
+                    else: return mtrx
+                    start_col -= 1
+                start_col += 1
+                start_row -= 1
+            else:
+                up, right = False, True
+                m -= 1
+                n -= 1
+                for i in range(m):
+                    if head:
+                        mtrx[start_row][start_col] = head.val
+                        head = head.next
+                    else: return mtrx
+                    start_row -= 1
+                start_row += 1
+                start_col += 1
+        return mtrx
+
+# C++ O(MN) O(MN)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> spiralMatrix(int m, int n, ListNode* head) {
+        std::vector<std::vector<int>> mtrx(m, std::vector<int>(n, -1));
+        int start_row = 0, start_col = 0;
+        bool right, down, left, up;
+        right = true;
+        down = left = up = false;
+        while (head) {
+            if (right) {
+                right = false;
+                down = true;
+                for (int move = 0; move < n; move++) {
+                    if (head){
+                        mtrx[start_row][start_col] = head->val;
+                        head = head->next;
+                    } else {
+                        return mtrx;
+                    }
+                    start_col += 1;
+                }
+                start_col -= 1;
+                start_row += 1;
+            } else if (down) {
+                down = false;
+                left = true;
+                m -= 1;
+                for (int move = 0; move < m; move++) {
+                    if (head) {
+                        mtrx[start_row][start_col] = head->val;
+                        head = head->next;
+                    } else {
+                        return mtrx;
+                    }
+                    start_row += 1;
+                }
+                start_row -= 1;
+                start_col -= 1;
+            } else if (left) {
+                left = false;
+                up = true;
+                n -= 1;
+                for (int move = 0; move < n; move++) {
+                    if (head) {
+                        mtrx[start_row][start_col] = head->val;
+                        head = head->next;
+                    } else {
+                        return mtrx;
+                    }
+                    start_col -= 1;
+                }
+                start_col += 1;
+                start_row -= 1;
+            } else {
+                up = false;
+                right = true;
+                m -= 1;
+                n -= 1;
+                for (int move = 0; move < m; move++) {
+                    if (head) {
+                        mtrx[start_row][start_col] = head->val;
+                        head = head->next;
+                    } else {
+                        return mtrx;
+                    }
+                    start_row -= 1;
+                }
+                start_row += 1;
+                start_col += 1;
+            }
+        }
+        return mtrx;
+    }
+};
