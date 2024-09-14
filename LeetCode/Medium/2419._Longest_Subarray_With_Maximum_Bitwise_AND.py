9f2265b37f460bbce4b3ae4bb1a9ca40ec4707be
Peter Kalugin <streetworkout200412@gmail.com>
2024-09-14 09:13:42 +0300

    Added solution "2419. Longest Subarray With Maximum Bitwise AND"

--- LeetCode/Medium/2419._Longest_Subarray_With_Maximum_Bitwise_AND.py
+++ LeetCode/Medium/2419._Longest_Subarray_With_Maximum_Bitwise_AND.py
@@ -31,3 +31,37 @@
 #
 # 1 <= nums.length <= 105
 # 1 <= nums[i] <= 106
+# Solution Sliding Window
+# Python O(N) O(1)
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        answer: int = 1 # in constraints minimum length of "nums" is 1
+        left: int = 0
+        target: int = max(nums)
+        for right in range(len(nums)):
+            while left <= right and nums[right] != target:
+                answer = max(answer, right - left)
+                left += 1
+            answer = max(answer, right - left + 1)
+        return answer
+# C++ O(N) O(1)
+#include <algorithm>
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int answer = 1; // in constraints minimum length of "nums" is 1
+        int target = 0;
+        for (int num : nums) {
+            target = std::max(target, num);
+        }
+        int left = 0;
+        for (int right = 0; right < nums.size(); ++right){
+            while (left <= right && nums[right] != target) {
+                answer = std::max(answer, right - left);
+                left += 1;
+            }
+            answer = std::max(answer, right - left + 1);
+        }
+        return answer;
+    }
+};
