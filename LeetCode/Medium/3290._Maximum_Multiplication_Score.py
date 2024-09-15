198116825aa8b8736ead9fb8fa6d949dfe3be124
Peter Kalugin <streetworkout200412@gmail.com>
2024-09-15 09:02:13 +0300

    Added solution "3290. Maximum Multiplication Score"

--- LeetCode/Medium/3290._Maximum_Multiplication_Score.py
+++ LeetCode/Medium/3290._Maximum_Multiplication_Score.py
@@ -31,3 +31,46 @@
 # a.length == 4
 # 4 <= b.length <= 105
 # -105 <= a[i], b[i] <= 105
+# Solution Dynamic Programming
+# Python O(MN) O(MN)
+class Solution:
+    def maxScore(self, a: List[int], b: List[int]) -> int:
+        n: int = len(b)
+        dp: list[list[int]] = [[-float('inf')] * 5 for _ in range(n + 1)]
+        dp[0][0] = 0
+        for i in range(1, n + 1):
+            for j in range(5):
+                dp[i][j] = dp[i - 1][j]
+            for j in range(1, 5):
+                dp[i][j] = max(
+                    dp[i][j],
+                    dp[i - 1][j - 1] + a[j - 1] * b[i - 1]
+                )
+        return dp[n][4]
+# C++ O(MN) O(MN)
+#include <vector>
+#include <limits>
+#include <algorithm>
+
+class Solution {
+public:
+    long long maxScore(std::vector<int>& a, std::vector<int>& b) {
+        int n = b.size();
+        std::vector<std::vector<long long>> dp(n + 1, std::vector<long long>(5, std::numeric_limits<long long>::min()));
+        dp[0][0] = 0;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 0; j < 5; ++j) {
+                dp[i][j] = dp[i - 1][j];
+            }
+            for (int j = 1; j < 5; ++j) {
+                if (dp[i - 1][j - 1] != std::numeric_limits<long long>::min()){
+                    dp[i][j] = std::max(
+                        dp[i][j],
+                        dp[i - 1][j - 1] + static_cast<long long>(a[j - 1]) * b[i - 1]
+                    );
+                }
+            }
+        }
+        return dp[n][4];
+    }
+};
