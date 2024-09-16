7c9660549927f38726c80adbba38ac2db54662d1
Peter Kalugin <streetworkout200412@gmail.com>
2024-09-16 21:07:45 +0300

    Added solution "Sum without highest and lowest number"

--- CodeWars/8kyu/Sum_without_highest_and_lowest_number.cpp
+++ CodeWars/8kyu/Sum_without_highest_and_lowest_number.cpp
@@ -12,3 +12,23 @@ Input validation
 If an empty value ( null, None, Nothing, nil etc. ) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0.
 
 FUNDAMENTALS
+#include<vector>
+using namespace std;
+
+int sum(vector<int> numbers)
+{
+  if (numbers.size() < 2) {
+    return 0;
+  }
+  int total_sum = 0, min_num = numbers[1], max_num = numbers[1];
+  for (size_t index = 0; index < numbers.size(); ++index) {
+    total_sum += numbers[index];
+    if (min_num > numbers[index]) {
+      min_num = numbers[index];
+    }
+    if (max_num < numbers[index]) {
+      max_num = numbers[index];
+    }
+  }
+  return total_sum - min_num - max_num;
+}
