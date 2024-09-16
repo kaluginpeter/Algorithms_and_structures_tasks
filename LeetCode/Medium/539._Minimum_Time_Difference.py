ec9ce9639e3e1557580cc4856b712e9d1d36e09d
Peter Kalugin <streetworkout200412@gmail.com>
2024-09-16 06:43:50 +0300

    Added soluition "539. Minimum Time Difference"

--- LeetCode/Medium/539._Minimum_Time_Difference.py
+++ LeetCode/Medium/539._Minimum_Time_Difference.py
@@ -15,3 +15,28 @@
 #
 # 2 <= timePoints.length <= 2 * 104
 # timePoints[i] is in the format "HH:MM".
+# Solution String Math
+# Python O(NlogN) O(N) DateTime
+from datetime import datetime, timedelta
+class Solution:
+    def findMinDifference(self, timePoints: List[str]) -> int:
+        timePoints.sort()
+        answer: int = 23 * 60 * 60
+        for idx in range(len(timePoints)):
+            first_time = datetime.strptime(timePoints[idx], '%H:%M')
+            second_time = datetime.strptime(timePoints[(idx + 1) % len(timePoints)], '%H:%M')
+            if second_time < first_time:
+                second_time += timedelta(days=1)
+            answer = min(answer, (second_time - first_time).total_seconds() / 60)
+        return int(answer)
+# Python O(NlogN) O(1) Math
+class Solution:
+    def findMinDifference(self, timePoints: List[str]) -> int:
+        for idx in range(len(timePoints)):
+            hours, minutes = map(int, timePoints[idx].split(':'))
+            timePoints[idx] = hours * 60 + minutes
+        timePoints.sort()
+        answer: int = 24 * 60 - timePoints[-1] + timePoints[0]
+        for i in range(len(timePoints) - 1):
+            answer = min(answer, timePoints[i + 1] - timePoints[i])
+        return answer
