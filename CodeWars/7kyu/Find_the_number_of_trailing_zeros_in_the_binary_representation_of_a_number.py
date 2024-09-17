323ef63336ae7a8cfc5a6c211ca0228ca2ec8e7f
Peter Kalugin <streetworkout200412@gmail.com>
2024-09-17 17:44:46 +0300

    Added description "Find the number of trailing zeros in the binary representation of a number."

---
+++ CodeWars/7kyu/Find_the_number_of_trailing_zeros_in_the_binary_representation_of_a_number.py
@@ -0,0 +1,7 @@
+# Given a number n, find the number of trailing zeros in its binary representation.
+#
+# Examples:
+# 4 should return 2 as it's represented as 100 and 5 should return 0  101
+# Limits:
+# 0 < n and n <= 10^4
+# LOGICBITS
