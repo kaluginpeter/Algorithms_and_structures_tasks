6da1a0dde79e4f582497f742b53b1a63235d5b03
Peter Kalugin <streetworkout200412@gmail.com>
2024-09-18 06:55:21 +0300

    Added solution "179. Largest Number"

--- LeetCode/Medium/179._Largest_Number.py
+++ LeetCode/Medium/179._Largest_Number.py
@@ -18,3 +18,21 @@
 #
 # 1 <= nums.length <= 100
 # 0 <= nums[i] <= 109
+# Solution
+# Python O(NlogN) O(N) Sorting
+from functools import cmp_to_key
+class Solution:
+    def comparison(self, x: str, y: str) -> int:
+            """
+            For ascending order, ex:
+                if x+y > y+x, then y should comes first.
+                elif x+y < y+x, then x should comes first.
+                else tie, choose any of them.
+            """
+            if x + y > y + x: return 1
+            elif x + y < y + x: return -1
+            return 0
+
+    def largestNumber(self, nums: List[int]) -> str:
+        answer: str = ''.join(sorted(map(str, nums), reverse=True, key=cmp_to_key(self.comparison)))
+        return answer if answer[0] != '0' else '0'
