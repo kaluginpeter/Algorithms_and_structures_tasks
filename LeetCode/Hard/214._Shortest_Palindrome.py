741f307fd4e23273ac15fc0ae59901a531bb1e5a
Peter Kalugin <streetworkout200412@gmail.com>
2024-09-20 06:49:08 +0300

    Added solution "214. Shortest Palindrome"

--- LeetCode/Hard/214._Shortest_Palindrome.py
+++ LeetCode/Hard/214._Shortest_Palindrome.py
@@ -20,3 +20,22 @@
 #
 # 0 <= s.length <= 5 * 104
 # s consists of lowercase English letters only.
+# Solution
+# Python O(N) O(N) Knuth Morris Pratt String Matching
+class Solution:
+    def shortestPalindrome(self, s: str) -> str:
+        concatenated_s: str = s + '#' + s[::-1]
+        pattern: list[int | None] = [None] * len(concatenated_s)
+        left, right = 0, 1
+        while right < len(concatenated_s):
+            if concatenated_s[left] == concatenated_s[right]:
+                pattern[right] = left
+                left, right = left + 1, right + 1
+            elif left and pattern[left - 1] is not None:
+                left = pattern[left - 1] + 1
+            elif left: left = 0
+            else: right += 1
+        if pattern[-1] is None:
+            return ''
+        pattern_length: int = len(s) - pattern[-1] - 1
+        return s[::-1][:pattern_length] + s
