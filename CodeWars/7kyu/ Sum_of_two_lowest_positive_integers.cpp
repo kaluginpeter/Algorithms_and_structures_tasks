962bb9de36955206ab220acac892bfb0f9b2109f
Peter Kalugin <streetworkout200412@gmail.com>
2024-09-20 16:52:34 +0300

    Added solution "Sum of two lowest positive integers"

--- CodeWars/7kyu/ Sum_of_two_lowest_positive_integers.cpp
+++ CodeWars/7kyu/ Sum_of_two_lowest_positive_integers.cpp
@@ -7,3 +7,20 @@ For example, when an array is passed like [19, 5, 42, 2, 77], the output should 
 
 ARRAYSFUNDAMENTALS
 */
+// Solution
+#include <vector>
+
+long sumTwoSmallestNumbers(std::vector<int> numbers)
+{
+    long int first = std::min(numbers[0], numbers[1]);
+    long int second = std::max(numbers[0], numbers[1]);
+    for (size_t idx = 2; idx < numbers.size(); ++idx) {
+      if (first >= numbers[idx]) {
+          second = first;
+          first = numbers[idx];
+      } else if (second > numbers[idx])  {
+          second = numbers[idx];
+      }
+    }
+    return first + second;
+}
