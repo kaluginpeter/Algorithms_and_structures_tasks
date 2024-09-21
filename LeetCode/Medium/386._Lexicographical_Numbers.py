7ace66f7c1f3da8ccddecce71067591971e0f636
Peter Kalugin <streetworkout200412@gmail.com>
2024-09-21 10:57:19 +0300

    Complete task "386. Lexicographical Numbers"

--- LeetCode/Medium/386._Lexicographical_Numbers.py
+++ LeetCode/Medium/386._Lexicographical_Numbers.py
@@ -17,3 +17,48 @@
 # Constraints:
 #
 # 1 <= n <= 5 * 104
+# Solution
+# Python O(N) O(logN) Depth-First-Search
+class Solution:
+    def dfs(self, current: int, target: int, result: list[int]) -> None:
+        if current > target:
+            return
+        if current > 0:
+            result.append(current)
+        for acc in range(10):
+            if (current == 0 and acc == 0) or current * 10 + acc > target:
+                continue
+            self.dfs(current * 10 + acc, target, result)
+
+    def lexicalOrder(self, n: int) -> List[int]:
+        output: list[int] = []
+        self.dfs(0, n, output)
+        return output
+
+# C++ O(N) O(logN) Depth-First-Search
+class Solution {
+public:
+    void dfs(int current, int target, std::vector<int>& result) {
+        if (current > target) {
+            return;
+        }
+        if (current > 0) {
+            result.push_back(current);
+        }
+        for (int acc = 0; acc < 10; ++acc) {
+            if (
+                (current == 0 && acc == 0) ||
+                (current * 10 + acc > target)
+            ) {
+                continue;
+            }
+            dfs(current * 10 + acc, target, result);
+        }
+    }
+
+    vector<int> lexicalOrder(int n) {
+        std::vector<int> output = {};
+        dfs(0, n, output);
+        return output;
+    }
+};
