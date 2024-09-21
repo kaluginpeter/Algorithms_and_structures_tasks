20b1a61939ea346e73e7ccedf5d54b3f4abe02ae
Peter Kalugin <streetworkout200412@gmail.com>
2024-09-21 17:43:19 +0300

    Added solution "209. Minimum Size Subarray Sum"

--- LeetCode/Medium/209._Minimum_Size_Subarray_Sum.py
+++ LeetCode/Medium/209._Minimum_Size_Subarray_Sum.py
@@ -27,3 +27,36 @@
 #
 #
 # Follow up: If you have figured out the O(n) solution, try coding another solution of which the time complexity is O(n log(n)).
+# Solution
+# Python Sliding Window Greedy O(N) O(1)
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        min_length: int = float('inf')
+        left: int = 0
+        current_sum: int = 0
+        for right in range(len(nums)):
+            current_sum += nums[right]
+            while current_sum >= target:
+                min_length = min(min_length, right - left + 1)
+                current_sum -= nums[left]
+                left += 1
+        return min_length if min_length != float('inf') else 0
+
+# C++ Sliding Window Greedy O(N) O(1)
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int min_length = 100001;
+        int current_sum = 0;
+        int left = 0;
+        for (int right = 0; right < nums.size(); ++right) {
+            current_sum += nums[right];
+            while (current_sum >= target) {
+                min_length = std::min(min_length, right - left + 1);
+                current_sum -= nums[left];
+                left += 1;
+            }
+        }
+        return min_length != 100001? min_length : 0;
+    }
+};
