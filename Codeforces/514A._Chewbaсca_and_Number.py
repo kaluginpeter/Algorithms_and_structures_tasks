204b8c0ffd3700e3cfa9305e47385219cf39c1ca
Peter Kalugin <streetworkout200412@gmail.com>
2024-09-21 10:53:57 +0300

    Added solution "514A. Chewbaсca and Number"

--- Codeforces/514A._Chewbaсca_and_Number.py
+++ Codeforces/514A._Chewbaсca_and_Number.py
@@ -20,3 +20,43 @@
 # 4545
 # outputCopy
 # 4444
+# Solution
+# Python O(N) O(1) Math Greedy String
+from typing import List
+import sys
+
+
+def solution(n: str) -> None:
+    answer: List[str] = []
+    for index in range(len(n)):
+        current_number: int = int(n[index])
+        minimum_number: int = min(current_number, 9 - current_number)
+        if index == 0 and minimum_number == 0:
+            minimum_number = current_number
+        answer.append(str(minimum_number))
+    print(''.join(answer))
+
+
+if __name__ == '__main__':
+    n: str = sys.stdin.readline().rstrip()
+    solution(n)
+
+# C++ O(N) O(1) Math Greedy String
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string init_number;
+    std::cin >> init_number;
+    std::string answer = "";
+    for (size_t index = 0; index < init_number.size(); ++index) {
+        int current_number = init_number[index] - '0';
+        int minimum_number = std::min(current_number, 9 - current_number);
+        if (index == 0 && minimum_number == 0) {
+            minimum_number = current_number;
+        }
+        answer += std::to_string(minimum_number);
+
+    }
+    std::cout << answer << std::endl;
+}
