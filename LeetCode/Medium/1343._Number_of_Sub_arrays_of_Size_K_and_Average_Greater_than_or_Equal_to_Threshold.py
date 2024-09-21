25a2e822de054cd9913fb6a2111dba156404bc11
Peter Kalugin <streetworkout200412@gmail.com>
2024-09-21 13:59:16 +0300

    Added solution "1343. Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold"

--- LeetCode/Medium/1343._Number_of_Sub_arrays_of_Size_K_and_Average_Greater_than_or_Equal_to_Threshold.py
+++ LeetCode/Medium/1343._Number_of_Sub_arrays_of_Size_K_and_Average_Greater_than_or_Equal_to_Threshold.py
@@ -20,3 +20,38 @@
 # 1 <= arr[i] <= 104
 # 1 <= k <= arr.length
 # 0 <= threshold <= 104
+# Solution
+# Python O(N) O(1) Sliding Window
+class Solution:
+    def numOfSubarrays(self, arr: List[int], k: int, threshold: int) -> int:
+        answer: int = 0
+        left: int = 0
+        current_sum: int = 0
+        for right in range(len(arr)):
+            if right - left + 1 > k:
+                current_sum -= arr[left]
+                left += 1
+            current_sum += arr[right]
+            if right - left + 1 == k:
+                answer += current_sum / k >= threshold
+        return answer
+# C++ O(N) O(1) Sliding Window
+class Solution {
+public:
+    int numOfSubarrays(vector<int>& arr, int k, int threshold) {
+        int answer = 0;
+        size_t left = 0;
+        int current_sum = 0;
+        for (size_t right = 0; right < arr.size(); ++right) {
+            if (right - left + 1 > k) {
+                current_sum -= arr[left];
+                left += 1;
+            }
+            current_sum += arr[right];
+            if (right - left + 1 == k) {
+                answer += current_sum / k >= threshold;
+            }
+        }
+        return answer;
+    }
+};
