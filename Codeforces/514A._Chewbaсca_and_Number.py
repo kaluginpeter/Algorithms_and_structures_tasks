1ee7ccd25d6ca36ad3a0461ae5c9509b3756670d
Peter Kalugin <streetworkout200412@gmail.com>
2024-09-21 10:52:39 +0300

    Added description "514A. Chewbaсca and Number"

---
+++ Codeforces/514A._Chewbaсca_and_Number.py
@@ -0,0 +1,22 @@
+# A. Chewbaсca and Number
+# time limit per test1 second
+# memory limit per test256 megabytes
+# Luke Skywalker gave Chewbacca an integer number x. Chewbacca isn't good at numbers but he loves inverting digits in them. Inverting digit t means replacing it with digit 9 - t.
+#
+# Help Chewbacca to transform the initial number x to the minimum possible positive number by inverting some (possibly, zero) digits. The decimal representation of the final number shouldn't start with a zero.
+#
+# Input
+# The first line contains a single integer x (1 ≤ x ≤ 1018) — the number that Luke Skywalker gave to Chewbacca.
+#
+# Output
+# Print the minimum possible positive number that Chewbacca can obtain after inverting some digits. The number shouldn't contain leading zeroes.
+#
+# Examples
+# inputCopy
+# 27
+# outputCopy
+# 22
+# inputCopy
+# 4545
+# outputCopy
+# 4444
