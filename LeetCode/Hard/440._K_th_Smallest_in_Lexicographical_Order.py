cc0cf0c047c4da0771c404a2cdbd3e6fc2bb077f
Peter Kalugin <streetworkout200412@gmail.com>
2024-09-22 07:50:50 +0300

    Added solution "440. K-th Smallest in Lexicographical Order"

--- LeetCode/Hard/440._K_th_Smallest_in_Lexicographical_Order.py
+++ LeetCode/Hard/440._K_th_Smallest_in_Lexicographical_Order.py
@@ -16,3 +16,58 @@
 # Constraints:
 #
 # 1 <= k <= n <= 109
+# Solution
+# Python O(logN) O(logN) DFS
+class Solution:
+    def count_steps(self, curr: int, n: int) -> int:
+        needed: int = 0
+        number1: int = curr
+        number2: int = curr + 1
+        while number1 <= n:
+            needed += min(n + 1, number2) - number1
+            number1 *= 10
+            number2 *= 10
+        return needed
+
+    def findKthNumber(self, n: int, k: int) -> int:
+        curr: int = 1
+        k -= 1
+        while k > 0:
+            needed_to_tranform: int = self.count_steps(curr, n)
+            if needed_to_tranform <= k:
+                curr += 1
+                k -= needed_to_tranform
+            else:
+                curr *= 10
+                k -= 1
+        return curr
+
+# C++ O(logN) O(logN) DFS
+class Solution {
+public:
+    long int count_steps(long int n, long int number1, long int number2) {
+        long int needed = 0;
+        while (number1 <= n) {
+            needed += std::min(n + 1, number2) - number1;
+            number1 *= 10;
+            number2 *= 10;
+        }
+        return needed;
+    }
+
+    int findKthNumber(long int n, long int k) {
+        k -= 1;
+        int current_number = 1;
+        while (k > 0) {
+            int needed_steps = count_steps(n, current_number, current_number + 1);
+            if (needed_steps <= k) {
+                k -= needed_steps;
+                current_number += 1;
+            } else {
+                k -= 1;
+                current_number *= 10;
+            }
+        }
+        return current_number;
+    }
+};
