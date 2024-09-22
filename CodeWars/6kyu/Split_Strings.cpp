993e3b00ddc5fc100cafd825c4d13d1d69904e3a
Peter Kalugin <streetworkout200412@gmail.com>
2024-09-22 09:01:38 +0300

    Added solution "Split Strings"

--- CodeWars/6kyu/Split_Strings.cpp
+++ CodeWars/6kyu/Split_Strings.cpp
@@ -7,3 +7,24 @@ Examples:
 * 'abcdef' => ['ab', 'cd', 'ef']
 REGULAR EXPRESSIONSSTRINGSALGORITHMS
 */
+// Solution
+#include <string>
+#include <vector>
+
+std::vector<std::string> solution(const std::string &s)
+{
+    std::vector<std::string> storage;
+    bool is_odd = s.size() % 2 == 1;
+    for (size_t index = 0; index < s.size(); index += 2) {
+        std::string pair;
+        if (index == s.size() - 1 && is_odd) {
+            pair = s[index];
+            pair += '_';
+        } else {
+            pair = s[index];
+            pair += s[index + 1];
+        }
+        storage.push_back(pair);
+    }
+    return storage;
+}
