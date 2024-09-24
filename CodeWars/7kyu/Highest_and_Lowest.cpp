dc5a9da092ae8c0dc1c783ce1cc8b78341ee8a8b
Peter Kalugin <streetworkout200412@gmail.com>
2024-09-24 18:18:14 +0300

    Added solution "Highest and Lowest"

--- CodeWars/7kyu/Highest_and_Lowest.cpp
+++ CodeWars/7kyu/Highest_and_Lowest.cpp
@@ -11,3 +11,18 @@ There will always be at least one number in the input string.
 Output string must be two numbers separated by a single space, and highest number is first.
 FUNDAMENTALSSTRINGS
 */
+// Soliution
+#include <string>
+#include <sstream>
+
+std::string highAndLow(const std::string& numbers){
+  std::istringstream iss(numbers);
+  int min_number = 10e7;
+  int max_number = -10e7;
+  int number;
+  while (iss >> number) {
+    min_number = std::min(min_number, number);
+    max_number = std::max(max_number, number);
+  }
+  return std::to_string(max_number) + " " + std::to_string(min_number);
+}
