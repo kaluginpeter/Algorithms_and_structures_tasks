9c55e7ec86624e689a00df2695aba45b0ce58f1d
Peter Kalugin <streetworkout200412@gmail.com>
2024-09-24 18:19:20 +0300

    Added description "1985A. Creating Words"

---
+++ Codeforces/1985A._Creating_Words.py
@@ -0,0 +1,45 @@
+# A. Creating Words
+# time limit per test1 second
+# memory limit per test256 megabytes
+# Matthew is given two strings a
+#  and b
+# , both of length 3
+# . He thinks it's particularly funny to create two new words by swapping the first character of a
+#  with the first character of b
+# . He wants you to output a
+#  and b
+#  after the swap.
+#
+# Note that the new words may not necessarily be different.
+#
+# Input
+# The first line contains t
+#  (1≤t≤100
+# )  — the number of test cases.
+#
+# The first and only line of each test case contains two space-separated strings, a
+#  and b
+# , both of length 3
+# . The strings only contain lowercase Latin letters.
+#
+# Output
+# For each test case, after the swap, output a
+#  and b
+# , separated by a space.
+#
+# Example
+# inputCopy
+# 6
+# bit set
+# cat dog
+# hot dog
+# uwu owo
+# cat cat
+# zzz zzz
+# outputCopy
+# sit bet
+# dat cog
+# dot hog
+# owu uwo
+# cat cat
+# zzz zzz
