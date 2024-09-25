436af1cd1f09ac5378672d2329a2b4c1712e9c9c
Peter Kalugin <streetworkout200412@gmail.com>
2024-09-25 07:17:27 +0300

    Added solution "2416. Sum of Prefix Scores of Strings"

--- LeetCode/Hard/2416._Sum_of_Prefix_Scores_of_Strings.py
+++ LeetCode/Hard/2416._Sum_of_Prefix_Scores_of_Strings.py
@@ -40,3 +40,83 @@
 # 1 <= words.length <= 1000
 # 1 <= words[i].length <= 1000
 # words[i] consists of lowercase English letters.
+# Solution
+# Python Trie O(NM) O(NM)
+class TrieNode:
+    def __init__(self) -> None:
+        self.childs: dict[str, TrieNode] = dict()
+        self.count: int = 0
+
+
+class Trie:
+    def __init__(self) -> None:
+        self.root: TrieNode = TrieNode()
+
+    def insert(self, word: str) -> None:
+        tmp: TrieNode = self.root
+        for char in word:
+            if char not in tmp.childs:
+                tmp.childs[char] = TrieNode()
+            tmp = tmp.childs[char]
+            tmp.count += 1
+
+
+class Solution:
+    def sumPrefixScores(self, words: List[str]) -> List[int]:
+        prefix_tree: Trie = Trie()
+        scores: list[int] = []
+        for word in words:
+            prefix_tree.insert(word)
+        for word in words:
+            current_score: int = 0
+            index: int = 0
+            tmp: TrieNode = prefix_tree.root
+            while index < len(word):
+                tmp = tmp.childs[word[index]]
+                current_score += tmp.count
+                index += 1
+            scores.append(current_score)
+        return scores
+
+# C++ Trie O(NM) O(NM)
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+class Solution {
+public:
+    struct TrieNode {
+        std::unordered_map<char, TrieNode*> childs;
+        int count = 0;
+    };
+
+    TrieNode* createTrie(const std::vector<std::string>& words) {
+        TrieNode* root = new TrieNode();
+        for (const std::string& word : words) {
+            TrieNode* tmp = root;
+            for (char letter : word) {
+                if (tmp->childs.find(letter) == tmp->childs.end()) {
+                    tmp->childs[letter] = new TrieNode();
+                }
+                tmp = tmp->childs[letter];
+                tmp->count++;
+            }
+        }
+        return root;
+    }
+
+    std::vector<int> sumPrefixScores(const std::vector<std::string>& words) {
+        TrieNode* prefixTree = createTrie(words);
+        std::vector<int> scores;
+        for (const std::string& word : words) {
+            TrieNode* tmp = prefixTree;
+            int currentScore = 0;
+            for (char letter : word) {
+                tmp = tmp->childs[letter];
+                currentScore += tmp->count;
+            }
+            scores.push_back(currentScore);
+        }
+        return scores;
+    }
+};
