1b9ae297c235aa9f5b858e86e6bbedccb879f2ac
Peter Kalugin <streetworkout200412@gmail.com>
2024-09-26 18:18:34 +0300

    Added solution "Switch on the Gravity"

--- CodeWars/7kyu/Switch_on_the_Gravity.py
+++ CodeWars/7kyu/Switch_on_the_Gravity.py
@@ -38,3 +38,14 @@
 # Each block falls individually, meaning there are no rigid objects. Think about it like falling sand in Minecraft as opposed to the rigid blocks in Tetris.
 #
 # ALGORITHMSMATRIXARRAYS
+def switch_gravity(lst):
+    for cur_col in range(len(lst[0])):
+        cur_row: int = len(lst) - 1
+        while cur_row >= 0 and lst[cur_row][cur_col] == '#':
+            cur_row -= 1
+        if cur_row < 0: continue
+        for row in range(cur_row, -1, -1):
+            if lst[row][cur_col] == '#':
+                lst[cur_row][cur_col], lst[row][cur_col] = lst[row][cur_col], lst[cur_row][cur_col]
+                cur_row -= 1
+    return lst
