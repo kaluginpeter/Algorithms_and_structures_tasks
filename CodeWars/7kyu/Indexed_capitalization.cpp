624e0031216262a56f2fe0be5640b9d038f91d67
Peter Kalugin <streetworkout200412@gmail.com>
2024-09-27 18:30:48 +0300

    Added description "Indexed capitalization"

---
+++ CodeWars/7kyu/Indexed_capitalization.cpp
@@ -0,0 +1,19 @@
+/*
+Given a string and an array of integers representing indices, capitalize all letters at the given indices.
+
+For example:
+
+capitalize("abcdef",[1,2,5]) = "aBCdeF"
+capitalize("abcdef",[1,2,5,100]) = "aBCdeF". There is no index 100.
+The input will be a lowercase string with no spaces and an array of digits.
+
+Good luck!
+
+Be sure to also try:
+
+Alternate capitalization
+
+String array revisal
+
+FUNDAMENTALS
+*/
