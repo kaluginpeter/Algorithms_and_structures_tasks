1a53e3f7726a309e2be00d4f4659e1c836736547
Peter Kalugin <streetworkout200412@gmail.com>
2024-09-29 11:47:41 +0300

    Added solution "Count characters in your string"

--- CodeWars/6kyu/Count_characters_in_your_string.cpp
+++ CodeWars/6kyu/Count_characters_in_your_string.cpp
@@ -5,3 +5,14 @@ What if the string is empty? Then the result should be empty object literal, {}.
 
 STRINGSFUNDAMENTALS
 */
+// Solution
+#include <map>
+#include <string>
+
+std::map<char, unsigned> count(const std::string& string) {
+    std::map<char, unsigned> storage;
+    for (char letter : string) {
+      storage[letter]++;
+    }
+    return storage;
+}
