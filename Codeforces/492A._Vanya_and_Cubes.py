f3351060e56bafe7a7ace2f7285a2dc08193eb1a
Peter Kalugin <streetworkout200412@gmail.com>
2024-10-01 18:09:04 +0300

    Added description "492A. Vanya and Cubes"

---
+++ Codeforces/492A._Vanya_and_Cubes.py
@@ -0,0 +1,24 @@
+# A. Vanya and Cubes
+# time limit per test1 second
+# memory limit per test256 megabytes
+# Vanya got n cubes. He decided to build a pyramid from them. Vanya wants to build the pyramid as follows: the top level of the pyramid must consist of 1 cube, the second level must consist of 1 + 2 = 3 cubes, the third level must have 1 + 2 + 3 = 6 cubes, and so on. Thus, the i-th level of the pyramid must have 1 + 2 + ... + (i - 1) + i cubes.
+#
+# Vanya wants to know what is the maximum height of the pyramid that he can make using the given cubes.
+#
+# Input
+# The first line contains integer n (1 ≤ n ≤ 104) — the number of cubes given to Vanya.
+#
+# Output
+# Print the maximum possible height of the pyramid in the single line.
+#
+# Examples
+# inputCopy
+# 1
+# outputCopy
+# 1
+# inputCopy
+# 25
+# outputCopy
+# 4
+# Note
+# Illustration to the second sample:
