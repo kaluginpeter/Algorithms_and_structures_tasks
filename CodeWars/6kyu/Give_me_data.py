401961c87aea1d61b3bd2e42692f254b819b6b20
Peter Kalugin <streetworkout200412@gmail.com>
2024-10-01 17:41:25 +0300

    Added description "Give me data"

---
+++ CodeWars/6kyu/Give_me_data.py
@@ -0,0 +1,22 @@
+# In this kata, your task is to write a function which will determine whether the user has or does not have the permission to download certain data.
+#
+# Data: movies, games, books.
+# Info about user's permissions is stored in a database with the following form: "movies_allow", "games_deny" ..
+#
+# When user wants to download a game and the db contains "games_deny", your function should return False.
+#
+# The user can also have everything allowed or denied: "*_allow", "*_deny".
+#
+# You can also have everything denied and some data allowed: "*_deny", "movies_allow".
+#
+# Count on incorrectness of permissions: user can have same data allowed and denied e.g.: "books_allow", "books_deny" - which results in "books_deny".
+#
+# hint: specific data has a higher priority than "*", so watch out for cases like: "*_allow", "books_deny" - in this case, the user can download everything apart from books.
+#
+# Example:
+#
+# { "books_allow", "movies_deny" }, "movies"  =>  False
+# { "books_allow", "movies_deny" }, "books"  =>  True
+# { "*_allow", "books_allow", "movies_deny" }, "games"  =>  True
+# { "*_allow", "*_deny" }, "movies"  =>  False
+# LOGICSETSFUNDAMENTALS
