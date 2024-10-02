dce95518ed84ae2a382362244e6e79b0635e7331
Peter Kalugin <streetworkout200412@gmail.com>
2024-10-02 17:55:37 +0300

    Added solution "Latin Squares"

--- CodeWars/7kyu/Latin_Squares.py
+++ CodeWars/7kyu/Latin_Squares.py
@@ -16,3 +16,6 @@
 # You might also enjoy Latin Square Validator and Euler Squares.
 #
 # ARRAYS
+# Solution
+def make_latin_square(n):
+    return [[(i + j) % n + 1 for j in range(n)] for i in range(n)]
