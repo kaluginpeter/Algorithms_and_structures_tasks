ffeb52cad9b8b83ff32ad393c5d1b587fd1bc418
Peter Kalugin <streetworkout200412@gmail.com>
2024-10-05 08:40:53 +0300

    Added solution "567. Permutation in String"

--- LeetCode/Medium/567._Permutation_in_String.py
+++ LeetCode/Medium/567._Permutation_in_String.py
@@ -44,3 +44,55 @@ class Solution:
             else: storage[s2[right]] -= 1
             if right - left + 1 == k: return True
         return False
+
+# Python O(N) O(N) Hash Map Two Pointers Sliding Window
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        storage: dict[str, int] = dict()
+        for idx in range(len(s1)):
+            storage[s1[idx]] = storage.get(s1[idx], 0) + 1
+        left: int = 0
+        for right in range(len(s2)):
+            while s2[right] in storage and not storage[s2[right]]:
+                storage[s2[left]] += 1
+                left += 1
+            if s2[right] in storage:
+                storage[s2[right]] -= 1
+                if right - left + 1 == len(s1): return True
+            else:
+                while left < right:
+                    storage[s2[left]] = storage.get(s2[left], 0) + 1
+                    left += 1
+                left += 1
+        return False
+
+# C++ O(N) O(N) HashMap Two Pointers Sliding Window
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        std::unordered_map<char, int> storage;
+        for (char letter : s1) {
+            storage[letter]++;
+        }
+        int left = 0;
+        for (int right = 0; right < s2.size(); ++right) {
+            while (storage.count(s2[right]) && !storage[s2[right]]) {
+                storage[s2[left]]++;
+                left += 1;
+            }
+            if (storage.count(s2[right])) {
+                storage[s2[right]]--;
+                if (right - left + 1 == s1.size()) {
+                    return true;
+                }
+            } else {
+                while (left < right) {
+                    storage[s2[left]]++;
+                    left += 1;
+                }
+                left += 1;
+            }
+        }
+        return false;
+    }
+};
