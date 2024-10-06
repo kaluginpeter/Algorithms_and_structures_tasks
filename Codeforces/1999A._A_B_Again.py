ad49037fccd9848387717998d4ed2ce508b2ed0c
Peter Kalugin <streetworkout200412@gmail.com>
2024-10-06 09:09:43 +0300

    Added description "1999A. A+B Again?"

---
+++ Codeforces/1999A._A_B_Again.py
@@ -0,0 +1,39 @@
+# A. A+B Again?
+# time limit per test1 second
+# memory limit per test256 megabytes
+# Given a two-digit positive integer n
+# , find the sum of its digits.
+#
+# Input
+# The first line contains an integer t
+#  (1≤t≤90
+# ) — the number of test cases.
+#
+# The only line of each test case contains a single two-digit positive integer n
+#  (10≤n≤99
+# ).
+#
+# Output
+# For each test case, output a single integer — the sum of the digits of n
+# .
+#
+# Example
+# inputCopy
+# 8
+# 77
+# 21
+# 40
+# 34
+# 19
+# 84
+# 10
+# 99
+# outputCopy
+# 14
+# 3
+# 4
+# 7
+# 10
+# 12
+# 1
+# 18
