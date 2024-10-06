d9b888712fe21bd25e1a7ee0b83e51f9e299b1d8
Peter Kalugin <streetworkout200412@gmail.com>
2024-10-06 08:23:15 +0300

    Added description "3309. Maximum Possible Number by Binary Concatenation"

---
+++ LeetCode/Medium/3309._Maximum_Possible_Number_by_Binary_Concatenation.py
@@ -0,0 +1,34 @@
+# You are given an array of integers nums of size 3.
+#
+# Return the maximum possible number whose binary representation can be formed by concatenating the binary representation of all elements in nums in some order.
+#
+# Note that the binary representation of any number does not contain leading zeros.
+#
+#
+#
+# Example 1:
+#
+# Input: nums = [1,2,3]
+#
+# Output: 30
+#
+# Explanation:
+#
+# Concatenate the numbers in the order [3, 1, 2] to get the result "11110", which is the binary representation of 30.
+#
+# Example 2:
+#
+# Input: nums = [2,8,16]
+#
+# Output: 1296
+#
+# Explanation:
+#
+# Concatenate the numbers in the order [2, 8, 16] to get the result "10100010000", which is the binary representation of 1296.
+#
+#
+#
+# Constraints:
+#
+# nums.length == 3
+# 1 <= nums[i] <= 127
