3e670fa68fae5f3b58f2e3391b8e5c095a14b888
Peter Kalugin <streetworkout200412@gmail.com>
2024-10-06 17:25:35 +0300

    Added solution "3305. Count of Substrings Containing Every Vowel and K Consonants I"

--- LeetCode/Medium/3305._Count_of_Substrings_Containing_Every_Vowel_and_K_Consonants_I.py
+++ LeetCode/Medium/3305._Count_of_Substrings_Containing_Every_Vowel_and_K_Consonants_I.py
@@ -46,3 +46,53 @@
 # 5 <= word.length <= 250
 # word consists only of lowercase English letters.
 # 0 <= k <= word.length - 5
+# Solution
+# Python Brute Force O(N**2) O(1)
+class Solution:
+    def countOfSubstrings(self, word: str, k: int) -> int:
+        count: int = 0
+        for left in range(len(word)):
+            vow: dict = dict()
+            const: int = 0
+            for right in range(left, len(word)):
+                if word[right] in 'aeoiu':
+                    vow[word[right]] = vow.get(word[right], 0) + 1
+                else: const += 1
+                if len(vow) == 5 and const == k: count += 1
+        return count
+# Python Sliding Window O(N) O(1)
+from collections import defaultdict
+class Solution:
+    def countOfSubstrings(self, word: str, k: int) -> int:
+        vowels: set[str] = set("aeiou")
+        hashmap: dict[str, int] = defaultdict(int)
+        consonants: int = 0
+        left: int = 0
+        answer: int = 0
+        for right in range(len(word)):
+            if word[right] in vowels:
+                hashmap[word[right]] += 1
+            else: consonants += 1
+            while consonants > k:
+                if word[left] in hashmap :
+                    hashmap[word[left]] -= 1
+                    if hashmap[word[left]] == 0:
+                        del hashmap[word[left]]
+                else: consonants -= 1
+                left += 1
+            if len(hashmap) == 5 and consonants == k:
+                answer += 1
+                second_left: int = left
+                second_hashmap: dict[str, int] = hashmap.copy()
+                second_consonants: int = consonants
+                while len(second_hashmap) == 5:
+                    if word[second_left] in second_hashmap:
+                        second_hashmap[word[second_left]] -= 1
+                        if second_hashmap[word[second_left]] == 0:
+                            del second_hashmap[word[second_left]]
+                    else:second_consonants -= 1
+                    second_left += 1
+                    if second_consonants == k and len(second_hashmap) == 5:
+                        answer += 1
+                    if consonants < k: break
+        return answer
