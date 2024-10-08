ef92415f6a78c04c782daf944f1f9b8d295f9e58
Peter Kalugin <streetworkout200412@gmail.com>
2024-10-08 18:00:38 +0300

    Added solution "Matrix Exponentiation"

--- CodeWars/6kyu/Matrix_Exponentiation.py
+++ CodeWars/6kyu/Matrix_Exponentiation.py
@@ -9,3 +9,27 @@
 # External aiding modules disabled
 #
 # ARRAYSMATRIXALGORITHMSLOGIC
+# Solution
+def matrix_multiply(A, B):
+    n = len(A)
+    result = [[0] * n for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                result[i][j] += A[i][k] * B[k][j]
+    return result
+
+
+def calc(matrix, n):
+    if n == 0:
+        size = len(matrix)
+        return [[1 if i == j else 0 for j in range(size)] for i in range(size)]
+    elif n == 1:
+        return matrix
+
+    half_power = calc(matrix, n // 2)
+
+    if n % 2 == 0:
+        return matrix_multiply(half_power, half_power)
+    else:
+        return matrix_multiply(matrix, matrix_multiply(half_power, half_power))
