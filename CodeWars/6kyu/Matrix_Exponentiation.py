9f97c33d250ebb6b70097d5933077b8db14fcf5e
Peter Kalugin <streetworkout200412@gmail.com>
2024-10-08 18:00:19 +0300

    Added description "Matrix Exponentiation"

---
+++ CodeWars/6kyu/Matrix_Exponentiation.py
@@ -0,0 +1,11 @@
+# Your task in this kata is to write a function which will take a square matrix and a non-negative integer n as inputs and return a matrix raised to the power n.
+#
+# Be ready to handle big ns.
+#
+# Example:
+#
+# A = [[1, 2], [1, 0]]
+# calc(A, 2) = [[3, 2], [1, 2]]
+# External aiding modules disabled
+#
+# ARRAYSMATRIXALGORITHMSLOGIC
