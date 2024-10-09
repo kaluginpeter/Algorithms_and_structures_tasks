7fa4c886e94ebc44b2abbdefeaee80ecf3252bea
Peter Kalugin <streetworkout200412@gmail.com>
2024-10-09 06:37:46 +0300

    Added solution "921. Minimum Add to Make Parentheses Valid"

--- LeetCode/Medium/921._Minimum_Add_to_Make_Parentheses_Valid.py
+++ LeetCode/Medium/921._Minimum_Add_to_Make_Parentheses_Valid.py
@@ -24,3 +24,39 @@
 #
 # 1 <= s.length <= 1000
 # s[i] is either '(' or ')'.
+# Solution
+# C++ O(N) O(1) Stack Greedy Counting
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        int open_br = 0, close_br = 0;
+        int count = 0;
+        for (size_t index = 0; index < s.size(); ++index) {
+            if (s[index] == '(') {
+                open_br += 1;
+            } else {
+                close_br += 1;
+                if (close_br > open_br) {
+                    close_br -= 1;
+                    count += 1;
+                }
+            }
+        }
+        return count + std::max(open_br - close_br, 0);
+    }
+};
+
+# Python O(N) O(1) Counting Greedy Stack
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+        open_br: int = 0
+        close_br: int = 0
+        count: int = 0
+        for brace in s:
+            if brace == '(':
+                open_br += 1
+            else: close_br += 1
+            if close_br > open_br:
+                count += 1
+                close_br -= 1
+        return count + max(open_br - close_br, 0)
