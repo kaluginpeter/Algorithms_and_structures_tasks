7e47743eacfea5c4fb37432736a9ab7fd7e13eda
Peter Kalugin <streetworkout200412@gmail.com>
2024-10-11 16:21:37 +0300

    Added solution "489B. BerSU Ball"

--- Codeforces/489B._BerSU_Ball.py
+++ Codeforces/489B._BerSU_Ball.py
@@ -37,3 +37,74 @@
 # 1 2 3
 # outputCopy
 # 2
+# Solution
+# Python O(NlogN) O(1) Two Pointers
+import sys
+
+
+def solution(boys: list, girls: list) -> str:
+    boys.sort()
+    girls.sort()
+    left: int = 0
+    right: int = 0
+    pairs: int = 0
+    while left < len(boys) and right < len(girls):
+        if abs(boys[left] - girls[right]) <= 1:
+            pairs += 1
+            left += 1
+            right += 1
+        elif boys[left] < girls[right]:
+            left += 1
+        else:
+            right += 1
+    return str(pairs)
+
+
+if __name__ == '__main__':
+    n: int = int(sys.stdin.readline().rstrip())
+    boys: list = list(map(int, sys.stdin.readline().rstrip().split()))
+    m: int = int(sys.stdin.readline().rstrip())
+    girls: list = list(map(int, sys.stdin.readline().rstrip().split()))
+    sys.stdout.write(solution(boys, girls))
+
+
+# C++ O(NlogN) O(1) Two Pointers
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    int n;
+    std::cin >> n;
+    std::vector<int> boys;
+    for (int i = 0; i < n; ++i) {
+        int boy;
+        std::cin >> boy;
+        boys.push_back(boy);
+    }
+    int m;
+    std::cin >> m;
+    std::vector<int> girls;
+    for (int i = 0; i < m; ++i) {
+        int girl;
+        std::cin >> girl;
+        girls.push_back(girl);
+    }
+    std::sort(boys.begin(), boys.end());
+    std::sort(girls.begin(), girls.end());
+    int left = 0, right = 0;
+    int pairs = 0;
+    while (left < boys.size() && right < girls.size()) {
+        if (std::abs(boys[left] - girls[right]) <= 1) {
+            pairs += 1;
+            left += 1;
+            right += 1;
+        } else if (boys[left] < girls[right]) {
+            left += 1;
+        } else {
+            right += 1;
+        }
+    }
+    std::cout << pairs << std::endl;
+}
