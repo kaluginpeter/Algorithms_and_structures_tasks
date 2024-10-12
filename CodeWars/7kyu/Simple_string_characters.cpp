8fbeada965811682ea3874b1bff693c801b475be
Peter Kalugin <streetworkout200412@gmail.com>
2024-10-12 09:10:45 +0300

    Added description "Simple string characters"

---
+++ CodeWars/7kyu/Simple_string_characters.cpp
@@ -0,0 +1,12 @@
+/*
+In this Kata, you will be given a string and your task will be to return a list of ints detailing the count of uppercase letters, lowercase, numbers and special characters (everything else), as follows.
+
+The order is: uppercase letters, lowercase letters, numbers and special characters.
+
+"*'&ABCDabcde12345" --> [ 4, 5, 5, 3 ]
+More examples in the test cases.
+
+Good luck!
+
+FUNDAMENTALS
+*/
