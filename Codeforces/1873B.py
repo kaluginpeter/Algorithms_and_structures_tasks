84d87c671e722f759a0550ad0ada562b9260eb80
Peter Kalugin <streetworkout200412@gmail.com>
2024-10-12 09:05:13 +0300

    Added description "1873B. Good Kid"

---
+++ Codeforces/1873B.py
@@ -0,0 +1,44 @@
+# B. Good Kid
+# time limit per test1 second
+# memory limit per test256 megabytes
+# Slavic is preparing a present for a friend's birthday. He has an array a
+#  of n
+#  digits and the present will be the product of all these digits. Because Slavic is a good kid who wants to make the biggest product possible, he wants to add 1
+#  to exactly one of his digits.
+#
+# What is the maximum product Slavic can make?
+#
+# Input
+# The first line contains a single integer t
+#  (1≤t≤104
+# ) — the number of test cases.
+#
+# The first line of each test case contains a single integer n
+#  (1≤n≤9
+# ) — the number of digits.
+#
+# The second line of each test case contains n
+#  space-separated integers ai
+#  (0≤ai≤9
+# ) — the digits in the array.
+#
+# Output
+# For each test case, output a single integer — the maximum product Slavic can make, by adding 1
+#  to exactly one of his digits.
+#
+# Example
+# inputCopy
+# 4
+# 4
+# 2 2 1 2
+# 3
+# 0 1 2
+# 5
+# 4 3 2 3 4
+# 9
+# 9 9 9 9 9 9 9 9 9
+# outputCopy
+# 16
+# 2
+# 432
+# 430467210
