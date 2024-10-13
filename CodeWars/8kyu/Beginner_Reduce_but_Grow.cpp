bcdd6c8eef26ccb15ca9fcf66dc7ab6bffe91e8c
Peter Kalugin <streetworkout200412@gmail.com>
2024-10-13 10:54:51 +0300

    Added solution "Beginner - Reduce but Grow"

--- CodeWars/8kyu/Beginner_Reduce_but_Grow.cpp
+++ CodeWars/8kyu/Beginner_Reduce_but_Grow.cpp
@@ -4,3 +4,12 @@ Given a non-empty array of integers, return the result of multiplying the values
 [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
 FUNDAMENTALSARRAYS
 */
+// Solution
+#include <vector>
+int grow(std::vector<int> nums) {
+  int accumulate = 1;
+  for (int num : nums) {
+    accumulate *= num;
+  }
+  return accumulate;
+}
