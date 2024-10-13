65ecbf538b18dbb2fe6dd08f634e97356389e726
Peter Kalugin <streetworkout200412@gmail.com>
2024-10-13 09:59:11 +0300

    Added solution "3319. K-th Largest Perfect Subtree Size in Binary Tree"

--- LeetCode/Medium/3319._K_th_Largest_Perfect_Subtree_Size_in_Binary_Tree.py
+++ LeetCode/Medium/3319._K_th_Largest_Perfect_Subtree_Size_in_Binary_Tree.py
@@ -52,3 +52,61 @@
 # The number of nodes in the tree is in the range [1, 2000].
 # 1 <= Node.val <= 2000
 # 1 <= k <= 1024
+# Solution
+# Python O(N) O(N) Depth-First-Search
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def traverse(self, root: Optional[TreeNode], depth: int, trees: list[int]) -> int:
+        if not root:
+            return 0
+        left_part = self.traverse(root.left, 1, trees)
+        right_part = self.traverse(root.right, 1, trees)
+        if left_part != right_part or min(left_part, right_part) == -1: return -1
+        trees.append(left_part + right_part + 1)
+        return right_part + left_part + 1
+
+    def kthLargestPerfectSubtree(self, root: Optional[TreeNode], k: int) -> int:
+        trees = []
+        self.traverse(root, 0, trees)
+        trees.sort(reverse=True)
+        return trees[k - 1] if k <= len(trees) else -1
+
+# C++ O(N) O(N) Depth-First-Search
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int traverse(TreeNode* root, int depth, std::vector<int>& roots) {
+        if (!root) {
+            return 0;
+        }
+        int left_part = traverse(root->left, 1, roots);
+        int right_part = traverse(root->right, 1, roots);
+        if (left_part != right_part || std::min(left_part, right_part) == -1) {
+            return -1;
+        }
+        roots.push_back(left_part + right_part + 1);
+        return left_part + right_part + 1;
+    }
+
+    int kthLargestPerfectSubtree(TreeNode* root, int k) {
+        std::vector<int> roots;
+        traverse(root, 0, roots);
+        std::sort(roots.begin(), roots.end(), std::greater<>());
+        return (k > roots.size() ? -1 : roots[k - 1]);
+    }
+};
