af0e9135832461977f63deb8fcde05e1bc2760f5
Peter Kalugin <streetworkout200412@gmail.com>
2024-10-14 06:44:28 +0300

    Added solution "2530. Maximal Score After Applying K Operations"

--- LeetCode/Medium/2530._Maximal_Score_After_Applying_K_Operations.py
+++ LeetCode/Medium/2530._Maximal_Score_After_Applying_K_Operations.py
@@ -31,3 +31,35 @@
 #
 # 1 <= nums.length, k <= 105
 # 1 <= nums[i] <= 109
+# Solution
+# Python O(NlogN + KlogN) O(N) Heap Priority Queue Greedy
+class Solution:
+    def maxKelements(self, nums: List[int], k: int) -> int:
+        heap: list[int] = []
+        for num in nums:
+            heapq.heappush(heap, -num)
+        score: int = 0
+        for _ in range(k):
+            current_move: int = -heapq.heappop(heap)
+            score += current_move
+            heapq.heappush(heap, -math.ceil(current_move / 3))
+        return score
+
+# C++ O(NlogN + KlogN) O(N) Heap Greedy Priority Queue
+class Solution {
+public:
+    long long maxKelements(vector<int>& nums, int k) {
+        std::priority_queue<int, std::vector<int>> max_heap;
+        for (int num : nums) {
+            max_heap.push(num);
+        }
+        long long score = 0;
+        for (int move = 0; move < k; ++move) {
+            int current_num = max_heap.top();
+            max_heap.pop();
+            score += current_num;
+            max_heap.push((current_num % 3 == 0 ? current_num / 3 : current_num / 3 + 1));
+        }
+        return score;
+    }
+};
