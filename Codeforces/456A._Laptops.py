16d23e605f89d1cf015155816b6ba58ada8675cc
Peter Kalugin <streetworkout200412@gmail.com>
2024-10-16 09:44:03 +0300

    Added solution "456A. Laptops"

--- Codeforces/456A._Laptops.py
+++ Codeforces/456A._Laptops.py
@@ -22,3 +22,51 @@
 # 2 1
 # outputCopy
 # Happy Alex
+# Solution
+# Python O(NLogN) O(1) Two Pointers Sorting
+import sys
+
+
+def solution() -> str:
+    n: int = int(sys.stdin.readline().rstrip())
+    tablets: list = []
+    for _ in range(n):
+        price, quality = map(int, sys.stdin.readline().rstrip().split())
+        tablets.append((price, quality))
+    tablets.sort()
+    left: int = 0
+    for right in range(1, n):
+        if tablets[right][0] > tablets[left][0] and tablets[right][1] < tablets[left][1]:
+            return 'Happy Alex'
+        left += 1
+    return 'Poor Alex'
+
+
+if __name__ == '__main__':
+    sys.stdout.write(solution())
+
+# C++ O(NlogN) O(1) Two Pointers Sorting
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    int n;
+    std::cin >> n;
+    std::vector<std::pair<int, int>> tablets;
+    for (int i = 0; i < n; ++i) {
+        int price, quality;
+        std::cin >> price >> quality;
+        tablets.push_back({price, quality});
+    }
+    std::sort(tablets.begin(), tablets.end());
+    std::size_t left = 0;
+    for (size_t right = 1; right < tablets.size(); ++right) {
+        if (tablets[right].first > tablets[left].first && tablets[left].second > tablets[right].second) {
+            std::cout << "Happy Alex" << std::endl;
+            return 0;
+        }
+        ++left;
+    }
+    std::cout << "Poor Alex" << std::endl;
+}
