94e465d4d75b4c7779ebc4fa73d6c271fb39aa14
Peter Kalugin <streetworkout200412@gmail.com>
2024-10-17 09:43:47 +0300

    Added solution "Collaboration Station"

--- CodeWars/6kyu/Collaboration_Station.py
+++ CodeWars/6kyu/Collaboration_Station.py
@@ -9,3 +9,45 @@
 # Treat null messages as empty strings (C#, JavaScript). If your number of friends is greater than the length of your message, some friends will simply receive all dashes. If you have zero friends (or less than zero), return null (empty vector for C++);
 #
 # FUNDAMENTALS
+# Solution
+# Python
+def split_message(message, count):
+    if count <= 0: return None
+    friends: list[str] = []
+    words: list[str] = list(message)
+    for friend in range(count):
+        friend_part: list[str] = []
+        friend_idx: int = friend
+        for idx in range(len(words)):
+            if idx == friend_idx:
+                friend_part.append(words[idx])
+                words[idx] = '-'
+                friend_idx += count
+            else:
+                friend_part.append('-')
+        friends.append(''.join(friend_part))
+    return friends
+
+# C++
+#include <string>
+#include <vector>
+
+std::vector<std::string> splitMessage(std::string message, int count)
+{
+    std::vector<std::string> result;
+    for (int frnd = 0; frnd < count; ++frnd) {
+      std::string friend_part = "";
+      int friend_idx = frnd;
+      for (size_t idx = 0; idx < message.size(); ++idx) {
+        if (idx == static_cast<size_t>(friend_idx)) {
+          friend_part += message[idx];
+          message[idx] = '-';
+          friend_idx += count;
+        } else {
+          friend_part += '-';
+        }
+      }
+      result.push_back(friend_part);
+    }
+    return result;
+}
