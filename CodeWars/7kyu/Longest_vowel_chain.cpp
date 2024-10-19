1535cb952f520a09038b5317565d73de7758341d
Peter Kalugin <streetworkout200412@gmail.com>
2024-10-19 11:35:25 +0300

    Added description "Longest vowel chain"

---
+++ CodeWars/7kyu/Longest_vowel_chain.cpp
@@ -0,0 +1,13 @@
+/*
+The vowel substrings in the word codewarriors are o,e,a,io. The longest of these has a length of 2. Given a lowercase string that has alphabetic characters only (both vowels and consonants) and no spaces, return the length of the longest vowel substring. Vowels are any of aeiou.
+
+Good luck!
+
+If you like substring Katas, please try:
+
+Non-even substrings
+
+Vowel-consonant lexicon
+
+STRINGSFUNDAMENTALS
+*/
