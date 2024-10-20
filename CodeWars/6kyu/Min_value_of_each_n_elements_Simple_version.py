b36f9565c4bae691691f652dc5899fad10d3c4d2
Peter Kalugin <streetworkout200412@gmail.com>
2024-10-20 09:13:28 +0300

    Added description "Min value of each n elements -- Simple version"

---
+++ CodeWars/6kyu/Min_value_of_each_n_elements_Simple_version.py
@@ -0,0 +1,26 @@
+# When no more interesting kata can be resolved, I just choose to create the new kata, to solve their own, to enjoy the process --myjinxin2015 said
+#
+# Description:
+# Complete the function. Two arguments will be given:
+#
+# arr: An array that contains some integers(positve,negative or zero).
+# n: A positive integer. n <= arr.length.
+# Your task is to find the minimum value of each n adjacent elements in arr. Returns by a new array. For example:
+#
+# arr = [1,2,3,10,-5], n = 2   ==>  [1,2,3,-5]
+# # The min value of each 2 adjacent elements are:
+# [(1,2)...] --> 1,
+# [.(2,3)..] --> 2
+# [..(3,10).] --> 3
+# [...(10,-5)] --> -5
+# Some Examples
+# [1,-2,3,-4,5,-6,7,8], 1  ==>  [1,-2,3,-4,5,-6,7,8]
+# [1,-2,3,-4,5,-6,7,8], 2  ==>  [-2,-2,-4,-4,-6,-6,7]
+# [1,-2,3,-4,5,-6,7,8], 3  ==>  [-2,-4,-4,-6,-6,-6]
+# [1,-2,3,-4,5,-6,7,8], 4  ==>  [-4,-4,-6,-6,-6]
+# Note:
+#
+# You can assume that all inputs are valid.
+# This is a simple version, please look forward to the challenge version ;-)
+# A bit difficulty, A bit of fun, happy coding ;-)
+# ALGORITHMSPUZZLES
