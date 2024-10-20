7c60994ee01108b2d056cf705d76bfbaae040771
Peter Kalugin <streetworkout200412@gmail.com>
2024-10-20 08:39:20 +0300

    Added solution "1106. Parsing A Boolean Expression"

--- LeetCode/Hard/1106._Parsing_A_Boolean_Expression.py
+++ LeetCode/Hard/1106._Parsing_A_Boolean_Expression.py
@@ -37,3 +37,103 @@
 #
 # 1 <= expression.length <= 2 * 104
 # expression[i] is one following characters: '(', ')', '&', '|', '!', 't', 'f', and ','.
+# Solution
+# Python O(N) O(N) Stack String
+class Solution:
+    def boolean_calc(self, left: bool, right: bool, comparison_method: str) -> bool:
+        if comparison_method == '&':
+            left = left and right
+        elif comparison_method == '|':
+            left = left or right
+        return left
+
+    def parseBoolExpr(self, expression: str) -> bool:
+        stack: list[tuple[str, bool]] = []
+        comparison_method: str = ''
+        answer: bool = None
+        for idx in range(len(expression)):
+            if expression[idx] == 't':
+                if answer is None:
+                    answer = True
+                else:
+                    answer = self.boolean_calc(answer, True, comparison_method)
+
+            elif expression[idx] == 'f':
+                if answer is None:
+                    answer = False
+                else:
+                    answer = self.boolean_calc(answer, False, comparison_method)
+
+            elif expression[idx] in '!&|':
+                stack.append((comparison_method, answer))
+                comparison_method = expression[idx]
+                answer = None
+
+            elif expression[idx] == ')':
+                if comparison_method == '!':
+                    answer = not answer
+                prev_comparison_method, prev_answer = stack.pop()
+                comparison_method = prev_comparison_method
+                if prev_answer is not None:
+                    answer = self.boolean_calc(prev_answer, answer, comparison_method)
+
+            elif expression[idx] in ',(':
+                continue
+
+        return answer
+
+# C++ O(N) O(N) Stack String
+class Solution {
+public:
+    bool booleanCalc(bool left, bool right, char comparisonMethod) {
+        if (comparisonMethod == '&') {
+            left = left && right;
+        } else if (comparisonMethod == '|') {
+            left = left | right;
+        }
+        return left;
+    }
+
+    bool parseBoolExpr(string expression) {
+        std::vector<std::tuple<char, bool, bool>> stack;
+        char comparisonMethod = 'None';
+        bool answer = false;
+        bool seenFirst = true;
+        std::string operators = "!&|";
+        std::string canSkip = "(,";
+        for (int index = 0; index < expression.size(); ++index) {
+            if (expression[index] == 't') {
+                if (seenFirst) {
+                    seenFirst = false;
+                    answer = true;
+                } else {
+                    answer = booleanCalc(answer, true, comparisonMethod);
+                }
+            } else if (expression[index] == 'f') {
+                if (seenFirst) {
+                    seenFirst = false;
+                    answer = false;
+                } else {
+                    answer = booleanCalc(answer, false, comparisonMethod);
+                }
+            } else if (operators.find(expression[index]) != std::string::npos) {
+                stack.push_back(std::make_tuple(comparisonMethod, answer, seenFirst));
+                seenFirst = true;
+                comparisonMethod = expression[index];
+            } else if (expression[index] == ')') {
+                auto [prevComparisonMethod, prevSeen, prevAnswer] = stack[stack.size() - 1];
+                stack.pop_back();
+                if (comparisonMethod == '!') {
+                    answer = !answer;
+                }
+                comparisonMethod = prevComparisonMethod;
+                if (!prevSeen) {
+                    answer = booleanCalc(answer, prevAnswer, comparisonMethod);
+                }
+            } else if (canSkip.find(expression[index]) != std::string::npos) {
+                continue;
+            }
+        }
+        return answer;
+    }
+};
