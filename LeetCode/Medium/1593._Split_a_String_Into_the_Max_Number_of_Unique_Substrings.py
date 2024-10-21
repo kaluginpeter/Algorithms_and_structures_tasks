e757742206083dcb3830d50d29464e2e4f64dce7
Peter Kalugin <streetworkout200412@gmail.com>
2024-10-21 08:53:45 +0300

    Added solution "1593. Split a String Into the Max Number of Unique Substrings"

--- LeetCode/Medium/1593._Split_a_String_Into_the_Max_Number_of_Unique_Substrings.py
+++ LeetCode/Medium/1593._Split_a_String_Into_the_Max_Number_of_Unique_Substrings.py
@@ -28,3 +28,57 @@
 # 1 <= s.length <= 16
 #
 # s contains only lower case English letters.
+# Solution
+# Python O(2**N) O(N)
+class Solution:
+    def backtrack(self, s: str, left: int, right: int, seen: set[str], mx: list[int]) -> bool:
+        while right < len(s):
+            if s[left:right] not in seen:
+                seen.add(s[left:right])
+                self.backtrack(s, right, right + 1, seen, mx)
+                seen.remove(s[left:right])
+            right += 1
+        if right == len(s) and s[left:right] in seen:
+            return False
+        seen.add(s[left:right])
+        mx[0] = max(mx[0], len(seen))
+        seen.remove(s[left:right])
+        return True
+
+    def maxUniqueSplit(self, s: str) -> int:
+        seen: set[str] = set()
+        mx: list[int] = [0]
+        self.backtrack(s, 0, 1, seen, mx)
+        return mx[0]
+
+# C++ O(2**N) O(N)
+#include <string>
+#include <unordered_set>
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    void backtrack(std::string& s, int start, std::unordered_set<std::string>& seen, int& maxUnique) {
+        if (start == s.size()) {
+            maxUnique = std::max(maxUnique, static_cast<int>(seen.size()));
+            return;
+        }
+
+        for (int end = start + 1; end <= s.size(); ++end) {
+            std::string substring = s.substr(start, end - start);
+            if (seen.count(substring) == 0) {
+                seen.insert(substring);
+                backtrack(s, end, seen, maxUnique);
+                seen.erase(substring);
+            }
+        }
+    }
+
+    int maxUniqueSplit(std::string s) {
+        std::unordered_set<std::string> seen;
+        int maxUnique = 0;
+        backtrack(s, 0, seen, maxUnique);
+        return maxUnique;
+    }
+};
