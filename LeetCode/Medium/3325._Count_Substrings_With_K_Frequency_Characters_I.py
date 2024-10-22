6602a4cc52ca315e54e08accbb1df3dcacfccceb
Peter Kalugin <streetworkout200412@gmail.com>
2024-10-22 13:05:56 +0300

    Added solution "3325. Count Substrings With K-Frequency Characters I"

--- LeetCode/Medium/3325._Count_Substrings_With_K_Frequency_Characters_I.py
+++ LeetCode/Medium/3325._Count_Substrings_With_K_Frequency_Characters_I.py
@@ -35,3 +35,38 @@
 # 1 <= s.length <= 3000
 # 1 <= k <= s.length
 # s consists only of lowercase English letters.
+# Solution
+# Python O(N) O(1) HashMap Sliding Window
+class Solution:
+    def numberOfSubstrings(self, s: str, k: int) -> int:
+        n: int = len(s)
+        valid_substrings: int = (n + 1) * n // 2
+        left: int = 0
+        hashmap: dict[str, int] = dict()
+        for right in range(n):
+            hashmap[s[right]] = hashmap.get(s[right], 0) + 1
+            while hashmap[s[right]] >= k:
+                hashmap[s[left]] -= 1
+                left += 1
+            valid_substrings -= right - left + 1
+        return valid_substrings
+
+# C++ O(N) O(1) HashMap Sliding Window
+class Solution {
+public:
+    int numberOfSubstrings(string s, int k) {
+        int n = s.size();
+        int validSubstrings = (n + 1) * n / 2;
+        int left = 0;
+        std::unordered_map<char, int> hashmap;
+        for (int right = 0; right < s.size(); ++right) {
+            ++hashmap[s[right]];
+            while (hashmap[s[right]] >= k) {
+                --hashmap[s[left]];
+                ++left;
+            }
+            validSubstrings -= right - left + 1;
+        }
+        return validSubstrings;
+    }
+};
