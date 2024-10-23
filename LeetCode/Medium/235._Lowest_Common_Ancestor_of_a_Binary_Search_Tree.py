86ebdc55dcb0b94b61827ad4681b563b13c6350c
Peter Kalugin <streetworkout200412@gmail.com>
2024-10-23 17:10:13 +0300

    Added solution "235. Lowest Common Ancestor of a Binary Search Tree"

--- LeetCode/Medium/235._Lowest_Common_Ancestor_of_a_Binary_Search_Tree.py
+++ LeetCode/Medium/235._Lowest_Common_Ancestor_of_a_Binary_Search_Tree.py
@@ -29,3 +29,119 @@
 # All Node.val are unique.
 # p != q
 # p and q will exist in the BST.
+# Solution
+# C++ O(N) O(N) Least Common Path Depth-First-Search
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool dfsTraverse(TreeNode* root, TreeNode* x, std::vector<TreeNode*>& path) {
+        if (!root) {
+            return false;
+        }
+        path.push_back(root);
+        if (root->val == x->val) {
+            return true;
+        }
+        bool leftPart = dfsTraverse(root->left, x, path);
+        bool rightPart = dfsTraverse(root->right, x, path);
+        if (!leftPart && !rightPart) {
+            path.pop_back();
+        }
+        return leftPart || rightPart;
+
+    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        std::vector<TreeNode*> pPath;
+        dfsTraverse(root, p, pPath);
+        std::vector<TreeNode*> qPath;
+        dfsTraverse(root, q, qPath);
+        int index = 0;
+        while (index < std::min(pPath.size(), qPath.size()) && pPath[index]->val == qPath[index]->val) {
+            ++index;
+        }
+        --index;
+        return pPath[index];
+    }
+};
+
+# Python O(N) O(N) Lowest Common Path Depth-First-Search
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def find_path(self, root: TreeNode, target: TreeNode, path: list[TreeNode]) -> bool:
+        if not root:
+            return False
+        path.append(root)
+        if root.val == target.val:
+            return True
+        left_part: bool = self.find_path(root.left, target, path)
+        right_part: bool = self.find_path(root.right, target, path)
+        if not left_part and not right_part:
+            path.pop()
+        return left_part or right_part
+
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        p_path: list[TreeNode] = []
+        self.find_path(root, p, p_path)
+        q_path: list[TreeNode] = []
+        self.find_path(root, q, q_path)
+        idx: int = 0
+        while idx < min(len(p_path), len(q_path)) and p_path[idx] == q_path[idx]:
+            idx += 1
+        idx -= 1
+        return p_path[idx]
+
+
+# C++ O(h) O(h) BST Least Common Path Depth-First-Search
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root->val > std::max(p->val, q->val)) {
+            return lowestCommonAncestor(root->left, p, q);
+        } else if (root->val < std::min(p->val, q->val)) {
+            return lowestCommonAncestor(root->right, p, q);
+        } else {
+            return root;
+        }
+    }
+};
+
+# Python O(h) O(h) BST Least Common Path Depth-First-Search
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root.val < min(p.val, q.val):
+            return self.lowestCommonAncestor(root.right, p, q)
+        elif root.val > max(p.val, q.val):
+            return self.lowestCommonAncestor(root.left, p, q)
+        return root
