43cd3a1baca3547acd17b53ad8069f626b954095
Peter Kalugin <streetworkout200412@gmail.com>
2024-10-23 08:29:18 +0300

    Added description "1926A. Vlad and the Best of Five"

---
+++ Codeforces/1926A._Vlad_and_the_Best_of_Five.py
@@ -0,0 +1,50 @@
+# A. Vlad and the Best of Five
+# time limit per test1 second
+# memory limit per test256 megabytes
+# Vladislav has a string of length 5
+# , whose characters are each either A
+#  or B
+# .
+#
+# Which letter appears most frequently: A
+#  or B
+# ?
+#
+# Input
+# The first line of the input contains an integer t
+#  (1≤t≤32
+# ) — the number of test cases.
+#
+# The only line of each test case contains a string of length 5
+#  consisting of letters A
+#  and B
+# .
+#
+# All t
+#  strings in a test are different (distinct).
+#
+# Output
+# For each test case, output one letter (A
+#  or B
+# ) denoting the character that appears most frequently in the string.
+#
+# Example
+# inputCopy
+# 8
+# ABABB
+# ABABA
+# BBBAB
+# AAAAA
+# BBBBB
+# BABAA
+# AAAAB
+# BAAAA
+# outputCopy
+# B
+# A
+# B
+# A
+# B
+# A
+# A
+# A
