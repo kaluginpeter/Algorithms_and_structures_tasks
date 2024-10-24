43c398be07566ac22328164f71bf766c319d5d4e
Peter Kalugin <streetworkout200412@gmail.com>
2024-10-24 07:36:07 +0300

    Added solution "Make the Deadfish Swim"

--- CodeWars/6kyu/Make_the_Deadfish_Swim.cpp
+++ CodeWars/6kyu/Make_the_Deadfish_Swim.cpp
@@ -12,3 +12,23 @@ Invalid characters should be ignored.
 parse("iiisdoso") => [ 8, 64 ]
 PARSINGALGORITHMS
 */
+// Solution
+#include <vector>
+#include <string>
+
+std::vector<int> parse(const std::string &data) {
+  std::vector<int> output;
+  int startValue = 0;
+  for (char letter : data) {
+    if (letter == 'i') {
+      ++startValue;
+    } else if (letter == 'd') {
+      --startValue;
+    } else if (letter == 's') {
+      startValue *= startValue;
+    } else if (letter == 'o') {
+      output.push_back(startValue);
+    }
+  }
+  return output;
+}
