72facd7b4f2aace9aba7bf787387509e569fa633
Peter Kalugin <streetworkout200412@gmail.com>
2024-10-29 18:07:34 +0300

    Added solution "2684. Maximum Number of Moves in a Grid"

--- LeetCode/Medium/2684._Maximum_Number_of_Moves_in_a_Grid.py
+++ LeetCode/Medium/2684._Maximum_Number_of_Moves_in_a_Grid.py
@@ -32,3 +32,65 @@
 # 2 <= m, n <= 1000
 # 4 <= m * n <= 105
 # 1 <= grid[i][j] <= 106
+# Solution
+# Python O(NM) O(NM) Dynamic Programming Depth-First-Search
+class Solution:
+    dirs: list[int] = [-1, 0, 1]
+
+    def dfs(self, row: int, col: int, grid: list[list[int]], dp: list[list[int]]) -> int:
+        M, N = len(grid), len(grid[0])
+        if dp[row][col] != -1:
+            return dp[row][col]
+        max_moves: int = 0
+        for move in self.dirs:
+            new_row, new_col = row + move, col + 1
+            if (
+                0 <= new_row < M
+                and 0 <= new_col < N
+                and grid[row][col] < grid[new_row][new_col]
+            ):
+                max_moves = max(
+                    max_moves, 1 + self.dfs(new_row, new_col, grid, dp)
+                )
+        dp[row][col] = max_moves
+        return max_moves
+
+    def maxMoves(self, grid: list[list[int]]) -> int:
+        M, N = len(grid), len(grid[0])
+        dp: list[list[int]] = [[-1] * N for _ in range(M)]
+        max_moves: int = 0
+        for row in range(M):
+            moves_required: int = self.dfs(row, 0, grid, dp)
+            max_moves = max(max_moves, moves_required)
+        return max_moves
+
+# C++ O(NM) O(NM) Dynamic Programming Depth-First-Search
+class Solution:
+    dirs: list[int] = [-1, 0, 1]
+
+    def dfs(self, row: int, col: int, grid: list[list[int]], dp: list[list[int]]) -> int:
+        M, N = len(grid), len(grid[0])
+        if dp[row][col] != -1:
+            return dp[row][col]
+        max_moves: int = 0
+        for move in self.dirs:
+            new_row, new_col = row + move, col + 1
+            if (
+                0 <= new_row < M
+                and 0 <= new_col < N
+                and grid[row][col] < grid[new_row][new_col]
+            ):
+                max_moves = max(
+                    max_moves, 1 + self.dfs(new_row, new_col, grid, dp)
+                )
+        dp[row][col] = max_moves
+        return max_moves
+
+    def maxMoves(self, grid: list[list[int]]) -> int:
+        M, N = len(grid), len(grid[0])
+        dp: list[list[int]] = [[-1] * N for _ in range(M)]
+        max_moves: int = 0
+        for row in range(M):
+            moves_required: int = self.dfs(row, 0, grid, dp)
+            max_moves = max(max_moves, moves_required)
+        return max_moves
