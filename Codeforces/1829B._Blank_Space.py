eb73a73f7cfd6e7d15242d1d971e5cf4c812d6b5
Peter Kalugin <streetworkout200412@gmail.com>
2024-10-30 17:44:12 +0300

    Added description "1829B. Blank Space"

---
+++ Codeforces/1829B._Blank_Space.py
@@ -0,0 +1,50 @@
+# B. Blank Space
+# time limit per test1 second
+# memory limit per test256 megabytes
+# You are given a binary array a
+#  of n
+#  elements, a binary array is an array consisting only of 0
+# s and 1
+# s.
+#
+# A blank space is a segment of consecutive elements consisting of only 0
+# s.
+#
+# Your task is to find the length of the longest blank space.
+#
+# Input
+# The first line contains a single integer t
+#  (1≤t≤1000
+# ) — the number of test cases.
+#
+# The first line of each test case contains a single integer n
+#  (1≤n≤100
+# ) — the length of the array.
+#
+# The second line of each test case contains n
+#  space-separated integers ai
+#  (0≤ai≤1
+# ) — the elements of the array.
+#
+# Output
+# For each test case, output a single integer — the length of the longest blank space.
+#
+# Example
+# InputCopy
+# 5
+# 5
+# 1 0 0 1 0
+# 4
+# 0 1 1 1
+# 1
+# 0
+# 3
+# 1 1 1
+# 9
+# 1 0 0 0 1 0 0 0 1
+# OutputCopy
+# 2
+# 1
+# 1
+# 0
+# 3
