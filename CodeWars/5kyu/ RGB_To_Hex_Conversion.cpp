7eaa61104b4085c1717d1c1eacaf9ad1bd83fe73
Peter Kalugin <streetworkout200412@gmail.com>
2024-11-01 18:28:07 +0300

    Added solution "RGB To Hex Conversion"

--- CodeWars/5kyu/ RGB_To_Hex_Conversion.cpp
+++ CodeWars/5kyu/ RGB_To_Hex_Conversion.cpp
@@ -10,3 +10,25 @@ Examples (input --> output):
 148, 0, 211   --> "9400D3"
 Algorithms
 */
+// Solution
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <string>
+
+std::string rgb_to_hex(int r, int g, int b)
+{
+    if (r > 255) r = 255;
+    if (r < 0) r = 0;
+    if (g > 255) g = 255;
+    if (g < 0) g = 0;
+    if (b > 255) b = 255;
+    if (b < 0) b = 0;
+    std::stringstream ss;
+    ss << std::uppercase << std::hex << std::setfill('0')
+       << std::setw(2) << r
+       << std::setw(2) << g
+       << std::setw(2) << b;
+
+    return ss.str();
+}
