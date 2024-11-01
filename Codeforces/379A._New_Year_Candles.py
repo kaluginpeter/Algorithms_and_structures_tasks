478dec70a6c6838023ef53d7526cbe303ad512a4
Peter Kalugin <streetworkout200412@gmail.com>
2024-11-01 18:29:33 +0300

    Added description "379A. New Year Candles"

---
+++ Codeforces/379A._New_Year_Candles.py
@@ -0,0 +1,26 @@
+# A. New Year Candles
+# time limit per test1 second
+# memory limit per test256 megabytes
+# Vasily the Programmer loves romance, so this year he decided to illuminate his room with candles.
+#
+# Vasily has a candles.When Vasily lights up a new candle, it first burns for an hour and then it goes out. Vasily is smart, so he can make b went out candles into a new candle. As a result, this new candle can be used like any other new candle.
+#
+# Now Vasily wonders: for how many hours can his candles light up the room if he acts optimally well? Help him find this number.
+#
+# Input
+# The single line contains two integers, a and b (1 ≤ a ≤ 1000; 2 ≤ b ≤ 1000).
+#
+# Output
+# Print a single integer — the number of hours Vasily can light up the room for.
+#
+# Examples
+# InputCopy
+# 4 2
+# OutputCopy
+# 7
+# InputCopy
+# 6 3
+# OutputCopy
+# 8
+# Note
+# Consider the first sample. For the first four hours Vasily lights up new candles, then he uses four burned out candles to make two new ones and lights them up. When these candles go out (stop burning), Vasily can make another candle. Overall, Vasily can light up the room for 7 hours.
