fc69cec7aa2c928aa406d2ab25d61e19fbae2310
Peter Kalugin <streetworkout200412@gmail.com>
2024-11-01 18:30:11 +0300

    Added solution "379A. New Year Candles"

--- Codeforces/379A._New_Year_Candles.py
+++ Codeforces/379A._New_Year_Candles.py
@@ -24,3 +24,40 @@
 # 8
 # Note
 # Consider the first sample. For the first four hours Vasily lights up new candles, then he uses four burned out candles to make two new ones and lights them up. When these candles go out (stop burning), Vasily can make another candle. Overall, Vasily can light up the room for 7 hours.
+# Solution
+# C++ O(logN) O(1) Math
+#include <iostream>
+
+int main() {
+    int a, b;
+    std::cin >> a >> b;
+    int total_hours = 0;
+    int burnt_out = 0;
+    while (a > 0) {
+        total_hours += a;
+        burnt_out += a;
+
+        a = burnt_out / b;
+        burnt_out = burnt_out % b;
+    }
+    std::cout << total_hours << std::endl;
+}
+
+# Python O(logN) O(1) Math
+import sys
+
+
+def solution(a: int, b: int) -> str:
+    total_hours: int = 0
+    burn_out: int = 0
+    while a > 0:
+        total_hours += a
+        burn_out += a
+        a = burn_out // b
+        burn_out %= b
+    return str(total_hours)
+
+
+if __name__ == '__main__':
+    a, b = map(int, sys.stdin.readline().rstrip().split())
+    sys.stdout.write(solution(a, b))
