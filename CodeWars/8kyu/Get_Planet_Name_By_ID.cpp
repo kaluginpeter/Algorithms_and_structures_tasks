ad96cfdbd916a65edc3dc6db521ef97158572872
Peter Kalugin <streetworkout200412@gmail.com>
2024-11-03 06:56:02 +0300

    Added solution "Get Planet Name By ID"

--- CodeWars/8kyu/Get_Planet_Name_By_ID.cpp
+++ CodeWars/8kyu/Get_Planet_Name_By_ID.cpp
@@ -6,3 +6,21 @@ Example (Input --> Output ):
 3 --> "Earth"
 Debugging
 */
+// Solution
+#include <string>
+
+std::string get_planet_name(int id)
+{
+    std::string name;
+    switch (id) {
+    case 1: name = "Mercury"; break;
+    case 2: name = "Venus"; break;
+    case 3: name = "Earth"; break;
+    case 4: name = "Mars"; break;
+    case 5: name = "Jupiter"; break;
+    case 6: name = "Saturn"; break;
+    case 7: name = "Uranus"; break;
+    case 8: name = "Neptune"; break;
+    }
+    return name;
+}
