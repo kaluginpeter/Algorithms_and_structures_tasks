ae9d6c296251a09a1782f5fa708b1ed3365a2dde
Peter Kalugin <streetworkout200412@gmail.com>
2024-11-03 06:41:51 +0300

    Added solution "796. Rotate String"

--- LeetCode/Easy/796._Rotate_String.py
+++ LeetCode/Easy/796._Rotate_String.py
@@ -15,6 +15,7 @@
 # 1 <= s.length, goal.length <= 100
 # s and goal consist of lowercase English letters.
 # Solution
+# Python O(N**2) O(N) String
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
         for i in range(len(s)):
@@ -22,3 +23,15 @@ class Solution:
                 return True
             s = s[1:]+s[0]
         return False
+
+# C++ O(N) O(N) String
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        return s.size() == goal.size() && (s + s).find(goal) != std::string::npos;
+    }
+};
+# Python O(N) O(N) String
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        return len(s) == len(goal) and goal in s + s
