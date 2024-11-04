b5c4ce051dac854be626255dbc15a393dc4698ad
Peter Kalugin <streetworkout200412@gmail.com>
2024-11-04 17:53:26 +0300

    Added solution "36. Valid Sudoku"

--- LeetCode/Medium/36._Valid_Sudoku.py
+++ LeetCode/Medium/36._Valid_Sudoku.py
@@ -44,3 +44,60 @@
 # board.length == 9
 # board[i].length == 9
 # board[i][j] is a digit 1-9 or '.'.
+# Solution
+# Python O(MN) O(MN) HashMap Matrix
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows: dict[int, set[str]] = defaultdict(set)
+        cols: dict[int, set[str]] = defaultdict(set)
+        squares: dict[tuple[int, int], set[str]] = defaultdict(set)
+        for r in range(len(board)):
+            for c in range(len(board[0])):
+                if board[r][c] == '.':
+                    continue
+                if (
+                    board[r][c] in rows[r]
+                    or board[r][c] in cols[c]
+                    or board[r][c] in squares[(r // 3, c // 3)]
+                ):
+                    return False
+                rows[r].add(board[r][c])
+                cols[c].add(board[r][c])
+                squares[(r // 3, c // 3)].add(board[r][c])
+        return True
+
+# C++ O(MN) O(MN) HashMap Matrix
+struct PairHash {
+    template <class T1, class T2>
+    std::size_t operator() (const std::pair<T1, T2>& pair) const {
+        auto hash1 = std::hash<T1>{}(pair.first);
+        auto hash2 = std::hash<T2>{}(pair.second);
+        return hash1 ^ (hash2 << 1);
+    }
+};
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        std::unordered_map<int, std::unordered_set<char>> cols;
+        std::unordered_map<int, std::unordered_set<char>> rows;
+        std::unordered_map<std::pair<int, int>, std::unordered_set<char>, PairHash> squares;
+        for (int r = 0; r < board.size(); ++r) {
+            for (int c = 0; c < board[0].size(); ++c) {
+                if (board[r][c] == '.') {
+                    continue;
+                }
+                if (
+                    cols[c].count(board[r][c])
+                    || rows[r].count(board[r][c])
+                    || squares[{r / 3, c / 3}].count(board[r][c])
+                ) {
+                    return false;
+                }
+                cols[c].insert(board[r][c]);
+                rows[r].insert(board[r][c]);
+                squares[{r / 3, c / 3}].insert(board[r][c]);
+            }
+        }
+        return true;
+    }
+};
