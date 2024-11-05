8cf5bf7c3bda4a4d5604b10b3978271c3df5e3a4
Peter Kalugin <streetworkout200412@gmail.com>
2024-11-05 18:09:32 +0300

    Added solution "Is this a triangle?"

--- CodeWars/7kyu/ Is_this_a_triangle.cpp
+++ CodeWars/7kyu/ Is_this_a_triangle.cpp
@@ -15,3 +15,17 @@ Input -> Output
 1,2,9 -> false
 MathematicsFundamentals
 */
+// Solution
+namespace Triangle
+{
+  bool isTriangle(int a, int b, int c)
+  {
+    if (a == 0 || b == 0 || c == 0) {
+      return false;
+    }
+    if (a + b > c && a + c > b && b + c > a) {
+      return true;
+    }
+    return false;
+  }
+};
