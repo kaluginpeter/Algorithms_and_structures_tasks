a43d513d0eff489e89479f3f744eb5216a0b1e2f
Peter Kalugin <streetworkout200412@gmail.com>
2024-11-07 14:58:42 +0300

    Added solution "Minimum difference in duplicate characters"

--- CodeWars/6kyu/Minimum_difference_in_duplicate_characters.py
+++ CodeWars/6kyu/Minimum_difference_in_duplicate_characters.py
@@ -25,3 +25,15 @@
 #
 # "abc" => null
 # Strings
+# Solution
+def min_repeating_character_difference(text):
+    hashmap: dict[str, int] = dict()
+    main_diff: int = float('inf')
+    main_char: str = ''
+    for idx in range(len(text)):
+        if text[idx] in hashmap:
+            if idx - hashmap[text[idx]] < main_diff:
+                main_diff = idx - hashmap[text[idx]]
+                main_char = text[idx]
+        hashmap[text[idx]] = idx
+    return (main_diff, main_char) if main_char else None
