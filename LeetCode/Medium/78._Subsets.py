21d0a411620c909927f2cf4c5fcab7f2f2e7e05d
Peter Kalugin <streetworkout200412@gmail.com>
2024-11-07 16:51:21 +0300

    Added solution "78. Subsets"

--- LeetCode/Medium/78._Subsets.py
+++ LeetCode/Medium/78._Subsets.py
@@ -21,3 +21,41 @@
 # 1 <= nums.length <= 10
 # -10 <= nums[i] <= 10
 # All the numbers of nums are unique.
+# Solution
+# Python O(N * 2**N) O(N * 2**N) Backtracking
+class Solution:
+    def helper(self, idx: int, nums: list[list[int]], init: list[int], output: list[list[int]]) -> None:
+        if idx == len(nums):
+            output.append(init.copy())
+            return
+        init.append(nums[idx])
+        self.helper(idx + 1, nums, init, output)
+        init.pop()
+        self.helper(idx + 1, nums, init, output)
+
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        output: list[list[int]] = []
+        init: list[int] = []
+        self.helper(0, nums, init, output)
+        return output
+
+# C++ O(N * 2**N) O(N * 2**N) Backtracking
+class Solution {
+public:
+    void helper(int index, std::vector<int>& nums, std::vector<int>& init, std::vector<std::vector<int>>& output) {
+        if (index == nums.size()) {
+            output.push_back(std::vector<int>(init));
+            return;
+        }
+        init.push_back(nums[index]);
+        helper(index + 1, nums, init, output);
+        init.pop_back();
+        helper(index + 1, nums, init, output);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        std::vector<std::vector<int>> output;
+        std::vector<int> init;
+        helper(0, nums, init, output);
+        return output;
+    }
+};
