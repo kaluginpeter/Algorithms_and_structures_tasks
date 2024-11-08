b21906578323079f787205bd8866b443ed3466cd
Peter Kalugin <streetworkout200412@gmail.com>
2024-11-08 17:46:18 +0300

    Added solution "39. Combination Sum"

--- LeetCode/Medium/39._Combination_Sum.py
+++ LeetCode/Medium/39._Combination_Sum.py
@@ -32,3 +32,53 @@
 # 2 <= candidates[i] <= 40
 # All elements of candidates are distinct.
 # 1 <= target <= 40
+# Solution
+# Python O(N * C(N, N)) O(N * C(N, N)) Backtracking
+class Solution:
+    def helper(self, idx: int, cur_sum: int, candidates: list[int], target: int, init: list[int], output: list[list[int]]) -> None:
+        if cur_sum == target:
+            output.append(init.copy())
+            return
+        if idx == len(candidates) or cur_sum > target:
+            return
+        for next_idx in range(idx, len(candidates)):
+            init.append(candidates[next_idx])
+            self.helper(next_idx, cur_sum + candidates[next_idx], candidates, target, init, output)
+            init.pop()
+
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        output: list[list[int]] = []
+        init: list[int] = []
+        self.helper(0, 0, candidates, target, init, output)
+        return output
+
+# C++ O(N * C(N, N)) O(N * C(N, N)) Backtracking
+class Solution {
+public:
+    void helper(
+        int index, int curSum,
+        std::vector<int>& candidates,
+        int target,
+        std::vector<int>& init,
+        std::vector<std::vector<int>>& output
+    ) {
+        if (curSum == target) {
+            output.push_back(init);
+            return;
+        }
+        if (curSum > target || index >= candidates.size()) {
+            return;
+        }
+        for (int nextIndex = index; nextIndex < candidates.size(); ++nextIndex) {
+            init.push_back(candidates[nextIndex]);
+            helper(nextIndex, curSum + candidates[nextIndex], candidates, target, init, output);
+            init.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        std::vector<std::vector<int>> output;
+        std::vector<int> init;
+        helper(0, 0, candidates, target, init, output);
+        return output;
+    }
+};
