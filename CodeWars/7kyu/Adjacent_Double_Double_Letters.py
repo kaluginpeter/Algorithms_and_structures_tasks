136b31e45417a4ae6c23d6a9f38ea5dba89b028e
Peter Kalugin <streetworkout200412@gmail.com>
2024-11-10 10:23:57 +0300

    Added solution "Adjacent Double Double Letters"

--- CodeWars/7kyu/Adjacent_Double_Double_Letters.py
+++ CodeWars/7kyu/Adjacent_Double_Double_Letters.py
@@ -15,3 +15,8 @@
 # Note: all the words will be lowercase, without any symbols or spaces
 #
 # FundamentalsStrings
+# Solution
+# Python O(N) O(1)
+def adjacent_double_double_letters(word):
+    if len(word) < 4: return False
+    return any(word[idx - 3] == word[idx - 2] and word[idx - 1] == word[idx] for idx in range(3, len(word)))
