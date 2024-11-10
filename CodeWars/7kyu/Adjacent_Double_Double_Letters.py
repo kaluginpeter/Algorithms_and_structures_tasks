0dfa29692d3ce5bfeecd8ed27ae826155015b18f
Peter Kalugin <streetworkout200412@gmail.com>
2024-11-10 10:23:41 +0300

    Added description "Adjacent Double Double Letters"

---
+++ CodeWars/7kyu/Adjacent_Double_Double_Letters.py
@@ -0,0 +1,17 @@
+# Given a string which is a word, return True if the word has adjacent double double letters, like in 'balloon'.
+#
+# For example,
+#
+# 'balloon' -> True, 'lloo' are adjacent double double letters
+#
+# 'baaloon' -> False, because even though there are two double letters, they are not adjacent
+#
+# 'baboonn' -> True, here 'oonn' are adjacent double double letters
+#
+# 'matte'   -> False, because there is only one pair of double letters
+#
+# 'aaaaaaah'  -> True, any substring 'aaaa' makes it a word with adjacent double double letters
+#
+# Note: all the words will be lowercase, without any symbols or spaces
+#
+# FundamentalsStrings
