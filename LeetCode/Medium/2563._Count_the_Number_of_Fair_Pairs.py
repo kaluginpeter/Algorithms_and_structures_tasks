01c3f1c72af2e87253dbe872a0becbcd9b092d7d
Peter Kalugin <streetworkout200412@gmail.com>
2024-11-13 17:16:59 +0300

    Added solution "2563. Count the Number of Fair Pairs"

--- LeetCode/Medium/2563._Count_the_Number_of_Fair_Pairs.py
+++ LeetCode/Medium/2563._Count_the_Number_of_Fair_Pairs.py
@@ -24,3 +24,49 @@
 # nums.length == n
 # -109 <= nums[i] <= 109
 # -109 <= lower <= upper <= 109
+# Solution
+# Python O(NlogN) O(1) BinarySearch
+class Solution:
+    def binary_search(self, nums: list[int], left: int, right: int, target: int) -> int:
+        while left <= right:
+            middle: int = left + (right - left) // 2
+            if nums[middle] >= target:
+                right = middle - 1
+            else:
+                left = middle + 1
+        return right + 1
+
+    def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
+        nums.sort()
+        output: int = 0
+        for idx in range(len(nums)):
+            left: int = self.binary_search(nums, idx + 1, len(nums) - 1, lower - nums[idx])
+            right: itn = self.binary_search(nums, idx + 1, len(nums) - 1, upper - nums[idx] + 1)
+            output += right - left
+        return output
+
+# C++ O(NlogN) O(1) BinarySearch
+class Solution {
+public:
+    int binarySearch(std::vector<int>& nums, int left, int right, int target) {
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
+            if (nums[middle] >= target) {
+                right = middle - 1;
+            } else {
+                left = middle + 1;
+            }
+        }
+        return right + 1;
+    }
+    long long countFairPairs(vector<int>& nums, int lower, int upper) {
+        std::sort(nums.begin(), nums.end());
+        long long output = 0;
+        for (int index = 0; index < nums.size(); ++index) {
+            int left = binarySearch(nums, index + 1, nums.size() - 1, lower - nums[index]);
+            int right = binarySearch(nums, index+ 1, nums.size() - 1, upper - nums[index] + 1);
+            output += right - left;
+        }
+        return output;
+    }
+};
