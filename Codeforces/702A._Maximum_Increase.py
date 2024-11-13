c7896d229149382ce28fe81cfdfaf92056b0476e
Peter Kalugin <streetworkout200412@gmail.com>
2024-11-13 17:57:48 +0300

    Added description "702A. Maximum Increase"

---
+++ Codeforces/702A._Maximum_Increase.py
@@ -0,0 +1,31 @@
+# A. Maximum Increase
+# time limit per test1 second
+# memory limit per test256 megabytes
+# You are given array consisting of n integers. Your task is to find the maximum length of an increasing subarray of the given array.
+#
+# A subarray is the sequence of consecutive elements of the array. Subarray is called increasing if each element of this subarray strictly greater than previous.
+#
+# Input
+# The first line contains single positive integer n (1 ≤ n ≤ 105) — the number of integers.
+#
+# The second line contains n positive integers a1, a2, ..., an (1 ≤ ai ≤ 109).
+#
+# Output
+# Print the maximum length of an increasing subarray of the given array.
+#
+# Examples
+# InputCopy
+# 5
+# 1 7 2 11 15
+# OutputCopy
+# 3
+# InputCopy
+# 6
+# 100 100 100 100 100 100
+# OutputCopy
+# 1
+# InputCopy
+# 3
+# 1 2 3
+# OutputCopy
+# 3
