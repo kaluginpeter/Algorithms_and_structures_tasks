ff8b60bad22287446b4d548d2c5da8bf03127770
Peter Kalugin <streetworkout200412@gmail.com>
2024-11-14 19:32:24 +0300

    Added description "1971A. My First Sorting Problem"

---
+++ Codeforces/1971A._My_First_Sorting_Problem.py
@@ -0,0 +1,54 @@
+# A. My First Sorting Problem
+# time limit per test1 second
+# memory limit per test256 megabytes
+# You are given two integers x
+#  and y
+# .
+#
+# Output two integers: the minimum of x
+#  and y
+# , followed by the maximum of x
+#  and y
+# .
+#
+# Input
+# The first line contains a single integer t
+#  (1≤t≤100
+# ) — the number of test cases.
+#
+# The only line of each test case contains two space-separated integers x
+#  and y
+#  (0≤x,y≤9
+# ).
+#
+# Output
+# For each test case, output two integers: the minimum of x
+#  and y
+# , followed by the maximum of x
+#  and y
+# .
+#
+# Example
+# InputCopy
+# 10
+# 1 9
+# 8 4
+# 1 4
+# 3 4
+# 2 0
+# 2 4
+# 6 9
+# 3 3
+# 0 0
+# 9 9
+# OutputCopy
+# 1 9
+# 4 8
+# 1 4
+# 3 4
+# 0 2
+# 2 4
+# 6 9
+# 3 3
+# 0 0
+# 9 9
