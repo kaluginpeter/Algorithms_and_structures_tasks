74e4a7757f97084cc12d858b50282f01383a4236
Peter Kalugin <streetworkout200412@gmail.com>
2024-11-15 17:00:12 +0300

    Added description "Balance the parentheses"

---
+++ CodeWars/7kyu/Balance_the_parentheses.py
@@ -0,0 +1,14 @@
+# Your job is to fix the parentheses so that all opening and closing parentheses (brackets) have matching counterparts. You will do this by appending parenthesis to the beginning or end of the string. The result should be of minimum length. Don't add unnecessary parenthesis.
+#
+# The input will be a string of varying length, only containing '(' and/or ')'.
+#
+# For example:
+#
+# Input: ")("
+# Output: "()()"
+#
+# Input: "))))(()("
+# Output: "(((())))(()())"
+# Enjoy!
+#
+# Refactoring
