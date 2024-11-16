1b08efb3106f2effcc1a301bf5d46692d2b13d55
Peter Kalugin <streetworkout200412@gmail.com>
2024-11-16 17:53:34 +0300

    Added solution "199. Binary Tree Right Side View"

--- LeetCode/Medium/199._Binary_Tree_Right_Side_View.py
+++ LeetCode/Medium/199._Binary_Tree_Right_Side_View.py
@@ -21,3 +21,70 @@
 #
 # The number of nodes in the tree is in the range [0, 100].
 # -100 <= Node.val <= 100
+# Solution
+# Python O(N) O(N) Breadth-First-Search
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        output: list[int] = []
+        cur_nodes: list[TreeNode] = []
+        if root:
+            cur_nodes.append(root)
+        next_nodes: list[TreeNode] = []
+        while cur_nodes:
+            rightmost_node: Optional[TreeNode] = None
+            for node in cur_nodes:
+                if node.left:
+                    next_nodes.append(node.left)
+                if node.right:
+                    next_nodes.append(node.right)
+                rightmost_node = node
+            output.append(rightmost_node.val)
+            cur_nodes = next_nodes
+            next_nodes = []
+        return output
+
+# C++ O(N) O(N) Breadth-First-Search
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        std::vector<int> output;
+        std::vector<TreeNode*> curNodes;
+        if (root) {
+            curNodes.push_back(root);
+        }
+        std::vector<TreeNode*> nextNodes;
+        while (curNodes.size()) {
+            TreeNode* rightmostNode = nullptr;
+            for (TreeNode* node : curNodes) {
+                if (node->left) {
+                    nextNodes.push_back(node->left);
+                }
+                if (node->right) {
+                    nextNodes.push_back(node->right);
+                }
+                rightmostNode = node;
+            }
+            output.push_back(rightmostNode->val);
+            curNodes = nextNodes;
+            nextNodes.clear();
+        }
+        return output;
+    }
+};
