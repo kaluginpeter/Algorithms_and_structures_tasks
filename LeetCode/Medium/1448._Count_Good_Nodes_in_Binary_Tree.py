20559180e62fe10926914f8a67373f6d5241eb33
Peter Kalugin <streetworkout200412@gmail.com>
2024-11-16 18:16:51 +0300

    Added solution "1448. Count Good Nodes in Binary Tree"

--- LeetCode/Medium/1448._Count_Good_Nodes_in_Binary_Tree.py
+++ LeetCode/Medium/1448._Count_Good_Nodes_in_Binary_Tree.py
@@ -33,3 +33,110 @@
 #
 # The number of nodes in the binary tree is in the range [1, 10^5].
 # Each node's value is between [-10^4, 10^4].
+# Solution
+# C++ O(N) O(N) Depth-First-Search
+class Solution {
+public:
+    int good_nodes;
+    void dfs(TreeNode* root, int ancestorValue) {
+        if (root->val >= ancestorValue) {
+            ++good_nodes;
+        }
+        if (root->left) {
+            dfs(root->left, std::max(root->val, ancestorValue));
+        }
+        if (root->right) {
+            dfs(root->right, std::max(root->val, ancestorValue));
+        }
+    };
+
+    int goodNodes(TreeNode* root) {
+        good_nodes = 0;
+        dfs(root, root->val);
+        return good_nodes;
+    }
+};
+# Python O(N) O(N) Depth-First-Search
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    good_nodes: int
+    def dfs(self, root: TreeNode, ancestor_value: int) -> None:
+        if root.val >= ancestor_value:
+            self.good_nodes += 1
+        if root.left:
+            self.dfs(root.left, max(root.val, ancestor_value))
+        if root.right:
+            self.dfs(root.right, max(root.val, ancestor_value))
+
+    def goodNodes(self, root: TreeNode) -> int:
+        self.good_nodes = 0
+        self.dfs(root, root.val)
+        return self.good_nodes
+
+# Python O(N) O(h) Breadth-First-Search
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        good_nodes: int = 0
+        cur_nodes: list[tuple[TreeNode, int]] = [(root, root.val)]
+        next_nodes: list[tuple[TreeNode, int]] = []
+        while cur_nodes:
+            for node, ancestor_value in cur_nodes:
+                if node.val >= ancestor_value:
+                    good_nodes += 1
+                if node.left:
+                    next_nodes.append((node.left, max(node.val, ancestor_value)))
+                if node.right:
+                    next_nodes.append((node.right, max(node.val, ancestor_value)))
+            cur_nodes = next_nodes
+            next_nodes = []
+        return good_nodes
+
+# C++ O(N) O(h) Breadth-First-Search
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int goodNodes(TreeNode* root) {
+        int good_nodes = 0;
+        std::vector<std::pair<TreeNode*, int>> curNodes = {{root, root->val}};
+        std::vector<std::pair<TreeNode*, int>> nextNodes;
+        while (curNodes.size()) {
+            for (std::pair<TreeNode*, int>& p : curNodes) {
+                TreeNode* node = p.first;
+                int ancestorValue = p.second;
+                if (ancestorValue <= node->val) {
+                    ++good_nodes;
+                }
+                if (node->left) {
+                    nextNodes.push_back({node->left, std::max(ancestorValue, node->val)});
+                }
+                if (node->right) {
+                    nextNodes.push_back({node->right, std::max(ancestorValue, node->val)});
+                }
+            }
+            curNodes = nextNodes;
+            nextNodes.clear();
+        }
+        return good_nodes;
+    }
+};
