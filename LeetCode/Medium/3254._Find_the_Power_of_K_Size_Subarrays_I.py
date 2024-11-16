21daba862cce7bc10805f3d9e38ae5cb8f8db76b
Peter Kalugin <streetworkout200412@gmail.com>
2024-11-16 10:16:49 +0300

    Added solution "3254. Find the Power of K-Size Subarrays I"

--- LeetCode/Medium/3254._Find_the_Power_of_K_Size_Subarrays_I.py
+++ LeetCode/Medium/3254._Find_the_Power_of_K_Size_Subarrays_I.py
@@ -64,3 +64,83 @@ class Solution:
                     left += 1
             right += 1
         return result
+
+# C++ O(N) O(N) Sliging Window
+class Solution {
+public:
+    vector<int> resultsArray(vector<int>& nums, int k) {
+        if (k == 1) {
+            return nums;
+        }
+        std::vector<int> output;
+        int left = 0;
+        int right = 1;
+        int n = nums.size();
+        while (left <= n - k && right < n) {
+            if (nums[right] - 1 == nums[right - 1]) {
+                if (right - left + 1 == k) {
+                    output.push_back(nums[right]);
+                    ++left;
+                }
+            } else {
+                while (left < right && left <= n - k) {
+                    output.push_back(-1);
+                    ++left;
+                }
+            }
+            ++right;
+        }
+        return output;
+    }
+};
+
+# Python O(N) O(N) Monotonic Stack
+from collections import deque
+class Solution:
+    def resultsArray(self, nums: List[int], k: int) -> List[int]:
+        output: list[int] = []
+        stack: list[int] = deque()
+        for idx in range(k - 1):
+            while stack and stack[-1] + 1 != nums[idx]:
+                stack.popleft()
+            stack.append(nums[idx])
+        n: int = len(nums)
+        for right in range(k - 1, n):
+            while stack and stack[-1] + 1 != nums[right]:
+                stack.popleft()
+            stack.append(nums[right])
+            if len(stack) == k:
+                output.append(stack[-1])
+                stack.popleft()
+            else:
+                output.append(-1)
+        return output
+
+# C++ O(N) O(N) Monotonic Stack
+class Solution {
+public:
+    vector<int> resultsArray(vector<int>& nums, int k) {
+        std::vector<int> output;
+        std::deque<int> stack;
+        for (int index = 0; index < k - 1; ++index) {
+            while (stack.size() && stack.back() + 1 != nums[index]) {
+                stack.pop_front();
+            }
+            stack.push_back(nums[index]);
+        }
+        int n = nums.size();
+        for (int right = k - 1; right < n; ++right) {
+            while (stack.size() && stack.back() + 1 != nums[right]) {
+                stack.pop_front();
+            }
+            stack.push_back(nums[right]);
+            if (stack.size() == k) {
+                output.push_back(nums[right]);
+                stack.pop_front();
+            } else {
+                output.push_back(-1);
+            }
+        }
+        return output;
+    }
+};
