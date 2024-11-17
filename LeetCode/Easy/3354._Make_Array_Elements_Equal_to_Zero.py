8bc77bb6e1d9adf750f0e353f1c3c6b9ab5cdb7b
Peter Kalugin <streetworkout200412@gmail.com>
2024-11-17 09:04:44 +0300

    Added solution "3354. Make Array Elements Equal to Zero"

--- LeetCode/Easy/3354._Make_Array_Elements_Equal_to_Zero.py
+++ LeetCode/Easy/3354._Make_Array_Elements_Equal_to_Zero.py
@@ -47,3 +47,52 @@
 # 1 <= nums.length <= 100
 # 0 <= nums[i] <= 100
 # There is at least one element i where nums[i] == 0.
+# Solution
+# Python O(N) O(N) Prefix Suffix
+class Solution:
+    def countValidSelections(self, nums: List[int]) -> int:
+        prefix: list[int] = []
+        for idx in range(len(nums)):
+            if not idx:
+                prefix.append(nums[idx])
+            else:
+                prefix.append(nums[idx] + prefix[-1])
+        positions: int = 0
+        for idx in range(len(nums)):
+            if not nums[idx]:
+                prefix_sum: int = prefix[idx]
+                suffix_sum: int = prefix[-1] - prefix[idx]
+                if prefix_sum == suffix_sum:
+                    positions += 2
+                elif abs(prefix_sum - suffix_sum) == 1:
+                    positions += 1
+        return positions
+
+# C++ O(N) O(N) Prefix Suffix
+class Solution {
+public:
+    int countValidSelections(vector<int>& nums) {
+        std::vector<int> prefix;
+        int n = nums.size();
+        for (int index = 0; index < n; ++index) {
+            if (!index) {
+                prefix.push_back(nums[index]);
+            } else {
+                prefix.push_back(nums[index] + prefix[prefix.size() - 1]);
+            }
+        }
+        int positions = 0;
+        for (int index = 0; index < n; ++index) {
+            if (!nums[index]) {
+                int prefixSum = prefix[index];
+                int suffixSum = prefix[n - 1] - prefix[index];
+                if (prefixSum == suffixSum) {
+                    positions += 2;
+                } else if (std::abs(prefixSum - suffixSum) == 1) {
+                    ++positions;
+                }
+            }
+        }
+        return positions;
+    }
+};
