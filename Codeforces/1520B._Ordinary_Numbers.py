e8caa0a72d11b4c75da555df207a21e5425eef62
Peter Kalugin <streetworkout200412@gmail.com>
2024-11-20 17:37:07 +0300

    Added description "1520B. Ordinary Numbers"

---
+++ Codeforces/1520B._Ordinary_Numbers.py
@@ -0,0 +1,47 @@
+# B. Ordinary Numbers
+# time limit per test2 seconds
+# memory limit per test256 megabytes
+# Let's call a positive integer n
+#  ordinary if in the decimal notation all its digits are the same. For example, 1
+# , 2
+#  and 99
+#  are ordinary numbers, but 719
+#  and 2021
+#  are not ordinary numbers.
+#
+# For a given number n
+# , find the number of ordinary numbers among the numbers from 1
+#  to n
+# .
+#
+# Input
+# The first line contains one integer t
+#  (1≤t≤104
+# ). Then t
+#  test cases follow.
+#
+# Each test case is characterized by one integer n
+#  (1≤n≤109
+# ).
+#
+# Output
+# For each test case output the number of ordinary numbers among numbers from 1
+#  to n
+# .
+#
+# Example
+# InputCopy
+# 6
+# 1
+# 2
+# 3
+# 4
+# 5
+# 100
+# OutputCopy
+# 1
+# 2
+# 3
+# 4
+# 5
+# 18
