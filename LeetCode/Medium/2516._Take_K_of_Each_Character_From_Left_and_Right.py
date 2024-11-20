4ced59cfe3612ef85f3b48ce85408bf2b56dfa24
Peter Kalugin <streetworkout200412@gmail.com>
2024-11-20 07:07:35 +0300

    Added solution "2516. Take K of Each Character From Left and Right"

--- LeetCode/Medium/2516._Take_K_of_Each_Character_From_Left_and_Right.py
+++ LeetCode/Medium/2516._Take_K_of_Each_Character_From_Left_and_Right.py
@@ -25,3 +25,46 @@
 # 1 <= s.length <= 105
 # s consists of only the letters 'a', 'b', and 'c'.
 # 0 <= k <= s.length
+# Solution
+# Python O(N) O(N) Sliding Window HashMap
+class Solution:
+    def takeCharacters(self, s: str, k: int) -> int:
+        hashmap: dict[str, int] = dict()
+        for letter in s:
+            hashmap[letter] = hashmap.get(letter, 0) + 1
+        n: int = len(s)
+        max_sub: int = -1
+        left: int = 0
+        for right in range(n):
+            hashmap[s[right]] -= 1
+            while left <= right and hashmap[s[right]] < k:
+                hashmap[s[left]] += 1
+                left += 1
+            if hashmap.get('a', 0) >= k and hashmap.get('b', 0) >= k and hashmap.get('c', 0) >= k:
+                max_sub = max(max_sub, right - left + 1)
+        return n - max_sub if max_sub != -1 else -1
+
+# C++ O(N) O(N) Sliding Window HashMap
+class Solution {
+public:
+    int takeCharacters(string s, int k) {
+        int n = s.size();
+        std::unordered_map<char, int> hashmap;
+        for (char letter : s) {
+            ++hashmap[letter];
+        }
+        int left = 0;
+        int maxSub = -1;
+        for (int right = 0; right < n; ++right) {
+            --hashmap[s[right]];
+            while (left <= right && hashmap[s[right]] < k) {
+                ++hashmap[s[left]];
+                ++left;
+            }
+            if (hashmap['a'] >= k && hashmap['b'] >= k && hashmap['c'] >= k) {
+                maxSub = std::max(maxSub, right - left + 1);
+            }
+        }
+        return (maxSub != -1? n - maxSub : -1);
+    }
+};
