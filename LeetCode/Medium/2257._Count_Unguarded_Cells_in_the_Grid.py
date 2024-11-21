5bbcef0e8c104ee270eb6f343b9b3be29161ea61
Peter Kalugin <streetworkout200412@gmail.com>
2024-11-21 07:18:49 +0300

    Added solution "2257. Count Unguarded Cells in the Grid"

--- LeetCode/Medium/2257._Count_Unguarded_Cells_in_the_Grid.py
+++ LeetCode/Medium/2257._Count_Unguarded_Cells_in_the_Grid.py
@@ -32,3 +32,95 @@
 # 0 <= rowi, rowj < m
 # 0 <= coli, colj < n
 # All the positions in guards and walls are unique.
+# Solution
+# Python O(MN + G(MN)) O(MN), where G is number of guards. Simulation Matrix
+class Solution:
+    def fill_matrix(self, row: int, col: int, matrix: list[list[str]], m: int, n: int) -> None:
+        up_row: int = row - 1
+        while up_row >= 0 and matrix[up_row][col] != 'W' and matrix[up_row][col] != 'X':
+            matrix[up_row][col] = 'G'
+            up_row -= 1
+        down_row: int = row + 1
+        while down_row < m and matrix[down_row][col] != 'W' and matrix[down_row][col] != 'X':
+            matrix[down_row][col] = 'G'
+            down_row += 1
+        right_col: int = col + 1
+        while right_col < n and matrix[row][right_col] != 'W' and matrix[row][right_col] != 'X':
+            matrix[row][right_col] = 'G'
+            right_col += 1
+        left_col: int = col - 1
+        while left_col >= 0 and matrix[row][left_col] != 'W' and matrix[row][left_col] != 'X':
+            matrix[row][left_col] = 'G'
+            left_col -= 1
+
+    def countUnguarded(self, m: int, n: int, guards: List[List[int]], walls: List[List[int]]) -> int:
+        matrix: list[list[str]] = [['F'] * n for row in range(m)]
+        for wall in walls:
+            row, col = wall
+            matrix[row][col] = 'W'
+        for guard in guards:
+            row, col = guard
+            matrix[row][col] = 'X'
+        for guard in guards:
+            row, col = guard
+            self.fill_matrix(row, col, matrix, m, n)
+        freedom: int = 0
+        for row in range(m):
+            for col in range(n):
+                if matrix[row][col] == 'F':
+                    freedom += 1
+        return freedom
+
+# C++ O(MN + G(MN)) O(MN), where G is number of guards. Matrix Simulation
+class Solution {
+public:
+    void fillMatrix(int row, int col, std::vector<std::vector<char>>& matrix, int m, int n) {
+        int upRow = row - 1;
+        while (upRow >= 0 && matrix[upRow][col] != 'W' && matrix[upRow][col] != 'X') {
+            matrix[upRow][col] = 'G';
+            --upRow;
+        }
+        int downRow = row + 1;
+        while (downRow < m && matrix[downRow][col] != 'W' && matrix[downRow][col] != 'X') {
+            matrix[downRow][col] = 'G';
+            ++downRow;
+        }
+        int rightCol = col + 1;
+        while (rightCol < n && matrix[row][rightCol] != 'W' && matrix[row][rightCol] != 'X') {
+            matrix[row][rightCol] = 'G';
+            ++rightCol;
+        }
+        int leftCol = col - 1;
+        while (leftCol >= 0 && matrix[row][leftCol] != 'W' && matrix[row][leftCol] != 'X') {
+            matrix[row][leftCol] = 'G';
+            --leftCol;
+        }
+    }
+    int countUnguarded(int m, int n, vector<vector<int>>& guards, vector<vector<int>>& walls) {
+        std::vector<std::vector<char>>matrix(m, std::vector<char>(n, 'F'));
+        for (std::vector<int>& wall : walls) {
+            int row = wall[0];
+            int col = wall[1];
+            matrix[row][col] = 'W';
+        }
+        for (std::vector<int>& guard : guards) {
+            int row = guard[0];
+            int col = guard[1];
+            matrix[row][col] = 'X';
+        }
+        for (std::vector<int>& guard : guards) {
+            int row = guard[0];
+            int col = guard[1];
+            fillMatrix(row, col, matrix, m, n);
+        }
+        int freedom = 0;
+        for (int row = 0; row < m; ++row) {
+            for (int col = 0; col < n; ++col) {
+                if (matrix[row][col] == 'F') {
+                    ++freedom;
+                }
+            }
+        }
+        return freedom;
+    }
+};
