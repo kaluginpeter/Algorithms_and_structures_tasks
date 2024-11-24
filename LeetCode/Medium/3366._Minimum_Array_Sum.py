ab2df082a3b07904c73619464951e78e66d58681
Peter Kalugin <streetworkout200412@gmail.com>
2024-11-24 13:59:53 +0300

    Added solution "3366. Minimum Array Sum"

--- LeetCode/Medium/3366._Minimum_Array_Sum.py
+++ LeetCode/Medium/3366._Minimum_Array_Sum.py
@@ -41,3 +41,77 @@
 # 0 <= nums[i] <= 105
 # 0 <= k <= 105
 # 0 <= op1, op2 <= nums.length
+# Solution
+# Python O(N * op1 * op2) O(N * op1 * op2) DynamicProgramming
+class Solution:
+    def minArraySum(self, nums: List[int], k: int, op1: int, op2: int) -> int:
+        n: int = len(nums)
+        dp: list[list[list[int]]] = [
+            [
+                [float('inf')] * (op2 + 1) for _ in range(op1 + 1)
+            ] for _ in range(n + 1)
+        ]
+        for x in range(op1 + 1):
+            for y in range(op2 + 1):
+                dp[0][x][y] = 0
+        for i in range(1, n + 1):
+            for x in range(op1 + 1):
+                for y in range(op2 + 1):
+                    dp[i][x][y]: int = dp[i-1][x][y] + nums[i-1]
+                    if x > 0: # Try to divide by 2
+                        halved = (nums[i-1] + 1) // 2
+                        dp[i][x][y] = min(dp[i][x][y], dp[i-1][x-1][y] + halved)
+                    if y > 0 and nums[i-1] >= k: # Try to subtract k
+                        reduced: int = nums[i-1] - k
+                        dp[i][x][y] = min(dp[i][x][y], dp[i-1][x][y-1] + reduced)
+                    if x > 0 and y > 0 and nums[i-1] >= k: # Try to subtract and divide by 2
+                        reduced_then_halved: int = ((nums[i-1] - k + 1) // 2)
+                        dp[i][x][y] = min(dp[i][x][y], dp[i-1][x-1][y-1] + max(reduced_then_halved, 0))
+                    if x > 0 and y > 0 and (nums[i-1] + 1) // 2 >= k: # Try to divide by 2 and subtract
+                        halved_then_reduced: int = ((nums[i-1] + 1) // 2) - k
+                        dp[i][x][y] = min(dp[i][x][y], dp[i-1][x-1][y-1] + max(halved_then_reduced, 0))
+        return dp[n][op1][op2]
+
+# C++ O(N * op1 * op2) O(N * op1 * op2) DynamicProgramming
+class Solution {
+public:
+    int minArraySum(vector<int>& nums, int k, int op1, int op2) {
+        int n = nums.size();
+        std::vector<std::vector<std::vector<int>>> dp(
+            n + 1, std::vector<std::vector<int>>(
+                op1 + 1, std::vector<int>(
+                    op2 + 1, 100001
+                )
+            )
+        );
+        for (int x = 0; x < op1 + 1; ++x) {
+            for (int y = 0; y < op2 + 1; ++y) {
+                dp[0][x][y] = 0;
+            }
+        }
+        for (int i = 1; i < n + 1; ++i) {
+            for (int x = 0; x < op1 + 1; ++x) {
+                for (int y = 0; y < op2 + 1; ++y) {
+                    dp[i][x][y] = dp[i - 1][x][y] + nums[i - 1];
+                    if (x > 0) { // Try to divide by 2
+                        int halved = (nums[i - 1] + 1) / 2;
+                        dp[i][x][y] = std::min(dp[i][x][y], dp[i - 1][x - 1][y] + halved);
+                    }
+                    if (y > 0 && nums[i - 1] >= k) { // Try to subract k
+                        int reduced = nums[i - 1] - k;
+                        dp[i][x][y] = std::min(dp[i][x][y], dp[i - 1][x][y - 1] + reduced);
+                    }
+                    if (x > 0 && y > 0 && nums[i - 1] >= k) { // Try to subtract and divide by 2
+                        int reducedThanHalved = std::max((nums[i - 1] - k + 1) / 2, 0);
+                        dp[i][x][y] = std::min(dp[i][x][y], dp[i - 1][x - 1][y - 1] + reducedThanHalved);
+                    }
+                    if (x > 0 && y > 0 && (nums[i - 1] + 1) / 2 >= k) { // Try to divide by 2 and subract
+                        int halvedThanReduced = std::max((nums[i - 1] + 1) / 2 - k, 0);
+                        dp[i][x][y] = std::min(dp[i][x][y], dp[i - 1][x - 1][y - 1] + halvedThanReduced);
+                    }
+                }
+            }
+        }
+        return dp[n][op1][op2];
+    }
+};
