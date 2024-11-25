7a67e54a9a6aeecd09ca2ad850d9a8be2bd9e5cf
Peter Kalugin <streetworkout200412@gmail.com>
2024-11-25 18:14:31 +0300

    Added solution "773. Sliding Puzzle"

--- LeetCode/Hard/773._Sliding_Puzzle.py
+++ LeetCode/Hard/773._Sliding_Puzzle.py
@@ -39,3 +39,96 @@
 # board[i].length == 3
 # 0 <= board[i][j] <= 5
 # Each value board[i][j] is unique.
+# Solution
+# Python O(MN * (MN)!) O((MN)!) Breadth-First-Search Matrix
+class Solution:
+    directions: list[list[int]] = [
+        [1, 3],
+        [0, 2, 4],
+        [1, 5],
+        [0, 4],
+        [3, 5, 1],
+        [4, 2],
+    ]
+
+    def _swap(self, s: str, i: int, j: int) -> str:
+        s: list[str] = list(s)
+        s[i], s[j] = s[j], s[i]
+        return ''.join(s)
+
+    def slidingPuzzle(self, board: List[List[int]]) -> int:
+
+        target: str = '123450'
+        start_state: str = ''.join(str(num) for row in board for num in row)
+        seen: set[str] = set()
+        queue: list[str] = deque([start_state])
+        seen.add(start_state)
+        steps: int = 0
+        while queue:
+            for _ in range(len(queue)):
+                current_state: str = queue.popleft()
+                if current_state == target:
+                    return steps
+                zero_index: int = current_state.index('0')
+                for next_index in self.directions[zero_index]:
+                    next_state: str = self._swap(current_state, zero_index, next_index)
+                    if next_state in seen:
+                        continue
+                    seen.add(next_state)
+                    queue.append(next_state)
+            steps += 1
+        return -1
+
+# C++ O(MN * (MN)!) O((MN)!) Breadth-First-Search Matrix
+class Solution {
+public:
+    void swap(std::string& state, int before, int after) {
+        char temp = state[before];
+        state[before] = state[after];
+        state[after] = temp;
+    }
+    int slidingPuzzle(vector<vector<int>>& board) {
+        std::vector<std::vector<int>> directions = {
+            {1, 3},
+            {0, 2, 4},
+            {1, 5},
+            {0, 4},
+            {3, 5, 1},
+            {4, 2},
+        };
+        std::string target = "123450";
+        std::string startState = "";
+        for (int row = 0; row < 2; ++row) {
+            for (int col = 0; col < 3; ++col) {
+                startState += std::to_string(board[row][col]);
+            }
+        }
+        std::unordered_set<std::string> seen;
+        seen.insert(startState);
+        std::deque<std::string> queue;
+        queue.push_back(startState);
+        int moves = 0;
+        while (queue.size()) {
+            int n = queue.size();
+            for (int rep = 0; rep < n; ++rep) {
+                std::string currentState = queue.front();
+                queue.pop_front();
+                if (currentState == target) {
+                    return moves;
+                }
+                int zeroIndex = static_cast<int>(currentState.find('0'));
+                for (int nextIndex : directions[zeroIndex]) {
+                    std::string nextState = currentState;
+                    swap(nextState, zeroIndex, nextIndex);
+                    if (seen.count(nextState)) {
+                        continue;
+                    }
+                    seen.insert(nextState);
+                    queue.push_back(nextState);
+                }
+            }
+            ++moves;
+        }
+        return -1;
+    }
+};
