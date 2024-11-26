c6b15bde957b9cc7f5f8a9a64ea9d7c2a979a47e
Peter Kalugin <streetworkout200412@gmail.com>
2024-11-26 17:33:38 +0300

    Added solution "Collision Detection"

--- CodeWars/7kyu/Collision_Detection.py
+++ CodeWars/7kyu/Collision_Detection.py
@@ -9,3 +9,9 @@
 # alt text
 #
 # Fundamentals
+# Solution
+import math
+def collision(x1, y1, radius1, x2, y2, radius2):
+    distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+    radius_sum = radius1 + radius2
+    return distance <= radius_sum
