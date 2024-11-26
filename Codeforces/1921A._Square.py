0a522b7494e8b76607df563c1217184ae871007a
Peter Kalugin <streetworkout200412@gmail.com>
2024-11-26 07:08:39 +0300

    Added description "1921A. Square"

---
+++ Codeforces/1921A._Square.py
@@ -0,0 +1,40 @@
+# A. Square
+# time limit per test1 second
+# memory limit per test256 megabytes
+# A square of positive (strictly greater than 0
+# ) area is located on the coordinate plane, with sides parallel to the coordinate axes. You are given the coordinates of its corners, in random order. Your task is to find the area of the square.
+#
+# Input
+# Each test consists of several testcases. The first line contains one integer t
+#  (1≤t≤100
+# ) — the number of testcases. The following is a description of the testcases.
+#
+# Each testcase contains four lines, each line contains two integers xi,yi
+#  (−1000≤xi,yi≤1000
+# ), coordinates of the corners of the square.
+#
+# It is guaranteed that there is a square with sides parallel to the coordinate axes, with positive (strictly greater than 0
+# ) area, with corners in given points.
+#
+# Output
+# For each test case, print a single integer, the area of the square.
+#
+# Example
+# InputCopy
+# 3
+# 1 2
+# 4 5
+# 1 5
+# 4 2
+# -1 1
+# 1 -1
+# 1 1
+# -1 -1
+# 45 11
+# 45 39
+# 17 11
+# 17 39
+# OutputCopy
+# 9
+# 4
+# 784
