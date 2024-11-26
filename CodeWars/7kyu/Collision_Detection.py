0d00c4840569482d0450a3e8782f94124b0683da
Peter Kalugin <streetworkout200412@gmail.com>
2024-11-26 17:33:22 +0300

    Added description "Collision Detection"

---
+++ CodeWars/7kyu/Collision_Detection.py
@@ -0,0 +1,11 @@
+# Create a function to determine whether or not two circles are colliding. You will be given the position of both circles in addition to their radii:
+#
+# def collision(x1, y1, radius1, x2, y2, radius2):
+#   # collision?
+# If a collision is detected, return true. If not, return false.
+#
+# Here's a geometric diagram of what the circles passed in represent:
+#
+# alt text
+#
+# Fundamentals
