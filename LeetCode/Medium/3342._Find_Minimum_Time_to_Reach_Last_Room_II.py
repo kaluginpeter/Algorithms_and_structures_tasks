06313064d73d432924a2e35c48592d42a95eaed7
Peter Kalugin <streetworkout200412@gmail.com>
2024-11-26 20:47:53 +0300

    Added solution "3342. Find Minimum Time to Reach Last Room II"

--- LeetCode/Medium/3342._Find_Minimum_Time_to_Reach_Last_Room_II.py
+++ LeetCode/Medium/3342._Find_Minimum_Time_to_Reach_Last_Room_II.py
@@ -47,3 +47,67 @@
 # 2 <= n == moveTime.length <= 750
 # 2 <= m == moveTime[i].length <= 750
 # 0 <= moveTime[i][j] <= 109
+# Solution
+# Python O(ElogE) O(ElogE) Dijkstra
+class Solution:
+    def minTimeToReach(self, moveTime: List[List[int]]) -> int:
+        m, n = len(moveTime), len(moveTime[0])
+        pq: list[tuple[int]] = [(0, 0, 0, 0)]
+        min_time: list[list[int]] = [[float('inf')] * n for _ in range(m)]
+        min_time[0][0] = 0
+        directions: list[tuple[int]] = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        while pq:
+            curr_time, r, c, is_extra = heapq.heappop(pq)
+            if r == m - 1 and c == n - 1:
+                return max(curr_time, moveTime[r][c])
+
+            if curr_time > min_time[r][c]:
+                continue
+
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < m and 0 <= nc < n:
+                    next_time = max(curr_time, moveTime[nr][nc]) + [1, 2][is_extra]
+                    if next_time < min_time[nr][nc]:
+                        min_time[nr][nc] = next_time
+                        heapq.heappush(pq, (next_time, nr, nc, ~is_extra))
+        return -1
+
+# C++ O(ElogE) O(ElogE) Dijkstra
+class Solution {
+public:
+    int minTimeToReach(vector<vector<int>>& moveTime) {
+        int m = moveTime.size();
+        int n = moveTime[0].size();
+        std::priority_queue<std::vector<int>, std::vector<std::vector<int>>, std::greater<std::vector<int>>> pq;
+        pq.push({0, 0, 0, 0});
+        std::vector<std::vector<int>> minTime(m, std::vector<int>(n, 1100000000));
+        std::vector<std::pair<int, int>> directions = {
+            {0, 1}, {0, -1}, {1, 0}, {-1, 0}
+        };
+        while (pq.size()) {
+            int currentTime = pq.top()[0];
+            int currentRow = pq.top()[1];
+            int currentCol = pq.top()[2];
+            bool isExtra = pq.top()[3];
+            pq.pop();
+            if (currentRow == m - 1 && currentCol == n - 1) {
+                return std::max(currentTime, moveTime[m - 1][n - 1]);
+            }
+            if (currentTime >= minTime[currentRow][currentCol]) {
+                continue;
+            }
+            minTime[currentRow][currentCol] = currentTime;
+            for (std::pair<int, int> direction : directions) {
+                int nextRow = currentRow + direction.first;
+                int nextCol = currentCol + direction.second;
+
+                if (0 <= std::min(nextRow, nextCol) && nextRow < m && nextCol < n) {
+                    int nextTime = std::max(currentTime, moveTime[nextRow][nextCol]) + (isExtra? 2 : 1);
+                    pq.push({nextTime, nextRow, nextCol, !isExtra});
+                }
+            }
+        }
+        return -1;
+    }
+};
