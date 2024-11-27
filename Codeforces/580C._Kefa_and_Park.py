7fc0970357e3d3b1feb766ab58bd29305319a5d0
Peter Kalugin <streetworkout200412@gmail.com>
2024-11-27 17:20:58 +0300

    Added solution "580C. Kefa and Park"

--- Codeforces/580C._Kefa_and_Park.py
+++ Codeforces/580C._Kefa_and_Park.py
@@ -47,3 +47,106 @@
 # Note to the first sample test:The vertices containing cats are marked red. The restaurants are at vertices 2, 3, 4. Kefa can't go only to the restaurant located at vertex 2.
 #
 # Note to the second sample test:The restaurants are located at vertices 4, 5, 6, 7. Kefa can't go to restaurants 6, 7.
+# Solution
+# C++ O(N) O(N) DFS
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+void dfs(int node, int consecutiveCats, int m, const vector<int>& cats, const unordered_map<int, vector<int>>& adjList, int& count, vector<bool>& visited) {
+    visited[node] = true;
+    if (cats[node] == 1) {
+        consecutiveCats++;
+    } else {
+        consecutiveCats = 0;
+    }
+    if (consecutiveCats > m) {
+        return;
+    }
+
+    bool isLeaf = true;
+    for (int neighbor : adjList.at(node)) {
+        if (!visited[neighbor]) {
+            isLeaf = false;
+            dfs(neighbor, consecutiveCats, m, cats, adjList, count, visited);
+        }
+    }
+    if (isLeaf) {
+        count++;
+    }
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> cats(n + 1);
+    for (int i = 1; i <= n; ++i) {
+        cin >> cats[i];
+    }
+
+    unordered_map<int, vector<int>> adjList;
+    for (int i = 0; i < n - 1; ++i) {
+        int x, y;
+        cin >> x >> y;
+        adjList[x].push_back(y);
+        adjList[y].push_back(x);
+    }
+
+    int count = 0;
+    vector<bool> visited(n + 1, false);
+
+    dfs(1, 0, m, cats, adjList, count, visited);
+
+    cout << count << "\n";
+    return 0;
+}
+
+# Python O(N) O(N) DFS
+import sys
+
+
+def solution(n: int, m: int) -> None:
+    cats = list(map(int, sys.stdin.readline().rstrip().split()))
+    cats = [0] + cats
+    adj_list = {i: [] for i in range(1, n + 1)}
+
+    for _ in range(n - 1):
+        x, y = map(int, sys.stdin.readline().rstrip().split())
+        adj_list[x].append(y)
+        adj_list[y].append(x)
+
+    visited = [False] * (n + 1)
+    count = 0
+    stack = [(1, 0)]
+    while stack:
+        root, cat = stack.pop()
+        if visited[root]:
+            continue
+        visited[root] = True
+
+        if cats[root]:
+            cat += 1
+        else:
+            cat = 0
+
+        if cat > m:
+            continue
+
+        is_leaf = True
+        for neighbor in adj_list[root]:
+            if not visited[neighbor]:
+                is_leaf = False
+                stack.append((neighbor, cat))
+
+        if is_leaf:
+            count += 1
+
+    sys.stdout.write(str(count) + '\n')
+
+
+if __name__ == '__main__':
+    n, m = map(int, sys.stdin.readline().rstrip().split())
+    solution(n, m)
