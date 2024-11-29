9d01dc74c2cce2c947746a9d2415a38bf693ea8c
Peter Kalugin <streetworkout200412@gmail.com>
2024-11-29 16:40:26 +0300

    Added solution "2577. Minimum Time to Visit a Cell In a Grid"

--- LeetCode/Hard/2577._Minimum_Time_to_Visit_a_Cell_In_a_Grid.py
+++ LeetCode/Hard/2577._Minimum_Time_to_Visit_a_Cell_In_a_Grid.py
@@ -40,3 +40,82 @@
 # 0 <= grid[i][j] <= 105
 # grid[0][0] == 0
 #
+# Solution
+# Python O(ElogE) O(ElogE) Matrix Shortest Path Dijkstra
+class Solution:
+    def minimumTime(self, grid: List[List[int]]) -> int:
+        m: int = len(grid)
+        n: int = len(grid[0])
+        min_heap: list[tuple[int, int, int]] = [(0, 0, 0)]
+        seen: set[tuple[int, int]] = set()
+        seen.add((0, 0, 0))
+        moves: list[tuple[int, int]] = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        have_move: bool = False
+        while min_heap:
+            cur_time, cur_row, cur_col = heapq.heappop(min_heap)
+            if cur_row == m - 1 and cur_col == n - 1:
+                return cur_time
+            for move in moves:
+                next_row, next_col = cur_row + move[0], cur_col + move[1]
+                if 0 <= min(next_row, next_col) and next_row < m and next_col < n:
+                    if cur_time + 1 < grid[next_row][next_col] and not have_move:
+                        continue
+                    if (next_row, next_col) in seen:
+                        continue
+                    next_time: int = 0
+                    if cur_time >= grid[next_row][next_col]:
+                        next_time = cur_time + 1
+                    else:
+                        diff: int = grid[next_row][next_col] - cur_time
+                        next_time = grid[next_row][next_col] + (0 if diff & 1 else 1)
+                    have_move = True
+                    seen.add((next_row, next_col))
+                    heapq.heappush(min_heap, (next_time, next_row, next_col))
+        return -1
+
+# C++ O(ElogE) O(ElogE) Matrix Shortest Path Dijkstra
+class Solution {
+public:
+    int minimumTime(vector<vector<int>>& grid) {
+        std::priority_queue<std::tuple<int, int, int>, std::vector<std::tuple<int, int, int>>, std::greater<std::tuple<int, int, int>>> minHeap;
+        minHeap.push({0, 0, 0});
+        int m = grid.size();
+        int n = grid[0].size();
+        std::vector<std::vector<int>> minTime(m, std::vector<int>(n, 100000000));
+        std::vector<std::pair<int, int>> moves = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        bool haveMove = false;
+        while (minHeap.size()) {
+            auto [curTime, curRow, curCol] = minHeap.top();
+            minHeap.pop();
+            if (curRow == m - 1 && curCol == n - 1) {
+                return curTime;
+            }
+            for (std::pair<int, int> move : moves) {
+                int nextRow = curRow + move.first;
+                int nextCol = curCol + move.second;
+                if (
+                    0 <= std::min(nextRow, nextCol)
+                    && nextRow < m && nextCol < n
+                ) {
+                    if (curTime + 1 < grid[nextRow][nextCol] && !haveMove) {
+                        continue;
+                    }
+                    int nextTime = 0;
+                    if (curTime >= grid[nextRow][nextCol]) {
+                        nextTime = curTime + 1;
+                    } else {
+                        int diff = grid[nextRow][nextCol] - curTime;
+                        nextTime = (diff % 2? grid[nextRow][nextCol] : grid[nextRow][nextCol] + 1);
+                    }
+                    if (nextTime < minTime[nextRow][nextCol]) {
+                        minTime[nextRow][nextCol] = nextTime;
+                        haveMove = true;
+                        minHeap.push({nextTime, nextRow, nextCol});
+                    }
+
+                }
+            }
+        }
+        return -1;
+    }
+};
