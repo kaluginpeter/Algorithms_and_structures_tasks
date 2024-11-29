e1ed09870d0a9a671f186bf923fcb4e6afba5eb7
Peter Kalugin <streetworkout200412@gmail.com>
2024-11-29 17:20:20 +0300

    Added description "1996A. Legs"

---
+++ Codeforces/1996A._Legs.py
@@ -0,0 +1,35 @@
+# A. Legs
+# time limit per test2 seconds
+# memory limit per test256 megabytes
+# It's another beautiful day on Farmer John's farm.
+#
+# After Farmer John arrived at his farm, he counted n
+#  legs. It is known only chickens and cows live on the farm, and a chicken has 2
+#  legs while a cow has 4
+# .
+#
+# What is the minimum number of animals Farmer John can have on his farm assuming he counted the legs of all animals?
+#
+# Input
+# The first line contains single integer t
+#  (1≤t≤103
+# ) — the number of test cases.
+#
+# Each test case contains an integer n
+#  (2≤n≤2⋅103
+# , n
+#  is even).
+#
+# Output
+# For each test case, output an integer, the minimum number of animals Farmer John can have on his farm.
+#
+# Example
+# InputCopy
+# 3
+# 2
+# 6
+# 8
+# OutputCopy
+# 1
+# 2
+# 2
