e748b4eb378e784c36a4c20569203bbb85d07f38
Peter Kalugin <streetworkout200412@gmail.com>
2024-11-30 12:48:22 +0300

    Added solution "2097. Valid Arrangement of Pairs"

--- LeetCode/Hard/2097._Valid_Arrangement_of_Pairs.py
+++ LeetCode/Hard/2097._Valid_Arrangement_of_Pairs.py
@@ -42,3 +42,71 @@
 # starti != endi
 # No two pairs are exactly the same.
 # There exists a valid arrangement of pairs.
+# Solution
+# Python O(V+ E) Eulerian Circuit
+from collections import defaultdict, deque
+class Solution:
+    def validArrangement(self, pairs):
+        adj_list: dict[int, list[int]] = defaultdict(deque)
+        in_degree, out_degree = defaultdict(int), defaultdict(int)
+        for pair in pairs:
+            start, end = pair
+            adj_list[start].append(end)
+            out_degree[start] += 1
+            in_degree[end] += 1
+        path: list[int] = []
+        def visit(node: int) -> None:
+            while adj_list[node]:
+                next_node: int = adj_list[node].popleft()
+                visit(next_node)
+            path.append(node)
+
+        start_node: int = pairs[0][0]
+        for node in out_degree:
+            if out_degree[node] - in_degree[node] == 1:
+                start_node = node
+                break
+        visit(start_node)
+        path.reverse()
+        output: list[list[int]] = [
+            [path[i - 1], path[i]] for i in range(1, len(path))
+        ]
+        return output
+
+# C++ O(V + E) O(V + E) Eulerian Circuit
+class Solution {
+public:
+    void visit(int& node, std::unordered_map<int, std::deque<int>>& adjList, std::vector<int>& path) {
+        while (!adjList[node].empty()) {
+            int nextNode = adjList[node].front();
+            adjList[node].pop_front();
+            visit(nextNode, adjList, path);
+        }
+        path.push_back(node);
+    }
+    vector<vector<int>> validArrangement(vector<vector<int>>& pairs) {
+        std::unordered_map<int, std::deque<int>> adjList;
+        std::unordered_map<int, int> inDegree, outDegree;
+        for (std::vector<int>& pair : pairs) {
+            int start = pair[0], end = pair[1];
+            adjList[start].push_back(end);
+            ++inDegree[end];
+            ++outDegree[start];
+        }
+        std::vector<int> path;
+        int startNode = pairs[0][0];
+        for (auto& node : outDegree) {
+            if (node.second - inDegree[node.first] == 1) {
+                startNode = node.first;
+                break;
+            }
+        }
+        visit(startNode, adjList, path);
+        std::vector<std::vector<int>> output;
+        int n = path.size();
+        for (int idx = n - 2; idx >= 0; --idx) {
+            output.push_back({path[idx + 1], path[idx]});
+        }
+        return output;
+    }
+};
