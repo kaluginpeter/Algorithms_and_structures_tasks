10597637820a87ccbb4f32372ee29c3802117a1d
Peter Kalugin <streetworkout200412@gmail.com>
2024-12-03 20:12:11 +0300

    Added description "Calculate the resultant force"

---
+++ CodeWars/7kyu/Calculate_the_resultant_force.py
@@ -0,0 +1,22 @@
+# Given two forces (F1 and F2 ) and the angle F2 makes with F1 find the resultant force R and the angle it makes with F1.
+#
+# input
+# Three values :
+#
+# F1
+# F2 making an angle θ with F1
+# angle θ
+# output
+# An array consisting of two values :
+#
+# R (the resultant force)
+# angle R makes with F1 (φ)
+# notes
+# Units for each of the following are given as under :
+#
+# F1 = Newton
+# F2 = Newton
+# angle θ = degree
+# R = Newton
+# φ = degree
+# PhysicsGeometryAlgorithms
