d69df5f8e754efe8f2bfa0fa41eee32036c59897
Peter Kalugin <streetworkout200412@gmail.com>
2024-12-04 18:00:21 +0300

    Added solution "1915B. Not Quite Latin Square"

--- Codeforces/1915B._Not_Quite_Latin_Square.py
+++ Codeforces/1915B._Not_Quite_Latin_Square.py
@@ -57,3 +57,55 @@
 # The correct Latin squares for the three test cases are shown below:
 #
 # ⎡⎣⎢ACBBACCBA⎤⎦⎥⎡⎣⎢BCACABABC⎤⎦⎥⎡⎣⎢CBAACBBAC⎤⎦⎥
+# Solution
+# C++ O(N) O(1) Matrix
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+void solution(int t) {
+    for (int i = 0; i < t; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            std::string row;
+            std::cin >> row;
+            if (row.find('?') != std::string::npos) {
+                std::vector<char> letters = {'A', 'B', 'C'};
+                for (char l : row) {
+                    auto it = std::find(letters.begin(), letters.end(), l);
+                    if (it != letters.end()) {
+                        letters.erase(it);
+                    }
+                }
+                std::cout << letters[0] << "\n";
+            }
+
+        }
+    }
+}
+
+int main() {
+    int t;
+    std::cin >> t;
+    solution(t);
+}
+
+# Python O(N) O(1) Matrix
+import sys
+
+
+def solution(t: int) -> None:
+    for _ in range(t):
+        for i in range(3):
+            row: str = sys.stdin.readline().rstrip()
+            if '?' in row:
+                letters: list = ['A', 'B', 'C']
+                for l in row:
+                    if l in letters:
+                        letters.remove(l)
+                sys.stdout.write(letters[0] + '\n')
+
+
+if __name__ == '__main__':
+    t: int = int(sys.stdin.readline().rstrip())
+    solution(t)
