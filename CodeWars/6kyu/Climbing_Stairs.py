7c6190f5be84951a4b54039668eddd8eca4fd6ab
Peter Kalugin <streetworkout200412@gmail.com>
2024-12-05 18:30:03 +0300

    Added description "Climbing Stairs"

---
+++ CodeWars/6kyu/Climbing_Stairs.py
@@ -0,0 +1,33 @@
+# Task
+# A staircase is given with a non-negative cost per each step. Once you pay the cost, you can either climb one or two steps. Create a solution to find the minimum sum of costs to reach the top. You can start at either of the first two steps.
+#
+# Examples
+# Stairs: [0, 2, 2, 1]
+# Step 0: Start on first step
+# Step 1: Pay 0 and climb two steps to reach the third step
+# Step 2: Pay 2 and climb two steps to reach the top
+# Total spent: 2
+#
+# Stairs: [0, 2, 3, 2]
+# Step 0: Start on first step
+# Step 1: Pay 0 and climb two steps to reach the third step
+# Step 2: Pay 3 and climb two steps to reach the top
+# Total spent: 3
+#
+# Stairs: [10, 15, 20]
+# Step 0: Start on the second step
+# Step 1: Pay 15 and climb two steps to reach the top
+# Total spent: 15
+#
+# Stairs [0, 0, 0, 0, 0, 0]
+# Take any path, because every step is free!
+#
+# Stairs [0, 1, 2, 0, 1, 2]
+# Step 0: Start on the second step
+# Step 1: Pay 1 and climb two steps to reach the fourth step
+# Step 2: Pay 0 and climb one step to reach the fifth step
+# Step 3: Pay 1 and climb two steps to reach the top
+# Total spent: 2
+# Notes
+# 2 <= number of steps <= 1000
+# LogicAlgorithmsDynamic Programming
