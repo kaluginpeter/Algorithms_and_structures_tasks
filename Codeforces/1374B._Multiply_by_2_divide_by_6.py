9cc0898dd7da1372664c183d52a8eee456405d9d
Peter Kalugin <streetworkout200412@gmail.com>
2024-12-07 12:23:38 +0300

    Added solution "1374B. Multiply by 2, divide by 6"

--- Codeforces/1374B._Multiply_by_2_divide_by_6.py
+++ Codeforces/1374B._Multiply_by_2_divide_by_6.py
@@ -90,3 +90,69 @@
 # divide by 6
 #  and get 1
 # .
+# Solution
+# C++ O(logN) O(1) Math
+#include <iostream>
+
+int minMovesToOne(int n) {
+    if (n == 1) {
+        return 0;
+    }
+    int count2 = 0;
+    int count3 = 0;
+    while (n % 2 == 0) {
+        n /= 2;
+        ++count2;
+    }
+    while (n % 3 == 0) {
+        n /= 3;
+        ++count3;
+    }
+    if (n != 1 || count2 > count3) {
+        return -1;
+    }
+    return count3 + (count3 - count2);
+}
+
+void solution(int t) {
+    for (int i = 0; i < t; ++i) {
+        int n;
+        std::cin >> n;
+        std::cout << minMovesToOne(n) << "\n";
+    }
+}
+
+int main() {
+    int t;
+    std::cin >> t;
+    solution(t);
+}
+
+# Python O(logN) O(1) Math
+import sys
+
+
+def min_moves_to_one(n: int) -> int:
+    if n == 1:
+        return 0
+    count2: int = 0
+    count3: int = 0
+    while n % 2 == 0:
+        n //= 2
+        count2 += 1
+    while n % 3 == 0:
+        n //= 3
+        count3 += 1
+    if n != 1 or count2 > count3:
+        return -1
+
+    return count3 + (count3 - count2)
+
+    return results
+
+
+if __name__ == '__main__':
+    t: int = int(sys.stdin.readline().rstrip())
+    for _ in range(t):
+        n: int = int(sys.stdin.readline().rstrip())
+        sys.stdout.write(str(min_moves_to_one(n)) + '\n')
