c20fa1f1a314e9141f5793141101285a2f0960e2
Peter Kalugin <streetworkout200412@gmail.com>
2024-12-08 15:41:13 +0300

    Added solution "3381. Maximum Subarray Sum With Length Divisible by K"

--- LeetCode/Medium/3381._Maximum_Subarray_Sum_With_Length_Divisible_by_K.py
+++ LeetCode/Medium/3381._Maximum_Subarray_Sum_With_Length_Divisible_by_K.py
@@ -42,3 +42,44 @@
 #
 # 1 <= k <= nums.length <= 2 * 105
 # -109 <= nums[i] <= 109
+# Solution
+# Python O(N) O(N) Prefix Sum
+class Solution:
+    def maxSubarraySum(self, nums: List[int], k: int) -> int:
+        n: int = len(nums)
+        prefix_sum: list[int] = list()
+        for idx in range(n):
+            if not idx: prefix_sum.append(nums[idx])
+            else: prefix_sum.append(nums[idx] + prefix_sum[-1])
+        min_prefix_sum: list[int] = [float('inf')] * k
+        min_prefix_sum[-1] = 0
+        max_sum: int = float('-inf')
+        for idx in range(n):
+            max_sum = max(max_sum, prefix_sum[idx] - min_prefix_sum[idx % k])
+            min_prefix_sum[idx % k] = min(min_prefix_sum[idx % k], prefix_sum[idx])
+        return max_sum
+
+# C++ O(N) O(N) PrefixSum
+class Solution {
+public:
+    long long maxSubarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+        std::vector<long long> prefixSum;
+        for (int idx = 0; idx < n; ++idx) {
+            if (!idx) {
+                prefixSum.push_back(nums[idx]);
+            } else {
+                prefixSum.push_back(nums[idx] + prefixSum[idx - 1]);
+            }
+        }
+        std::vector<long long> minPrefixSum(k, 1000000000000000);
+        minPrefixSum[k - 1] = 0;
+        long long maxScore = -1000000000000000;
+        for (int idx = 0; idx < n; ++idx) {
+            int remainder = idx % k;
+            maxScore = std::max(maxScore, prefixSum[idx] - minPrefixSum[remainder]);
+            minPrefixSum[remainder] = std::min(minPrefixSum[remainder], prefixSum[idx]);
+        }
+        return maxScore;
+    }
+};
