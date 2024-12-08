06d3c6c2be156228aa96cbe7c1b13f36f0919ef3
Peter Kalugin <streetworkout200412@gmail.com>
2024-12-08 10:14:37 +0300

    Added solution "Large Factorials"

--- CodeWars/4kyu/solution.cpp
+++ CodeWars/4kyu/solution.cpp
@@ -15,3 +15,34 @@ I have removed the use of require in the javascript language.
 
 AlgorithmsBig Integers
 */
+// Solution
+#include <string>
+void multiply(int n, std::string& digits) {
+  long long additional = 0;
+  for (size_t index = 0; index < digits.size(); ++index) {
+    long long result = (digits[index] - '0') * n + additional;
+    additional = result / 10;
+    digits[index] = '0' + (result % 10);
+  }
+  while (additional) {
+    digits += std::to_string(additional % 10);
+    additional /= 10;
+  }
+}
+
+std::string getFactorial(int n) {
+  std::string digits = "1";
+  for (int move = 1; move <= n; ++move) {
+    multiply(move, digits);
+  }
+  return digits;
+}
+
+std::string factorial(int n){
+  if (n < 0) {
+    return "";
+  }
+  std::string result = getFactorial(n);
+  std::reverse(result.begin(), result.end());
+  return result;
+}
