4b21f6d73ae2ab5073f046a8d5d47c555fef4254
Peter Kalugin <streetworkout200412@gmail.com>
2024-12-08 18:27:14 +0300

    Added solution "3380. Maximum Area Rectangle With Point Constraints I"

--- LeetCode/Medium/3380._Maximum_Area_Rectangle_With_Point_Constraints_I.py
+++ LeetCode/Medium/3380._Maximum_Area_Rectangle_With_Point_Constraints_I.py
@@ -53,3 +53,73 @@
 # points[i].length == 2
 # 0 <= xi, yi <= 100
 # All the given points are unique.
+# Solution
+# C++ O(N**5) O(1) Simulation
+class Solution {
+public:
+    bool check(int& ll, int& lr, int& ul, int& ur, std::vector<std::vector<int>>& points) {
+        for (int idx = 0; idx < points.size(); ++idx) {
+            if (idx == ll || idx == lr || idx == ul || idx == ur) continue;
+            int& x = points[idx][0];
+            int& y = points[idx][1];
+            if (x >= points[ll][0] && x <= points[lr][0] && y <= points[ul][1] && y >= points[ll][1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    int maxRectangleArea(vector<vector<int>>& points) {
+        int maxArea = -1;
+        int n = points.size();
+        int ll, lr, ul, ur;
+        for (ll = 0; ll < n; ++ll) {
+            for (lr = 0; lr < n; ++lr) {
+                if (lr == ll) continue;
+                for (ul = 0; ul < n; ++ul) {
+                    if (ul == ll || ul == lr) continue;
+                    for (ur = 0; ur < n; ++ur) {
+                        if (ur == ll || ur == lr || ur == ul) continue;
+                        if (
+                            points[ll][1] == points[lr][1] // same y-axis for lower points
+                            && points[ul][1] == points[ur][1] // same y-axis for upper points
+                            && points[ll][0] == points[ul][0] // same x-asis for lower points
+                            && points[lr][0] == points[ur][0] // same x-asis for right points
+                            && points[ll][0] <= points[lr][0] // left point comes before right point
+                            && check(ll, lr, ul, ur, points) // not points lays inside rectangle
+                            ) {
+                                std::cout << ll << " " << lr << " " << ul << " " << ur << "\n";
+                            maxArea = std::max(maxArea, (points[lr][0] - points[ll][0]) * (points[ul][1] - points[ll][1]));
+                        }
+                    }
+                }
+            }
+        }
+        return maxArea;
+    }
+};
+
+# Python O(N**5) O(1) Simulation
+class Solution:
+    def check(self, a: int, b: int, c: int, d: int, points: list[list[int]]) -> bool:
+        for f in points:
+            if f in (a, b, c, d):
+                continue
+            elif (a[0] <= f[0] <= b[0]) and (c[1] >= f[1] and f[1] >= a[1]):
+                return False
+        return True
+
+    def maxRectangleArea(self, points: List[List[int]]) -> int:
+        max_area: int = -1
+        n: int = len(points)
+        for lower_left in range(n):
+            for lower_right in range(n):
+                if lower_right == lower_left: continue
+                for upper_left in range(n):
+                    if upper_left in (lower_left, lower_right): continue
+                    for upper_right in range(n):
+                        if upper_right in (upper_left, lower_right, lower_left): continue
+                        a, b, c, d = points[lower_left], points[lower_right], points[upper_left], points[upper_right]
+                        if a[1] == b[1] and a[0] < b[0] and c[1] == d[1] and a[0] == c[0] and b[0] == d[0]:
+                            if self.check(a, b, c, d, points):
+                                max_area = max(max_area, (b[0] - a[0]) * (c[1] - a[1]))
+        return max_area
