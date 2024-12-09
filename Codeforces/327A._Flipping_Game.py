afae66a5e85be5be7fa151c21c09bda1a3d0f128
Peter Kalugin <streetworkout200412@gmail.com>
2024-12-09 17:02:45 +0300

    Added solution "327A. Flipping Game"

--- Codeforces/327A._Flipping_Game.py
+++ Codeforces/327A._Flipping_Game.py
@@ -28,3 +28,75 @@
 # In the first case, flip the segment from 2 to 5 (i = 2, j = 5). That flip changes the sequence, it becomes: [1 1 1 0 1]. So, it contains four ones. There is no way to make the whole sequence equal to [1 1 1 1 1].
 #
 # In the second case, flipping only the second and the third element (i = 2, j = 3) will turn all numbers into 1.
+# Solution
+# C++ O(N**3) O(1)
+#include <iostream>
+#include <vector>
+
+
+void solution(int& n, std::vector<int>& nums) {
+    int initialCountOfOnes = 0;
+    for (int value : nums) {
+        if (value == 1) {
+            initialCountOfOnes++;
+        }
+    }
+    int maxOnes = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = i; j < n; j++) {
+            int count0s = 0;
+            int count1s = 0;
+            for (int k = i; k <= j; k++) {
+                if (nums[k] == 0) {
+                    count0s++;
+                } else {
+                    count1s++;
+                }
+            }
+            int newCountOfOnes = initialCountOfOnes + count0s - count1s;
+            maxOnes = std::max(maxOnes, newCountOfOnes);
+        }
+    }
+    if (initialCountOfOnes == n) {
+        std::cout << n - 1 << "\n";
+        return;
+    }
+    std::cout << maxOnes << "\n";
+}
+
+
+int main() {
+    int n;
+    std::cin >> n;
+    std::vector<int> nums;
+    for (int i = 0; i < n; ++i) {\
+        int num;
+        std::cin >> num;
+        nums.push_back(num);
+    }
+    solution(n, nums);
+}
+
+# Python O(N**3) O(1)
+import sys
+
+
+def solution(n: int, nums: list) -> None:
+    initial_count_of_ones: int = sum(nums)
+    max_ones: int = 0
+    for i in range(n):
+        for j in range(i, n):
+            count_0s: int = sum(1 for k in range(i, j + 1) if nums[k] == 0)
+            count_1s: int = sum(1 for k in range(i, j + 1) if nums[k] == 1)
+            new_count_of_ones: int = initial_count_of_ones + count_0s - count_1s
+
+            max_ones = max(max_ones, new_count_of_ones)
+    if initial_count_of_ones == n:
+        sys.stdout.write(str(n - 1) + '\n')
+        return
+    sys.stdout.write(str(max_ones) + '\n')
+
+if __name__ == '__main__':
+    n: int = int(sys.stdin.readline().rstrip())
+    nums: list = list(map(int, sys.stdin.readline().rstrip().split()))
+    solution(n, nums)
