353d64fb983fc1e80bb1e2ed22e8ff63dfb94004
Peter Kalugin <streetworkout200412@gmail.com>
2024-12-10 17:25:50 +0300

    Added description "Simple Fun #299: Look And Say And Sum"

---
+++ CodeWars/5kyu/Simple_Fun_299_Look_And_Say_And_Sum.py
@@ -0,0 +1,28 @@
+# Task
+# Get the digits sum of nth number from the Look-and-Say sequence(1-based).
+#
+# 1, 11, 21, 1211, 111221, 312211, 13112221, 1113213211, ...
+#
+# Input/Output
+# [input] integer n
+#
+# nth number in the sequence to get, where 1 <= n <= 55 and n=1 is "1".
+#
+# [output] an integer
+#
+# The sum of digits in nth number from the Look-and-Say sequence.
+#
+# Example
+# For n = 2, the output should be 2.
+#
+# "11" --> 1 + 1 --> 2
+#
+# For n = 3, the output should be 3.
+#
+# "21" --> 2 + 1 --> 3
+#
+# For n = 4, the output should be 5.
+#
+# "1211" --> 1 + 2 + 1 + 1 --> 5
+#
+# Puzzles
