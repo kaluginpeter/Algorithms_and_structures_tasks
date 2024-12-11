f12e85181312412666be5f282769df3a5f660b89
Peter Kalugin <streetworkout200412@gmail.com>
2024-12-11 07:07:05 +0300

    Added solution "2779. Maximum Beauty of an Array After Applying Operation"

--- LeetCode/Medium/2779._Maximum_Beauty_of_an_Array_After_Applying_Operation.py
+++ LeetCode/Medium/2779._Maximum_Beauty_of_an_Array_After_Applying_Operation.py
@@ -35,3 +35,32 @@
 #
 # 1 <= nums.length <= 105
 # 0 <= nums[i], k <= 105
+# Solution
+# Python O(NlogN) O(1) Sorting Two Pointers Sliding Window
+class Solution:
+    def maximumBeauty(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        max_length: int = 0
+        left: int = 0
+        for right in range(len(nums)):
+            while nums[right] - nums[left] > 2 * k:
+                left += 1
+            max_length = max(max_length, right - left + 1)
+        return max_length
+
+# C++ O(NlogN) O(1) Sorting Two Pointers Sliding Window
+class Solution {
+public:
+    int maximumBeauty(vector<int>& nums, int k) {
+        std::sort(nums.begin(), nums.end());
+        int maxLength = 0;
+        int left = 0;
+        for (int right = 0; right < nums.size(); ++right) {
+            while (nums[right] - nums[left] > 2 * k) {
+                ++left;
+            }
+            maxLength = std::max(maxLength, right - left + 1);
+        }
+        return maxLength;
+    }
+};
