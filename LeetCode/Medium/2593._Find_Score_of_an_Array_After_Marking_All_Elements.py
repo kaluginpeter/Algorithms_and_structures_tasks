c244c83a8a346d6776c3291b1f0feb4e52bb577a
Peter Kalugin <streetworkout200412@gmail.com>
2024-12-13 17:15:56 +0300

    Added solution "2593. Find Score of an Array After Marking All Elements"

--- LeetCode/Medium/2593._Find_Score_of_an_Array_After_Marking_All_Elements.py
+++ LeetCode/Medium/2593._Find_Score_of_an_Array_After_Marking_All_Elements.py
@@ -34,3 +34,143 @@
 #
 # 1 <= nums.length <= 105
 # 1 <= nums[i] <= 106
+# Solution
+# Python O(NlogN) O(N) Bitmask
+class Solution:
+    def findScore(self, nums: List[int]) -> int:
+        num_index: list[tuple[int, int]] = []
+        n: int = len(nums)
+        for idx in range(n):
+            num_index.append((nums[idx], idx))
+        num_index.sort()
+        bitmask: list[bool] = [False] * n
+        score: int = 0
+        for main_idx in range(n):
+            num, idx = num_index[main_idx]
+            if bitmask[idx]: continue
+            bitmask[max(idx - 1, 0)] = True
+            bitmask[idx] = True
+            bitmask[min(idx + 1, n - 1)] = True
+            score += num
+        return score
+
+# C++ O(NlogN) O(N) Bitmask
+class Solution {
+public:
+    long long findScore(vector<int>& nums) {
+        int n = nums.size();
+        long long score = 0;
+        std::vector<std::pair<int, int>> numIndex;
+        for (int idx = 0; idx < n; ++idx) {
+            numIndex.push_back({nums[idx], idx});
+        }
+        std::sort(numIndex.begin(), numIndex.end());
+        std::vector<bool> bitmask(n, false);
+        for (std::pair<int, int>& p : numIndex) {
+            if (bitmask[p.second]) {
+                continue;
+            }
+            bitmask[std::max(p.second - 1, 0)] = true;
+            bitmask[p.second] = true;
+            bitmask[std::min(p.second + 1, n - 1)] = true;
+            score += p.first;
+        }
+        return score;
+    }
+};
+
+# Python O(NlogN) O(N) HashSet
+class Solution:
+    def findScore(self, nums: List[int]) -> int:
+        num_index: list[tuple[int, int]] = []
+        n: int = len(nums)
+        for idx in range(n):
+            num_index.append((nums[idx], idx))
+        num_index.sort()
+        hashset: set[int] = set()
+        score: int = 0
+        for main_idx in range(n):
+            num, idx = num_index[main_idx]
+            if idx in hashset: continue
+            hashset.add(idx - 1)
+            hashset.add(idx)
+            hashset.add(idx + 1)
+            score += num
+        return score
+
+# C++ O(N) O(N) HashSet
+class Solution {
+public:
+    long long findScore(vector<int>& nums) {
+        long long score = 0;
+        int maxNum = 0;
+        for (int& num : nums) {
+            if (num > maxNum) {
+                maxNum = num;
+            }
+        }
+        std::vector<std::vector<int>> bucket(maxNum + 1);
+        for (int idx = 0; idx < nums.size(); ++idx) {
+            bucket[nums[idx]].push_back(idx);
+        }
+        std::unordered_set<int> hashset;
+        int num = 0;
+        while (num < maxNum + 1) {
+            if (!bucket[num].empty()) {
+                for (int& numIdx : bucket[num]) {
+                    if (!hashset.count(numIdx)) {
+                        score += num;
+                        hashset.insert(numIdx - 1);
+                        hashset.insert(numIdx);
+                        hashset.insert(numIdx + 1);
+                    }
+                }
+            }
+            ++num;
+        }
+        return score;
+    }
+};
+
+# Python O(NlogN) O(N) Min Heap
+class Solution:
+    def findScore(self, nums: List[int]) -> int:
+        min_heap: list[tuple[int, int]] = []
+        for idx in range(len(nums)):
+            heapq.heappush(min_heap, (nums[idx], idx))
+        score: int = 0
+        hashset: set[int] = set()
+        while min_heap:
+            num, idx = heapq.heappop(min_heap)
+            if idx in hashset: continue
+            hashset.add(idx - 1)
+            hashset.add(idx)
+            hashset.add(idx + 1)
+            score += num
+        return score
+
+# C++ O(NlogN) O(N) Min Heap
+class Solution {
+public:
+    long long findScore(vector<int>& nums) {
+        long long score = 0;
+        std::unordered_set<int> hashset;
+        std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<std::pair<int, int>>> minHeap;
+        for (int idx = 0; idx < nums.size(); ++idx) {
+            minHeap.push({nums[idx], idx});
+        }
+        while (minHeap.size()) {
+            int num = minHeap.top().first;
+            int idx = minHeap.top().second;
+            minHeap.pop();
+            if (hashset.count(idx)) {
+                continue;
+            }
+            hashset.insert(idx);
+            hashset.insert(idx - 1);
+            hashset.insert(idx + 1);
+            score += num;
+        }
+        return score;
+    }
+};
