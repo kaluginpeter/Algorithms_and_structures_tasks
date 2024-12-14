f73a773b669682c5cd1167321d29d9fcb238a282
Peter Kalugin <streetworkout200412@gmail.com>
2024-12-14 11:05:40 +0300

    Added description "Find the index of the second occurrence of a letter in a string"

---
+++ CodeWars/7kyu/Find_the_index_of_the_second_occurrence_of_a_letter_in_a_string.cpp
@@ -0,0 +1,11 @@
+/*
+In this kata, you need to write a function that takes a string and a letter as input and then returns the index of the second occurrence of that letter in the string. If there is no such letter in the string, or if the letter occurs only once in the string, -1 should be returned.
+
+Examples:
+
+for inputs  "Hello world!!!", 'l'  ->  return 3
+for inputs  "Hello world!!!", 'A'  ->  return -1
+Good luck ;) And don't forget to rate this kata if you liked it.
+
+FundamentalsStrings
+*/
