4dfc1ba581d4d1b4424e79e433ac55fa111b02ac
Peter Kalugin <streetworkout200412@gmail.com>
2024-12-14 10:38:44 +0300

    Added solution "2762. Continuous Subarrays"

--- LeetCode/Medium/2762._Continuous_Subarrays.py
+++ LeetCode/Medium/2762._Continuous_Subarrays.py
@@ -35,3 +35,47 @@
 #
 # 1 <= nums.length <= 105
 # 1 <= nums[i] <= 109
+# Solution
+# Python O(NlogN) O(N) Priority Queue
+class Solution:
+    def continuousSubarrays(self, nums: List[int]) -> int:
+        output: int = 0
+        left: int = 0
+        min_heap: list[tuple[int, int]] = []
+        max_heap: list[tuple[int, int]] = []
+        for right in range(len(nums)):
+            heapq.heappush(min_heap, (nums[right], right))
+            heapq.heappush(max_heap, (-nums[right], right))
+            while -max_heap[0][0] - min_heap[0][0] > 2:
+                if min_heap[0][1] < max_heap[0][1]:
+                    left = heapq.heappop(min_heap)[1] + 1
+                else:
+                    left = heapq.heappop(max_heap)[1] + 1
+            output += right - left + 1
+        return output
+
+# C++ O(NlogN) O(N) Priority Queue
+class Solution {
+public:
+    long long continuousSubarrays(vector<int>& nums) {
+        long long output = 0;
+        std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<std::pair<int, int>>> minHeap;
+        std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>> maxHeap;
+        int left = 0;
+        for (int right = 0; right < nums.size(); ++right) {
+            minHeap.push({nums[right], right});
+            maxHeap.push({nums[right], right});
+            while (maxHeap.top().first - minHeap.top().first > 2) {
+                if (maxHeap.top().second < minHeap.top().second) {
+                    left = std::max(left, maxHeap.top().second + 1);
+                    maxHeap.pop();
+                } else {
+                    left = std::max(left, minHeap.top().second + 1);
+                    minHeap.pop();
+                }
+            }
+            output += right - left + 1;
+        }
+        return output;
+    }
+};
