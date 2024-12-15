0b18c6d99a00c850cf43ff7d15491ea63b60ead5
Peter Kalugin <streetworkout200412@gmail.com>
2024-12-15 20:28:27 +0300

    Added description "Lexographic sort with a twist"

---
+++ CodeWars/6kyu/Lexographic_sort_with_a_twist.py
@@ -0,0 +1,11 @@
+# My 6th kata, implement a lexicographic sort order to make longer items go before any of their prefixes.
+#
+# In Python, write a function (custom_sort) that takes an input list (lst) of strings. The function should return a new list of strings sorted lexiographically but with longer items before their prefixes. The test cases give some examples.
+#
+# You can assume that all strings will consist of ASCII printable characters and that the largest character will be '~', which is chr(126). For an extra challenge code a solution that makes no such assumptions.
+#
+# Do vote and provide any feedback on the kata.
+#
+# If you like this kata, do checkout my other katas.
+#
+# AlgorithmsArraysSortingData Structures
