7bed321bbdae21ea58616b62fe6f5e075828c84e
Peter Kalugin <streetworkout200412@gmail.com>
2024-12-15 12:50:30 +0300

    Added solution "1792. Maximum Average Pass Ratio"

--- LeetCode/Medium/1792._Maximum_Average_Pass_Ratio.py
+++ LeetCode/Medium/1792._Maximum_Average_Pass_Ratio.py
@@ -25,3 +25,58 @@
 # classes[i].length == 2
 # 1 <= passi <= totali <= 105
 # 1 <= extraStudents <= 105
+# Solution
+# Python O(NlogN + MlogN) O(N) Priority Queue
+class Solution:
+    def get_ratio(self, passed: int, total: int) -> float:
+        if passed == total: return 0
+        return (passed + 1) / (total + 1) - passed / total
+
+    def maxAverageRatio(self, classes: List[List[int]], extraStudents: int) -> float:
+        min_heap: list[list[float, int, int]] = []
+        for exam in classes:
+            passed, total = exam
+            heapq.heappush(min_heap, (-self.get_ratio(passed, total), passed, total))
+        while extraStudents:
+            ratio, passed, total = heapq.heappop(min_heap)
+            passed, total = passed + 1, total + 1
+            heapq.heappush(min_heap, (-self.get_ratio(passed, total), passed, total))
+            extraStudents -= 1
+        return sum(exam[1] / exam[2] for exam in min_heap) / len(classes)
+
+# C++ O(NlogN + MlogN) O(N) Priority Queue
+class Solution {
+public:
+    double getRatio(int& passed, int& total) {
+        if (passed == total) {
+            return 0;
+        }
+        double dP = static_cast<double>(passed);
+        double dT = static_cast<double>(total);
+        return (dP + 1) / (dT + 1) - dP / dT;
+    }
+
+    double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
+        std::priority_queue<std::tuple<double, int, int>, std::vector<std::tuple<double, int, int>>> maxHeap;
+        for (std::vector<int>& exam : classes) {
+            int& passed = exam[0];
+            int& total = exam[1];
+            maxHeap.push({getRatio(passed, total), passed, total});
+        }
+        while (extraStudents) {
+            auto [ratio, passed, total] = maxHeap.top();
+            maxHeap.pop();
+            ++passed;
+            ++total;
+            maxHeap.push({getRatio(passed, total), passed, total});
+            --extraStudents;
+        }
+        double averageRatio = 0;
+        while (maxHeap.size()) {
+            auto [ratio, passed, total] = maxHeap.top();
+            averageRatio += static_cast<double>(passed) / static_cast<double>(total);
+            maxHeap.pop();
+        }
+        return averageRatio / static_cast<double>(classes.size());
+    }
+};
