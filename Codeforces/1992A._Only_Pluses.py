be70222d3952a0d249bb4a890fd8ac07f005ef5b
Peter Kalugin <streetworkout200412@gmail.com>
2024-12-16 17:43:50 +0300

    Added solution "1992A. Only Pluses"

--- Codeforces/1992A._Only_Pluses.py
+++ Codeforces/1992A._Only_Pluses.py
@@ -49,3 +49,58 @@
 # OutputCopy
 # 100
 # 600
+# Solution
+# C++ Simulation O(1) O(1)
+#include <iostream>
+#include <algorithm>
+
+int maxProductAfterIncrements(int a, int b, int c) {
+    int max_product = a * b * c;
+    for (int i = 0; i <= 5; ++i) {
+        for (int j = 0; j <= 5 - i; ++j) {
+            int k = 5 - i - j;
+            int current_product = (a + i) * (b + j) * (c + k);
+            max_product = std::max(max_product, current_product);
+        }
+    }
+
+    return max_product;
+}
+
+void solution(int t) {
+    for (int i = 0; i < t; ++i) {
+        int a, b, c;
+        std::cin >> a >> b >> c;
+        int result = maxProductAfterIncrements(a, b, c);
+        std::cout << result << "\n";
+    }
+}
+
+
+int main() {
+    int t;
+    std::cin >> t;
+    solution(t);
+}
+
+# Python O(1) O(1) Simulation
+import sys
+
+def max_product(a: int, b: int, c: int) -> int:
+    max_product: int = a * b * c
+    for i in range(6):
+        j: int = 0
+        while j <= 5 - i:
+            k: int = 5 - i - j
+            current_product: int = (a + i) * (b + j) * (c + k)
+            max_product = max(max_product, current_product)
+            j += 1
+    return max_product
+
+def solution(t: int) -> None:
+    for _ in range(t):
+        a, b, c = map(int, sys.stdin.readline().rstrip().split())
+        sys.stdout.write(str(max_product(a, b, c)) + '\n')
+if __name__ == '__main__':
+    t: int = int(sys.stdin.readline().rstrip())
+    solution(t)
