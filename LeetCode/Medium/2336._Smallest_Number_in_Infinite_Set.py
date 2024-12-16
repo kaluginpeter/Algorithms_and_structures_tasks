258d626e36177cda2c01eba1faaa44edd601bcbe
Peter Kalugin <streetworkout200412@gmail.com>
2024-12-16 21:48:25 +0300

    Added solution "2336. Smallest Number in Infinite Set"

--- LeetCode/Medium/2336._Smallest_Number_in_Infinite_Set.py
+++ LeetCode/Medium/2336._Smallest_Number_in_Infinite_Set.py
@@ -32,3 +32,67 @@
 #
 # 1 <= num <= 1000
 # At most 1000 calls will be made in total to popSmallest and addBack.
+# Solution
+# Python O(logN) O(N) Priority Queue HashSet
+class SmallestInfiniteSet:
+
+    def __init__(self):
+        self.current_number: int = 1
+        self.min_heap: list[int] = []
+        self.added_back: set[int] = set()
+
+    def popSmallest(self) -> int:
+        output: int = 0
+        if not self.min_heap or self.min_heap[0] > self.current_number:
+            output = self.current_number
+            self.current_number += 1
+        else:
+            self.added_back.remove(self.min_heap[0])
+            output = heapq.heappop(self.min_heap)
+        return output
+
+    def addBack(self, num: int) -> None:
+        if num < self.current_number and num not in self.added_back:
+            heapq.heappush(self.min_heap, num)
+            self.added_back.add(num)
+
+
+# Your SmallestInfiniteSet object will be instantiated and called as such:
+# obj = SmallestInfiniteSet()
+# param_1 = obj.popSmallest()
+# obj.addBack(num)
+
+# C++ O(logN) O(N) Priority Queue HashSet
+class SmallestInfiniteSet {
+public:
+    int currentNumber = 1;
+    std::priority_queue<int, std::vector<int>, std::greater<int>> minHeap;
+    std::unordered_set<int> addedBack;
+    SmallestInfiniteSet() {};
+
+    int popSmallest() {
+        int output = 0;
+        if (minHeap.empty() || minHeap.top() > currentNumber) {
+            output = currentNumber++;
+        } else {
+            output = minHeap.top();
+            addedBack.erase(minHeap.top());
+            minHeap.pop();
+        }
+        return output;
+    }
+
+    void addBack(int num) {
+        if (num < currentNumber && addedBack.find(num) == addedBack.end()) {
+            minHeap.push(num);
+            addedBack.insert(num);
+        }
+    }
+};
+
+/**
+ * Your SmallestInfiniteSet object will be instantiated and called as such:
+ * SmallestInfiniteSet* obj = new SmallestInfiniteSet();
+ * int param_1 = obj->popSmallest();
+ * obj->addBack(num);
+ */
