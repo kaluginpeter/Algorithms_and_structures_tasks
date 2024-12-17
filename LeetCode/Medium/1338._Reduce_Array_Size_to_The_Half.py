3f3c82e24adf0026c3c2edd3a65145bf1236df3e
Peter Kalugin <streetworkout200412@gmail.com>
2024-12-17 20:42:05 +0300

    Added solution "1338. Reduce Array Size to The Half"

--- LeetCode/Medium/1338._Reduce_Array_Size_to_The_Half.py
+++ LeetCode/Medium/1338._Reduce_Array_Size_to_The_Half.py
@@ -23,3 +23,88 @@
 # 2 <= arr.length <= 105
 # arr.length is even.
 # 1 <= arr[i] <= 105
+# Solution
+# Python O(NlogN) O(N) Priority Queue
+class Solution:
+    def minSetSize(self, arr: List[int]) -> int:
+        hashmap: dict[int, int] = dict()
+        n: int = 0
+        for num in arr:
+            hashmap[num] = hashmap.get(num, 0) + 1
+            n += 1
+        min_heap: list[tuple[int, int]] = []
+        for num, freq in hashmap.items():
+            heapq.heappush(min_heap, (-freq, num))
+        removed: int = 0
+        size: int = 0
+        while removed < n / 2:
+            removed += -heapq.heappop(min_heap)[0]
+            size += 1
+        return size
+
+# C++ O(NlogN) O(N) Priority Queue
+class Solution {
+public:
+    int minSetSize(vector<int>& arr) {
+        std::unordered_map<int, int> hashmap;
+        int n = 0;
+        for (int& num : arr) {
+            ++hashmap[num];
+            ++n;
+        }
+        std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>> maxHeap;
+        for (auto& pair : hashmap) {
+            maxHeap.push({pair.second, pair.first});
+        }
+        int removed = 0;
+        int size = 0;
+        while (removed < n / 2) {
+            ++size;
+            removed += maxHeap.top().first;
+            maxHeap.pop();
+        }
+        return size;
+    }
+};
+
+# Python O(NlogN) O(N) Sorting Greedy
+class Solution:
+    def minSetSize(self, arr: List[int]) -> int:
+        hashmap: dict[int, int] = dict()
+        n: int = 0
+        for num in arr:
+            hashmap[num] = hashmap.get(num, 0) + 1
+            n += 1
+        num_freq: list[tuple[int, int]] = sorted(hashmap.items(), key=lambda x: x[1])
+        removed: int = 0
+        size: int = 0
+        while removed < n / 2:
+            size += 1
+            removed += num_freq.pop()[1]
+        return size
+
+# C++ O(NlogN) O(N) Sorting Greedy
+class Solution {
+public:
+    int minSetSize(vector<int>& arr) {
+        std::unordered_map<int, int> hashmap;
+        int n = 0;
+        for (int& num : arr) {
+            ++hashmap[num];
+            ++n;
+        }
+        std::vector<std::pair<int, int>> numFreq;
+        for (auto& pair : hashmap) {
+            numFreq.push_back({pair.second, pair.first});
+        }
+        std::sort(numFreq.begin(), numFreq.end());
+        int removed = 0;
+        int size = 0;
+        while (removed < n / 2) {
+            ++size;
+            removed += numFreq[numFreq.size() - 1].first;
+            numFreq.pop_back();
+        }
+        return size;
+    }
+};
