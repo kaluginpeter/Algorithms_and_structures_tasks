0a0b2b05a37bd641c395e19b3f4eb41a17b53ea2
Peter Kalugin <streetworkout200412@gmail.com>
2024-12-17 21:28:16 +0300

    Added solution "973. K Closest Points to Origin"

--- LeetCode/Medium/973._K_Closest_Points_to_Origin.py
+++ LeetCode/Medium/973._K_Closest_Points_to_Origin.py
@@ -27,3 +27,37 @@
 #
 # 1 <= k <= points.length <= 104
 # -104 <= xi, yi <= 104
+# Solution
+# Python O(NlogN) O(N) Priority Queue
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        min_heap: list[tuple[float, int, int]] = []
+        for point in points:
+            x, y = point
+            heapq.heappush(min_heap, ((x**2 + y**2)**.5, x, y))
+        output: list[list[int, int]] = []
+        for _ in range(k):
+            dist, x, y = heapq.heappop(min_heap)
+            output.append([x, y])
+        return output
+
+# C++ O(NlogN) O(N) Priority Queue
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        std::priority_queue<std::pair<double, int>, std::vector<std::pair<double, int>>, std::greater<std::pair<double, int>>> minHeap;
+        for (int idx = 0; idx < points.size(); ++idx) {
+            std::vector<int>& point = points[idx];
+            double x = static_cast<double>(point[0]);
+            double y = static_cast<double>(point[1]);
+            double dist = std::sqrt(std::pow(x, 2) + std::pow(y, 2));
+            minHeap.push({dist, idx});
+        }
+        std::vector<std::vector<int>> output;
+        for (int i = 0; i < k; ++i) {
+            output.push_back(points[minHeap.top().second]);
+            minHeap.pop();
+        }
+        return output;
+    }
+};
