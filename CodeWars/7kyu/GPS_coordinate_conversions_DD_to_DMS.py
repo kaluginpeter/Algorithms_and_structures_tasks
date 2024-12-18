76b1efd3a41af3a4273ef51043f5b672dfb4bc1d
Peter Kalugin <streetworkout200412@gmail.com>
2024-12-18 07:13:09 +0300

    Added solution "GPS coordinate conversions - DD to DMS"

--- CodeWars/7kyu/GPS_coordinate_conversions_DD_to_DMS.py
+++ CodeWars/7kyu/GPS_coordinate_conversions_DD_to_DMS.py
@@ -16,3 +16,20 @@
 # about WGS 84 on Wikipedia
 #
 # StringsFundamentals
+# Solution
+def convert_to_dms(dd_lat, dd_lon):
+    def dd_to_dms(dd, is_latitude):
+        if is_latitude:
+            direction = 'N' if dd >= 0 else 'S'
+        else:
+            direction = 'E' if dd >= 0 else 'W'
+        dd = abs(dd)
+        degrees = int(dd)
+        minutes = int((dd - degrees) * 60)
+        seconds = (dd - degrees - minutes / 60) * 3600
+        seconds_rounded = round(seconds, 3)
+        dms = f"{degrees:03d}*{minutes:02d}'{seconds_rounded:06.3f}\"{direction}"
+        return dms
+    dms_lat = dd_to_dms(dd_lat, True)
+    dms_lon = dd_to_dms(dd_lon, False)
+    return (dms_lat, dms_lon)
