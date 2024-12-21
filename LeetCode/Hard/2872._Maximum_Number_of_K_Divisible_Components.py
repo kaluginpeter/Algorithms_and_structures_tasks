b90d0ff3e7fb040abbd2aebb6b4ef9d2b53fcfe5
Peter Kalugin <streetworkout200412@gmail.com>
2024-12-21 13:19:31 +0300

    Added description "2872. Maximum Number of K-Divisible Components"

--- LeetCode/Hard/2872._Maximum_Number_of_K_Divisible_Components.py
+++ LeetCode/Hard/2872._Maximum_Number_of_K_Divisible_Components.py
@@ -40,3 +40,90 @@
 # 1 <= k <= 109
 # Sum of values is divisible by k.
 # The input is generated such that edges represents a valid tree.
+# Solution
+# Python O(E + N) O(E) Depth First Search
+class Solution:
+    components: int = 0
+    total_sum: int = 0
+
+    def build_adj_list(self, edges: list[list[int]]) -> dict[int, list[int]]:
+        adj_list: dict[int, list[int]] = dict()
+        for edge in edges:
+            x, y = edge
+            if x not in adj_list:
+                adj_list[x] = list()
+            if y not in adj_list:
+                adj_list[y] = list()
+            adj_list[x].append(y)
+            adj_list[y].append(x)
+        return adj_list
+
+    def dfs(self, source: int, adj_list: dict[int, list[int]], values: list[int], k: int, seen: set[int]) -> int:
+        cur_sub_tree_sum: int = values[source]
+        for vertex in adj_list.get(source, []):
+            if vertex in seen: continue
+            seen.add(vertex)
+            child_sum: int = self.dfs(vertex, adj_list, values, k, seen)
+            if child_sum % k == 0 and (self.total_sum - child_sum) % k == 0:
+                self.components += 1
+                self.total_sum -= child_sum
+            else:
+                cur_sub_tree_sum += child_sum
+        return cur_sub_tree_sum
+
+    def maxKDivisibleComponents(self, n: int, edges: List[List[int]], values: List[int], k: int) -> int:
+        self.components = 1
+        self.total_sum = sum(values)
+        seen: set[int] = set()
+        seen.add(0)
+        adj_list: dict[int, list[int]] = self.build_adj_list(edges)
+        self.dfs(0, adj_list, values, k, seen)
+        return self.components
+
+# C++ O(E + N) O(E) Depth First Search
+class Solution {
+public:
+    int components = 0;
+    long long totalSum = 0;
+    std::unordered_map<int, std::vector<int>> buildAdjList(std::vector<std::vector<int>>& edges) {
+        std::unordered_map<int, std::vector<int>> adjList;
+        for (std::vector<int>& edge : edges) {
+            int x = std::min(edge[0], edge[1]);
+            int y = std::max(edge[0], edge[1]);
+            adjList[x].push_back(y);
+            adjList[y].push_back(x);
+        }
+        return adjList;
+    }
+    long long dfs(int source, std::unordered_map<int, std::vector<int>>& adjList, int& k, std::vector<int>& values, std::unordered_set<int>& seen) {
+        long long curSubTreeSum = values[source];
+        for (int edge : adjList[source]) {
+            if (seen.count(edge)) {
+                continue;
+            } else {
+                seen.insert(edge);
+            }
+            long long childSum = dfs(edge, adjList, k, values, seen);
+            if (childSum % k == 0 && (totalSum - childSum) % k == 0) {
+                ++components;
+                totalSum -= childSum;
+            } else {
+                curSubTreeSum += childSum;
+            }
+        }
+        return curSubTreeSum;
+
+    }
+    int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) {
+        components = 1;
+        totalSum = 0;
+        for (int val : values) {
+            totalSum += val;
+        }
+        std::unordered_set<int> seen;
+        std::unordered_map<int, std::vector<int>> adjList = buildAdjList(edges);
+        seen.insert(0);
+        dfs(0, adjList, k, values, seen);
+        return components;
+    }
+};
