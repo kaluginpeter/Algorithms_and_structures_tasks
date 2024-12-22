aaf9bf6717655b0becd671709d223e81939c6b83
Peter Kalugin <streetworkout200412@gmail.com>
2024-12-22 20:02:49 +0300

    Added solution "3397. Maximum Number of Distinct Elements After Operations"

--- LeetCode/Medium/3397._Maximum_Number_of_Distinct_Elements_After_Operations.py
+++ LeetCode/Medium/3397._Maximum_Number_of_Distinct_Elements_After_Operations.py
@@ -34,3 +34,38 @@
 # 1 <= nums.length <= 105
 # 1 <= nums[i] <= 109
 # 0 <= k <= 109
+# Solution
+# Python O(NlogN) O(1) Sorting Greedy
+class Solution:
+    def maxDistinctElements(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        lower: int = nums[0] - k
+        count: int = 1
+        for num in nums[1:]:
+            if lower < num - k: # We need to update lower bound
+                lower = num - k
+                count += 1
+            elif num + k > lower: # Number can be placed at lower + 1
+                lower += 1
+                count += 1
+        return count
+
+# C++ O(NlogN) O(1) Sorting Greedy
+class Solution {
+public:
+    int maxDistinctElements(vector<int>& nums, int k) {
+        std::sort(nums.begin(), nums.end());
+        int lower = nums[0] - k;
+        int count = 1;
+        for (int idx = 1; idx < nums.size(); ++idx) { // We need to update lower bound
+            if (lower < nums[idx] - k) {
+                lower = nums[idx] - k;
+                ++count;
+            } else if (lower < nums[idx] + k) { // Number can be placed at lower + 1
+                ++lower;
+                ++count;
+            }
+        }
+        return count;
+    }
+};
