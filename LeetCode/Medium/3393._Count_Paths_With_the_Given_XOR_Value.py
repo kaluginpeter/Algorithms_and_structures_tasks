766f22d996556180e45a2c365ababb07500fe096
Peter Kalugin <streetworkout200412@gmail.com>
2024-12-22 19:14:44 +0300

    Added solution "3393. Count Paths With the Given XOR Value"

--- LeetCode/Medium/3393._Count_Paths_With_the_Given_XOR_Value.py
+++ LeetCode/Medium/3393._Count_Paths_With_the_Given_XOR_Value.py
@@ -52,3 +52,57 @@
 # 1 <= n == grid[r].length <= 300
 # 0 <= grid[r][c] < 16
 # 0 <= k < 16
+# Solution
+# Python O(N**3) O(N**3) Dynamic Programming
+class Solution:
+    def countPathsWithXorValue(self, grid: List[List[int]], k: int) -> int:
+        m: int = len(grid)
+        n: int = len(grid[0])
+        MOD: int = 10**9 + 7
+        dp: dict[int, dict[int, dict[int, int]]] = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
+        initial_xor: int = grid[0][0]
+        dp[0][0][initial_xor] = 1
+        for r in range(m):
+            for c in range(n):
+                for xor_value in list(dp[r][c].keys()):
+                    count: int = dp[r][c][xor_value]
+                    if c + 1 < n:
+                        new_xor: int = xor_value ^ grid[r][c + 1]
+                        dp[r][c + 1][new_xor] = (dp[r][c + 1][new_xor] + count) % MOD
+                    if r + 1 < m:
+                        new_xor: int = xor_value ^ grid[r + 1][c]
+                        dp[r + 1][c][new_xor] = (dp[r + 1][c][new_xor] + count) % MOD
+        return dp[m - 1][n - 1][k]
+
+# C++ O(N**3) O(N**3) Dynamic Programming
+class Solution {
+public:
+    int countPathsWithXorValue(vector<vector<int>>& grid, int k) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int MOD = 1000000007;
+        std::unordered_map<
+            int, std::unordered_map<
+                int, std::unordered_map<int, int>
+            >
+        > dp;
+        int initialXor = grid[0][0];
+        dp[0][0][initialXor] = 1;
+        for (int r = 0; r < m; ++r) {
+            for (int c = 0; c < n; ++c) {
+                for (auto& pair : dp[r][c]) {
+                    int count = dp[r][c][pair.first]; // xorValue
+                    if (c + 1 < n) {
+                        int newXor = pair.first ^ grid[r][c + 1];
+                        dp[r][c + 1][newXor] = (dp[r][c + 1][newXor] + count) % MOD;
+                    }
+                    if (r + 1 < m) {
+                        int newXor = pair.first ^ grid[r + 1][c];
+                        dp[r + 1][c][newXor] = (dp[r + 1][c][newXor] + count) % MOD;
+                    }
+                }
+            }
+        }
+        return dp[m - 1][n - 1][k];
+    }
+};
