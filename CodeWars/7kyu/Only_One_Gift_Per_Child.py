b9cecdbe9a55d5b913cce19924b9e29df575f926
Peter Kalugin <streetworkout200412@gmail.com>
2024-12-23 18:19:28 +0300

    Added description "Only One Gift Per Child"

---
+++ CodeWars/7kyu/Only_One_Gift_Per_Child.py
@@ -0,0 +1,14 @@
+# Santa is handing out gifts in the kindergarten. Many toddlers are around there and everyone should have the opportunity to have a seat on Santa's lap. But there's Peter, a 5 year old boy, who is a bit naughty. He tries to get two gifts. After he got the first one, he lines up again in the queue of children.
+#
+# But fortunately Santa is not alone. His elves keep a list with the names of the children, which already were on Santa's lap. We know, that each child name is unique. If a child tries to get a second gift, the elves will tell Santa.
+#
+# OK, let's help Santa and his elves with a simple function handOutGift() (hand_out_gift in Ruby/Python, HandOutGift in C# ) which takes the name of the next child. If this child already got a gift, it must throw an error in order to remind Santa.
+#
+# Example:
+#
+# hand_out_gift("Peter");
+# hand_out_gift("Alison");
+# hand_out_gift("John");
+# hand_out_gift("Maria");
+# hand_out_gift("Peter"); # <-- must throw an error
+# Algorithms
