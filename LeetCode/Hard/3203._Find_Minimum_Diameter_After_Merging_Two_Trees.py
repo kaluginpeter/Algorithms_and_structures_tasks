5513332be21a5c451b837e50a80bb332feaace15
Peter Kalugin <streetworkout200412@gmail.com>
2024-12-24 17:35:27 +0300

    Added solution "3203. Find Minimum Diameter After Merging Two Trees"

--- LeetCode/Hard/3203._Find_Minimum_Diameter_After_Merging_Two_Trees.py
+++ LeetCode/Hard/3203._Find_Minimum_Diameter_After_Merging_Two_Trees.py
@@ -42,3 +42,91 @@
 # edges2[i] = [ui, vi]
 # 0 <= ui, vi < m
 # The input is generated such that edges1 and edges2 represent valid trees.
+# Solution
+# Python O(N + M) O(N + M) DFS
+class Solution:
+    def minimumDiameterAfterMerge(
+        self, edges1: list[list[int]], edges2: list[list[int]]
+    ) -> int:
+        n: int = len(edges1) + 1
+        m: int = len(edges2) + 1
+        adj_list1: list[list[int]] = self.build_adj_list(n, edges1)
+        adj_list2: list[list[int]] = self.build_adj_list(m, edges2)
+        diameter1, _ = self.find_diameter(adj_list1, 0, -1)
+        diameter2, _ = self.find_diameter(adj_list2, 0, -1)
+        combined_diameter: int = ceil(diameter1 / 2) + ceil(diameter2 / 2) + 1
+        return max(diameter1, diameter2, combined_diameter)
+
+    def build_adj_list(
+        self, size: int, edges: list[list[int]]
+    ) -> list[list[int]]:
+        adj_list: list[list[int]] = [[] for _ in range(size)]
+        for edge in edges:
+            adj_list[edge[0]].append(edge[1])
+            adj_list[edge[1]].append(edge[0])
+        return adj_list
+
+    def find_diameter(
+        self, adj_list: list[list[int]], node: int, parent: int
+    ) -> tuple[int, int]:
+        max_depth1 = max_depth2 = 0
+        diameter: int = 0
+        for neighbor in adj_list[node]:
+            if neighbor == parent:
+                continue
+            child_diameter, depth = self.find_diameter(adj_list, neighbor, node)
+            depth += 1
+            diameter = max(diameter, child_diameter)
+            if depth > max_depth1:
+                max_depth2 = max_depth1
+                max_depth1 = depth
+            elif depth > max_depth2:
+                max_depth2 = depth
+        diameter = max(diameter, max_depth1 + max_depth2)
+        return diameter, max_depth1
+
+# C++ O(N + M) O(N + M)
+class Solution {
+public:
+    std::vector<std::vector<int>> buildAdjList(int& size, std::vector<std::vector<int>>& edges) {
+        std::vector<std::vector<int>> adjList(size, std::vector<int>());
+        for (std::vector<int>& edge : edges) {
+            adjList[edge[0]].push_back(edge[1]);
+            adjList[edge[1]].push_back(edge[0]);
+        }
+        return adjList;
+    }
+    std::vector<int> findDiameter(std::vector<std::vector<int>>& adjList, int& node, int& parent) {
+        int maxDepth1 = 0, maxDepth2 = 0;
+        int diameter = 0;
+        for (int& neighbor : adjList[node]) {
+            if (neighbor == parent) {
+                continue;
+            }
+            std::vector<int> res = findDiameter(adjList, neighbor, node);
+            int childDiameter = res[0];
+            int depth = res[1] + 1;
+            diameter = std::max(diameter, childDiameter);
+            if (depth > maxDepth1) {
+                maxDepth2 = maxDepth1;
+                maxDepth1 = depth;
+            } else if (depth > maxDepth2) {
+                maxDepth2 = depth;
+            }
+        }
+        diameter = std::max(diameter, maxDepth1 + maxDepth2);
+        return {diameter, maxDepth1};
+    }
+
+    int minimumDiameterAfterMerge(vector<vector<int>>& edges1, vector<vector<int>>& edges2) {
+        int n = edges1.size() + 1;
+        int m = edges2.size() + 1;
+        std::vector<std::vector<int>> adjList1 = buildAdjList(n, edges1);
+        std::vector<std::vector<int>> adjList2 = buildAdjList(m, edges2);
+        int node = 0, parent = -1;
+        int diameter1 = findDiameter(adjList1, node, parent)[0];
+        int diameter2 = findDiameter(adjList2, node, parent)[0];
+        int combinedDiameter = (diameter1 + 1) / 2 + (diameter2 + 1) / 2 + 1;
+        return std::max(diameter1, std::max(diameter2, combinedDiameter));
+    }
+};
