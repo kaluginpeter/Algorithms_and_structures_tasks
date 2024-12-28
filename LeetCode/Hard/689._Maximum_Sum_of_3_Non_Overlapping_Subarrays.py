c32b1b439f79b77ff159710ba59156dc353d8fe0
Peter Kalugin <streetworkout200412@gmail.com>
2024-12-28 13:38:50 +0300

    Added solution "689. Maximum Sum of 3 Non-Overlapping Subarrays"

--- LeetCode/Hard/689._Maximum_Sum_of_3_Non_Overlapping_Subarrays.py
+++ LeetCode/Hard/689._Maximum_Sum_of_3_Non_Overlapping_Subarrays.py
@@ -21,3 +21,92 @@
 # 1 <= nums.length <= 2 * 104
 # 1 <= nums[i] < 216
 # 1 <= k <= floor(nums.length / 3)
+# Solution
+# Python O(NM) O(NM) Dynamic Programming
+class Solution:
+    def maxSumOfThreeSubarrays(self, nums: List[int], k: int) -> List[int]:
+        k_sums: list[int] = [sum(nums[:k])]
+        for i in range(k, len(nums)):
+            k_sums.append(k_sums[-1] - nums[i - k] + nums[i])
+        dp: dict[tuple[int, int]] = dict()
+
+        def get_max_sum(i: int, count: int) -> int:
+            if i > len(nums) - k or count == 3:
+                return 0
+            if (i, count) in dp:
+                return dp[(i, count)]
+            include: int = k_sums[i] + get_max_sum(i + k, count + 1)
+            skip: int = get_max_sum(i + 1, count)
+            dp[(i, count)] = max(include, skip)
+            return dp[(i, count)]
+
+        def get_indices() -> list[int]:
+            i: int = 0
+            indices: list[int] = []
+            while i <= len(nums) - k and len(indices) < 3:
+                include: int = k_sums[i] + get_max_sum(i + k, len(indices) + 1)
+                skip: int = get_max_sum(i + 1, len(indices))
+                if include >= skip:
+                    indices.append(i)
+                    i += k
+                else:
+                    i += 1
+            return indices
+
+        return get_indices()
+
+# C++ O(NM) O(NM) Dynamic Programming
+struct TupleHash {
+    template <typename T1, typename T2>
+    std::size_t operator()(const std::tuple<T1, T2>& t) const {
+        auto hash1 = std::hash<T1>{}(std::get<0>(t));
+        auto hash2 = std::hash<T2>{}(std::get<1>(t));
+        return hash1 ^ (hash2 << 1);
+    }
+};
+class Solution {
+public:
+    int getMaxSum(int i, int count, std::vector<int>& nums, std::vector<int>& kSums, int& k, std::unordered_map<std::tuple<int, int>, int, TupleHash>& dp) {
+        if ((i > nums.size() - k) || (count == 3)) {
+            return 0;
+        }
+        if (dp.count({i, count})) {
+            return dp[{i, count}];
+        }
+        int include = kSums[i] + getMaxSum(i + k, count + 1, nums, kSums, k, dp);
+        int skip = getMaxSum(i + 1, count, nums, kSums, k, dp);
+        dp[{i, count}] = std::max(include, skip);
+        return dp[{i, count}];
+    }
+    std::vector<int> getIndices (std::vector<int>& nums, std::vector<int>& kSums, int& k, std::unordered_map<std::tuple<int, int>, int, TupleHash>& dp) {
+        int i = 0;
+        std::vector<int> indices;
+        while ((i <= nums.size() - k) && (indices.size() < 3)) {
+            int include = kSums[i] + getMaxSum(i + k, indices.size() + 1, nums, kSums, k, dp);
+            int skip = getMaxSum(i + 1, indices.size(), nums, kSums, k, dp);
+            if (include >= skip) {
+                indices.push_back(i);
+                i += k;
+            } else {
+                i += 1;
+            }
+        }
+        return indices;
+    }
+    vector<int> maxSumOfThreeSubarrays(vector<int>& nums, int k) {
+        std::vector<int> kSums;
+        int curSum = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (i < k) {
+                curSum += nums[i];
+                if (i == k - 1) {
+                    kSums.push_back(curSum);
+                }
+            } else {
+                kSums.push_back(kSums[kSums.size() - 1] - nums[i - k] + nums[i]);
+            }
+        }
+        std::unordered_map<std::tuple<int, int>, int, TupleHash> dp;
+        return getIndices(nums, kSums, k, dp);
+    }
+};
