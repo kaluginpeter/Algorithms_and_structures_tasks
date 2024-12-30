cebe743264cfd4e2805083413eb376b69dc6cdf2
Peter Kalugin <streetworkout200412@gmail.com>
2024-12-30 12:40:12 +0300

    Added description "Array Manipulation"

---
+++ CodeWars/7kyu/Array_Manipulation.py
@@ -0,0 +1,13 @@
+# Given an array of positive integers, replace every element with the least greater element to its right. If there is no greater element to its right, replace it with -1. For instance, given the array
+#
+# [8, 58, 71, 18, 31, 32, 63, 92, 43, 3, 91, 93, 25, 80, 28],
+#
+# the desired output is
+#
+# [18, 63, 80, 25, 32, 43, 80, 93, 80, 25, 93, -1, 28, -1, -1].
+#
+# Your task is to create a function that takes in an array as its argument, manipulates the array as described above, then return the resulting array.
+#
+# Note: Return a new array, rather than modifying the passed array.
+#
+# ArraysBinary Search TreesAlgorithms
