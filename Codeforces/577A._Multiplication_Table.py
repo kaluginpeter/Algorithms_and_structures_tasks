bbe9bbb598e22ff87b42d858db25fb2ac3f2909c
Peter Kalugin <streetworkout200412@gmail.com>
2024-12-31 11:41:51 +0300

    Added description "577A. Multiplication Table"

---
+++ Codeforces/577A._Multiplication_Table.py
@@ -0,0 +1,28 @@
+# A. Multiplication Table
+# time limit per test1 second
+# memory limit per test256 megabytes
+# Let's consider a table consisting of n rows and n columns. The cell located at the intersection of i-th row and j-th column contains number i × j. The rows and columns are numbered starting from 1.
+#
+# You are given a positive integer x. Your task is to count the number of cells in a table that contain number x.
+#
+# Input
+# The single line contains numbers n and x (1 ≤ n ≤ 105, 1 ≤ x ≤ 109) — the size of the table and the number that we are looking for in the table.
+#
+# Output
+# Print a single number: the number of times x occurs in the table.
+#
+# Examples
+# InputCopy
+# 10 5
+# OutputCopy
+# 2
+# InputCopy
+# 6 12
+# OutputCopy
+# 4
+# InputCopy
+# 5 13
+# OutputCopy
+# 0
+# Note
+# A table for the second sample test is given below. The occurrences of number 12 are marked bold.
