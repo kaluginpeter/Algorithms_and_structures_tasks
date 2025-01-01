7c80712653037229a9d6792791cc9a5c7483a9fc
Peter Kalugin <streetworkout200412@gmail.com>
2025-01-01 10:23:40 +0300

    Added solution "Maximum Depth of Nested Brackets"

--- CodeWars/6kyu/Maximum_Depth_of_Nested_Brackets.py
+++ CodeWars/6kyu/Maximum_Depth_of_Nested_Brackets.py
@@ -11,3 +11,27 @@
 # If the input is empty or doesn't contain brackets, an array containing only the original string must be returned.
 #
 # StringsPerformanceAlgorithms
+# Solution
+def strings_in_max_depth(s) -> list[str]:
+    output: list[tuple[int, str]] = []
+    stack: list[tuple[int, str]] = []
+    depth: int = 0
+    cur_string: str = ''
+    for i in range(len(s)):
+        if s[i] == '(':
+            stack.append((depth, cur_string))
+            depth += 1
+            cur_string = ''
+        elif s[i] == ')':
+            if not output or output[0][0] < depth:
+                output.clear()
+                output.append((depth, cur_string))
+            elif output and output[0][0] == depth:
+                output.append((depth, cur_string))
+            cur_string = stack.pop()[1]
+            depth -= 1
+        else:
+            cur_string += s[i]
+    if not output:
+        output.append((depth, cur_string))
+    return [substring for depth, substring in output]
