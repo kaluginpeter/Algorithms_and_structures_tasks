0e5fe3e5db1ded20cb5e11d9f996a47b2c59e7d9
kaluginpeter <streetworkout200412@gmail.com>
2025-01-02 12:33:47 +0300

    Added solution "519B. A and B and Compilation Errors"

--- Codeforces/519B._A_and_B_and_Compilation_Errors.py
+++ Codeforces/519B._A_and_B_and_Compilation_Errors.py
@@ -44,3 +44,67 @@
 # In the first test sample B first corrects the error number 8, then the error number 123.
 #
 # In the second test sample B first corrects the error number 1, then the error number 3. Note that if there are multiple errors with the same number, B can correct only one of them in one step.
+# Solution
+# Python O(NlogN) O(1) Sorting Greedy
+from __future__ import annotations
+import sys
+
+
+def solution(n: int, first: list[int], second: list[int], third: list[int]) -> None:
+    i: int = 0
+    while i < len(second) and first[i] == second[i]:
+        i += 1
+    sys.stdout.write(f'{first[i]}\n')
+    j: int = 0
+    while j < len(third) and second[j] == third[j]:
+        j += 1
+    sys.stdout.write(f'{second[j]}\n')
+
+
+if __name__ == '__main__':
+    n: int = int(sys.stdin.readline().rstrip())
+    first: list[int] = sorted(map(int, sys.stdin.readline().rstrip().split()))
+    second: list[int] = sorted(map(int, sys.stdin.readline().rstrip().split()))
+    third: list[int] = sorted(map(int, sys.stdin.readline().rstrip().split()))
+    solution(n, first, second, third)
+
+# C++ O(NlogN) O(1) Sorting Greedy
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+void solution() {
+    int n;
+    std::cin >> n;
+    std::vector<int> first(n);
+    for (int i = 0; i < n; ++i) {
+        std::cin >> first[i];
+    }
+    std::vector<int> second(n - 1);
+    for (int i = 0; i < n - 1; ++i) {
+        std::cin >> second[i];
+    }
+    std::vector<int> third(n - 2);
+    for (int i = 0; i < n - 2; ++i) {
+        std::cin >> third[i];
+    }
+    std::sort(first.begin(), first.end());
+    std::sort(second.begin(), second.end());
+    std::sort(third.begin(), third.end());
+    int i = 0;
+    while (i < n - 1 && first[i] == second[i]) {
+        ++i;
+    }
+    std::cout << first[i] << "\n";
+    int j = 0;
+    while (j < n - 2 && second[j] == third[j]) {
+        ++j;
+    }
+    std::cout << second[j] << "\n";
+}
+
+
+int main() {
+    solution();
+}
