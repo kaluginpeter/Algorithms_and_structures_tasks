104a639f37b6ef2e8213f20d6614751dd8c67953
kaluginpeter <streetworkout200412@gmail.com>
2025-01-05 11:22:37 +0300

    Added solution "454. 4Sum II"

--- LeetCode/Medium/454._4Sum_II.py
+++ LeetCode/Medium/454._4Sum_II.py
@@ -26,3 +26,39 @@
 # n == nums4.length
 # 1 <= n <= 200
 # -228 <= nums1[i], nums2[i], nums3[i], nums4[i] <= 228
+# Solution
+# Python O(N**2) O(N**2) HashMap
+class Solution:
+    def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
+        first_pairs: dict[int, int] = dict()
+        for i in range(len(nums1)):
+            for j in range(len(nums2)):
+                pair_sum: int = nums1[i] + nums2[j]
+                first_pairs[pair_sum] = first_pairs.get(pair_sum, 0) + 1
+        output: int = 0
+        for i in range(len(nums3)):
+            for j in range(len(nums4)):
+                pair_sum: int = nums3[i] + nums4[j]
+                output += first_pairs.get(-pair_sum, 0)
+        return output
+
+# C++ O(N**2) O(N**2) HashMap
+class Solution {
+public:
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+        std::unordered_map<long long, int> firstPairs;
+        for (int i = 0; i < nums1.size(); ++i) {
+            for (int j = 0; j < nums2.size(); ++j) {
+                ++firstPairs[{nums1[i] + nums2[j]}];
+            }
+        }
+        int output = 0;
+        for (int i = 0; i < nums3.size(); ++i) {
+            for (int j = 0; j < nums4.size(); ++j) {
+                long long pairSum = nums3[i] + nums4[j];
+                output += firstPairs[-pairSum];
+            }
+        }
+        return output;
+    }
+};
