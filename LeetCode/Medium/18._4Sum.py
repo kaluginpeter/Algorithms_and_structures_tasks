45c1b2f036e39febf1d01ed5cf255d22821f1639
kaluginpeter <streetworkout200412@gmail.com>
2025-01-05 10:44:19 +0300

    Added solution "18. 4Sum"

--- LeetCode/Medium/18._4Sum.py
+++ LeetCode/Medium/18._4Sum.py
@@ -22,3 +22,78 @@
 # 1 <= nums.length <= 200
 # -109 <= nums[i] <= 109
 # -109 <= target <= 109
+# Solution
+# Python O(N**3) O(N**4) Two Pointers Sorting
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        output: list[list[int]] = []
+        nums.sort()
+        n: int = len(nums)
+        for i in range(n):
+            if i and nums[i] == nums[i - 1]:
+                continue
+            for j in range(i + 1, n):
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+                first_pair: int = nums[i] + nums[j]
+                left: int = j + 1
+                right: int = n - 1
+                while left < right:
+                    second_pair: int = nums[left] + nums[right]
+                    cur_sum: int = first_pair + second_pair
+                    if cur_sum == target:
+                        output.append([nums[i], nums[j], nums[left], nums[right]])
+                        left += 1
+                        right -= 1
+                        while left < right and nums[left] == nums[left - 1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right + 1]:
+                            right -= 1
+                    elif cur_sum < target:
+                        left += 1
+                    else:
+                        right -= 1
+        return output
+
+# C++ O(N**3) O(N**4) Two Pointers Sorting
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        std::sort(nums.begin(), nums.end());
+        std::vector<std::vector<int>> output;
+        int n = nums.size();
+        for (int i = 0; i < n; ++i) {
+            if (i && nums[i - 1] == nums[i]) {
+                continue;
+            }
+            for (int j = i + 1; j < n; ++j) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                long long firstPair = nums[i] + nums[j];
+                int left = j + 1;
+                int right = n - 1;
+                while (left < right) {
+                    long long secondPair = nums[left] + nums[right];
+                    long long curSum = firstPair + secondPair;
+                    if (curSum == target) {
+                        output.push_back({nums[i], nums[j], nums[left], nums[right]});
+                        ++left;
+                        --right;
+                        while (left < right && nums[left] == nums[left - 1]) {
+                            ++left;
+                        }
+                        while (left < right && nums[right] == nums[right + 1]) {
+                            --right;
+                        }
+                    } else if (curSum < target) {
+                        ++left;
+                    } else {
+                        --right;
+                    }
+                }
+            }
+        }
+        return output;
+    }
+};
