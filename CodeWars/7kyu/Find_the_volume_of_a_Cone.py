d956b568f2e0d2694346038ed5abbe66ec1aa4a0
kaluginpeter <streetworkout200412@gmail.com>
2025-01-06 10:22:47 +0300

    Added description "Find the volume of a Cone."

---
+++ CodeWars/7kyu/Find_the_volume_of_a_Cone.py
@@ -0,0 +1,5 @@
+# Find the volume of a cone whose radius and height are provided as parameters to the function volume. Use the value of PI provided by your language (for example: Math.PI in JS, math.pi in Python or Math::PI in Ruby) and round down the volume to an Interger.
+#
+# If you complete this kata and there are no issues, please remember to give it a ready vote and a difficulty rating. :)
+#
+# GeometryFundamentals
