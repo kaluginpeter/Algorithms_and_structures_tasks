603476f70f240795357b31ba563be78e9cfb33af
kaluginpeter <streetworkout200412@gmail.com>
2025-01-07 11:36:42 +0300

    Added solution "Group Repeating Fractions"

--- CodeWars/6kyu/Group_Repeating_Fractions.py
+++ CodeWars/6kyu/Group_Repeating_Fractions.py
@@ -9,3 +9,25 @@
 # repeating_fractions(1,3) === '0.(3)'
 # repeating_fractions(2,888) === '0.(0)(2)5(2)5(2)5(2)5(2)5(2)'
 # StringsFundamentals
+def repeating_fractions(numerator: int, denominator: int) -> str:
+    pattern: str = str(numerator / denominator)
+    n: int = len(pattern)
+    output: list[str] = []
+    was_point: bool = False
+    left: int = 0
+    right: int = 0
+    while right < n:
+        while right < n and pattern[left] == pattern[right]:
+            right += 1
+        if right - left > 1:
+            if not was_point:
+                output.append(pattern[left:right])
+            else:
+                output.append(f'({pattern[left]})')
+            left = right
+        else:
+            output.append(str(pattern[left]))
+            if pattern[left] == '.':
+                was_point = True
+            left += 1
+    return ''.join(output)
