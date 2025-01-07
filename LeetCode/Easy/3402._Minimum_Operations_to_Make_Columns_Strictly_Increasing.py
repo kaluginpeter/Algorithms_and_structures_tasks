15346f1497a0b19fe98a35167eb39f7f28382302
kaluginpeter <streetworkout200412@gmail.com>
2025-01-07 19:23:06 +0300

    Added solution "3402. Minimum Operations to Make Columns Strictly Increasing"

--- LeetCode/Easy/3402._Minimum_Operations_to_Make_Columns_Strictly_Increasing.py
+++ LeetCode/Easy/3402._Minimum_Operations_to_Make_Columns_Strictly_Increasing.py
@@ -37,3 +37,37 @@
 # n == grid[i].length
 # 1 <= m, n <= 50
 # 0 <= grid[i][j] < 2500
+# Solution
+# Python O(MN) O(1) Matrix Greedy
+class Solution:
+    def minimumOperations(self, grid: List[List[int]]) -> int:
+        operations: int = 0
+        for col in range(len(grid[0])):
+            prev_value: int = grid[0][col]
+            for row in range(1, len(grid)):
+                if grid[row][col] <= prev_value:
+                    operations += prev_value - grid[row][col] + 1
+                    prev_value += 1
+                else:
+                    prev_value = grid[row][col]
+        return operations
+
+# C++ O(MN) O(1) Matrix Greedy
+class Solution {
+public:
+    int minimumOperations(vector<vector<int>>& grid) {
+        int operations = 0;
+        for (int col = 0; col < grid[0].size(); ++col) {
+            int prevValue = grid[0][col];
+            for (int row = 1; row < grid.size(); ++row) {
+                if (grid[row][col] <= prevValue) {
+                    operations += prevValue - grid[row][col] + 1;
+                    ++prevValue;
+                } else {
+                    prevValue = grid[row][col];
+                }
+            }
+        }
+        return operations;
+    }
+};
