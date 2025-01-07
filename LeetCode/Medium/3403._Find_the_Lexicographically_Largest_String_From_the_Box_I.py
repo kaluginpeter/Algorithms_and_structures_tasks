f145127428432ad8de0141c941f3d82def121f24
kaluginpeter <streetworkout200412@gmail.com>
2025-01-07 19:49:20 +0300

    Added solution "3403. Find the Lexicographically Largest String From the Box I"

--- LeetCode/Medium/3403._Find_the_Lexicographically_Largest_String_From_the_Box_I.py
+++ LeetCode/Medium/3403._Find_the_Lexicographically_Largest_String_From_the_Box_I.py
@@ -1,4 +1,3 @@
-
 # You are given a string word, and an integer numFriends.
 #
 # Alice is organizing a game for her numFriends friends. There are multiple rounds in the game, where in each round:
@@ -41,3 +40,32 @@
 # 1 <= word.length <= 5 * 103
 # word consists only of lowercase English letters.
 # 1 <= numFriends <= word.length
+# Solution
+# Python O(NM) O(M) Greedy
+class Solution:
+    def answerString(self, word: str, numFriends: int) -> str:
+        if numFriends == 1:
+            return word
+        output: str = ''
+        n: int = len(word)
+        max_length: int = n - numFriends + 1
+        for idx in range(n):
+            output = max(output, word[idx:idx + max_length])
+        return output
+
+# C++ O(NM) O(M) Greedy
+class Solution {
+public:
+    string answerString(string word, int numFriends) {
+        if (numFriends == 1) {
+            return word;
+        }
+        std::string output = "";
+        int n = word.size();
+        int maxLength = n - numFriends + 1;
+        for (int idx = 0; idx < n; ++idx) {
+            output = std::max(output, word.substr(idx, maxLength));
+        }
+        return output;
+    }
+};
