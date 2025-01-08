c024ab068aeb3710d593e0348c44cb9c9e3254d6
kaluginpeter <streetworkout200412@gmail.com>
2025-01-08 17:18:37 +0300

    Added solution "3412. Find Mirror Score of a String"

--- LeetCode/Medium/3412._Find_Mirror_Score_of_a_String.py
+++ LeetCode/Medium/3412._Find_Mirror_Score_of_a_String.py
@@ -42,3 +42,39 @@
 #
 # 1 <= s.length <= 105
 # s consists only of lowercase English letters.
+# Solution
+# Python O(N) O(N) Stack HashMap
+class Solution:
+    def calculateScore(self, s: str) -> int:
+        total_score: int = 0
+        pool: dict[str, list[int]] = dict()
+        for idx in range(len(s)):
+            cur_char: str = s[idx]
+            rev_char: str = chr(219 - ord(cur_char))
+            if pool.get(rev_char, []):
+                total_score += idx - pool.get(rev_char).pop()
+            else:
+                if cur_char not in pool:
+                    pool[cur_char] = []
+                pool[cur_char].append(idx)
+        return total_score
+
+# C++ O(N) O(N) HashMap Stack
+class Solution {
+public:
+    long long calculateScore(string s) {
+        long long totalScore = 0;
+        std::unordered_map<char, std::vector<int>> pool;
+        for (int idx = 0; idx < s.size(); ++idx) {
+            char& curChar = s[idx];
+            char revChar = static_cast<char>( (25 - (curChar - 'a')) + 97 );
+            if (pool[revChar].size() > 0) {
+                totalScore += idx - pool[revChar][pool[revChar].size() - 1];
+                pool[revChar].pop_back();
+            } else {
+                pool[curChar].push_back(idx);
+            }
+        }
+        return totalScore;
+    }
+};
