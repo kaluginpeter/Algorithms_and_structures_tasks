05ba248a762fc2bc188346b7f6df71e51233258c
kaluginpeter <streetworkout200412@gmail.com>
2025-01-09 17:58:04 +0300

    Added description "Imperfect Fibonacci Rabbits"

---
+++ CodeWars/6kyu/Imperfect_Fibonacci_Rabbits.py
@@ -0,0 +1,25 @@
+# Background
+# This is an extension of this kata so I suggest you complete that one first.
+#
+# Imperfect Rabbits
+# In this kata, our rabbits are sadly imperfect, they only live for a few months, and then they die! As in the previous kata however, our rabbits:
+#
+# Take one month to mature.
+# Always birth equal numbers of male and female offspring.
+# Never have to compete for resources.
+# Have no predators.
+# At the start of the first month you start with one pair of immature rabbits.
+#
+# Kata
+# In this kata you must implement the method provided to you with arguments n, b, l, where:
+#
+# n is the number of the months to simulate,
+# b is the number of pairs each adult pair gives birth to, and,
+# l is the lifespan in months of a rabbit.
+# Examples
+# n = 6, b = 3, l = 6
+# return 96
+#
+# n = 8, b = 3, l = 4
+# return 423
+# Algorithms
