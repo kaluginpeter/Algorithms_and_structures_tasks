15b074f75ea9ce0a3bd3e6d45ae5cf8e924984ed
kaluginpeter <streetworkout200412@gmail.com>
2025-01-10 17:33:18 +0300

    Added solution "320A. Magic Numbers"

--- Codeforces/320A._Magic_Numbers.py
+++ Codeforces/320A._Magic_Numbers.py
@@ -24,3 +24,56 @@
 # 441231
 # OutputCopy
 # NO
+# Solution
+# C++ O(N) O(1) Simulation
+#include <iostream>
+#include <string>
+
+
+void solution() {
+    std::string n;
+    std::cin >> n;
+    bool isValid = true;
+    int i = 0;
+    for (int j = 0; j < n.size(); ++j) {
+        if (n[j] != '4') {
+            if (n[j] != '1') {
+                isValid = false;
+                break;
+            }
+            i = j + 1;
+        } else if ((j - i + 1 == 3) || (j == 0)) {
+            isValid = false;
+            break;
+        }
+    }
+    std::cout << (isValid? "YES" : "NO") << "\n";
+}
+
+
+int main() {
+    solution();
+}
+
+# Python O(N) O(1) Simulation
+import sys
+
+
+def solution() -> None:
+    n: str = sys.stdin.readline().rstrip()
+    is_valid: bool = True
+    i: int = 0
+    for j in range(len(n)):
+        if n[j] != '4':
+            if n[j] != '1':
+                is_valid = False
+                break
+            i = j + 1
+        elif j - i + 1 == 3 or j == 0:
+            is_valid = False
+            break
+    sys.stdout.write(['NO', 'YES'][is_valid] + '\n')
+
+
+if __name__ == '__main__':
+    solution()
