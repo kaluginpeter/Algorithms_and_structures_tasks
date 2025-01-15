cb24b703baf72bdd2f6bb0f10a43c900c9e7850e
kaluginpeter <streetworkout200412@gmail.com>
2025-01-15 18:43:28 +0300

    Added description "1950B. Upscaling"

---
+++ Codeforces/1950B._Upscaling.py
@@ -0,0 +1,58 @@
+# B. Upscaling
+# time limit per test1 second
+# memory limit per test256 megabytes
+# You are given an integer n
+# . Output a 2n×2n
+#  checkerboard made of 2×2
+#  squares alternating '#
+# ' and '.
+# ', with the top-left cell being '#
+# '.
+#
+#
+# The picture above shows the answers for n=1,2,3,4
+# .
+#
+# Input
+# The first line contains an integer t
+#  (1≤t≤20
+# ) — the number of test cases.
+#
+# The only line of each test case contains a single integer n
+#  (1≤n≤20
+# ) — it means you need to output a checkerboard of side length 2n
+# .
+#
+# Output
+# For each test case, output 2n
+#  lines, each containing 2n
+#  characters without spaces — the checkerboard, as described in the statement. Do not output empty lines between test cases.
+#
+# Example
+# InputCopy
+# 4
+# 1
+# 2
+# 3
+# 4
+# OutputCopy
+# ##
+# ##
+# ##..
+# ##..
+# ..##
+# ..##
+# ##..##
+# ##..##
+# ..##..
+# ..##..
+# ##..##
+# ##..##
+# ##..##..
+# ##..##..
+# ..##..##
+# ..##..##
+# ##..##..
+# ##..##..
+# ..##..##
+# ..##..##
