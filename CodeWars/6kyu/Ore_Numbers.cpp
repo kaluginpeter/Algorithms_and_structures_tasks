779f8518ab149a9e8f368a3bfcfd5d5283540b45
kaluginpeter <streetworkout200412@gmail.com>
2025-01-16 18:01:34 +0300

    Added description "Ore Numbers"

---
+++ CodeWars/6kyu/Ore_Numbers.cpp
@@ -0,0 +1,14 @@
+/*
+Ore Numbers (also called Harmonic Divisor Numbers) are numbers for which the harmonic mean of all their divisors (including the number itself) equals an integer.
+
+For example, 6 is an Ore Number because its harmonic mean is exactly 2:
+
+H(6) = 4 / (1/1 + 1/2 + 1/3 + 1/6) = 2
+Your task is to complete the function returns true if the given number is an Ore Number and false otherwise.
+
+You can assume all inputs will be valid positive integers.
+
+Hint: The harmonic mean is the total number of divisors divided by the sum of their reciprocals.
+
+MathematicsFundamentalsAlgorithms
+*/
