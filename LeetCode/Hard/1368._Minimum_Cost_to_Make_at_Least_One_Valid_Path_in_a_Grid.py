ba201290f138837881e509afd571d10ecc04baa3
kaluginpeter <streetworkout200412@gmail.com>
2025-01-18 09:30:54 +0300

    Added solution "1368. Minimum Cost to Make at Least One Valid Path in a Grid"

--- LeetCode/Hard/1368._Minimum_Cost_to_Make_at_Least_One_Valid_Path_in_a_Grid.py
+++ LeetCode/Hard/1368._Minimum_Cost_to_Make_at_Least_One_Valid_Path_in_a_Grid.py
@@ -41,3 +41,73 @@
 # n == grid[i].length
 # 1 <= m, n <= 100
 # 1 <= grid[i][j] <= 4
+# Solution
+# Python O(NM + ElogV) O(NM + ElogV) Graph Shortest Path Dijkstra
+class Solution:
+    def minCost(self, grid: List[List[int]]) -> int:
+        n: int = len(grid)
+        m: int = len(grid[0])
+        moves: tuple[tuple[int, int, int]] = (
+            (0, 1, 1), (0, -1, 2), (1, 0, 3), (-1, 0, 4)
+        )
+        min_heap: list[tuple[int, int, int]] = [(0, 0, 0)]
+        seen: list[list[int]] = [[float('inf')] * m for row in range(n)]
+        seen[0][0] = 0
+        while min_heap:
+            cur_cost, cur_row, cur_col = heapq.heappop(min_heap)
+            if cur_row == n - 1 and cur_col == m - 1:
+                return cur_cost
+
+            for x, y, direction in moves:
+                next_row, next_col = cur_row + x, cur_col + y
+                weight: int = int(grid[cur_row][cur_col] != direction)
+                if (
+                        0 <= min(next_row, next_col)
+                        and next_row < n and next_col < m
+                        and (next_row, next_col) not in seen
+                        and cur_cost + weight < seen[next_row][next_col]
+                ):
+                    heapq.heappush(min_heap, (cur_cost + weight, next_row, next_col))
+                    seen[next_row][next_col] = cur_cost + weight
+
+# C++ O(NM + ElogV) O(NM + ElogV) Graph Shortest Path Dijkstra
+class Solution {
+public:
+    int minCost(vector<vector<int>>& grid) {
+        std::vector<std::tuple<int, int, int>> moves {
+            {0, 1, 1}, {0, -1, 2}, {1, 0, 3}, {-1, 0, 4}
+        };
+        int n = grid.size();
+        int m =  grid[0].size();
+        std::vector<std::vector<int>> seen (n, std::vector<int>(m, INT32_MAX));
+        seen[0][0] = 0;
+        std::priority_queue<
+            std::tuple<int, int, int>,
+            std::vector<std::tuple<int, int, int>>,
+            std::greater<std::tuple<int, int, int>>
+        > minHeap;
+        minHeap.push({0, 0, 0});
+        while (!minHeap.empty()) {
+            auto [curCost, curRow, curCol] = minHeap.top();
+            minHeap.pop();
+            if (curRow == n - 1 && curCol == m - 1) return curCost;
+            for (auto& move : moves) {
+                auto [x, y, direction] = move;
+                int nextRow = curRow + x;
+                int nextCol = curCol + y;
+                int weight = grid[curRow][curCol] != direction;
+                if (
+                    0 <= std::min(nextRow, nextCol)
+                    && nextRow < n && nextCol < m
+                    && curCost + weight < seen[nextRow][nextCol]
+                ) {
+                    minHeap.push(
+                        {curCost + weight, nextRow, nextCol}
+                    );
+                    seen[nextRow][nextCol] = curCost + weight;
+                }
+            }
+        }
+        return -1;
+    }
+};
