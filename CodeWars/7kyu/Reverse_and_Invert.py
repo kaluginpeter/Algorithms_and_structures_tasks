796bb6f018e1b27bb5116fc56a8c10c0502f8a2d
kaluginpeter <streetworkout200412@gmail.com>
2025-01-21 19:17:51 +0300

    Added description "Reverse and Invert"

---
+++ CodeWars/7kyu/Reverse_and_Invert.py
@@ -0,0 +1,7 @@
+# Reverse and invert all integer values in a given list.
+#
+# [1,12,'a',3.4,87,99.9,-42,50,5.6] --> [-1,-21,-78,24,-5]
+# Remove all types other than integer.
+#
+# ListsFundamentals
+# Solu
