f8642db8dba402460241b15589178cb838b88ddd
kaluginpeter <streetworkout200412@gmail.com>
2025-01-23 17:21:11 +0300

    Added description "Sequence to 1"

---
+++ CodeWars/7kyu/Sequence_to_1.cpp
@@ -0,0 +1,15 @@
+/*
+Get the number n to return the sequence from n to 1. The number n can be negative and also large number. (See the range as the following)
+
+Example :
+n=5  >> [5,4,3,2,1]
+n=-1 >> [-1,0,1]
+
+Range :
+Python     -9999 < n < 9999
+Javascript -9999 < n < 9999
+c++        -9999 < n < 9999
+Crystal    -9999 < n < 9999
+Ruby       -9999 < n < 9999
+Fundamentals
+*/
