34edb5553f70de87ec8c91d0afbddf6f6d3d60ef
kaluginpeter <streetworkout200412@gmail.com>
2025-01-28 18:46:52 +0300

    Added description "1971B. Different String"

---
+++ Codeforces/1971B._Different_String.py
@@ -0,0 +1,64 @@
+# B. Different String
+# time limit per test1 second
+# memory limit per test256 megabytes
+# You are given a string s
+#  consisting of lowercase English letters.
+#
+# Rearrange the characters of s
+#  to form a new string r
+#  that is not equal to s
+# , or report that it's impossible.
+#
+# Input
+# The first line contains a single integer t
+#  (1≤t≤1000
+# ) — the number of test cases.
+#
+# The only line of each test case contains a string s
+#  of length at most 10
+#  consisting of lowercase English letters.
+#
+# Output
+# For each test case, if no such string r
+#  exists as described in the statement, output "NO" (without quotes).
+#
+# Otherwise, output "YES" (without quotes). Then, output one line — the string r
+# , consisting of letters of string s
+# .
+#
+# You can output "YES" and "NO" in any case (for example, strings "yEs", "yes", and "Yes" will be recognized as a positive response).
+#
+# If multiple answers are possible, you can output any of them.
+#
+# Example
+# InputCopy
+# 8
+# codeforces
+# aaaaa
+# xxxxy
+# co
+# d
+# nutdealer
+# mwistht
+# hhhhhhhhhh
+# OutputCopy
+# YES
+# forcodesec
+# NO
+# YES
+# xxyxx
+# YES
+# oc
+# NO
+# YES
+# undertale
+# YES
+# thtsiwm
+# NO
+# Note
+# In the first test case, another possible answer is forcescode
+# .
+#
+# In the second test case, all rearrangements of aaaaa
+#  are equal to aaaaa
+# .
