08878e7b78bc45baf6ed15d345767b5069a3c1bb
kaluginpeter <streetworkout200412@gmail.com>
2025-01-29 18:35:59 +0300

    Added description "Satisfying numbers"

---
+++ CodeWars/7kyu/Satisfying_numbers.py
@@ -0,0 +1,15 @@
+# 2520 is the smallest number that can be divided by each of the integers from 1 to 10 without any remainder.
+#
+# Find the smallest positive number that is evenly divisible by all of the integers from 1 to n, (in this kata,
+# 1
+# ≤
+# n
+# ≤
+# 40
+# 1≤n≤40).
+#
+# For example:
+#
+# 5 --> 60      // 1 to 5 can all divide evenly into 60
+# 10 --> 2520
+# Algorithms
