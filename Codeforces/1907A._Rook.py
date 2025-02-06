5919af2ff5e0e2387a39bc6252252134f1aa3fe9
kaluginpeter <streetworkout200412@gmail.com>
2025-02-06 17:48:33 +0300

    Added solution "1907A. Rook"

--- Codeforces/1907A._Rook.py
+++ Codeforces/1907A._Rook.py
@@ -40,3 +40,49 @@
 # d7
 # c5
 # d4
+# Solution
+# C++ O(1) O(1) Simulation
+#include <bits/stdc++.h>
+
+
+void solution() {
+    std::string moves = "abcdefgh";
+    int t;
+    std::cin >> t;
+    for (int i = 0; i < t; ++i) {
+        std::string pos;
+        std::cin >> pos;
+        for (int d = 1; d <= 8; ++d) {
+            if ((pos[1] - '0') == d) continue;
+            std::cout << pos[0] << d << "\n";
+        }
+        for (char& ch : moves) {
+            if (ch == pos[0]) continue;
+            std::cout << ch << pos[1] << "\n";
+        }
+    }
+}
+
+
+int main() {
+    solution();
+}
+
+# Python O(1) O(1) Simulation
+import sys
+
+
+def solution() -> None:
+    t: int = int(sys.stdin.readline().rstrip())
+    for _ in range(t):
+        pos: str = sys.stdin.readline().rstrip()
+        for d in range(1, 9):
+            if d == int(pos[1]): continue
+            sys.stdout.write(f'{pos[0]}{d}\n')
+        for ch in 'abcdefgh':
+            if ch == pos[0]: continue
+            sys.stdout.write(f'{ch}{pos[1]}\n')
+
+
+if __name__ == '__main__':
+    solution()
