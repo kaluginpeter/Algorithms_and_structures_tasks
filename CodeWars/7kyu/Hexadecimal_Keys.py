bb613bedc8af81f0cd6058528883bcc1dbdf29c8
kaluginpeter <streetworkout200412@gmail.com>
2025-02-07 18:21:00 +0300

    Added solution "Hexadecimal Keys"

--- CodeWars/7kyu/Hexadecimal_Keys.py
+++ CodeWars/7kyu/Hexadecimal_Keys.py
@@ -10,3 +10,13 @@
 # input: "2533", result: 9328 (primes: 89, 107)
 # input: "1ba9", result: 6912 (primes: 73, 97)
 # Algorithms
+# Solution
+def is_prime(n: int) -> bool:
+    for d in range(2, int(n**.5) + 1):
+        if n % d == 0: return 0
+    return 1
+primes: set[int] = set(num for num in range(1, 10**5 + 1) if is_prime(num))
+def find_key(encryption_key):
+    t: int = int(encryption_key, 16)
+    for p in primes:
+        if t // p in primes and t % p == 0: return (p - 1) * (t // p - 1)
