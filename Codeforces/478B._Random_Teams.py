3615a8373eda46e76d4d79bdbab8ff56226eefaf
kaluginpeter <streetworkout200412@gmail.com>
2025-02-10 17:47:02 +0300

    Added description "478B. Random Teams"

---
+++ Codeforces/478B._Random_Teams.py
@@ -0,0 +1,32 @@
+# B. Random Teams
+# time limit per test1 second
+# memory limit per test256 megabytes
+# n participants of the competition were split into m teams in some manner so that each team has at least one participant. After the competition each pair of participants from the same team became friends.
+#
+# Your task is to write a program that will find the minimum and the maximum number of pairs of friends that could have formed by the end of the competition.
+#
+# Input
+# The only line of input contains two integers n and m, separated by a single space (1 ≤ m ≤ n ≤ 109) — the number of participants and the number of teams respectively.
+#
+# Output
+# The only line of the output should contain two integers kmin and kmax — the minimum possible number of pairs of friends and the maximum possible number of pairs of friends respectively.
+#
+# Examples
+# InputCopy
+# 5 1
+# OutputCopy
+# 10 10
+# InputCopy
+# 3 2
+# OutputCopy
+# 1 1
+# InputCopy
+# 6 3
+# OutputCopy
+# 3 6
+# Note
+# In the first sample all the participants get into one team, so there will be exactly ten pairs of friends.
+#
+# In the second sample at any possible arrangement one team will always have two participants and the other team will always have one participant. Thus, the number of pairs of friends will always be equal to one.
+#
+# In the third sample minimum number of newly formed friendships can be achieved if participants were split on teams consisting of 2 people, maximum number can be achieved if participants were split on teams of 1, 1 and 4 people.
