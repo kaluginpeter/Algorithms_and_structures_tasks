941cef51ed6d106ea7815a9e5c4e42f44f0fc71b
kaluginpeter <streetworkout200412@gmail.com>
2025-02-12 06:52:39 +0300

    Added solution "2342. Max Sum of a Pair With Equal Sum of Digits"

--- LeetCode/Medium/2342._Max_Sum_of_a_Pair_With_Equal_Sum_of_Digits.py
+++ LeetCode/Medium/2342._Max_Sum_of_a_Pair_With_Equal_Sum_of_Digits.py
@@ -23,3 +23,54 @@
 #
 # 1 <= nums.length <= 105
 # 1 <= nums[i] <= 109
+# Solution
+# Python O(NlogM + NlogN) O(N) HashMap Math
+class Solution:
+    def get_corresponding_value(self, num: int) -> int:
+        accumulate: int = 0
+        while num:
+            accumulate += num % 10
+            num //= 10
+        return accumulate
+#         return sum(int(digit) for digit in str(num))
+
+    def maximumSum(self, nums: List[int]) -> int:
+        hashmap: dict[int, list[int]] = dict()
+        for num in nums:
+            cor_value: int = self.get_corresponding_value(num)
+            if cor_value not in hashmap: hashmap[cor_value] = []
+            hashmap[cor_value].append(num)
+        mx_num: int = -1
+        for group, elements in hashmap.items():
+            if len(elements) > 1:
+                elements.sort()
+                mx_num = max(mx_num, elements[-1] + elements[-2])
+        return mx_num
+
+# C++ O(NlogM + NlogN) O(N) HashMap Math
+class Solution {
+public:
+    int correspondingValue(int num) {
+        int accumulate = 0;
+        while (num) {
+            accumulate += num % 10;
+            num /= 10;
+        }
+        return accumulate;
+    }
+    int maximumSum(vector<int>& nums) {
+        std::unordered_map<int, std::vector<int>> hashmap;
+        for (int& num : nums) {
+            hashmap[correspondingValue(num)].push_back(num);
+        }
+        int mxNum = -1;
+        for (auto& p : hashmap) {
+            if (p.second.size() > 1) {
+                std::sort(p.second.begin(), p.second.end());
+                int n = p.second.size();
+                mxNum = std::max(mxNum, p.second[n - 2] + p.second[n - 1]);
+            }
+        }
+        return mxNum;
+    }
+};
