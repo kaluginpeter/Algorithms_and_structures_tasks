d7080316d48b892bc5243e46cbf7ee4bd305d9a4
kaluginpeter <streetworkout200412@gmail.com>
2025-02-17 07:06:13 +0300

    Added solution "1079. Letter Tile Possibilities"

--- LeetCode/Medium/1079._Letter_Tile_Possibilities.py
+++ LeetCode/Medium/1079._Letter_Tile_Possibilities.py
@@ -23,3 +23,47 @@
 #
 # 1 <= tiles.length <= 7
 # tiles consists of uppercase English letters.
+# Solution
+# Python O(2**N) O(N) Backtracking
+class Solution:
+    def backtrack(self, chunks: list[int]) -> int:
+        permutations: int = 0
+        for chunk in range(26):
+            if not chunks[chunk]: continue
+            permutations += 1
+            chunks[chunk] -= 1
+            permutations += self.backtrack(chunks)
+            chunks[chunk] += 1
+        return permutations
+
+    def numTilePossibilities(self, tiles: str) -> int:
+        chunks: list[int] = [0] * 26
+        for letter in tiles:
+            chunks[ord(letter) - 65] += 1
+        return self.backtrack(chunks)
+
+# C++ O(2**N) O(N) Backtracking
+class Solution {
+public:
+    void backtrack(std::string& substr, std::string& tiles, std::unordered_set<std::string>& seen, std::vector<bool>& used) {
+        if (substr.size() == tiles.size()) return;
+        for (int idx = 0; idx < tiles.size(); ++idx) {
+            if (used[idx]) continue;
+            used[idx] = true;
+            substr.push_back(tiles[idx]);
+            seen.insert(substr);
+            backtrack(substr, tiles, seen, used);
+            used[idx] = false;
+            substr.pop_back();
+        }
+    }
+
+    int numTilePossibilities(string tiles) {
+        std::string substr = "";
+        int curIdx = 0;
+        std::vector<bool> used (7, false);
+        std::unordered_set<std::string> seen;
+        backtrack(substr, tiles, seen, used);
+        return seen.size();
+    }
+};
