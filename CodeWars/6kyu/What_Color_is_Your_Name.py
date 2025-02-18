eae8238b7d4522844dcad971aeccbc7644f36f4b
kaluginpeter <streetworkout200412@gmail.com>
2025-02-18 18:14:22 +0300

    Added solution "What Color is Your Name?"

--- CodeWars/6kyu/What_Color_is_Your_Name.py
+++ CodeWars/6kyu/What_Color_is_Your_Name.py
@@ -16,3 +16,18 @@
 # NOTE: The function should return None/nil when the input is less than two chars.
 #
 # PuzzlesStringsAlgorithms
+# Solution
+from math import prod
+def string_color(name):
+    if len(name) < 2: return None
+    output: list[str] = []
+    x: str = str(hex(sum(ord(char) for char in name) % 256))[2:].upper()
+    if len(x) == 1: x = '0' + x
+    output.append(x)
+    y: str = str(hex(prod(ord(char) for char in name) % 256))[2:].upper()
+    if len(y) == 1: y = '0' + y
+    output.append(y)
+    z: str = str(hex(abs(ord(name[0]) - sum(ord(char) for char in name[1:])) % 256))[2:].upper()
+    if len(z) == 1: z = '0' + z
+    output.append(z)
+    return ''.join(output)
