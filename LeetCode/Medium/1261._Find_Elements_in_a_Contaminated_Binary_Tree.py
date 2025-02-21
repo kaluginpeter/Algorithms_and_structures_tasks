afe45098f1476dab88eebbc5c1a609c27c3cb45c
kaluginpeter <streetworkout200412@gmail.com>
2025-02-21 07:02:22 +0300

    Added solution "1261. Find Elements in a Contaminated Binary Tree"

--- LeetCode/Medium/1261._Find_Elements_in_a_Contaminated_Binary_Tree.py
+++ LeetCode/Medium/1261._Find_Elements_in_a_Contaminated_Binary_Tree.py
@@ -60,3 +60,87 @@
 # The total number of nodes is between [1, 104]
 # Total calls of find() is between [1, 104]
 # 0 <= target <= 106
+# Solution
+# Python O(V) O(V) Tree HashSet Depth-First-Search
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class FindElements:
+    def __init__(self, root: Optional[TreeNode]):
+        self.root: TreeNode = root
+        self.hashset: set[int] = set()
+        self.recover_tree(root, 0, self.hashset)
+
+    def recover_tree(self, root: TreeNode, depth: int, hashset: set[int]):
+        if not depth:
+            root.val = 0
+            hashset.add(root.val)
+        if root.left:
+            root.left.val = root.val * 2 + 1
+            hashset.add(root.left.val)
+            self.recover_tree(root.left, depth + 1, hashset)
+        if root.right:
+            root.right.val = root.val * 2 + 2
+            hashset.add(root.right.val)
+            self.recover_tree(root.right, depth + 1, hashset)
+
+    def find(self, target: int) -> bool:
+        return target in self.hashset
+
+# Your FindElements object will be instantiated and called as such:
+# obj = FindElements(root)
+# param_1 = obj.find(target)
+
+# C++ O(V) O(V) Tree HashSet Depth-First-Search
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class FindElements {
+private:
+    TreeNode* mainRoot;
+    std::unordered_set<int> hashset;
+public:
+    void recoverTree(TreeNode* root, int depth, std::unordered_set<int>& hashset) {
+        if (!depth) {
+            root->val = 0;
+            hashset.insert(root->val);
+        }
+        if (root->left) {
+            root->left->val = root->val * 2 + 1;
+            hashset.insert(root->left->val);
+            recoverTree(root->left, depth + 1, hashset);
+        }
+        if (root->right) {
+            root->right->val = root->val * 2 + 2;
+            hashset.insert(root->right->val);
+            recoverTree(root->right, depth + 1, hashset);
+        }
+    }
+
+    FindElements(TreeNode* root) {
+        hashset = {};
+        recoverTree(root, 0, hashset);
+        mainRoot = root;
+    }
+
+    bool find(int target) {
+        return hashset.count(target);
+    }
+};
+
+/**
+ * Your FindElements object will be instantiated and called as such:
+ * FindElements* obj = new FindElements(root);
+ * bool param_1 = obj->find(target);
+ */
