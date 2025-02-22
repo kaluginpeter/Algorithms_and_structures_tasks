29451ccc043782598152657ea15ae2cdd80703d6
kaluginpeter <streetworkout200412@gmail.com>
2025-02-22 08:53:17 +0300

    Added solution "214A. System of Equations"

--- Codeforces/214A._System_of_Equations.py
+++ Codeforces/214A._System_of_Equations.py
@@ -29,3 +29,40 @@
 # 0
 # Note
 # In the first sample the suitable pair is integers (3, 0). In the second sample the suitable pair is integers (3, 5). In the third sample there is no suitable pair.
+# Solution
+# C++ O(a) O(1) Math
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solution() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+    int pairs = 0;
+    for (int a = 0; a <= n; ++a) {
+        int b = n - pow(a, 2);
+        if (b >= 0 && a + pow(b, 2) == m) ++pairs;
+    }
+    printf("%d\n", pairs);
+}
+
+
+int main() {
+    solution();
+}
+
+# Python O(a) O(1) Math
+import sys
+
+
+def solution() -> None:
+    n, m = map(int, sys.stdin.readline().rstrip().split())
+    pairs: int = 0
+    for a in range(n + 1):
+        b: int = int(n - a**2)
+        if b >= 0 and a + b**2 == m: pairs += 1
+    sys.stdout.write(f'{pairs}\n')
+
+
+if __name__ == '__main__':
+    solution()
