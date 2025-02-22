f6962c97160894b4f5c02c47187844346b903615
kaluginpeter <streetworkout200412@gmail.com>
2025-02-22 08:35:26 +0300

    Added solution "1028. Recover a Tree From Preorder Traversal"

--- LeetCode/Hard/1028._Recover_a_Tree_From_Preorder_Traversal.py
+++ LeetCode/Hard/1028._Recover_a_Tree_From_Preorder_Traversal.py
@@ -29,3 +29,97 @@
 #
 # The number of nodes in the original tree is in the range [1, 1000].
 # 1 <= Node.val <= 109
+# Solution
+# Python O(N) O(H) Binary Tree Simulation Greedy
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def parse_node(self, idx: int, traversal: str, n: int) -> tuple[int, int, int]:
+        depth: int = 0
+        while idx < n and traversal[idx] == '-':
+            depth += 1
+            idx += 1
+        node_val: int = 0
+        while idx < n and traversal[idx] != '-':
+            node_val = node_val * 10 + int(traversal[idx])
+            idx += 1
+        return (idx, node_val, depth)
+
+    def recoverFromPreorder(self, traversal: str) -> Optional[TreeNode]:
+        levels: list[TreeNode] = []
+        n: int = len(traversal)
+        idx: int = 0
+        while idx < n:
+            idx, node_val, depth = self.parse_node(idx, traversal, n)
+            child: TreeNode = TreeNode(node_val)
+            if not depth: # Root node
+                levels.append(child)
+                continue
+            if depth == len(levels): # Child at new level
+                levels.append(child)
+            else: # Child at existing level
+                levels[depth] = child
+            parent: TreeNode = levels[depth - 1]
+            if not parent.left:
+                parent.left = child
+            else:
+                parent.right = child
+        return levels[0]
+
+# C++ O(N) O(H) BinaryTree Simulation Greedy
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    tuple<int, int, int> parseNode(int& idx, const string& traversal, const int& n) {
+        int depth = 0;
+        while (idx < n && traversal[idx] == '-') {
+            ++depth;
+            ++idx;
+        }
+        int nodeVal = 0;
+        while (idx < n && traversal[idx] != '-') {
+            nodeVal = nodeVal * 10 + (traversal[idx] - '0');
+            ++idx;
+        }
+        return {idx, nodeVal, depth};
+    }
+
+    TreeNode* recoverFromPreorder(string traversal) {
+        vector<TreeNode*> levels;
+        int idx = 0;
+        int n = traversal.size();
+        while (idx < n) {
+            tuple<int, int, int> data = parseNode(idx, traversal, n);
+            idx = get<0>(data);
+            int nodeVal = get<1>(data), depth = get<2>(data);
+            TreeNode* child = new TreeNode(nodeVal);
+            if (!depth) { // Root node
+                levels.push_back(child);
+                continue;
+            }
+            if (depth == levels.size()) { // New level child
+                levels.push_back(child);
+            } else { // Existing level child
+                levels[depth] = child;
+            }
+            TreeNode* parent = levels[depth - 1];
+            if (!parent->left) parent->left = child;
+            else parent->right = child;
+        }
+        return levels[0];
+    }
+};
