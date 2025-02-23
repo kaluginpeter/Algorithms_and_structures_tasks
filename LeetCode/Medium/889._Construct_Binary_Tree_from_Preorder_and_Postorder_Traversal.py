495c935e8e374106e439fcf19e20808a2897c9c9
kaluginpeter <streetworkout200412@gmail.com>
2025-02-23 09:16:41 +0300

    Added solution "889. Construct Binary Tree from Preorder and Postorder Traversal"

--- LeetCode/Medium/889._Construct_Binary_Tree_from_Preorder_and_Postorder_Traversal.py
+++ LeetCode/Medium/889._Construct_Binary_Tree_from_Preorder_and_Postorder_Traversal.py
@@ -24,3 +24,80 @@
 # 1 <= postorder[i] <= postorder.length
 # All the values of postorder are unique.
 # It is guaranteed that preorder and postorder are the preorder traversal and postorder traversal of the same binary tree.
+# Solution
+# Python O(N) O(H) Depth-First-Search
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def constructFromPrePost(self, preorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+        levels: list[TreeNode] = []
+        pre_idx: int = 0
+        n: int = len(preorder)
+        post_idx: int = 0
+        m: int = len(postorder)
+        depth: int = 0
+        while pre_idx < n:
+            cur_node_val: int = -1
+            while pre_idx < n and cur_node_val != postorder[post_idx]:
+                child: TreeNode = TreeNode(preorder[pre_idx])
+                if depth == len(levels): levels.append(child)
+                else: levels[depth] = child
+                if depth:
+                    parent: TreeNode = levels[depth - 1]
+                    if not parent.left: parent.left = child
+                    else: parent.right = child
+                cur_node_val = child.val
+                depth += 1
+                pre_idx += 1
+            while post_idx < m and levels[depth - 1].val == postorder[post_idx]:
+                depth -= 1
+                post_idx += 1
+
+        return levels[0]
+
+# C++ O(N) O(H) Depth-First-Search
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* constructFromPrePost(vector<int>& preorder, vector<int>& postorder) {
+        vector<TreeNode*> levels;
+        int postIdx = 0, m = postorder.size();
+        int preIdx = 0, n = preorder.size();;
+        int depth = 0;
+        while (preIdx < n) {
+            int curNodeVal = -1;
+            while (preIdx < n && curNodeVal != postorder[postIdx]) {
+                TreeNode* child = new TreeNode(preorder[preIdx]);
+                if (depth == levels.size()) levels.push_back(child);
+                else levels[depth] = child;
+                if (depth) {
+                    TreeNode* parent = levels[depth - 1];
+                    if (!parent->left) parent->left = child;
+                    else parent->right = child;
+                }
+                curNodeVal = child->val;
+                ++depth;
+                ++preIdx;
+            }
+            while (postIdx < m && levels[depth - 1]->val == postorder[postIdx]) {
+                --depth;
+                ++postIdx;
+            }
+        }
+        return levels[0];
+    }
+};
