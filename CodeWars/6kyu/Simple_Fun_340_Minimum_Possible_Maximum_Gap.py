8b19da7e3a0b2f0ac7802c57f8cdcc3c78a57b8d
kaluginpeter <streetworkout200412@gmail.com>
2025-02-24 19:34:25 +0300

    Added description "Simple Fun #340: Minimum Possible Maximum Gap"

---
+++ CodeWars/6kyu/Simple_Fun_340_Minimum_Possible_Maximum_Gap.py
@@ -0,0 +1,29 @@
+# Task
+# In an integer array, gap means the difference between two adjacent elements.
+#
+# Maximum gap means the maximum value of all gaps.
+#
+# Given a sorted integer array arr. Then, we remove an element arbitrarily from the inner part of an array(inner part means that not include the first and last elements).
+#
+# Your task is to find the minimum possible maximum gap after the remove operation.
+#
+# Example
+# For arr = [1,2,5,7,8], the output should be 3.
+#
+# In this case, we can remove one element of 2,5,7
+# If remove 2, the maximum gap of [1,5,7,8] is 4
+# (The deffrence between 1 and 5)
+# If remove 5, the maximum gap of [1,2,7,8] is 5
+# (The deffrence between 2 and 7)
+# If remove 2, the maximum gap of [1,2,5,8] is 3
+# (The deffrence between 2 and 5, or 5 and 8)
+# So, the minimum possible maximum gap is 3
+# For arr = [1,4,6], the output should be 5.
+#
+# We can only remove 4 from the array. So, 6 - 1 = 5
+#
+# For arr = [1,2,3,4,5], the output should be 2.
+#
+# We can remove one element of 2,3,4. Whichever element is removed, the maximum gap always be 2.
+#
+# Algorithms
