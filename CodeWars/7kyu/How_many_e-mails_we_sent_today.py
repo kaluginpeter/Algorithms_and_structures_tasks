dc035dc65bec8da0c70cfb9dd88868a7b10746c8
kaluginpeter <streetworkout200412@gmail.com>
2025-02-26 18:10:26 +0300

    Added description "How many e-mails we sent today?"

---
+++ CodeWars/7kyu/How_many_e-mails_we_sent_today.py
@@ -0,0 +1,19 @@
+# Every day we can send from the server a certain limit of e-mails.
+#
+# Task:
+# Write a function that will return the integer number of e-mails sent in the percentage of the limit.
+#
+# Example:
+#
+# limit       - 1000;
+# emails sent - 101;
+# return      - 10%; // because integer from 10,1 = 10
+# Arguments:
+# sent: number of e-mails sent today (integer)
+# limit: number of e-mails you can send today (integer)
+# When:
+# the argument sent = 0, then return the message: "No e-mails sent";
+# the argument sent >= limit, then return the message: "Daily limit is reached";
+# the argument limit is empty, then default limit = 1000 emails;
+# Good luck!
+# FundamentalsLogicMathematics
