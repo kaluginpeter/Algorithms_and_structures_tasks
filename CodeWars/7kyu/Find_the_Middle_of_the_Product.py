627d52fe4905b6cd01a909ba5f459157d03d87ba
kaluginpeter <streetworkout200412@gmail.com>
2025-02-27 20:19:36 +0300

    Added solution "Find the Middle of the Product"

--- CodeWars/7kyu/Find_the_Middle_of_the_Product.py
+++ CodeWars/7kyu/Find_the_Middle_of_the_Product.py
@@ -11,3 +11,16 @@
 # NOTE: Remove leading zeros if product is even and the first digit of the two is a zero. Example 2016 -> 1
 #
 # FundamentalsStrings
+# Solution
+def find_middle(st):
+    if not st or not isinstance(st, str): return -1
+    prev: int = -1
+    for letter in st:
+        if letter.isdigit():
+            if prev == -1: prev = int(letter)
+            else: prev *= int(letter)
+    if prev == -1: return -1
+    str_prev: str = str(prev)
+    if len(str_prev) & 1:
+        return int(str_prev[len(str_prev) // 2])
+    return int(str_prev[len(str_prev) // 2 - 1:len(str_prev) // 2 + 1])
