d5a3fc2baefe4fa9735bc28fe44ce3f5de41cdee
kaluginpeter <streetworkout200412@gmail.com>
2025-02-27 19:33:50 +0300

    Added solution "873. Length of Longest Fibonacci Subsequence"

--- LeetCode/Medium/873._Length_of_Longest_Fibonacci_Subsequence.py
+++ LeetCode/Medium/873._Length_of_Longest_Fibonacci_Subsequence.py
@@ -24,3 +24,60 @@
 #
 # 3 <= arr.length <= 1000
 # 1 <= arr[i] < arr[i + 1] <= 109
+# Solution
+# Python O(N**2LogM) O(N) Greedy Number Theory
+class Solution:
+    def lenLongestFibSubseq(self, arr: List[int]) -> int:
+        n: int = len(arr)
+        seen: set[int] = set()
+        seen.add(arr[0])
+        seen.add(arr[1])
+        dp: list[int] = [0] * n
+        for i in range(2, n):
+            for j in range(i - 1, -1, -1):
+                a: int = arr[i]
+                b: int = arr[j]
+                c: int = a - b
+                path: int = 0
+                while c in seen:
+                    if c >= b: break
+                    if a == arr[i]: path += 3
+                    else: path += 1
+                    a = b
+                    b = c
+                    c = a - b
+                dp[i] = max(dp[i], path)
+            seen.add(arr[i])
+        return max(dp)
+
+# C++ O(N**2logM) O(N) Greedy Number Theory
+class Solution {
+public:
+    int lenLongestFibSubseq(vector<int>& arr) {
+        int n = arr.size();
+        std::vector<int> dp (n, 0);
+        std::unordered_set<int> seen = {arr[0], arr[1]};
+        for (int i = 2; i < n; ++i) {
+            for (int j = i - 1; j >= 0; --j) {
+                int a = arr[i];
+                int b = arr[j];
+                int c = a - b;
+                int path = 0;
+                while (seen.count(c)) {
+                    if (c >= b) break;
+                    if (a == arr[i]) path += 3;
+                    else path += 1;
+                    a = b;
+                    b = c;
+                    c = a - b;
+                }
+                dp[i] = std::max(dp[i], path);
+
+            }
+            seen.insert(arr[i]);
+        }
+        int answer = 0;
+        for (int& num : dp) answer = std::max(answer, num);
+        return answer;
+    }
+};
