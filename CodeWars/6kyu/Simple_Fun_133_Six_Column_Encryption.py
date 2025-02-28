946c08bcc32286f9696a00090a0b09b08e7a1226
kaluginpeter <streetworkout200412@gmail.com>
2025-02-28 19:04:12 +0300

    Added solution "Simple Fun #133: Six Column Encryption"

--- CodeWars/6kyu/Simple_Fun_133_Six_Column_Encryption.py
+++ CodeWars/6kyu/Simple_Fun_133_Six_Column_Encryption.py
@@ -20,3 +20,15 @@
 # encrypted message
 #
 # Puzzles
+# Solution
+def six_column_encryption(msg):
+    matrix: list[list[str]] = []
+    row: list[str] = []
+    for letter in msg:
+        row.append(letter if letter != ' ' else '.')
+        if len(row) == 6:
+            matrix.append(row[::])
+            row.clear()
+    if row:
+        matrix.append(row + ['.'] * (6 - len(row)))
+    return ' '.join(''.join(matrix[i][j] for i in range(len(matrix))) for j in range(6))
