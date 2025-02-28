d68ef46da1459633a0305b320392e0136757fd40
kaluginpeter <streetworkout200412@gmail.com>
2025-02-28 18:17:15 +0300

    Added solution "1092. Shortest Common Supersequence"

--- LeetCode/Hard/1092._Shortest_Common_Supersequence.py
+++ LeetCode/Hard/1092._Shortest_Common_Supersequence.py
@@ -22,3 +22,75 @@
 #
 # 1 <= str1.length, str2.length <= 1000
 # str1 and str2 consist of lowercase English letters.
+# Solution
+# Python O(NM) O(NM) Dynamic Programming
+class Solution:
+    def shortestCommonSupersequence(self, str1: str, str2: str) -> str:
+        n: int = len(str1)
+        m: int = len(str2)
+        dp: list[list[int]] = [[0] * (m + 1) for _ in range(n + 1)]
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                if str1[i - 1] == str2[j - 1]: dp[i][j] = dp[i - 1][j - 1] + 1
+                else: dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        lcs: list[str] = []
+        i, j = n, m
+        while dp[i][j]:
+            if str1[i - 1] == str2[j - 1]:
+                lcs.append(str1[i - 1])
+                i -= 1
+                j -= 1
+            elif dp[i][j] == dp[i - 1][j]:
+                lcs.append(str1[i - 1])
+                i -= 1
+            else:
+                lcs.append(str2[j - 1])
+                j -= 1
+        while i:
+            lcs.append(str1[i - 1])
+            i -= 1
+        while j:
+            lcs.append(str2[j - 1])
+            j -= 1
+        return ''.join(reversed(lcs))
+
+# C++ O(NM) O(NM) DynamicProgramming
+class Solution {
+public:
+    string shortestCommonSupersequence(string str1, string str2) {
+        int n = str1.size();
+        int m = str2.size();
+        vector<vector<int>> dp (n + 1, vector<int>(m + 1, 0));
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= m; ++j) {
+                if (str1[i - 1] == str2[j - 1]) dp[i][j] = dp[i - 1][j - 1] + 1;
+                else dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        string lcs = "";
+        int i = n, j = m;
+        while (dp[i][j]) {
+            if (str1[i - 1] == str2[j - 1]) {
+                lcs.push_back(str1[i - 1]);
+                --i;
+                --j;
+            } else if (dp[i][j] == dp[i - 1][j]) {
+                lcs.push_back(str1[i - 1]);
+                --i;
+            } else {
+                lcs.push_back(str2[j - 1]);
+                --j;
+            }
+        }
+        while (i > 0) {
+            lcs.push_back(str1[i - 1]);
+            --i;
+        }
+        while (j > 0) {
+            lcs.push_back(str2[j - 1]);
+            --j;
+        }
+        reverse(lcs.begin(), lcs.end());
+        return lcs;
+    }
+};
