8fdc7217a32d01fa7856bf9b3498576a2a0e36b5
kaluginpeter <streetworkout200412@gmail.com>
2025-03-01 09:21:14 +0300

    Added description "Sectional Array Sort"

---
+++ CodeWars/7kyu/Sectional_Array_Sort.py
@@ -0,0 +1,19 @@
+# In this kata you are given an array to sort but you're expected to start sorting from a specific position of the array (in ascending order) and optionally you're given the number of items to sort.
+#
+# Inputs:
+# An array to sort
+# The starting index for sorting
+# (Optional) The number of items to sort
+# If the number of items to sort is not provided or is zero, sort the array from the starting position to the end.
+#
+# Examples:
+# array: [1, 2, 5, 7, 4, 6, 3, 9, 8]
+# start: 2
+# length: not specified
+# expected result: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+#
+# array: [9, 7, 4, 2, 5, 3, 1, 8, 6]
+# start: 2
+# length: 5
+# expected result: [9, 7, 1, 2, 3, 4, 5, 8, 6]
+# ArraysSortingData StructuresAlgorithms
