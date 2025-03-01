2adf880a7e5d090598ddfe4db47789a7002bd6c5
kaluginpeter <streetworkout200412@gmail.com>
2025-03-01 09:13:27 +0300

    Added description "1968A. Maximize?"

---
+++ Codeforces/1968A._Maximize.py
@@ -0,0 +1,51 @@
+# A. Maximize?
+# time limit per test2 seconds
+# memory limit per test256 megabytes
+# You are given an integer x
+# . Your task is to find any integer y
+#  (1≤y<x)
+#  such that gcd(x,y)+y
+#  is maximum possible.
+#
+# Note that if there is more than one y
+#  which satisfies the statement, you are allowed to find any.
+#
+# gcd(a,b)
+#  is the Greatest Common Divisor of a
+#  and b
+# . For example, gcd(6,4)=2
+# .
+#
+# Input
+# The first line contains a single integer t
+#  (1≤t≤1000
+# ) — the number of test cases.
+#
+# Each of the following t
+#  lines contains a single integer x
+#  (2≤x≤1000
+# ).
+#
+# Output
+# For each test case, output any y
+#  (1≤y<x
+# ), which satisfies the statement.
+#
+# Example
+# InputCopy
+# 7
+# 10
+# 7
+# 21
+# 100
+# 2
+# 1000
+# 6
+# OutputCopy
+# 5
+# 6
+# 18
+# 98
+# 1
+# 750
+# 3
