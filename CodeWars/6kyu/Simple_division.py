b777521fa9f92aa176ef92ecfd3603324443a895
kaluginpeter <streetworkout200412@gmail.com>
2025-03-02 08:04:18 +0300

    Added description "Simple division"

---
+++ CodeWars/6kyu/Simple_division.py
@@ -0,0 +1,13 @@
+# In this Kata, you will be given two numbers, a and b, and your task is to determine if the first number a is divisible by all the prime factors of the second number b. For example: solve(15,12) = False because 15 is not divisible by all the prime factors of 12 (which include2).
+#
+# See test cases for more examples.
+#
+# Good luck!
+#
+# If you like this Kata, please try:
+#
+# Sub-array division
+#
+# Divisor harmony
+#
+# Fundamentals
