e1a5dcf2e81c2e962cc3c61858778c267793e403
kaluginpeter <streetworkout200412@gmail.com>
2025-03-13 10:10:25 +0300

    Added solution "894A. QAQ"

--- Codeforces/894A._QAQ.py
+++ Codeforces/894A._QAQ.py
@@ -25,3 +25,47 @@
 # 3
 # Note
 # In the first example there are 4 subsequences "QAQ": "QAQAQYSYIOIWIN", "QAQAQYSYIOIWIN", "QAQAQYSYIOIWIN", "QAQAQYSYIOIWIN".
+# Solution
+# C++ O(N) O(1) Combinatorial Dynamic Programming
+#include <iostream>
+#include <string>
+
+
+void solution() {
+    std::string sequence;
+    std::cin >> sequence;
+    int output = 0;
+    int x = 0;
+    int prev = 0;
+    for (int right = 0; right < sequence.size(); ++right) {
+        if (sequence[right] == 'A') prev += x;
+        else if (sequence[right] == 'Q') ++x;
+        if (sequence[right] == 'Q') output += prev;
+    }
+    std::printf("%d\n", output);
+}
+
+
+int main() {
+    solution();
+}
+
+# Python O(N) O(1) Combinatorial DynamicProgramming
+import sys
+
+
+def solution() -> None:
+    sequence: str = sys.stdin.readline().rstrip()
+    prev: int = 0
+    x: int = 0
+    output: int = 0
+    for right in range(len(sequence)):
+        if sequence[right] == 'A': prev += x
+        elif sequence[right] == 'Q':
+            output += prev
+            x += 1
+    sys.stdout.write('{}\n'.format(output))
+
+
+if __name__ == '__main__':
+    solution()
