20d1d9c232cd271f6157ade02db93a06cdf1cb46
kaluginpeter <streetworkout200412@gmail.com>
2025-03-13 10:30:42 +0300

    Added description "Begin your day with a challenge, but an easy one."

---
+++ CodeWars/6kyu/Begin_your_day_with_a_challenge_but_an_easy_one.py
@@ -0,0 +1,16 @@
+# There are no explanations. You have to create the code that gives the following results in Python, Ruby, and Haskell:
+#
+# one_two_three(0) == [0, 0]
+# one_two_three(1) == [1, 1]
+# one_two_three(2) == [2, 11]
+# one_two_three(3) == [3, 111]
+# one_two_three(19) == [991, 1111111111111111111]
+# And it should give the following results in Javascript, Scala, D, Go, Rust, C, and Java:
+#
+# oneTwoThree(0) == ['0', '0']
+# oneTwoThree(1) == ['1', '1']
+# oneTwoThree(3) == ['3', '111']
+# oneTwoThree(19) == ['991', '1111111111111111111']
+# In C, the results are to be assigned to seperate pointers.
+#
+# Puzzles
