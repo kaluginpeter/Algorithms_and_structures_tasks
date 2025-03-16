7ccf1526f668cbf250915739c20a51b066bf7d25
kaluginpeter <streetworkout200412@gmail.com>
2025-03-16 09:54:46 +0300

    Added description "Prime factorization"

---
+++ CodeWars/6kyu/Prime_factorization.py
@@ -0,0 +1,8 @@
+# The prime factorization of a positive integer is a list of the integer's prime factors, together with their multiplicities; the process of determining these factors is called integer factorization. The fundamental theorem of arithmetic says that every positive integer has a single unique prime factorization.
+#
+# The prime factorization of 24, for instance, is (2^3) * (3^1).
+#
+# Write a class called PrimeFactorizer (inheriting directly from object) whose constructor accepts exactly 1 int and has a property factor containing a dictionary where the keys are prime numbers and the values are the multiplicities.
+#
+# PrimeFactorizer(24).factor #should return { 2: 3, 3: 1 }
+# Algorithms
