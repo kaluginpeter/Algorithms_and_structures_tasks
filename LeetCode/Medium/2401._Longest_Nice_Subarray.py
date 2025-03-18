1eddbc8c78de4efdbda0c9caa2680c4c778f3c91
kaluginpeter <streetworkout200412@gmail.com>
2025-03-18 10:40:21 +0300

    Added solution "2401. Longest Nice Subarray"

--- LeetCode/Medium/2401._Longest_Nice_Subarray.py
+++ LeetCode/Medium/2401._Longest_Nice_Subarray.py
@@ -30,3 +30,87 @@
 #
 # 1 <= nums.length <= 105
 # 1 <= nums[i] <= 109
+# Solution
+# Python O(N) O(1) BitManipulation SlidingWindow
+from collections.abc import Iterator
+import math
+
+
+class Solution:
+    def get_bound(self, n: int) -> int:
+        if n == 0: return 1
+        return n.bit_length()
+
+    def get_bits(self, n: int) -> Iterator[int]:
+        while n:
+            yield n % 2
+            n //= 2
+
+    def add(self, hashmap: list[int], n: int) -> None:
+        bound: int = self.get_bound(n)
+        bits: Iterator[int] = self.get_bits(n)
+        for idx in range(bound):
+            hashmap[idx] |= next(bits)
+
+    def subtract(self, hashmap: list[int], n: int) -> None:
+        bound: int = self.get_bound(n)
+        bits: Iterator[int] = self.get_bits(n)
+        for idx in range(bound):
+            if next(bits): hashmap[idx] = 0
+
+    def can_add(self, hashmap: list[int], n: int) -> bool:
+        bound: int = self.get_bound(n)
+        bits: Iterator[int] = self.get_bits(n)
+        for idx in range(bound):
+            if hashmap[idx] & next(bits):  return False
+        return True
+
+    def longestNiceSubarray(self, nums: List[int]) -> int:
+        hashmap: list[int] = [0] * 32
+        max_len: int = 0
+        left: int = 0
+        for right in range(len(nums)):
+            while not self.can_add(hashmap, nums[right]):
+                self.subtract(hashmap, nums[left])
+                left += 1
+            self.add(hashmap, nums[right])
+            max_len = max(max_len, right - left + 1)
+        return max_len
+
+# C++ O(N) O(1) BitManipulation SlidingWindow
+class Solution {
+public:
+    bool canAdd(bitset<32>& hashmap, int& n) {
+        bitset<32> nBits (n);
+        for (int idx = 0; idx < 32; ++idx) {
+            if (hashmap[idx] & nBits[idx]) return false;
+        }
+        return true;
+    }
+    void subtract(bitset<32>& hashmap, int& n) {
+        bitset<32> nBits (n);
+        for (int idx = 0; idx < 32; ++idx) {
+            if (nBits[idx]) hashmap[idx] = 0;
+        }
+    }
+    void add(bitset<32>& hashmap, int& n) {
+        bitset<32> nBits (n);
+        for (int idx = 0; idx < 32; ++idx) {
+            if (nBits[idx]) hashmap[idx] = 1;
+        }
+    }
+    int longestNiceSubarray(vector<int>& nums) {
+        int maxLen = 0;
+        int left = 0;
+        bitset<32> hashmap;
+        for (int right = 0; right < nums.size(); ++right) {
+            while (!canAdd(hashmap, nums[right])) {
+                subtract(hashmap, nums[left]);
+                ++left;
+            }
+            add(hashmap, nums[right]);
+            maxLen = max(maxLen, right - left + 1);
+        }
+        return maxLen;
+    }
+};
