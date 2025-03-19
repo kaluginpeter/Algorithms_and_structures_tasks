e33bd221bb6fb6b8c7be9fbdd7c90646cbdb29e4
kaluginpeter <streetworkout200412@gmail.com>
2025-03-19 11:08:48 +0300

    Added description "Score From Permutations Of Combinations of an Integer"

---
+++ CodeWars/6kyu/Score_From_Permutations_Of_Combinations_of_an_Integer.py
@@ -0,0 +1,16 @@
+# We define the score of permutations of combinations, of an integer number (the function to obtain this value:sc_perm_comb) as the total sum of all the numbers obtained from the permutations of all the possible combinations of its digits. For example we have the number 348.
+#
+# sc_perm_comb(348) = 3 + 4 + 8 + 34 + 38 + 48 + 43 + 83 + 84 + 348 + 384 + 834 + 843 + 438 + 483  = 3675
+# If the number has a digit 0, the numbers formed by a leading 0 should be discarded:
+#
+# sc_perm_comb(340) = 3 + 4 + 34 + 30 + 40 + 43 + 340 + 304 + 430 + 403 = 1631
+# Duplicate permutations can occur if the number has more than once the same digit, but they should be added only once in the result:
+#
+# sc_perm_comb(333) = 3 + 33 + 333 = 369
+# If the number has only one digit its score is the same number:
+#
+# sc_perm_comb(6) = 6
+# sc_perm_comb(0) = 0
+# Enjoy it!!
+#
+# FundamentalsMathematicsPermutations
