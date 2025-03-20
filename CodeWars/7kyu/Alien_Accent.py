fa894e6d45d992494750ac2c9f24dfd2602e91b5
kaluginpeter <streetworkout200412@gmail.com>
2025-03-20 16:17:01 +0300

    Added description "Alien Accent"

---
+++ CodeWars/7kyu/Alien_Accent.py
@@ -0,0 +1,9 @@
+# The Story:
+# Aliens from Kepler 27b have immigrated to Earth! They have learned English and go to our stores, eat our food, dress like us, ride Ubers, use Google, etc. However, they speak English a little differently. Can you write a program that converts our English to their Alien English?
+#
+# Task:
+# Write a function that receives a lowercase string and converts it from our English to Alien English. They tend to speak the letter a like o and o like a u.
+#
+# "hello" ---> "hellu"
+# "codewars" ---> "cudewors"
+# StringsFundamentals
