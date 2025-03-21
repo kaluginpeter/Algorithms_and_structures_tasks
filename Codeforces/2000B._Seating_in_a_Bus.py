f73e3e167a963efa4bbdfb2d0d4e4575e3a8d765
kaluginpeter <streetworkout200412@gmail.com>
2025-03-21 13:40:05 +0300

    Added solution "2000B. Seating in a Bus"

--- Codeforces/2000B._Seating_in_a_Bus.py
+++ Codeforces/2000B._Seating_in_a_Bus.py
@@ -75,3 +75,62 @@
 # NO
 # Note
 # The first test case is explained in the problem statement.
+# Solution
+# C++ O(N) O(1) Two Pointers
+#include <iostream>
+
+
+void solution() {
+    int t;
+    std::scanf("%d", &t);
+    for (int i = 0; i < t; ++i) {
+        int n;
+        std::scanf("%d", &n);
+        int left = 0;
+        int right = 0;
+        bool isValid = true;
+        for (int idx = 0; idx < n; ++idx) {
+            int seat;
+            std::scanf("%d", &seat);
+            if (!idx) {
+                left = seat;
+                right = seat;
+                continue;
+            }
+            if ((seat != left - 1) && (seat != right + 1)) isValid = false;
+            else if (seat == left - 1) left = seat;
+            else right = seat;
+        }
+        std::cout << (isValid? "YES" : "NO") << "\n";
+    }
+}
+
+
+int main() {
+    solution();
+}
+# Python O(N) O(1) TwoPointers
+import sys
+
+
+def solution() -> None:
+    t: int = int(sys.stdin.readline().rstrip())
+    for _ in range(t):
+        n: int = int(sys.stdin.readline().rstrip())
+        nums: iter[int] = map(int, sys.stdin.readline().rstrip().split())
+        left = right = 0
+        is_valid: bool = True
+        for idx in range(n):
+            seat: int = next(nums)
+            if not idx:
+                left = right = seat
+                continue
+            elif seat != left - 1 and seat != right + 1:
+                is_valid = False
+            elif seat == left - 1: left -= 1
+            else: right += 1
+        sys.stdout.write('{}\n'.format('YES' if is_valid else 'NO'))
+
+
+if __name__ == '__main__':
+    solution()
