fa9585a47d0c6d2fbe1159487fa429feabb84f46
kaluginpeter <streetworkout200412@gmail.com>
2025-03-22 12:27:49 +0300

    Added description "Count number of zeros from 1 to N"

---
+++ CodeWars/7kyu/Count_number_of_zeros_from_1_to_N.py
@@ -0,0 +1,8 @@
+# Create an algorithm to count the number of zeros that appear between 1 and N.
+#
+# Examples
+#  10  -->   1  // 10
+#  20  -->   2  // 10, 20
+# 100  -->  11  // 10, 20, 30, 40, 50, 60, 70, 80, 90, 100
+# 200  -->  31
+# Algorithms
