be797aaf527d4c933914e4012f134b6e0c445e44
kaluginpeter <streetworkout200412@gmail.com>
2025-03-23 08:35:54 +0300

    Added solution "1976. Number of Ways to Arrive at Destination"

--- LeetCode/Medium/1976._Number_of_Ways_to_Arrive_at_Destination.py
+++ LeetCode/Medium/1976._Number_of_Ways_to_Arrive_at_Destination.py
@@ -34,3 +34,66 @@
 # ui != vi
 # There is at most one road connecting any two intersections.
 # You can reach any intersection from any other intersection.
+# Solution
+# Python O(|E|log(|V|)) O(|V|) Dijkstra ShortestPath DynamicProgramming Graph
+class Solution:
+    def countPaths(self, n: int, roads: List[List[int]]) -> int:
+        MOD: int = 1000000007
+        adj_list: list[list[tuple[int, int]]] = [[] for _ in range(n)]
+        for u, v, w in roads:
+            adj_list[u].append((v, w))
+            adj_list[v].append((u, w))
+        path_count: list[int] = [0] * n
+        path_count[0] = 1
+        path_cost: list[int] = [float('inf')] * n
+        path_cost[0] = 0
+        min_heap: list[tuple[int, int]] = [(0, 0)]
+        while min_heap:
+            cur_cost, vertex = heapq.heappop(min_heap)
+            for neighbor, weight in adj_list[vertex]:
+                next_cost: int = cur_cost + weight
+                if next_cost < path_cost[neighbor]:
+                    path_cost[neighbor] = next_cost
+                    path_count[neighbor] = path_count[vertex]
+                    heapq.heappush(min_heap, (next_cost, neighbor))
+                elif next_cost == path_cost[neighbor]:
+                    path_count[neighbor] = (path_count[neighbor] + path_count[vertex]) % MOD
+        return path_count[n - 1]
+
+# C++ O(|E|log(|V|)) O(|V|) Graph Dijkstra DynamicProgramming
+class Solution {
+public:
+    int countPaths(int n, vector<vector<int>>& roads) {
+        long long MOD = 1000000007;
+        vector<vector<pair<int, int>>> adjList (n, vector<pair<int, int>>());
+        for (vector<int>& road : roads) {
+            int& u = road[0];
+            int& v = road[1];
+            int& w = road[2];
+            adjList[u].push_back({v, w});
+            adjList[v].push_back({u, w});
+        }
+        vector<long long> pathCount (n, 0);
+        pathCount[0] = 1;
+        vector<long long> costVertex (n, 1e12);
+        costVertex[0] = 0;
+        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> minHeap;
+        minHeap.push({0, 0});
+        while (!minHeap.empty()) {
+            long long curCost = minHeap.top().first;
+            int vertex = minHeap.top().second;
+            minHeap.pop();
+            for (pair<int, int>& edge : adjList[vertex]) {
+                long long nextCost = curCost + edge.second;
+                if (nextCost < costVertex[edge.first]) {
+                    costVertex[edge.first] = nextCost;
+                    pathCount[edge.first] = pathCount[vertex];
+                    minHeap.push({nextCost, edge.first});
+                } else if (nextCost == costVertex[edge.first]) {
+                    pathCount[edge.first] = (pathCount[edge.first] + pathCount[vertex]) % MOD;
+                }
+            }
+        }
+        return pathCount[n - 1];
+    }
+};
