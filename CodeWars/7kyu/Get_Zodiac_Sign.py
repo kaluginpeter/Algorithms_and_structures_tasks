0ffa06cd479e7330ac6d979c161dd3d5f438e1bc
kaluginpeter <streetworkout200412@gmail.com>
2025-03-25 10:20:10 +0300

    Added solution "Get Zodiac Sign"

--- CodeWars/7kyu/Get_Zodiac_Sign.py
+++ CodeWars/7kyu/Get_Zodiac_Sign.py
@@ -24,3 +24,17 @@
 # Aquarius (January 20 to February 18)
 # Pisces (February 19 to March 20)
 # Fundamentals
+# Solution spaghetti code
+def get_zodiac_sign(day, month):
+    if (month >= 12 and day >= 22) or (month == 1 and day <= 19): return SIGNS[0]
+    elif (month == 1 and day >= 20) or (month == 2 and day <= 18): return SIGNS[1]
+    elif (month == 2 and day >= 19) or (month == 3 and day <= 20): return SIGNS[2]
+    elif (month == 3 and day >= 21) or (month == 4 and day <= 19): return SIGNS[3]
+    elif (month == 4 and day >= 20) or (month == 5 and day <= 20): return SIGNS[4]
+    elif (month == 5 and day >= 21) or (month == 6 and day <= 20): return SIGNS[5]
+    elif (month == 6 and day >= 21) or (month == 7 and day <= 22): return SIGNS[6]
+    elif (month == 7 and day >= 23) or (month == 8 and day <= 22): return SIGNS[7]
+    elif (month == 8 and day >= 23) or (month == 9 and day <= 22): return SIGNS[8]
+    elif (month == 9 and day >= 23) or (month == 10 and day <= 22): return SIGNS[9]
+    elif (month == 10 and day >= 23) or (month == 11 and day <= 21): return SIGNS[10]
+    elif (month == 11 and day >= 22) or (month == 12 and day <= 21): return SIGNS[11]
