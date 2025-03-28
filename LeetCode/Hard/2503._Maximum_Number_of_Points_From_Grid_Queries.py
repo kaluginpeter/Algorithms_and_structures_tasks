2b69d721ffc93b1af8b0424a2729adccf082bfa2
kaluginpeter <streetworkout200412@gmail.com>
2025-03-28 12:03:45 +0300

    Added solution "2503. Maximum Number of Points From Grid Queries"

--- LeetCode/Hard/2503._Maximum_Number_of_Points_From_Grid_Queries.py
+++ LeetCode/Hard/2503._Maximum_Number_of_Points_From_Grid_Queries.py
@@ -33,3 +33,147 @@
 # k == queries.length
 # 1 <= k <= 104
 # 1 <= grid[i][j], queries[i] <= 106
+# Solution
+# Python O(QlogQ + NM) O(Q + NM) Breadth-First-Search Memoization Matrix
+class Solution:
+    moves: list[tuple[int, int]] = [
+        (1, 0), (-1, 0), (0, 1), (0, -1)
+    ]
+
+    def get_path(self, bound: int, grid: list[list[int]], seen: list[list[bool]], last: list[tuple[int, int]]) -> int:
+        n, m = len(grid), len(grid[0])
+        cells: int = 0
+        q: list[tuple[int, int]] = []
+        i, k = 0, len(last) - 1
+        while i <= k:
+            if grid[last[i][0]][last[i][1]] < bound:
+                q.append(last[i])
+                last[i] = last[k]
+                last.pop()
+                k -= 1
+                cells += 1
+            else: i += 1
+        while q:
+            cur_row, cur_col = q.pop()
+            for x, y in self.moves:
+                next_row, next_col = cur_row + x, cur_col + y
+                if (
+                    (0 <= next_row < n) and (0 <= next_col < m)
+                    and (not seen[next_row][next_col])
+                ):
+                    if grid[next_row][next_col] >= bound:
+                        last.append((next_row, next_col))
+                    else:
+                        q.append((next_row, next_col))
+                        cells += 1
+                    seen[next_row][next_col] = True
+        return cells
+
+    def maxPoints(self, grid: List[List[int]], queries: List[int]) -> List[int]:
+        q: int = len(queries)
+        query_to_index: dict[int, list[int]] = dict()
+        for i in range(q):
+            if queries[i] not in query_to_index:
+                query_to_index[queries[i]] = []
+            query_to_index[queries[i]].append(i)
+        queries.sort()
+        seen: list[list[bool]] = [[False] * len(grid[0]) for _ in range(len(grid))]
+        last: list[tuple[int, int]] = []
+        capacity: int = len(grid) * len(grid[0])
+        prev: int = 0
+        output: list[int] = [0] * q
+        i: int = 0
+        while i < q:
+            if not seen[0][0] and grid[0][0] < queries[i]:
+                seen[0][0] = True
+                last.append((0, 0))
+            result = self.get_path(queries[i], grid, seen, last)
+            output[query_to_index[queries[i]][-1]] = result + prev
+            query_to_index[queries[i]].pop()
+            i += 1
+            while i < q and ((queries[i] == queries[i - 1]) or (result + prev == capacity)):
+                output[query_to_index[queries[i]][-1]] = result + prev
+                query_to_index[queries[i]].pop()
+                i += 1
+            prev += result
+        return output
+
+# C++ O(QlogQ + NM) O(Q + NM) Matrix Memoization Breadth-First-Search
+class Solution {
+private:
+    vector<pair<int, int>> moves = {
+        {1, 0}, {0, 1}, {0, -1}, {-1, 0}
+    };
+public:
+    int getPath (int& bound, vector<vector<int>>& grid, vector<vector<bool>>& seen, vector<tuple<int, int>>& last) {
+        int n = grid.size(), m = grid[0].size();
+        vector<tuple<int, int>> q;
+        int cells = 0;
+        int k = last.size() - 1;
+        int i = 0;
+        while (i <= k) {
+            if (grid[get<0>(last[i])][get<1>(last[i])] < bound) {
+                q.push_back(tuple<int, int>(last[i]));
+                ++cells;
+                last[i] = last[k];
+                last.pop_back();
+                --k;
+            } else ++i;
+        }
+        while (!q.empty()) {
+            int curRow = get<0>(q.back());
+            int curCol = get<1>(q.back());
+            q.pop_back();
+            for (pair<int, int>& move : moves) {
+                int nextRow = curRow + move.first;
+                int nextCol = curCol + move.second;
+                if (
+                    (nextRow >= 0 && nextRow < n)
+                    && (nextCol >= 0 && nextCol < m)
+                    && (!seen[nextRow][nextCol])
+                ) {
+                    if (grid[nextRow][nextCol] >= bound) {
+                        last.push_back({nextRow, nextCol});
+                        seen[nextRow][nextCol] = true;
+                        continue;
+                    }
+                    ++cells;
+                    seen[nextRow][nextCol] = true;
+                    q.push_back({nextRow, nextCol});
+                }
+            }
+        }
+        return cells;
+    }
+    vector<int> maxPoints(vector<vector<int>>& grid, vector<int>& queries) {
+        int q = queries.size();
+        unordered_map<int, vector<int>> queryIndex;
+        for (int i = 0; i < q; ++i) {
+            queryIndex[queries[i]].push_back(i);
+        }
+        sort(queries.begin(), queries.end());
+        int capacity = grid.size() * grid[0].size();
+        vector<vector<bool>> seen (grid.size(), vector<bool>(grid[0].size(), false));
+        vector<int> output (q, 0);
+        vector<tuple<int, int>> last;
+        size_t i = 0;
+        int prev = 0;
+        while (i < q) {
+            if (!seen[0][0] && grid[0][0] < queries[i]) {
+                seen[0][0] = true;
+                last.push_back({0, 0});
+            }
+            int result = getPath(queries[i], grid, seen, last);
+            output[queryIndex[queries[i]].back()] = result + prev;
+            queryIndex[queries[i]].pop_back();
+            ++i;
+            while (i < q && ((queries[i] == queries[i - 1]) || (result + prev == capacity))) {
+                output[queryIndex[queries[i]].back()] = result + prev;
+                queryIndex[queries[i]].pop_back();
+                ++i;
+            }
+            prev += result;
+        }
+        return output;
+    }
+};
