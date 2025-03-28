2d37ca7636d4dea9edb6d599cc9d6294a54c6118
kaluginpeter <streetworkout200412@gmail.com>
2025-03-28 12:46:32 +0300

    Added solution "Chess Fun #9: Bishops And Rooks"

--- CodeWars/5kyu/Chess_Fun_9_Bishops_And_Rooks.py
+++ CodeWars/5kyu/Chess_Fun_9_Bishops_And_Rooks.py
@@ -13,3 +13,51 @@
 # number of safe squares on the board.
 #
 # Puzzles
+# Solution
+def bishops_and_rooks(M):
+    n, m = len(M), len(M[0])
+    for i in range(n):
+        for j in range(m):
+            if M[i][j] == -1:
+                x, y = i - 1, j - 1
+                while x >= 0 and y >= 0:
+                    if M[x][y] not in {0, 2}: break
+                    M[x][y] = 2
+                    x, y = x - 1, y - 1
+                x, y = i - 1, j + 1
+                while x >= 0 and y < m:
+                    if M[x][y] not in {0, 2}: break
+                    M[x][y] = 2
+                    x, y = x - 1, y + 1
+                x, y = i + 1, j - 1
+                while x < n and y >= 0:
+                    if M[x][y] not in {0, 2}: break
+                    M[x][y] = 2
+                    x, y = x + 1, y - 1
+                x, y = i + 1, j + 1
+                while x < n and y < m:
+                    if M[x][y] not in {0, 2}: break
+                    M[x][y] = 2
+                    x, y = x + 1, y + 1
+            elif M[i][j] == 1:
+                x = i - 1
+                while x >= 0:
+                    if M[x][j] not in {0, 2}: break
+                    M[x][j] = 2
+                    x -= 1
+                x = i + 1
+                while x < n:
+                    if M[x][j] not in {0, 2}: break
+                    M[x][j] = 2
+                    x += 1
+                y = j - 1
+                while y >= 0:
+                    if M[i][y] not in {0, 2}: break
+                    M[i][y] = 2
+                    y -= 1
+                y = j + 1
+                while y < m:
+                    if M[i][y] not in {0, 2}: break
+                    M[i][y] = 2
+                    y += 1
+    return sum(sum(not cell for cell in row) for row in M)
