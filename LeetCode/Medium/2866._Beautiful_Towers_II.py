afb79a8b14dc08f9b10b740901a314fe6d347f93
kaluginpeter <streetworkout200412@gmail.com>
2025-04-02 23:14:12 +0300

    Added solution "2866. Beautiful Towers II"

--- LeetCode/Medium/2866._Beautiful_Towers_II.py
+++ LeetCode/Medium/2866._Beautiful_Towers_II.py
@@ -45,3 +45,91 @@
 #
 # 1 <= n == maxHeights.length <= 105
 # 1 <= maxHeights[i] <= 109
+# Solution
+# Python O(N) O(N) Monotonic Stack Stack
+class Solution:
+    def maximumSumOfHeights(self, heights: List[int]) -> int:
+        n: int = len(heights)
+        cur_sum: int = 0
+        left_part: list[int] = []
+        left_mountain: list[int] = []
+        for right in range(n):
+            while left_part and heights[left_part[-1]] > heights[right]:
+                if len(left_part) > 1:
+                    cur_sum -= (left_part[-1] - left_part[-2]) * heights[left_part[-1]]
+                else:
+                    cur_sum -= (left_part[-1] + 1) * heights[left_part[-1]]
+                left_part.pop()
+            if left_part:
+                cur_sum += (right - left_part[-1]) * heights[right]
+            else:
+                cur_sum += (right + 1) * heights[right]
+            left_part.append(right)
+            left_mountain.append(cur_sum)
+
+        max_sum: int = 0
+        cur_sum = 0
+        right_part: list[int] = []
+        for left in range(n - 1, -1, -1):
+            max_sum = max(max_sum, left_mountain[left] + cur_sum)
+            while right_part and heights[right_part[-1]] > heights[left]:
+                if len(right_part) > 1:
+                    cur_sum -= (right_part[-2] - right_part[-1]) * heights[right_part[-1]]
+                else:
+                    cur_sum -= (n - right_part[-1]) * heights[right_part[-1]]
+                right_part.pop()
+            if right_part:
+                cur_sum += (right_part[-1] - left) * heights[left]
+            else:
+                cur_sum += (n - left) * heights[left]
+            right_part.append(left)
+
+        return max_sum
+
+# C++ O(N) O(N) MonotonicStack Stack
+class Solution {
+public:
+    long long maximumSumOfHeights(vector<int>& heights) {
+        long long maxSum = 0, curSum = 0;
+        vector<int> leftPart;
+        vector<long long> leftMountain;
+        int n = heights.size();
+        for (int right = 0; right < n; ++right) {
+            while (!leftPart.empty() && heights[leftPart.back()] > heights[right]) {
+                if (leftPart.size() > 1) {
+                    curSum -= (leftPart.back() - leftPart[leftPart.size() - 2]) * static_cast<long long>(heights[leftPart.back()]);
+                } else {
+                    curSum -= heights[leftPart.back()] * static_cast<long long>(leftPart.back() + 1);
+                }
+                leftPart.pop_back();
+            }
+            if (!leftPart.empty()) {
+                curSum += heights[right] * static_cast<long long>(right - leftPart.back());
+            } else {
+                curSum += heights[right] * static_cast<long long>(right + 1);
+            }
+            leftPart.push_back(right);
+            leftMountain.push_back(curSum);
+        }
+        curSum = 0;
+        vector<int> rightPart;
+        for (int left = n - 1; left >= 0; --left) {
+            maxSum = max(maxSum, leftMountain[left] + curSum);
+            while (!rightPart.empty() && heights[rightPart.back()] > heights[left]) {
+                if (rightPart.size() > 1) {
+                    curSum -= (rightPart[rightPart.size() - 2] - rightPart.back()) * static_cast<long long>(heights[rightPart.back()]);
+                } else {
+                    curSum -= (n - rightPart.back()) * static_cast<long long>(heights[rightPart.back()]);
+                }
+                rightPart.pop_back();
+            }
+            if (!rightPart.empty()) {
+                curSum += (rightPart.back() - left) * static_cast<long long>(heights[left]);
+            } else {
+                curSum += (n - left) * static_cast<long long>(heights[left]);
+            }
+            rightPart.push_back(left);
+        }
+        return maxSum;
+    }
+};
