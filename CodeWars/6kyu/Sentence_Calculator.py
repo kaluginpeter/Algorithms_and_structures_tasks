097977e58f0775550eee17bfdaa5777211f0360f
kaluginpeter <streetworkout200412@gmail.com>
2025-04-02 11:10:20 +0300

    Added description "Sentence Calculator"

---
+++ CodeWars/6kyu/Sentence_Calculator.py
@@ -0,0 +1,10 @@
+# Sentence Calculator
+# Calculate the total score (sum of the individual character scores) of a sentence given the following score rules for each allowed group of characters:
+#
+# Lower case [a-z]: 'a'=1, 'b'=2, 'c'=3, ..., 'z'=26
+# Upper case [A-Z]: 'A'=2, 'B'=4, 'C'=6, ..., 'Z'=52
+# Digits [0-9] their numeric value: '0'=0, '1'=1, '2'=2, ..., '9'=9
+# Other characters: 0 value
+# Note: input will always be a string
+#
+# Fundamentals
