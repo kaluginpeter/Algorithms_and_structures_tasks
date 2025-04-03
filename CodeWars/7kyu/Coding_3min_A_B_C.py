255b7b06f2f7627daae62b13bfa7e609eb73c0ec
kaluginpeter <streetworkout200412@gmail.com>
2025-04-03 11:48:06 +0300

    Added description "Coding 3min : A*B=C"

---
+++ CodeWars/7kyu/Coding_3min_A_B_C.py
@@ -0,0 +1,37 @@
+# This is the simple version of Fastest Code series. If you need some challenges, please try the Performance version
+#
+# Task:
+# Give you a number array numbers and a number c.
+#
+# Find out a pair of numbers(we called them number a and number b) from the array numbers, let a*b=c, result format is an array [a,b]
+#
+# The array numbers is a sorted array, value range: -100...100
+#
+# The result will be the first pair of numbers, for example,findAB([1,2,3,4,5,6],6) should return [1,6], instead of [2,3]
+#
+# Please see more example in testcases.
+#
+# Series:
+# Bug in Apple
+# Father and Son
+# Jumping Dutch act
+# Planting Trees
+# Give me the equation
+# Find the murderer
+# Reading a Book
+# Eat watermelon
+# Special factor
+# Guess the Hat
+# Symmetric Sort
+# Are they symmetrical?
+# Max Value
+# Trypophobia
+# Virus in Apple
+# Balance Attraction
+# Remove screws I
+# Remove screws II
+# Regular expression compression
+# Collatz Array(Split or merge)
+# Tidy up the room
+# Waiting for a Bus
+# PuzzlesGames
