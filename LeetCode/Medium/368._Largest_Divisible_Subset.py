533fe0edbd74a46abe745f7d5d0ebd4b5852aeec
kaluginpeter <streetworkout200412@gmail.com>
2025-04-06 09:02:54 +0300

    Added solution "368. Largest Divisible Subset"

--- LeetCode/Medium/368._Largest_Divisible_Subset.py
+++ LeetCode/Medium/368._Largest_Divisible_Subset.py
@@ -22,3 +22,57 @@
 # 1 <= nums.length <= 1000
 # 1 <= nums[i] <= 2 * 109
 # All the integers in nums are unique.
+# Solution
+# Python O(NlogN + N^2) O(N) DynamicProgramming Math Sorting
+class Solution:
+    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        n: int = len(nums)
+        dp: list[int] = [1] * n
+        prev: list[int] = [-1] * n
+        for right in range(n):
+            for left in range(right):
+                if nums[right] % nums[left] == 0:
+                    if dp[left] + 1 > dp[right]:
+                        dp[right] = dp[left] + 1
+                        prev[right] = left
+        idx: int = 0
+        for i in range(1, n):
+            if dp[idx] < dp[i]: idx = i
+        path: list[int] = [nums[idx]]
+        while prev[idx] != -1:
+            path.append(nums[prev[idx]])
+            idx = prev[idx]
+        return path
+
+# C++ O(NlogN + N^2) O(N) DynamicProgramming Sorting Math
+class Solution {
+public:
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        vector<int> dp (n, 1);
+        vector<int> prev(n, -1);
+        for (int right = 0; right < nums.size(); ++right) {
+            for (int left = 0; left < right; ++left) {
+                if (nums[right] % nums[left] == 0) {
+                    if (dp[left] + 1 > dp[right]) {
+                        dp[right] = dp[left] + 1;
+                        prev[right] = left;
+                    }
+                }
+            }
+        }
+        int idx = 0;
+        for (int i = 1; i < n; ++i) {
+            if (dp[i] > dp[idx]) idx = i;
+        }
+        vector<int> path = {nums[idx]};
+        while (prev[idx] != -1) {
+            path.push_back(nums[prev[idx]]);
+            idx = prev[idx];
+        }
+        return path;
+    }
+
+};
