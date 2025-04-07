fbc289db4c20eb5e467534d49fd5db933152797a
kaluginpeter <streetworkout200412@gmail.com>
2025-04-07 13:58:50 +0300

    Added description "Regexp Basics - is it a six bit unsigned number?"

---
+++ CodeWars/7kyu/Regexp_Basics_is_it_a_six_bit_unsigned_number.py
@@ -0,0 +1,5 @@
+# Implement String#six_bit_number?, which should return true if given object is a number representable by 6 bit unsigned integer (0-63), false otherwise.
+#
+# It should only accept numbers in canonical representation, so no leading +, extra 0s, spaces etc.
+#
+# Regular ExpressionsFundamentals
