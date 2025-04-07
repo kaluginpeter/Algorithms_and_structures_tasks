307cc1604eb8e227f435a159db5c14892b2e9910
kaluginpeter <streetworkout200412@gmail.com>
2025-04-07 12:22:12 +0300

    Added solution "1981. Minimize the Difference Between Target and Chosen Elements"

--- LeetCode/Medium/1981._Minimize_the_Difference_Between_Target_and_Chosen_Elements.py
+++ LeetCode/Medium/1981._Minimize_the_Difference_Between_Target_and_Chosen_Elements.py
@@ -44,3 +44,41 @@
 # 1 <= m, n <= 70
 # 1 <= mat[i][j] <= 70
 # 1 <= target <= 800
+# Solution
+# Python O(NMK) O(K) DynamicProgramming Matrix
+class Solution:
+    def minimizeTheDifference(self, mat: List[List[int]], target: int) -> int:
+        sums: set[int] = {0}
+        for row in mat:
+            sums = {prev_sum + num for num in row for prev_sum in sums}
+        return min(abs(target - mat_sum) for mat_sum in sums)
+
+# C++ O(NMK) O(K) DynamicProgramming Matrix
+class Solution {
+public:
+    int minimizeTheDifference(vector<vector<int>>& mat, int target) {
+        int n = mat.size(), m = mat[0].size(), K = 4901;
+        vector<bool> prevDp (K, false);
+        vector<bool> curDp (K, false);
+        prevDp[0] = true;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                for (int k = K - mat[i][j] - 1; k >= 0; --k) {
+                    if (prevDp[k]) {
+                        curDp[k + mat[i][j]] = true;;
+                    }
+                }
+            }
+            prevDp = curDp;
+            curDp = vector<bool> (K, false);
+        }
+        int minDiff = INT32_MAX;
+        for (int k = 0; k < K; ++k) {
+            if (prevDp[k]) {
+                int diff = abs(target - k);
+                minDiff = min(minDiff, diff);
+            }
+        }
+        return minDiff;
+    }
+};
