5b1a72de4ed55dd6e2d3058198693e37a0ac2f1f
kaluginpeter <streetworkout200412@gmail.com>
2025-04-08 11:32:46 +0300

    Added solution "Regexp Basics - is it all whitespace?"

--- CodeWars/7kyu/Regexp_Basics_is_it_all_whitespace.py
+++ CodeWars/7kyu/Regexp_Basics_is_it_all_whitespace.py
@@ -1,3 +1,13 @@
 # Implement String#whitespace?(str) (Ruby), String.prototype.whitespace(str) (JavaScript), String::whitespace(str) (CoffeeScript), or whitespace(str) (Python), which should return true/True if given object consists exclusively of zero or more whitespace characters, false/False otherwise.
 #
 # Regular ExpressionsFundamentals
+# Solution
+def whitespace(string):
+    idx: int = 0
+    while idx < len(string):
+        if string[idx:].startswith(' '): idx += 1
+        elif string[idx:].startswith('\n'): idx += 1
+        elif string[idx:].startswith('\r'): idx += 1
+        elif string[idx:].startswith('\t'): idx += 1
+        else: return False
+    return True
