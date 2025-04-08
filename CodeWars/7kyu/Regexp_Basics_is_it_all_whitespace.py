c7701b7888bdeede9002e17961549b172844b65d
kaluginpeter <streetworkout200412@gmail.com>
2025-04-08 11:32:24 +0300

    Added description "Regexp Basics - is it all whitespace?"

---
+++ CodeWars/7kyu/Regexp_Basics_is_it_all_whitespace.py
@@ -0,0 +1,3 @@
+# Implement String#whitespace?(str) (Ruby), String.prototype.whitespace(str) (JavaScript), String::whitespace(str) (CoffeeScript), or whitespace(str) (Python), which should return true/True if given object consists exclusively of zero or more whitespace characters, false/False otherwise.
+#
+# Regular ExpressionsFundamentals
