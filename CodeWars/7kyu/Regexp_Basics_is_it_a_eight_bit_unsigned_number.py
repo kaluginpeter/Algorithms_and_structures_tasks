e03532e15d8b2d017325a75e0b94edd73904c1df
kaluginpeter <streetworkout200412@gmail.com>
2025-04-09 11:28:48 +0300

    Added solution "Regexp Basics - is it a eight bit unsigned number?"

--- CodeWars/7kyu/Regexp_Basics_is_it_a_eight_bit_unsigned_number.py
+++ CodeWars/7kyu/Regexp_Basics_is_it_a_eight_bit_unsigned_number.py
@@ -3,3 +3,6 @@
 # It should only accept numbers in canonical representation, so no leading +, extra 0s, spaces etc.
 #
 # Regular ExpressionsFundamentals
+# Solution
+def eight_bit_number(n):
+    return bool(n) and n.isdigit() and 0 <= int(n) <= 255 and not n.startswith('+') and not (n.startswith('0') and len(n) > 1)
