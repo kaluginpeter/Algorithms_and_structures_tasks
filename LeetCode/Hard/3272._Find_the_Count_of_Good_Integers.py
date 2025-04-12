e96317c6689ef7db3c0408a1081d007a9520c9a9
kaluginpeter <streetworkout200412@gmail.com>
2025-04-12 09:49:39 +0300

    Added solution "3272. Find the Count of Good Integers"

--- LeetCode/Hard/3272._Find_the_Count_of_Good_Integers.py
+++ LeetCode/Hard/3272._Find_the_Count_of_Good_Integers.py
@@ -46,3 +46,56 @@
 #
 # 1 <= n <= 10
 # 1 <= k <= 9
+# Solution
+# Python O(NlogN * 10^M) O(N * 10^M) Math Combinatorics
+class Solution:
+    def countGoodIntegers(self, n: int, k: int) -> int:
+        hashset: set[str] = set()
+        base: int = 10 ** ((n - 1) // 2)
+        odd: int = n & 1
+        for num in range(base, base * 10):
+            str_num: str = str(num)
+            str_num += str_num[-1 + -odd::-1]
+            palindrom: int = int(str_num)
+            if palindrom % k == 0:
+                hashset.add(''.join(sorted(str_num)))
+        prefix_sum: list[int] = [1] * (n + 1)
+        count: int = 0
+        for i in range(1, n + 1): prefix_sum[i] = prefix_sum[i - 1] * i
+        for s in hashset:
+            digits: list[int] = [0] * 10
+            for d in s: digits[ord(d) - 48] += 1
+            part: int = (n - digits[0]) * prefix_sum[n - 1]
+            for d in digits: part //= prefix_sum[d]
+            count += part
+        return count
+
+# C++ O(NlogN * 10^M) O(N * 10^M) Math Combinatorics
+class Solution {
+public:
+    long long countGoodIntegers(int n, int k) {
+        unordered_set<string> hashset;
+        int base = pow(10, (n - 1) / 2);
+        int odd = n & 1;
+        for (int num = base; num < base * 10; ++num) {
+            string strNum = to_string(num);
+            strNum += string(strNum.rbegin() + odd, strNum.rend());
+            long long palindrom = stoll(strNum);
+            if (palindrom % k == 0) {
+                sort(strNum.begin(), strNum.end());
+                hashset.emplace(strNum);
+            }
+        }
+        vector<long long> prefixSum(n + 1, 1);
+        long long count = 0;
+        for (int i = 1; i <= n; i++) prefixSum[i] = prefixSum[i - 1] * i;
+        for (const string &s : hashset) {
+            vector<int> digits(10);
+            for (char d : s) ++digits[d - '0'];
+            long long part = (n - digits[0]) * prefixSum[n - 1];
+            for (int d : digits) part /= prefixSum[d];
+            count += part;
+        }
+        return count;
+    }
+};
