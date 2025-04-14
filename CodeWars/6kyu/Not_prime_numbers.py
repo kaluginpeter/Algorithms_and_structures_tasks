3389cc35e4bb29616635bd5eef9f526dcbc9c799
kaluginpeter <streetworkout200412@gmail.com>
2025-04-14 09:06:55 +0300

    Added solution "Not prime numbers"

--- CodeWars/6kyu/Not_prime_numbers.py
+++ CodeWars/6kyu/Not_prime_numbers.py
@@ -15,3 +15,29 @@
 # Find your caterer
 #
 # FundamentalsPerformanceAlgorithms
+# Solution
+nums: set[int] = set()
+
+def not_prime(n: int) -> bool:
+    bound: int = int(n**.5) + 1
+    return any(n % d == 0 for d in range(2, bound))
+
+def not_primes(a, b):
+    output: list[int] = []
+    primes: set[int] = {2, 3, 5, 7}
+    for i in range(a, b):
+        if i in nums:
+            output.append(i)
+            continue
+        x: int = i
+        is_valid: bool = True
+        while i:
+            if i % 10 not in primes:
+                is_valid = False
+                break
+            i //= 10
+        if is_valid:
+            if not_prime(x):
+                output.append(x)
+                nums.add(x)
+    return output
