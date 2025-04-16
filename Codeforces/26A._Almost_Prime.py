5b7f18df3667cc448b56a2c69e7e0ac4d37fc603
kaluginpeter <streetworkout200412@gmail.com>
2025-04-16 07:36:56 +0300

    Added description "26A. Almost Prime"

---
+++ Codeforces/26A._Almost_Prime.py
@@ -0,0 +1,20 @@
+# A. Almost Prime
+# time limit per test2 seconds
+# memory limit per test256 megabytes
+# A number is called almost prime if it has exactly two distinct prime divisors. For example, numbers 6, 18, 24 are almost prime, while 4, 8, 9, 42 are not. Find the amount of almost prime numbers which are between 1 and n, inclusive.
+#
+# Input
+# Input contains one integer number n (1 ≤ n ≤ 3000).
+#
+# Output
+# Output the amount of almost prime numbers between 1 and n, inclusive.
+#
+# Examples
+# InputCopy
+# 10
+# OutputCopy
+# 2
+# InputCopy
+# 21
+# OutputCopy
+# 8
