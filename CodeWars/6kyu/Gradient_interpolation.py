e6a8255c2bda4065319c476cbc17257835449efd
kaluginpeter <streetworkout200412@gmail.com>
2025-04-16 07:50:04 +0300

    Added solution "Gradient interpolation"

--- CodeWars/6kyu/Gradient_interpolation.py
+++ CodeWars/6kyu/Gradient_interpolation.py
@@ -3,3 +3,11 @@
 # Your task is to implement a higher-order function that takes two colors in RGB format, and returns another function representing a linear gradient which interpolates between them using an integer (lying in range [0; 100]) provided as an input and returns this interpolated color. The resulting RGB components must be rounded down.
 #
 # Fundamentals
+# Solution
+def gradient(c1, c2):
+    def inner(n: int) -> tuple[int, int, int]:
+        r = c1[0] + (c2[0] - c1[0]) * n // 100
+        g = c1[1] + (c2[1] - c1[1]) * n // 100
+        b = c1[2] + (c2[2] - c1[2]) * n // 100
+        return (r, g, b)
+    return inner
