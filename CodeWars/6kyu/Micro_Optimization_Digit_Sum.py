b791eb1c9795cf160763da06a34026ee3c18a166
kaluginpeter <streetworkout200412@gmail.com>
2025-04-21 08:27:22 +0300

    Added description "Micro Optimization: Digit Sum"

---
+++ CodeWars/6kyu/Micro_Optimization_Digit_Sum.py
@@ -0,0 +1,10 @@
+# You wrote a program that can calculate the sum of all the digits of a non-negative integer.
+#
+# However, it's not fast enough. Can you make it faster?
+#
+# Input size
+# Python:
+#
+# 50 random tests of n <= 2^64
+# 50 random tests of n <= 2^120000
+# Fundamentals
