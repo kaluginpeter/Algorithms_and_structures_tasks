8e9395c35b3b057d663ecc135412dd57a4cbb360
kaluginpeter <streetworkout200412@gmail.com>
2025-04-24 09:29:45 +0300

    Added solution "1814A. Coins"

--- Codeforces/1814A._Coins.py
+++ Codeforces/1814A._Coins.py
@@ -52,3 +52,43 @@
 #
 # In the fourth test case, you can take one coin with denomination k=8
 # .
+# Solution
+# C++ O(1) O(1) Math
+#include <iostream>
+
+
+void solution() {
+    int t;
+    std::scanf("%d", &t);
+    for (int i = 0; i < t; ++i) {
+        long long n, k;
+        std::scanf("%lld %lld", &n, &k);
+        if (k & 1) {
+            std::cout << (k == 1 || n >= 0? "YES" : "NO") << "\n";
+        } else {
+            std::cout << (n % 2 == 0? "YES" : "NO") << "\n";
+        }
+    }
+}
+
+
+int main() {
+    solution();
+}
+
+# Python O(1) O(1) Math
+import sys
+
+
+def solution() -> None:
+    t: int = int(sys.stdin.readline().rstrip())
+    for _ in range(t):
+        n, k = map(int, sys.stdin.readline().rstrip().split())
+        if k & 1:
+            sys.stdout.write('{}\n'.format(['NO', 'YES'][k & 1 or n >= 0]))
+        else:
+            sys.stdout.write('{}\n'.format(['NO', 'YES'][n % 2 == 0]))
+
+
+if __name__ == '__main__':
+    solution()
