450173e44aa446f0e0b721aa00866343a3674793
kaluginpeter <streetworkout200412@gmail.com>
2025-04-26 12:10:57 +0300

    Added solution "2444. Count Subarrays With Fixed Bounds"

--- LeetCode/Hard/2444._Count_Subarrays_With_Fixed_Bounds.py
+++ LeetCode/Hard/2444._Count_Subarrays_With_Fixed_Bounds.py
@@ -26,3 +26,69 @@
 #
 # 2 <= nums.length <= 105
 # 1 <= nums[i], minK, maxK <= 106
+# Solution
+# Python O(N) O(N) SlidingWindow
+class Solution:
+    def countSubarrays(self, nums: List[int], minK: int, maxK: int) -> int:
+        output: int = 0
+        left: int = 0
+        hashmap: dict[int, int] = dict()
+        already_valid: int = 0
+        is_min: bool = False
+        is_max: bool = False
+        for right in range(len(nums)):
+            if nums[right] < minK or nums[right] > maxK:
+                already_valid = 0
+                left = right + 1
+                is_min, is_max = False, False
+                hashmap.clear()
+            else:
+                hashmap[nums[right]] = hashmap.get(nums[right], 0) + 1
+                if nums[right] == minK: is_min = True
+                if nums[right] == maxK: is_max = True
+                while is_min and is_max:
+                    hashmap[nums[left]] -= 1
+                    already_valid += 1
+                    if not hashmap[nums[left]]:
+                        if nums[left] == minK: is_min = False
+                        if nums[left] == maxK: is_max = False
+                        del hashmap[nums[left]]
+                    left += 1
+                output += already_valid
+        return output
+
+# C++ O(N) O(N) SlidingWindow
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, int minK, int maxK) {
+        long long output = 0;
+        int left = 0, alreadyValid = 0;
+        bool isMin = false, isMax = false;
+        unordered_map<int, int> hashmap;
+        for (int right = 0; right < nums.size(); ++right) {
+            if (nums[right] < minK || nums[right] > maxK) {
+                hashmap.clear();
+                alreadyValid = 0;
+                left = right + 1;
+                isMin = false;
+                isMax = false;
+            } else {
+                ++hashmap[nums[right]];
+                if (nums[right] == minK) isMin = true;
+                if (nums[right] == maxK) isMax = true;
+                while (isMin && isMax) {
+                    ++alreadyValid;
+                    --hashmap[nums[left]];
+                    if (!hashmap[nums[left]]) {
+                        if (nums[left] == minK) isMin = false;
+                        else if (nums[left] == maxK) isMax = false;
+                        hashmap.erase(nums[left]);
+                    }
+                    ++left;
+                }
+                output += alreadyValid;
+            }
+        }
+        return output;
+    }
+};
