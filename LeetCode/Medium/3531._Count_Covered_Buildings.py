7d7b1f09714164aa456098f46eefcc7ee745f1b5
kaluginpeter <streetworkout200412@gmail.com>
2025-04-27 07:42:44 +0300

    Added solution "3531. Count Covered Buildings"

--- LeetCode/Medium/3531._Count_Covered_Buildings.py
+++ LeetCode/Medium/3531._Count_Covered_Buildings.py
@@ -58,3 +58,74 @@
 # buildings[i] = [x, y]
 # 1 <= x, y <= n
 # All coordinates of buildings are unique.
+# Solution
+# Python O(NlogN + NlogM) O(N) HashMap BinarySearch Sorting
+class Solution:
+    def binary_search(self, nums: list[int], target: int) -> int:
+        left: int = 0
+        right: int = len(nums) - 1
+        while left <= right:
+            middle: int = left + ((right - left) >> 1)
+            if nums[middle] == target:
+                return middle
+            elif nums[middle] > target:
+                right = middle - 1
+            else:
+                left = middle + 1
+        return -1
+
+    def countCoveredBuildings(self, n: int, buildings: List[List[int]]) -> int:
+        buildings.sort()
+        x_axis: dict[int, int] = dict()
+        y_axis: dict[int, int] = dict()
+        for i in range(len(buildings)):
+            b: list[int] = buildings[i]
+            if b[0] not in x_axis:
+                x_axis[b[0]] = []
+            x_axis[b[0]].append(b[1])
+            if b[1] not in y_axis:
+                y_axis[b[1]] = []
+            y_axis[b[1]].append(b[0])
+        output: int = 0
+        for i in range(len(buildings)):
+            b: list[int] = buildings[i]
+            if (
+                    self.binary_search(x_axis[b[0]], b[1]) not in {0, len(x_axis[b[0]]) - 1}
+                    and self.binary_search(y_axis[b[1]], b[0]) not in {0, len(y_axis[b[1]]) - 1}
+            ):
+                output += 1
+
+        return output
+
+# C++ O(NlogN + NlogM) O(N) BinarySearch Sorting HashMap
+class Solution {
+public:
+    int binarySearch(int &target, vector<int> &nums) {
+        int left = 0, right = nums.size() - 1;
+        while (left <= right) {
+            int middle = left + ((right - left) >> 1);
+            if (nums[middle] == target) return middle;
+            else if (nums[middle] > target) right = middle - 1;
+            else left = middle + 1;
+        }
+        return -1;
+    }
+
+    int countCoveredBuildings(int n, vector<vector<int>>& buildings) {
+        std::sort(buildings.begin(), buildings.end());
+        unordered_map<int, vector<int>> xAxis, yAxis;
+        for (vector<int> &building : buildings) {
+            xAxis[building[0]].push_back(building[1]);
+            yAxis[building[1]].push_back(building[0]);
+        }
+        int output = 0;
+        for (vector<int> &building : buildings) {
+            int xBound = binarySearch(building[1], xAxis[building[0]]);
+            if (!xBound || xBound == xAxis[building[0]].size() - 1) continue;
+            int yBound = binarySearch(building[0], yAxis[building[1]]);
+            if (!yBound || yBound == yAxis[building[1]].size() - 1) continue;
+            ++output;
+        }
+        return output;
+    }
+};
