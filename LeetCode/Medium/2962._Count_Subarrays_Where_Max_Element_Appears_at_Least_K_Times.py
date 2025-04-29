ee37d1a5735e45bb66d3e50de3efeac79413f350
kaluginpeter <streetworkout200412@gmail.com>
2025-04-29 09:28:34 +0300

    Added solution "2962. Count Subarrays Where Max Element Appears at Least K Times"

--- LeetCode/Medium/2962._Count_Subarrays_Where_Max_Element_Appears_at_Least_K_Times.py
+++ LeetCode/Medium/2962._Count_Subarrays_Where_Max_Element_Appears_at_Least_K_Times.py
@@ -36,3 +36,40 @@ class Solution:
                 left += 1
             ans += left
         return ans
+
+# Python O(N) O(1) SlidingWindow
+class Solution:
+    def countSubarrays(self, nums: List[int], k: int) -> int:
+        output: int = 0
+        left: int = 0
+        count: int = 0
+        already_valid: int = 0
+        max_element: int = max(nums)
+        for right in range(len(nums)):
+            if nums[right] == max_element: count += 1
+            while count >= k:
+                already_valid += 1
+                if nums[left] == max_element: count -= 1
+                left += 1
+            output += already_valid
+        return output
+
+# C++ O(N) O(1) SlidingWindow
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, int k) {
+        long long output = 0;
+        int left = 0, maxElement = *max_element(nums.begin(), nums.end());
+        int alreadyValid = 0, count = 0;
+        for (int right = 0; right < nums.size(); ++right) {
+            if (nums[right] == maxElement) ++count;
+            while (count >= k) {
+                ++alreadyValid;
+                if (nums[left] == maxElement) --count;
+                ++left;
+            }
+            output += alreadyValid;
+        }
+        return output;
+    }
+};
