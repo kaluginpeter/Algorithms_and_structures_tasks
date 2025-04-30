a9c8c2f018dcfafc992f31efd7d1e8454cc232b7
kaluginpeter <streetworkout200412@gmail.com>
2025-04-30 10:09:22 +0300

    Added description "Name Array Capping"

---
+++ CodeWars/7kyu/Name_Array_Capping.py
@@ -0,0 +1,6 @@
+# Create a function that accepts an array of names, and returns an array of each name with its first letter capitalized and the remainder in lowercase.
+#
+# Examples
+# ['jo', 'nelson', 'jurie'] -->  ['Jo', 'Nelson', 'Jurie']
+# ['KARLY', 'DANIEL', 'KELSEY'] --> ['Karly', 'Daniel', 'Kelsey']
+# StringsArraysFundamentals
