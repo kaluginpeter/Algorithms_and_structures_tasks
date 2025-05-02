154171319cdfecdbdaac7b5043c1e8d062dee6d2
kaluginpeter <streetworkout200412@gmail.com>
2025-05-02 12:06:56 +0300

    Added solution "838. Push Dominoes"

--- LeetCode/Medium/838._Push_Dominoes.py
+++ LeetCode/Medium/838._Push_Dominoes.py
@@ -32,3 +32,76 @@
 # n == dominoes.length
 # 1 <= n <= 105
 # dominoes[i] is either 'L', 'R', or '.'.
+# Solution
+# Python O(N) O(N*) SlidingWindow
+class Solution:
+    def pushDominoes(self, dominoes: str) -> str:
+        output: list[str] = list(dominoes)
+        prev_right: int = -1
+        for i in range(len(dominoes)):
+            if dominoes[i] == 'R':
+                if prev_right == -1:
+                    prev_right = i
+                    continue
+                for j in range(prev_right + 1, i): output[j] = 'R'
+                prev_right = i
+            elif dominoes[i] == 'L':
+                if prev_right == -1:
+                    j: int = i - 1
+                    while j >= 0 and dominoes[j] == '.':
+                        output[j] = 'L'
+                        j -= 1
+                    continue
+                left: int = prev_right + 1
+                right: int = i - 1
+                while left < right:
+                    output[left] = 'R'
+                    output[right] = 'L'
+                    left += 1
+                    right -= 1
+                prev_right = -1
+        if prev_right != -1:
+            for i in range(prev_right, len(dominoes)): output[i] = 'R'
+        return ''.join(output)
+
+# C++ O(N) O(1) SlidingWindow
+class Solution {
+public:
+    string pushDominoes(string dominoes) {
+        int prevRight = -1, n = dominoes.size();
+        for (int i = 0; i < n; ++i) {
+            if (dominoes[i] == 'R') {
+                if (prevRight == -1) {
+                    prevRight = i;
+                    continue;
+                }
+                for (int j = prevRight; j < i; ++j) {
+                    dominoes[j] = 'R';
+                }
+                prevRight = i;
+            } else if (dominoes[i] == 'L') {
+                if (prevRight == -1) {
+                    int j = i - 1;
+                    while (j >= 0 && dominoes[j] == '.') {
+                        dominoes[j] = 'L';
+                        --j;
+                    }
+                } else {
+                    int left = prevRight + 1;
+                    int right = i - 1;
+                    while (left < right) {
+                        dominoes[left] = 'R';
+                        dominoes[right] = 'L';
+                        ++left;
+                        --right;
+                    }
+                    prevRight = -1;
+                }
+            }
+        }
+        if (prevRight != -1) {
+            for (int i = prevRight; i < n; ++i) dominoes[i] = 'R';
+        }
+        return dominoes;
+    }
+};
