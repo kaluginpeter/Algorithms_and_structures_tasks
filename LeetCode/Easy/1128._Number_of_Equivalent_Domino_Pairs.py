9a448b2f87d9690281e472e40032f72234ffb96a
kaluginpeter <streetworkout200412@gmail.com>
2025-05-04 07:04:32 +0300

    Added solution "1128. Number of Equivalent Domino Pairs"

--- LeetCode/Easy/1128._Number_of_Equivalent_Domino_Pairs.py
+++ LeetCode/Easy/1128._Number_of_Equivalent_Domino_Pairs.py
@@ -32,3 +32,45 @@ class Solution:
             else:
                 d[t] = 1
         return count
+
+
+# Python O(N) O(N) HashMap Counting
+class Solution:
+    def numEquivDominoPairs(self, dominoes: List[List[int]]) -> int:
+        output: int = 0
+        hashmap: dict[tuple[int, int], int] = dict()
+        for domino in dominoes:
+            x: int = hashmap.get((domino[0], domino[1]), 0)
+            y: int = hashmap.get((domino[1], domino[0]), 0)
+            if domino[0] == domino[1]: y = 0
+            output += x + y
+            hashmap[(domino[0], domino[1])] = hashmap.get((domino[0], domino[1]), 0) + 1
+        return output
+
+# C++ O(N) O(N) HashMap Counting
+#include <unordered_map>
+#include <functional>
+
+struct PairHash {
+    size_t operator()(const std::pair<int, int>& p) const {
+        auto hash1 = std::hash<int>{}(p.first);
+        auto hash2 = std::hash<int>{}(p.second);
+        return hash1 ^ (hash2 << 1);
+    }
+};
+
+class Solution {
+public:
+    int numEquivDominoPairs(vector<vector<int>>& dominoes) {
+        unordered_map<pair<int, int>, int, PairHash> hashmap;
+        int output = 0;
+        for (vector<int>& domino : dominoes) {
+            int x = hashmap[{domino[0], domino[1]}];
+            int y = hashmap[{domino[1], domino[0]}];
+            if (domino[0] == domino[1]) y = 0;
+            output += x + y;
+            ++hashmap[{domino[0], domino[1]}];
+        }
+        return output;
+    }
+};
