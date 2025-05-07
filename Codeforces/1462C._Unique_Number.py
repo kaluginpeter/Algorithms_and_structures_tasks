9b1d8166c833e3dd379dfc8b47c95e37b577eb3d
kaluginpeter <streetworkout200412@gmail.com>
2025-05-07 10:27:22 +0300

    Added description "1462C. Unique Number"

---
+++ Codeforces/1462C._Unique_Number.py
@@ -0,0 +1,36 @@
+# C. Unique Number
+# time limit per test2 seconds
+# memory limit per test256 megabytes
+# You are given a positive number x
+# . Find the smallest positive integer number that has the sum of digits equal to x
+#  and all digits are distinct (unique).
+#
+# Input
+# The first line contains a single positive integer t
+#  (1≤t≤50
+# ) — the number of test cases in the test. Then t
+#  test cases follow.
+#
+# Each test case consists of a single integer number x
+#  (1≤x≤50
+# ).
+#
+# Output
+# Output t
+#  answers to the test cases:
+#
+# if a positive integer number with the sum of digits equal to x
+#  and all digits are different exists, print the smallest such number;
+# otherwise print -1.
+# Example
+# InputCopy
+# 4
+# 1
+# 5
+# 15
+# 50
+# OutputCopy
+# 1
+# 5
+# 69
+# -1
