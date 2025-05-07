3b32cee6e7e739f6a0b645d83f0c8397d56f261d
kaluginpeter <streetworkout200412@gmail.com>
2025-05-07 10:28:21 +0300

    Added solution "1462C. Unique Number"

--- Codeforces/1462C._Unique_Number.py
+++ Codeforces/1462C._Unique_Number.py
@@ -34,3 +34,69 @@
 # 5
 # 69
 # -1
+# Solution
+# C++ O(logN) O(logN) Math Greedy
+#include <iostream>
+#include <vector>
+
+
+void solution() {
+    int t;
+    std::scanf("%d", &t);
+    for (int i = 0; i < t; ++i) {
+        int n;
+        std::scanf("%d", &n);
+        int output = -1;
+        if (n > 45) {
+            std::printf("-1\n");
+            continue;
+        }
+        std::vector<bool> hashmap(10, false);
+        std::vector<int> digits;
+        while (n > 0) {
+            for (int digit = 9; digit > 0; --digit) {
+                if (!hashmap[digit] && digit <= n) {
+                    digits.push_back(digit);
+                    hashmap[digit] = true;
+                    n -= digit;
+                    break;
+                }
+            }
+        }
+        for (int j = digits.size() - 1; j >= 0; --j) {
+            std::printf("%d", digits[j]);
+        }
+        std::printf("\n");
+    }
+}
+
+
+int main() {
+    solution();
+}
+
+# Python O(logN) O(logN) Math Greedy
+import sys
+
+
+def solution() -> None:
+    t: int = int(sys.stdin.readline().rstrip())
+    for _ in range(t):
+        n: int = int(sys.stdin.readline().rstrip())
+        if n > 45:
+            sys.stdout.write('-1\n')
+            continue
+        digits: list[int] = []
+        used: list[bool] = [False] * 10
+        while n:
+            for digit in range(9, 0, -1):
+                if not used[digit] and digit <= n:
+                    used[digit] = True
+                    n -= digit
+                    digits.append(digit)
+                    break
+        sys.stdout.write('{}\n'.format(''.join(str(digit) for digit in reversed(digits))))
+
+
+if __name__ == '__main__':
+    solution()
