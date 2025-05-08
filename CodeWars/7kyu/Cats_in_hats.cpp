c7a9d285bae8a21090626dd42cd040ca4c88702d
kaluginpeter <streetworkout200412@gmail.com>
2025-05-08 11:18:26 +0300

    Added solution "Cats in hats"

--- CodeWars/7kyu/Cats_in_hats.cpp
+++ CodeWars/7kyu/Cats_in_hats.cpp
@@ -15,3 +15,22 @@ fix to 3 decimal places.
 
 Fundamentals
 */
+// Solution
+#include <string>
+#include <iomanip>
+#include <sstream>
+
+class Kata {
+public:
+    std::string height(int n) {
+        double start = 2000000.0;
+        double output = 0.0;
+        for (int i = 0; i <= n; ++i) {
+            output += start;
+            start /= 2.5;
+        }
+        std::ostringstream oss;
+        oss << std::fixed << std::setprecision(3) << output;
+        return oss.str();
+    }
+};
