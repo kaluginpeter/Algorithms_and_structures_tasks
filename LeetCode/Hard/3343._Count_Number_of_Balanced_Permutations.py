f2145344a3fc53cb550711db394a442f1660cfd4
kaluginpeter <streetworkout200412@gmail.com>
2025-05-09 11:18:09 +0300

    Added solution "3343. Count Number of Balanced Permutations"

--- LeetCode/Hard/3343._Count_Number_of_Balanced_Permutations.py
+++ LeetCode/Hard/3343._Count_Number_of_Balanced_Permutations.py
@@ -44,3 +44,78 @@
 #
 # 2 <= num.length <= 80
 # num consists of digits '0' to '9' only.
+# Solution
+# Python O(|num|^2 * (totalSum / 2)) O(|num|^2 * |num|(totalSum / 2)) DynamicProgramming String
+class Solution:
+    MOD: int = 1000000007
+    fact: list[int] = []
+    inv: list[int] = []
+    inv_fact: list[int] = []
+
+    def precompute(self, n: int) -> None:
+        self.fact = [1] * (n + 1)
+        for i in range(1, n + 1): self.fact[i] = self.fact[i - 1] * i % self.MOD
+        self.inv = [1] * (n + 1)
+        for i in range(2, n + 1): self.inv[i] = self.MOD - (self.MOD // i) * self.inv[self.MOD % i] % self.MOD
+        self.inv_fact = [1] * (n + 1)
+        for i in range(1, n + 1): self.inv_fact[i] = self.inv_fact[i - 1] * self.inv[i] % self.MOD
+
+    def countBalancedPermutations(self, num: str) -> int:
+        n: int = len(num)
+        total_sum: int = sum(int(digit) for digit in num)
+        if total_sum & 1: return 0
+        self.precompute(n)
+        half_sum: int = total_sum // 2
+        half_len: int = n // 2
+        dp: list[list[int]] = [[0] * (half_len + 1) for _ in range(half_sum + 1)]
+        dp[0][0] = 1
+        digits: list[int] = [0] * 10
+        for digit in num:
+            digits[int(digit)] += 1
+            for i in range(half_sum, int(digit) - 1, -1):
+                for j in range(half_len, 0, -1):
+                    dp[i][j] = (dp[i][j] + dp[i - int(digit)][j - 1]) % self.MOD
+        output: int = dp[half_sum][half_len]
+        output = output * self.fact[half_len] % self.MOD * self.fact[n - half_len] % self.MOD
+        for freq in digits:
+            output = output * self.inv_fact[freq] % self.MOD
+        return output
+
+# C++ O(|num|^2 * (totalSum / 2)) O(|num|^2 * |num|(totalSum / 2)) DynamicProgramming String
+class Solution {
+public:
+    int countBalancedPermutations(const string& num) {
+        int n = num.size(), totalSum = 0;
+        for (const char &digit : num) totalSum += digit - '0';
+        if (totalSum & 1) return 0;
+        precompute(n);
+        int halfSum = totalSum / 2, halfLen = n / 2;
+        vector<vector<int>> dp(halfSum + 1, vector<int>(halfLen + 1));
+        dp[0][0] = 1;
+        vector<int> digits(10);
+        for (const char &digit : num) {
+            ++digits[digit - '0'];
+            for (int i = halfSum; i >= digit - '0'; --i)
+                for (int j = halfLen; j > 0; --j)
+                    dp[i][j] = (dp[i][j] + dp[i - (digit - '0')][j - 1]) % MOD;
+        }
+        long long output = dp[halfSum][halfLen];
+        output = output * fact[halfLen] % MOD * fact[n - halfLen] % MOD;
+        for (int &digit : digits)
+            output = output * invFact[digit] % MOD;
+        return output;
+    }
+
+private:
+    static const int MOD = 1e9 + 7;
+    vector<long long> fact, inv, invFact;
+
+    void precompute(int n) {
+        fact.assign(n + 1, 1);
+        for (int i = 1; i <= n; ++i) fact[i] = fact[i - 1] * i % MOD;
+        inv.assign(n + 1, 1);
+        for (int i = 2; i <= n; ++i) inv[i] = MOD - (MOD / i) * inv[MOD % i] % MOD;
+        invFact.assign(n + 1, 1);
+        for (int i = 1; i <= n; ++i) invFact[i] = invFact[i - 1] * inv[i] % MOD;
+    }
+};
