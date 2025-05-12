cba77d276b2dd39390921ed816d001ea5221d71c
kaluginpeter <streetworkout200412@gmail.com>
2025-05-12 10:40:31 +0300

    Added solution "Geometry Basics: Triangle Perimeter in 2D"

--- CodeWars/7kyu/ Geometry_Basics_Triangle_Perimeter_in_2D.cpp
+++ CodeWars/7kyu/ Geometry_Basics_Triangle_Perimeter_in_2D.cpp
@@ -7,3 +7,12 @@ Write a function calculating perimeter of a Triangle defined this way.
 
 GeometryFundamentals
 */
+// Solution
+#include <cmath>
+#include <iostream>
+double triangle_perimeter(const Triangle& t){
+  double first = std::sqrt(std::pow(t.a.x - t.b.x, 2) + std::pow(t.a.y - t.b.y, 2));
+  double second = std::sqrt(std::pow(t.b.x - t.c.x, 2) + std::pow(t.b.y - t.c.y, 2));
+  double third = std::sqrt(std::pow(t.a.x - t.c.x, 2) + std::pow(t.a.y - t.c.y, 2));
+  return first + second + third;
+}
