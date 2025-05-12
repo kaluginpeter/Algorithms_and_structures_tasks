5996fbf3fdd2dbee74f18e7c3b99ffad2be95e9f
kaluginpeter <streetworkout200412@gmail.com>
2025-05-12 10:40:14 +0300

    Added description "Geometry Basics: Triangle Perimeter in 2D"

---
+++ CodeWars/7kyu/ Geometry_Basics_Triangle_Perimeter_in_2D.cpp
@@ -0,0 +1,9 @@
+/*
+This series of katas will introduce you to basics of doing geometry with computers.
+
+Point objects have x, y attributes. Triangle objects have attributes a, b, c describing their corners, each of them is a Point.
+
+Write a function calculating perimeter of a Triangle defined this way.
+
+GeometryFundamentals
+*/
