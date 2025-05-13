3ed6d36946e0720051b3a40c3e65fc8a29e791ab
kaluginpeter <streetworkout200412@gmail.com>
2025-05-13 21:07:34 +0300

    Added description "Geometry Basics: Dot Product in 3D"

---
+++ CodeWars/7kyu/Geometry_Basics_Dot_Product_in_3D.cpp
@@ -0,0 +1,13 @@
+/*
+This series of katas will introduce you to basics of doing geometry with computers.
+
+Vector objects have x, y, and z attributes.
+
+Write a function calculating dot product of Vector a and Vector b.
+
+You can read more about dot product on Wikipedia.
+
+Tests round answers to 6 decimal places.
+
+GeometryFundamentals
+*/
