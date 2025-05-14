a76ce4a4dd608afd715a87058cb3fa7a1bd5a5fc
kaluginpeter <streetworkout200412@gmail.com>
2025-05-14 20:01:26 +0300

    Added description "Geometry Basics: Cross Product in 3D"

---
+++ CodeWars/7kyu/ Geometry_Basics_Cross_Product_in_3D.cpp
@@ -0,0 +1,13 @@
+/*
+This series of katas will introduce you to basics of doing geometry with computers.
+
+Vector objects (struct in C) have x, y, and z attributes.
+
+Write a function calculating the cross product of Vector a and Vector b, the result should be a Vector object.
+
+You can read more about cross product on Wikipedia.
+
+Tests round answers to 6 decimal places.
+
+GeometryFundamentals
+*/
