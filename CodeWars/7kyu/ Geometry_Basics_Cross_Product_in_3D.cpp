fabf5f11fa335e6794577dd77ae4f9d8f2cd430d
kaluginpeter <streetworkout200412@gmail.com>
2025-05-14 20:01:44 +0300

    Added solution "Geometry Basics: Cross Product in 3D"

--- CodeWars/7kyu/ Geometry_Basics_Cross_Product_in_3D.cpp
+++ CodeWars/7kyu/ Geometry_Basics_Cross_Product_in_3D.cpp
@@ -11,3 +11,10 @@ Tests round answers to 6 decimal places.
 
 GeometryFundamentals
 */
+// Solution
+Vector cross_product(const Vector& a, const Vector& b) {
+    double x = a.y * b.z - a.z * b.y;
+    double y = a.z * b.x - a.x * b.z;
+    double z = a.x * b.y - a.y * b.x;
+    return Vector(x, y, z);
+}
