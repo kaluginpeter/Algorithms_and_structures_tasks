39f66169a1ee9c2c0c05fe8a1ce2312162d07ff2
kaluginpeter <streetworkout200412@gmail.com>
2025-05-18 09:58:41 +0300

    Added solution "1931. Painting a Grid With Three Different Colors"

--- LeetCode/Hard/1931._Painting_a_Grid_With_Three_Different_Colors.py
+++ LeetCode/Hard/1931._Painting_a_Grid_With_Three_Different_Colors.py
@@ -26,3 +26,87 @@
 #
 # 1 <= m <= 5
 # 1 <= n <= 1000
+# Solution
+# Python O(3^2M * N) O(3^2M) DynamicProgramming
+class Solution:
+    def colorTheGrid(self, m: int, n: int) -> int:
+        mod: int = 10**9 + 7
+        valid: dict[int, list[int]] = dict()
+        for mask in range(3**m):
+            color: list[int] = list()
+            mm: int = mask
+            for i in range(m):
+                color.append(mm % 3)
+                mm //= 3
+            if any(color[i] == color[i + 1] for i in range(m - 1)): continue
+            valid[mask] = color
+        adjacent: dict[int, list[int]] = defaultdict(list)
+        for mask1, color1 in valid.items():
+            for mask2, color2 in valid.items():
+                if not any(x == y for x, y in zip(color1, color2)): adjacent[mask1].append(mask2)
+        f: list[int] = [int(mask in valid) for mask in range(3**m)]
+        for i in range(1, n):
+            g: list[int] = [0] * (3**m)
+            for mask2 in valid.keys():
+                for mask1 in adjacent[mask2]:
+                    g[mask2] += f[mask1]
+                    if g[mask2] >= mod: g[mask2] -= mod
+            f = g
+        return sum(f) % mod
+
+# C++ O(3^2M * N) O(3^2M) DynamicProgramming
+class Solution {
+    static constexpr int mod = 1000000007;
+public:
+    int colorTheGrid(int m, int n) {
+        unordered_map<int, vector<int>> valid;
+        int mask_end = pow(3, m);
+        for (int mask = 0; mask < mask_end; ++mask) {
+            vector<int> color;
+            int mm = mask;
+            for (int i = 0; i < m; ++i) {
+                color.push_back(mm % 3);
+                mm /= 3;
+            }
+            bool check = true;
+            for (int i = 0; i < m - 1; ++i) {
+                if (color[i] == color[i + 1]) {
+                    check = false;
+                    break;
+                }
+            }
+            if (check) valid[mask] = move(color);
+        }
+        unordered_map<int, vector<int>> adjacent;
+        for (const auto& [mask1, color1] : valid) {
+            for (const auto& [mask2, color2] : valid) {
+                bool check = true;
+                for (int i = 0; i < m; ++i) {
+                    if (color1[i] == color2[i]) {
+                        check = false;
+                        break;
+                    }
+                }
+                if (check) adjacent[mask1].push_back(mask2);
+            }
+        }
+        vector<int> f(mask_end);
+        for (const auto& [mask, _] : valid) f[mask] = 1;
+        for (int i = 1; i < n; ++i) {
+            vector<int> g(mask_end);
+            for (const auto& [mask2, _] : valid) {
+                for (int mask1 : adjacent[mask2]) {
+                    g[mask2] += f[mask1];
+                    if (g[mask2] >= mod) g[mask2] -= mod;
+                }
+            }
+            f = move(g);
+        }
+        int ans = 0;
+        for (int num : f) {
+            ans += num;
+            if (ans >= mod) ans -= mod;
+        }
+        return ans;
+    }
+};
