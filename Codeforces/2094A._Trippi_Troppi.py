c4dfbde8e36aa43e58c23d2d4a36aa876c50353a
kaluginpeter <streetworkout200412@gmail.com>
2025-05-19 09:39:35 +0300

    Added description "2094A. Trippi Troppi"

---
+++ Codeforces/2094A._Trippi_Troppi.py
@@ -0,0 +1,37 @@
+# A. Trippi Troppi
+# time limit per test1 second
+# memory limit per test256 megabytes
+# Trippi Troppi resides in a strange world. The ancient name of each country consists of three strings. The first letter of each string is concatenated to form the country's modern name.
+#
+# Given the country's ancient name, please output the modern name.
+#
+# Input
+# The first line contains an integer t
+#  – the number of independent test cases (1≤t≤100
+# ).
+#
+# The following t
+#  lines each contain three space-separated strings. Each string has a length of no more than 10
+# , and contains only lowercase Latin characters.
+#
+# Output
+# For each test case, output the string formed by concatenating the first letter of each word.
+#
+# Example
+# InputCopy
+# 7
+# united states america
+# oh my god
+# i cant lie
+# binary indexed tree
+# believe in yourself
+# skibidi slay sigma
+# god bless america
+# OutputCopy
+# usa
+# omg
+# icl
+# bit
+# biy
+# sss
+# gba
