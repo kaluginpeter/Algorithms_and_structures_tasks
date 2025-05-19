aa5b448b9b0fe1739de9b0feb6a2d09fd8a4d66b
kaluginpeter <streetworkout200412@gmail.com>
2025-05-19 09:40:25 +0300

    Added solution "2094A. Trippi Troppi"

--- Codeforces/2094A._Trippi_Troppi.py
+++ Codeforces/2094A._Trippi_Troppi.py
@@ -35,3 +35,40 @@
 # biy
 # sss
 # gba
+# Solution
+# C++ O(N) O(1) String
+#include <iostream>
+#include <string>
+
+
+void solution() {
+    int t;
+    std::scanf("%d", &t);
+    for (int i = 0; i < t; ++i) {
+        std::string word;
+        std::cin >> word;
+        std::cout << word[0];
+        std::cin >> word;
+        std::cout << word[0];
+        std::cin >> word;
+        std::cout << word[0] << "\n";
+    }
+}
+
+
+int main() {
+    solution();
+}
+# Python O(N) O(1) String
+import sys
+
+
+def solution() -> None:
+    t: int = int(sys.stdin.readline().rstrip())
+    for _ in range(t):
+        a, b, c = sys.stdin.readline().rstrip().split()
+        sys.stdout.write('{}{}{}\n'.format(a[0], b[0], c[0]))
+
+
+if __name__ == '__main__':
+    solution()
