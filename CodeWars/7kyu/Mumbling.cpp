e884509d64e27169bd61b2fa81d4870322b51e08
kaluginpeter <streetworkout200412@gmail.com>
2025-05-20 09:50:08 +0300

    Added solution "Mumbling"

--- CodeWars/7kyu/Mumbling.cpp
+++ CodeWars/7kyu/Mumbling.cpp
@@ -9,3 +9,17 @@ The parameter of accum is a string which includes only letters from a..z and A..
 
 FundamentalsStringsPuzzles
 */
+// Solution
+class Accumul
+{
+public:
+    static std::string accum(const std::string &s) {
+      std::string output = "";
+      for (int i = 0; i < s.size(); ++i) {
+        output.push_back(std::toupper(s[i]));
+        for (int j = 0; j < i; ++j) output.push_back(std::tolower(s[i]));
+        if (i + 1 != s.size()) output.push_back('-');
+      }
+      return output;
+    };
+};
