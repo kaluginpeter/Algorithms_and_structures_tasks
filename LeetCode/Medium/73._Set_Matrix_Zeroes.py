ea97ab94cee340c513b33f41ddc9cf98ea455f0e
kaluginpeter <streetworkout200412@gmail.com>
2025-05-21 17:06:14 +0300

    Added solution "73. Set Matrix Zeroes"

--- LeetCode/Medium/73._Set_Matrix_Zeroes.py
+++ LeetCode/Medium/73._Set_Matrix_Zeroes.py
@@ -29,3 +29,59 @@
 # A straightforward solution using O(mn) space is probably a bad idea.
 # A simple improvement uses O(m + n) space, but still not the best solution.
 # Could you devise a constant space solution?
+# Solution
+# Python O(NM) O(1) Matrix
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        n: int = len(matrix)
+        m: int = len(matrix[0])
+        is_first_row: bool = False
+        is_first_col: bool = False
+        for r in range(n):
+            for c in range(m):
+                if not matrix[r][c]:
+                    if not r: is_first_row = True
+                    if not c: is_first_col = True
+                    matrix[0][c] = 0
+                    matrix[r][0] = 0
+        for r in range(1, n):
+            if not matrix[r][0]:
+                for c in range(m): matrix[r][c] = 0
+        for c in range(1, m):
+            if not matrix[0][c]:
+                for r in range(n): matrix[r][c] = 0
+        if is_first_row:
+            for c in range(m): matrix[0][c] = 0
+        if is_first_col:
+            for r in range(n): matrix[r][0] = 0
+
+# C++ O(NM) O(1) Matrix
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size(), m = matrix[0].size();
+        bool isFirstRow = false, isFirstCol = false;
+        for (int r = 0; r < n; ++r) {
+            for (int c = 0; c < m; ++c) {
+                if (!matrix[r][c]) {
+                    if (!r) isFirstRow = true;
+                    if (!c) isFirstCol = true;
+                    matrix[r][0] = 0;
+                    matrix[0][c] = 0;
+                }
+            }
+        }
+        for (int r = 1; r < n; ++r) {
+            if (!matrix[r][0]) {
+                for (int c = 0; c < m; ++c) matrix[r][c] = 0;
+            }
+        }
+        for (int c = 1; c < m; ++c) {
+            if (!matrix[0][c]) {
+                for (int r = 0; r < n; ++r) matrix[r][c] = 0;
+            }
+        }
+        if (isFirstRow) for (int c = 0; c < m; ++c) matrix[0][c] = 0;
+        if (isFirstCol) for (int r = 0; r < n; ++r) matrix[r][0] = 0;
+    }
+};
