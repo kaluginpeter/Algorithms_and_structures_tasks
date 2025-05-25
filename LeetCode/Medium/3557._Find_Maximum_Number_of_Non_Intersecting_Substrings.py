644dba49cad94aec2b2cc11ff2d60493e39a288c
kaluginpeter <streetworkout200412@gmail.com>
2025-05-25 08:17:31 +0300

    Added solution "3557. Find Maximum Number of Non Intersecting Substrings"

--- LeetCode/Medium/3557._Find_Maximum_Number_of_Non_Intersecting_Substrings.py
+++ LeetCode/Medium/3557._Find_Maximum_Number_of_Non_Intersecting_Substrings.py
@@ -32,3 +32,54 @@
 #
 # 1 <= word.length <= 2 * 105
 # word consists only of lowercase English letters.
+# Solution
+# Python O(N) O(N) DynamicProgramming O(N) O(N)
+class Solution:
+    def maxSubstrings(self, word: str) -> int:
+        n: int = len(word)
+        dp: list[int] = [0] * n
+        prev: list[list[int]] = [[] for _ in range(26)]
+        for i in range(n):
+            x: int = ord(word[i]) - 97
+            prev[x].append(i)
+            left: int = -1
+            for j in range(len(prev[x]) - 1, -1, -1):
+                if i - prev[x][j] + 1 >= 4:
+                    left = prev[x][j]
+                    break
+            if left < 0:
+                dp[i] = dp[i - 1] if i else 0
+                continue
+            take: int = (dp[left - 1] if left - 1 >= 0 else 0) + 1
+            not_take: int = dp[i - 1] if i else 0
+            dp[i] = max(take, not_take)
+
+        return dp[n - 1]
+
+# C++ O(N) O(N) DynamicProgramming
+class Solution {
+public:
+    int maxSubstrings(string word) {
+        int n = word.size();
+        vector<int> dp(n, 0);
+        vector<vector<int>> prev(26, vector<int>());
+        for (int i = 0; i < n; ++i) {
+            int left = -1;
+            for (int j = prev[word[i] - 'a'].size() - 1; j >= 0; --j) {
+                if (i - prev[word[i] - 'a'][j] + 1 >= 4) {
+                    left = prev[word[i] - 'a'][j];
+                    break;
+                }
+            }
+            prev[word[i] - 'a'].push_back(i);
+            if (left == -1) {
+                dp[i] = (i? dp[i - 1] : 0);
+                continue;
+            }
+            int take = (left - 1 >= 0? dp[left - 1] : 0) + 1;
+            int notTake = (i? dp[i - 1] : 0);
+            dp[i] = max(take, notTake);
+        }
+        return dp[n - 1];
+    }
+};
