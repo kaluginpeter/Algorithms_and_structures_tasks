fee20daa30b8451ae8cc68c56a73d436dcb2a6a1
kaluginpeter <streetworkout200412@gmail.com>
2025-05-26 19:13:53 +0300

    Added solution "1857. Largest Color Value in a Directed Graph"

--- LeetCode/Hard/1857._Largest_Color_Value_in_a_Directed_Graph.py
+++ LeetCode/Hard/1857._Largest_Color_Value_in_a_Directed_Graph.py
@@ -32,3 +32,105 @@
 # 0 <= m <= 105
 # colors consists of lowercase English letters.
 # 0 <= aj, bj < n
+# Solution
+# Python O(N) O(N26) DynamicProgramming TopologicalSorting
+class Solution:
+    output: int = 0
+
+    def has_cycle(self, source: int, adj_list: dict[int, list[int]], colors: str, seen: list[int], parent: list[int],
+                  nodes_hashmap: list[list[int]]) -> bool:
+        cur_nodes: list[int] = [source]
+        while cur_nodes:
+            node: int = cur_nodes.pop()
+            if not seen[node]:
+                seen[node] = 1
+                cur_nodes.append(node)
+                for neighbor in adj_list.get(node, []):
+                    if not seen[neighbor]:
+                        parent[neighbor] = node
+                        cur_nodes.append(neighbor)
+                    elif seen[neighbor] == 1:
+                        return True
+                    else:
+                        self.copy_hashmap(neighbor, node, nodes_hashmap)
+            elif seen[node] == 1:
+                seen[node] = 2
+                nodes_hashmap[node][ord(colors[node]) - 97] += 1
+                self.copy_hashmap(node, parent[node], nodes_hashmap)
+        return False
+
+    def copy_hashmap(self, from_: int, to: int, nodes_hashmap: list[list[int]]) -> None:
+        for i in range(26):
+            nodes_hashmap[to][i] = max(nodes_hashmap[to][i], nodes_hashmap[from_][i])
+            if nodes_hashmap[to][i] > self.output:
+                self.output = nodes_hashmap[to][i]
+
+    def largestPathValue(self, colors: str, edges: List[List[int]]) -> int:
+        self.output = 0
+        adj_list: dict[int, list[int]] = dict()
+        for u, v in edges:
+            if u not in adj_list: adj_list[u] = []
+            adj_list[u].append(v)
+        n: int = len(colors)
+        seen: list[int] = [0] * n
+        parent: list[int] = list(range(n))
+        nodes_hashmap: list[list[int]] = [[0] * 26 for _ in range(n)]
+        for vertex in range(n):
+            if not seen[vertex]:
+                if self.has_cycle(vertex, adj_list, colors, seen, parent, nodes_hashmap):
+                    return -1
+        return self.output
+
+# C++ O(N) O(N26) TopologicalSorting DynamicProgramming
+class Solution {
+public:
+    bool hasCycle(int source, unordered_map<int, vector<int>> &adjList, int &n, vector<int> &seen, vector<vector<int>> &nodesHashmap, string &colors, vector<int> &parent, int &output) {
+        vector<int> curNodes = {source};
+        int prev = -1;
+        while (!curNodes.empty()) {
+            int node = curNodes.back();
+            curNodes.pop_back();
+            if (!seen[node]) {
+                curNodes.push_back(node);
+                seen[node] = 1;
+                for (int neighbor : adjList[node]) {
+                    if (seen[neighbor] == 0) {
+                        parent[neighbor] = node;
+                        curNodes.push_back(neighbor);
+                        prev = neighbor;
+                    }
+                    else if (seen[neighbor] == 1) return -1;
+                    else copyHashmap(neighbor, node, nodesHashmap, output);
+                }
+            } else if (seen[node] == 1) {
+                seen[node] = 2;
+                ++nodesHashmap[node][colors[node] - 'a'];
+                copyHashmap(node, parent[node], nodesHashmap, output);
+            }
+        }
+        return false;
+    }
+
+    void copyHashmap(int &from, int &to, vector<vector<int>> &nodesHashmap, int &output) {
+        for (int i = 0; i < 26; ++i) {
+            nodesHashmap[to][i] = max(nodesHashmap[to][i], nodesHashmap[from][i]);
+            if (nodesHashmap[to][i] > output) output = nodesHashmap[to][i];
+        }
+    }
+
+    int largestPathValue(string colors, vector<vector<int>>& edges) {
+        unordered_map<int, vector<int>> adjList;
+        for (vector<int> &edge: edges) adjList[edge[0]].push_back(edge[1]);
+        int n = colors.size();
+        vector<vector<int>> nodesHashmap(n, vector<int>(26, 0));
+        vector<int> seen(n, 0), parent(n, 0);
+        int output = 0;
+        for (int vertex = 0; vertex < n; ++vertex) {
+            parent[vertex] = vertex;
+            if (!seen[vertex]) {
+                if (hasCycle(vertex, adjList, n, seen, nodesHashmap, colors, parent, output)) return -1;
+            }
+        }
+        return output;
+    }
+};
