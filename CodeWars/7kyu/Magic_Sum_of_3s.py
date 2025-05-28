4a53e3f5ec065a0b014753de0dce0ec5e47755bc
kaluginpeter <streetworkout200412@gmail.com>
2025-05-28 09:07:36 +0300

    Added description "Magic Sum of 3s"

---
+++ CodeWars/7kyu/Magic_Sum_of_3s.py
@@ -0,0 +1,9 @@
+# The magic sum of 3s is calculated on an array by summing up odd numbers which include the digit 3.
+#
+# Complete the function which accepts an array of integers and returns its magic sum of 3s.
+#
+# Example: [3, 12, 5, 8, 30, 13] results in 16 (3 + 13)
+#
+# If there is no such number in the array, 0 should be returned.
+#
+# Fundamentals
