faa94bf371136b28e4837f6688c717c100fa097d
kaluginpeter <streetworkout200412@gmail.com>
2025-05-28 09:15:07 +0300

    Added solution "3372. Maximize the Number of Target Nodes After Connecting Trees I"

--- LeetCode/Medium/3372._Maximize_the_Number_of_Target_Nodes_After_Connecting_Trees_I.py
+++ LeetCode/Medium/3372._Maximize_the_Number_of_Target_Nodes_After_Connecting_Trees_I.py
@@ -49,3 +49,90 @@
 # 0 <= ui, vi < m
 # The input is generated such that edges1 and edges2 represent valid trees.
 # 0 <= k <= 1000
+# Solution
+# Python O(N^2 + M^2) O(N + M) Tree Breadth-First-Search
+class Solution:
+    def count_neighbors(self, source: int, adj_list: list[list[int]], k: int) -> int:
+        seen: set[int] = set()
+        cur_nodes: list[int] = [source]
+        seen.add(source)
+        next_nodes: list[int] = []
+        length: int = 0
+        neighbors: int = 0
+        while cur_nodes and length <= k:
+            for node in cur_nodes:
+                neighbors += 1
+                for neighbor in adj_list[node]:
+                    if neighbor in seen: continue
+                    seen.add(neighbor)
+                    next_nodes.append(neighbor)
+            cur_nodes = next_nodes
+            next_nodes = []
+            length += 1
+        return neighbors
+
+    def maxTargetNodes(self, edges1: List[List[int]], edges2: List[List[int]], k: int) -> List[int]:
+        n: int = len(edges1) + 1
+        m: int = len(edges2) + 1
+        adj_list_edges1: list[list[int]] = [[] for _ in range(n)]
+        adj_list_edges2: list[list[int]] = [[] for _ in range(m)]
+        for u, v in edges1:
+            adj_list_edges1[u].append(v)
+            adj_list_edges1[v].append(u)
+        for u, v in edges2:
+            adj_list_edges2[u].append(v)
+            adj_list_edges2[v].append(u)
+        extra: int = 0
+        for vertex in range(m):
+            extra = max(extra, self.count_neighbors(vertex, adj_list_edges2, k - 1))
+        output: list[int] = [0] * n
+        for vertex in range(n):
+            output[vertex] = self.count_neighbors(vertex, adj_list_edges1, k) + extra
+        return output
+
+# C++ O(N^2 + M^2) O(N + M) Tree Breadth-First-Search
+class Solution {
+public:
+    int countNeighbors(int &source, vector<vector<int>> &adjList, int k) {
+        unordered_set<int> seen;
+        int neighbors = 0, length = 0;
+        seen.insert(source);
+        vector<int> curNodes = {source}, nextNodes;
+        while (!curNodes.empty() && length <= k) {
+            for (int &node : curNodes) {
+                ++neighbors;
+                for (int &neighbor : adjList[node]) {
+                    if (seen.count(neighbor)) continue;
+                    seen.insert(neighbor);
+                    nextNodes.push_back(neighbor);
+                }
+            }
+            ++length;
+            curNodes = nextNodes;
+            nextNodes.clear();
+        }
+        return neighbors;
+    }
+
+    vector<int> maxTargetNodes(vector<vector<int>>& edges1, vector<vector<int>>& edges2, int k) {
+        int n = edges1.size() + 1, m = edges2.size() + 1;
+        vector<vector<int>> adjListEdges1(n, vector<int>()), adjListEdges2(m, vector<int>());
+        for (vector<int> &edge : edges1) {
+            adjListEdges1[edge[0]].push_back(edge[1]);
+            adjListEdges1[edge[1]].push_back(edge[0]);
+        }
+        int extra = 0;
+        for (vector<int> &edge : edges2) {
+            adjListEdges2[edge[0]].push_back(edge[1]);
+            adjListEdges2[edge[1]].push_back(edge[0]);
+        }
+        for (int vertex = 0; vertex < m; ++vertex) {
+            extra = max(extra, countNeighbors(vertex, adjListEdges2, k - 1));
+        }
+        vector<int> output(n, 0);
+        for (int vertex = 0; vertex < n; ++vertex) {
+            output[vertex] = countNeighbors(vertex, adjListEdges1, k) + extra;
+        }
+        return output;
+    }
+};
