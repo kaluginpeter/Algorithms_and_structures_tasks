32fdcce2c402c3180604e3a3a17c9765f834a313
kaluginpeter <streetworkout200412@gmail.com>
2025-06-02 19:29:18 +0300

    Added description "Pluralization"

---
+++ CodeWars/7kyu/Pluralization.py
@@ -0,0 +1,24 @@
+# Your code should take an English noun with a regular plural form and return its plural.
+#
+# Rules for pluralization in English:
+#
+# If a singular noun ends in '-s', '-x', '-z', '-ch' or '-sh', add '-es'
+#
+# If a singular noun ends with a consonant and '-y', change '-y' to '-ies'.
+#
+# All other words just add '-s'
+#
+# None of the tests end in '-f' or '-o' and none are irregular nouns (e.g. child, mouse etc.)
+#
+# Examples
+#
+# table -> tables,
+# window -> windows,
+# church -> churches,
+# watch -> watches,
+# bus -> buses,
+# box -> boxes,
+# buzz -> buzzes,
+# fly -> flies
+#
+# Algorithms
