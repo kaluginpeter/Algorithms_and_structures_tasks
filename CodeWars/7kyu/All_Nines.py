61edbec211b471f69a7b3bc55e8caade3af17104
kaluginpeter <streetworkout200412@gmail.com>
2025-06-03 21:40:08 +0300

    Added description "All Nines"

---
+++ CodeWars/7kyu/All_Nines.py
@@ -0,0 +1,13 @@
+# Task
+# Given any positive integer x ≤ 4000, find the smallest positive integer m such that mx consists of all 9's. Return -1 if no such m exists.
+#
+# Examples:
+# 11 -> 9, because 11 * 9 == 99.
+#
+# 12 -> -1, because 12 is even, so no multiple of it can contain only nines.
+#
+# 13 -> 76923, because 13 * 76923 == 999999, and no smaller positive integer, when multiplied by 13, generates an integer containing only nines.
+#
+# NOTE: Although x ≤ 4000, m can be very very LARGE. Where necessary, the way of handling big integers appropriate to the language should be used.
+#
+# Mathematics
