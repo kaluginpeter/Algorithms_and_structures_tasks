3726665a957e00d7de0b939b9d48c39adabc4789
kaluginpeter <streetworkout200412@gmail.com>
2025-06-04 21:18:59 +0300

    Added solution "3403. Find the Lexicographically Largest String From the Box I"

--- LeetCode/Medium/3403._Find_the_Lexicographically_Largest_String_From_the_Box_I.py
+++ LeetCode/Medium/3403._Find_the_Lexicographically_Largest_String_From_the_Box_I.py
@@ -69,3 +69,42 @@ public:
         return output;
     }
 };
+
+# Python O(N^2) O(N) String Greedy
+class Solution:
+    def answerString(self, word: str, numFriends: int) -> str:
+        n: int = len(word)
+        if numFriends == 1: return word
+        candidates: list[int] = [0]
+        for i in range(1, n):
+            if word[i] > word[candidates[0]]:
+                candidates.clear()
+                candidates.append(i)
+            elif word[i] == word[candidates[0]]: candidates.append(i)
+        output: str = word[candidates[0]:candidates[0] + (n - max(0, numFriends - 1 - candidates[0]) - candidates[0])]
+        for i in range(1, len(candidates)):
+            candidate: str = word[candidates[i]:candidates[i] + (n - max(0, numFriends - 1 - candidates[i]) - candidates[i])]
+            if candidate > output: output = candidate
+        return output
+
+# C++ O(N^2) O(N) String Greedy
+class Solution {
+public:
+    string answerString(string word, int numFriends) {
+        int n = word.size();
+        if (numFriends == 1) return word;
+        vector<int> candidates = {0};
+        for (int i = 1; i < n; ++i) {
+            if (word[i] > word[candidates[0]]) {
+                candidates.clear();
+                candidates.push_back(i);
+            } else if (word[i] == word[candidates[0]]) candidates.push_back(i);
+        }
+        string output = word.substr(candidates[0], n - max(0, numFriends - 1 - candidates[0]) - candidates[0]);
+        for (int i = 1; i < candidates.size(); ++i) {
+            string candidate = word.substr(candidates[i], n - max(0, numFriends - 1 - candidates[i]) - candidates[i]);
+            if (candidate > output) output = candidate;
+        }
+        return output;
+    }
+};
