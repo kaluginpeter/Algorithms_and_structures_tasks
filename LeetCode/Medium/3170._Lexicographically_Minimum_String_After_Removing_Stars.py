481a217d9f39c5e67c95e94334558b076ba788d3
kaluginpeter <streetworkout200412@gmail.com>
2025-06-07 14:41:10 +0300

    Added solution "3170. Lexicographically Minimum String After Removing Stars"

--- LeetCode/Medium/3170._Lexicographically_Minimum_String_After_Removing_Stars.py
+++ LeetCode/Medium/3170._Lexicographically_Minimum_String_After_Removing_Stars.py
@@ -48,3 +48,57 @@ class Solution:
                 heapq.heappush(heap, (s[i], -i))
         heap.sort(key=lambda x: -x[1])
         return ''.join(i[0] for i in heap)
+
+
+# Python O(DN + NlogN) O(D + N) HashMap Stack PriorityQueue
+class Solution:
+    def clearStars(self, s: str) -> str:
+        n: int = len(s)
+        hashmap: list[list[int]] = [[] for _ in range(26)]
+        for i in range(n):
+            if s[i] != '*': hashmap[ord(s[i]) - 97].append(i)
+            else:
+                for j in range(26):
+                    if hashmap[j]:
+                        hashmap[j].pop()
+                        break
+        min_heap: list[int] = []
+        for i in range(26):
+            while hashmap[i]: heapq.heappush(min_heap, hashmap[i].pop())
+        output: list[str] = []
+        while min_heap:
+            output.append(s[heapq.heappop(min_heap)])
+        return ''.join(output)
+
+# C++ O(DN + NlogN) O(D + N) HashMap Stack PriorityQueue
+class Solution {
+public:
+    string clearStars(string s) {
+        vector<vector<int>> hashmap(26, vector<int>());
+        int n = s.size();
+        for (int i = 0; i < n; ++i) {
+            if (s[i] != '*') hashmap[s[i] - 'a'].push_back(i);
+            else {
+                for (int j = 0; j < 26; ++j) {
+                    if (!hashmap[j].empty()) {
+                        hashmap[j].pop_back();
+                        break;
+                    }
+                }
+            }
+        }
+        priority_queue<int> minHeap;
+        for (int i = 0; i < 26; ++i) {
+            while (!hashmap[i].empty()) {
+                minHeap.push(-hashmap[i].back());
+                hashmap[i].pop_back();
+            }
+        }
+        string output = "";
+        while (!minHeap.empty()) {
+            output.push_back(s[-minHeap.top()]);
+            minHeap.pop();
+        }
+        return output;
+    }
+};
