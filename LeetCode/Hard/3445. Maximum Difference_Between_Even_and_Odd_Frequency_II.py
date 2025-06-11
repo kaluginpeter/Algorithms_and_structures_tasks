adc1ec304c5f1dcf14bea28ace4c5ff482c2ab5e
kaluginpeter <streetworkout200412@gmail.com>
2025-06-11 20:34:53 +0300

    Added solution "3445. Maximum Difference Between Even and Odd Frequency II"

--- LeetCode/Hard/3445. Maximum Difference_Between_Even_and_Odd_Frequency_II.py
+++ LeetCode/Hard/3445. Maximum Difference_Between_Even_and_Odd_Frequency_II.py
@@ -43,3 +43,62 @@
 # s consists only of digits '0' to '4'.
 # The input is generated that at least one substring has a character with an even frequency and a character with an odd frequency.
 # 1 <= k <= s.length
+# Solution
+# Python O(N) O(N) DynamicProgramming String
+class Solution:
+    def maxDifference(self, s: str, k: int) -> int:
+        n: int = len(s)
+        output: int = float('-inf')
+        for a in range(5):
+            for b in range(5):
+                if a == b: continue
+                ps1: list[int] = [0] * (n + 1)
+                ps2: list[int] = [0] * (n + 1)
+                for i in range(1, n + 1):
+                    ps1[i] = ps1[i - 1] + int(s[i - 1] == str(a))
+                    ps2[i] = ps2[i - 1] + int(s[i - 1] == str(b))
+                dp: list[list[int]] = [[float('-inf')] * 2 for _ in range(2)]
+                left: int = 0
+                for right in range(k, n + 1):
+                    while right - left >= k and ps1[right] > ps1[left] and ps2[right] > ps2[left]:
+                        pa: int = ps1[left] & 1
+                        pb: int = ps2[left] & 1
+                        dp[pa][pb] = max(dp[pa][pb], ps2[left] - ps1[left])
+                        left += 1
+                    pa: int = ps1[right] & 1
+                    pb: int = ps2[right] & 1
+                    need: int = dp[1 - pa][pb]
+                    if need != float('-inf'): output = max(output, (ps1[right] - ps2[right]) + need)
+        return -1 if output == float('-inf') else output
+
+# C++ O(N) O(N) DynamicProgamming String
+class Solution {
+public:
+    int maxDifference(string s, int k) {
+        int n = s.size(), output = INT_MIN;
+        for (int a = 0; a <= 4; ++a) {
+            for (int b = 0; b <= 4; ++b) {
+                if (a == b) continue;
+                vector<int> ps1(n + 1);
+                vector<int> ps2(n + 1);
+                for (int i = 1; i <= n; ++i) {
+                    ps1[i] = ps1[i - 1] + (s[i - 1] - '0' == a);
+                    ps2[i] = ps2[i - 1] + (s[i - 1] - '0' == b);
+                }
+                vector<vector<int>> dp = {{INT_MIN, INT_MIN}, {INT_MIN, INT_MIN}};
+                int left = 0;
+                for (int right = k; right <= n; ++right) {
+                    while (right - left >= k && ps1[right] > ps1[left] && ps2[right] > ps2[left]) {
+                        int pa = ps1[left] % 2, pb = ps2[left] % 2;
+                        dp[pa][pb] = max(dp[pa][pb], ps2[left] - ps1[left]);
+                        ++left;
+                    }
+                    int pa = ps1[right] % 2, pb = ps2[right] % 2;
+                    int need = dp[1 - pa][pb];
+                    if (need != INT_MIN) output = max(output, (ps1[right] - ps2[right]) + need);
+                }
+            }
+        }
+        return (output != INT_MIN? output : -1);
+    }
+};
