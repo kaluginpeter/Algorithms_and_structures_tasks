2dcc2d1926743efb4a70729b5c3ab9a6a2d69fc9
kaluginpeter <streetworkout200412@gmail.com>
2025-06-13 14:00:19 +0300

    Added solution "Challenge Fun #16: Overlap Point"

--- CodeWars/5kyu/Challenge_Fun_16_Overlap_Point.py
+++ CodeWars/5kyu/Challenge_Fun_16_Overlap_Point.py
@@ -31,3 +31,36 @@
 # The number of integer points inside the intersection area between c1 and c2.
 #
 # Algorithms
+# Solution
+import math
+
+def math_stuff(c1, c2):
+    x1, y1, r1 = c1
+    x2, y2, r2 = c2
+    r1_sq = r1 * r1
+    r2_sq = r2 * r2
+    x_min = math.ceil(max(x1 - r1, x2 - r2))
+    x_max = math.floor(min(x1 + r1, x2 + r2))
+    total_points = 0
+    for x in range(int(x_min), int(x_max) + 1):
+        dx1_sq = (x - x1)**2
+        if dx1_sq > r1_sq: continue
+        h1 = math.sqrt(r1_sq - dx1_sq)
+        y1_start = y1 - h1
+        y1_end = y1 + h1
+        dx2_sq = (x - x2)**2
+        if dx2_sq > r2_sq: continue
+        h2 = math.sqrt(r2_sq - dx2_sq)
+        y2_start = y2 - h2
+        y2_end = y2 + h2
+        final_y_start = max(y1_start, y2_start)
+        final_y_end = min(y1_end, y2_end)
+        iy_start = math.ceil(final_y_start)
+        iy_end = math.floor(final_y_end)
+        if iy_start <= iy_end:
+            points_in_slice = iy_end - iy_start + 1
+            total_points += points_in_slice
+    return int(total_points)
+
+def overlap_point(c1, c2):
+    return math_stuff(c1, c2)
