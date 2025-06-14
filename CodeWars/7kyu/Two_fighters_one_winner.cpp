820323ddf1597961aceb3f76a232f0781f96de01
kaluginpeter <streetworkout200412@gmail.com>
2025-06-14 14:07:21 +0300

    Added description "Two fighters, one winner."

---
+++ CodeWars/7kyu/Two_fighters_one_winner.cpp
@@ -0,0 +1,60 @@
+/*
+Create a function that returns the name of the winner in a fight between two fighters.
+
+Each fighter takes turns attacking the other and whoever kills the other first is victorious. Death is defined as having health <= 0.
+
+Each fighter will be a Fighter object/instance. See the Fighter class below in your chosen language.
+
+Both health and damagePerAttack (damage_per_attack for python) will be integers larger than 0. You can mutate the Fighter objects.
+
+Your function also receives a third argument, a string, with the name of the fighter that attacks first.
+
+Example:
+  declare_winner(Fighter("Lew", 10, 2), Fighter("Harry", 5, 4), "Lew") => "Lew"
+
+  Lew attacks Harry; Harry now has 3 health.
+  Harry attacks Lew; Lew now has 6 health.
+  Lew attacks Harry; Harry now has 1 health.
+  Harry attacks Lew; Lew now has 2 health.
+  Lew attacks Harry: Harry now has -1 health and is dead. Lew wins.
+class Fighter
+{
+private:
+    std::string name;
+
+    int health;
+
+    int damagePerAttack;
+
+public:
+    Fighter(std::string name, int health, int damagePerAttack)
+    {
+        this->name = name;
+        this->health = health;
+        this->damagePerAttack = damagePerAttack;
+    }
+
+    ~Fighter() { };
+
+    std::string getName()
+    {
+        return name;
+    }
+
+    int getHealth()
+    {
+        return health;
+    }
+
+    int getDamagePerAttack()
+    {
+        return damagePerAttack;
+    }
+
+    void setHealth(int value)
+    {
+        health = value;
+    }
+};
+GamesAlgorithmsLogicFundamentals
+*/
