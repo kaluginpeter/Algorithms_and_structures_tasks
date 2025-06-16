502f05e14d8a40c9661df2cb1d60d72215153949
kaluginpeter <streetworkout200412@gmail.com>
2025-06-16 09:14:18 +0300

    Added description "Arithmetic List!"

---
+++ CodeWars/7kyu/Arithmetic_List.cpp
@@ -0,0 +1,14 @@
+/*
+In this kata, you will write an arithmetic list which is basically a list that contains consecutive terms in the sequence.
+You will be given three parameters :
+
+first the first term in the sequence
+c the constant that you are going to ADD ( since it is an arithmetic sequence...)
+l the number of terms that should be returned
+Useful link: Sequence
+
+Be sure to check out my Arithmetic sequence Kata first ;)
+Don't forget about the indexing pitfall ;)
+
+ArraysFundamentals
+*/
