ee6cba782e087b8e43d1c6637561a3f46e709d85
kaluginpeter <streetworkout200412@gmail.com>
2025-06-17 19:21:31 +0300

    Added solution "3405. Count the Number of Arrays with K Matching Adjacent Elements"

--- LeetCode/Hard/3405._Count_the_Number_of_Arrays_with_K_Matching_Adjacent_Elements.py
+++ LeetCode/Hard/3405._Count_the_Number_of_Arrays_with_K_Matching_Adjacent_Elements.py
@@ -44,3 +44,70 @@
 # 1 <= n <= 105
 # 1 <= m <= 105
 # 0 <= k <= n - 1
+# Solution
+# Python O(log(N - k)) O(1) Math Combinatorics
+class Solution:
+    MOD: int = 1000000007
+    bound: int = 100000
+    fact: list[int] = [0] * bound
+    inv_fact: list[int] = [0] * bound
+
+    def countGoodArrays(self, n: int, m: int, k: int) -> int:
+        self.preprocessing()
+        return self.binom_coef(n - 1, k) * m % self.MOD * self.binary_pow(m - 1, n - k - 1) % self.MOD
+
+    def binary_pow(self, x: int, n: int) -> int:
+        output: int = 1
+        while n:
+            if n & 1: output = output * x % self.MOD
+            x = x * x % self.MOD
+            n >>= 1
+        return output
+
+    def binom_coef(self, n: int, m: int) -> int:
+        return self.fact[n] * self.inv_fact[m] % self.MOD * self.inv_fact[n - m] % self.MOD
+
+    def preprocessing(self) -> None:
+        if self.fact[0]: return
+        self.fact[0] = 1
+        for i in range(1, self.bound):
+            self.fact[i] = self.fact[i - 1] * i % self.MOD
+        self.inv_fact[self.bound - 1] = self.binary_pow(self.fact[self.bound - 1], self.MOD - 2)
+        for i in range(self.bound - 1, 0, -1):
+            self.inv_fact[i - 1] = self.inv_fact[i] * i % self.MOD
+
+# C++ O(log(N - k)) O(1) Math Combinatorics
+class Solution {
+public:
+    int MOD = 1e9 + 7;
+    const static int bound = 1e5;
+    long long fact[bound];
+    long long inv_fact[bound];
+
+    int countGoodArrays(int n, int m, int k) {
+        preprocessing();
+        return binomCoef(n - 1, k) * m % MOD * binaryPow(m - 1, n - k - 1) % MOD;
+    }
+
+    long long binaryPow(long long x, int n) {
+        long long output = 1;
+        while (n) {
+            if (n & 1) output = output * x % MOD;
+            x = x * x % MOD;
+            n >>= 1;
+        }
+        return output;
+    }
+
+    long long binomCoef(int n, int m) {
+        return fact[n] * inv_fact[m] % MOD * inv_fact[n - m] % MOD;
+    }
+
+    void preprocessing() {
+        if (fact[0]) return;
+        fact[0] = 1;
+        for (int i = 1; i < bound; ++i) fact[i] = fact[i - 1] * i % MOD;
+        inv_fact[bound - 1] = binaryPow(fact[bound - 1], MOD - 2);
+        for (int i = bound - 1; i; --i) inv_fact[i - 1] = inv_fact[i] * i % MOD;
+    }
+};
