f3dbcaa54258ad64646b6fcee5138acbd5ab4408
kaluginpeter <streetworkout200412@gmail.com>
2025-06-20 09:09:28 +0300

    Added solution "3443. Maximum Manhattan Distance After K Changes"

--- LeetCode/Medium/3443._Maximum_Manhattan_Distance_After_K_Changes.py
+++ LeetCode/Medium/3443._Maximum_Manhattan_Distance_After_K_Changes.py
@@ -47,3 +47,67 @@
 # 1 <= s.length <= 105
 # 0 <= k <= s.length
 # s consists of only 'N', 'S', 'E', and 'W'.
+# Solution
+# Python O(N) O(1) Simulation Greedy
+class Solution:
+    output: int = 0
+    def simulate_process(self, support_y: str, enemy_y: str, support_x: str, k: int, s: str) -> None:
+        x: int = 0
+        y: int = 0
+        for move in s:
+            if move == support_y: y += 1
+            elif move == enemy_y:
+                if k:
+                    y += 1
+                    k -= 1
+                else: y -= 1
+            elif move == support_x: x += 1
+            else:
+                if k:
+                    x += 1
+                    k -= 1
+                else: x -= 1
+            self.output = max(self.output, abs(x) + abs(y))
+
+    def maxDistance(self, s: str, k: int) -> int:
+        self.output = 0
+        # common NE NW SE SW
+        self.simulate_process('N', 'S', 'E', k, s)
+        self.simulate_process('N', 'S', 'W', k, s)
+        self.simulate_process('S', 'N', 'E', k, s)
+        self.simulate_process('S', 'N', 'W', k, s)
+        return self.output
+
+# C++ O(N) O(1) Simulation Greedy
+class Solution {
+public:
+    void simulateProcess(char supportY, char enemyY, char supportX, int &output, int &k, std::string &s) {
+        int x = 0, y = 0, skipped = k;
+        for (char &move : s) {
+            if (move == supportY) ++y;
+            else if (move == enemyY) {
+                if (skipped) {
+                    --skipped;
+                    ++y;
+                } else --y;
+            }
+            else if (move == supportX) ++x;
+            else {
+                if (skipped) {
+                    --skipped;
+                    ++x;
+                } else --x;
+            }
+            output = std::max(output, std::abs(x) + std::abs(y));
+        }
+    }
+    int maxDistance(string s, int k) {
+        // common NE NW SE SW
+        int output = 0;
+        simulateProcess('N', 'S', 'E', output, k, s);
+        simulateProcess('N', 'S', 'W', output, k, s);
+        simulateProcess('S', 'N', 'E', output, k, s);
+        simulateProcess('S', 'N', 'W', output, k, s);
+        return output;
+    }
+};
