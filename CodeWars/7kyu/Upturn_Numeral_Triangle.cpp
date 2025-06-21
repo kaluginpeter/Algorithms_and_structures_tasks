6b162621785e500f590f6e00de0132f02d698e25
kaluginpeter <streetworkout200412@gmail.com>
2025-06-21 20:13:04 +0300

    Added description "Upturn Numeral Triangle"

---
+++ CodeWars/7kyu/Upturn_Numeral_Triangle.cpp
@@ -0,0 +1,47 @@
+/*
+Task
+Raj has got into a problem, he solved the triangle pattern but stuck in the codes of "inverse triangle". Help him with the codes and remember to get the output as per given in examples below.
+
+Rules:
+Take input as n which is always a natural number
+Space between each digit
+No space after the rows end
+Examples
+If n = 5, output should be:
+
+ 1 1 1 1 1
+  2 2 2 2
+   3 3 3
+    4 4
+     5
+If n = 9, output should be:
+
+ 1 1 1 1 1 1 1 1 1
+  2 2 2 2 2 2 2 2
+   3 3 3 3 3 3 3
+    4 4 4 4 4 4
+     5 5 5 5 5
+      6 6 6 6
+       7 7 7
+        8 8
+         9
+If n = 16, output should be:
+
+ 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+  2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+   3 3 3 3 3 3 3 3 3 3 3 3 3 3
+    4 4 4 4 4 4 4 4 4 4 4 4 4
+     5 5 5 5 5 5 5 5 5 5 5 5
+      6 6 6 6 6 6 6 6 6 6 6
+       7 7 7 7 7 7 7 7 7 7
+        8 8 8 8 8 8 8 8 8
+         9 9 9 9 9 9 9 9
+          0 0 0 0 0 0 0
+           1 1 1 1 1 1
+            2 2 2 2 2
+             3 3 3 3
+              4 4 4
+               5 5
+                6
+ASCII ArtFundamentals
+*/
