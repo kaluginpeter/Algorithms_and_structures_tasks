9514cddc38f8a0aec70a38e3b5e938ec2e5adaee
kaluginpeter <streetworkout200412@gmail.com>
2025-06-23 21:39:06 +0300

    Added solution "2081. Sum of k-Mirror Numbers"

--- LeetCode/Hard/2081._Sum_of_k_Mirror_Numbers.py
+++ LeetCode/Hard/2081._Sum_of_k_Mirror_Numbers.py
@@ -46,3 +46,88 @@
 #
 # 2 <= k <= 9
 # 1 <= n <= 30
+# Solution
+# Python O(sqrt(10^10)) O(1) Math
+class Solution:
+    digit: list[int] = [0] * 100
+
+    def is_palindrom(self, n: int, k: int) -> bool:
+        length: int = -1
+        while n:
+            length += 1
+            self.digit[length] = n % k
+            n //= k
+        left: int = 0
+        right: int = length
+        while left < right:
+            if self.digit[left] != self.digit[right]: return False
+            left += 1
+            right -= 1
+        return True
+
+    def kMirror(self, k: int, n: int) -> int:
+        self.digit = [0] * 100
+        left: int = 1
+        valid: int = 0
+        output: int = 0
+        while valid < n:
+            right: int = left * 10
+            for op in range(2):
+                i: int = left
+                while i < right and valid < n:
+                    combined: int = i
+                    x: int = [i // 10, i][op != 0]
+                    i += 1
+                    while x:
+                        combined = combined * 10 + x % 10
+                        x //= 10
+                    if self.is_palindrom(combined, k):
+                        valid += 1
+                        output += combined
+            left = right
+        return output
+
+# C++ O(sqrt(10^10)) O(1) Math
+class Solution {
+public:
+    std::vector<int> digit = std::vector<int>(100, 0);
+    bool isPalindrom(long long n, int k) {
+        int length = -1;
+        while (n) {
+            ++length;
+            digit[length] = n % k;
+            n /= k;
+        }
+        int left = 0, right = length;
+        while (left < length) {
+            if (digit[left] != digit[right]) return false;
+            ++left;
+            --right;
+        }
+        return true;
+    }
+
+    long long kMirror(int k, int n) {
+        int left = 1, valid = 0;
+        long long output = 0;
+        while (valid < n) {
+            int right = left * 10;
+            for (int op = 0; op < 2; ++op) {
+                for (int i = left; i < right && valid < n; ++i) {
+                    long long combined = i;
+                    int x = (!op? i / 10 : i);
+                    while (x) {
+                        combined = combined * 10 + x % 10;
+                        x /= 10;
+                    }
+                    if (isPalindrom(combined, k)) {
+                        ++valid;
+                        output += combined;
+                    }
+                }
+            }
+            left = right;
+        }
+        return output;
+    }
+};
