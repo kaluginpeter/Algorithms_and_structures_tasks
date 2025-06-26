fdd57a484e7b1dafff5ca62ea7b835311e6e4b89
kaluginpeter <streetworkout200412@gmail.com>
2025-06-26 08:50:36 +0300

    Added solution "Odd or Even?"

--- CodeWars/7kyu/Odd_or_Even.cpp
+++ CodeWars/7kyu/Odd_or_Even.cpp
@@ -19,3 +19,12 @@ Have fun!
 
 FundamentalsArrays
 */
+// Solution
+#include <string>
+#include <vector>
+#include <numeric>
+
+std::string odd_or_even(const std::vector<int> &arr)
+{
+    return std::accumulate(arr.begin(), arr.end(), 0) & 1 ? "odd" : "even";
+}
