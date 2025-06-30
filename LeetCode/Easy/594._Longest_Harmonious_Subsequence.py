9b0d8f4deb4ecd94d32036345712b04b19f6bb0a
kaluginpeter <streetworkout200412@gmail.com>
2025-06-30 08:43:02 +0300

    Added solution "594. Longest Harmonious Subsequence"

--- LeetCode/Easy/594._Longest_Harmonious_Subsequence.py
+++ LeetCode/Easy/594._Longest_Harmonious_Subsequence.py
@@ -36,3 +36,55 @@ class Solution:
             if i-1 in ht:
                 top = max(top, ht[i] + ht[i-1])
         return top
+
+
+# Python O(NlogN) O(1) Sorting SlidingWindow
+class Solution:
+    def findLHS(self, nums: List[int]) -> int:
+        output: int = 0
+        left: int = 0
+        nums.sort()
+        for right in range(len(nums)):
+            while left < right and nums[right] - nums[left] > 1: left += 1
+            if nums[right] - nums[left] == 1: output = max(output, right - left + 1)
+        return output
+
+# C++ O(NlogN) O(1) Sorting SlidingWindow
+class Solution {
+public:
+    int findLHS(vector<int>& nums) {
+        int output = 0;
+        int left = 0;
+        std::sort(nums.begin(), nums.end());
+        for (int right = 0; right < nums.size(); ++right) {
+            while (left < right && nums[right] - nums[left] > 1) ++left;
+            if (nums[right] - nums[left] == 1) output = std::max(output, right - left + 1);
+        }
+        return output;
+    }
+};
+
+# Python O(N) O(N) HashMap
+class Solution:
+    def findLHS(self, nums: List[int]) -> int:
+        output: int = 0
+        hashmap: dict[int, int] = dict()
+        for num in nums:
+            hashmap[num] = hashmap.get(num, 0) + 1
+        for num in nums:
+            if num - 1 in hashmap: output = max(output, hashmap[num] + hashmap[num - 1])
+        return output
+
+# C++ O(N) O(N) HashMap
+class Solution {
+public:
+    int findLHS(vector<int>& nums) {
+        int output = 0;
+        std::unordered_map<int, int> hashmap;
+        for (int &num : nums) ++hashmap[num];
+        for (int &num : nums) {
+            if (hashmap.count(num - 1)) output = std::max(output, hashmap[num] + hashmap[num - 1]);
+        }
+        return output;
+    }
+};
