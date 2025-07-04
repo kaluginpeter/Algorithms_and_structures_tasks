25a0b171861b65afdcd5c5598c76087e3410b734
kaluginpeter <streetworkout200412@gmail.com>
2025-07-04 21:19:17 +0300

    Added solution "2093B. Expensive Number"

--- Codeforces/2093B._Expensive_Number.py
+++ Codeforces/2093B._Expensive_Number.py
@@ -49,3 +49,62 @@
 # 4
 # 3
 # 0
+# Solution
+# C++ O(logN) O(logN) TwoPointers Greedy
+#include <iostream>
+#include <string>
+
+
+void solution() {
+    int t;
+    std::cin >> t;
+    for (int i = 0; i < t; ++i) {
+        std::string n = "";
+        std::cin >> n;
+        int left = 0;
+        for (int j = n.size() - 1; j >= 0; --j) {
+            if (n[j] != '0') {
+                left = j;
+                break;
+            }
+        }
+        int extra = 0;
+        for (int j = left + 1; j < n.size(); ++j) {
+            if (n[j] == '0') ++extra;
+        }
+        for (int j = 0; j < left; ++j) {
+            if (n[j] != '0') ++extra;
+        }
+        std::cout << extra << std::endl;
+
+    }
+}
+
+
+int main() {
+    solution();
+}
+
+# Python O(logN) O(logN) TwoPointers Greedy
+import sys
+
+
+def solution() -> None:
+    t: int = int(sys.stdin.readline().rstrip())
+    for _ in range(t):
+        sequence: str = sys.stdin.readline().rstrip()
+        left: int = 0
+        for i in range(len(sequence) - 1, -1, -1):
+            if sequence[i] != '0':
+                left = i
+                break
+        extra: int = 0
+        for i in range(left):
+            if sequence[i] != '0': extra += 1
+        for i in range(left + 1, len(sequence)):
+            if sequence[i] == '0': extra += 1
+        sys.stdout.write('{}\n'.format(extra))
+
+
+if __name__ == '__main__':
+    solution()
