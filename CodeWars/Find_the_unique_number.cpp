809a7d64d99cee5535078c1f8b720a14894b4c80
kaluginpeter <streetworkout200412@gmail.com>
2025-07-04 21:21:59 +0300

    Added description "Find the unique number"

---
+++ CodeWars/Find_the_unique_number.cpp
@@ -0,0 +1,16 @@
+/*
+There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+find_uniq(std::vector<float>{1, 1, 1, 2, 1, 1});  // --> 2
+find_uniq(std::vector<float>{0, 0, 0.55, 0, 0});  // --> 0.55
+It’s guaranteed that array contains at least 3 numbers.
+
+The tests contain some very huge arrays, so think about performance.
+
+This is the first kata in series:
+
+Find the unique number (this kata)
+Find the unique string
+Find The Unique
+FundamentalsAlgorithmsArraysPerformance
+*/
