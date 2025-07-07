3b7c878afe8cc9945e9f4f259a957dd22503f622
kaluginpeter <streetworkout200412@gmail.com>
2025-07-07 18:30:11 +0300

    Added solution "1353. Maximum Number of Events That Can Be Attended"

--- LeetCode/Medium/1353._Maximum_Number_of_Events_That_Can_Be_Attended.py
+++ LeetCode/Medium/1353._Maximum_Number_of_Events_That_Can_Be_Attended.py
@@ -27,3 +27,49 @@
 # 1 <= events.length <= 105
 # events[i].length == 2
 # 1 <= startDayi <= endDayi <= 105
+# Solution
+# Python O(N + T + NlogN) O(N) PriorityQueue SweepLine
+class Solution:
+    def maxEvents(self, events: List[List[int]]) -> int:
+        min_heap: list[int] = []
+        events.sort()
+        i: int = 0
+        n: int = len(events)
+        min_heap: list[int] = []
+        max_day: int = max(event[1] for event in events)
+        attended: int = 0
+        for day in range(max_day + 1):
+            while i < n and events[i][0] <= day:
+                heapq.heappush(min_heap, events[i][1])
+                i += 1
+            if min_heap:
+                attended += 1
+                heapq.heappop(min_heap)
+            while min_heap and min_heap[0] <= day: heapq.heappop(min_heap)
+        return attended
+
+# C++ O(N + T + NlogN) O(N) PriorityQueue SweepLine
+class Solution {
+public:
+    int maxEvents(vector<vector<int>>& events) {
+        std::priority_queue<int, std::vector<int>, std::greater<int>> minHeap;
+        int maxDay = 0;
+        for (std::vector<int> &event : events) {
+            maxDay = std::max(maxDay, event[1]);
+        }
+        std::sort(events.begin(), events.end());
+        int attended = 0, i = 0, n = events.size();
+        for (int day = 0; day <= maxDay; ++day) {
+            while (i < n && events[i][0] <= day) {
+                minHeap.push(events[i][1]);
+                ++i;
+            }
+            if (!minHeap.empty()) {
+                ++attended;
+                minHeap.pop();
+            }
+            while (!minHeap.empty() && minHeap.top() <= day) minHeap.pop();
+        }
+        return attended;
+    }
+};
