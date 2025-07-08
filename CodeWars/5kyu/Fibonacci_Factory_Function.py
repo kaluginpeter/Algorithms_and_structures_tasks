3ebb0e783d75cc066cd407dff98a1cdef2bbfb46
kaluginpeter <streetworkout200412@gmail.com>
2025-07-08 20:00:19 +0300

    Added description "Fibonacci Factory Function"

---
+++ CodeWars/5kyu/Fibonacci_Factory_Function.py
@@ -0,0 +1,10 @@
+# Create a factory function genfib that returns a function fib that always returns the next Fibonacci number on each invocation ( it should return 0 when being called the first time ).
+#
+# Example:
+#
+# fib = genfib()
+# fib() # returns 0
+# fib() # returns 1
+# fib() # returns 1
+# fib() # returns 2
+# MathematicsAlgorithms
