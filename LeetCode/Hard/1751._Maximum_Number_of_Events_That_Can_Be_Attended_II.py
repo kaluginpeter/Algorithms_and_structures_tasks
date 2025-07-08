33e52d9319104fa76bac386a56cb1c193a816252
kaluginpeter <streetworkout200412@gmail.com>
2025-07-08 19:54:48 +0300

    Added solution "1751. Maximum Number of Events That Can Be Attended II"

--- LeetCode/Hard/1751._Maximum_Number_of_Events_That_Can_Be_Attended_II.py
+++ LeetCode/Hard/1751._Maximum_Number_of_Events_That_Can_Be_Attended_II.py
@@ -36,3 +36,70 @@
 # 1 <= k * events.length <= 106
 # 1 <= startDayi <= endDayi <= 109
 # 1 <= valuei <= 106
+# Solution
+# Python O(KNlogN) O(KN) DynamicPrograming Sorting BinarySearch
+class Solution:
+    def get_leftmost_event(self, start: int, events: list[list[int]]) -> int:
+        left: int = 0
+        right: int = len(events) - 1
+        output: int = -1
+        while left <= right:
+            middle: int = left + ((right - left) >> 1)
+            if events[middle][1] >= start:
+                right = middle - 1
+            else:
+                output = middle
+                left = middle + 1
+        return output
+
+    def maxValue(self, events: List[List[int]], k: int) -> int:
+        n: int = len(events)
+        dp: list[list[int]] = [[0] * (n + 1) for _ in range(k + 1)]
+        events.sort(key=lambda event: (event[1], event[0]))
+        for i in range(1, k + 1):
+            for event in range(1, n + 1):
+                dp[i][event] = dp[i - 1][event]
+                leftmost_event: int = self.get_leftmost_event(events[event - 1][0], events)
+                dp[i][event] = max(
+                    dp[i][event - 1],
+                    events[event - 1][2] + [dp[i - 1][leftmost_event + 1], 0][leftmost_event == -1]
+                )
+        return dp[k][n]
+
+# C++ O(KNlogN) O(KN) DynamicProgramming BinarySearch Sorting
+class Solution {
+public:
+    int getLeftmostEvent(int&start, std::vector<std::vector<int>> &events) {
+        int left = 0, right = events.size() - 1;
+        int output = -1;
+        while (left <= right) {
+            int middle = left + ((right - left) >> 1);
+            if (events[middle][1] >= start) right = middle - 1;
+            else {
+                output = middle;
+                left = middle + 1;
+            }
+        }
+        return output;
+    }
+
+    int maxValue(vector<vector<int>>& events, int k) {
+        int n = events.size();
+        std::sort(events.begin(), events.end(), [](const std::vector<int> &x, const std::vector<int> &y){
+            if (x[1] != y[1]) return x[1] < y[1];
+            return x[0] < y[0];
+        });
+        std::vector<std::vector<int>> dp(k + 1, std::vector<int>(n + 1, 0));
+        for (int i = 1; i <= k; ++i) {
+            for (int event = 1; event <= n; ++event) {
+                dp[i][event] = dp[i - 1][event];
+                int leftMostEvent = getLeftmostEvent(events[event - 1][0], events);
+                dp[i][event] = std::max(
+                    dp[i][event - 1], // previous move
+                    events[event - 1][2] + (leftMostEvent == -1 ? 0 : dp[i - 1][leftMostEvent + 1]) // current move
+                );
+            }
+        }
+        return dp[k][n];
+    }
+};
