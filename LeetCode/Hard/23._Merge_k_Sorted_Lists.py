c6fb9b00d03d867a1eb549ecb26b521abc3364a9
kaluginpeter <streetworkout200412@gmail.com>
2025-07-09 20:36:25 +0300

    Added solution "23. Merge k Sorted Lists"

--- LeetCode/Hard/23._Merge_k_Sorted_Lists.py
+++ LeetCode/Hard/23._Merge_k_Sorted_Lists.py
@@ -34,3 +34,56 @@
 # -104 <= lists[i][j] <= 104
 # lists[i] is sorted in ascending order.
 # The sum of lists[i].length will not exceed 104.
+# Solution
+# Python O(NlogN) O(N) PriorityQueue
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        min_heap: list[int] = []
+        for linked_list in lists:
+            while linked_list:
+                heapq.heappush(min_heap, linked_list.val)
+                linked_list = linked_list.next
+        head: ListNode = ListNode()
+        tmp: ListNode = head
+        while min_heap:
+            tmp.next = ListNode(heapq.heappop(min_heap))
+            tmp = tmp.next
+        return head.next
+
+# C++ O(NlogN) O(N) PriorityQueue
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+            std::priority_queue<int, std::vector<int>, std::greater<int>> minHeap;
+            for (ListNode* &linkedList : lists) {
+                while (linkedList) {
+                    minHeap.push(linkedList->val);
+                    linkedList = linkedList->next;
+                }
+            }
+            ListNode *head = new ListNode();
+            ListNode *tmp = head;
+            while (!minHeap.empty()) {
+                ListNode *node = new ListNode(minHeap.top());
+                minHeap.pop();
+                tmp->next = node;
+                tmp = tmp->next;
+            }
+            return head->next;
+    }
+};
