4cb57159baa914903ad388d2b8d6eae9b4af0bcd
kaluginpeter <streetworkout200412@gmail.com>
2025-07-10 21:50:52 +0300

    Added solution "3440. Reschedule Meetings for Maximum Free Time II"

--- LeetCode/Medium/3440._Reschedule_Meetings_for_Maximum_Free_Time_II.py
+++ LeetCode/Medium/3440._Reschedule_Meetings_for_Maximum_Free_Time_II.py
@@ -67,3 +67,67 @@
 # 2 <= n <= 105
 # 0 <= startTime[i] < endTime[i] <= eventTime
 # endTime[i] <= startTime[i + 1] where i lies in the range [0, n - 2].
+# Solution
+# Python O(N) O(N) Prefix Suffix Array
+class Solution:
+    def maxFreeTime(self, eventTime: int, startTime: List[int], endTime: List[int]) -> int:
+        n: int = len(startTime)
+        segments: list[int] = [startTime[0]]
+        prefix_segments: list[int] = [startTime[0]]
+        for i in range(n - 1):
+            segments.append(startTime[i + 1] - endTime[i])
+            prefix_segments.append(max(prefix_segments[-1], startTime[i + 1] - endTime[i]))
+        segments.append(eventTime - endTime[-1])
+        prefix_segments.append(max(prefix_segments[-1], eventTime - endTime[-1]))
+
+        suffix_segments: list[int] = [0] * (n + 2)
+        suffix_segments[n + 1] = eventTime - endTime[-1]
+        for i in range(n - 1, 0, -1):
+            suffix_segments[i + 1] = max(suffix_segments[i + 2], startTime[i] - endTime[i - 1])
+        suffix_segments[0] = max(startTime[0], suffix_segments[1])
+
+        output: int = 0
+        for i in range(1, n + 1):
+            event_length: int = endTime[i - 1] - startTime[i - 1]
+            extra_space: int = segments[i - 1] + segments[i]
+            if max(
+                    prefix_segments[i - 2] if i - 2 >= 0 else 0,
+                    suffix_segments[i + 2] if i + 2 < n + 2 else 0
+            ) >= event_length: extra_space += event_length
+            output = max(output, extra_space)
+        return output
+
+# C++ O(N) O(N) Prefix Suffix Array
+class Solution {
+public:
+    int maxFreeTime(int eventTime, vector<int>& startTime, vector<int>& endTime) {
+        int n = startTime.size();
+        std::vector<int> prefixSegments, segments;
+        prefixSegments.push_back(startTime[0]);
+        segments.push_back(startTime[0]);
+        for (int i = 0; i < n - 1; ++i) {
+            prefixSegments.push_back(std::max(prefixSegments.back(), startTime[i + 1] - endTime[i]));
+            segments.push_back(startTime[i + 1] - endTime[i]);
+        }
+        prefixSegments.push_back(std::max(prefixSegments.back(), eventTime - endTime.back()));
+        segments.push_back(eventTime - endTime.back());
+
+        std::vector<int> suffixSegments(n + 2, 0);
+        suffixSegments[n + 1] = eventTime - endTime.back();
+        for (int i = n - 1; i > 0; --i) {
+            suffixSegments[i + 1] = std::max(suffixSegments[i + 2], startTime[i] - endTime[i - 1]);
+        }
+        suffixSegments[0] = std::max(suffixSegments[1], startTime[0]);
+        int output = 0;
+        for (int i = 1; i <= n; ++i) {
+            int currentEventLength = endTime[i - 1] - startTime[i - 1];
+            int extraSpace = segments[i - 1] + segments[i];
+            if (std::max(
+                (i - 2 >= 0 ? prefixSegments[i - 2] : 0),
+                (i + 2 <= n + 1 ? suffixSegments[i + 2] : 0)
+            ) >= currentEventLength) extraSpace += currentEventLength;
+            output = std::max(output, extraSpace);
+        }
+        return output;
+    }
+};
