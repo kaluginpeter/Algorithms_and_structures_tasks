9e819dba9fdb98aadfaddb365c65d015a436bf1b
kaluginpeter <streetworkout200412@gmail.com>
2025-07-10 21:59:11 +0300

    Added description "Finding queens on the board"

---
+++ CodeWars/7kyu/Finding_queens_on_the_board.py
@@ -0,0 +1,51 @@
+# Your task is to find the maximum number of queens that can be put on the board so that there would be one single unbeaten square (ie. threatened by no queen on the board).
+#
+# The Queen can move any distance vertically, horizontally and diagonally.
+#
+# Input
+# The queens(n) function takes the size of the chessboard.
+#
+# n
+# ∈
+# Z
+# n∈Z, so it can be negative, and values can go up to
+# 14
+# 1
+# 1000
+# 141
+# 1000
+#  .
+#
+# Output
+# The maximum number of queens to leave one single unbeaten square
+# Return 0 if n is negative.
+# Examples
+# n
+# =
+# 4
+# →
+# 6
+#
+# q
+# u
+# e
+# e
+# n
+# s
+# n=4→6 queens
+# n
+# =
+# 5
+# →
+# 12
+#
+# q
+# u
+# e
+# e
+# n
+# s
+# n=5→12 queens
+# Good luck )
+#
+# Algorithms
