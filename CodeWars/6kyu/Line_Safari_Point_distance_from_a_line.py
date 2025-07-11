7ba0288ffcb29288d6f82460e1e9859d198fddbb
kaluginpeter <streetworkout200412@gmail.com>
2025-07-11 17:42:59 +0300

    Added solution "Line Safari : Point distance from a line"

--- CodeWars/6kyu/Line_Safari_Point_distance_from_a_line.py
+++ CodeWars/6kyu/Line_Safari_Point_distance_from_a_line.py
@@ -14,3 +14,18 @@
 # if a and b are the same then just return distance between a and c
 # use Euclidean distance
 # GeometryAlgorithms
+# Solution
+from math import sqrt
+
+def distance(p1, p2):
+    return sqrt((p2[0] - p1[0])**2 + (p2[1] - p1[1])**2)
+
+def distance_from_line(a, b, c):
+    x1, y1 = a
+    x2, y2 = b
+    x0, y0 = c
+    if a == b: return distance(a, c)
+    numerator = abs((x2 - x1) * (y1 - y0) - (x1 - x0) * (y2 - y1))
+    denominator = distance(a, b)
+    if denominator == 0: return distance(a, c)
+    return numerator / denominator
