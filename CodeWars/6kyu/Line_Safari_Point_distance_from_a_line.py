5b5428707502018d4a0b5311434afdbcedb3e2cc
kaluginpeter <streetworkout200412@gmail.com>
2025-07-11 17:42:20 +0300

    Added description "Line Safari : Point distance from a line"

---
+++ CodeWars/6kyu/Line_Safari_Point_distance_from_a_line.py
@@ -0,0 +1,16 @@
+# Kata Task
+# Given 3 points a, b, c
+#
+#             c
+#
+#                      b
+#   a
+#
+# Find the shortest distance from point c to a straight line that passes through points a and b
+#
+# Notes
+#
+# all points are of the form [x,y] where x >= 0 and y >= 0
+# if a and b are the same then just return distance between a and c
+# use Euclidean distance
+# GeometryAlgorithms
