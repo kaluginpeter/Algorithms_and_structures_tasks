c917940e2453533fd0dbb0e4d79d39a483772df0
kaluginpeter <streetworkout200412@gmail.com>
2025-07-12 20:15:09 +0300

    Added description "Find the random seed"

---
+++ CodeWars/7kyu/Find_the_random_seed.py
@@ -0,0 +1,11 @@
+# Given a list of random integers, return the integer used to initialize the random number generator (the "seed") (0 <= n < 10000)
+#
+# The input list is comprised of the first 10 random integers between 0 and 100 (inclusive) produced after initializing the random number generator from the random module.
+#
+# Examples
+# input: [17, 72, 97, 8, 32, 15, 63, 97, 57, 60]
+# expected: 1
+#
+# input: [99, 56, 14, 0, 11, 74, 4, 85, 88, 10]
+# expected: 1234
+# Fundamentals
