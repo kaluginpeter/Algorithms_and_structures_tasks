9094f1f72ce90ef1f70711e877bc5a00b3c68264
kaluginpeter <streetworkout200412@gmail.com>
2025-07-13 16:06:18 +0300

    Added description "Circle cipher"

---
+++ CodeWars/7kyu/Circle_cipher.py
@@ -0,0 +1,13 @@
+# Imagine a circle. To encode the word codewars, we could split the circle into 8 parts (as codewars has 8 letters):
+#
+# Then add the letters in a clockwise direction:Then remove the circle:
+# If we read the result from left to right, we get csordaew.
+#
+# Decoding is the same process in reverse. If we decode csordaew, we get codewars.
+#
+# Examples:
+# encode "codewars" -> "csordaew"
+# decode "csordaew" -> "codewars"
+# encode "white" -> "wehti"
+# decode "wehti" -> "white"
+# CiphersAlgorithms
