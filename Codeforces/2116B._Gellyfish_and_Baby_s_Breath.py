29674dc5f3d63cdb60b48d81da8a6688d1042c5c
kaluginpeter <streetworkout200412@gmail.com>
2025-07-14 22:13:54 +0300

    Added solution "2116B. Gellyfish and Baby's Breath"

--- Codeforces/2116B._Gellyfish_and_Baby_s_Breath.py
+++ Codeforces/2116B._Gellyfish_and_Baby_s_Breath.py
@@ -88,3 +88,91 @@
 # r0=2p0+2q0=1+2=3
 # r1=max(2p0+2q1,2p1+2q0)=max(1+4,4+2)=6
 # r2=max(2p0+2q2,2p1+2q1,2p2+2q0)=(1+1,4+4,2+2)=8
+# Solution
+# C++ O(NlogN) O(1) Greedy
+#include <iostream>
+#include <vector>
+
+
+long long MOD = 998244353;
+
+int powMod(long long base, long long exponent, long long mod) {
+    long long output = 1;
+    while (exponent >= 1) {
+        if (exponent & 1) {
+            output = (output * base) % mod;
+            --exponent;
+        } else {
+            base = (base * base) % mod;
+            exponent /= 2;
+        }
+    }
+    return output;
+}
+
+void solution() {
+    int n;
+    std::cin >> n;
+    std::vector<long long> p(n, 0), q(n, 0);
+    for (int i = 0; i < n; ++i) std::cin >> p[i];
+    for (int i = 0; i < n; ++i) std::cin >> q[i];
+    int maxLeft = 0, maxRight = 0;
+    for (int i = 0; i < n; ++i) {
+        if (p[i] > p[maxLeft]) maxLeft = i;
+        else if (p[i] == p[maxLeft] && q[i - maxLeft] < q[0]) maxLeft = i;
+        if (q[maxRight] < q[i]) maxRight = i;
+        else if (q[maxRight] == q[i] && p[i - maxRight] < p[0]) maxRight = i;
+
+        if (p[maxLeft] > q[maxRight]) {
+            std::cout << (powMod(2, p[maxLeft], MOD) + powMod(2, q[i - maxLeft], MOD)) % MOD;
+        } else if (p[maxLeft] == q[maxRight] && q[i - maxLeft] >= p[i - maxRight]) {
+            std::cout << (powMod(2, p[maxLeft], MOD) + powMod(2, q[i - maxLeft], MOD)) % MOD;
+        } else {
+            std::cout << (powMod(2, q[maxRight], MOD) + powMod(2, p[i - maxRight], MOD)) % MOD;
+        }
+        std::cout << " ";
+    }
+    std::cout << std::endl;
+}
+
+
+int main() {
+    int t;
+    std::cin >> t;
+    while (t--) solution();
+}
+
+# Python O(NlogN) O(1) Greedy
+import sys
+
+
+def solution() -> None:
+    mod: int = 998244353
+    n: int = int(sys.stdin.readline().rstrip())
+    p: list[int] = list(map(int, sys.stdin.readline().rstrip().split()))
+    q: list[int] = list(map(int, sys.stdin.readline().rstrip().split()))
+    max_left: int = 0
+    max_right: int = 0
+    for i in range(n):
+        if p[i] > p[max_left]:
+            max_left = i
+        elif p[i] == p[max_left] and q[0] > q[i - max_left]:
+            max_left = i
+        if q[i] > q[max_right]:
+            max_right = i
+        elif q[i] == q[max_right] and p[0] > p[i - max_right]:
+            max_right = i
+
+        if p[max_left] > q[max_right]:
+            sys.stdout.write(str((pow(2, p[max_left], mod) + pow(2, q[i - max_left], mod)) % mod))
+        elif p[max_left] == q[max_right] and q[i - max_left] >= p[i - max_right]:
+            sys.stdout.write(str((pow(2, p[max_left], mod) + pow(2, q[i - max_left], mod)) % mod))
+        else:
+            sys.stdout.write(str((pow(2, q[max_right], mod) + pow(2, p[i - max_right], mod)) % mod))
+        sys.stdout.write(' ')
+    sys.stdout.write('\n')
+
+
+if __name__ == '__main__':
+    t: int = int(sys.stdin.readline().rstrip())
+    for _ in range(t): solution()
