776b058a2cf7301dfd8f9692e0d8e48696d19988
kaluginpeter <streetworkout200412@gmail.com>
2025-07-16 18:15:48 +0300

    Added solution "3201. Find the Maximum Length of Valid Subsequence I"

--- LeetCode/Medium/3201._Find_the_Maximum_Length_of_Valid_Subsequence_I.py
+++ LeetCode/Medium/3201._Find_the_Maximum_Length_of_Valid_Subsequence_I.py
@@ -51,3 +51,49 @@
 #
 # 2 <= nums.length <= 2 * 105
 # 1 <= nums[i] <= 107
+# Solution
+# Python O(N) O(1) Greedy
+class Solution:
+    def simulate(self, nums: list[int], has_even: bool, is_diff: bool) -> int:
+        output: int = 0
+        for num in nums:
+            if has_even and not (num & 1):
+                if is_diff: has_even = not has_even
+                output += 1
+            elif not has_even and num & 1:
+                if is_diff: has_even = not has_even
+                output += 1
+        return output
+
+    def maximumLength(self, nums: List[int]) -> int:
+        first: int = max(self.simulate(nums, False, True), self.simulate(nums, True, True))
+        second: int = max(self.simulate(nums, False, False), self.simulate(nums, True, False))
+        return max(first, second)
+
+# C++ O(N) O(1) Greedy
+class Solution {
+public:
+    int simulate(std::vector<int> &nums, bool needEven, bool isDiff) {
+        int output = 0;
+        for (int &num : nums) {
+            if (needEven && !(num & 1)) {
+                if (isDiff) needEven = !needEven;
+                ++output;
+            } else if (!needEven && (num & 1)) {
+                if (isDiff) needEven = !needEven;
+                ++output;
+            }
+        }
+        return output;
+    }
+
+    int maximumLength(vector<int>& nums) {
+        // 1st case, start with odd
+        // even + odd, odd + even
+        int first = std::max(simulate(nums, false, true), simulate(nums, true, true));
+        // 2nd case, start with even
+        // even + even, odd + odd
+        int second = std::max(simulate(nums, false, false), simulate(nums, true, false));
+        return std::max(first, second);
+    }
+};
