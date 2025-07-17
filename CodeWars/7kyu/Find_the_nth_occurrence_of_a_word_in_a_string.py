ddaca61febabc65d31bb8a55543cc10bfd4317c0
kaluginpeter <streetworkout200412@gmail.com>
2025-07-17 18:25:49 +0300

    Added description "Find the nth occurrence of a word in a string!"

---
+++ CodeWars/7kyu/Find_the_nth_occurrence_of_a_word_in_a_string.py
@@ -0,0 +1,21 @@
+# Description
+# You are required to implement a function that returns the index of the nth occurrence of a substring within a string (considering that those substring could overlap each other). If there are less than n occurrences of the substring, return -1.
+#
+# Example
+# substring = "example"
+# string = "This is an example. Return the nth occurrence of example in this example string."
+#
+# substring, string, 1 --> 11
+# substring, string, 2 --> 49
+# substring, string, 3 --> 65
+# substring, string, 4 --> -1
+# Multiple occurrences of a substring are allowed to overlap, e.g.
+#
+# substring = "TestTest"
+# string = "TestTestTestTest"
+#
+# substring, string, 1 --> 0
+# substring, string, 2 --> 4
+# substring, string, 3 --> 8
+# substring, string, 4 --> -1
+# Fundamentals
