1426cacfc15bbe83d2b9bb7d7fe3cbb8f0a42763
kaluginpeter <streetworkout200412@gmail.com>
2025-07-17 18:34:28 +0300

    Added solution "3202. Find the Maximum Length of Valid Subsequence II"

--- LeetCode/Medium/3202._Find_the_Maximum_Length_of_Valid_Subsequence_II.py
+++ LeetCode/Medium/3202._Find_the_Maximum_Length_of_Valid_Subsequence_II.py
@@ -32,3 +32,32 @@
 # 2 <= nums.length <= 103
 # 1 <= nums[i] <= 107
 # 1 <= k <= 103
+# Solution
+# Python O(NK) O(NK) DynamicProgramming
+class Solution:
+    def maximumLength(self, nums: List[int], k: int) -> int:
+        n: int = len(nums)
+        dp: list[list[int]] = [[0] * k for _ in range(n + 1)]
+        for i in range(1, n + 1):
+            for j in range(i - 1, 0, -1):
+                remainder: int = (nums[i - 1] + nums[j - 1]) % k
+                dp[i][remainder] = max(dp[i][remainder], max(dp[j][remainder], 1) + 1)
+        return max(max(chunk) for chunk in dp)
+
+# C++ O(NK) O(NK) DynamicProgramming
+class Solution {
+public:
+    int maximumLength(vector<int>& nums, int k) {
+        int n = nums.size();
+        std::vector<std::vector<int>> dp(n + 1, std::vector<int>(k, 0));
+        for (int i = 1; i <= n; ++i) {
+            for (int j = i - 1; j > 0; --j) {
+                int remainder = (nums[i - 1] + nums[j - 1]) % k;
+                dp[i][remainder] = std::max(dp[i][remainder], std::max(1, dp[j][remainder]) + 1);
+            }
+        }
+        int output = 0;
+        for (std::vector<int> &state : dp) output = std::max(output, *std::max_element(state.begin(), state.end()));
+        return output;
+    }
+};
