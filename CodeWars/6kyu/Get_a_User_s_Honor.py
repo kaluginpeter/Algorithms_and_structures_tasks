257a11bc34b9cfce1f4dc9bfd3861b19e65f612a
kaluginpeter <streetworkout200412@gmail.com>
2025-07-21 18:49:35 +0300

    Added description "Get a User's Honor"

---
+++ CodeWars/6kyu/Get_a_User_s_Honor.py
@@ -0,0 +1,24 @@
+# Task:
+# Write a function get_honor which accepts a username from someone at Codewars and returns an integer containing the user's honor. If input is invalid, raise an error.
+#
+# If you want/don't want your username to be in the tests, ask me in the discourse area. There can't be too many though because the server may time out.
+# Example¹:
+# >>> get_honor('dpleshkov')
+# 4418
+# >>> get_honor('jhoffner')
+# 21949
+# ¹ Honor may or may not be current to the user
+#
+# Libraries/Recommendations:
+# Fsharp:
+# open System.Net: use this namespace for opening a webpage(It's just a suggestion).
+# open System.Text.RegularExpressions: this namepsace will give you access to Regex.
+# Python:
+# urllib.request.urlopen: Opens up a webpage.
+# re: The RegEx library for Python.
+# bs4(BeautifulSoup): A tool for scraping HTML and XML.
+# Notes:
+# Time out / server errors often happen with the test cases depending on the status of the codewars website. Try submitting your code a few times or at different hours of the day if needed.
+# Feel free to voice your comments and concerns in the discourse area.
+# There is no example tests. Sorry, the honor may vary from time to time. I apologize for the inconvenience.
+# StringsRegular ExpressionsFundamentals
