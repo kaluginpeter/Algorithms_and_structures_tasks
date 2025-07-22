9a0b48b013ef4459e9e71b55c1a848520328d822
kaluginpeter <streetworkout200412@gmail.com>
2025-07-22 22:31:19 +0300

    Added solution "2126D. This Is the Last Time"

--- Codeforces/2126D._This_Is_the_Last_Time.py
+++ Codeforces/2126D._This_Is_the_Last_Time.py
@@ -85,3 +85,85 @@
 # In the fourth test case, it is beneficial to play at the 2
 # -nd casino right away and earn 4
 #  coins.
+# Solution
+# C++ O(NlogN) O(N) PriorityQueue
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <tuple>
+#include <set>
+
+struct Casino {
+    int l, r, real;
+};
+
+bool compareCasinos(const Casino& a, const Casino& b) {
+    return a.l < b.l;
+}
+
+void solution() {
+    int n;
+    long long k;
+    std::cin >> n >> k;
+    std::vector<Casino> casinos(n);
+    for (int i = 0; i < n; ++i) {
+        std::cin >> casinos[i].l >> casinos[i].r >> casinos[i].real;
+    }
+    std::sort(casinos.begin(), casinos.end(), compareCasinos);
+    std::multiset<std::pair<int, int>> available_casinos;
+    int casino_idx = 0;
+
+    while (true) {
+        while (casino_idx < n && casinos[casino_idx].l <= k) {
+            available_casinos.insert({casinos[casino_idx].real, casinos[casino_idx].r});
+            ++casino_idx;
+        }
+        while (!available_casinos.empty() && available_casinos.rbegin()->second < k) {
+            available_casinos.erase(std::prev(available_casinos.end()));
+        }
+        if (available_casinos.empty() || available_casinos.rbegin()->first <= k) break;
+        k = available_casinos.rbegin()->first;
+        available_casinos.erase(std::prev(available_casinos.end()));
+    }
+    std::cout << k << "\n";
+}
+
+int main() {
+    int t;
+    std::cin >> t;
+    while (t--) solution();
+}
+
+# Python O(NlogN) PriorityQueue
+import heapq
+import sys
+
+
+def solution() -> None:
+    n, k = map(int, sys.stdin.readline().split())
+
+    casinos: list[tuple[int, int, int]] = []
+    for _ in range(n):
+        l, r, real = map(int, sys.stdin.readline().split())
+        casinos.append((l, r, real))
+    casinos.sort()
+    available_casinos_heap: list[tuple[int, int]] = []
+    casino_idx: int = 0
+    while True:
+        while casino_idx < n and casinos[casino_idx][0] <= k:
+            l, r, real = casinos[casino_idx]
+            heapq.heappush(available_casinos_heap, (-real, r))
+            casino_idx += 1
+        while available_casinos_heap and available_casinos_heap[0][1] < k:
+            heapq.heappop(available_casinos_heap)
+        if not available_casinos_heap or -available_casinos_heap[0][0] <= k:
+            break
+        best_payout, _ = heapq.heappop(available_casinos_heap)
+        k = -best_payout
+
+    sys.stdout.write('{}\n'.format(k))
+
+
+if __name__ == "__main__":
+    t: int = int(sys.stdin.readline().rstrip())
+    for _ in range(t): solution()
