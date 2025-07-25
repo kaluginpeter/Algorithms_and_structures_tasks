401510af073cdaa1de63af94b00cd481a62561e5
kaluginpeter <streetworkout200412@gmail.com>
2025-07-25 09:50:30 +0300

    Added solution "What positions are covered?"

--- CodeWars/6kyu/What_positions_are_covered.py
+++ CodeWars/6kyu/What_positions_are_covered.py
@@ -17,3 +17,43 @@
 # Good luck :P
 #
 # AlgorithmsMatrix
+# Solution
+def squares_covered(pos: str, piece: str) -> list[list[int]]:
+    board = [[0 for _ in range(8)] for _ in range(8)]
+
+    col_char = pos[0]
+    row_char = pos[1]
+    col = ord(col_char) - ord('a')
+    row = 8 - int(row_char)
+    board[row][col] = -1
+    rook_dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    bishop_dirs = [(-1, -1), (-1, 1), (1, -1), (1, 1)]
+    knight_moves = [(-2, -1), (-2, 1), (-1, -2), (-1, 2),
+                    (1, -2), (1, 2), (2, -1), (2, 1)]
+    king_moves = rook_dirs + bishop_dirs
+    if piece in ('R', 'Q'):
+        for dr, dc in rook_dirs:
+            nr, nc = row + dr, col + dc
+            while 0 <= nr < 8 and 0 <= nc < 8:
+                board[nr][nc] = 1
+                nr += dr
+                nc += dc
+    if piece in ('B', 'Q'):
+        for dr, dc in bishop_dirs:
+            nr, nc = row + dr, col + dc
+            while 0 <= nr < 8 and 0 <= nc < 8:
+                board[nr][nc] = 1
+                nr += dr
+                nc += dc
+    if piece == 'N':
+        for dr, dc in knight_moves:
+            nr, nc = row + dr, col + dc
+            if 0 <= nr < 8 and 0 <= nc < 8:
+                board[nr][nc] = 1
+    if piece == 'K':
+        for dr, dc in king_moves:
+            nr, nc = row + dr, col + dc
+            if 0 <= nr < 8 and 0 <= nc < 8:
+                board[nr][nc] = 1
+
+    return board
