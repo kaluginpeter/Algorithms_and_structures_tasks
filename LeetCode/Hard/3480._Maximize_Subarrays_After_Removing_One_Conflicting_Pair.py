bb413e4c86c1b2e7cfadb6c26d63bbc28e0997bb
kaluginpeter <streetworkout200412@gmail.com>
2025-07-26 13:23:24 +0300

    Added solution "3480. Maximize Subarrays After Removing One Conflicting Pair"

--- LeetCode/Hard/3480._Maximize_Subarrays_After_Removing_One_Conflicting_Pair.py
+++ LeetCode/Hard/3480._Maximize_Subarrays_After_Removing_One_Conflicting_Pair.py
@@ -37,3 +37,42 @@
 # conflictingPairs[i].length == 2
 # 1 <= conflictingPairs[i][j] <= n
 # conflictingPairs[i][0] != conflictingPairs[i][1]
+# Solution
+# Python O(N + P) O(N) SweepLine
+class Solution:
+    def maxSubarrays(self, n: int, conflictingPairs: List[List[int]]) -> int:
+        rights: list[list[int]] = [[] for _ in range(n + 1)]
+        for a, b in conflictingPairs:
+            rights[max(a, b)].append(min(a, b))
+        output: int = 0
+        left: list[int] = [0, 0]
+        bonus: list[int] = [0] * (n + 1)
+        for right in range(1, n + 1):
+            for l in rights[right]:
+                if l > left[0]: left = [l, left[0]]
+                elif l > left[1]: left[1] = l
+            output += right - left[0]
+            if left[0] > 0: bonus[left[0]] += left[0] - left[1]
+        return output + max(bonus)
+
+# C++ O(N + P) O(N) SweepLine
+class Solution {
+public:
+    long long maxSubarrays(int n, std::vector<std::vector<int>>& conflictingPairs) {
+        std::vector<std::vector<int>> rights(n + 1, std::vector<int>());
+        for (const auto& pair : conflictingPairs) {
+            rights[std::max(pair[0], pair[1])].push_back(std::min(pair[0], pair[1]));
+        }
+        long long output = 0;
+        std::vector<long long> left = {0, 0}, bonus(n + 1, 0);
+        for (int right = 1; right <= n; ++right) {
+            for (int l : rights[right]) {
+                if (l > left[0]) left = {static_cast<long long>(l), left[0]};
+                else if (l > left[1]) left = {left[0], static_cast<long long>(l)};
+            }
+            output += right - left[0];
+            if (left[0] > 0) bonus[left[0]] += left[0] - left[1];
+        }
+        return output + *std::max_element(bonus.begin(), bonus.end());
+    }
+};
