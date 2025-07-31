c34b38646f3057d212c317601519d73c5a80b63f
kaluginpeter <streetworkout200412@gmail.com>
2025-07-31 14:54:42 +0300

    Added description "Codewars Leaderboard"

---
+++ CodeWars/6kyu/Codewars_Leaderboard.py
@@ -0,0 +1,6 @@
+# Get the list of integers for Codewars Leaderboard score (aka Honor) in descending order
+#
+# Note:
+# if it was the bad timing, the data could be updated during your test cases.
+# Try several times if you had such experience.
+# Fundamentals
