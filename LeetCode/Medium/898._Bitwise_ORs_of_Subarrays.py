59702830a7830bbfe7c5118d41f777f57f08f371
kaluginpeter <streetworkout200412@gmail.com>
2025-07-31 12:32:32 +0300

    Added solution "898. Bitwise ORs of Subarrays"

--- LeetCode/Medium/898._Bitwise_ORs_of_Subarrays.py
+++ LeetCode/Medium/898._Bitwise_ORs_of_Subarrays.py
@@ -29,3 +29,32 @@
 #
 # 1 <= arr.length <= 5 * 104
 # 0 <= arr[i] <= 109
+# Solution
+# Python O(NlogM) O(NlogM) BitManipulation
+class Solution:
+    def subarrayBitwiseORs(self, arr: List[int]) -> int:
+        subsets: set[int] = {0}
+        output: set[int] = set()
+        for num in arr:
+            subsets = {or_ | num for or_ in subsets} | {num}
+            output |= subsets
+        return len(output)
+
+# C++ O(NlogM) O(NlogM) BitManipulation
+class Solution {
+public:
+    int subarrayBitwiseORs(vector<int>& arr) {
+        std::unordered_set<int> seen = {0}, output;
+        int n = arr.size();
+        for (int i = 0; i < n; ++i) {
+            std::unordered_set<int> newSubs;
+            for (std::unordered_set<int>::const_iterator it = seen.cbegin(); it != seen.cend(); ++it) {
+                newSubs.insert(*it | arr[i]);
+            }
+            newSubs.insert(arr[i]);
+            seen = newSubs;
+            for (auto &num : seen) output.insert(num);
+        }
+        return output.size();
+    }
+};
