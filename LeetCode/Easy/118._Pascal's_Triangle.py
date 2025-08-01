14bb0fa5544a44b93577080dd892ae3c0466a01e
kaluginpeter <streetworkout200412@gmail.com>
2025-08-01 11:29:28 +0300

    Added solution "118. Pascal's Triangle"

--- LeetCode/Easy/118._Pascal's_Triangle.py
+++ LeetCode/Easy/118._Pascal's_Triangle.py
@@ -28,3 +28,40 @@ class Solution:
             row = [x + y for x, y in zip([0] + row, row + [0])]
             triangle.append(row)
         return triangle
+
+
+# Python O(N^2) O(N^2) Simulation
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        output: list[list[int]] = []
+        cur_row: list[int] = [1]
+        next_row: list[int] = []
+        for _ in range(numRows):
+            output.append(cur_row.copy())
+            next_row.clear()
+            next_row.append(1)
+            for i in range(len(cur_row) - 1):
+                next_row.append(cur_row[i] + cur_row[i + 1])
+            next_row.append(1)
+            cur_row = next_row[::]
+        return output
+
+# C++ O(N^2) O(N^2) Simulation
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        std::vector<int> curRow = {1}, nextRow;
+        std::vector<std::vector<int>> output;
+        for (int i = 0; i < numRows; ++i) {
+            output.push_back(curRow);
+            nextRow.clear();
+            nextRow.push_back(1);
+            for (int j = 0; j < curRow.size() - 1; ++j) {
+                nextRow.push_back(curRow[j] + curRow[j + 1]);
+            }
+            nextRow.push_back(1);
+            curRow = nextRow;
+        }
+        return output;
+    }
+};
