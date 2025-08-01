b671cd32239356a743bdb62128fe0f6833d04d19
kaluginpeter <streetworkout200412@gmail.com>
2025-08-01 11:18:19 +0300

    Added description "Codewars API"

---
+++ CodeWars/5kyu/Codewars_API.py
@@ -0,0 +1,11 @@
+# Get the list of integers for 'totalAuthored' and 'totalCompleted' of the nth ranker at Codewars Leaderboard.
+#
+# (1 <= n <= 500)
+#
+# See Example Test Cases for the expected data format.
+#
+# (Note 1 : Mind the title of this kata as well as https://dev.codewars.com/ )
+#
+# (Note 2 : It is recommended to finish Codewars Leaderboard before this kata. )
+#
+# Fundamentals
