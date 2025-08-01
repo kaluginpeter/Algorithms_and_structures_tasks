1cfb73e73bcb95bea080aaf8ea23dce2107e4a7f
kaluginpeter <streetworkout200412@gmail.com>
2025-08-01 11:18:42 +0300

    Added solution "Codewars API"

--- CodeWars/5kyu/Codewars_API.py
+++ CodeWars/5kyu/Codewars_API.py
@@ -9,3 +9,19 @@
 # (Note 2 : It is recommended to finish Codewars Leaderboard before this kata. )
 #
 # Fundamentals
+# Solution
+from bs4 import BeautifulSoup
+from requests import Response, get
+def get_code_challenges(n: int) -> list[int]:
+    response: Response = get('https://www.codewars.com/users/leaderboard')
+    soup: BeautifulSoup = BeautifulSoup(response.content, 'html.parser')
+    output: list[int] = []
+    i: int = 0
+    for user in soup.find('div', class_='leaderboard p-0').table:
+        user_data: str = user.find('td', class_='is-big')
+        if not user_data: continue
+        print(user_data.a.text)
+        i += 1
+        if i != n: continue
+        response_api: dict[str, int] = get(f'https://www.codewars.com/api/v1/users/{user_data.a.text}').json()['codeChallenges']
+        return [response_api['totalAuthored'], response_api['totalCompleted']]
