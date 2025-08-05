21c4fc22131318c9619aa3c3c4efb288b4c2db3d
kaluginpeter <streetworkout200412@gmail.com>
2025-08-05 18:05:53 +0300

    Added description "Subset Sum"

---
+++ CodeWars/5kyu/Subset_Sum.py
@@ -0,0 +1,10 @@
+# Given a possibly empty list of strictly positive numbers and a non-negative target number, return either a subset of the list summing to the target, or null or a similar empty value if no such subset exists.
+#
+# The subset must consist of unique ( by index ) list elements.
+# If a particular value occurs more than once in the input list, you can use it up to as many times as it occurs.
+# The empty subset sums to 0.
+# If multiple valid subsets exist, return any one of them.
+#
+# The target will never be much bigger than the sum of the input list, and often quite a bit smaller.
+#
+# Performance
