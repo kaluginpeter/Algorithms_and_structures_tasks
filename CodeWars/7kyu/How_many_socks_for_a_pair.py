a957c00da7bb678f5e430c4d874e00ffa613a03d
kaluginpeter <streetworkout200412@gmail.com>
2025-08-07 17:57:53 +0300

    Added description "How many socks for a pair?"

---
+++ CodeWars/7kyu/How_many_socks_for_a_pair.py
@@ -0,0 +1,16 @@
+# I have a number of different colours of socks mixed up in a drawer and I require a number of matching pairs of socks. Fortunately, I don't mind what colour the pairs are and I have an infinite number of each colour of sock.
+#
+# Your task is to create a function with inputs colours and pairs that returns the minimum number of socks that I would have to take from my drawer to guarantee pairs number of matching pairs.
+#
+# Example:
+# for colours = 2, pairs = 1, return 3, because it takes 3 socks to guarantee 1 pair of socks with 2 different possible colours to select from.
+#
+# More examples:
+#
+# for colours = 1, pairs = 2: return 4
+# for colours = 2, pairs = 3: return 7
+# for colours = 4, pairs = 2: return 7
+# for colours = 4, pairs = 4: return 11
+# Note: colours and pairs are both positive integers.
+#
+# FundamentalsMathematics
