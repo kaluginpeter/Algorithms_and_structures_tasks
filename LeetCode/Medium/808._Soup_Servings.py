9660e886939d6f9d39dec2291becb14660702157
kaluginpeter <streetworkout200412@gmail.com>
2025-08-08 14:30:39 +0300

    Added solution "808. Soup Servings"

--- LeetCode/Medium/808._Soup_Servings.py
+++ LeetCode/Medium/808._Soup_Servings.py
@@ -48,3 +48,49 @@ class Solution:
             d[(x, y)] = 0.25 * (dyn_prog(x-4, y) + dyn_prog(x-3, y-1) + dyn_prog(x-2, y-2) + dyn_prog(x-1, y-3))
             return d[(x,y)]
         return dyn_prog(n, n)
+
+
+# Python O(N^2)* O(201*201) DynamicProgramming Math
+from math import ceil
+
+dp: list[list[float]] = [[-1] * 201 for _ in range(201)]
+
+
+class Solution:
+    def dfs(self, a: int, b: int) -> float:
+        if a <= 0 and b <= 0:
+            return 0.5
+        elif a <= 0 and b > 0:
+            return 1
+        elif a > 0 and b <= 0:
+            return 0
+        if dp[a][b] != -1: return dp[a][b]
+        dp[a][b] = 0.25 * (
+                    self.dfs(a - 4, b) + self.dfs(a - 3, b - 1) + self.dfs(a - 2, b - 2) + self.dfs(a - 1, b - 3))
+        return dp[a][b]
+
+    def soupServings(self, n: int) -> float:
+        if n > 4450: return 1
+        bound: int = ceil(n / 25)
+        return self.dfs(bound, bound)
+
+# C++ O(N^2)* O(201*201) DynamicProgramming Math
+class Solution {
+    double dp[201][201];
+public:
+    double dfs(int a, int b) {
+        if (a <= 0 && b <= 0) return 0.5;
+        else if (a > 0 && b <= 0) return 0;
+        else if (a <= 0 && b > 0) return 1;
+        else if (dp[a][b] != -1) return dp[a][b];
+        dp[a][b] = 0.25 * (dfs(a - 4, b) + (dfs(a - 3, b - 1) + dfs(a - 2, b - 2) + dfs(a - 1, b - 3)));
+        return dp[a][b];
+    }
+
+    double soupServings(int n) {
+        if (n >= 4450) return 1;
+        std::fill(&dp[0][0], &dp[0][0] + 201 * 201, -1);
+        int bound = std::ceil(static_cast<double>(n) / 25);
+        return dfs(bound, bound);
+    }
+};
