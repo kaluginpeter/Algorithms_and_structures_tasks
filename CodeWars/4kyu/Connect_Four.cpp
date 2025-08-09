bf0dcb830e27f7f5ebd74a386c36c54667e77c0b
kaluginpeter <streetworkout200412@gmail.com>
2025-08-09 11:51:18 +0300

    Added solution "Connect Four"

--- CodeWars/4kyu/Connect_Four.cpp
+++ CodeWars/4kyu/Connect_Four.cpp
@@ -27,3 +27,50 @@ You should return "Yellow", "Red" or "Draw" accordingly.
 
 Fundamentals
 */
+// Solution
+#include <string>
+#include <vector>
+#include <iostream>
+
+
+int makeMove(int column, int player, std::vector<std::vector<int>> &grid) {
+    for (int i = 5; i >= 0; --i) {
+      if (grid[i][column] == -1) {
+          grid[i][column] = player;
+          return i;
+      }
+    }
+    return -1;
+};
+bool checkPlayer(int column, int row, const std::vector<std::vector<int>> &grid) {
+    int left = column, right = column;
+    while (left >= 0 && grid[row][left] == grid[row][column]) --left;
+    while (right < 7 && grid[row][right] == grid[row][column]) ++right;
+    if (right - left - 1 >= 4) return true;
+    left = row;
+    right = row;
+    while (left >= 0 && grid[left][column] == grid[row][column]) --left;
+    while (right < 6 && grid[right][column] == grid[row][column]) ++right;
+    if (right - left - 1 >= 4) return true;
+    left = 0;
+    right = 0;
+    while (row - left >= 0 && column - left >= 0 && grid[row - left][column - left] == grid[row][column]) ++left;
+    while (row + right < 6 && column + right < 7 && grid[row][column] == grid[row + right][column + right]) ++right;
+    if (left + right - 1 >= 4) return true;
+    left = 0;
+    right = 0;
+    while (row - left >= 0 && column + left < 7 && grid[row][column] == grid[row - left][column + left]) ++left;
+    while (row + right < 6 && column - right >= 0 && grid[row][column] == grid[row + right][column - right]) ++right;
+    return left + right - 1 >= 4;
+};
+
+
+std::string who_is_winner(std::vector<std::string> pieces_position_list)
+{
+  std::vector<std::vector<int>> grid(6, std::vector<int>(7, -1));
+  for (const std::string &move : pieces_position_list) {
+      int row = makeMove(move[0] - 'A', (move.substr(2) == "Red" ? 1 : 0), grid);
+      if (checkPlayer(move[0] - 'A', row, grid)) return move.substr(2);
+  }
+  return "Draw";
+}
