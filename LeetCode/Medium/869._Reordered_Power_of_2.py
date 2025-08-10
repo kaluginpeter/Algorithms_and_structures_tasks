9f395ea58b2467a58553259d0a58a9828ba032e8
kaluginpeter <streetworkout200412@gmail.com>
2025-08-10 10:22:38 +0300

    Added solution "869. Reordered Power of 2"

--- LeetCode/Medium/869._Reordered_Power_of_2.py
+++ LeetCode/Medium/869._Reordered_Power_of_2.py
@@ -17,3 +17,49 @@
 # Constraints:
 #
 # 1 <= n <= 109
+# Solution
+# Python O(logN) O(logN) HashMap Counting
+class Solution:
+    def is_equal(self, x: int, y: int) -> bool:
+        digits_x: list[int] = [0] * 10
+        digits_y: list[int] = [0] * 10
+        while x:
+            digits_x[x % 10] += 1
+            x //= 10
+        while y:
+            digits_y[y % 10] += 1
+            y //= 10
+        for i in range(10):
+            if digits_x[i] != digits_y[i]: return False
+        return True
+
+    def reorderedPowerOf2(self, n: int) -> bool:
+        for i in range(31):
+            if self.is_equal(1 << i, n): return True
+        return False
+
+# C++ O(logN) O(logN) HashMap Counting
+class Solution {
+public:
+    bool isEqual(long long x, long long y) {
+        std::array<int, 10> xHashmap{}, yHashmap{};
+        while (x) {
+            ++xHashmap[x % 10];
+            x /= 10;
+        }
+        while (y) {
+            ++yHashmap[y % 10];
+            y /= 10;
+        }
+        for (int i = 0; i < 10; ++i) {
+            if (xHashmap[i] != yHashmap[i]) return false;
+        }
+        return true;
+    }
+    bool reorderedPowerOf2(int n) {
+        for (int i = 0; i < 31; ++i) {
+            if (isEqual(1 << i, n)) return true;
+        }
+        return false;
+    }
+};
