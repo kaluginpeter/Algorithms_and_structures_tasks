5ae3109be0c28197e28a7a8cff5ec7d90452da7d
kaluginpeter <streetworkout200412@gmail.com>
2025-08-10 15:23:07 +0300

    Added solution "2043A. Coin Transformation"

--- Codeforces/2043A._Coin_Transformation.py
+++ Codeforces/2043A._Coin_Transformation.py
@@ -49,3 +49,47 @@
 #  into two coins with value 4
 # . Each of the resulting coins can be transformed into two coins with value 1
 # .
+# Solution
+# C++ O(log4(N)) O(1) Math
+#include <iostream>
+
+
+void solution() {
+    long long n;
+    std::cin >> n;
+    int power = 0;
+    while (n > 3) {
+        n /= 4;
+        ++power;
+    }
+    long long output = 1;
+    while (power > 0) {
+        output *= 2;
+        --power;
+    }
+    std::cout << output << std::endl;
+}
+
+
+int main() {
+    int t;
+    std::cin >> t;
+    while (t--) solution();
+}
+
+# Python O(log4(N)) O(1) Math
+import sys
+
+
+def solution() -> None:
+    n: int = int(sys.stdin.readline().rstrip())
+    power: int = 0
+    while n > 3:
+        power += 1
+        n //= 4
+    sys.stdout.write('{}\n'.format(pow(2, power)))
+
+
+if __name__ == '__main__':
+    t: int = int(sys.stdin.readline().rstrip())
+    for _ in range(t): solution()
