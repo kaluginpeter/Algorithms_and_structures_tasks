fe6bde7d776ecffadef4607657da1571e25e7548
kaluginpeter <streetworkout200412@gmail.com>
2025-08-13 14:51:05 +0300

    Added solution "Sharkovsky's Theorem"

--- CodeWars/6kyu/Sharkovsky_s_Theorem.py
+++ CodeWars/6kyu/Sharkovsky_s_Theorem.py
@@ -160,3 +160,28 @@
 # b are non-zero positive integers.
 #
 # MathematicsAlgorithms
+# Solution
+def is_power_two(x):
+    return x != 0 and (x & (x - 1)) == 0
+
+def sharkovsky(a, b):
+    if a == b: return False
+    a_power = is_power_two(a)
+    b_power = is_power_two(b)
+    if not a_power and not b_power:
+        exp_a = 0
+        temp_a = a
+        while temp_a % 2 == 0:
+            temp_a //= 2
+            exp_a += 1
+        exp_b = 0
+        temp_b = b
+        while temp_b % 2 == 0:
+            temp_b //= 2
+            exp_b += 1
+        if exp_a < exp_b: return True
+        elif exp_a > exp_b: return False
+        return temp_a < temp_b
+    elif a_power and b_power:return a > b
+    if not a_power:return True
+    return False
