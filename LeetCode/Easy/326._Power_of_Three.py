a14c5519e2b406a0cd345a00513e2966093da625
kaluginpeter <streetworkout200412@gmail.com>
2025-08-13 14:10:45 +0300

    Added solution "326. Power of Three"

--- LeetCode/Easy/326._Power_of_Three.py
+++ LeetCode/Easy/326._Power_of_Three.py
@@ -27,3 +27,17 @@ class Solution:
         while 3**x < n:
             x += 1
         return 3**x == n
+
+
+# Python O(logN) O(1) Math
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        return n > 0 and pow(3, log(1 << 31) / log(3) // 1) % n == 0
+
+# C++ O(logN) O(1) Math
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        return n > 0 && static_cast<int>(std::pow(3, static_cast<int>(std::log(INT_MAX) / std::log(3)))) % n == 0;
+    }
+};
