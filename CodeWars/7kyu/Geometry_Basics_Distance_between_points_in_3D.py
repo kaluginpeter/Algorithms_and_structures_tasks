9fb792db7336142b1b2a1e34d23ec1a09b2816bd
kaluginpeter <streetworkout200412@gmail.com>
2025-08-15 12:32:54 +0300

    Added description "Geometry Basics: Distance between points in 3D"

---
+++ CodeWars/7kyu/Geometry_Basics_Distance_between_points_in_3D.py
@@ -0,0 +1,7 @@
+# This series of katas will introduce you to basics of doing geometry with computers.
+#
+# Point objects have x, y, and z attributes. For Haskell there are Point data types described with record syntax with fields x, y, and z.
+#
+# Write a function calculating distance between Point a and Point b.
+#
+# GeometryFundamentals
