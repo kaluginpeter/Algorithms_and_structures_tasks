e619d2dae10885620f1f23c154e4a49d100a2e5b
kaluginpeter <streetworkout200412@gmail.com>
2025-08-17 13:31:35 +0300

    Added solution "2019A. Max Plus Size"

--- Codeforces/2019A._Max_Plus_Size.py
+++ Codeforces/2019A._Max_Plus_Size.py
@@ -58,3 +58,51 @@
 # In the third test case, you can color the array as follows: [3,3,3,3,4,1,2,3,4,5]
 # . Your score is max([3,3,4,3,5])+size([3,3,4,3,5])=5+5=10
 # . This is the maximum score you can get.
+# Solution
+# C++ O(N) O(1) Greedy
+#include <iostream>
+#include <vector>
+#include <cstdint>
+
+
+void solution() {
+    int n;
+    std::cin >> n;
+    std::vector<int> nums(n, 0);
+    for (int i = 0; i < n; ++i) std::cin >> nums[i];
+    int maxEven = 0, maxOdd = 0;
+    for (int i = 0; i < n; ++i) {
+        if (i + 1 & 1) maxOdd = std::max(maxOdd, nums[i]);
+        else maxEven = std::max(maxEven, nums[i]);
+    }
+    int first = maxEven + n / 2, second = maxOdd + (n + 1) / 2;
+    std::cout << std::max(first, second) << std::endl;
+}
+
+
+int main() {
+    int t;
+    std::cin >> t;
+    while (t--) solution();
+}
+
+# Python O(N) O(1) Greedy
+import sys
+
+
+def solution() -> None:
+    n: int = int(sys.stdin.readline().rstrip())
+    nums: list[int] = list(map(int, sys.stdin.readline().rstrip().split()))
+    max_even: int = 0
+    max_odd: int = 0
+    for i in range(n):
+        if i + 1 & 1: max_odd = max(max_odd, nums[i])
+        else: max_even = max(max_even, nums[i])
+    first: int = max_even + n // 2
+    second: int = max_odd + (n + 1) // 2
+    sys.stdout.write('{}\n'.format(max(first, second)))
+
+
+if __name__ == '__main__':
+    t: int = int(sys.stdin.readline().rstrip())
+    for _ in range(t): solution()
