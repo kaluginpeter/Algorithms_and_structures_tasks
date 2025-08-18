3c59d7fd087f3e790cc1a7774ca762a3d344a6c3
kaluginpeter <streetworkout200412@gmail.com>
2025-08-18 13:46:21 +0300

    Added solution "679. 24 Game"

--- LeetCode/Hard/679._24_Game.py
+++ LeetCode/Hard/679._24_Game.py
@@ -27,3 +27,61 @@
 #
 # cards.length == 4
 # 1 <= cards[i] <= 9
+# Solution
+# Python O(4^N * N^2) O(1) Backtracking Math
+class Solution:
+    def backtrack(self, nums: list[float]) -> bool:
+        if len(nums) == 1: return abs(nums[0] - 24) < 1e-6
+        n: int = len(nums)
+        for i in range(n):
+            for j in range(n):
+                if i == j: continue
+                next_: list[float] = []
+                for k in range(n):
+                    if k != i and k != j: next_.append(nums[k])
+                a: float = nums[i]
+                b: float = nums[j]
+                ops: list[float] = [a + b, a - b, b - a, a * b]
+                if abs(a) > 1e-6: ops.append(b / a)
+                if abs(b) > 1e-6: ops.append(a / b)
+                for op in ops:
+                    next_.append(op)
+                    if self.backtrack(next_): return True
+                    next_.pop()
+        return False
+
+    def judgePoint24(self, cards: List[int]) -> bool:
+        return self.backtrack(cards)
+
+# C++ O(4^N * N^2) O(1) Backtracking Math
+class Solution {
+public:
+    bool backtrack(std::vector<double>& nums) {
+        if (nums.size() == 1) return std::fabs(nums[0] - 24.0) < 1e-6;
+        int n = nums.size();
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i == j) continue;
+                std::vector<double> next;
+                for (int k = 0; k < n; ++k) {
+                    if (k != i && k != j) next.push_back(nums[k]);
+                }
+                double a = nums[i], b = nums[j];
+                std::vector<double> ops = {a + b, a - b, b - a, a * b};
+                if (std::fabs(b) > 1e-6) ops.push_back(a / b);
+                if (std::fabs(a) > 1e-6) ops.push_back(b / a);
+                for (double op : ops) {
+                    next.push_back(op); // take
+                    if (backtrack(next)) return true;
+                    next.pop_back(); // not take
+                }
+            }
+        }
+        return false;
+    }
+
+    bool judgePoint24(std::vector<int>& cards) {
+        std::vector<double> nums(cards.begin(), cards.end());
+        return backtrack(nums);
+    }
+};
