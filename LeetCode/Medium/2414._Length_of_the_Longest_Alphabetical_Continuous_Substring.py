6c1baf14ba4da34b35bc8ab4abaf57d6cc9140f2
kaluginpeter <streetworkout200412@gmail.com>
2025-08-19 08:53:36 +0300

    Added solution "2414. Length of the Longest Alphabetical Continuous Substring"

--- LeetCode/Medium/2414._Length_of_the_Longest_Alphabetical_Continuous_Substring.py
+++ LeetCode/Medium/2414._Length_of_the_Longest_Alphabetical_Continuous_Substring.py
@@ -23,3 +23,26 @@
 # 1 <= s.length <= 105
 # s consists of only English lowercase letters.
 #
+# Solution
+# Python O(N) O(1) TwoPointers Greedy
+class Solution:
+    def longestContinuousSubstring(self, s: str) -> int:
+        output: int = 0
+        left: int = 0
+        for right in range(len(s)):
+            if ord(s[right]) - ord(s[right - 1 if right else 0]) != 1: left = right
+            output = max(output, right - left + 1)
+        return output
+
+# C++ O(N) O(1) TwoPointers Greedy
+class Solution {
+public:
+    int longestContinuousSubstring(string s) {
+        int output = 0, left = 0;
+        for (int right = 0; right < s.size(); ++right) {
+            if (s[right] - s[(right ? right - 1 : 0)] != 1) left = right;
+            output = std::max(output, right - left + 1);
+        }
+        return output;
+    }
+};
