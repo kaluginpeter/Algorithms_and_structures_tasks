9b3bf335bd17327d9c9834069273161c62997c4b
kaluginpeter <streetworkout200412@gmail.com>
2025-08-19 16:50:21 +0300

    Added description "2010A. Alternating Sum of Numbers"

---
+++ Codeforces/2010A._Alternating_Sum_of_Numbers.py
@@ -0,0 +1,39 @@
+# A. Alternating Sum of Numbers
+# time limit per test2 seconds
+# memory limit per test256 megabytes
+# You are given a sequence of integers. Output the alternating sum of this sequence. In other words, output a1−a2+a3−a4+a5−…
+# . That is, the signs of plus and minus alternate, starting with a plus.
+#
+# Input
+# The first line of the test contains one integer t
+#  (1≤t≤1000
+# ) — the number of test cases. Then follow t
+#  test cases.
+#
+# The first line of each test case contains one integer n
+#  (1≤n≤50
+# ) — the length of the sequence. The second line of the test case contains n
+#  integers a1,a2,…,an
+#  (1≤ai≤100
+# ).
+#
+# Output
+# Output t
+#  lines. For each test case, output the required alternating sum of the numbers.
+#
+# Example
+# InputCopy
+# 4
+# 4
+# 1 2 3 17
+# 1
+# 100
+# 2
+# 100 100
+# 5
+# 3 1 4 1 5
+# OutputCopy
+# -15
+# 100
+# 0
+# 10
