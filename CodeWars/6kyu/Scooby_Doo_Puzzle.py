f80bdeabec722b5f2b05aee317e91533cb0d9ac0
kaluginpeter <streetworkout200412@gmail.com>
2025-08-22 14:12:36 +0300

    Added description "Scooby Doo Puzzle"

---
+++ CodeWars/6kyu/Scooby_Doo_Puzzle.py
@@ -0,0 +1,62 @@
+# Introduction
+# Good one Shaggy! We all love to watch Scooby Doo, Shaggy Rogers, Fred Jones, Daphne Blake and Velma Dinkley solve the clues and figure out who was the villain. The story plot rarely differed from one episode to the next. Scooby and his team followed the clue then unmasked the villain at the end.
+# Task
+# Your task is to initially solve the clues and then use those clues to unmask the villain. You will be given a string of letters that you must manipulate in a way that the clues guide you. You must then output the villain.
+# You will be given an Array of potential villains and you must only return the correct masked villain.
+#
+# Potential Villains for the example test cases
+# `Black Knights, Puppet Master, Ghost Clowner, Witch Doctors, Waxed Phantom, Manor Phantom, Ghost Bigfoot, Haunted Horse, Davy Crockett, Captain Injun, Greens Gloobs, Ghostly Manor, Netty Crabbes, King Katazuma, Gators Ghouls, Headless Jack, Mambas Wambas, Medicines Man, Demon Sharker, Kelpy Monster, Gramps Vamper, Phantom Racer, Skeletons Men, Moon Monsters`
+# There will be different villains for the main test cases!
+#
+# Clue 1: The first clue is in a 'house' on 'String Class' Avenue.
+# Good luck!
+#
+# Kata Series
+# If you enjoyed this, then please try one of my other Katas. Any feedback, translations and grading of beta Katas are greatly appreciated. Thank you.
+#
+# Rank
+# Maze Runner
+#
+# Rank
+# Scooby Doo Puzzle
+#
+# Rank
+# Driving License
+#
+# Rank
+# Connect 4
+#
+# Rank
+# Vending Machine
+#
+# Rank
+# Snakes and Ladders
+#
+# Rank
+# Mastermind
+#
+# Rank
+# Guess Who?
+#
+# Rank
+# Am I safe to drive?
+#
+# Rank
+# Mexican Wave
+#
+# Rank
+# Pigs in a Pen
+#
+# Rank
+# Hungry Hippos
+#
+# Rank
+# Plenty of Fish in the Pond
+#
+# Rank
+# Fruit Machine
+#
+# Rank
+# Car Park Escape
+#
+# PuzzlesCryptographyAlgorithms
