cfb867c98d3710ee611510afe3fce8c48cb1ebf5
kaluginpeter <streetworkout200412@gmail.com>
2025-08-27 21:01:10 +0300

    Added solution "3459. Length of Longest V-Shaped Diagonal Segment"

--- LeetCode/Hard/3459._Length_of_Longest_V_Shaped_Diagonal_Segment.py
+++ LeetCode/Hard/3459._Length_of_Longest_V_Shaped_Diagonal_Segment.py
@@ -69,3 +69,73 @@
 # 1 <= n, m <= 500
 # grid[i][j] is either 0, 1 or 2.
 #
+# Solution
+# Python O(NM) O(NM) Depth-First-Search Matrix Memoization
+class Solution:
+    def dfs(self, cx: int, cy: int, direction: int, turn: bool, target: int, dirs: list[tuple[int, int]], memo: list[list[list[list[int]]]], grid: list[list[int]], n: int, m: int) -> int:
+        nx: int = cx + dirs[direction][0]
+        ny: int = cy + dirs[direction][1]
+        if nx < 0 or ny < 0 or nx >= n or ny >= m or grid[nx][ny] != target: return 0
+        elif memo[nx][ny][direction][turn] != -1: return memo[nx][ny][direction][turn]
+        max_step: int = self.dfs(nx, ny, direction, turn, 2 - target, dirs, memo, grid, n, m)
+        if turn:
+            max_step = max(max_step, self.dfs(nx, ny, (direction + 1) % 4, False, 2 - target, dirs, memo, grid, n, m))
+        max_step += 1
+        memo[nx][ny][direction][turn] = max_step
+        return max_step
+
+    def lenOfVDiagonal(self, grid: List[List[int]]) -> int:
+        n: int = len(grid)
+        m: int = len(grid[0])
+        dirs: list[tuple[int, int]] = [(1, 1), (1, -1), (-1, -1), (-1, 1)]
+        memo: list[list[list[list[int]]]] = [[[[-1] * 2 for _ in range(4)] for _ in range(m)] for _ in range(n)]
+        output: int = 0
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] != 1: continue
+                for direction in range(4):
+                    output = max(output, self.dfs(i, j, direction, True, 2, dirs, memo, grid, n, m) + 1)
+        return output
+
+# C++ O(NM) O(NM) Depth-First-Search Matrix Memoization
+using a2i = std::array<int, 2>;
+using a4a2i = std::array<a2i, 4>;
+using a501a501a4a2i = std::array<std::array<a4a2i, 501>, 501>;
+
+class Solution {
+public:
+    int dfs(int cx, int cy, int direction, bool turn, int target, const a4a2i &dirs, a501a501a4a2i &memo, const std::vector<std::vector<int>> &grid, const int &n, const int &m) {
+        int nx = cx + dirs[direction][0];
+        int ny = cy + dirs[direction][1];
+        if (nx < 0 || ny < 0 || nx >= n || ny >= m || grid[nx][ny] != target) return 0;
+        if (memo[nx][ny][direction][turn] != -1) return memo[nx][ny][direction][turn];
+        int maxStep = dfs(nx, ny, direction, turn, 2 - target, dirs, memo, grid, n, m);
+        if (turn) {
+            maxStep = std::max(maxStep, dfs(nx, ny, (direction + 1) % 4, false, 2 - target, dirs, memo, grid, n, m));
+        }
+        ++maxStep;
+        memo[nx][ny][direction][turn] = maxStep;
+        return maxStep;
+    };
+
+    int lenOfVDiagonal(std::vector<std::vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        a4a2i dirs = {a2i{1, 1}, a2i{1, -1}, a2i{-1, -1}, a2i{-1, 1}};
+        a501a501a4a2i memo = {};
+        for (auto &a : memo)
+            for (auto &b : a)
+                for (auto &c : b)
+                    c.fill(-1);
+        int output = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (grid[i][j] == 1) {
+                    for (int direction = 0; direction < 4; ++direction) {
+                        output = std::max(output, dfs(i, j, direction, true, 2, dirs, memo, grid, n, m) + 1);
+                    }
+                }
+            }
+        }
+        return output;
+    }
+};
