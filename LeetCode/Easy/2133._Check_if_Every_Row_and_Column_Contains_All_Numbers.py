8891cdb0ea58a08439229894c14aa0a41f93d323
kaluginpeter <streetworkout200412@gmail.com>
2025-08-30 09:24:51 +0300

    Added solution "2133. Check if Every Row and Column Contains All Numbers"

--- LeetCode/Easy/2133._Check_if_Every_Row_and_Column_Contains_All_Numbers.py
+++ LeetCode/Easy/2133._Check_if_Every_Row_and_Column_Contains_All_Numbers.py
@@ -25,3 +25,37 @@
 # n == matrix.length == matrix[i].length
 # 1 <= n <= 100
 # 1 <= matrix[i][j] <= n
+# Solution
+# Python O(NM) O(max(N, M)) Matrix HashSet
+class Solution:
+    def checkValid(self, matrix: List[List[int]]) -> bool:
+        n: int = len(matrix)
+        m: int = len(matrix[0])
+        for i in range(n):
+            row: set[int] = set()
+            for j in range(m): row.add(matrix[i][j])
+            if len(row) != n: return False
+        for j in range(m):
+            col: set[int] = set()
+            for i in range(n): col.add(matrix[i][j])
+            if len(col) != m: return False
+        return True
+
+# C++ O(NM) O(max(N, M)) Matrix HashSet
+class Solution {
+public:
+    bool checkValid(vector<vector<int>>& matrix) {
+        size_t n = matrix.size(), m = matrix[0].size();
+        for (size_t i = 0; i < n; ++i) {
+            std::unordered_set<int> row;
+            for (size_t j = 0; j < m; ++j) row.insert(matrix[i][j]);
+            if (row.size() != n) return false;
+        }
+        for (size_t j = 0; j < m; ++j) {
+            std::unordered_set<int> col;
+            for (size_t i = 0; i < n; ++i) col.insert(matrix[i][j]);
+            if (col.size() != m) return false;
+        }
+        return true;
+    }
+};
