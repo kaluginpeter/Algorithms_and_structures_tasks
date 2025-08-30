26e3ad4ed71e672bdb99969dcc164683358f4c4b
kaluginpeter <streetworkout200412@gmail.com>
2025-08-30 10:50:25 +0300

    Added solution "Sudoku Solver"

--- CodeWars/3kyu/Sudoku_Solver.py
+++ CodeWars/3kyu/Sudoku_Solver.py
@@ -26,3 +26,40 @@
 #   [2,8,7,4,1,9,6,3,5],
 #   [3,4,5,2,8,6,1,7,9]]
 # GamesGame SolversAlgorithms
+# Solution
+def backtrack(r: int, c: int, board: list[list[int]], row_hash: list[set[int]], col_hash: list[set[int]], square_hash: list[set[int]], n: int, m: int) -> bool:
+    if r == n: return True
+    n_r: int = r
+    n_c: int = c + 1
+    if n_c == m:
+        n_c = 0
+        n_r += 1
+    if board[r][c]: return backtrack(n_r, n_c, board, row_hash, col_hash, square_hash, n, m)
+    for k in range(1, 10):
+        square: int = r // 3 * 3 + c // 3
+        if k in row_hash[r] or k in col_hash[c] or k in square_hash[square]: continue
+        board[r][c] = k
+        row_hash[r].add(k)
+        col_hash[c].add(k)
+        square_hash[square].add(k)
+        if backtrack(n_r, n_c, board, row_hash, col_hash, square_hash, n, m): return True
+        board[r][c] = 0
+        row_hash[r].remove(k)
+        col_hash[c].remove(k)
+        square_hash[square].remove(k)
+    return False
+
+def sudoku(board: list[list[int]]) -> list[list[int]]:
+    n: int = len(board)
+    m: int = len(board[0])
+    row_hash: list[set[int]] = [set() for _ in range(n)]
+    col_hash: list[set[int]] = [set() for _ in range(m)]
+    square_hash: list[set[int]] = [set() for _ in range((n // 3 + 1) * (m // 3 + 1))]
+    for i in range(n):
+        for j in range(m):
+            if board[i][j] == '.': continue
+            row_hash[i].add(board[i][j])
+            col_hash[j].add(board[i][j])
+            square_hash[i // 3 * 3 + j // 3].add(board[i][j])
+    backtrack(0, 0, board, row_hash, col_hash, square_hash, n, m)
+    return board
