4aba3c9cca0d5f97e9c94147a18a0394e41540dc
kaluginpeter <streetworkout200412@gmail.com>
2025-08-31 11:24:11 +0300

    Added solution "2008B. Square or Not"

--- Codeforces/2008B._Square_or_Not.py
+++ Codeforces/2008B._Square_or_Not.py
@@ -75,3 +75,81 @@
 # 1
 # 1
 # There is no square matrix in the fourth case, such that the string can be obtained from it.
+# Solution
+# Python O(N) O(K) String
+#include <iostream>
+#include <string>
+
+
+void solution() {
+    size_t n;
+    std::cin >> n;
+    std::string matrix;
+    std::cin >> matrix;
+    size_t zeros = 0, left = 0;
+    for (size_t right = 0; right < n; ++right) {
+        if (matrix[right] != '0') left = right + 1;
+        zeros = std::max(zeros, right - left + 1);
+    }
+    std::string pattern = "1";
+    for (size_t i = 0; i < zeros; ++i) pattern.push_back('0');
+    pattern.push_back('1');
+    bool isValid = true;
+    size_t chunk = pattern.size();
+    for (size_t start = chunk; start < n - chunk; start += chunk) {
+        for (size_t i = 0; i < chunk; ++i) {
+            if (matrix[start + i] != pattern[i]) {
+                isValid = false;
+                break;
+            }
+        }
+        if (!isValid) break;
+    }
+    for (size_t i = 0; i < chunk; ++i) {
+        if (matrix[i] != '1' || matrix[n - i - 1] != '1') isValid = false;
+    }
+    isValid = isValid && (chunk == n / chunk && n % chunk == 0);
+    std::cout << (isValid ? "Yes" : "No") << std::endl;
+}
+
+
+int main() {
+    int t;
+    std::cin >> t;
+    while (t--) solution();
+}
+
+# Python O(N) O(K) String
+import sys
+
+
+def solution() -> None:
+    n: int = int(sys.stdin.readline().rstrip())
+    matrix: str = sys.stdin.readline().rstrip()
+    zeros: int = 0
+    left: int = 0
+    for right in range(n):
+        if matrix[right] != '0': left = right + 1
+        zeros = max(zeros, right - left + 1)
+    pattern: str = '1' + '0' * zeros + '1'
+    chunk: int = len(pattern)
+    if chunk != n // chunk or n % chunk != 0:
+        sys.stdout.write('No\n')
+        return
+    is_valid: bool = True
+    for i in range(chunk, n - chunk, chunk):
+        for j in range(chunk):
+            if matrix[i + j] != pattern[j]:
+                is_valid = False
+                break
+        if not is_valid: break
+    for i in range(chunk):
+        if matrix[i] != '1' or matrix[n - i - 1] != '1':
+            is_valid = False
+            break
+    sys.stdout.write('{}\n'.format(['No', 'Yes'][is_valid]))
+
+
+if __name__ == '__main__':
+    t: int = int(sys.stdin.readline().rstrip())
+    for _ in range(t): solution()
