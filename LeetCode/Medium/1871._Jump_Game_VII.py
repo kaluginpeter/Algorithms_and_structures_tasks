e917f79066bbadeaa9409bfcf71098f370098c94
kaluginpeter <streetworkout200412@gmail.com>
2025-09-01 16:14:27 +0300

    Added solution "1871. Jump Game VII"

--- LeetCode/Medium/1871._Jump_Game_VII.py
+++ LeetCode/Medium/1871._Jump_Game_VII.py
@@ -25,3 +25,36 @@
 # s[i] is either '0' or '1'.
 # s[0] == '0'
 # 1 <= minJump <= maxJump < s.length
+# Solution
+# Python O(N) O(N) Deque DynamicProgramming
+class Solution:
+    def canReach(self, s: str, minJump: int, maxJump: int) -> bool:
+        n: int = len(s)
+        data: list[int] = deque([0])
+        for i in range(1, n):
+            if s[i] == '1': continue
+            while data and data[0] + maxJump < i: data.popleft()
+            if not data: return False
+            if data[0] + minJump <= i:
+                if i == n - 1: return True
+                data.append(i)
+        return False
+
+# C++ O(N) O(N) Deque DynamicProgramming
+class Solution {
+public:
+    bool canReach(string s, int minJump, int maxJump) {
+        size_t n = s.size();
+        std::deque<size_t> data = {0};
+        for (size_t i = 1; i < n; ++i) {
+            if (s[i] == '1') continue;
+            while (!data.empty() && (data.front() + maxJump < i)) data.pop_front();
+            if (data.empty()) return false;
+            else if (data.front() + minJump <= i) {
+                data.push_back(i);
+                if (i == n - 1) return true;
+            }
+        }
+        return false;
+    }
+};
