30390344817b952725477962b40523e5bf749711
kaluginpeter <streetworkout200412@gmail.com>
2025-09-02 08:59:09 +0300

    Added solution "3027. Find the Number of Ways to Place People II"

--- LeetCode/Hard/3027._Find_the_Number_of_Ways_to_Place_People_II.py
+++ LeetCode/Hard/3027._Find_the_Number_of_Ways_to_Place_People_II.py
@@ -47,3 +47,39 @@
 # -109 <= points[i][0], points[i][1] <= 109
 # All points[i] are distinct.
 #
+# Solution
+# Python O(NlogN + N^2) O(1) Sorting Geometry
+class Solution:
+    def numberOfPairs(self, points: List[List[int]]) -> int:
+        points.sort(key=lambda point: (point[0], -point[1]))
+        output: int = 0
+        for i in range(len(points)):
+            upper_y: int = float('-inf')
+            for j in range(i + 1, len(points)):
+                if points[i][1] < points[j][1] or points[j][1] <= upper_y: continue
+                upper_y = max(upper_y, points[j][1])
+                output += 1
+        return output
+
+# C++ O(NlogN + N^2) O(1) Sorting Geometry
+class Solution {
+public:
+    int numberOfPairs(vector<vector<int>>& points) {
+        int output = 0;
+        std::sort(points.begin(), points.end(), [](const std::vector<int> &x, const std::vector<int> &y) {
+            if (x[0] != y[0]) return x[0] < y[0];
+            return x[1] >= y[1];
+        });
+        size_t n = points.size();
+        for (size_t i = 0; i < n; ++i) {
+            long long lowerY = INT64_MIN;
+            for (size_t j = i + 1; j < n; ++j) {
+                if (points[i][1] < points[j][1]) continue;
+                else if (points[j][1] <= lowerY) continue;
+                lowerY = std::max(lowerY, static_cast<long long>(points[j][1]));
+                ++output;
+            }
+        }
+        return output;
+    }
+};
