e7aba26b804416424c7eddcf879d5620022a4b0b
kaluginpeter <streetworkout200412@gmail.com>
2025-09-04 08:19:21 +0300

    Added description "Calendar Week"

---
+++ CodeWars/6kyu/Calendar_Week.py
@@ -0,0 +1,15 @@
+# According to ISO 8601, the first calendar week (1) starts with the week containing the first thursday in january. Every year consists of 52 calendar weeks (53 in case of a year starting on a Thursday or a leap year starting on a Wednesday).
+#
+# Your task is to calculate the calendar week (1-53) from a given date. For example, the calendar week for the date 2019-01-01 (string) should be 1 (int).
+#
+# Good luck 👍
+#
+# See also ISO week date and Week Number on Wikipedia for further information about calendar weeks.
+#
+# On whatweekisit.org you may click through the calender and study calendar weeks in more depth.
+#
+# heads-up: imports like require(xxx) for JS have been disabled
+#
+# Thanks to @ZED.CWT, @Unnamed and @proxya for their feedback.
+#
+# Date TimeAlgorithms
