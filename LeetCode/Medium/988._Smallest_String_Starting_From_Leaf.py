b16888d8f1dd1d2b1853407f0398e888e59cb063
kaluginpeter <streetworkout200412@gmail.com>
2025-09-04 20:19:46 +0300

    Added solution "988. Smallest String Starting From Leaf"

--- LeetCode/Medium/988._Smallest_String_Starting_From_Leaf.py
+++ LeetCode/Medium/988._Smallest_String_Starting_From_Leaf.py
@@ -30,3 +30,65 @@
 #
 # The number of nodes in the tree is in the range [1, 8500].
 # 0 <= Node.val <= 25
+# Solution
+# Python O(N + DH) O(H) String Depth-First-Search Tree
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    output: str = ''
+    def dfs(self, root: TreeNode, path: list[str]) -> None:
+        path.append(chr(root.val + 97))
+        if not root.left and not root.right:
+            path.reverse()
+            if not self.output or ''.join(path) < self.output: self.output = ''.join(path)
+            path.reverse()
+            path.pop()
+            return
+        if root.left: self.dfs(root.left, path)
+        if root.right: self.dfs(root.right, path)
+        path.pop()
+
+    def smallestFromLeaf(self, root: Optional[TreeNode]) -> str:
+        self.output = ''
+        path: list[str] = []
+        self.dfs(root, path)
+        return self.output
+
+# C++ O(N + DH) O(H) String Tree Depth-First-Search
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void dfs(TreeNode *root, std::string &path, std::string &output) {
+        path.push_back(static_cast<char>(root->val + 'a'));
+        if (!root->left && !root->right) {
+            std::reverse(path.begin(), path.end());
+            if (output.empty() || path < output) output = std::string(path);
+            std::reverse(path.begin(), path.end());
+            path.pop_back();
+            return;
+        };
+        if (root->left) dfs(root->left, path, output);
+        if (root->right) dfs(root->right, path, output);
+        path.pop_back();
+    }
+
+    string smallestFromLeaf(TreeNode* root) {
+        std::string output = "", path = "";
+        dfs(root, path, output);
+        return output;
+    }
+};
