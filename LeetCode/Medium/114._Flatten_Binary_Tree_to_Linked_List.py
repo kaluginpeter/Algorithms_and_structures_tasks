b267b1589ef3fdcbb4efd6d7378eb31fe21dcfca
kaluginpeter <streetworkout200412@gmail.com>
2025-09-04 20:22:09 +0300

    Added solution "114. Flatten Binary Tree to Linked List"

--- LeetCode/Medium/114._Flatten_Binary_Tree_to_Linked_List.py
+++ LeetCode/Medium/114._Flatten_Binary_Tree_to_Linked_List.py
@@ -26,3 +26,51 @@
 #
 #
 # Follow up: Can you flatten the tree in-place (with O(1) extra space)?
+# Solution
+# Python O(N) O(1) Tree
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def flatten(self, root: Optional[TreeNode]) -> None:
+        cur: TreeNode = root
+        while cur:
+            if cur.left:
+                prev: TreeNode = cur.left
+                while prev.right: prev = prev.right
+                prev.right = cur.right
+                cur.right = cur.left
+                cur.left = None
+            cur = cur.right
+
+# C++ O(N) O(1) Tree
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        TreeNode *cur = root;
+        while (cur) {
+            if (cur->left) {
+                TreeNode *prev = cur->left;
+                while (prev->right) prev = prev->right;
+                prev->right = cur->right;
+                cur->right = cur->left;
+                cur->left = nullptr;
+            }
+            cur = cur->right;
+        }
+    }
+};
