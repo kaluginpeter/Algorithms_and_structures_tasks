fcfb2ecee2cad915d0baf76e8fe4142f69185587
kaluginpeter <streetworkout200412@gmail.com>
2025-09-05 19:02:11 +0300

    Added solution "462. Minimum Moves to Equal Array Elements II"

--- LeetCode/Medium/462._Minimum_Moves_to_Equal_Array_Elements_II.py
+++ LeetCode/Medium/462._Minimum_Moves_to_Equal_Array_Elements_II.py
@@ -24,3 +24,36 @@
 # n == nums.length
 # 1 <= nums.length <= 105
 # -109 <= nums[i] <= 109
+# Solution
+# Python O(NlogN + N) O(1) Sorting Math
+class Solution:
+    def minMoves2(self, nums: List[int]) -> int:
+        nums.sort()
+        output: int = float('inf')
+        left: int = 0
+        right: int = sum(nums)
+        n: int = len(nums)
+        for i in range(n):
+            prefix: int = i * nums[i] - left
+            suffix: int = right - (n - i) * nums[i]
+            output = min(output, prefix + suffix)
+            left += nums[i]
+            right -= nums[i]
+        return output
+
+# C++ O(NlogN + N) O(1) Sorting Math
+class Solution {
+public:
+    int minMoves2(vector<int>& nums) {
+        std::sort(nums.begin(), nums.end());
+        size_t n = nums.size();
+        long long output = INT64_MAX, left = 0, right = std::accumulate(nums.begin(), nums.end(), 0LL);
+        for (size_t i = 0; i < n; ++i) {
+            long long prefix = (i * nums[i] - left), suffix = (right - (n - i) * nums[i]);
+            output = std::min(output, prefix + suffix);
+            right -= nums[i];
+            left += nums[i];
+        }
+        return output;
+    }
+};
