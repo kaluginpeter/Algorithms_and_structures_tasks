2a3cefaa026750112a4d122dbd4f4448867207bd
kaluginpeter <streetworkout200412@gmail.com>
2025-09-06 17:33:50 +0300

    Added description "Hyper Sphere"

---
+++ CodeWars/6kyu/Hyper_Sphere.py
@@ -0,0 +1,50 @@
+# You will be given an array of cordinates and a radius. The function should return if the coordinates describe a point within the given radius of the origin.
+#
+# In two dimensions the condition that any [x, y] coordinate lies in a given radius (= a circle) is:
+#
+# x
+# 2
+# +
+# y
+# 2
+# ≤
+# r
+# 2
+# x
+# 2
+#  +y
+# 2
+#  ≤r
+# 2
+#
+#
+# This generalises to higher dimensions as follows:
+#
+# x
+# 2
+# +
+# y
+# 2
+# +
+# z
+# 2
+# +
+# .
+# .
+# .
+# ≤
+# r
+# 2
+# x
+# 2
+#  +y
+# 2
+#  +z
+# 2
+#  +...≤r
+# 2
+#
+#
+# Note: a point with no coordinates should return true, as in zero dimensions all points are the same point
+#
+# MathematicsArraysFundamentals
