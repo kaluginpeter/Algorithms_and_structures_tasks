9d91120808617831ac17453b03decce073ec6248
kaluginpeter <streetworkout200412@gmail.com>
2025-09-09 20:46:01 +0300

    Added solution "6. Zigzag Conversion"

--- LeetCode/Medium/6._Zigzag_Conversion.py
+++ LeetCode/Medium/6._Zigzag_Conversion.py
@@ -34,3 +34,48 @@
 # 1 <= s.length <= 1000
 # s consists of English letters (lower-case and upper-case), ',' and '.'.
 # 1 <= numRows <= 1000
+# Solution
+# Python O(N) O(N) TwoPointers String
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        start: int = 0
+        end: int = max(1, numRows * 2 - 2)
+        step: int = max(1, numRows * 2 - 2)
+        segment: int = 0
+        n: int = len(s)
+        output: list[str] = []
+        while start <= end:
+            i: int = start
+            while i < n:
+                output.append(s[i])
+                i += step
+                if step and segment and i < n: output.append(s[i])
+                i += segment
+            segment += 2
+            step -= 2
+            start += 1
+            end -= 1
+        return ''.join(output)
+
+# C++ O(N) O(N) String TwoPointers
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        std::string output = "";
+        size_t start = 0, end = std::max(1, numRows * 2 - 2), segment = 0, n = s.size(), step = std::max(1, numRows * 2 - 2);
+        while (start <= end) {
+            int i = start;
+            while (i < n) {
+                output.push_back(s[i]);
+                i += step;
+                if (segment && step && i < n) output.push_back(s[i]);
+                i += segment;
+            }
+            step -= 2;
+            segment += 2;
+            ++start;
+            --end;
+        }
+        return output;
+    }
+};
