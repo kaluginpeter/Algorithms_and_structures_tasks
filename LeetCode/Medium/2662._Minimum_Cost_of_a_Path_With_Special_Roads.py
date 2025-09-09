a0b7397912279113a67ef45224989164ba43b1cb
kaluginpeter <streetworkout200412@gmail.com>
2025-09-09 20:43:12 +0300

    Added solution "2662. Minimum Cost of a Path With Special Roads"

--- LeetCode/Medium/2662._Minimum_Cost_of_a_Path_With_Special_Roads.py
+++ LeetCode/Medium/2662._Minimum_Cost_of_a_Path_With_Special_Roads.py
@@ -57,3 +57,71 @@
 # startX <= x1i, x2i <= targetX
 # startY <= y1i, y2i <= targetY
 # 1 <= costi <= 105
+# Solution
+# Python O(N^2logN) O(N^2) PriorityQueue
+class Solution:
+    def get_distance(self, x1: int, y1: int, x2: int, y2: int) -> int:
+        return abs(x2 - x1) + abs(y2 - y1)
+
+    def minimumCost(self, start: List[int], target: List[int], specialRoads: List[List[int]]) -> int:
+        output: int = self.get_distance(start[0], start[1], target[0], target[1])
+        seen: dict[tuple[int, int], int] = dict()
+        seen[(start[0], start[1])] = 0
+        min_heap: list[tuple[int, int, int]] = []
+        for path in specialRoads:
+            dist: int = self.get_distance(start[0], start[1], path[0], path[1]) + path[4]
+            seen[(path[2], path[3])] = min(seen.get((path[2], path[3]), float('inf')), dist)
+            heapq.heappush(min_heap, (dist, path[2], path[3]))
+        while min_heap:
+            cost, x, y = heapq.heappop(min_heap)
+            output = min(output, cost + self.get_distance(x, y, target[0], target[1]))
+            for sx, sy, ex, ey, c in specialRoads:
+                dist: int = cost + self.get_distance(x, y, sx, sy) + c
+                if (ex, ey) not in seen or seen[(ex, ey)] > dist:
+                    seen[(ex, ey)] = dist
+                    heapq.heappush(min_heap, (dist, ex, ey))
+        return output
+
+# C++ O(N^2logN) O(N^2) PriorityQueue Graph
+using tiii = std::tuple<int, int, int>;
+struct pair_hash {
+    size_t operator()(const pair<int, int>& p) const {
+        return hash<long long>()(((long long)p.first << 32) ^ p.second);
+    }
+};
+class Solution {
+public:
+    int getDistance(const int &x1, const int &y1, const int &x2, const int &y2) {
+        return std::abs(x2 - x1) + std::abs(y2 - y1);
+    }
+
+    int minimumCost(vector<int>& start, vector<int>& target, vector<vector<int>>& specialRoads) {
+        size_t n = specialRoads.size();
+        int output = getDistance(start[0], start[1], target[0], target[1]);
+        std::priority_queue<tiii, std::vector<tiii>, std::greater<tiii>> minHeap;
+        std::unordered_map<std::pair<int, int>, int, pair_hash> seen;
+        seen[std::pair<int, int>(start[0], start[1])] = 0;
+        for (size_t i = 0; i < n; ++i) {
+            int dist = getDistance(start[0], start[1], specialRoads[i][0], specialRoads[i][1]) + specialRoads[i][4];
+            std::pair<int, int> ceil = {specialRoads[i][2], specialRoads[i][3]};
+            if (!seen.count(ceil) || seen[ceil] > dist) {
+                seen[ceil] = dist;
+                minHeap.push(std::make_tuple(dist, specialRoads[i][2], specialRoads[i][3]));
+            }
+        }
+        while (!minHeap.empty()) {
+            tiii cur = minHeap.top();
+            minHeap.pop();
+            output = std::min(output, std::get<0>(cur) + getDistance(std::get<1>(cur), std::get<2>(cur), target[0], target[1]));
+            for (const auto &p : specialRoads) {
+                int dist = std::get<0>(cur) + getDistance(std::get<1>(cur), std::get<2>(cur), p[0], p[1]) + p[4];
+                std::pair<int, int> nextMove = {p[2], p[3]};
+                if (!seen.count(nextMove) || seen[nextMove] > dist) {
+                    seen[nextMove] = dist;
+                    minHeap.push(std::make_tuple(dist, p[2], p[3]));
+                }
+            }
+        }
+        return output;
+    }
+};
