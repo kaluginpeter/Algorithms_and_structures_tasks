305103968e140430bf4cb38c144397c2c5cd3723
kaluginpeter <streetworkout200412@gmail.com>
2025-09-10 21:26:31 +0300

    Added description "Mr. Safety's treasures"

---
+++ CodeWars/6kyu/Mr_Safety_s_treasures.py
@@ -0,0 +1,15 @@
+# Introduction
+# Mr. Safety loves numeric locks and his Nokia 3310. He locked almost everything in his house. He is so smart and he doesn't need to remember the combinations. He has an algorithm to generate new passcodes on his Nokia cell phone.
+# postimage
+#
+# Task
+# Can you crack his numeric locks? Mr. Safety's treasures wait for you. Write an algorithm to open his numeric locks. Can you do it without his Nokia 3310?
+#
+# Input
+# The str or message (Python) input string consists of lowercase and upercase characters. It's a real object that you want to unlock.
+#
+# Output
+# Return a string that only consists of digits.
+# Example
+# ``` unlock("Nokia") // => 66542 unlock("Valut") // => 82588 unlock("toilet") // => 864538 ```
+# Puzzles
