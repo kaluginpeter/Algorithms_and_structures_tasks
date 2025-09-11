ee86b3476721d2e81f0115c9259f7003389159ce
kaluginpeter <streetworkout200412@gmail.com>
2025-09-11 08:59:24 +0300

    Added description "Wordify an integer"

---
+++ CodeWars/6kyu/Wordify_an_integer.py
@@ -0,0 +1,13 @@
+# Turn a given number (an integer > 0, < 1000) into the equivalent English words. For the purposes of this kata, no hyphen is needed in numbers 21-99.
+#
+# Examples:
+#
+# 1   --> "one"
+# 12  --> "twelve"
+# 17  --> "seventeen"
+# 56  --> "fifty six"
+# 90  --> "ninety"
+# 326 --> "three hundred twenty six"
+# Based on "Speech module" mission from Checkio.
+#
+# Algorithms
