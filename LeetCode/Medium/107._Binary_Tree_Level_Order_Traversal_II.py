7a7bfb6f626a11409f6ca0b9b7aabe5e86658d3b
kaluginpeter <streetworkout200412@gmail.com>
2025-09-11 21:07:27 +0300

    Added solution "107. Binary Tree Level Order Traversal II"

--- LeetCode/Medium/107._Binary_Tree_Level_Order_Traversal_II.py
+++ LeetCode/Medium/107._Binary_Tree_Level_Order_Traversal_II.py
@@ -21,3 +21,54 @@
 #
 # The number of nodes in the tree is in the range [0, 2000].
 # -1000 <= Node.val <= 1000
+# Solution
+# Python O(N) O(N + H) Depth-First-Search
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def dfs(self, root: TreeNode, depth: int, output: list[list[int]]) -> None:
+        if not root: return
+        while depth >= len(output): output.append([])
+        output[depth].append(root.val)
+        if root.left: self.dfs(root.left, depth + 1, output)
+        if root.right: self.dfs(root.right, depth + 1, output)
+
+    def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
+        output: list[list[int]] = []
+        self.dfs(root, 0, output)
+        output.reverse()
+        return output
+
+# C++ O(N) O(N + H) Depth-First-Search
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void dfs(TreeNode* root, size_t depth, std::vector<std::vector<int>> &output) {
+        if (!root) return;
+        while (depth >= output.size()) output.push_back(std::vector<int>());
+        output[depth].push_back(root->val);
+        if (root->left) dfs(root->left, depth + 1, output);
+        if (root->right) dfs(root->right, depth + 1, output);
+    }
+
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        std::vector<std::vector<int>> output;
+        dfs(root, 0, output);
+        std::reverse(output.begin(), output.end());
+        return output;
+    }
+};
