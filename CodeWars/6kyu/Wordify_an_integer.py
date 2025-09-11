d2924c529794860b989862e261b536eb1c38593c
kaluginpeter <streetworkout200412@gmail.com>
2025-09-11 08:59:45 +0300

    Added solution "Wordify an integer"

--- CodeWars/6kyu/Wordify_an_integer.py
+++ CodeWars/6kyu/Wordify_an_integer.py
@@ -11,3 +11,16 @@
 # Based on "Speech module" mission from Checkio.
 #
 # Algorithms
+# Solution
+def wordify(n):
+    ones = ['zero', 'one', 'two', 'three', 'four',
+            'five', 'six', 'seven', 'eight', 'nine',
+            'ten', 'eleven', 'twelve', 'thirteen', 'fourteen',
+            'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+    tens = ['zero', 'ten', 'twenty', 'thirty', 'forty',
+            'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+    if n >= 100:
+        if n % 100: return "{0} hundred {1}".format(wordify(n // 100), wordify(n % 100))
+        else: return "{0} hundred".format(wordify(n // 100))
+    if n < 20: return ones[n]
+    return "{0}{1}".format(tens[n // 10], " {0}".format(wordify(n % 10)) if n % 10 else "")
