3a6c871d93bd898ac330a193fc908edf93f1e378
kaluginpeter <streetworkout200412@gmail.com>
2025-09-12 09:30:04 +0300

    Added solution "79. Word Search"

--- LeetCode/Medium/79._Word_Search.py
+++ LeetCode/Medium/79._Word_Search.py
@@ -31,3 +31,70 @@
 #
 #
 # Follow up: Could you use search pruning to make your solution faster with a larger board?
+# Solution
+# Python O(NMK) O(K) Depth-First-Search Matrix String
+class Solution:
+    def dfs(self, i: int, j: int, board: list[list[str]], path: list[str], word: str, n: int, m: int) -> bool:
+        path.append(board[i][j])
+        if len(path) == len(word) and path[-1] == word[-1]: return True
+        board[i][j] = '.'
+
+        if j and board[i][j - 1] == word[len(path)]:
+            if self.dfs(i, j - 1, board, path, word, n, m): return True
+        if i and board[i - 1][j] == word[len(path)]:
+            if self.dfs(i - 1, j, board, path, word, n, m): return True
+        if j + 1 < m and board[i][j + 1] == word[len(path)]:
+            if self.dfs(i, j + 1, board, path, word, n, m): return True
+        if i + 1 < n and board[i + 1][j] == word[len(path)]:
+            if self.dfs(i + 1, j, board, path, word, n, m): return True
+
+        board[i][j] = path.pop()
+        return False
+
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        n: int = len(board)
+        m: int = len(board[0])
+        path: list[str] = []
+        for i in range(n):
+            for j in range(m):
+                if board[i][j] != word[0]: continue
+                if self.dfs(i, j, board, path, word, n, m): return True
+        return False
+
+# C++ O(NMK) O(K) Depth-First-Search Matrix String
+class Solution {
+public:
+    bool dfs(size_t i, size_t j, std::vector<std::vector<char>> &board, std::string &path, std::string &word, size_t n, size_t m) {
+        path.push_back(board[i][j]);
+        if (path.size() == word.size() && path.back() == word.back()) return true;
+        board[i][j] = '.';
+
+        if (j && board[i][j - 1] == word[path.size()]) {
+            if (dfs(i, j - 1, board, path, word, n, m)) return true;
+        }
+        if (i && board[i - 1][j] == word[path.size()]) {
+            if (dfs(i - 1, j, board, path, word, n, m)) return true;
+        }
+        if (j + 1 < m && board[i][j + 1] == word[path.size()]) {
+            if (dfs(i, j + 1, board, path, word, n, m)) return true;
+        }
+        if (i + 1 < n && board[i + 1][j] == word[path.size()]) {
+            if (dfs(i + 1, j, board, path, word, n, m)) return true;
+        }
+
+        board[i][j] = path.back();
+        path.pop_back();
+        return false;
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        size_t n = board.size(), m = board[0].size();
+        std::string path = "";
+        for (size_t i = 0; i < n; ++i) {
+            for (size_t j = 0; j < m; ++j) {
+                if (board[i][j] != word[0]) continue;
+                if (dfs(i, j, board, path, word, n, m)) return true;
+            }
+        }
+        return false;
+    }
+};
