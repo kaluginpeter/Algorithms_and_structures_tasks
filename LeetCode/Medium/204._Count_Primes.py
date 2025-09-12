77effdcc1734c69a5e5d729671b64ec38e002042
kaluginpeter <streetworkout200412@gmail.com>
2025-09-12 19:20:13 +0300

    Added solution "204. Count Primes"

--- LeetCode/Medium/204._Count_Primes.py
+++ LeetCode/Medium/204._Count_Primes.py
@@ -20,3 +20,31 @@
 # Constraints:
 #
 # 0 <= n <= 5 * 106
+# Solution
+# Python O(NlogN) O(N) DynamicProgramming
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        output: int = int(n > 2)
+        dp: list[bool] = [True] * n
+        for i in range(3, n, 2):
+            if not dp[i]: continue
+            elif i % 2 == 0: continue
+            output += 1
+            for j in range(i + i, n, i): dp[j] = False
+        return output
+
+# C++ O(NlogN) O(N) DynamicProgramming
+class Solution {
+public:
+    int countPrimes(int n) {
+        int output = (n > 2 ? 1 : 0);
+        std::vector<bool> dp(n, true);
+        for (size_t i = 3; i < n; i += 2) {
+            if (!dp[i]) continue;
+            else if (i % 2 == 0) continue;
+            ++output;
+            for (size_t j = i + i; j < n; j += i) dp[j] = false;
+        }
+        return output;
+    }
+};
