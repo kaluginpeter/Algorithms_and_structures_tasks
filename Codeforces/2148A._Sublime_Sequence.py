3444e3b459f8a6eae28126111e0919ac2b523ddb
kaluginpeter <streetworkout200412@gmail.com>
2025-09-13 21:10:29 +0300

    Added description "2148A. Sublime Sequence"

---
+++ Codeforces/2148A._Sublime_Sequence.py
@@ -0,0 +1,41 @@
+# A. Sublime Sequence
+# time limit per test1 second
+# memory limit per test256 megabytes
+# Farmer John has an integer x
+# . He creates a sequence of length n
+#  by alternating integers x
+#  and −x
+# , starting with x
+# .
+#
+# For example, if n=5
+# , the sequence looks like: x,−x,x,−x,x
+# .
+#
+# He asks you to find the sum of all integers in the sequence.
+#
+# Input
+# The first line contains an integer t
+#  (1≤t≤100
+# )  — the number of test cases.
+#
+# The only line of input for each test case is two integers x
+#  and n
+#  (1≤x,n≤10
+# ).
+#
+# Output
+# For each test case, output the sum of all integers in the sequence.
+#
+# Example
+# InputCopy
+# 4
+# 1 4
+# 2 5
+# 3 6
+# 4 7
+# OutputCopy
+# 0
+# 2
+# 0
+# 4
