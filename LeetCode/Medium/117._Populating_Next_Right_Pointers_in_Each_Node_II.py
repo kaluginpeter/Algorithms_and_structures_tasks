e58a03bffec3ff169154e67af616165b45f2487b
kaluginpeter <streetworkout200412@gmail.com>
2025-09-13 21:03:37 +0300

    Added solution "117. Populating Next Right Pointers in Each Node II"

--- LeetCode/Medium/117._Populating_Next_Right_Pointers_in_Each_Node_II.py
+++ LeetCode/Medium/117._Populating_Next_Right_Pointers_in_Each_Node_II.py
@@ -34,3 +34,74 @@
 #
 # You may only use constant extra space.
 # The recursive approach is fine. You may assume implicit stack space does not count as extra space for this problem.
+# Solution
+# Python O(N) O(1) Tree Depth-First-Search
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        node: 'Node' = root
+        while node:
+            dummy: 'Node' = Node()
+            cur: 'Node' = dummy
+            while node:
+                if node.left:
+                    cur.next = node.left
+                    cur = cur.next
+                if node.right:
+                    cur.next = node.right
+                    cur = cur.next
+                node = node.next
+            node = dummy.next
+        return root
+
+# C++ O(N) O(1) Depth-First-Search Tree
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        Node* node = root;
+        while (node) {
+            Node* dummy = new Node();
+            Node* cur = dummy;
+            while (node) {
+                if (node->left) {
+                    cur->next = node->left;
+                    cur = cur->next;
+                }
+                if (node->right) {
+                    cur->next = node->right;
+                    cur = cur->next;
+                }
+                node = node->next;
+            }
+            node = dummy->next;
+        }
+        return root;
+    }
+};
