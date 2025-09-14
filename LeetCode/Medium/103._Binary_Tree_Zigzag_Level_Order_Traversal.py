0fb1f8894be69a0318ec2bfe00207349e75b5a03
kaluginpeter <streetworkout200412@gmail.com>
2025-09-14 20:01:40 +0300

    Added solution "103. Binary Tree Zigzag Level Order Traversal"

--- LeetCode/Medium/103._Binary_Tree_Zigzag_Level_Order_Traversal.py
+++ LeetCode/Medium/103._Binary_Tree_Zigzag_Level_Order_Traversal.py
@@ -21,3 +21,66 @@
 #
 # The number of nodes in the tree is in the range [0, 2000].
 # -100 <= Node.val <= 100
+# Solution
+# Python O(N) O(N) Breadth-First-Search
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        output: list[list[int]] = []
+        i: int = 0
+        cur_nodes: list[int] = [root]
+        next_nodes: list[int] = []
+        while cur_nodes:
+            level: list[int] = []
+            for node in cur_nodes:
+                if not node: continue
+                level.append(node.val)
+                if node.left: next_nodes.append(node.left)
+                if node.right: next_nodes.append(node.right)
+            if i & 1: level.reverse()
+            if level: output.append(level)
+            i += 1
+            cur_nodes = next_nodes.copy()
+            next_nodes.clear()
+        return output
+
+# C++ O(N) O(N) Breadth-First-Search
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        std::vector<std::vector<int>> output;
+        std::vector<TreeNode*> curNodes = {root}, nextNodes = {};
+        size_t i = 0;
+        while (!curNodes.empty()) {
+            std::vector<int> level;
+            for (TreeNode* node : curNodes) {
+                if (!node) continue;
+                level.push_back(node->val);
+                if (node->left) nextNodes.push_back(node->left);
+                if (node->right) nextNodes.push_back(node->right);
+            }
+            if (i & 1) std::reverse(level.begin(), level.end());
+            if (!level.empty()) output.push_back(level);
+            ++i;
+            curNodes = nextNodes;
+            nextNodes.clear();
+        }
+        return output;
+    }
+};
