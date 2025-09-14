ae17046b534e4b9e98c3d8db080635385c02016f
kaluginpeter <streetworkout200412@gmail.com>
2025-09-14 19:59:01 +0300

    Added solution "222. Count Complete Tree Nodes"

--- LeetCode/Easy/222._Count_Complete_Tree_Nodes.py
+++ LeetCode/Easy/222._Count_Complete_Tree_Nodes.py
@@ -26,3 +26,41 @@
 # The number of nodes in the tree is in the range [0, 5 * 104].
 # 0 <= Node.val <= 5 * 104
 # The tree is guaranteed to be complete.
+# Solution
+# Python O(N) O(H) Depth-First-Search
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def dfs(self, root: TreeNode) -> int:
+        if not root: return 0
+        return self.dfs(root.left) + self.dfs(root.right) + 1
+
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        return self.dfs(root)
+
+# C++ O(N) O(H) Depth-First-Search
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int dfs(TreeNode* root) {
+        if (!root) return 0;
+        return dfs(root->left) + dfs(root->right) + 1;
+    }
+    int countNodes(TreeNode* root) {
+        return dfs(root);
+    }
+};
