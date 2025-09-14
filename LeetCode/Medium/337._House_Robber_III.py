1533f15afe62bb44d91cd9b1fd279a1f840dacf5
kaluginpeter <streetworkout200412@gmail.com>
2025-09-14 19:47:39 +0300

    Added solution "337. House Robber III"

--- LeetCode/Medium/337._House_Robber_III.py
+++ LeetCode/Medium/337._House_Robber_III.py
@@ -24,3 +24,50 @@
 #
 # The number of nodes in the tree is in the range [1, 104].
 # 0 <= Node.val <= 104
+# Solution
+# Python O(N) O(H) DynamicProgramming Depth-First-Search
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def dfs(self, root: TreeNode) -> list[int]:
+        if not root: return [0, 0]
+        left: list[int] = self.dfs(root.left)
+        right: list[int] =  self.dfs(root.right)
+        take: int = left[1] + right[1] + root.val
+        not_take: int = max(left) + max(right)
+        return [take, not_take]
+
+    def rob(self, root: Optional[TreeNode]) -> int:
+        return max(self.dfs(root))
+
+# C++ O(N) O(H) DynamicProgramming Depth-First-Search
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    std::vector<int> dfs(TreeNode* root) {
+        if (!root) return std::vector<int>(2, 0);
+        std::vector<int> left = dfs(root->left);
+        std::vector<int> right = dfs(root->right);
+        int take = root->val + left[1] + right[1];
+        int notTake = *std::max_element(left.begin(), left.end()) + *std::max_element(right.begin(), right.end());
+        return std::vector<int>({take, notTake});
+    }
+    int rob(TreeNode* root) {
+        std::vector<int> output = dfs(root);
+        return *std::max_element(output.begin(), output.end());
+    }
+};
