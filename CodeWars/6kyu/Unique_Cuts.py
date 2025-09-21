bd4cce96b01b71979890f7621eb29e499eb3a5e4
kaluginpeter <streetworkout200412@gmail.com>
2025-09-21 20:08:40 +0300

    Added description "Unique Cuts"

---
+++ CodeWars/6kyu/Unique_Cuts.py
@@ -0,0 +1,15 @@
+# Task
+# The function is given a string with lower-case characters. Split the string into as many substrings as possible such that each character appears in only one substring. Return the list of lengths of the resulting substrings.
+#
+# Examples
+# "abbccc" ➞ [1, 2, 3]
+# # "a", "bb", "ccc"
+#
+# "abbacdceef" ➞ [4, 3, 2, 1]
+# # "abba", "cdc", "ee", "f"
+#
+# "abacded" ➞ [3, 1, 3]
+# # "aba", "c", "ded"
+#
+# "abcdea" ➞ [6]
+# # "abcdea" because first letter is equal to the last letter.
