0aa985e558bd8087c05cf1f3542132130a449695
kaluginpeter <streetworkout200412@gmail.com>
2025-09-21 20:09:07 +0300

    Added solution "Unique Cuts"

--- CodeWars/6kyu/Unique_Cuts.py
+++ CodeWars/6kyu/Unique_Cuts.py
@@ -13,3 +13,22 @@
 #
 # "abcdea" ➞ [6]
 # # "abcdea" because first letter is equal to the last letter.
+# Solution
+def split_string(st):
+    in_: list[int] = [-1] * 26
+    out_: list[int] = [-1] * 26
+    for i in range(len(st)):
+        j: int = ord(st[i]) - 97
+        if in_[j] == -1: in_[j] = i
+        out_[j] = i
+    output: list[int] = []
+    left: int = 0
+    end: int = 0
+    for right in range(len(st)):
+        idx: int = ord(st[right]) - 97
+        if right > end:
+            output.append(end - left + 1)
+            left = right
+        end = max(end, out_[idx])
+    output.append(len(st) - left)
+    return output
