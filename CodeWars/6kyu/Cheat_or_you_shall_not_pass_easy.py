ae04d988f8dedeb9695ec5961ba46d13709388ed
kaluginpeter <streetworkout200412@gmail.com>
2025-09-22 21:40:43 +0300

    Added solution "Cheat or you shall not pass: easy"

--- CodeWars/6kyu/Cheat_or_you_shall_not_pass_easy.py
+++ CodeWars/6kyu/Cheat_or_you_shall_not_pass_easy.py
@@ -1,3 +1,11 @@
 # All you have to do is use your cheating talent to pass this kata.
 #
 # Puzzles
+# Solution
+import random
+
+def solution() -> int:
+    state = random.getstate()
+    temp = random.Random()
+    temp.setstate(state)
+    return temp.randint(0, 1000)
