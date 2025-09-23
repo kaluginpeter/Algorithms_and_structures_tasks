b10d4a8c38b6f39bf8aea8c696a7c37c5985dcbe
kaluginpeter <streetworkout200412@gmail.com>
2025-09-23 21:24:41 +0300

    Added solution "3153. Sum of Digit Differences of All Pairs"

--- LeetCode/Medium/3153._Sum_of_Digit_Differences_of_All_Pairs.py
+++ LeetCode/Medium/3153._Sum_of_Digit_Differences_of_All_Pairs.py
@@ -35,3 +35,50 @@
 # 2 <= nums.length <= 105
 # 1 <= nums[i] < 109
 # All integers in nums have the same number of digits.
+# Solution
+# Python O(Nlog(max(N)) + log(max(N))) O(log(max(N)) * 10) HashMap BitManipulation
+class Solution:
+    def sumDigitDifferences(self, nums: List[int]) -> int:
+        hashmap: list[list[int]] = [[0] * 10 for _ in range(33)]
+        count: list[int] = [0] * 33
+        for num in nums:
+            place: int = 0
+            while num:
+                hashmap[place][num % 10] += 1
+                count[place] += 1
+                num //= 10
+                place += 1
+        output: int = 0
+        for bit in range(33):
+            for d in range(10):
+                if not hashmap[bit][d]: continue
+                count[bit] -= hashmap[bit][d]
+                output += hashmap[bit][d] * count[bit]
+        return output
+
+# C++ O(Nlog(max(N)) + log(max(N))) O(log(max(N))) BitManipulation HashMap
+class Solution {
+public:
+    long long sumDigitDifferences(vector<int>& nums) {
+        std::array<std::array<int, 10>, 33> hashmap{};
+        std::array<int, 33> count{};
+        for (int num : nums) {
+            int place = 0;
+            while (num) {
+                ++hashmap[place][num % 10];
+                ++count[place];
+                num /= 10;
+                ++place;
+            }
+        }
+        long long output = 0;
+        for (int bit = 0; bit < 33; ++bit) {
+            for (int d = 0; d < 10; ++d) {
+                if (!hashmap[bit][d]) continue;
+                count[bit] -= hashmap[bit][d];
+                output += static_cast<long long>(hashmap[bit][d]) * count[bit];
+            }
+        }
+        return output;
+    }
+};
