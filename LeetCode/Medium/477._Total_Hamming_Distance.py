977e7515e17f01f4540d6a652863ddcfb7346143
kaluginpeter <streetworkout200412@gmail.com>
2025-09-23 21:21:42 +0300

    Added solution "477. Total Hamming Distance"

--- LeetCode/Medium/477._Total_Hamming_Distance.py
+++ LeetCode/Medium/477._Total_Hamming_Distance.py
@@ -23,3 +23,48 @@
 # 1 <= nums.length <= 104
 # 0 <= nums[i] <= 109
 # The answer for the given input will fit in a 32-bit integer.
+# Solution
+# Python O(Nlog(max(N)) + log(max(N))) O(log(max(N))) BitManipulation HashMap Math
+class Solution:
+    def totalHammingDistance(self, nums: List[int]) -> int:
+        hashmap: list[list[int, int]] = [[0, 0] for _ in range(33)]
+        for num in nums:
+            bit: int = 32
+            while num:
+                hashmap[bit][num & 1] += 1
+                bit -= 1
+                num >>= 1
+            while bit >= 0:
+                hashmap[bit][0] += 1
+                bit -= 1
+        output: int = 0
+        for zeros, ones in hashmap:
+            if not zeros or not ones: continue
+            output += zeros * ones
+        return output
+
+# C++ O(Nlog(max(N)) + log(max(N))) O(log(max(N))) BitManipulation HashMap Math
+class Solution {
+public:
+    int totalHammingDistance(vector<int>& nums) {
+        std::array<std::array<int, 2>, 33> hashmap{};
+        for (int& num : nums) {
+            int bit = 32;
+            while (num) {
+                ++hashmap[bit][num % 2];
+                --bit;
+                num >>= 1;
+            }
+            while (bit >= 0) {
+                ++hashmap[bit][0];
+                --bit;
+            }
+        }
+        int output = 0;
+        for (int bit = 0; bit < 33; ++bit) {
+            if (!hashmap[bit][1] || !hashmap[bit][0]) continue;
+            output += hashmap[bit][0] * hashmap[bit][1];
+        }
+        return output;
+    }
+};
