54fa5e94edbd60e3da912d6eee0fdd8625d53e08
kaluginpeter <streetworkout200412@gmail.com>
2025-09-23 21:26:44 +0300

    Added solution "474. Ones and Zeroes"

--- LeetCode/Medium/474._Ones_and_Zeroes.py
+++ LeetCode/Medium/474._Ones_and_Zeroes.py
@@ -26,3 +26,53 @@
 # 1 <= strs[i].length <= 100
 # strs[i] consists only of digits '0' and '1'.
 # 1 <= m, n <= 100
+# Solution
+# Python O(LNM) O(NM) DynamicProgramming
+class Solution:
+    def get_score(self, row: str) -> tuple[int, int]:
+        zeros: int = 0
+        ones: int = 0
+        for ch in row:
+            if int(ch): ones += 1
+            else: zeros += 1
+        return (zeros, ones)
+
+    def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+        dp: list[list[int]] = [[0] * (m + 1) for _ in range(n + 1)]
+        for cell in strs:
+            zeros, ones = self.get_score(cell)
+            for i in range(n, ones - 1, -1):
+                for j in range(m, zeros - 1, -1):
+                    dp[i][j] = max(
+                        dp[i][j],
+                        dp[i - ones][j - zeros] + 1
+                    )
+        return dp[n][m]
+
+# C++ O(LNM) O(NM) DynamicProgramming
+class Solution {
+public:
+    std::pair<int, int> getFreq(std::string& cell) {
+        int zeros = 0, ones = 0;
+        for (char& ch : cell) {
+            zeros += ch == '0';
+            ones += ch == '1';
+        }
+        return std::pair<int, int>(zeros, ones);
+    }
+    int findMaxForm(vector<string>& strs, int m, int n) {
+        std::vector<std::vector<int>> dp(n + 1, std::vector<int>(m + 1, 0));
+        for (std::string& cell : strs) {
+            std::pair<int, int> freq = getFreq(cell); // <zeros, ones>
+            for (int i = n; i >= freq.second; --i) {
+                for (int j = m; j >= freq.first; --j) {
+                    dp[i][j] = std::max({
+                        dp[i][j],
+                        dp[i - freq.second][j - freq.first] + 1,
+                    });
+                }
+            }
+        }
+        return dp[n][m];
+    }
+};
