9f44659f130f29474e396a1d4c6f5d45d26386ff
kaluginpeter <streetworkout200412@gmail.com>
2025-09-23 21:14:51 +0300

    Added description "TIY-FizzBuzz"

---
+++ CodeWars/7kyu/TIY_FizzBuzz.py
@@ -0,0 +1,9 @@
+# In this exercise, you will have to create a function named tiyFizzBuzz. This function will take on a string parameter and will return that string with some characters replaced, depending on the value:
+#
+# If a letter is a upper case consonants, replace that character with "Iron".
+# If a letter is a lower case consonants or a non-alpha character, do nothing to that character
+# If a letter is a upper case vowel, replace that character with "Iron Yard".
+# If a letter is a lower case vowel, replace that character with "Yard".
+# Ready?
+#
+# Fundamentals
