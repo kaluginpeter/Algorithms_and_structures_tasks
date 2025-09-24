d4d12a7097adf4d74c868ce601294399ad7ff2fb
kaluginpeter <streetworkout200412@gmail.com>
2025-09-24 09:07:20 +0300

    Added solution "166. Fraction to Recurring Decimal"

--- LeetCode/Medium/166._Fraction_to_Recurring_Decimal.py
+++ LeetCode/Medium/166._Fraction_to_Recurring_Decimal.py
@@ -26,3 +26,57 @@
 #
 # -231 <= numerator, denominator <= 231 - 1
 # denominator != 0
+# Solution
+# Python O(logN) O(logN) HashMap
+class Solution:
+    def fractionToDecimal(self, numerator: int, denominator: int) -> str:
+        if not numerator: return '0'
+        fraction: list[str] = []
+        if (numerator < 0) ^ (denominator < 0): fraction.append('-')
+        dividend: int = abs(numerator)
+        divisor: int = abs(denominator)
+        fraction.append(str(dividend // divisor))
+        remainder: int = dividend % divisor
+        if not remainder: return ''.join(fraction)
+        fraction.append('.')
+        seen: dict[int, int] = dict()
+        while remainder:
+            if remainder in seen:
+                fraction.insert(seen[remainder], '(')
+                fraction.append(')')
+                break
+            else:
+                seen[remainder] = len(fraction)
+                remainder *= 10
+                fraction.append(str(remainder // divisor))
+                remainder %= divisor
+        return ''.join(fraction)
+
+# C++ O(logN) O(logN) HashMap
+class Solution {
+public:
+    std::string fractionToDecimal(int numerator, int denominator) {
+        if (numerator == 0) return "0";
+        std::string fraction = "";
+        if ((numerator < 0) ^ (denominator < 0)) fraction.push_back('-');
+        long long dividend = std::llabs(static_cast<long long>(numerator));
+        long long divisor = std::llabs(static_cast<long long>(denominator));
+        fraction += std::to_string(dividend / divisor);
+        long long remainder = dividend % divisor;
+        if (remainder == 0) return fraction;
+        fraction.push_back('.');
+        std::unordered_map<long long, int> seen;
+        while (remainder) {
+            if (seen.count(remainder)) {
+                fraction.insert(seen[remainder], "(");
+                fraction.push_back(')');
+                break;
+            }
+            seen[remainder] = fraction.size();
+            remainder *= 10;
+            fraction += std::to_string(remainder / divisor);
+            remainder %= divisor;
+        }
+        return fraction;
+    }
+};
