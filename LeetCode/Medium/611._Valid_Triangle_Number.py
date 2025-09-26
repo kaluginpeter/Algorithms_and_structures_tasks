9cc1a822b2cb6844501a47b79c59754db7745979
kaluginpeter <streetworkout200412@gmail.com>
2025-09-26 08:06:57 +0300

    Added solution "611. Valid Triangle Number"

--- LeetCode/Medium/611._Valid_Triangle_Number.py
+++ LeetCode/Medium/611._Valid_Triangle_Number.py
@@ -20,3 +20,52 @@
 #
 # 1 <= nums.length <= 1000
 # 0 <= nums[i] <= 1000
+# Solution
+# Python O(N^2logN) O(1) BinarySearch Sorting
+class Solution:
+    def get_k(self, nums: list[int], target: int) -> int:
+        left: int = 0
+        right: int = len(nums) - 1
+        while left <= right:
+            middle: int = left + ((right - left) >> 1)
+            if nums[middle] >= target: right = middle - 1
+            else: left = middle + 1
+        return right
+
+    def triangleNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        output: int = 0
+        n: int = len(nums)
+        for i in range(n):
+            for j in range(i + 1, n):
+                target: int = nums[i] + nums[j]
+                k: int = self.get_k(nums, target)
+                if k > j: output += k - j
+        return output
+
+# C++ O(N^2logN) O(1) Sorting BinarySearch
+class Solution {
+public:
+    int getK(std::vector<int>& nums, int& target) {
+        int left = 0, right = nums.size() - 1;
+        while (left <= right) {
+            int middle = left + ((right - left) >> 1);
+            if (nums[middle] >= target) right = middle - 1;
+            else left = middle + 1;
+        }
+        return right;
+    }
+    int triangleNumber(vector<int>& nums) {
+        std::sort(nums.begin(), nums.end());
+        size_t n = nums.size();
+        int output = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                int target = nums[i] + nums[j];
+                int k = getK(nums, target);
+                if (k > j) output += k - j;
+            }
+        }
+        return output;
+    }
+};
