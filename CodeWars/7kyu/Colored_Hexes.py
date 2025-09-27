4a23e13036ace9cc2deb95672bd086bac7c18bbc
kaluginpeter <streetworkout200412@gmail.com>
2025-09-27 20:33:05 +0300

    Added description "Colored Hexes!"

---
+++ CodeWars/7kyu/Colored_Hexes.py
@@ -0,0 +1,26 @@
+# You're looking through different hex codes, and having trouble telling the difference between #000001 and #100000
+#
+# We need a way to tell which is red, and which is blue!
+#
+# That's where you create hex color !!!
+#
+# It should read an RGB input, and return whichever value (red, blue, or green) is of greatest concentration!
+#
+# But, if multiple colors are of equal concentration, you should return their mix!
+#
+# red + blue = magenta
+#
+# green + red = yellow
+#
+# blue + green = cyan
+#
+# red + blue + green = white
+# One last thing, if the string given is empty, or has all 0's, return black!
+#
+# Examples:
+#
+# codes = "087 255 054" -> "green"
+# codes = "181 181 170" -> "yellow"
+# codes = "000 000 000" -> "black"
+# codes = "001 001 001" -> "white"
+# Fundamentals
