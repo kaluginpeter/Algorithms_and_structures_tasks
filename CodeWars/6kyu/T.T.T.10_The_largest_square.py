b985d15ee6516f6d70bc8dde2cbfd2bd88be19e3
kaluginpeter <streetworkout200412@gmail.com>
2025-09-28 20:07:36 +0300

    Added description "T.T.T.10: The largest square"

---
+++ CodeWars/6kyu/T.T.T.10_The_largest_square.py
@@ -0,0 +1,18 @@
+# Task
+# Give you a 2D array wall:
+#
+# [
+# ["X"," "," "," "," "," "],
+# [" "," "," "," "," "," "],
+# [" "," "," "," "," "," "],
+# [" "," "," "," "," "," "],
+# [" "," "," "," "," "," "],
+# [" "," "," "," "," "," "]
+# ]
+# " " is the blank part, "X" is the hole in the wall, please find the largest square (no hole) on the wall, return its area.
+#
+# The above example should return 25, because the maximum square that can be found is 5X5
+#
+# Please see more example in testcases.
+#
+# PuzzlesGames
