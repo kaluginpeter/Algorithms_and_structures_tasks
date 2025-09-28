76011bc37a16cbf88585540c90232c3766d364f5
kaluginpeter <streetworkout200412@gmail.com>
2025-09-28 20:07:59 +0300

    Added solution "T.T.T.10: The largest square"

--- CodeWars/6kyu/T.T.T.10_The_largest_square.py
+++ CodeWars/6kyu/T.T.T.10_The_largest_square.py
@@ -16,3 +16,17 @@
 # Please see more example in testcases.
 #
 # PuzzlesGames
+# Solution
+def _max(wall):
+    n: int = len(wall)
+    m: int = len(wall[0])
+    dp: list[list[int]] = [[0] * m for _ in range(n)]
+    for i in range(n):
+        for j in range(m):
+            if wall[i][j] == 'X': continue
+            dp[i][j] = min(
+                (dp[i][j - 1] if j else 0),
+                (dp[i - 1][j] if i else 0),
+                (dp[i - 1][j - 1] if i and j else 0)
+            ) + 1
+    return max((max(row, default=0) for row in dp), default=0)**2
