c55a1baed2be17c29d4027893b1da87e53aaf799
kaluginpeter <streetworkout200412@gmail.com>
2025-09-30 21:38:46 +0300

    Added solution "Simple Fun #218: Replace Words"

--- CodeWars/6kyu/Simple_Fun_218_Replace_Words.py
+++ CodeWars/6kyu/Simple_Fun_218_Replace_Words.py
@@ -21,3 +21,20 @@
 # The word "I" and word "better" exchanged their positions; The word "am" and word "than" exchanged their positions; The middle length word "him" remain in its original position.
 #
 # Fundamentals
+# Solution
+from collections import defaultdict
+def replace_words(sentence):
+    words: list[str] = sentence.split()
+    hashmap: dict[int, list[int]] = defaultdict(list)
+    for i in range(len(words)):
+        hashmap[words[i]].append(i)
+    correct: list[str] = sorted(words, key=lambda word: len(word))
+    while len(correct) > 1:
+        left: int = hashmap[correct.pop(0)].pop(0)
+        right: int = hashmap[correct.pop()].pop(0)
+        words[left], words[right] = words[right], words[left]
+    for i in range(len(words)):
+        if not i: words[i] = words[i].title()
+        elif words[i].lower() == 'i': words[i] = words[i].upper()
+        else: words[i] = words[i].lower()
+    return ' '.join(words)
