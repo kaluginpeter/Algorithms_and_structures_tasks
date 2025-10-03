1a124d5779330e7ed3d6a67bde3ccb9df5e110ad
kaluginpeter <streetworkout200412@gmail.com>
2025-10-03 19:28:48 +0300

    Added description "Convert between radians and degrees"

---
+++ CodeWars/7kyu/Convert_between_radians_and_degrees.py
@@ -0,0 +1,8 @@
+# Extend the Math object/module to convert degrees to radians and vice versa. Return the result in string format, rounded to two decimal points when not an integer, otherwise truncate the result (see the examples).
+#
+# Note that all methods of Math object are static.
+#
+# Examples
+# math.degrees(math.pi)  -->  "180deg"
+# math.radians(180)      -->  "3.14rad"
+# MathematicsFundamentals
