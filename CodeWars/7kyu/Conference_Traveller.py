f9495eb62ec5c11db4b47a5a1f3787d3ed0f9b93
kaluginpeter <streetworkout200412@gmail.com>
2025-10-08 22:25:57 +0300

    Added solution "Conference Traveller"

--- CodeWars/7kyu/Conference_Traveller.py
+++ CodeWars/7kyu/Conference_Traveller.py
@@ -19,3 +19,8 @@
 # Cities offered = ["Stockholm","Paris","Melbourne"]
 # Expected solution ---> "Paris"
 # FundamentalsArraysLists
+# Solution
+def conference_picker(cities_visited, cities_offered):
+    for city in cities_offered:
+        if city not in cities_visited: return city
+    return "No worthwhile conferences this year!"
