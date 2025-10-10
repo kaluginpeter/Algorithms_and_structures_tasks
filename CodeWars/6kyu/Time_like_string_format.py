e15ce6b63e02141aaeff24632cf1cd7147eea8d9
kaluginpeter <streetworkout200412@gmail.com>
2025-10-10 13:26:39 +0300

    Added solution "Time-like string format"

--- CodeWars/6kyu/Time_like_string_format.py
+++ CodeWars/6kyu/Time_like_string_format.py
@@ -9,3 +9,10 @@
 # 3351  --> '33:51'
 # 10000 --> raise an exception
 # StringsFundamentals
+# Solution
+def solution(hour):
+    x: str = str(hour)
+    if len(x) > 4 or len(x) <= 2: raise Exception()
+    if len(x) == 3:
+        return x[0] + ':' + x[1:]
+    return x[:2] + ':' + x[2:]
