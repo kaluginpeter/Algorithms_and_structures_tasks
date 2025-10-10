928da95c26a2494916a6dbaaf8310615dfc7306d
kaluginpeter <streetworkout200412@gmail.com>
2025-10-10 13:26:12 +0300

    Added description "Time-like string format"

---
+++ CodeWars/6kyu/Time_like_string_format.py
@@ -0,0 +1,11 @@
+# Build up a method that takes a positive integer and formats it to a 'time - like' format.
+#
+# The method must raise an exception if its hour length is less than 3 digits and greater than 4.
+#
+# Examples:
+# 800   --> '8:00'
+# 1000  --> '10:00'
+# 1451  --> '14:51'
+# 3351  --> '33:51'
+# 10000 --> raise an exception
+# StringsFundamentals
