64da37e97d5c95eecdec64ab8f2764f7699d9ef6
kaluginpeter <streetworkout200412@gmail.com>
2025-10-16 08:43:02 +0300

    Added solution "Simple directions reversal"

--- CodeWars/7kyu/Simple_directions_reversal.py
+++ CodeWars/7kyu/Simple_directions_reversal.py
@@ -9,3 +9,15 @@
 # Please also try Simple remove duplicates
 #
 # Algorithms
+def solve(arr):
+    if len(arr) <= 1: return arr
+    path: list[str] = ['Begin ' + ' '.join(arr[-1].split()[1:])]
+    prev: str = arr[-1].split()[0]
+    if prev == 'Right': prev = 'Left'
+    else: prev = 'Right'
+    for i in range(len(arr) - 2, -1, -1):
+        path.append(prev + ' ' + ' '.join(arr[i].split()[1:]))
+        prev = arr[i].split()[0]
+        if prev == 'Right': prev = 'Left'
+        else: prev = 'Right'
+    return path
