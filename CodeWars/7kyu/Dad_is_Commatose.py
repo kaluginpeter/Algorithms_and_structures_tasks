0438e8f043b1c04669f0d8ffe1b1aa3f34197bb9
kaluginpeter <streetworkout200412@gmail.com>
2025-10-17 21:42:45 +0300

    Added solution "Dad is Commatose"

--- CodeWars/7kyu/Dad_is_Commatose.py
+++ CodeWars/7kyu/Dad_is_Commatose.py
@@ -3,3 +3,11 @@
 # Write a function that takes a string as an argument and returns a string with the extraneous commas removed. The returned string should not end with a comma or have any trailing whitespace.
 #
 # FundamentalsStringsRegular Expressions
+# Solution
+def dad_filter(string):
+    string = string.split()
+    for i in range(len(string)):
+        was_comma: bool = string[i].endswith(',')
+        string[i] = string[i].replace(',', '')
+        if was_comma and i + 1 != len(string): string[i] += ','
+    return ' '.join(string)
