2282560f86cf3738e0a694bc7aa90d83cffcee88
kaluginpeter <streetworkout200412@gmail.com>
2025-10-17 21:40:26 +0300

    Added description "Dad is Commatose"

---
+++ CodeWars/7kyu/Dad_is_Commatose.py
@@ -0,0 +1,5 @@
+# Your dad doesn't really get punctuation, and he's always putting extra commas in when he posts. You can tolerate the run-on sentences, as he does actually talk like that, but those extra commas have to go!
+#
+# Write a function that takes a string as an argument and returns a string with the extraneous commas removed. The returned string should not end with a comma or have any trailing whitespace.
+#
+# FundamentalsStringsRegular Expressions
