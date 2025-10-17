c293af8cf808be4f0bf7890699d10548824041fd
kaluginpeter <streetworkout200412@gmail.com>
2025-10-17 21:37:52 +0300

    Added solution "3003. Maximize the Number of Partitions After Operations"

--- LeetCode/Hard/3003._Maximize_the_Number_of_Partitions_After_Operations.py
+++ LeetCode/Hard/3003._Maximize_the_Number_of_Partitions_After_Operations.py
@@ -57,3 +57,58 @@
 # s consists only of lowercase English letters.
 # 1 <= k <= 26
 #
+# Solution
+# C++ O(NM) O(N) Prefix Suffix String
+class Solution {
+public:
+    int maxPartitionsAfterOperations(string s, int k) {
+        int n = s.length();
+        std::vector<std::vector<int>> left(n, std::vector<int>(3)), right(n, std::vector<int>(3));
+        int num = 0, mask = 0, count = 0;
+        for (int i = 0; i < n - 1; ++i) {
+            int binary = 1 << (s[i] - 'a');
+            if (!(mask & binary)) {
+                ++count;
+                if (count <= k) mask |= binary;
+                else {
+                    ++num;
+                    mask = binary;
+                    count = 1;
+                }
+            }
+            left[i + 1][0] = num;
+            left[i + 1][1] = mask;
+            left[i + 1][2] = count;
+        }
+        num = 0, mask = 0, count = 0;
+        for (int i = n - 1; i > 0; --i) {
+            int binary = 1 << (s[i] - 'a');
+            if (!(mask & binary)) {
+                ++count;
+                if (count <= k) mask |= binary;
+                else {
+                    ++num;
+                    mask = binary;
+                    count = 1;
+                }
+            }
+            right[i - 1][0] = num;
+            right[i - 1][1] = mask;
+            right[i - 1][2] = count;
+        }
+        int output = 0;
+        for (int i = 0; i < n; ++i) {
+            int seg = left[i][0] + right[i][0] + 2;
+            int totMask = left[i][1] | right[i][1];
+            int totCount = 0;
+            while (totMask) {
+                totMask = totMask & (totMask - 1);
+                ++totCount;
+            }
+            if (left[i][2] == k && right[i][2] == k && totCount < 26) ++seg;
+            else if (std::min(totCount + 1, 26) <= k) --seg;
+            output = std::max(output, seg);
+        }
+        return output;
+    }
+};
