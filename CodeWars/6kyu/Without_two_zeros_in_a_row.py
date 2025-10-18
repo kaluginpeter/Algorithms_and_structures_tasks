3ab768365597314b1d82bb008bccc3fef2cb619a
kaluginpeter <streetworkout200412@gmail.com>
2025-10-18 08:55:23 +0300

    Added description "Without two zeros in a row"

---
+++ CodeWars/6kyu/Without_two_zeros_in_a_row.py
@@ -0,0 +1,14 @@
+# You are given a positive integer n.
+#
+# The task is to calculate how many binary numbers without leading zeros (such that their length is n and they do not contain two zeros in a row) there are. Note that zero itself ("0") meets the conditions (for n = 1).
+#
+# For example, there are three eligible binary numbers of length 3:
+#
+# 101
+# 110
+# 111
+# 100 is not suitable.
+#
+# Remember that n may be fairly big (up to 10^4). Good luck!
+#
+# Dynamic Programming
