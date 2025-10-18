9255accd7bb923e64a04b13d3da8d39913492d99
kaluginpeter <streetworkout200412@gmail.com>
2025-10-18 21:29:56 +0300

    Added solution "Without two zeros in a row"

--- CodeWars/6kyu/Without_two_zeros_in_a_row.py
+++ CodeWars/6kyu/Without_two_zeros_in_a_row.py
@@ -12,3 +12,11 @@
 # Remember that n may be fairly big (up to 10^4). Good luck!
 #
 # Dynamic Programming
+# Solution
+def zeros(n: int) -> int:
+    if n == 1: return 2
+    dp: list[int] = [0] * (n + 1)
+    dp[0] = 1
+    for i in range(1, n):
+        dp[i] = dp[i - 1] + (dp[i - 2] if i >= 2 else 1)
+    return dp[n - 1]
