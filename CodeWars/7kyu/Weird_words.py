6fd5ac281495395f580b4853517b7a0af4b55c16
kaluginpeter <streetworkout200412@gmail.com>
2025-10-19 19:02:03 +0300

    Added description "Weird words"

---
+++ CodeWars/7kyu/Weird_words.py
@@ -0,0 +1,12 @@
+# Change every letter in a given string to the next letter in the alphabet. The function takes a single parameter s (string).
+#
+# Notes:
+#
+# Spaces and special characters should remain the same.
+# Capital letters should transfer in the same way but remain capitilized.
+# Examples
+# "Hello"               -->  "Ifmmp"
+# "What is your name?"  -->  "Xibu jt zpvs obnf?"
+# "zoo"                 -->  "app"
+# "zzZAaa"              -->  "aaABbb"
+# StringsRegular ExpressionsFundamentals
