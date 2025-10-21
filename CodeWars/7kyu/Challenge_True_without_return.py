233d270030b9499d62a81f67d187e94369b6b926
kaluginpeter <streetworkout200412@gmail.com>
2025-10-21 08:44:16 +0300

    Added description "Challenge: True without return"

---
+++ CodeWars/7kyu/Challenge_True_without_return.py
@@ -0,0 +1,11 @@
+# There's not much explaining for this kata.
+#
+# Make it so if I call solve() it gives an output of True.
+#
+# But you are not allowed to use the return keyword!
+#
+# If you do disregard the challenge, at least look at others solutions for learning value.
+#
+# Good luck!
+#
+# PuzzlesRestrictedFundamentals
