63fb16217118fe26a4773f764d60e8071f19af77
kaluginpeter <streetworkout200412@gmail.com>
2025-10-24 08:53:51 +0300

    Added description "Boxlines"

---
+++ CodeWars/7kyu/Boxlines.py
@@ -0,0 +1,14 @@
+# Boxlines
+# Given a X*Y*Z box built by arranging 1*1*1 unit boxes, write a function f(X,Y,Z) that returns the number of edges (hence, boxlines) of length 1 (both inside and outside of the box)
+#
+# Notes
+# Adjacent unit boxes share the same edges, so a 2*1*1 box will have 20 edges, not 24 edges
+# X,Y and Z are strictly positive, and can go as large as 2^16 - 1
+# Example
+# The following is a diagram of a 2*1*1 box. Count how many line segments you can see!
+#
+# Interactive No longer interactive :( diagram made by @awesomead
+#
+# This is my first kata, so I hope every one will enjoy it <3
+#
+# FundamentalsGeometryMathematics
