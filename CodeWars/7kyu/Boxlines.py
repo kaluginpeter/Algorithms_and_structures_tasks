f31d23478d6b69100c60bb564b5218dd790d3676
kaluginpeter <streetworkout200412@gmail.com>
2025-10-24 08:54:21 +0300

    Added solution "Boxlines"

--- CodeWars/7kyu/Boxlines.py
+++ CodeWars/7kyu/Boxlines.py
@@ -12,3 +12,6 @@
 # This is my first kata, so I hope every one will enjoy it <3
 #
 # FundamentalsGeometryMathematics
+# Solution
+def f(X,Y,Z):
+    return X * (Y + 1) * (Z + 1) + Y * (X + 1) * (Z + 1) + Z * (X + 1) * (Y + 1)
