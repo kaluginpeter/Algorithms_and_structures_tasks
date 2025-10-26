702b806aa9ae7f3b4997ed96a92b0d9e2c06c607
kaluginpeter <streetworkout200412@gmail.com>
2025-10-26 09:30:04 +0300

    Added solution "2043. Simple Bank System"

--- LeetCode/Medium/2043._Simple_Bank_System.py
+++ LeetCode/Medium/2043._Simple_Bank_System.py
@@ -39,3 +39,80 @@
 # 1 <= n, account, account1, account2 <= 105
 # 0 <= balance[i], money <= 1012
 # At most 104 calls will be made to each function transfer, deposit, withdraw.
+# Solution
+# C++ O(N) for init and O(1) for query O(N) Memory Array
+class Bank {
+private:
+    std::vector<long long> balance;
+public:
+    Bank(vector<long long>& balance) {
+        this->balance = balance;
+    }
+
+    bool transfer(int account1, int account2, long long money) {
+        if (account1 - 1 < 0 || account1 - 1 >= balance.size()) return false;
+        if (account2 - 1 < 0 || account2 - 1 >= balance.size()) return false;
+        if (balance[account1 - 1] < money) return false;
+        balance[account1 - 1] -= money;
+        balance[account2 - 1] += money;
+        return true;
+    }
+
+    bool deposit(int account, long long money) {
+        if (account - 1 < 0 || account - 1 >= balance.size()) return false;
+        balance[account - 1] += money;
+        return true;
+    }
+
+    bool withdraw(int account, long long money) {
+        if (account - 1 < 0 || account - 1 >= balance.size()) return false;
+        if (balance[account - 1] < money) return false;
+        balance[account - 1] -= money;
+        return true;
+    }
+};
+
+/**
+ * Your Bank object will be instantiated and called as such:
+ * Bank* obj = new Bank(balance);
+ * bool param_1 = obj->transfer(account1,account2,money);
+ * bool param_2 = obj->deposit(account,money);
+ * bool param_3 = obj->withdraw(account,money);
+ */
+
+# Python O(N) for init and O(1) for each query. Memory O(N) Array
+class Bank:
+
+    def __init__(self, balance: List[int]):
+        self.balance: list[int] = balance
+
+    def is_valid_account(self, account: int) -> bool:
+        return 0 <= account - 1 < len(self.balance)
+
+    def is_enough_balance(self, account: int, bound: int) -> bool:
+        return self.balance[account - 1] >= bound
+
+    def transfer(self, account1: int, account2: int, money: int) -> bool:
+        if not self.is_valid_account(account1) or not self.is_valid_account(account2):
+            return False
+        if not self.is_enough_balance(account1, money): return False
+        self.balance[account1 - 1] -= money
+        self.balance[account2 - 1] += money
+        return True
+
+    def deposit(self, account: int, money: int) -> bool:
+        if not self.is_valid_account(account): return False
+        self.balance[account - 1] += money
+        return True
+
+    def withdraw(self, account: int, money: int) -> bool:
+        if not self.is_valid_account(account): return False
+        if not self.is_enough_balance(account, money): return False
+        self.balance[account - 1] -= money
+        return True
+
+# Your Bank object will be instantiated and called as such:
+# obj = Bank(balance)
+# param_1 = obj.transfer(account1,account2,money)
+# param_2 = obj.deposit(account,money)
+# param_3 = obj.withdraw(account,money)
