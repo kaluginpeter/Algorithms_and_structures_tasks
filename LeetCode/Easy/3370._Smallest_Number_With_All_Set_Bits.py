62f9eae415f42bb771633ab7b0f89ae9c9048727
kaluginpeter <streetworkout200412@gmail.com>
2025-10-29 08:58:27 +0300

    Added description "3370. Smallest Number With All Set Bits"

---
+++ LeetCode/Easy/3370._Smallest_Number_With_All_Set_Bits.py
@@ -0,0 +1,42 @@
+# You are given a positive number n.
+#
+# Return the smallest number x greater than or equal to n, such that the binary representation of x contains only set bits
+#
+#
+#
+# Example 1:
+#
+# Input: n = 5
+#
+# Output: 7
+#
+# Explanation:
+#
+# The binary representation of 7 is "111".
+#
+# Example 2:
+#
+# Input: n = 10
+#
+# Output: 15
+#
+# Explanation:
+#
+# The binary representation of 15 is "1111".
+#
+# Example 3:
+#
+# Input: n = 3
+#
+# Output: 3
+#
+# Explanation:
+#
+# The binary representation of 3 is "11".
+#
+#
+#
+# Constraints:
+#
+# 1 <= n <= 1000
+#
