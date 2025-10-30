fa19919b9e1fcfd4fe7c1811505723484b889156
kaluginpeter <streetworkout200412@gmail.com>
2025-10-30 08:52:10 +0300

    Added description "Sorted Arrays"

---
+++ CodeWars/6kyu/Sorted_Arrays.cpp
@@ -0,0 +1,10 @@
+/*
+Given any number of arrays each sorted in ascending order, find the nth smallest number of all their elements.
+
+All the arguments except the last will be arrays, the last argument is n.
+
+nthSmallest([1,5], [2], [4,8,9], 4) // returns 5 because it's the 4th smallest value
+Be mindful of performance.
+
+ArraysAlgorithmsPerformance
+*/
