b7fd50e5c6de610734b8fc584757e4f14ef6d695
kaluginpeter <streetworkout200412@gmail.com>
2025-10-30 08:52:32 +0300

    Added solution "Sorted Arrays"

--- CodeWars/6kyu/Sorted_Arrays.cpp
+++ CodeWars/6kyu/Sorted_Arrays.cpp
@@ -8,3 +8,15 @@ Be mindful of performance.
 
 ArraysAlgorithmsPerformance
 */
+// Solution
+#include <vector>
+#include <queue>
+
+int nthSmallest(const std::vector<std::vector<int>>& arr, int n) {
+    std::priority_queue<int, std::vector<int>, std::greater<int>> minHeap;
+    for (const std::vector<int>& v : arr) {
+        for (const int& num : v) minHeap.push(num);
+    }
+    for (int i = 0; i < n - 1; ++i) minHeap.pop();
+    return minHeap.top();
+}
