87989b556797eaa3dcb0c4e5114a76b7b4d0f61b
kaluginpeter <streetworkout200412@gmail.com>
2025-11-01 20:07:14 +0300

    Added description "The rarest pepe"

---
+++ CodeWars/6kyu/The_rarest_pepe.py
@@ -0,0 +1,9 @@
+# The pepe market is on the verge of collapse. In a last-ditch attempt to make some quick cash, you decide to sift through the thousands of pepes on the internet to identify the rarest ones, which are worth more. Since doing this by hand would be tedious, you decide to use your programming skills to streamline the process.
+#
+# Your task in this kata is to implement a function that, given a non-empty list of pepes (pepes), returns the rarest pepe in the list.
+#
+# If two or more pepes are equally rare, return a sorted list of these pepes.
+# If the rarest pepe (or pepes) has a frequency of 5 or more, then there are no truly rare pepes, so your function should return "No rare pepes!".
+# More info on rare pepes here.
+#
+# Algorithms
