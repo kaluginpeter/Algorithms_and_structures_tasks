d0acdd98f6833d2b0340ff66c0d46d4f46f3af80
kaluginpeter <streetworkout200412@gmail.com>
2025-11-04 10:04:14 +0300

    Added solution "3318. Find X-Sum of All K-Long Subarrays I"

--- LeetCode/Easy/3318._Find_X_Sum_of_All_K_Long_Subarrays_I.py
+++ LeetCode/Easy/3318._Find_X_Sum_of_All_K_Long_Subarrays_I.py
@@ -53,3 +53,61 @@ class Solution:
             sequence: list[int] = sorted(hashmap.keys(), key=lambda x: (hashmap[x], x), reverse=True)
             ans.append(sum(y * hashmap[y] for y in sequence[:x]))
         return ans
+
+
+
+# Python O(ND + (N-K+1)(D+DlogD) + X) O(ND + X) PrefixArray HashMap Sorting
+class Solution:
+    def findXSum(self, nums: List[int], k: int, x: int) -> List[int]:
+        prefix_map: list[list[int]] = []
+        hashmap: list[int] = [0] * 51
+        prefix_map.append(hashmap.copy())
+        for num in nums:
+            hashmap[num] += 1
+            prefix_map.append(hashmap.copy())
+        n: int = len(nums)
+        answer: list[int] = [0] * (n + 1 - k)
+        for i in range(k, n + 1):
+            dataset: list[tuple[int, int]] = []
+            for j in range(51):
+                rem_freq: int = prefix_map[i][j] - prefix_map[i - k][j]
+                if rem_freq: dataset.append((rem_freq, j))
+            dataset.sort(reverse=True)
+            x_sum: int = sum(freq * num for freq, num in dataset[:min(x, len(dataset))])
+            answer[i - k] = x_sum
+        return answer
+
+# C++ O(ND + (N-K+1)(D+DlogD) + X) O(ND + X) PrefixArray HashMap Sorting
+class Solution {
+public:
+    vector<int> findXSum(vector<int>& nums, int k, int x) {
+        std::vector<std::vector<int>> prefixMap;
+        std::vector<int> hashmap(51, 0);
+        prefixMap.push_back(hashmap);
+        for (int& num : nums) {
+            ++hashmap[num];
+            prefixMap.push_back(hashmap);
+        }
+        size_t n = nums.size();
+        std::vector<int> answer(n + 1 - k, 0);
+        for (size_t i = k; i <= n; ++i) {
+            std::vector<std::pair<int, int>> dataset;
+            for (int j = 0; j < 51; ++j) {
+                int remFreq = prefixMap[i][j] - prefixMap[i - k][j];
+                if (remFreq) dataset.push_back({j, remFreq});
+            }
+            std::sort(
+                dataset.begin(),
+                dataset.end(),
+                [&](const std::pair<int, int>& x, const std::pair<int, int>& y) {
+                    if (x.second != y.second) return x.second > y.second;
+                    return x.first > y.first;
+                }
+            );
+            int xSum = 0;
+            for (size_t i = 0; i < std::min(static_cast<size_t>(x), dataset.size()); ++i) xSum += dataset[i].first * dataset[i].second;
+            answer[i - k] = xSum;
+        }
+        return answer;
+    }
+};
