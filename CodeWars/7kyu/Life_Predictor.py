aa414fd88da8beff4cbec7a75b7a50a19b791bba
kaluginpeter <streetworkout200412@gmail.com>
2025-11-04 11:15:01 +0300

    Added description "Life Predictor"

---
+++ CodeWars/7kyu/Life_Predictor.py
@@ -0,0 +1,14 @@
+# Almost everyone knows their own birthday. However, we had grown for about ten month when we were born. Few of us know the exact date when our life truly began. So let's build a life predictor to help us figure out when the special moment is.
+#
+# The period of preganacy lasts about 280 days with an error of one or two weeks. Here we will just ignore the error and use 280 days to make the prediction.
+#
+# The calendar in this kata is Gregorian Calendar. Leap year is something you should pay some extra attention. Since Gregorian Calendar is not exist until October 1582, all the test cases will be dates after that moment.
+#
+# The input is the birth date in the "YYYY-MM-DD" format.
+#
+# The output date is also in the same format.
+#
+#   life_predictor("2000-09-18")  #result is "1999-12-13"
+# Hope you will enjoy the kata and find some interesting facts!
+#
+# Date Time
