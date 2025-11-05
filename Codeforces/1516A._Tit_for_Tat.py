44ffd6dfd1eb1a3dff62908d89fcdf2d1916aa46
kaluginpeter <streetworkout200412@gmail.com>
2025-11-05 19:36:35 +0300

    Added solution "1516A. Tit for Tat"

--- Codeforces/1516A._Tit_for_Tat.py
+++ Codeforces/1516A._Tit_for_Tat.py
@@ -62,3 +62,55 @@
 # In the second test case, we start by subtracting 1
 #  from the first element and adding 1
 #  to the second. Then, we can't get any lexicographically smaller arrays, because we can't make any of the elements negative.
+# Solution
+# C++ O(N) O(1) Greedy
+#include <iostream>
+#include <vector>
+
+
+void solution() {
+    size_t n;
+    int k;
+    std::cin >> n >> k;
+    std::vector<int> nums(n, 0);
+    for (size_t i = 0; i < n; ++i) std::cin >> nums[i];
+    size_t left = 0;
+    while (left < n - 1) {
+        int canTake = std::min(k, nums[left]);
+        nums[left] -= canTake;
+        k -= canTake;
+        ++left;
+        nums[n - 1] += canTake;
+
+    }
+    for (int& num : nums) std::cout << num << " ";
+    std::cout << "\n";
+}
+
+
+int main() {
+    size_t t;
+    std::cin >> t;
+    while (t--) solution();
+}
+
+# Python O(N) O(1) Greedy
+import sys
+
+
+def solution() -> None:
+    n, k = map(int, sys.stdin.readline().rstrip().split())
+    nums: list[int] = list(map(int, sys.stdin.readline().rstrip().split()))
+    left: int = 0
+    while left < n - 1:
+        can_take: int = min(k, nums[left])
+        nums[left] -= can_take
+        nums[-1] += can_take
+        k -= can_take
+        left += 1
+    sys.stdout.write('{}\n'.format(' '.join(map(str, nums))))
+
+
+if __name__ == '__main__':
+    t: int = int(sys.stdin.readline().rstrip())
+    for _ in range(t): solution()
