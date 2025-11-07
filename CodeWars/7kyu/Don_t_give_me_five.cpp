c831041533d084344865e4984e899a859a2f48ce
kaluginpeter <streetworkout200412@gmail.com>
2025-11-07 21:16:36 +0300

    Added solution "Don't give me five!"

--- CodeWars/7kyu/Don_t_give_me_five.cpp
+++ CodeWars/7kyu/Don_t_give_me_five.cpp
@@ -17,3 +17,40 @@ I have also created other katas. Take a look if you enjoyed this kata!
 
 MathematicsArraysAlgorithms
 */
+// Solution
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+static ll F_nonneg(ll n) {
+    if (n < 0) return 0;
+    string s = to_string(n);
+    int L = s.size();
+    ll count = 0;
+    vector<ll> pow9(L+1, 1);
+    for (int i = 1; i <= L; ++i) pow9[i] = pow9[i-1] * 9LL;
+    bool prefix_has_5 = false;
+    for (int i = 0; i < L; ++i) {
+        int d = s[i] - '0';
+        int choices_less = 0;
+        for (int dig = 0; dig < d; ++dig) if (dig != 5) ++choices_less;
+        int remaining = L - i - 1;
+        count += (ll)choices_less * pow9[remaining];
+        if (d == 5) { prefix_has_5 = true; break; }
+    }
+    if (!prefix_has_5) count += 1;
+    return count;
+}
+
+ll dontGiveMeFive(ll start, ll end) {
+    if (end < 0) {
+        ll a = -end;
+        ll b = -start;
+        return F_nonneg(b) - F_nonneg(a - 1);
+    } else if (start >= 0) return F_nonneg(end) - F_nonneg(start - 1);
+    else {
+        ll neg_count = F_nonneg(-start) - F_nonneg(0);
+        ll nonneg_count = F_nonneg(end);
+        return neg_count + nonneg_count;
+    }
+}
