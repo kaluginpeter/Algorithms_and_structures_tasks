d963059f4cf5c0233f0da1a521799094ec415167
kaluginpeter <streetworkout200412@gmail.com>
2025-11-08 19:52:28 +0300

    Added description "Pizza pieces"

---
+++ CodeWars/6kyu/Pizza_pieces.py
@@ -0,0 +1,5 @@
+# In her trip to Italy, Elizabeth Gilbert made it her duty to eat perfect pizza. One day she ordered one for dinner, and then some Italian friends appeared at her room. The problem is that there were many people who ask for a piece of pizza at that moment, and she had a knife that only cuts straight.
+#
+# Given the number of pizza cuts, find the maximum amount of pieces of pizza that you can get (not necessarily of equal size). If the number of cuts is negative, return -1 instead (or Nothing in Haskell).
+#
+# MathematicsAlgorithms
