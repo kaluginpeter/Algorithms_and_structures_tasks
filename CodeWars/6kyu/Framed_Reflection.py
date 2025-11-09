003ce1e3bb1e5970ab475230f1bad8b65a48d2f7
kaluginpeter <streetworkout200412@gmail.com>
2025-11-09 20:41:06 +0300

    Added description "Framed Reflection"

---
+++ CodeWars/6kyu/Framed_Reflection.py
@@ -0,0 +1,11 @@
+# 100th Kata
+# You are given a message (text) that you choose to read in a mirror (weirdo). Return what you would see, complete with the mirror frame. Example:
+#
+# 'Hello World'
+#
+# would give:
+#
+#
+# Words in your solution should be left-aligned.
+#
+# FundamentalsStringsArrays
