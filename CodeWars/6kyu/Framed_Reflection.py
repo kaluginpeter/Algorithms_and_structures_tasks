9f06642ba00d58034cfa8b3ad4d22850afd42cbb
kaluginpeter <streetworkout200412@gmail.com>
2025-11-09 20:41:28 +0300

    Added solution "Framed Reflection"

--- CodeWars/6kyu/Framed_Reflection.py
+++ CodeWars/6kyu/Framed_Reflection.py
@@ -9,3 +9,13 @@
 # Words in your solution should be left-aligned.
 #
 # FundamentalsStringsArrays
+# Solution
+def mirror(text):
+    words: list[str] = [word[::-1] for word in text.split()]
+    bound: int = max(len(word) for word in words)
+    output: list[str] = ['*' * (bound + 4)]
+    for word in words:
+        part: str = '* ' + word + ' ' * (bound - len(word)) + ' *'
+        output.append(part)
+    output.append('*' * (bound + 4))
+    return '\n'.join(output)
