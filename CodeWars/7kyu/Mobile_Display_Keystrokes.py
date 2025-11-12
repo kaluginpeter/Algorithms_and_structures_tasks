bb4459450a5129479d1264eb9bd73e6eabdbe88f
kaluginpeter <streetworkout200412@gmail.com>
2025-11-12 21:12:40 +0300

    Added description "Mobile Display Keystrokes"

---
+++ CodeWars/7kyu/Mobile_Display_Keystrokes.py
@@ -0,0 +1,15 @@
+# Do you remember the old mobile display keyboards? Do you also remember how inconvenient it was to write on it?
+# Well, here you have to calculate how many keystrokes you have to do for a specific word.
+#
+# This is the layout:
+#
+#
+# Given a string, return the number of keystrokes necessary to type it. You can assume that the input will entirely consist of characters included in the mobile layout (lowercase letters, digits, and the symbols * and #).
+#
+# Examples
+# "*#"       =>  2 (1 + 1)
+# "123"      =>  3 (1 + 1 + 1)
+# "abc"      =>  9 (2 + 3 + 4)
+# "codewars" => 26 (4 + 4 + 2 + 3 + 2 + 2 + 4 + 5)
+# Fundamentals
+# Solution
