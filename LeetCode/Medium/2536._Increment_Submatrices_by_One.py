e150a96c0f29949b508934e1fd81d46f03dee9dd
kaluginpeter <streetworkout200412@gmail.com>
2025-11-14 08:54:32 +0300

    Added solution "2536. Increment Submatrices by One"

--- LeetCode/Medium/2536._Increment_Submatrices_by_One.py
+++ LeetCode/Medium/2536._Increment_Submatrices_by_One.py
@@ -30,3 +30,36 @@
 # 1 <= queries.length <= 104
 # 0 <= row1i <= row2i < n
 # 0 <= col1i <= col2i < n
+# Solution
+# Python O(QN + NM) O(NM) Matrix PrefixSum
+class Solution:
+    def rangeAddQueries(self, n: int, queries: List[List[int]]) -> List[List[int]]:
+        matrix: list[list[int]] = [[0] * n for _ in range(n)]
+        for r1, c1, r2, c2 in queries:
+            for i in range(r1, r2 + 1):
+                matrix[i][c1] += 1
+                if c2 + 1 < n: matrix[i][c2 + 1] -= 1
+        for i in range(n):
+            for j in range(1, n):
+                matrix[i][j] += matrix[i][j - 1]
+        return matrix
+
+# C++ O(QN + NM) O(NM) Matrix PrefixSum
+class Solution {
+public:
+    vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
+        std::vector<std::vector<int>> matrix(n, std::vector<int>(n, 0));
+        for (std::vector<int>& q : queries) {
+            for (size_t i = q[0]; i <= q[2]; ++i) {
+                ++matrix[i][q[1]];
+                if (q[3] + 1 < n) --matrix[i][q[3] + 1];
+            }
+        }
+        for (size_t i = 0; i < n; ++i) {
+            for (size_t j = 1; j < n; ++j) {
+               matrix[i][j] += matrix[i][j - 1];
+            }
+        }
+        return matrix;
+    }
+};
