2b3a49e92c38cf1baea48366b612c045256e40fc
kaluginpeter <streetworkout200412@gmail.com>
2025-11-15 21:03:14 +0300

    Added solution "3234. Count the Number of Substrings With Dominant Ones"

--- LeetCode/Medium/3234._Count_the_Number_of_Substrings_With_Dominant_Ones.py
+++ LeetCode/Medium/3234._Count_the_Number_of_Substrings_With_Dominant_Ones.py
@@ -46,3 +46,52 @@
 #
 # 1 <= s.length <= 4 * 104
 # s consists only of characters '0' and '1'.
+# Solution
+# Python O(Nsqrt(N)) O(N) Prefix SlidingWindow
+class Solution:
+    def numberOfSubstrings(self, s: str) -> int:
+        n: int = len(s)
+        pre: list[int] = [0] * (n + 1)
+        pre[0] = -1
+        for i in range(n):
+            if not i or s[i - 1] == '0': pre[i + 1] = i
+            else: pre[i + 1] = pre[i]
+        output: int = 0
+        for i in range(1, n + 1):
+            zeros: int = s[i - 1] == '0'
+            j: int = i
+            while j and zeros**2 <= n:
+                ones: int = i - pre[j] - zeros
+                if zeros**2 <= ones:
+                    output += min(j - pre[j], ones - zeros**2 + 1)
+                j = pre[j]
+                zeros += 1
+        return output
+
+# C++ O(Nsqrt(N)) O(N) SlidingWindow
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        int n = s.size();
+        std::vector<int> pre(n + 1);
+        pre[0] = -1;
+        for (int i = 0; i < n; ++i) {
+            if (i == 0 || (i > 0 && s[i - 1] == '0')) pre[i + 1] = i;
+            else pre[i + 1] = pre[i];
+        }
+        int output = 0;
+        for (int i = 1; i <= n; ++i) {
+            int zeros = s[i - 1] == '0';
+            int j = i;
+            while (j > 0 && zeros * zeros <= n) {
+                int ones = (i - pre[j]) - zeros;
+                if (zeros * zeros <= ones) {
+                    output += std::min(j - pre[j], ones - zeros * zeros + 1);
+                }
+                j = pre[j];
+                ++zeros;
+            }
+        }
+        return output;
+    }
+};
