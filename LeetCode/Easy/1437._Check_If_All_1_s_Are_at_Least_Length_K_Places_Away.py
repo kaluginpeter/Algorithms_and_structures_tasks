789703e4434ad38f52efe00c63c96f7c5cb3d7f5
kaluginpeter <streetworkout200412@gmail.com>
2025-11-17 08:19:23 +0300

    Added solution "1437. Check If All 1's Are at Least Length K Places Away"

--- LeetCode/Easy/1437._Check_If_All_1_s_Are_at_Least_Length_K_Places_Away.py
+++ LeetCode/Easy/1437._Check_If_All_1_s_Are_at_Least_Length_K_Places_Away.py
@@ -21,3 +21,33 @@
 # 1 <= nums.length <= 105
 # 0 <= k <= nums.length
 # nums[i] is 0 or 1
+# Solution
+# Python O(N) O(1) TwoPointers
+class Solution:
+    def kLengthApart(self, nums: List[int], k: int) -> bool:
+        left: int = 0
+        right: int = 0
+        n: int = len(nums)
+        while right < n:
+            while left < n and not nums[left]: left += 1
+            right = left + 1
+            while right < n and not nums[right]: right += 1
+            if right < n and right - left - 1 < k: return False
+            left = right
+        return True
+
+# C++ O(N) O(1) TwoPointers
+class Solution {
+public:
+    bool kLengthApart(vector<int>& nums, int k) {
+        size_t left = 0, right = 0, n = nums.size();
+        while (right < n) {
+            while (left < n && !nums[left]) ++left;
+            right = left + 1;
+            while (right < n && !nums[right]) ++right;
+            if (right < n && (right - left - 1) < k) return false;
+            left = right;
+        }
+        return true;
+    }
+};
