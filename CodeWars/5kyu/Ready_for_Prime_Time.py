7cf484633c0d798f700bfb2e8ae78b2cc0148e43
kaluginpeter <streetworkout200412@gmail.com>
2025-11-17 08:59:25 +0300

    Added description "(Ready for) Prime Time"

---
+++ CodeWars/5kyu/Ready_for_Prime_Time.py
@@ -0,0 +1,8 @@
+# We need prime numbers and we need them now!
+#
+# Write a method that takes a maximum bound and returns all primes up to and including the maximum bound.
+#
+# For example,
+#
+# 11 => [2, 3, 5, 7, 11]
+# MathematicsAlgorithms
