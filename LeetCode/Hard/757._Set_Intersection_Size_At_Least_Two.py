ee6bd52bd7d35c0bce709c94051e778120bd5cfe
kaluginpeter <streetworkout200412@gmail.com>
2025-11-20 21:56:49 +0300

    Added solution "757. Set Intersection Size At Least Two"

--- LeetCode/Hard/757._Set_Intersection_Size_At_Least_Two.py
+++ LeetCode/Hard/757._Set_Intersection_Size_At_Least_Two.py
@@ -32,3 +32,38 @@
 # 1 <= intervals.length <= 3000
 # intervals[i].length == 2
 # 0 <= starti < endi <= 108
+# Solution
+# Python O(NlogN) O(1) Sorting
+class Solution:
+    def intersectionSizeTwo(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key=lambda point: (point[1], -point[0]))
+        output: int = 2
+        R: int = intervals[0][1]
+        L: int = R - 1
+        for start, end in intervals[1:]:
+            if L >= start: continue
+            if start > R: output += 1
+            output += 1
+            L = R if start <= R else end - 1
+            R = end
+        return output
+
+# C++ O(NlogN) O(1) Sorting
+class Solution {
+public:
+    int intersectionSizeTwo(vector<vector<int>>& intervals) {
+        std::sort(intervals.begin(), intervals.end(), [&](const std::vector<int>& x, const std::vector<int>& y) {
+            if (x[1] != y[1]) return x[1] < y[1];
+            return x[0] > y[0];
+        });
+        int R = intervals[0][1], L = R - 1, output = 2;
+        for (size_t i = 1; i < intervals.size(); ++i) {
+            if (L >= intervals[i][0]) continue;
+            if (intervals[i][0] > R) ++output;
+            ++output;
+            L = (intervals[i][0] > R ? intervals[i][1] - 1 : R);
+            R = intervals[i][1];
+        }
+        return output;
+    }
+};
