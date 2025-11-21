140e2622ab1103b1269a1fa71d2f2be68c62aea9
kaluginpeter <streetworkout200412@gmail.com>
2025-11-21 21:54:05 +0300

    Added description "Print a Rectangle Using Asterisks"

---
+++ CodeWars/7kyu/Print_a_Rectangle_Using_Asterisks.py
@@ -0,0 +1,14 @@
+# Write a method that, given two arguments, width and height, returns a string representing a rectangle with those dimensions.
+#
+# The rectangle should be filled with spaces, and its borders should be composed of asterisks (*).
+#
+# For example, given width = 3 and height = 3:
+#
+# ***
+# * *
+# ***
+# End each line of the string (including the last one) with a carriage return-line feed combination.
+#
+# Note: You may assume that width and height will always be greater than zero.
+#
+# StringsASCII ArtAlgorithms
