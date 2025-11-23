b5583bce99872f8944730f0f47664c30d1012b48
kaluginpeter <streetworkout200412@gmail.com>
2025-11-23 17:48:31 +0300

    Added solution "1262. Greatest Sum Divisible by Three"

--- LeetCode/Medium/1262._Greatest_Sum_Divisible_by_Three.py
+++ LeetCode/Medium/1262._Greatest_Sum_Divisible_by_Three.py
@@ -23,3 +23,33 @@
 #
 # 1 <= nums.length <= 4 * 104
 # 1 <= nums[i] <= 104
+# Solution
+# Python O(ND) O(D) DynamicProgramming
+class Solution:
+    def maxSumDivThree(self, nums: List[int]) -> int:
+        dp: list[int] = [0, float('-inf'), float('-inf')]
+        for num in nums:
+            next_dp: list[int] = dp.copy()
+            for i in range(3):
+                next_dp[(i + num % 3) % 3] = max(next_dp[(i + num % 3) % 3], dp[i] + num)
+            dp = next_dp
+        return dp[0]
+
+# C++ O(ND) O(D) DynamicProgramming
+class Solution {
+public:
+    int maxSumDivThree(vector<int>& nums) {
+        std::vector<int> dp = {0, INT_MIN, INT_MIN};
+        for (int& num : nums) {
+            std::vector<int> nextDp = dp;
+            for (int i = 0; i < 3; ++i) {
+                nextDp[(i + num % 3) % 3] = std::max(
+                    nextDp[(i + num % 3) % 3],
+                    dp[i] + num
+                );
+            }
+            dp = std::move(nextDp);
+        }
+        return dp[0];
+    }
+};
