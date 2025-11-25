7c9f9c257ba8e28dad58d6035d7e82eca1d3f3c1
kaluginpeter <streetworkout200412@gmail.com>
2025-11-25 09:04:23 +0300

    Added description "Consecutive Digit Constraints"

---
+++ CodeWars/5kyu/Consecutive_Digit_Constraints.py
@@ -0,0 +1,15 @@
+# This kata is based on Project Euler Problem 164
+#
+# Objective
+# Write a function that takes a number n and returns the number of n digit numbers (without leading zeros) that no three consecutive digits have a sum greater than 9
+#
+# Examples
+# | n  |  number of numbers
+# |--------------------------
+# | 1  |  9 (0 doesn't count)
+# | 2  | 45
+# | 3  | 165
+# Note:
+# n ranges from 1 to 500
+# Code length limit = 5000 to avoid hard coded solutions
+# MathematicsAlgorithms
