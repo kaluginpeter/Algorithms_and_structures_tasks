f2c80d7b2ed7cdc93914de1e21c005a7c5a5fb14
kaluginpeter <streetworkout200412@gmail.com>
2025-11-25 09:04:42 +0300

    Added solution "Consecutive Digit Constraints"

--- CodeWars/5kyu/Consecutive_Digit_Constraints.py
+++ CodeWars/5kyu/Consecutive_Digit_Constraints.py
@@ -13,3 +13,19 @@
 # n ranges from 1 to 500
 # Code length limit = 5000 to avoid hard coded solutions
 # MathematicsAlgorithms
+# Solution
+def number_of_numbers(n):
+    if n == 1: return 9
+    dp = [[0] * 10 for _ in range(10)]
+    for a in range(1, 10):
+        for b in range(10):
+            if a + b <= 9: dp[a][b] = 1
+    for _ in range(3, n + 1):
+        new_dp = [[0] * 10 for _ in range(10)]
+        for a in range(10):
+            for b in range(10):
+                if dp[a][b] == 0: continue
+                for c in range(10):
+                    if a + b + c <= 9: new_dp[b][c] += dp[a][b]
+        dp = new_dp
+    return sum(sum(row) for row in dp)
