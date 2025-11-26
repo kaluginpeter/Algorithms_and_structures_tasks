09ddfe288c5d738483c660de5efb778ac9e35674
kaluginpeter <streetworkout200412@gmail.com>
2025-11-26 21:41:25 +0300

    Added solution "2435. Paths in Matrix Whose Sum Is Divisible by K"

--- LeetCode/Hard/2435._Paths_in_Matrix_Whose_Sum_Is_Divisible_by_K.py
+++ LeetCode/Hard/2435._Paths_in_Matrix_Whose_Sum_Is_Divisible_by_K.py
@@ -34,3 +34,46 @@
 # 1 <= m * n <= 5 * 104
 # 0 <= grid[i][j] <= 100
 # 1 <= k <= 50
+# Solution
+# Python O(NMK) O(MK) DynamicProgramming
+class Solution:
+    def numberOfPaths(self, grid: List[List[int]], k: int) -> int:
+        n: int = len(grid)
+        m: int = len(grid[0])
+        dp: list[list[int]] = [[0] * k for _ in range(m + 1)]
+        mod: int = 1000000007
+        for i in range(1, n + 1):
+            next_dp: list[list[int]] = [[0] * k for _ in range(m + 1)]
+            if i == 1: next_dp[1][grid[0][0] % k] = 1
+            for j in range(1, m + 1):
+                for grid_sum in range(k):
+                    rem: int = (grid_sum + grid[i - 1][j - 1]) % k
+                    next_dp[j][rem] = (next_dp[j][rem] + next_dp[j - 1][grid_sum]) % mod
+                    next_dp[j][rem] = (next_dp[j][rem] + dp[j][grid_sum]) % mod
+            dp = next_dp
+        return dp[m][0]
+
+# C++ O(NMK) O(MK) DynamicProgramming
+constexpr int mod = 1e9 + 7;
+class Solution {
+public:
+    int numberOfPaths(vector<vector<int>>& grid, int k) {
+        size_t n = grid.size(), m = grid[0].size();
+        std::vector<std::array<int, 50>> dp(m + 1, std::array<int, 50>{});
+        for (size_t i = 1; i <= n; ++i) {
+            std::vector<std::array<int, 50>> nextDp(m + 1, std::array<int, 50>{});
+            if (i == 1) nextDp[1][grid[0][0] % k] = 1;
+            for (size_t j = 1; j <= m; ++j) {
+                for (size_t gridSum = 0; gridSum < k; ++gridSum) {
+                    size_t rem = (gridSum + grid[i - 1][j - 1]) % k;
+                    // check move right
+                    nextDp[j][rem] = (nextDp[j][rem] + nextDp[j - 1][gridSum]) % mod;
+                    // check move down
+                    nextDp[j][rem] = (nextDp[j][rem] + dp[j][gridSum]) % mod;
+                }
+            }
+            dp = nextDp;
+        }
+        return dp[m][0];
+    }
+};
