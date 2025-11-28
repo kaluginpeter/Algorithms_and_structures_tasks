746c619ad6e14540f81c190b1924ea5cfe2759ee
kaluginpeter <streetworkout200412@gmail.com>
2025-11-28 21:21:25 +0300

    Added solution "The most common letter"

--- CodeWars/6kyu/The_most_common_letter.py
+++ CodeWars/6kyu/The_most_common_letter.py
@@ -8,3 +8,23 @@
 # ('real talk bro', 'n') => 'neal talk bno'
 # ('great job go ahead', 'k') => 'grekt job go khekd'
 # FundamentalsStringsArrays
+# Solution
+from collections import defaultdict
+
+def replace_common(st, letter):
+    bound: int = -1
+    target: str = ''
+    seen: dict[str, int] = defaultdict(int)
+    first: dict[str, int] = dict()
+    for i in range(len(st)):
+        char = st[i]
+        if char not in first: first[char] = i
+        if char == ' ': continue
+        seen[char] += 1
+        if seen[char] > bound:
+            bound = seen[char]
+            target = char
+        elif seen[char] == bound and first[char] < first[target]:
+            bound = seen[char]
+            target = char
+    return st.replace(target, letter)
