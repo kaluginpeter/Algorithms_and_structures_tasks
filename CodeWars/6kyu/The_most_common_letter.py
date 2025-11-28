745765c21dce4970a0cdb32cfb84e71c4aca5940
kaluginpeter <streetworkout200412@gmail.com>
2025-11-28 21:20:58 +0300

    Added description "The most common letter"

---
+++ CodeWars/6kyu/The_most_common_letter.py
@@ -0,0 +1,10 @@
+# Find the most common letter (not a space) in the given string (comprised of at least 3 lowercase words) and replace it with the given letter.
+#
+# If such letters are two or more, choose the one that appears earliest in the string.
+#
+# For example:
+#
+# ('my mom loves me as never did', 't') => 'ty tot loves te as never did'
+# ('real talk bro', 'n') => 'neal talk bno'
+# ('great job go ahead', 'k') => 'grekt job go khekd'
+# FundamentalsStringsArrays
