8ce805703623b86c0b032e548a9a416ae32a36ce
kaluginpeter <streetworkout200412@gmail.com>
2025-11-29 21:45:14 +0300

    Added solution "Anagram Detection"

--- CodeWars/7kyu/ Anagram_Detection.cpp
+++ CodeWars/7kyu/ Anagram_Detection.cpp
@@ -12,3 +12,14 @@ Examples
 
 StringsFundamentals
 */
+// Solution
+#include <string>
+#include <algorithm>
+bool isAnagram(std::string test, std::string original){
+  std::transform(test.begin(), test.end(), test.begin(), tolower);
+  std::sort(test.begin(), test.end());
+  std::transform(original.begin(), original.end(), original.begin(), tolower);
+  std::sort(original.begin(), original.end());
+
+  return test == original;
+}
