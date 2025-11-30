9d7d5f7da14963a4269cb79087c56257fd6a469a
kaluginpeter <streetworkout200412@gmail.com>
2025-11-30 20:47:19 +0300

    Added solution "Finding people with a speeding record"

--- CodeWars/7kyu/Finding_people_with_a_speeding_record.py
+++ CodeWars/7kyu/Finding_people_with_a_speeding_record.py
@@ -24,3 +24,12 @@
 # GROUP BY is your friend...
 #
 # SQLDatabasesFundamentals
+# Solution
+SELECT
+    p.id AS person_id,
+    p.birthdate,
+    COALESCE(SUM(r.speed_delta), 0) AS total_speed_delta
+FROM People p
+LEFT JOIN Records r ON r.person_id = p.id
+GROUP BY p.id, p.birthdate
+ORDER BY p.id ASC;
