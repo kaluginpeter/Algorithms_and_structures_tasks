2ea4526d6691ef1562385934e9e32300e620049f
kaluginpeter <streetworkout200412@gmail.com>
2025-12-03 21:53:28 +0300

    Added description "Decimals or groups of thousands??"

---
+++ CodeWars/6kyu/Decimals_or_groups_of_thousands.py
@@ -0,0 +1,9 @@
+# Countries such as the U.S. and China use a dot to represent a decimal point, they also use a comma to seperate groups of thousands e.g. 4,353.56
+#
+# Other countries (mostly in Europe) instead use a comma to represent a decimal point, they also use a dot to seperate groups of thousands e.g. 4.353,56
+#
+# Your task is to sum up an array of string repensentation of numbers being in one of the two formats mentioned above with AT MOST two decimal places. The resulted sum should be a string with the format of xx,xxx.xx (keep two decimal places, seperate groups of thousands with comma if necessary)
+#
+# This Kata is inspired by one of the bugs that almost happened due to the differences in number formatting ( •̀ω•́ )σ
+#
+# Regular ExpressionsFundamentals
