d893d6a95b15a3021aab2f2752a9f9e3378a01ab
kaluginpeter <streetworkout200412@gmail.com>
2025-12-04 21:32:49 +0300

    Added description "Pascal's Diagonals"

---
+++ CodeWars/5kyu/Pascal_s_Diagonals.py
@@ -0,0 +1,8 @@
+# Create a function that returns an array containing the first l numbers from the nth diagonal of Pascal's triangle.
+#
+# n = 0 should generate the first diagonal of the triangle (the ones).
+# The first number in each diagonal should be 1.
+# If l = 0, return an empty array.
+# Both n and l will be non-negative integers in all test cases.
+#
+# Algorithms
