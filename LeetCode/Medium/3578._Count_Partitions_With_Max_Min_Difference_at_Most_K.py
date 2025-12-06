ff0b25abb395e63c06aecd7ed686cf5ab0452fc9
kaluginpeter <streetworkout200412@gmail.com>
2025-12-06 21:24:25 +0300

    Added solution "3578. Count Partitions With Max-Min Difference at Most K"

--- LeetCode/Medium/3578._Count_Partitions_With_Max_Min_Difference_at_Most_K.py
+++ LeetCode/Medium/3578._Count_Partitions_With_Max_Min_Difference_at_Most_K.py
@@ -41,3 +41,59 @@
 # 2 <= nums.length <= 5 * 104
 # 1 <= nums[i] <= 109
 # 0 <= k <= 109
+# Solution
+# Python O(N) O(N) PrefixSum
+mod: int = 10**9 + 7
+class Solution:
+    def countPartitions(self, nums: List[int], k: int) -> int:
+        n: int = len(nums)
+        dp: list[int] = [0] * (n + 1)
+        prefix: list[int] = [0] * (n + 1)
+        min_q: list[int] = deque()
+        max_q: list[int] = deque()
+        dp[0] = 1
+        prefix[0] = 1
+        j: int = 0
+        for i in range(n):
+            while max_q and nums[max_q[-1]] <= nums[i]: max_q.pop()
+            max_q.append(i)
+            while min_q and nums[min_q[-1]] >= nums[i]: min_q.pop()
+            min_q.append(i)
+            while max_q and min_q and nums[max_q[0]] - nums[min_q[0]] > k:
+                if max_q[0] == j: max_q.popleft()
+                if min_q[0] == j: min_q.popleft()
+                j += 1
+            if j > 0:
+                dp[i + 1] = (prefix[i] - prefix[j - 1] + mod) % mod
+            else:
+                dp[i + 1] = prefix[i] % mod
+            prefix[i + 1] = (prefix[i] + dp[i + 1]) % mod
+        return dp[n]
+
+# C++ O(N) O(N) Prefix
+constexpr long long mod = 1000000007;
+class Solution {
+public:
+    int countPartitions(vector<int>& nums, int k) {
+        int n = nums.size();
+        std::vector<long long> dp(n + 1), prefix(n + 1);
+        std::deque<int> minQ, maxQ;
+        dp[0] = 1;
+        prefix[0] = 1;
+        for (int i = 0, j = 0; i < n; ++i) {
+            while (!maxQ.empty() && nums[maxQ.back()] <= nums[i]) maxQ.pop_back();
+            maxQ.push_back(i);
+            while (!minQ.empty() && nums[minQ.back()] >= nums[i]) minQ.pop_back();
+            minQ.push_back(i);
+            while (!maxQ.empty() && !minQ.empty() &&
+                   nums[maxQ.front()] - nums[minQ.front()] > k) {
+                if (maxQ.front() == j) maxQ.pop_front();
+                if (minQ.front() == j) minQ.pop_front();
+                ++j;
+            }
+            dp[i + 1] = (prefix[i] - (j > 0 ? prefix[j - 1] : 0) + mod) % mod;
+            prefix[i + 1] = (prefix[i] + dp[i + 1]) % mod;
+        }
+        return dp[n];
+    }
+};
