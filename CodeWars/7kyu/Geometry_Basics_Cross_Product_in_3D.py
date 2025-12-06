3d6df3e4840ddf0c9c7b4bce9dd7219ecc1cf568
kaluginpeter <streetworkout200412@gmail.com>
2025-12-06 21:27:58 +0300

    Added description "Geometry Basics: Cross Product in 3D"

---
+++ CodeWars/7kyu/Geometry_Basics_Cross_Product_in_3D.py
@@ -0,0 +1,9 @@
+# This series of katas will introduce you to basics of doing geometry with computers.
+#
+# Vector objects (struct in C) have x, y, and z attributes. x, y and z are floating-point numbers.
+#
+# Write a function calculating the cross product of Vector a and Vector b, the result should be a Vector object.
+#
+# You can read more about cross product on Wikipedia.
+#
+# GeometryFundamentals
