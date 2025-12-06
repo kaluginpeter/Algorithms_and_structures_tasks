7234a7c81afc88779fbe30e1b77ce6b8a0656e71
kaluginpeter <streetworkout200412@gmail.com>
2025-12-06 21:28:18 +0300

    Added solution "Geometry Basics: Cross Product in 3D"

--- CodeWars/7kyu/Geometry_Basics_Cross_Product_in_3D.py
+++ CodeWars/7kyu/Geometry_Basics_Cross_Product_in_3D.py
@@ -7,3 +7,11 @@
 # You can read more about cross product on Wikipedia.
 #
 # GeometryFundamentals
+# Solution
+from preloaded import Vector
+
+def cross_product(a: Vector, b: Vector) -> Vector:
+    x: int = a.y * b.z - a.z * b.y
+    y: int = a.z * b.x - a.x * b.z
+    z: int = a.x * b.y - a.y * b.x
+    return Vector(x, y, z)
