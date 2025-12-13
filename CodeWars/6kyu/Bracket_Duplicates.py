ee790d3fa8f698e6d2a5840864d87828d40a57c5
kaluginpeter <streetworkout200412@gmail.com>
2025-12-13 21:35:35 +0300

    Added description "Bracket Duplicates"

---
+++ CodeWars/6kyu/Bracket_Duplicates.py
@@ -0,0 +1,7 @@
+# Create a program that will take in a string as input and, if there are duplicates of more than two alphabetical characters in the string, returns the string with all the extra characters in a bracket.
+#
+# For example, the input "aaaabbcdefffffffg" should return "aa[aa]bbcdeff[fffff]g"
+#
+# Please also ensure that the input is a string, and return "Please enter a valid string" if it is not.
+#
+# StringsRegular ExpressionsAlgorithms
