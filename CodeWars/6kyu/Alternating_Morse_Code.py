700ac3b40efa39a6cc448538266c4a41c7b305aa
kaluginpeter <streetworkout200412@gmail.com>
2025-12-16 22:04:12 +0300

    Added solution "Alternating Morse Code"

--- CodeWars/6kyu/Alternating_Morse_Code.py
+++ CodeWars/6kyu/Alternating_Morse_Code.py
@@ -76,3 +76,20 @@
 # In order to prevent hardcoded solutions, your code's limit size needs to be less than 5000 characters.
 # MORSE_CODE preloaded dictionary is given to you, for a more convenient debugging.
 # AlgorithmsRecursion
+# Solution
+from preloaded import MORSE_CODE
+
+def alternating_morse_code(letter: str) -> set[str]:
+    output: set[str] = set()
+    if '--' in MORSE_CODE[letter] or '..' in MORSE_CODE[letter]: return output
+    cur_nodes: list[tuple[str, str]] = [(letter, MORSE_CODE[letter])]
+    next_nodes: list[tuple[str, str]] = []
+    while cur_nodes:
+        for node, path in cur_nodes:
+            output.add(node)
+            for ch, seq in MORSE_CODE.items():
+                if path[-1] != seq[0] and '--' not in seq and '..' not in seq and ch not in node:
+                    next_nodes.append((node + ch, path + seq))
+        cur_nodes = next_nodes
+        next_nodes = []
+    return output
