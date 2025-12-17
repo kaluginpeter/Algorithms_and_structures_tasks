e1947ab16f4556a7ea7dd9a969c42c2893be5757
kaluginpeter <streetworkout200412@gmail.com>
2025-12-17 22:07:03 +0300

    Added solution "3573. Best Time to Buy and Sell Stock V"

--- LeetCode/Medium/3573._Best_Time_to_Buy_and_Sell_Stock_V.py
+++ LeetCode/Medium/3573._Best_Time_to_Buy_and_Sell_Stock_V.py
@@ -42,3 +42,39 @@
 # 2 <= prices.length <= 103
 # 1 <= prices[i] <= 109
 # 1 <= k <= prices.length / 2
+# Solution
+# Python O(NK) O(K) DynamicProgramming
+class Solution:
+    def maximumProfit(self, prices: List[int], k: int) -> int:
+        n: int = len(prices)
+        dp: list[list[int]] = [[0, 0, 0] for _ in range(k + 1)]
+        for j in range(1, k + 1):
+            dp[j][1] = -prices[0]
+            dp[j][2] = prices[0]
+        for i in range(1, n):
+            for j in range(k, 0, -1):
+                dp[j][0] = max(dp[j][0], max(dp[j][1] + prices[i], dp[j][2] - prices[i]))
+                dp[j][1] = max(dp[j][1], dp[j - 1][0] - prices[i])
+                dp[j][2] = max(dp[j][2], dp[j - 1][0] + prices[i])
+        return dp[k][0]
+
+# C++ O(NK) O(K) DynamicProgramming
+class Solution {
+public:
+    long long maximumProfit(vector<int>& prices, int k) {
+        int n = prices.size();
+        std::vector<std::vector<long long>> dp(k + 1, std::vector<long long>(3));
+        for (int j = 1; j <= k; ++j) {
+            dp[j][1] = -prices[0];
+            dp[j][2] = prices[0];
+        }
+        for (int i = 1; i < n; ++i) {
+            for (int j = k; j > 0; --j) {
+                dp[j][0] = std::max(dp[j][0], std::max(dp[j][1] + prices[i], dp[j][2] - prices[i]));
+                dp[j][1] = std::max(dp[j][1], dp[j - 1][0] - prices[i]);
+                dp[j][2] = std::max(dp[j][2], dp[j - 1][0] + prices[i]);
+            }
+        }
+        return dp[k][0];
+    }
+};
