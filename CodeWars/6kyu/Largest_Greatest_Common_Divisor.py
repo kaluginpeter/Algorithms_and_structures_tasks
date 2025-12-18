f08288e28178f5aaf5eb29ed1a93f340748e34f3
kaluginpeter <streetworkout200412@gmail.com>
2025-12-18 22:22:02 +0300

    Added description "Largest Greatest Common Divisor"

---
+++ CodeWars/6kyu/Largest_Greatest_Common_Divisor.py
@@ -0,0 +1,70 @@
+# You are given positive integers,
+# a
+# a and
+# b
+# b
+# (
+# a
+# <
+# b
+# )
+# (a<b).
+#
+# Find a positive integer
+# k
+# k such that
+# gcd
+# (
+# a
+# +
+# k
+# ,
+# b
+# +
+# k
+# )
+# gcd(a+k,b+k) is maximized.
+#
+# If there are multiple values of
+# k
+# k that satisfy the condition above, return the smallest one.
+#
+# Here
+# gcd
+# (
+# x
+# ,
+# y
+# )
+# gcd(x,y) means the greatest common divisor of
+# x
+# x and
+# y
+# y.
+#
+# C
+# o
+# n
+# s
+# t
+# r
+# a
+# i
+# n
+# t
+# s
+# Constraints
+#
+# 1
+# ≤
+# a
+# <
+# b
+# ≤
+# 1
+# 0
+# 18
+# 1≤a<b≤10
+# 18
+#
+# Algebra
