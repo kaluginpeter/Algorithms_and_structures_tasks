546856f3af285706839ffd55ff293b80f92466f4
kaluginpeter <streetworkout200412@gmail.com>
2025-12-19 23:17:11 +0300

    Added solution "2092. Find All People With Secret"

--- LeetCode/Hard/2092._Find_All_People_With_Secret.py
+++ LeetCode/Hard/2092._Find_All_People_With_Secret.py
@@ -48,3 +48,60 @@
 # xi != yi
 # 1 <= timei <= 105
 # 1 <= firstPerson <= n - 1
+# Solution
+# Python O(E + ElogE + (V + E) + V) O(V + E) Graph Depth-First-Search Sorting
+class Solution:
+    def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
+        graph: dict[int, list[tuple[int, int]]] = dict()
+        for u, v, t in meetings:
+            if u not in graph: graph[u] = []
+            if v not in graph: graph[v] = []
+            graph[u].append((t, v))
+            graph[v].append((t, u))
+        for vertex, edges in graph.items(): edges.sort()
+        was_death: list[int] = [float('inf')] * n
+        was_death[0] = 0
+        was_death[firstPerson] = 0
+        q: list[tuple[int, int]] = [(0, 0), (firstPerson, 0)]
+        while q:
+            zomby, infected_at = q.pop()
+            for meeting, victum in graph.get(zomby, []):
+                if infected_at <= meeting and meeting < was_death[victum]:
+                    was_death[victum] = meeting
+                    q.append((victum, meeting))
+        return [victum for victum in range(n) if was_death[victum] != float('inf')]
+
+# C++ O(E + ElogE + (V + E) + V) O(V + E) Sorting Depth-First-Search Graph
+class Solution {
+public:
+    vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson) {
+        std::unordered_map<int, std::vector<std::pair<int, int>>> graph;
+        for (auto& meeting : meetings) {
+            int u = meeting[0], v = meeting[1], t = meeting[2];
+            graph[v].emplace_back(t, u);
+            graph[u].emplace_back(t, v);
+        }
+        for (auto& p : graph) std::sort(p.second.begin(), p.second.end());
+        std::vector<int> secretAt(n, INT_MAX);
+        secretAt[0] = 0;
+        secretAt[firstPerson] = 0;
+        std::queue<std::pair<int, int>> q;
+        q.emplace(0, 0);
+        q.emplace(firstPerson, 0);
+        while (!q.empty()) {
+            auto [person, known] = q.front();
+            q.pop();
+            for (auto [meetingTime, nextPerson] : graph[person]) {
+                if (known <= meetingTime && meetingTime < secretAt[nextPerson]) {
+                    secretAt[nextPerson] = meetingTime;
+                    q.emplace(nextPerson, meetingTime);
+                }
+            }
+        }
+        std::vector<int> output;
+        for (int i = 0; i < n; ++i) {
+            if (secretAt[i] != INT_MAX) output.push_back(i);
+        }
+        return output;
+    }
+};
