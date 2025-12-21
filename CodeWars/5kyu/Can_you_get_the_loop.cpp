7b16200596950a133aeb8ba36797e2f33ed2a5af
kaluginpeter <streetworkout200412@gmail.com>
2025-12-21 22:37:52 +0300

    Added description "Can you get the loop ?"

---
+++ CodeWars/5kyu/Can_you_get_the_loop.cpp
@@ -0,0 +1,17 @@
+/*
+You are given a node that is the beginning of a linked list. This list contains a dangling piece and a loop. Your objective is to determine the length of the loop.
+
+For example in the following picture the size of the dangling piece is 3 and the loop size is 12:
+
+
+
+// Use the `getNext()` method to get the following node.
+nodePtr->getNext()
+Notes:
+
+do NOT mutate the nodes!
+in some cases there can be just a loop, with no dangling piece.
+Don't miss dmitry's article in the discussion after you pass the Kata !!
+
+AlgorithmsLinked ListsPerformance
+*/
