61e19243d91dc50a1d2a5e8efe82890b0181f917
kaluginpeter <streetworkout200412@gmail.com>
2025-12-25 21:43:38 +0300

    Added description "[Code Golf] - Factorial"

---
+++ CodeWars/6kyu/Code_Golf_Factorial.py
@@ -0,0 +1,24 @@
+# This is a code golf kata where the goal is to write the shortest program that computes the factorial of a non-negative integer n.
+#
+# where:
+# The factorial of n is defined as:
+#
+# n! = n * (n - 1)! for n > 0
+# 0! = 1
+# Constraints:
+# The program must calculate the factorial for integers n in the range of 0 to 500 (inclusive).
+#
+# The code length must not reach 30 characters.
+#
+# You are not allowed to use any form of if, else, elif, or similar conditional statements
+#
+# No built-in libraries that compute the factorial (e.g., eval or math.factorial) can be used.
+#
+#
+# For the mathematical formula of factorial and more details on its properties, you can refer to Factorial.
+#
+# The program should be concise and efficient, adhering to the above constraints while correctly calculating factorials.
+#
+# Good luck!
+#
+# MathematicsAlgorithmsRestricted
