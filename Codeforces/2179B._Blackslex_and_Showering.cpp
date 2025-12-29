39781eebbf0818d550bcd48d2daef8b859b0d47a
kaluginpeter <streetworkout200412@gmail.com>
2025-12-29 17:31:00 +0300

    Added description "2179B. Blackslex and Showering"

---
+++ Codeforces/2179B._Blackslex_and_Showering.cpp
@@ -0,0 +1,59 @@
+/*
+B. Blackslex and Showering
+time limit per test2 seconds
+memory limit per test256 megabytes
+After his IMO medalist friend showered for two hours so that "he doesn't have to shower this again this week," Blackslex will be late for class!
+
+In order to get to class, Blackslex must take the crowded elevator to many floors in a certain order. Because he is a hacker, he can skip visiting up to one floor without the other people knowing. His time taken is the sum of the absolute differences between consecutive floor numbers. Find the minimum time taken, given that he can skip up to one floor.
+
+More formally, given an array a=[a1,a2,…,an]
+ of n
+ integers, you can choose up to one index k∈{1,2,…,n}
+ to erase such that the sum
+∑i=1n−2|bi−bi+1|
+is minimized, where b=[a1,…,ak−1,ak+1,…,an]
+ is the array after erasing element ak
+. Report the minimum sum.
+
+Input
+The first line contains one integer t
+ (1≤t≤104
+) — the number of test cases.
+
+The first line of each test case contains one integer n
+ (3≤n≤2⋅105
+) — the size of the array.
+
+The second line contains n
+ integers a1,a2,…,an
+ (1≤ai≤100
+).
+
+It is guaranteed that the sum of n
+ does not exceed 2⋅105
+ over all test cases.
+
+Output
+For each test case, output a single real number — the minimum time taken.
+
+Example
+InputCopy
+3
+5
+4 15 1 7 9
+3
+2 4 8
+6
+11 13 17 19 23 29
+OutputCopy
+11
+2
+12
+Note
+For the first test case, one optimal index to remove from [4,15,1,7,9]
+ is k=2
+. The array becomes [4,1,7,9]
+ and the time taken is 11
+. For the second test case, the optimal index to remove is k=3
+.
+*/
