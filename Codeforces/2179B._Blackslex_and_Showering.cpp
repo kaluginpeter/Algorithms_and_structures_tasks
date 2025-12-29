bffe2da90a9a9e55362968ff784639bd33402455
kaluginpeter <streetworkout200412@gmail.com>
2025-12-29 17:31:33 +0300

    Added solution "2179B. Blackslex and Showering"

--- Codeforces/2179B._Blackslex_and_Showering.cpp
+++ Codeforces/2179B._Blackslex_and_Showering.cpp
@@ -57,3 +57,35 @@ For the first test case, one optimal index to remove from [4,15,1,7,9]
 . For the second test case, the optimal index to remove is k=3
 .
 */
+// Solution
+// C++ O(N) O(1) PrefixSum
+#include <iostream>
+#include <vector>
+
+
+void solution() {
+    size_t n;
+    std::cin >> n;
+    std::vector<int> nums(n);
+    int total = 0;
+    for (size_t i = 0; i < n; ++i) {
+        std::cin >> nums[i];
+        if (i) total += std::abs(nums[i - 1] - nums[i]);
+    }
+    int output = total;
+    for (size_t i = 0; i < n; ++i) {
+        if (!i) output = std::min(output, total - std::abs(nums[i] - nums[i + 1]));
+        else if (i + 1 == n) output = std::min(output, total - std::abs(nums[i] - nums[i - 1]));
+        else {
+            output = std::min(output, total - std::abs(nums[i - 1] - nums[i]) - std::abs(nums[i] - nums[i + 1]) + std::abs(nums[i - 1] - nums[i + 1]));
+        }
+    }
+    std::cout << output << '\n';
+}
+
+
+int main() {
+    size_t t;
+    std::cin >> t;
+    while (t--) solution();
+}
