773075c5229531553895decb75d996f017b51780
kaluginpeter <streetworkout200412@gmail.com>
2025-12-30 22:59:02 +0300

    Added solution "840. Magic Squares In Grid"

--- LeetCode/Medium/840._Magic_Squares_In_Grid.py
+++ LeetCode/Medium/840._Magic_Squares_In_Grid.py
@@ -75,3 +75,86 @@ class Solution:
                 col_rep += 1
             row_rep += 1
         return magic_squares
+
+
+# Python O(NM) O(1) Matrix
+class Solution:
+    def numMagicSquaresInside(self, grid: List[List[int]]) -> int:
+        output: int = 0
+        n: int = len(grid)
+        m: int = len(grid[0])
+        for row in range(n - 2):
+            for col in range(m - 2):
+                seen = [False] * 10
+                is_valid: bool = True
+                for i in range(3):
+                    for j in range(3):
+                        num = grid[row + i][col + j]
+                        if num < 1 or num > 9:
+                            is_valid = False
+                            break
+                        if seen[num]:
+                            is_valid = False
+                            break
+                        seen[num] = True
+                    if not is_valid: break
+                if not is_valid: continue
+                diagonal1 = (
+                    grid[row][col] + grid[row + 1][col + 1] + grid[row + 2][col + 2]
+                )
+                diagonal2 = (
+                    grid[row + 2][col] + grid[row + 1][col + 1] + grid[row][col + 2]
+                )
+                if diagonal1 != diagonal2: continue
+                row1 = grid[row][col] + grid[row][col + 1] + grid[row][col + 2]
+                row2 = (
+                    grid[row + 1][col] + grid[row + 1][col + 1] + grid[row + 1][col + 2]
+                )
+                row3 = (
+                    grid[row + 2][col] + grid[row + 2][col + 1] + grid[row + 2][col + 2]
+                )
+
+                if not (row1 == diagonal1 and row2 == diagonal1 and row3 == diagonal1): continue
+                col1 = grid[row][col] + grid[row + 1][col] + grid[row + 2][col]
+                col2 = (
+                    grid[row][col + 1] + grid[row + 1][col + 1] + grid[row + 2][col + 1]
+                )
+                col3 = (
+                    grid[row][col + 2] + grid[row + 1][col + 2] + grid[row + 2][col + 2]
+                )
+
+                if not (col1 == diagonal1 and col2 == diagonal1 and col3 == diagonal1): continue
+                output += 1
+        return output
+
+# C++ O(NM) O(1) Matrix
+class Solution {
+public:
+    int numMagicSquaresInside(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size(), output = 0;
+        for (int i = 0; i < n - 2; ++i) {
+            for (int j = 0; j < m - 2; ++j) {
+                std::unordered_set<int> contain;
+                for (int k = 0; k < 3; ++k) {
+                    for (int l = 0; l < 3; ++l) {
+                        if (grid[i + k][j + l] > 9 || !grid[i + k][j + l]) break;
+                        contain.insert(grid[i + k][j + l]);
+                    }
+                }
+                if (contain.size() != 9) continue;
+                contain.clear();
+                contain.insert(grid[i][j] + grid[i + 1][j] + grid[i + 2][j]);
+                contain.insert(grid[i][j + 1] + grid[i + 1][j + 1] + grid[i + 2][j + 1]);
+                contain.insert(grid[i][j + 2] + grid[i + 1][j + 2] + grid[i + 2][j + 2]);
+                contain.insert(grid[i][j] + grid[i][j + 1] + grid[i][j + 2]);
+                contain.insert(grid[i + 1][j] + grid[i + 1][j + 1] + grid[i + 1][j + 2]);
+                contain.insert(grid[i + 2][j] + grid[i + 2][j + 1] + grid[i + 2][j + 2]);
+                contain.insert(grid[i][j] + grid[i + 1][j + 1] + grid[i + 2][j + 2]);
+                contain.insert(grid[i + 2][j] + grid[i + 1][j + 1] + grid[i][j + 2]);
+                if (contain.size() != 1) continue;
+                ++output;
+            }
+        }
+        return output;
+    }
+};
