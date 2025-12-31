789cbcefcf7ed11d92a0804f27445ef8ba6dd08f
kaluginpeter <streetworkout200412@gmail.com>
2025-12-31 21:13:20 +0300

    Added solution "1970. Last Day Where You Can Still Cross"

--- LeetCode/Hard/1970._Last_Day_Where_You_Can_Still_Cross.py
+++ LeetCode/Hard/1970._Last_Day_Where_You_Can_Still_Cross.py
@@ -39,3 +39,72 @@
 # 1 <= ri <= row
 # 1 <= ci <= col
 # All the values of cells are unique.
+# Solution
+# Python O(NMlog(NM)) O(NM) BinarySearch Matrix Depth-First-Search
+moves: list[tuple[int, int]] = [
+    (1, 0), (-1, 0), (0, 1), (0, -1)
+]
+class Solution:
+    def check(self, middle: int, row: int, col: int, cells: list[list[int]]) -> bool:
+        matrix: list[list[int]] = [[True] * (col + 1) for _ in range(row + 1)]
+        for i in range(middle):
+            matrix[cells[i][0]][cells[i][1]] = False
+        cur_nodes: list[tuple[int, int]] = [(1, j) for j in range(1, col + 1) if matrix[1][j]]
+        while cur_nodes:
+            i, j = cur_nodes.pop()
+            if i == row: return True
+            for x, y in moves:
+                ni: int = i + x
+                nj: int = j + y
+                if not (0 < ni <= row) or not (0 < nj <= col) or not matrix[ni][nj]:
+                    continue
+                matrix[ni][nj] = False
+                cur_nodes.append((ni, nj))
+        return False
+    def latestDayToCross(self, row: int, col: int, cells: List[List[int]]) -> int:
+        left: int = 0
+        right: int = row * col
+        while left <= right:
+            middle: int = left + ((right - left) >> 1)
+            if self.check(middle, row, col, cells): left = middle + 1
+            else: right = middle - 1
+        return right
+
+# C++ O(NMlog(NM)) O(NM) Depth-First-Search BinarySearch Matrix
+const std::vector<std::pair<int, int>> moves = {
+    {1, 0}, {-1, 0}, {0, 1}, {0, -1}
+};
+class Solution {
+public:
+    bool check(int& middle, int& row, int& col, std::vector<std::vector<int>>& cells) {
+        std::vector<std::vector<bool>> matrix(row + 1, std::vector<bool>(col + 1, true));
+        for (int i = 0; i < middle; ++i) {
+            matrix[cells[i][0]][cells[i][1]] = false;
+        }
+        std::vector<std::pair<int, int>> curNodes;
+        for (int j = 1; j <= col; ++j) {
+            if (matrix[1][j]) curNodes.push_back({1, j});
+        }
+        while (!curNodes.empty()) {
+            auto [i, j] = curNodes.back();
+            curNodes.pop_back();
+            if (i == row) return true;
+            for (auto& [x, y] : moves) {
+                int nI = i + x, nJ = j + y;
+                if (!nI || nI > row || !nJ || nJ > col || !matrix[nI][nJ]) continue;
+                matrix[nI][nJ] = false;
+                curNodes.push_back({nI, nJ});
+            }
+        }
+        return false;
+    }
+    int latestDayToCross(int row, int col, vector<vector<int>>& cells) {
+        int left = 0, right = row * col;
+        while (left <= right) {
+            int middle = left + ((right - left) >> 1);
+            if (check(middle, row, col, cells)) left = middle + 1;
+            else right = middle - 1;
+        }
+        return right;
+    }
+};
