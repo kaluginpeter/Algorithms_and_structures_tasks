c343b48931fc5f59132f40d170b726755be5ac34
kaluginpeter <streetworkout200412@gmail.com>
2026-01-03 22:36:38 +0300

    Added description "[Code Golf] XOR string reduction"

---
+++ CodeWars/Code_Golf_XOR_string_reduction.py
@@ -0,0 +1,21 @@
+# Description :
+# Given a string consisting entirely of binary digits (0 , 1) seperated using spaces. Find the XOR of all digits and return the answer.
+#
+# Examples :
+# Given
+#
+# "1 0 0 1 0" --> 0
+# "1 0 1 1 1 0 0 1 0 0 0 0" --> 1
+# How :
+# 1 0 0 1 0
+# Solving :
+#
+# (1 XOR 0) (0 XOR 1) 0
+# 1 1 0
+# (1 XOR 1) 0
+# 0 0
+# 0 XOR 0
+# 0 ---> Answer
+# This is code-golf so shortest code is winner. The limit of solution is set to 40 chars (exclusive).
+#
+# StringsMathematicsRestricted
