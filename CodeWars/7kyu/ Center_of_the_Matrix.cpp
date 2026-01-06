183cacc0a8030f4b07ec1a9fa26f50bad283393c
kaluginpeter <streetworkout200412@gmail.com>
2026-01-06 23:44:38 +0300

    Added description "Center of the Matrix"

---
+++ CodeWars/7kyu/ Center_of_the_Matrix.cpp
@@ -0,0 +1,9 @@
+/*
+Create a method that returns the center of a 2-dim array.
+
+It should return std::nullopt if there is no single center element.
+
+You can assume all inputs will be rectangular matricies in array form. ex: [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+FundamentalsMatrix
+*/
