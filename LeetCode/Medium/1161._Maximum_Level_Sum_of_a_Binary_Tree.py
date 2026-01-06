8eb6b7662914a8e86ad6eba6a975635c93e50a32
kaluginpeter <streetworkout200412@gmail.com>
2026-01-06 23:25:52 +0300

    Added solution "1161. Maximum Level Sum of a Binary Tree"

--- LeetCode/Medium/1161._Maximum_Level_Sum_of_a_Binary_Tree.py
+++ LeetCode/Medium/1161._Maximum_Level_Sum_of_a_Binary_Tree.py
@@ -25,3 +25,67 @@
 # The number of nodes in the tree is in the range [1, 104].
 # -105 <= Node.val <= 105
 #
+# Solution
+# Python O(N) O(N) Breadth-First-Search
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxLevelSum(self, root: Optional[TreeNode]) -> int:
+        cur_nodes: list[TreeNode] = [root]
+        next_nodes: list[TreeNode] = []
+        output: int = 0
+        bound: int = float('-inf')
+        depth: int = 0
+        while cur_nodes:
+            depth += 1
+            cur_level: int = 0
+            for node in cur_nodes:
+                cur_level += node.val
+                if node.left: next_nodes.append(node.left)
+                if node.right: next_nodes.append(node.right)
+            if cur_level > bound:
+                bound = cur_level
+                output = depth
+            cur_nodes = next_nodes.copy()
+            next_nodes.clear()
+        return output
+
+# C++ O(N) O(N) Breadth-First-Search
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxLevelSum(TreeNode* root) {
+        std::vector<TreeNode*> curNodes = {root}, nextNodes;
+        int output = 0, bound = INT32_MIN, depth = 0;
+        while (!curNodes.empty()) {
+            ++depth;
+            int curLevel = 0;
+            for (TreeNode* node : curNodes) {
+                curLevel += node->val;
+                if (node->left) nextNodes.push_back(node->left);
+                if (node->right) nextNodes.push_back(node->right);
+            }
+            if (curLevel > bound) {
+                bound = curLevel;
+                output = depth;
+            }
+            curNodes = nextNodes;
+            nextNodes.clear();
+        }
+        return output;
+    }
+};
