7f648536070b0159c2eba73c9b1578b4bc644224
kaluginpeter <streetworkout200412@gmail.com>
2026-01-08 23:32:23 +0300

    Added description "Lucky Sevens"

---
+++ CodeWars/6kyu/Lucky_Sevens.py
@@ -0,0 +1,24 @@
+# A "Lucky Seven" is the number seven surrounded by numbers that add up to form a perfect cube. The surrounding numbers will be described as the numbers directly above, below, and next to (not diagonally) the number 7. You will be given a 2D array containing at least 1 lucky seven. Your function should return the number of lucky sevens in the array.
+#
+# Here are some "Lucky Sevens" for example:
+#
+# [ [ 'x', 513, 'x', 'x', 'x' ],
+#   [ 82,   7,  32,  'x', 'x' ],
+#   [ 'x', 102, 'x', 'x', 'x' ],
+#   [ 'x', 'x', 'x', 'x', 'x' ],
+#   [ 'x', 'x', 'x', 'x', 'x' ] ]
+#
+# [ [ 'x', 'x', 'x', 'x', 'x' ],
+#   [ 'x', 'x', 'x', 'x', 'x' ],
+#   [ 'x', 'x', 'x', 'x', 'x' ],
+#   [ 'x', 'x', 'x', 'x',  9  ],
+#   [ 'x', 'x', 'x', 55,   7  ] ]
+#
+# [ [ 'x', 'x', 'x', 'x', 'x' ],
+#   [ 'x',  1,  'x', 'x', 'x' ],
+#   [  0,   7,   0,  'x', 'x' ],
+#   [ 'x',  0,  'x', 'x', 'x' ],
+#   [ 'x', 'x', 'x', 'x', 'x' ] ]
+# Good luck!
+#
+# Fundamentals
