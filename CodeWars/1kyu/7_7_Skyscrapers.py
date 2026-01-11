6949b7223775d4f9ea7b6a4a7336aea98caf17f7
kaluginpeter <streetworkout200412@gmail.com>
2026-01-11 07:43:58 +0300

    Added solution " 7×7 Skyscrapers" for Python

--- CodeWars/1kyu/7_7_Skyscrapers.py
+++ CodeWars/1kyu/7_7_Skyscrapers.py
@@ -23,3 +23,156 @@
 # Conceptis Puzzles have heaps of these puzzles, from 4×4 up to 7×7 and unsolvable within CodeWars time constraints. Old puzzles from there were used for the tests. They also have lots of other logic, numbers and mathematical puzzles, and their puzzle user interface is generally nice, very nice.
 #
 # AlgorithmsGamesPerformance
+# Solution
+def solve_puzzle(clues):
+    GRID_SIZE = 7
+    DIR_COUNT = 4
+    ALL_BITS = (1 << GRID_SIZE) - 1
+    lineStart = [
+        0,1,2,3,4,5,6,
+        6,13,20,27,34,41,48,
+        48,47,46,45,44,43,42,
+        42,35,28,21,14,7,0
+    ]
+    lineStep = [
+        7,7,7,7,7,7,7,
+        -1,-1,-1,-1,-1,-1,-1,
+        -7,-7,-7,-7,-7,-7,-7,
+        1,1,1,1,1,1,1
+    ]
+    cellMask = [ALL_BITS] * (GRID_SIZE * GRID_SIZE)
+    finalGrid = [[0] * GRID_SIZE for _ in range(GRID_SIZE)]
+    locked = [True] * (GRID_SIZE * GRID_SIZE)
+    cluesData = clues[:]
+    def assignCell(index, value):
+        forbid = ALL_BITS ^ (1 << value)
+        rowBase = index - index % GRID_SIZE
+        colBase = index % GRID_SIZE
+        for i in range(GRID_SIZE):
+            cellMask[rowBase + i] &= forbid
+            cellMask[colBase + i * GRID_SIZE] &= forbid
+        cellMask[index] = 1 << value
+
+    def propagateUnique():
+        changes = 0
+        for i in range(DIR_COUNT // 2 * GRID_SIZE):
+            valueCells = {}
+            p = lineStart[i]
+            for _ in range(GRID_SIZE):
+                for v in range(GRID_SIZE):
+                    if cellMask[p] & (1 << v):
+                        valueCells.setdefault(v, []).append(p)
+                p += lineStep[i]
+            for v, cells in valueCells.items():
+                if len(cells) == 1:
+                    pos = cells[0]
+                    if cellMask[pos] != (1 << v):
+                        assignCell(pos, v)
+                        changes += 1
+        return changes
+
+    def pruneTwoClue():
+        cnt = 0
+        for i in range(DIR_COUNT * GRID_SIZE):
+            if cluesData[i] != 2:
+                continue
+            allowed = ALL_BITS
+            for v in range(GRID_SIZE - 1, -1, -1):
+                allowed ^= 1 << v
+                if cellMask[lineStart[i]] & (1 << v):
+                    break
+            p = lineStart[i] + lineStep[i]
+            for _ in range(1, GRID_SIZE):
+                if cellMask[p] & (1 << (GRID_SIZE - 1)):
+                    break
+                if (cellMask[p] | allowed) != allowed:
+                    cellMask[p] &= allowed
+                    cnt += 1
+                p += lineStep[i]
+        return cnt
+
+    def countBits(x):
+        return x.bit_count()
+
+    def checkConsistency():
+        for i in range(DIR_COUNT * GRID_SIZE):
+            if cluesData[i] == 0:
+                continue
+            p = lineStart[i]
+            complete = True
+            for _ in range(GRID_SIZE):
+                if countBits(cellMask[p]) != 1:
+                    complete = False
+                    break
+                p += lineStep[i]
+            if complete:
+                tallest = 0
+                visible = 0
+                p = lineStart[i]
+                for _ in range(GRID_SIZE):
+                    if cellMask[p] > tallest:
+                        tallest = cellMask[p]
+                        visible += 1
+                    p += lineStep[i]
+                if visible != cluesData[i]:
+                    return False
+        return True
+
+    def extractSolution():
+        for i in range(GRID_SIZE * GRID_SIZE):
+            r, c = divmod(i, GRID_SIZE)
+            for v in range(GRID_SIZE):
+                if cellMask[i] == (1 << v):
+                    finalGrid[r][c] = v + 1
+
+    def search():
+        chosen = -1
+        best = 10**9
+        for i in range(GRID_SIZE * GRID_SIZE):
+            options = countBits(cellMask[i])
+            if options < best and not locked[i]:
+                best = options
+                chosen = i
+        if chosen == -1:
+            if checkConsistency():
+                extractSolution()
+                return True
+            return False
+
+        backup = cellMask[:]
+        for v in range(GRID_SIZE - 1, -1, -1):
+            if not (cellMask[chosen] & (1 << v)):
+                continue
+            locked[chosen] = True
+            assignCell(chosen, v)
+            if checkConsistency() and search():
+                return True
+            locked[chosen] = False
+            cellMask[:] = backup[:]
+        return False
+
+    def resetState():
+        for i in range(GRID_SIZE * GRID_SIZE):
+            cellMask[i] = ALL_BITS
+            locked[i] = True
+
+    def preprocess():
+        for i in range(DIR_COUNT * GRID_SIZE):
+            if cluesData[i] == 0:
+                continue
+            p = lineStart[i]
+            for k in range(GRID_SIZE):
+                mask = ALL_BITS
+                for v in range(GRID_SIZE + k - cluesData[i] + 1, GRID_SIZE):
+                    mask ^= 1 << v
+                cellMask[p] &= mask
+                p += lineStep[i]
+        while propagateUnique() > 0:
+            pass
+        pruneTwoClue()
+    resetState()
+    preprocess()
+    for i in range(GRID_SIZE * GRID_SIZE):
+        if countBits(cellMask[i]) > 1: locked[i] = False
+    search()
+    return [row[:] for row in finalGrid]
