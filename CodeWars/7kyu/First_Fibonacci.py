2d489d8ea0c12d17d4e0d7deaa48c30cc2f8d8c6
kaluginpeter <streetworkout200412@gmail.com>
2026-01-15 23:46:57 +0300

    Added solution "First Fibonacci"

--- CodeWars/7kyu/First_Fibonacci.py
+++ CodeWars/7kyu/First_Fibonacci.py
@@ -14,3 +14,8 @@
 # Good luck!
 #
 # Mathematics
+# Solution
+def solution(first, second):
+    while second - first >= 0 and second - first <= first:
+        first, second = second - first, first
+    return first, second
