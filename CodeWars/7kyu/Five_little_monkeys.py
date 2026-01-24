d1e373d0fb946d3ca8b590b98dc838a379b948c9
kaluginpeter <streetworkout200412@gmail.com>
2026-01-24 21:34:19 +0300

    Added description "Five little monkeys"

---
+++ CodeWars/7kyu/Five_little_monkeys.py
@@ -0,0 +1,39 @@
+# Return the lyrics of the nursery rhyme "Five little monkeys" (see below).
+#
+# But even monkeys can copy-paste, so you have to be smarter and make it short, because your code can be maximum 450 bytes long! (The original text is 800 bytes)
+#
+# Hint: use loops and variables for repeated text, or any other way to reduce your code size.
+#
+# Happy coding!
+#
+# Five little monkeys jumping on the bed,
+# One fell off and bumped his head.
+# Mother called the doctor and the doctor said:
+# No more monkeys jumping on the bed!
+#
+# Four little monkeys jumping on the bed,
+# One fell off and bumped his head.
+# Mother called the doctor and the doctor said:
+# No more monkeys jumping on the bed!
+#
+# Three little monkeys jumping on the bed,
+# One fell off and bumped his head.
+# Mother called the doctor and the doctor said:
+# No more monkeys jumping on the bed!
+#
+# Two little monkeys jumping on the bed,
+# One fell off and bumped his head.
+# Mother called the doctor and the doctor said:
+# No more monkeys jumping on the bed!
+#
+# One little monkey jumping on the bed,
+# He fell off and bumped his head.
+# Mother called the doctor and the doctor said:
+# Put those monkeys right to bed!
+# My other katas
+# If you enjoyed this kata then please try my other katas! :-)
+#
+# Translations are welcome!
+# (except for Ruby, which is coming shortly)
+#
+# StringsRestricted
