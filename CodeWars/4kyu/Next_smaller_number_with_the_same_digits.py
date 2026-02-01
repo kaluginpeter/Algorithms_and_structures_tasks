700a994218d31f3407e35b3778d26036b3764708
kaluginpeter <streetworkout200412@gmail.com>
2026-02-01 22:31:51 +0300

    Added solution "Next smaller number with the same digits"

--- CodeWars/4kyu/Next_smaller_number_with_the_same_digits.py
+++ CodeWars/4kyu/Next_smaller_number_with_the_same_digits.py
@@ -15,3 +15,16 @@
 # The function you write for this challenge is the inverse of this kata: "Next bigger number with the same digits."
 #
 # StringsMathematicsAlgorithms
+# Solution
+def next_smaller(n):
+    digits = list(str(n))
+    l = len(digits)
+    for i in range(l - 2, -1, -1):
+        if digits[i] > digits[i + 1]: break
+    else: return -1
+    for j in range(l - 1, i, -1):
+        if digits[j] < digits[i]: break
+    digits[i], digits[j] = digits[j], digits[i]
+    digits[i + 1:] = sorted(digits[i + 1:], reverse=True)
+    if digits[0] == '0': return -1
+    return int("".join(digits))
