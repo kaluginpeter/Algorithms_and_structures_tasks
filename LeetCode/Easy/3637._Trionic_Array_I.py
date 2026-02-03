e7774cd037f0614549b52e81e244ace1a49984e2
kaluginpeter <streetworkout200412@gmail.com>
2026-02-03 23:00:22 +0300

    Added solution "3637. Trionic Array I"

--- LeetCode/Easy/3637._Trionic_Array_I.py
+++ LeetCode/Easy/3637._Trionic_Array_I.py
@@ -38,3 +38,46 @@
 #
 # 3 <= n <= 100
 # -1000 <= nums[i] <= 1000
+# Solution
+# Python O(N) O(1) Simulation Greedy
+class Solution:
+    def isTrionic(self, nums: List[int]) -> bool:
+        is_incr: bool = True
+        is_decr: bool = False
+        was_decr: bool = False
+        for i in range(1, len(nums)):
+            if nums[i] > nums[i - 1]:
+                if is_decr:
+                    if was_decr: return False
+                    was_decr = True
+                    is_decr = False
+                    is_incr = True
+            elif nums[i] < nums[i - 1]:
+                if was_decr or i == 1: return False
+                is_decr = True
+                is_incr = False
+            else: return False
+        return was_decr
+
+# C++ O(N) O(1) Greedy Simulation
+class Solution {
+public:
+    bool isTrionic(vector<int>& nums) {
+        bool isIncr = true, isDecr = false, wasDecr = false;
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[i] > nums[i - 1]) {
+                if (isDecr) {
+                    if (wasDecr) return false;
+                    wasDecr = true;
+                    isDecr = false;
+                    isIncr = true;
+                }
+            } else if (nums[i] < nums[i - 1]) {
+                if (wasDecr || i == 1) return false;
+                isDecr = true;
+                isIncr = false;
+            } else return false;
+        }
+        return wasDecr;
+    }
+};
