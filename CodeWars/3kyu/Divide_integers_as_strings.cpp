c93f98d687336cdcd6650af842fc0848dfe0842d
kaluginpeter <streetworkout200412@gmail.com>
2026-02-03 23:11:54 +0300

    Added description "Divide integers as strings"

---
+++ CodeWars/3kyu/Divide_integers_as_strings.cpp
@@ -0,0 +1,8 @@
+/*
+Given positive integers a and b as strings, evaluate a / b and return the quotient and the remainder as strings in the form [quotient, remainder].
+
+a and b can be very large (at the order of 10^150 to 10^200)
+As usual, your result should not have leading 0s
+require is disabled in JavaScript. Do it yourself ;-)
+StringsFundamentalsBig IntegersAlgorithms
+*/
