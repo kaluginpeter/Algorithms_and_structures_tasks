f25e55cefe3751d08bba4c4321af60eaed194437
kaluginpeter <streetworkout200412@gmail.com>
2026-02-13 23:04:16 +0300

    Added description "The longest bracket substring in the string"

---
+++ CodeWars/5kyu/The_longest_bracket_substring_in_the_string.py
@@ -0,0 +1,25 @@
+# When no more interesting kata can be resolved, I just choose to create the new kata, to solve their own, to enjoy the process --myjinxin2015 said
+#
+# Description:
+# Given a string str consisting of some number of "(" and ")" characters, your task is to find the longest substring in str such that all "(" in the substring are closed by a matching ")". The result is the length of that substring.
+#
+# For example:
+#
+# "()()(" => 4
+# Because "()()" is the longest substring, which has a length of 4
+# Note:
+# All inputs are valid.
+# If no such substring found, return 0.
+# Please pay attention to the performance of code. ;-)
+# In the performance test(100000 brackets str x 100 testcases), the time consuming of each test case should be within 35ms. This means, your code should run as fast as a rocket ;-)
+# Some Examples
+#  "" => 0
+# "()" => 2
+# "()(" => 2
+# "()()" => 4
+# "()()(" => 4
+# "(()())" => 6
+# "(()(())" => 6
+# "())(()))" => 4
+# "))((" => 0
+# PuzzlesPerformanceAlgorithms
