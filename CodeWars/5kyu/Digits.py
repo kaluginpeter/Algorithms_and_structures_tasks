f11ef10e321a9127c429cf1eff6194947f13be62
kaluginpeter <streetworkout200412@gmail.com>
2026-02-14 22:08:43 +0300

    Added description "Digits"

---
+++ CodeWars/5kyu/Digits.py
@@ -0,0 +1,15 @@
+# The code consists of four unique digits (from 0 to 9).
+# Tests will call your solution; you should answer with an array of four digits.
+# Your input is number of matches (the same digit in the same place) with your previous answer. For the first call input value is -1 (i.e. each new test starts with input -1)
+# You have to find the code in 16 calls or less. You are the best. Do it.
+#
+# For example
+# The code is [1, 2, 3, 4]
+# 1st call return [1, 3, 4, 5] will give 1 match in next input
+# 2nd call return [1, 2, 3, 0] will give 3 matches in next input
+# 3rd call return [1, 2, 3, 4] will not give 4 matches in next input, because you're the champion!
+#
+# P.S.
+# If the task seems too difficult, try this game with almost the same rules first.
+#
+# Algorithms
