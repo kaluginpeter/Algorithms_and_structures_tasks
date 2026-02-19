19d8e583862a2c847f4d1401bd5e5cf7be924be5
kaluginpeter <streetworkout200412@gmail.com>
2026-02-19 23:05:37 +0300

    Added solution "696. Count Binary Substrings"

--- LeetCode/Easy/696._Count_Binary_Substrings.py
+++ LeetCode/Easy/696._Count_Binary_Substrings.py
@@ -22,3 +22,38 @@
 #
 # 1 <= s.length <= 105
 # s[i] is either '0' or '1'.
+# Solution
+# Python O(N) O(1) SlidingWindow
+class Solution:
+    def countBinarySubstrings(self, s: str) -> int:
+        n: int = len(s)
+        left: int = 0
+        right: int = 0
+        zeros: int = 0
+        ones: int = 0
+        output: int = 0
+        while right < n:
+            while right < n and s[left] == s[right]: right += 1
+            if s[left] == '1': ones = right - left
+            else: zeros = right - left
+            output += min(zeros, ones)
+            left = right
+        return output
+
+# C++ O(N) O(1) SlidingWindow
+class Solution {
+public:
+    int countBinarySubstrings(string s) {
+        int n = s.size(), output = 0;
+        int left = 0, right = 0;
+        int ones = 0, zeros = 0;
+        while (right < n) {
+            while (right < n && s[left] == s[right]) ++right;
+            if (s[left] == '1') ones = right - left;
+            else zeros = right - left;
+            output += std::min(zeros, ones);
+            left = right;
+        }
+        return output;
+    }
+};
