603fd11da1b8ca07750a9caafebda5565e8f9ba7
kaluginpeter <streetworkout200412@gmail.com>
2026-02-23 21:56:07 +0300

    Added solution "1461. Check If a String Contains All Binary Codes of Size K"

--- LeetCode/Medium/1461._Check_If_a_String_Contains_All_Binary_Codes_of_Size_K.py
+++ LeetCode/Medium/1461._Check_If_a_String_Contains_All_Binary_Codes_of_Size_K.py
@@ -24,3 +24,34 @@
 # 1 <= s.length <= 5 * 105
 # s[i] is either '0' or '1'.
 # 1 <= k <= 20
+# Solution
+# Python O(2**K + N) O(N - K + 1) SlidingWindow BitManipulation
+class Solution:
+    def hasAllCodes(self, s: str, k: int) -> bool:
+        seen: set[int] = set()
+        tmp: int = 0
+        for i in range(len(s)):
+            tmp <<= 1
+            if s[i] == '1': tmp += 1
+            if i >= k: tmp &= (1 << k) - 1
+            if i + 1 >= k: seen.add(tmp)
+        return all(mask in seen for mask in range(1 << k))
+
+# C++ O(2**K + N) O(N - K + 1) BitManipulation SlidingWindow
+class Solution {
+public:
+    bool hasAllCodes(string s, int k) {
+        std::unordered_set<int> seen;
+        int tmp = 0;
+        for (int i = 0; i < s.size(); ++i) {
+            tmp <<= 1;
+            if (s[i] == '1') ++tmp;
+            if (i >= k) tmp &= (1 << k) - 1;
+            if (i + 1 >= k) seen.insert(tmp);
+        }
+        for (int mask = 0; mask < (1 << k); ++mask) {
+            if (!seen.count(mask)) return false;
+        }
+        return true;
+    }
+};
