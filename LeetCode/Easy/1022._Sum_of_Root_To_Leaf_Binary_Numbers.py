37a548a5b0d905ac5cd1bb3780cd657bf499effe
kaluginpeter <streetworkout200412@gmail.com>
2026-02-24 23:12:59 +0300

    Added solution "1022. Sum of Root To Leaf Binary Numbers"

--- LeetCode/Easy/1022._Sum_of_Root_To_Leaf_Binary_Numbers.py
+++ LeetCode/Easy/1022._Sum_of_Root_To_Leaf_Binary_Numbers.py
@@ -24,3 +24,57 @@
 # The number of nodes in the tree is in the range [1, 1000].
 # Node.val is 0 or 1.
 #
+# Solution
+# Python O(N) O(h) DepthFirstSearch
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    output: int = 0
+    def dfs(self, root: TreeNode, acc: int) -> None:
+        acc <<= 1
+        if root.val: acc += 1
+        if not root.left and not root.right:
+            self.output += acc
+            return
+        if root.left: self.dfs(root.left, acc)
+        if root.right: self.dfs(root.right, acc)
+
+    def sumRootToLeaf(self, root: Optional[TreeNode]) -> int:
+        self.output = 0
+        self.dfs(root, 0)
+        return self.output
+
+# C++ O(N) O(h) DepthFirstSearch
+/**
+ * Definition for a binary tree nodes.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void dfs(TreeNode* root, int& output, int acc) {
+        acc <<= 1;
+        if (root->val) ++acc;
+        if (!root->left && !root->right) {
+            output += acc;
+            return;
+        }
+        if (root->left) dfs(root->left, output, acc);
+        if (root->right) dfs(root->right, output, acc);
+    }
+    int sumRootToLeaf(TreeNode* root) {
+        int output = 0;
+        dfs(root, output, 0);
+        return output;
+    }
+};
