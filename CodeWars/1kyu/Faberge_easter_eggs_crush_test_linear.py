62c9d3b5a7914018d8eb549f1aedab50ce1d3823
kaluginpeter <streetworkout200412@gmail.com>
2026-02-24 23:17:20 +0300

    Added description "Faberge easter eggs crush test [linear]"

---
+++ CodeWars/1kyu/Faberge_easter_eggs_crush_test_linear.py
@@ -0,0 +1,14 @@
+# This is the SUPER performance version of This kata.
+#
+# You task is exactly the same as that kata. But this time, you should output result % 998244353, or otherwise the result would be too large.
+#
+# Data range
+# sometimes
+#   n <= 80000
+#   m <= 100000
+# while sometimes
+#   n <= 3000
+#   m <= 2^200
+# There are 150 random tests. You will need more than just a naive linear algorithm for this task :D
+#
+# PerformanceAlgorithmsMathematics
