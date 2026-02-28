029f75c2f4548b2983ce38db7a1fcb7bc86bdf04
kaluginpeter <streetworkout200412@gmail.com>
2026-02-28 22:51:42 +0300

    Added solution "Number of Proper Fractions with Denominator d"

--- CodeWars/4kyu/Number_of_Proper_Fractions_with_Denominator_d.py
+++ CodeWars/4kyu/Number_of_Proper_Fractions_with_Denominator_d.py
@@ -18,3 +18,29 @@
 # Edit: to be extra precise, the term should be "reduced" fractions, thanks to girianshiido for pointing this out and sorry for the use of an improper word :)
 #
 # FundamentalsMathematics
+# Solution
+def proper_fractions(n):
+    if n == 1: return 0
+    temp = 1
+    m = n
+    l = int(n ** 0.5) + 1
+    for i in range(1, l):
+        if is_prime(i):
+            if m % i == 0:
+                temp *= i - 1
+                m /= i
+            while m % i == 0:
+                temp *= i
+                m /= i
+    if m > 1:
+        temp *= m - 1
+    return temp
+
+
+def is_prime(n):
+    if n == 0 or n == 1: return False
+    i = 2
+    while (i <= n ** 0.5):
+        if n % i == 0: return False
+        i += 1
+    return True
