b113ed304c612aebdbf16bf840f45c6cdaef7479
kaluginpeter <streetworkout200412@gmail.com>
2026-03-01 21:39:28 +0300

    Added description "Regular Expression - Check if divisible by 0b111 (7)"

---
+++ CodeWars/2kyu/Regular_Expression_Check_if_divisible_by_0b111_7.py
@@ -0,0 +1,8 @@
+# Create a regular expression capable of evaluating binary strings (which consist of only 1's and 0's) and determining whether the given string represents a number divisible by 7.
+#
+# Note:
+#
+# Empty strings should be rejected.
+# Your solution should reject strings with any character other than 0 and 1.
+# No leading 0's will be tested unless the string exactly denotes 0.
+# PuzzlesRegular ExpressionsStringsAlgorithms
