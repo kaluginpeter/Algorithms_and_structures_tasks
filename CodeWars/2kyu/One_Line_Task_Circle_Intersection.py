3aa9f49dde20e57228695473ddd833fa000373a1
kaluginpeter <streetworkout200412@gmail.com>
2026-03-02 20:46:24 +0300

    Added description "One Line Task: Circle Intersection"

---
+++ CodeWars/2kyu/One_Line_Task_Circle_Intersection.py
@@ -0,0 +1,16 @@
+# Task
+# Given two congruent circles a and b of radius r, return the area of their intersection rounded down to the nearest integer.
+#
+# Code Limit
+# Javascript: Less than 94 characters.
+#
+# Python: Less than 128 characters.
+#
+# To remain consistent across version, your code should also not include the 'assignment operator' :=.
+#
+# Example
+# For c1 = [0, 0], c2 = [7, 0] and r = 5,
+#
+# the output should be 14.
+#
+# PuzzlesRestricted
