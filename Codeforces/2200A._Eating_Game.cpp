3c6fe58b1b60183a920f16c70b375b0df77aa719
kaluginpeter <streetworkout200412@gmail.com>
2026-03-06 21:47:43 +0300

    Added description "2200A. Eating Game"

---
+++ Codeforces/2200A._Eating_Game.cpp
@@ -0,0 +1,56 @@
+/*
+A. Eating Game
+time limit per test1 second
+memory limit per test256 megabytes
+There are n
+ players playing a game at a circular table. The i
+-th player has ai
+ dishes to eat. They take turns eating the food, and any player can go first.
+
+During their turn, if player i
+ has any dishes remaining, they must eat exactly one dish. Then, player (imodn)+1
+ starts their turn. This continues until all dishes are finished.
+
+The player who eats the last dish is considered the winner. Determine the number of players that can possibly be winners.
+
+Input
+The first line contains an integer t
+ (1≤t≤5000
+), the number of test cases.
+
+The first line of each test case contains an integer n
+ (1≤n≤10
+).
+
+The second line of each test case contains n
+ integers, the elements of a
+ (1≤ai≤10
+).
+
+Output
+For each test case, output a line with the answer.
+
+Example
+InputCopy
+3
+1
+10
+2
+6 7
+4
+1 4 3 4
+OutputCopy
+1
+1
+2
+Note
+In the first test case, player 1
+ wins for every starting player.
+
+In the second test case, player 2
+ wins for every starting player.
+
+In the third test case, players 2
+ and 4
+ can win.
+*/
