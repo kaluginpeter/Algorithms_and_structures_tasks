3be7a842048133f9416f429100da01dfe71284ba
kaluginpeter <streetworkout200412@gmail.com>
2026-03-08 19:29:43 +0300

    Added solution "Puzzle Fighter"

--- CodeWars/1kyu/Puzzle_Fighter.py
+++ CodeWars/1kyu/Puzzle_Fighter.py
@@ -136,3 +136,412 @@
 # If you enjoyed this kata, be sure to check out my other katas.
 #
 # GamesLogicGame SolversAlgorithms
+# Solution
+from collections import defaultdict
+
+ROTATIONS   = [[1,0], [0,-1], [-1,0], [0,1]]
+DIRS_POWA   = [[0,-1], [0,1], [1,0], [-1,0]]
+DIRS_SINGLE = [[0,1], [1,0]]
+
+C,X,Y,H,W,S = range(6)
+
+LX,LY = 12,6
+
+START = (0,3,0)
+
+RL = "RL"
+AB = "AB"
+L  = "L"
+A  = "A"
+
+CRASH_GEMS = "rgby"
+
+
+def puzzle_fighter(arrMoves):
+
+    board = [[None]*LY for _ in range(LX)]
+    gemsByColors = defaultdict(set)
+
+    def getStartPoses(moves, c1, c2):
+        x,y,iR = START
+
+        for m in moves:
+
+            if m in RL:
+                y += -1 if m==L else 1
+
+            if m in AB:
+                iR += -1 if m==A else 1
+
+            iR %= 4
+
+            dy = ROTATIONS[iR][1]
+
+            if y>=LY or y+dy>=LY:
+                y = min(LY-1, LY-1-dy)
+
+            elif y<0 or y+dy<0:
+                y = max(0, -dy)
+
+        dx,dy = ROTATIONS[iR]
+
+        if dx < 0:
+            x += 1
+
+        return [[x,y,c1],[x+dx,y+dy,c2]]
+
+    def moveGem(x0,y,x):
+
+        g = board[x0][y]
+
+        c,_,_,h,w,s = g
+
+        g[X],g[Y] = x,y
+
+        gemsByColors[c].remove((x0,y))
+        gemsByColors[c].add((x,y))
+
+        for dx in reversed(range(h)):
+            for yy in range(y,y+w):
+
+                board[x0+dx][yy] = None
+                board[x+dx][yy] = g
+
+    def couldFallFromAbove(x,y,w):
+
+        out = set()
+
+        x -= 1
+
+        if x < 0:
+            return out
+
+        for yy in range(y,y+w):
+
+            g = board[x][yy]
+
+            if g:
+                out.add((g[X],g[Y]))
+
+        return out
+
+    def drop(posToDrop):
+
+        movedGems = set()
+
+        toDrop = sorted(posToDrop)
+
+        while toDrop:
+
+            x,y = toDrop.pop()
+
+            if board[x][y] is None:
+                continue
+
+            c,x0,y0,h,w,_ = board[x][y]
+
+            while x+h < LX and all(board[x+h][yy] is None for yy in range(y,y+w)):
+                x += 1
+
+            if x == x0 and x > 1:
+                continue
+
+            movedGems.add((x,y))
+
+            moveGem(x0,y,x)
+
+            for nx,ny in couldFallFromAbove(x0,y,w):
+
+                i=0
+                while i<len(toDrop) and toDrop[i][0]<nx:
+                    i+=1
+
+                toDrop.insert(i,(nx,ny))
+
+        return movedGems
+
+    def flood(color,x,y):
+
+        what = color + color.swapcase()
+
+        bag = {(x,y)}
+        seen = {(x,y)}
+
+        while bag:
+
+            new=set()
+
+            for a,b in bag:
+
+                for da,db in ROTATIONS:
+
+                    na,nb = a+da, b+db
+
+                    if not (0<=na<LX and 0<=nb<LY):
+                        continue
+
+                    g = board[na][nb]
+
+                    if g and g[C] in what and (na,nb) not in seen:
+                        new.add((na,nb))
+
+            seen |= new
+            bag = new
+
+        roots=set()
+
+        for a,b in seen:
+            roots.add((board[a][b][X], board[a][b][Y]))
+
+        return roots if len(roots)>1 else set()
+
+    def explode(movedGems):
+
+        colorsToExplode = ["0"]
+
+        for x,y in gemsByColors["0"]:
+            if x < LX-1:
+                colorsToExplode.append(board[x+1][y][C])
+                colorsToExplode.append(board[x+1][y][C].swapcase())
+
+        colorsToExplode=set(colorsToExplode)
+
+        posToExplode=set()
+
+        for c in colorsToExplode:
+            posToExplode |= gemsByColors[c]
+
+        for c in CRASH_GEMS:
+
+            for x,y in gemsByColors[c]:
+                posToExplode |= flood(c,x,y)
+
+        toDropNext=set()
+
+        for x,y in posToExplode:
+
+            c,x,y,h,w,_ = board[x][y]
+
+            for pos in couldFallFromAbove(x,y,w):
+                toDropNext.add(pos)
+
+            gemsByColors[c].discard((x,y))
+
+            for xx in range(x,x+h):
+                for yy in range(y,y+w):
+                    board[xx][yy]=None
+
+        movedGems -= posToExplode
+        toDropNext -= posToExplode
+
+        return movedGems,toDropNext
+
+    def makePowa(movedGems,toDropNext):
+
+        gemAreasByCols=defaultdict(set)
+
+        for x,y in movedGems:
+
+            c,_,_,h,w,_=board[x][y]
+
+            gemAreasByCols[c]|=flood(c,x,y)
+
+        q=list(gemAreasByCols.items())
+
+        while q:
+
+            c,areas=q.pop()
+
+            fresh,removed=iGotThePowa(areas,toDropNext)
+
+            gemsByColors[c]-=removed
+
+            if fresh:
+                q.insert(0,(c,fresh))
+
+        return toDropNext
+
+    def iGotThePowa(areas,toDropNext):
+
+        ones=[]
+        powa=[]
+
+        for x,y in areas:
+
+            g=board[x][y]
+
+            if g[S]==1:
+                ones.append(g)
+            else:
+                powa.append(g)
+
+        fresh=set()
+        removed=set()
+
+        for grp,isSingle in [(ones,True),(powa,False)]:
+
+            grp.sort()
+
+            for g in grp:
+
+                c,x,y,h,w,s=g
+
+                if id(g)!=id(board[x][y]) or x==LX-1 or y==LY-1:
+                    continue
+
+                expanded=expand(DIRS_SINGLE if isSingle else DIRS_POWA,isSingle,c,x,y,h,w,s)
+
+                if expanded:
+
+                    x,y,h,w,s = g[1:] = expanded
+
+                    if isSingle:
+                        powa.append(g)
+
+                    fresh.add((x,y))
+
+                    toRemove=set()
+
+                    for xx in range(x,x+h):
+                        for yy in range(y,y+w):
+                            board[xx][yy]=g
+                            toRemove.add((xx,yy))
+
+                    toRemove.remove((x,y))
+
+                    removed |= toRemove
+
+                    cross = toDropNext & toRemove
+
+                    if cross:
+                        toDropNext.add((x,y))
+                        toDropNext -= cross
+
+        return fresh,removed
+
+    def expand(dirs,isSingle,c,x,y,h,w,s):
+
+        data=start=[x,y,h,w]
+
+        if isSingle:
+
+            triad=[board[x+dx][y+dy] for dx,dy in [(0,1),(1,0),(1,1)]]
+
+            if any(g is None for g in triad):
+                return []
+
+            if not all(g[C]==c and g[S]==1 for g in triad):
+                return []
+
+            data=[x,y,2,2]
+
+        for dx,dy in dirs:
+
+            x,y,h,w=data
+
+            while True:
+
+                rngX=range(x,x+h) if dx==0 else [x+dx+(h-1 if dx==1 else 0)]
+                rngY=range(y,y+w) if dy==0 else [y+dy+(w-1 if dy==1 else 0)]
+
+                collect=collectNextLine(rngX,rngY,dx,dy,isSingle,c,x,y,h,w,s)
+
+                if not collect:
+                    break
+
+                if dx!=0:
+                    if dx==1:
+                        h+=1
+                    else:
+                        x-=1
+                        h+=1
+
+                if dy!=0:
+                    if dy==1:
+                        w+=1
+                    else:
+                        y-=1
+                        w+=1
+
+                if isSingle or goodAlignment(collect,dx,dy,x,y,h,w):
+                    data=[x,y,h,w]
+
+        if start!=data:
+
+            x,y,h,w=data
+
+            return [x,y,h,w,h*w]
+
+        return []
+
+    def collectNextLine(rngX,rngY,dx,dy,isSingle,c,x,y,h,w,s):
+
+        collect=set()
+
+        for a in rngX:
+            for b in rngY:
+
+                if not (0<=a<LX and 0<=b<LY):
+                    continue
+
+                g=board[a][b]
+
+                if g is None:
+                    return []
+
+                if g[C]!=c:
+                    return []
+
+                if isSingle and g[S]>1:
+                    return []
+
+                if dx!=0 and not (y<=g[Y]<y+w and y<g[Y]+g[W]<=y+w):
+                    return []
+
+                if dy!=0 and not (x<=g[X]<x+h and x<g[X]+g[H]<=x+h):
+                    return []
+
+                collect.add(tuple(g))
+
+        return collect
+
+    def goodAlignment(collect,dx,dy,x,y,h,w):
+
+        if dy==-1:
+            return all(g[Y]==y for g in collect)
+
+        if dy==1:
+            return all(g[Y]+g[W]==y+w for g in collect)
+
+        if dx==-1:
+            return all(g[X]==x for g in collect)
+
+        return all(g[X]+g[H]==x+h for g in collect)
+
+    for pair,moves in arrMoves:
+
+        gemsPair=getStartPoses(moves,pair[0],pair[1])
+
+        if not all(board[x][y] is None for x,y,_ in gemsPair):
+            break
+
+        for x,y,c in gemsPair:
+
+            board[x][y]=[c,x,y,1,1,1]
+
+            gemsByColors[c].add((x,y))
+
+        toDrop={(x,y) for x,y,_ in gemsPair}
+
+        while toDrop:
+
+            moved=drop(toDrop)
+
+            powaAndDrop=explode(moved)
+
+            toDrop=makePowa(*powaAndDrop)
+
+    return "\n".join(
+        "".join(" " if g is None else g[C] for g in row)
+        for row in board
+    )
