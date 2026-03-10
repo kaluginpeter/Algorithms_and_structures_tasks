1e4cbbdcf2aa0e195948aff2a37c75793e023aa1
kaluginpeter <streetworkout200412@gmail.com>
2026-03-10 19:54:16 +0300

    Added solution "3130. Find All Possible Stable Binary Arrays II"

--- LeetCode/Hard/3130._Find_All_Possible_Stable_Binary_Arrays_II.py
+++ LeetCode/Hard/3130._Find_All_Possible_Stable_Binary_Arrays_II.py
@@ -46,3 +46,50 @@
 # Constraints:
 #
 # 1 <= zero, one, limit <= 1000
+# Solution
+# Python O(NM) O(NM2) DynamicProgramming
+mod: int = 10**9 + 7
+class Solution:
+    def numberOfStableArrays(self, zero: int, one: int, limit: int) -> int:
+        dp: list[list[list[int]]] = [[[0] * 2 for _ in range(one + 1)] for _ in range(zero + 1)]
+        bound: int = min(zero, limit)
+        for i in range(bound + 1): dp[i][0][0] = 1
+        bound = min(one, limit)
+        for j in range(bound + 1): dp[0][j][1] = 1
+        for i in range(1, zero + 1):
+            for j in range(1, one + 1):
+                if i > limit:
+                    dp[i][j][0] = dp[i - 1][j][0] + dp[i - 1][j][1] - dp[i - limit - 1][j][1]
+                else: dp[i][j][0] = dp[i - 1][j][0] + dp[i - 1][j][1]
+                dp[i][j][0] = (dp[i][j][0] % mod + mod) % mod
+                if j > limit:
+                    dp[i][j][1] = dp[i][j - 1][1] + dp[i][j - 1][0] - dp[i][j - limit - 1][0]
+                else: dp[i][j][1] = dp[i][j - 1][1] + dp[i][j - 1][0]
+                dp[i][j][1] = (dp[i][j][1] % mod + mod) % mod
+        return (dp[zero][one][0] + dp[zero][one][1]) % mod
+
+# C++ O(NM) O(NM2) DynamicProgramming
+static constexpr long long mod = 1e9 + 7;
+class Solution {
+public:
+    int numberOfStableArrays(int zero, int one, int limit) {
+        std::vector<std::vector<std::array<long long, 2>>> dp(zero + 1, std::vector<std::array<long long, 2>>(one + 1, std::array<long long, 2>{}));
+        size_t bound = std::min(zero, limit);
+        for (size_t i = 0; i <= bound; ++i) dp[i][0][0] = 1;
+        bound = std::min(one, limit);
+        for (size_t j = 0; j <= bound; ++j) dp[0][j][1] = 1;
+        for (size_t i = 1; i <= zero; ++i) {
+            for (size_t j = 1; j <= one; ++j) {
+                if (i > limit) {
+                    dp[i][j][0] = dp[i - 1][j][0] + dp[i - 1][j][1] - dp[i - limit - 1][j][1];
+                } else dp[i][j][0] = dp[i - 1][j][0] + dp[i - 1][j][1];
+                dp[i][j][0] = (dp[i][j][0] % mod + mod) % mod;
+                if (j > limit) {
+                    dp[i][j][1] = dp[i][j - 1][1] + dp[i][j - 1][0] - dp[i][j - limit - 1][0];
+                } else dp[i][j][1] = dp[i][j - 1][1] + dp[i][j - 1][0];
+                dp[i][j][1] = (dp[i][j][1] % mod + mod) % mod;
+            }
+        }
+        return (dp[zero][one][0] + dp[zero][one][1]) % mod;
+    }
+};
