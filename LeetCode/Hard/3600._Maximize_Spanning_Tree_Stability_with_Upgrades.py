75a8eafd0d92031f8033bce4847f2a3a046aac16
kaluginpeter <streetworkout200412@gmail.com>
2026-03-12 21:20:08 +0300

    Added solution "3600. Maximize Spanning Tree Stability with Upgrades"

--- LeetCode/Hard/3600._Maximize_Spanning_Tree_Stability_with_Upgrades.py
+++ LeetCode/Hard/3600._Maximize_Spanning_Tree_Stability_with_Upgrades.py
@@ -60,3 +60,117 @@
 # 0 <= k <= n
 # There are no duplicate edges.
 #
+# Solution
+# Python O(ElogE) O(E + N) PriorityQueue DSU
+class Solution:
+    components: int
+    parent: list[int]
+    size: list[int]
+    def find(self, x: int) -> int:
+        while x != self.parent[x]:
+            self.parent[x] = self.parent[self.parent[x]]
+            x = self.parent[x]
+        return x
+    def union(self, u: int, v: int) -> bool:
+        pu: int = self.find(u)
+        pv: int = self.find(v)
+        if pu == pv: return 0
+        self.components -= 1
+        if self.size[pu] > self.size[pv]:
+            self.size[pu] += self.size[pv]
+            self.parent[pv] = pu
+        else:
+            self.size[pv] += self.size[pu]
+            self.parent[pu] = pv
+        return 1
+
+    def maxStability(self, n: int, edges: List[List[int]], k: int) -> int:
+        self.components = n
+        self.parent = list(range(n))
+        self.size = [1] * n
+        must: list[int] = []
+        flex: list[int] = []
+        for it in edges:
+            if it[3]: must.append(it)
+            else: flex.append(it)
+        bound: int = float('inf')
+        for u, v, w, _ in must:
+            bound = min(bound, w)
+            if not self.union(u, v): return -1
+        flex.sort(key=lambda x: -x[2])
+        pq: list[int] = []
+        for u, v, w, _ in flex:
+            if self.union(u, v): heapq.heappush(pq, w)
+        for _ in range(k):
+            if not pq: break
+            x: int = heapq.heappop(pq)
+            bound = min(bound, 2 * x)
+        if self.components != 1: return -1
+        if pq: return min(bound, pq[0])
+        return bound
+
+# C++ O(ElogE) O(E + N) DSU PriorityQueue
+class Solution {
+public:
+    int components;
+    std::vector<int> parent, size;
+
+    int union_(int u, int v){
+        int pu = find(u);
+        int pv = find(v);
+        if (pu == pv) return 0;
+        --components;
+        if(size[pu] > size[pv]){
+            size[pu] += size[pv];
+            parent[pv] = pu;
+        } else {
+            size[pv] += size[pu];
+            parent[pu] = pv;
+        }
+        return 1;
+    }
+
+    int find(int x){
+        while (x != parent[parent[x]]) {
+            parent[x] = parent[parent[x]];
+            x = parent[x];
+        }
+        return x;
+    }
+
+    int maxStability(int n, vector<vector<int>>& edges, int k) {
+        components = n;
+        parent.resize(n);
+        size.resize(n, 1);
+        for (int i = 0; i < n; ++i) parent[i] = i;
+        std::vector<std::vector<int>> must, flex;
+        for (auto &it : edges){
+            if (it[3] == 1) must.emplace_back(it);
+            else flex.emplace_back(it);
+        }
+        int bound = INT_MAX;
+        for(auto &it : must){
+            int u = it[0], v = it[1], w = it[2];
+            bound = std::min(bound, w);
+            if (!union_(u, v)) return -1;
+        }
+        std::sort(flex.begin(), flex.end(), [](vector<int>& a, vector<int>& b){
+            return a[2] > b[2]; // by strength in descending order
+        });
+        std::priority_queue<int, std::vector<int>, std::greater<int>> pq;
+        for (auto &it : flex){
+            int u = it[0];
+            int v = it[1];
+            int w = it[2];
+            if (union_(u, v)) pq.push(w);
+        }
+        while(k-- && !pq.empty()){
+            int x = pq.top();
+            pq.pop();
+            bound = std::min(bound, 2 * x);
+        }
+        if (components != 1) return -1;
+        if (!pq.empty()) return std::min(bound, pq.top());
+        return bound;
+    }
+};
