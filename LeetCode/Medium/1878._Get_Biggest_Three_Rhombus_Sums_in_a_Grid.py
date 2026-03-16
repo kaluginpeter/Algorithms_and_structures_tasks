3fd2a8a068ba6f5429d2d5aff4fd6cfeb92a7f34
kaluginpeter <streetworkout200412@gmail.com>
2026-03-16 20:11:20 +0300

    Added solution "1878. Get Biggest Three Rhombus Sums in a Grid"

--- LeetCode/Medium/1878._Get_Biggest_Three_Rhombus_Sums_in_a_Grid.py
+++ LeetCode/Medium/1878._Get_Biggest_Three_Rhombus_Sums_in_a_Grid.py
@@ -40,3 +40,137 @@
 # n == grid[i].length
 # 1 <= m, n <= 50
 # 1 <= grid[i][j] <= 105
+# Solution
+# Python O(NM + NMmin(N,M)log2(NMmin(N, M))) O(NM) PrefixSum Matrix
+class Solution:
+    def getBiggestThree(self, grid: List[List[int]]) -> List[int]:
+        n: int = len(grid)
+        m: int = len(grid[0])
+        left_diag: list[list[int]] = [[0] * (m + 2) for _ in range(n + 2)]
+        right_diag: list[list[int]] = [[0] * (m + 2) for _ in range(n + 2)]
+        i: int = 0
+        j: int = 0
+        while j < m:
+            tmpi: int = 1
+            tmpj: int = j + 1
+            while tmpi <= n and tmpj > 0:
+                left_diag[tmpi][tmpj] += left_diag[tmpi - 1][tmpj + 1] + grid[tmpi - 1][tmpj - 1]
+                tmpi += 1
+                tmpj -= 1
+            j += 1
+        i = 1
+        j = m - 1
+        while i < n:
+            tmpi: int = i + 1
+            tmpj: int = m
+            while tmpi <= n and tmpj > 0:
+                left_diag[tmpi][tmpj] += left_diag[tmpi - 1][tmpj + 1] + grid[tmpi - 1][tmpj - 1]
+                tmpi += 1
+                tmpj -= 1
+            i += 1
+        i = 0
+        j = m
+        while j > 0:
+            tmpi: int = i + 1
+            tmpj: int = j
+            while tmpi <= n and tmpj <= m:
+                right_diag[tmpi][tmpj] += right_diag[tmpi - 1][tmpj - 1] + grid[tmpi - 1][tmpj - 1]
+                tmpi += 1
+                tmpj += 1
+            j -= 1
+        i = 1
+        j = 0
+        while i < n:
+            tmpi: int = i + 1
+            tmpj: int = j + 1
+            while tmpi <= n and tmpj <= m:
+                right_diag[tmpi][tmpj] += right_diag[tmpi - 1][tmpj - 1] + grid[tmpi - 1][tmpj - 1]
+                tmpi += 1
+                tmpj += 1
+            i += 1
+        seen: set[int] = set()
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                bound: int = min(j - 1, m - j)
+                bound = min(bound, (n - i) >> 1)
+                for k in range(bound + 1):
+                    rhombus_sum: int = (
+                        (left_diag[i + k][j - k] - left_diag[i - 1][j + 1])
+                        + (right_diag[i + k][j + k] - right_diag[i - 1][j - 1])
+                        - grid[i - 1][j - 1]
+                        + (left_diag[i + k + k][j] - left_diag[i + k][j + k])
+                        + (right_diag[i + k + k][j] - right_diag[i + k][j - k])
+                        - (0 if not k else grid[i - 1 + k + k][j - 1])
+                    )
+                    seen.add(rhombus_sum)
+        return sorted(seen, reverse=True)[:3]
+
+# C++ O(NM + NMmin(N, M)log2(NMmin(N, M))) O(NM) Matrix PrefixSum
+class Solution {
+public:
+    vector<int> getBiggestThree(vector<vector<int>>& grid) {
+        size_t n = grid.size(), m = grid[0].size();
+        std::vector<std::vector<uint32_t>>
+            leftDiag(n + 2, std::vector<uint32_t>(m + 2, 0)),
+            rightDiag(n + 2, std::vector<uint32_t>(m + 2, 0));
+        size_t i = 0, j = 0;
+        while (j < m) {
+            size_t tmpi = 1, tmpj = j + 1;
+            while (tmpi <= n && tmpj > 0) {
+                leftDiag[tmpi][tmpj] += leftDiag[tmpi - 1][tmpj + 1] + grid[tmpi - 1][tmpj - 1];
+                ++tmpi; --tmpj;
+            }
+            ++j;
+        }
+        i = 1; j = m - 1;
+        while (i < n) {
+            size_t tmpi = i + 1, tmpj = m;
+            while (tmpi <= n && tmpj > 0) {
+                leftDiag[tmpi][tmpj] += leftDiag[tmpi - 1][tmpj + 1] + grid[tmpi - 1][tmpj - 1];
+                ++tmpi; --tmpj;
+            }
+            ++i;
+        }
+        i = 0; j = m;
+        while (j > 0) {
+            size_t tmpi = i + 1, tmpj = j;
+            while (tmpi <= n && tmpj <= m) {
+                rightDiag[tmpi][tmpj] += rightDiag[tmpi - 1][tmpj - 1] + grid[tmpi - 1][tmpj - 1];
+                ++tmpi; ++tmpj;
+            }
+            --j;
+        }
+        i = 1; j = 0;
+        while (i < n) {
+            size_t tmpi = i + 1, tmpj = j + 1;
+            while (tmpi <= n && tmpj <= m) {
+                rightDiag[tmpi][tmpj] += rightDiag[tmpi - 1][tmpj - 1] + grid[tmpi - 1][tmpj - 1];
+                ++tmpi; ++tmpj;
+            }
+            ++i;
+        }
+        std::set<int> seen;
+        for (size_t i = 1; i <= n; ++i) {
+            for (size_t j = 1; j <= m; ++j) {
+                size_t bound = std::min(j - 1, m - j);
+                bound = std::min(bound, (n - i) >> 1);
+                for (size_t k = 0; k <= bound; ++k) {
+                    uint32_t rhombusSum =
+                    (leftDiag[i + k][j - k] - leftDiag[i - 1][j + 1])
+                    + (rightDiag[i + k][j + k] - rightDiag[i - 1][j - 1])
+                    - grid[i - 1][j - 1]
+                    + (leftDiag[i + k + k][j] - leftDiag[i + k][j + k])
+                    + (rightDiag[i + k + k][j] - rightDiag[i + k][j - k])
+                    - (k ? grid[i - 1 + k + k][j - 1] : 0);
+                    seen.insert(rhombusSum);
+                }
+            }
+        }
+        std::vector<int> output;
+        while (!seen.empty() && output.size() < 3) {
+            output.push_back(*--seen.end());
+            seen.erase(*--seen.end());
+        }
+        return output;
+    }
+};
