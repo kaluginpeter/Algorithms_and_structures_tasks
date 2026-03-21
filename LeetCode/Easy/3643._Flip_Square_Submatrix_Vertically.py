f74563f96ab0c736e717fe5caedf883cfe420f6e
kaluginpeter <streetworkout200412@gmail.com>
2026-03-21 20:01:49 +0300

    Added solution "3643. Flip Square Submatrix Vertically"

--- LeetCode/Easy/3643._Flip_Square_Submatrix_Vertically.py
+++ LeetCode/Easy/3643._Flip_Square_Submatrix_Vertically.py
@@ -42,3 +42,27 @@
 # 0 <= y < n
 # 1 <= k <= min(m - x, n - y)
 #
+# Solution
+# Python O(K^2) O(1) Matrix TwoPointers
+class Solution:
+    def reverseSubmatrix(self, grid: List[List[int]], x: int, y: int, k: int) -> List[List[int]]:
+        n: int = len(grid)
+        i: int = 0
+        while x + i < x + k - i - 1:
+            for kx in range(k):
+                grid[x + i][y + kx], grid[x + k - i - 1][y + kx] = grid[x + k - i - 1][y + kx], grid[x + i][y + kx]
+            i += 1
+        return grid
+
+# C++ O(K^2) O(1) TwoPointers Matrix
+class Solution {
+public:
+    vector<vector<int>> reverseSubmatrix(vector<vector<int>>& grid, int x, int y, int k) {
+        size_t n = grid.size(), i = 0;
+        while (x + i < x + k - i - 1) {
+            for (size_t kx = 0; kx < k; ++kx) std::swap(grid[x + i][y + kx], grid[x + k - i - 1][y + kx]);
+            ++i;
+        }
+        return grid;
+    }
+};
