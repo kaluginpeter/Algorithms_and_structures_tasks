8369b2caf4d3cf6fd2e3200a8fd1dd5d810b0731
kaluginpeter <streetworkout200412@gmail.com>
2026-03-22 21:15:52 +0300

    Added solution "1886. Determine Whether Matrix Can Be Obtained By Rotation"

--- LeetCode/Easy/1886._Determine_Whether_Matrix_Can_Be_Obtained_By_Rotation.py
+++ LeetCode/Easy/1886._Determine_Whether_Matrix_Can_Be_Obtained_By_Rotation.py
@@ -88,3 +88,35 @@ public:
         return false;
     }
 };
+
+
+# C++ O(NM) O(1) Matrix
+# C++ O(NM) O(1) Matrix
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        size_t squareSize = matrix.size(), startRow = 0, startCol = 0;
+        while (squareSize > 1) {
+            for (size_t i = 0; i < squareSize - 1; ++i) {
+                size_t ulr = startRow, ulc = startCol + i;
+                size_t urr = startRow + i, urc = startCol + squareSize - 1;
+                size_t llr = startRow + squareSize - 1 - i, llc = startCol;
+                size_t lrr = startRow + squareSize - 1, lrc = startCol + squareSize - 1 - i;
+                std::swap(matrix[ulr][ulc], matrix[urr][urc]);
+                std::swap(matrix[ulr][ulc], matrix[llr][llc]);
+                std::swap(matrix[llr][llc], matrix[lrr][lrc]);
+            }
+            ++startRow;
+            ++startCol;
+            squareSize -= 2;
+        }
+    }
+
+    bool findRotation(vector<vector<int>>& mat, vector<vector<int>>& target) {
+        for (int i = 0; i < 4; ++i) {
+            if (mat == target) return true;
+            rotate(mat);
+        }
+        return false;
+    }
+};
