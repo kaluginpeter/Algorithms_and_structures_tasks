8d1ad89fc4c21294d7759a59ff2b87dd6ff91f7e
kaluginpeter <streetworkout200412@gmail.com>
2026-03-24 21:20:35 +0300

    Added solution "2906. Construct Product Matrix"

--- LeetCode/Medium/2906._Construct_Product_Matrix.py
+++ LeetCode/Medium/2906._Construct_Product_Matrix.py
@@ -30,3 +30,47 @@
 # 1 <= m == grid[i].length <= 105
 # 2 <= n * m <= 105
 # 1 <= grid[i][j] <= 109
+# Solution
+# Python O(NM) O(1) PrefixSum Matrix Math
+mod: int = 12345
+class Solution:
+    def constructProductMatrix(self, grid: List[List[int]]) -> List[List[int]]:
+        n: int = len(grid)
+        m: int = len(grid[0])
+        output: list[list[int]] = [[0] * m for _ in range(n)]
+        acc: int = 1
+        for i in range(n):
+            for j in range(m):
+                output[i][j] = acc
+                acc = acc * grid[i][j] % mod
+        acc = 1
+        for i in range(n - 1, -1, -1):
+            for j in range(m - 1, -1, -1):
+                output[i][j] = output[i][j] * acc % mod
+                acc = acc * grid[i][j] % mod
+        return output
+
+# C++ O(NM) O(1) PrefixSum Math Matrix
+constexpr int mod = 12345;
+class Solution {
+public:
+    vector<vector<int>> constructProductMatrix(vector<vector<int>>& grid) {
+        size_t n = grid.size(), m = grid[0].size();
+        std::vector<std::vector<int>> output(n, std::vector<int>(m));
+        long long acc = 1;
+        for (size_t i = 0; i < n; ++i) {
+            for (size_t j = 0; j < m; ++j) {
+                output[i][j] = acc;
+                acc = acc * grid[i][j] % mod;
+            }
+        }
+        acc = 1;
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = m - 1; j >= 0; --j) {
+                output[i][j] = output[i][j] * acc % mod;
+                acc = acc * grid[i][j] % mod;
+            }
+        }
+        return output;
+    }
+};
