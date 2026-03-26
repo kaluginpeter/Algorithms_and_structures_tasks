59db52333f420cd4b7db38816ee8e9e7f9cd55f1
kaluginpeter <streetworkout200412@gmail.com>
2026-03-26 22:07:09 +0300

    Added solution "Bird Mountain"

--- CodeWars/5kyu/Bird_Mountain.cpp
+++ CodeWars/5kyu/Bird_Mountain.cpp
@@ -45,3 +45,54 @@ Bird Mountain
 Bird Mountain - the river
 AlgorithmsGraph Theory
 */
+#include <bits/stdc++.h>
+using namespace std;
+
+int peak_height(vector<string>& mountain) {
+    int n = mountain.size();
+    int m = mountain[0].size();
+
+    vector<vector<int>> height(n, vector<int>(m, 0));
+    queue<pair<int,int>> q;
+
+    auto is_boundary = [&](int i, int j) {
+        if (mountain[i][j] != '^') return false;
+
+        vector<pair<int,int>> dirs = {{-1,0},{1,0},{0,-1},{0,1}};
+        for (auto [dx, dy] : dirs) {
+            int ni = i + dx, nj = j + dy;
+            if (ni < 0 || nj < 0 || ni >= n || nj >= m || mountain[ni][nj] == ' ')
+                return true;
+        }
+        return false;
+    };
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (is_boundary(i, j)) {
+                height[i][j] = 1;
+                q.push({i, j});
+            }
+        }
+    }
+
+    int maxh = 0;
+    vector<pair<int,int>> dirs = {{-1,0},{1,0},{0,-1},{0,1}};
+
+    while (!q.empty()) {
+        auto [i, j] = q.front(); q.pop();
+        maxh = max(maxh, height[i][j]);
+
+        for (auto [dx, dy] : dirs) {
+            int ni = i + dx, nj = j + dy;
+
+            if (ni >= 0 && nj >= 0 && ni < n && nj < m) {
+                if (mountain[ni][nj] == '^' && height[ni][nj] == 0) {
+                    height[ni][nj] = height[i][j] + 1;
+                    q.push({ni, nj});
+                }
+            }
+        }
+    }
+
+    return maxh;
+}
