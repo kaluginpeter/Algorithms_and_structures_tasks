a13c9215b8997c6ac2fd6ce3e9646ac50a9f2034
kaluginpeter <streetworkout200412@gmail.com>
2026-03-26 22:03:22 +0300

    Added solution "3548. Equal Sum Grid Partition II"

--- LeetCode/Hard/3548._Equal_Sum_Grid_Partition_II.cpp
+++ LeetCode/Hard/3548._Equal_Sum_Grid_Partition_II.cpp
@@ -65,3 +65,61 @@ Constraints:
 1 <= grid[i][j] <= 105
 
 */
+// Solution
+// C++ O(NM) O(NM) Matrix
+class Solution {
+public:
+    vector<vector<int>> rotation(vector<vector<int>>& grid) {
+        size_t m = grid.size(), n = grid[0].size();
+        vector tmp(n, vector<int>(m));
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) tmp[j][m - 1 - i] = grid[i][j];
+        }
+        return tmp;
+    }
+    bool canPartitionGrid(vector<vector<int>>& grid) {
+        long long total = 0, sum, tag;
+        size_t m = grid.size(), n = grid[0].size();
+        unordered_set<long long> exist;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) total += grid[i][j];
+        }
+        for (int k = 0; k < 4; ++k) {
+            exist.clear();
+            exist.insert(0);
+            sum = 0;
+            m = grid.size();
+            n = grid[0].size();
+            if (m < 2) {
+                grid = rotation(grid);
+                continue;
+            }
+            if (n == 1) {
+                for (int i = 0; i < m - 1; ++i) {
+                    sum += grid[i][0];
+                    tag = sum * 2 - total;
+                    if (tag == 0 || tag == grid[0][0] || tag == grid[i][0]) return true;
+                }
+                grid = rotation(grid);
+                continue;
+            }
+            for (int i = 0; i < m - 1; ++i) {
+                for (int j = 0; j < n; ++j) {
+                    exist.insert(grid[i][j]);
+                    sum += grid[i][j];
+                }
+                tag = sum * 2 - total;
+                if (i == 0) {
+                    if (tag == 0 || tag == grid[0][0] ||
+                        tag == grid[0][n - 1]) {
+                        return true;
+                    }
+                    continue;
+                }
+                if (exist.contains(tag)) return true;
+            }
+            grid = rotation(grid);
+        }
+        return false;
+    }
+};
