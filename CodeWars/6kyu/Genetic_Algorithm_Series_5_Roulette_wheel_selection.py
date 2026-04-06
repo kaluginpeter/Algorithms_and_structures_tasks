5f452aafc18fbcd4fd8109c1a0468b23a42e8f5a
kaluginpeter <streetworkout200412@gmail.com>
2026-04-06 23:53:37 +0300

    Added description "Genetic Algorithm Series - #5 Roulette wheel selection"

---
+++ CodeWars/6kyu/Genetic_Algorithm_Series_5_Roulette_wheel_selection.py
@@ -0,0 +1,21 @@
+# The "Roulette wheel selection", also known as "Fitness proportionate selection", is a genetic operator used in genetic algorithms for selecting potentially useful solutions for recombination.
+#
+# Your task is to implement it.
+#
+# roulette
+#
+# You throw a coin in and has a chance to fall in one of the slots, the higher the fitness the higher the chance the element has to be selected.
+#
+# Given the population and fitnesses, your task is to run the roulette to return one element.
+#
+# Note: Some tests are random. If you think your algorithm is correct but the result fails, trying again should work.
+#
+# See other katas from this series
+# Genetic Algorithm Series - #1 Generate
+# Genetic Algorithm Series - #2 Mutation
+# Genetic Algorithm Series - #3 Crossover
+# Genetic Algorithm Series - #4 Get population and fitnesses
+# Genetic Algorithm Series - #5 Roulette wheel selection
+# This kata is a piece of2 kyuBinary Genetic Algorithm
+#
+# AlgorithmsGenetic AlgorithmsArrays
