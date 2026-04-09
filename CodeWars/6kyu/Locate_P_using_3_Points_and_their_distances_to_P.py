e46149dac48f53cad5ab7ec7f0f3b28210b082f4
kaluginpeter <streetworkout200412@gmail.com>
2026-04-09 21:30:35 +0300

    Added description "Locate P using 3 Points and their distances to P"

---
+++ CodeWars/6kyu/Locate_P_using_3_Points_and_their_distances_to_P.py
@@ -0,0 +1,7 @@
+# Task
+# Given Points A, B, C ∈ ℤ2 and dA, dB, dC ∈ ℕ their respective squared euclidian distances to a certain point P ∈ ℤ2, return the value of P.
+#
+# Note
+# A, B, and C will always be distinct and non-collinear
+#
+# MathematicsGeometry
