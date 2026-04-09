b8a253ac39928846584aeb5a020186889770d747
kaluginpeter <streetworkout200412@gmail.com>
2026-04-09 21:30:58 +0300

    Added solution "Locate P using 3 Points and their distances to P"

--- CodeWars/6kyu/Locate_P_using_3_Points_and_their_distances_to_P.py
+++ CodeWars/6kyu/Locate_P_using_3_Points_and_their_distances_to_P.py
@@ -5,3 +5,18 @@
 # A, B, and C will always be distinct and non-collinear
 #
 # MathematicsGeometry
+# Solution
+def triangulate(A, dA, B, dB, C, dC):
+    xA, yA = A
+    xB, yB = B
+    xC, yC = C
+    a1 = 2 * (xB - xA)
+    b1 = 2 * (yB - yA)
+    c1 = dA - dB + xB**2 - xA**2 + yB**2 - yA**2
+    a2 = 2 * (xC - xA)
+    b2 = 2 * (yC - yA)
+    c2 = dA - dC + xC**2 - xA**2 + yC**2 - yA**2
+    det = a1 * b2 - a2 * b1
+    x = (c1 * b2 - c2 * b1) // det
+    y = (a1 * c2 - a2 * c1) // det
+    return (x, y)
