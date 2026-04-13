f2fd8a9e7de5d1acc1271b95be59bbe6ddc21c18
kaluginpeter <streetworkout200412@gmail.com>
2026-04-13 19:34:22 +0300

    Added solution "The Santa Express"

--- CodeWars/5kyu/The_Santa_Express.py
+++ CodeWars/5kyu/The_Santa_Express.py
@@ -33,3 +33,41 @@
 # There is always at least 2 possible routes.
 # There will never be duplicates in the list of houses.
 # CombinatoricsPerformance
+# Solution
+from math import dist  # Euclidean distance function
+
+
+def delivery_route(houses: list[tuple[int, int]]) -> list[tuple[int, int]]:
+    """Find the optimal route between all points in the array."""
+    n = len(houses)
+    dp = [[float('inf')] * n for _ in range(1 << n)]
+    parent = [[-1] * n for _ in range(1 << n)]
+    dp[1][0] = 0
+
+    for mask in range(1 << n):
+        for u in range(n):
+            if not (mask & (1 << u)): continue
+            for v in range(n):
+                if mask & (1 << v): continue
+                new_mask = mask | (1 << v)
+                new_cost = dp[mask][u] + dist(houses[u], houses[v])
+                if new_cost < dp[new_mask][v]:
+                    dp[new_mask][v] = new_cost
+                    parent[new_mask][v] = u
+    full_mask = (1 << n) - 1
+    best_cost = float('inf')
+    last = -1
+    for i in range(1, n):
+        cost = dp[full_mask][i] + dist(houses[i], houses[0])
+        if cost < best_cost:
+            best_cost = cost
+            last = i
+    path = []
+    mask = full_mask
+    while last != -1:
+        path.append(last)
+        prev = parent[mask][last]
+        mask ^= (1 << last)
+        last = prev
+    path.reverse()
+    return [houses[i] for i in path] + [houses[0]]
