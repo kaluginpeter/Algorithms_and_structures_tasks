f754d5ff0f56f6223675d7ac3a4e41189c7911d2
kaluginpeter <streetworkout200412@gmail.com>
2026-04-15 23:34:23 +0300

    Added solution "Conway's Game of Life - Unlimited Edition"

--- CodeWars/4kyu/Conway_s_Game_of_Life_Unlimited_Edition.py
+++ CodeWars/4kyu/Conway_s_Game_of_Life_Unlimited_Edition.py
@@ -12,3 +12,45 @@
 #
 # print(htmlize(cells))
 # GamesArraysPuzzlesCellular Automata
+# Solution
+from preloaded import htmlize # this can help you debug your code
+from itertools import product
+
+def f(x):
+    ni, nj = len(x), len(x[0])
+    output = set()
+    for i in range(ni):
+        for j in range(nj):
+            if x[i][j]: output.add((i, j))
+    return output
+
+def g(x):
+    if not x: mi = mj = ni = nj = 0
+    else:
+        min_i = min(map(lambda x: x[0], x))
+        min_j = min(map(lambda x: x[1], x))
+        max_i = max(map(lambda x: x[0], x))
+        max_j = max(map(lambda x: x[1], x))
+    range_i = max_i - min_i + 1
+    range_j = max_j - min_j + 1
+    output = [[False for j in range(range_j)] for i in range(range_i)]
+    for (i, j) in x: output[i - min_i][j - min_j] = True
+    return output
+
+def get_generation(cells, generations):
+    dp = f(cells)
+    for n in range(generations):
+        neighs = set()
+        next_dp = set()
+        for (i, j) in dp:
+            for (di, dj) in product((-1, 0, 1), (-1, 0, 1)):
+                neighs.add((i + di, j + dj))
+        for (i, j) in neighs:
+            neighbours = 0
+            for (di, dj) in product((-1, 0, 1), (-1, 0, 1)):
+                if (i + di, j + dj) in dp and (di, dj) != (0, 0):
+                    neighbours += 1
+            if neighbours == 3: next_dp.add((i, j))
+            elif neighbours == 2 and (i, j) in dp: next_dp.add((i, j))
+        dp = next_dp
+    return g(dp)
