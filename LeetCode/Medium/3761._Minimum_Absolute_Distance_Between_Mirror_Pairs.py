30c6947e02c06fc60c9978a3baa7b6b2f5ea24d1
kaluginpeter <streetworkout200412@gmail.com>
2026-04-17 11:29:56 +0300

    Added solution "3761. Minimum Absolute Distance Between Mirror Pairs"

--- LeetCode/Medium/3761._Minimum_Absolute_Distance_Between_Mirror_Pairs.py
+++ LeetCode/Medium/3761._Minimum_Absolute_Distance_Between_Mirror_Pairs.py
@@ -52,3 +52,52 @@
 #
 # 1 <= nums.length <= 105
 # 1 <= nums[i] <= 109​​​​​​​
+# Solution
+# Python O(Nlog10(max(N))) O(N) Math HashMap
+class Solution:
+    def get_mirror(self, x: int) -> int:
+        output: int = 0
+        while x:
+            output = output * 10 + x % 10
+            x //= 10
+        return output
+
+    def minMirrorPairDistance(self, nums: List[int]) -> int:
+        n: int = len(nums)
+        seen: dict[int, int] = defaultdict(int)
+        diff: int = -1
+        for i in range(n):
+            if nums[i] in seen:
+                cur_diff: int = i - seen[nums[i]]
+                if diff == -1 or diff > cur_diff:
+                    diff = cur_diff
+            seen[self.get_mirror(nums[i])] = i
+        return diff
+
+# C++ O(Nlog10(max(N))) O(N) HashMap Math
+class Solution {
+public:
+    long long getMirror(int x) {
+        long long output = 0;
+        while (x) {
+            output = output * 10 + x % 10;
+            x /= 10;
+        }
+        return output;
+    }
+
+    int minMirrorPairDistance(vector<int>& nums) {
+        size_t n = nums.size();
+        std::unordered_map<long long, size_t> seen;
+        size_t diff = 0;
+        for (size_t i = 0; i < n; ++i) {
+            if (seen.count(nums[i])) {
+                size_t curDiff = i - seen[nums[i]];
+                if (!diff || diff > curDiff) diff = curDiff;
+            }
+            long long revert = getMirror(nums[i]);
+            seen[revert] = i;
+        }
+        return (diff ? diff : -1);
+    }
+};
