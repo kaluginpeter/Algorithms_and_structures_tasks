7e7969c2dcc5384b93750b2bb27efd5199b20a44
kaluginpeter <streetworkout200412@gmail.com>
2026-04-21 11:18:21 +0300

    Added solution "1722. Minimize Hamming Distance After Swap Operations"

--- LeetCode/Medium/1722._Minimize_Hamming_Distance_After_Swap_Operations.py
+++ LeetCode/Medium/1722._Minimize_Hamming_Distance_After_Swap_Operations.py
@@ -35,3 +35,119 @@
 # allowedSwaps[i].length == 2
 # 0 <= ai, bi <= n - 1
 # ai != bi
+# Solution
+# Python O(Na(N) + E) O(NV) DisjointSetUnion
+class DSU:
+    def __init__(self, n: int, source: list[int]) -> None:
+        self.rank: list[int] = [1] * n
+        self.parent: list[int] = [0] * n
+        self.component: list[dict[int, int]] = [defaultdict(int) for _ in range(n)]
+        for i in range(n):
+            self.parent[i] = i
+            self.component[i][source[i]] = 1
+
+    def find(self, x) -> int:
+        while x != self.parent[x]:
+            self.parent[x] = self.parent[self.parent[x]]
+            x = self.parent[x]
+        return x
+
+    def union(self, u: int, v: int) -> None:
+        pu: int = self.find(u)
+        pv: int = self.find(v)
+        if pu == pv: return
+        if self.rank[pu] > self.rank[pv]:
+            self.parent[pv] = pu
+            self.rank[pu] += self.rank[pv]
+            for node, freq in self.component[pv].items():
+                self.component[pu][node] += freq
+            self.component[pv].clear()
+        else:
+            self.parent[pu] = pv
+            self.rank[pv] += self.rank[pu]
+            for node, freq in self.component[pu].items():
+                self.component[pv][node] += freq
+            self.component[pu].clear()
+
+
+class Solution:
+    def minimumHammingDistance(self, source: List[int], target: List[int], allowedSwaps: List[List[int]]) -> int:
+        n: int = len(source)
+        dsu: DSU = DSU(n, source)
+        for u, v in allowedSwaps: dsu.union(u, v)
+        output: int = 0
+        for i in range(n):
+            parent: int = dsu.find(i)
+            if dsu.component[parent][target[i]]:
+                dsu.component[parent][target[i]] -= 1
+            else:
+                output += 1
+        return output
+
+# C++ O(Na(N) + E) O(NV) DisjointSetUnion
+class DSU {
+private:
+    std::vector<int> rank, parent;
+    std::vector<std::unordered_map<int, int>> component;
+public:
+    DSU (int n, std::vector<int>& source) {
+        rank.resize(n, 1);
+        parent.resize(n);
+        component.resize(n);
+        for (int i = 0; i < n; ++i) {
+            parent[i] = i;
+            ++component[i][source[i]];
+        }
+    };
+
+    int find(int x) {
+        while (x != parent[x]) {
+            parent[x] = parent[parent[x]];
+            x = parent[x];
+        }
+        return x;
+    }
+
+    void union_(int x, int y) {
+        int px = find(x), py = find(y);
+        if (px == py) return;
+        if (rank[px] > rank[py]) {
+            rank[px] += rank[py];
+            parent[py] = px;
+            for (auto& node : component[py]) {
+                component[px][node.first] += node.second;
+            }
+            component[py].clear();
+        } else {
+            rank[py] += rank[px];
+            parent[px] = py;
+            for (auto& node : component[px]) {
+                component[py][node.first] += node.second;
+            }
+            component[px].clear();
+        }
+    }
+
+    std::unordered_map<int, int>& getComponent(int x) {
+        return component[x];
+    }
+};
+
+class Solution {
+public:
+    int minimumHammingDistance(vector<int>& source, vector<int>& target, vector<vector<int>>& allowedSwaps) {
+        int n = source.size();
+        DSU dsu = DSU(n, source);
+        for (std::vector<int>& edge : allowedSwaps) {
+            dsu.union_(edge[0], edge[1]);
+        }
+        int output = 0;
+        for (int i = 0; i < n; ++i) {
+            int parent = dsu.find(i);
+            std::unordered_map<int, int>& component = dsu.getComponent(parent);
+            if (component[target[i]]) --component[target[i]];
+            else ++output;
+        }
+        return output;
+    }
+};
