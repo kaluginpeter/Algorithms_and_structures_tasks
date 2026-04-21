afc2071e29e8935cc10312bda50375cc1ec45b10
kaluginpeter <streetworkout200412@gmail.com>
2026-04-21 11:13:23 +0300

    Added description "Find the unique number"

---
+++ CodeWars/6kyu/Find_the_unique_number_.py
@@ -0,0 +1,7 @@
+# Complete the function which returns the only unique number from an array.
+#
+# All numbers in the unsorted array are present twice, except the one you have to find. The numbers are always valid integer values between 1 and 2147483647, so no need for type and error checking. The array contains at least one number and may contain millions of numbers. So make sure your solution is optimized for speed.
+#
+# Example
+# [ 1, 8, 4, 4, 6, 1, 8 ]  -->  6
+# ArraysPerformanceFundamentals
