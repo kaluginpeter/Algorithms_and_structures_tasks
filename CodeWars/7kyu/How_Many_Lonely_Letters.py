87633051e3a0dc59f0a15fd737a63c4d7a5bc9d3
kaluginpeter <streetworkout200412@gmail.com>
2026-04-22 22:06:07 +0300

    Added solution "How Many Lonely Letters?"

--- CodeWars/7kyu/How_Many_Lonely_Letters.py
+++ CodeWars/7kyu/How_Many_Lonely_Letters.py
@@ -26,3 +26,16 @@
 # Input: "A-dA" -> Output: 1
 # Input: "zz" -> Output: 0
 # StringsFundamentalsAlgorithms
+# Solution
+def count_lonely_letters(text):
+    text = text.lower()
+    output: int = 0
+    hashmap: dict[str, int] = dict()
+    for char in text:
+        hashmap[char] = hashmap.get(char, 0) + 1
+    for i in range(len(text)):
+        if hashmap[text[i]] > 1 or not (ord('a') <= ord(text[i]) <= ord('z')): continue
+        left: bool = hashmap.get(chr(ord(text[i]) - 1) if text[i] != 'a' else '', 0)
+        right: bool = False if text[i] == 'z' else hashmap.get(chr(ord(text[i]) + 1), 0)
+        if not left and not right: output += 1
+    return output
