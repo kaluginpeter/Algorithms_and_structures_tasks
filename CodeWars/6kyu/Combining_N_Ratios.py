be8f52fd13fef3df288a3593de8b1d212e9fc022
kaluginpeter <streetworkout200412@gmail.com>
2026-04-24 20:35:31 +0300

    Added solution "Combining N Ratios"

--- CodeWars/6kyu/Combining_N_Ratios.py
+++ CodeWars/6kyu/Combining_N_Ratios.py
@@ -19,3 +19,19 @@
 #  ] (1 to 1e5 inclusive).
 #
 # AlgorithmsFundamentals
+# Solution
+from math import gcd
+from functools import reduce
+
+def ratio_combinator(ratios):
+    pairs = [tuple(map(int, r.split(":"))) for r in ratios]
+    result = [pairs[0][0], pairs[0][1]]
+    for left, right in pairs[1:]:
+        current = result[-1]
+        lcm = current * left // gcd(current, left)
+        m1 = lcm // current
+        m2 = lcm // left
+        result = [x * m1 for x in result]
+        result.append(right * m2)
+    g = reduce(gcd, result)
+    return ":".join(str(x // g) for x in result)
