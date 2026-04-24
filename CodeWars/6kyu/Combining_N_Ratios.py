f69acf5e57c44aa9e506e2c730772cf6fab0ff40
kaluginpeter <streetworkout200412@gmail.com>
2026-04-24 20:35:06 +0300

    Added description "Combining N Ratios"

---
+++ CodeWars/6kyu/Combining_N_Ratios.py
@@ -0,0 +1,21 @@
+# This is a more difficult version of this kata - I suggest solving that kata before attempting this one.
+#
+# Task
+# Your task is similar to the kata above, however now you are not limited to only 2 sets of ratios - you will be given an array of ratios as strings, and you must return a combined ratio in its simplest form, as a string. See the example below:
+#
+# Example:
+#
+# ratio_combinator(["12:4","3:7","24:30"]) -> '36:12:28:35'
+# The given input ratios may not be in their simplest form, and the inputs range from
+# [
+# 1
+# ,
+# 1
+# 0
+# 5
+# ]
+# [1,10
+# 5
+#  ] (1 to 1e5 inclusive).
+#
+# AlgorithmsFundamentals
