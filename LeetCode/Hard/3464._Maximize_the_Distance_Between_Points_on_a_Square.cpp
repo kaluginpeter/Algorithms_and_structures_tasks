78486b9363d12737966be16fdc5df92c1663f675
kaluginpeter <streetworkout200412@gmail.com>
2026-04-25 22:21:03 +0300

    Added solution "3464. Maximize the Distance Between Points on a Square"

--- LeetCode/Hard/3464._Maximize_the_Distance_Between_Points_on_a_Square.cpp
+++ LeetCode/Hard/3464._Maximize_the_Distance_Between_Points_on_a_Square.cpp
@@ -59,3 +59,45 @@ points[i] lies on the boundary of the square.
 All points[i] are unique.
 4 <= k <= min(25, points.length)
 */
+// Solution
+// C++ Sorting BinarySearch O(NKlogMlogN) O(N)
+class Solution {
+public:
+    int maxDistance(int side, vector<vector<int>>& points, int k) {
+        std::vector<long long> arr;
+        for (auto& p : points) {
+            int x = p[0], y = p[1];
+            if (!x) arr.push_back(y);
+            else if (y == side) arr.push_back(side + x);
+            else if (x == side) arr.push_back(side * 3LL - y);
+            else arr.push_back(side * 4LL - x);
+        }
+        std::sort(arr.begin(), arr.end());
+        auto check = [&](long long limit) -> bool {
+            for (long long start : arr) {
+                long long end = start + side * 4LL - limit;
+                long long cur = start;
+                for (int i = 0; i < k - 1; i++) {
+                    auto it = ranges::lower_bound(arr, cur + limit);
+                    if (it == arr.end() || *it > end) {
+                        cur = -1;
+                        break;
+                    }
+                    cur = *it;
+                }
+                if (cur >= 0) return true;
+            }
+            return false;
+        };
+        long long left = 1, right = side;
+        int output = 0;
+        while (left <= right) {
+            long long mid = left + ((right - left) >> 1);
+            if (check(mid)) {
+                left = mid + 1;
+                output = mid;
+            } else right = mid - 1;
+        }
+        return output;
+    }
+};
