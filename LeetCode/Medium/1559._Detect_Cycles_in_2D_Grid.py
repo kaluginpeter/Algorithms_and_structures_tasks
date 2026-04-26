8e1ee7dbc1ce656e98d1ea587378065ff5d08d61
kaluginpeter <streetworkout200412@gmail.com>
2026-04-26 22:40:29 +0300

    Added solution "1559. Detect Cycles in 2D Grid"

--- LeetCode/Medium/1559._Detect_Cycles_in_2D_Grid.py
+++ LeetCode/Medium/1559._Detect_Cycles_in_2D_Grid.py
@@ -38,3 +38,76 @@
 # n == grid[i].length
 # 1 <= m, n <= 500
 # grid consists only of lowercase English letters.
+# Solution
+# Python O(NM) O(NM) Matrix Depth-First-Search
+class Solution:
+    moves: list[tuple[int, int]] = [
+        (0, 1), (0, -1), (1, 0), (-1, 0)
+    ]
+    def check(self, start: int, n: int, m: int, grid: list[list[str]], seen: list[list[bool]]) -> bool:
+        st: list[tuple[int, int]] = []
+        st.append((start // m, start % m, float('inf')))
+        while st:
+            i, j, parent = st.pop()
+            if not seen[i][j]: seen[i][j] = True
+            for dx, dy in self.moves:
+                ni: int = i + dx
+                nj: int = j + dy
+                if not (0 <= ni < n) or not (0 <= nj < m): continue
+                if grid[ni][nj] != grid[i][j]: continue
+                if ni * m + nj == parent: continue
+                if seen[ni][nj]: return True
+                st.append((ni, nj, i * m + j))
+        return False
+
+    def containsCycle(self, grid: List[List[str]]) -> bool:
+        n: int = len(grid)
+        m: int = len(grid[0])
+        seen: list[list[bool]] = [[False] * m for _ in range(n)]
+        for i in range(n):
+            for j in range(m):
+                if seen[i][j]: continue
+                if self.check(i * m + j, n, m, grid, seen): return True
+        return False
+
+# C++ O(NM) O(NM) Depth-First-Search Matrix
+class Solution {
+    const std::vector<std::pair<int, int>> moves = {
+        {1, 0}, {-1, 0}, {0, 1}, {0, -1}
+    };
+public:
+    bool check(size_t start, const size_t& n, const size_t& m,
+        const vector<vector<char>>& grid,
+        vector<vector<bool>>& seen
+    ) {
+        std::vector<std::tuple<size_t,size_t,size_t>> st;
+        st.push_back({start / m, start % m, SIZE_MAX});
+        while (!st.empty()) {
+            auto [i, j, parent] = st.back();
+            st.pop_back();
+            if (!seen[i][j]) seen[i][j] = true;
+            for (auto [dx, dy] : moves) {
+                size_t ni = i + dx;
+                size_t nj = j + dy;
+                if (ni >= n || nj >= m) continue;
+                if (grid[ni][nj] != grid[i][j]) continue;
+                if (ni * m + nj == parent) continue;
+                if (seen[ni][nj]) return true;
+                st.push_back({ni, nj, i * m + j});
+            }
+        }
+        return false;
+    };
+
+    bool containsCycle(vector<vector<char>>& grid) {
+        size_t n = grid.size(), m = grid[0].size();
+        std::vector<std::vector<bool>> seen(n, std::vector<bool>(m, false));
+        for (size_t i = 0; i < n; ++i) {
+            for (size_t j = 0; j < m; ++j) {
+                if (seen[i][j]) continue;
+                if (check(i * m + j, n, m, grid, seen)) return true;
+            }
+        }
+        return false;
+    }
+};
