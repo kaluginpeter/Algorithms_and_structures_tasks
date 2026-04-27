5cf903b18231a2e497a3fd8e91fee405382a1d7d
kaluginpeter <streetworkout200412@gmail.com>
2026-04-27 21:05:57 +0300

    Added solution "Combining Ratios"

--- CodeWars/7kyu/Combining_Ratios.py
+++ CodeWars/7kyu/Combining_Ratios.py
@@ -11,3 +11,18 @@
 # return as a string in the format "9:3:7"
 #
 # For a harder version goto this kata
+# Solution
+from math import gcd
+
+
+def merge_ratios(ratio1, ratio2):
+    a, b1 = map(int, ratio1.split(":"))
+    b2, c = map(int, ratio2.split(":"))
+    lcm = b1 * b2 // gcd(b1, b2)
+    m1 = lcm // b1
+    m2 = lcm // b2
+    a *= m1
+    b = lcm
+    c *= m2
+    g = gcd(gcd(a, b), c)
+    return f"{a // g}:{b // g}:{c // g}"
