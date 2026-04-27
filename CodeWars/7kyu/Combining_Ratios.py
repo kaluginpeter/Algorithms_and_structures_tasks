61618117f18eead74399140fb3cb19246aa94661
kaluginpeter <streetworkout200412@gmail.com>
2026-04-27 21:05:32 +0300

    Added description "Combining Ratios"

---
+++ CodeWars/7kyu/Combining_Ratios.py
@@ -0,0 +1,13 @@
+# 2 ratios given as strings in the form A:B and B:C
+#
+# eg 12:4 3:7
+#
+# Your program must return the ratio of A:B:C in simplest form
+#
+# eg 12:4 3:7 ---> 9:3:7
+#
+# Given Ratios are not in simplest form A,B and C are always in the range of 1 --> 1e5
+#
+# return as a string in the format "9:3:7"
+#
+# For a harder version goto this kata
