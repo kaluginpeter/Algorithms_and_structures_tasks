8fdb65b9367173d7da88f1c1c4b5bb6df7841da1
kaluginpeter <streetworkout200412@gmail.com>
2026-04-28 22:56:33 +0300

    Added description "Oh come on! Hello world! once again?"

---
+++ CodeWars/7kyu/Oh_come_on_Hello_world_once_again.cpp
@@ -0,0 +1,11 @@
+/*
+Task
+Write a function f (in C# F) that returns Hello, world! string. But there is one thing, you cannot use helowrd letters.
+
+The C# test cases ignore checking for banned letters in words class, string and public.
+The C++ test cases ignore checking for banned letters in words string, std and return.
+
+Have fun!
+
+PuzzlesRestricted
+*/
