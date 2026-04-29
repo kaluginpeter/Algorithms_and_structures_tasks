b46a9dd028bc1104b4cb9d8b15626bf663fd993f
kaluginpeter <streetworkout200412@gmail.com>
2026-04-29 23:18:21 +0300

    Added solution "3225. Maximum Score From Grid Operations"

--- LeetCode/Hard/3225._Maximum_Score_From_Grid_Operations.cpp
+++ LeetCode/Hard/3225._Maximum_Score_From_Grid_Operations.cpp
@@ -37,3 +37,48 @@ Constraints:
 n == grid[i].length
 0 <= grid[i][j] <= 109
 */
+// Solution
+// C++ O(N^3) O(N^3) DynamicProgramming
+class Solution {
+public:
+    long long maximumScore(vector<vector<int>>& grid) {
+        size_t n = grid[0].size();
+        if (n == 1) return 0;
+        vector<vector<std::vector<long long>>> dp(n, vector<std::vector<long long>>(n + 1, std::vector<long long>(n + 1, 0)));
+        vector<std::vector<long long>> prevMax(n + 1, std::vector<long long>(n + 1, 0));
+        vector<std::vector<long long>> prevSuffixMax(n + 1, std::vector<long long>(n + 1, 0));
+        vector<std::vector<long long>> colSum(n, std::vector<long long>(n + 1, 0));
+        for (int c = 0; c < n; ++c) {
+            for (int r = 1; r <= n; ++r) colSum[c][r] = colSum[c][r - 1] + grid[r - 1][c];
+        }
+        for (int i = 1; i < n; ++i) {
+            for (int currH = 0; currH <= n; ++currH) {
+                for (int prevH = 0; prevH <= n; ++prevH) {
+                    if (currH <= prevH) {
+                        long long extraScore = colSum[i][prevH] - colSum[i][currH];
+                        dp[i][currH][prevH] = std::max(dp[i][currH][prevH], prevSuffixMax[prevH][0] + extraScore);
+                    } else {
+                        long long extraScore = colSum[i - 1][currH] - colSum[i - 1][prevH];
+                        dp[i][currH][prevH] = std::max({dp[i][currH][prevH], prevSuffixMax[prevH][currH], prevMax[prevH][currH] + extraScore});
+                    }
+                }
+            }
+            for (int currH = 0; currH <= n; ++currH) {
+                prevMax[currH][0] = dp[i][currH][0];
+                for (int prevH = 1; prevH <= n; ++prevH) {
+                    long long penalty = (prevH > currH) ? (colSum[i][prevH] - colSum[i][currH]) : 0;
+                    prevMax[currH][prevH] = std::max(prevMax[currH][prevH - 1], dp[i][currH][prevH] - penalty);
+                }
+                prevSuffixMax[currH][n] = dp[i][currH][n];
+                for (int prevH = n - 1; prevH >= 0; --prevH) {
+                    prevSuffixMax[currH][prevH] = std::max(prevSuffixMax[currH][prevH + 1], dp[i][currH][prevH]);
+                }
+            }
+        }
+        long long output = 0;
+        for (int k = 0; k <= n; ++k) {
+            output = std::max({output, dp[n - 1][n][k], dp[n - 1][0][k]});
+        }
+        return output;
+    }
+};
