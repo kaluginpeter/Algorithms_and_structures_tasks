6896dbdc123e5f9a2ce1562379133fdded5af521
kaluginpeter <streetworkout200412@gmail.com>
2026-05-02 22:01:37 +0300

    Added description "Sum and Length"

---
+++ CodeWars/7kyu/Sum_and_Length.cpp
@@ -0,0 +1,15 @@
+/*
+Sum and Length
+In this kata you must return a string value, where the first part is the sum of positive numbers and the second part is the number of negative numbers.
+
+Knowing that the first 0 is negative, the second is positive, the third is negative, and so on...
+
+Examples
+[-1,2,3,4,0,1,0,-2,0,-3]
+
+==> '10 5'
+
+sum of positives: 10 = 2 + 3 + 4 + 0 (second) + 1
+count of negatives: 5 ( -1, 0 (first), -2, 0 (third), -3 )
+Fundamentals
+*/
