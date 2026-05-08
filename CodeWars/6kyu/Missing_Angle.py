368ca56f45cfb16310fac5fb66818b97fa174928
kaluginpeter <streetworkout200412@gmail.com>
2026-05-08 20:25:39 +0300

    Added description "Missing Angle"

---
+++ CodeWars/6kyu/Missing_Angle.py
@@ -0,0 +1,16 @@
+# Below is a right-angled triangle:
+#
+#   |\
+#   | \
+#   |  \
+#   |   \
+# o |    \ h
+#   |     \
+#   |    θ \
+#   |_______\
+#      a
+# Your challange is to write a function that calculates the angle θ in degrees. You will be given three arguments representing each side: o, h and a. One of the arguments equals zero. Use the length of the two other sides to calculate θ.
+#
+# Round the result to the nearest integer, except in C and JavaScript.
+#
+# AlgorithmsMathematicsGeometry
