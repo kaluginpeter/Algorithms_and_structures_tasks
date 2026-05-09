e8459fcca8e12f98858fcae534529a9ed8eb3cb1
kaluginpeter <streetworkout200412@gmail.com>
2026-05-09 21:15:36 +0300

    Added solution "1914. Cyclically Rotating a Grid"

--- LeetCode/Medium/1914._Cyclically_Rotating_a_Grid.cpp
+++ LeetCode/Medium/1914._Cyclically_Rotating_a_Grid.cpp
@@ -35,3 +35,40 @@ Both m and n are even integers.
 1 <= grid[i][j] <= 5000
 1 <= k <= 109
 */
+// Solution
+// C++ O(NM) O(N + M) Matrix
+class Solution {
+public:
+    vector<vector<int>> rotateGrid(vector<vector<int>>& grid, int k) {
+        size_t n = grid.size(), m = grid[0].size();
+        for (int start = 0; start < std::min(n, m) >> 1; ++start) {
+            std::vector<int> data;
+            for (size_t i = start; i < n - start; ++i) {
+                data.push_back(grid[i][start]);
+            }
+            for (size_t j = start + 1; j < m - start; ++j) {
+                data.push_back(grid[n - start - 1][j]);
+            }
+            for (int i = n - start - 2; i >= start; --i) {
+                data.push_back(grid[i][m - start - 1]);
+            }
+            for (int j = m - start - 2; j > start; --j) {
+                data.push_back(grid[start][j]);
+            }
+            size_t bound = data.size(), step = k % bound, idx = 0;
+            for (size_t i = start; i < n - start; ++i) {
+                grid[i][start] = (data[(idx++ + bound - step) % bound]);
+            }
+            for (size_t j = start + 1; j < m - start; ++j) {
+                grid[n - start - 1][j] = (data[(idx++ + bound - step) % bound]);
+            }
+            for (int i = n - start - 2; i >= start; --i) {
+                grid[i][m - start - 1] = (data[(idx++ + bound - step) % bound]);
+            }
+            for (int j = m - start - 2; j > start; --j) {
+                grid[start][j] = (data[(idx++ + bound - step) % bound]);
+            }
+        }
+        return grid;
+    }
+};
