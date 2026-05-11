9202a5425b78ce2d381d289c0cb164c95d2a4dfe
kaluginpeter <streetworkout200412@gmail.com>
2026-05-11 22:58:16 +0300

    Added solution "Subsets Parity - Extended"

--- CodeWars/5kyu/Subsets_Parity_Extended.py
+++ CodeWars/5kyu/Subsets_Parity_Extended.py
@@ -106,3 +106,38 @@
 # This problem is an extension of myjinxin2015's Sub Sets Parity kata.
 #
 # Number TheoryPerformance
+# Solution
+from math import isqrt
+
+
+def factorize(x):
+    factors = {}
+    while x % 2 == 0:
+        factors[2] = factors.get(2, 0) + 1
+        x //= 2
+    f = 3
+    while f * f <= x:
+        while x % f == 0:
+            factors[f] = factors.get(f, 0) + 1
+            x //= f
+        f += 2
+    if x > 1: factors[x] = factors.get(x, 0) + 1
+    return factors
+
+def vp_factorial(n, p):
+    s = 0
+    while n:
+        n //= p
+        s += n
+    return s
+def subset_divisibility(n: int, k: int, d: int) -> bool:
+    factors = factorize(d)
+    for p, need in factors.items():
+        have = (
+            vp_factorial(n, p)
+            - vp_factorial(k, p)
+            - vp_factorial(n - k, p)
+        )
+
+        if have < need: return False
+    return True
