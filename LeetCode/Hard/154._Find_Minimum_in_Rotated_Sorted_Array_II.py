4f2aa5d2e9c30b4b09c815a6d017a8f2037c94fc
kaluginpeter <streetworkout200412@gmail.com>
2026-05-16 22:57:13 +0300

    Added solution "154. Find Minimum in Rotated Sorted Array II"

--- LeetCode/Hard/154._Find_Minimum_in_Rotated_Sorted_Array_II.py
+++ LeetCode/Hard/154._Find_Minimum_in_Rotated_Sorted_Array_II.py
@@ -66,3 +66,28 @@ public:
         return output;
     }
 };
+
+
+# C++ O(N) O(1) BinarySearch
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int n = nums.size() - 1;
+        int last = nums[n];
+        int left = 0, right = n;
+
+        while (left < n && nums[left] == last)
+            left++;
+
+        while (left < right) {
+            int mid = left + right >> 1;
+
+            if (nums[mid] > last)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+
+        return nums[left];
+    }
+};
