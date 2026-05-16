11d587f2c8e5fa3063e39f33f2099b1d817d1037
kaluginpeter <streetworkout200412@gmail.com>
2026-05-16 22:50:10 +0300

    Added description "Float Stringification"

---
+++ CodeWars/7kyu/Float_Stringification.cpp
@@ -0,0 +1,7 @@
+/*
+The function is supposed to convert a floating-point number to a string, but it is not working correctly. Can you figure out why ?
+
+Converting the string back to a floating-point number should yield the original number (this is called a round-trip).
+
+DebuggingStrings
+*/
