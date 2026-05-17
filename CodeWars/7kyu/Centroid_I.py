4489f914d50224abe44b239ca99b8028c94aedad
kaluginpeter <streetworkout200412@gmail.com>
2026-05-17 20:28:36 +0300

    Added solution "Centroid I"

--- CodeWars/7kyu/Centroid_I.py
+++ CodeWars/7kyu/Centroid_I.py
@@ -3,3 +3,10 @@
 # Return the results as a list of 3 floats.
 #
 # Algorithms
+# Solution
+def centroid(points: list[list[float]]) -> list[float]:
+    n = len(points)
+    x = sum(p[0] for p in points) / n
+    y = sum(p[1] for p in points) / n
+    z = sum(p[2] for p in points) / n
+    return [x, y, z]
