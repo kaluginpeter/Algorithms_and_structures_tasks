49862c6f42fde5592b216a9f9cfe801ea57ffa8e
kaluginpeter <streetworkout200412@gmail.com>
2026-05-19 15:32:54 +0300

    Added description "Number of integer partitions with distinct parts"

---
+++ CodeWars/5kyu/Number_of_integer_partitions_with_distinct_parts.py
@@ -0,0 +1,10 @@
+# This Kata is based on Number of integer partitions.
+#
+# An integer partition with distinct parts of n is a decreasing list of positive integers which sum to n, in which no number occurs more than once.
+#
+# For example, there are 3 integer partitions of 5:
+#
+# [5], [4,1], [3,2].
+# Write a function which returns the number of integer partition with distinct parts of n (1 <= n <= 600).
+#
+# MathematicsAlgorithmsDiscrete Mathematics
