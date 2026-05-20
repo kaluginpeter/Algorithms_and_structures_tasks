04da4238d20de39139da4f8d6f8956a93c2045d0
kaluginpeter <streetworkout200412@gmail.com>
2026-05-20 11:48:17 +0300

    Added description "Snail Length"

---
+++ CodeWars/6kyu/Snail_Length.py
@@ -0,0 +1,57 @@
+# This kata was inspired by this kata. You do not need either kata to solve the other.
+#
+# Task
+# The figure below shows the snail coordinates. The sequence of its points begins as follows: (0, 0), (1, 0), (1, 1), (-1, 1), ....Bpgn7st.md.png
+#
+# The spiral:
+#
+# starts by moving right,
+# then continues counterclockwise,
+# expanding outward forever.
+# Given the coordinates (x, y), return the length of the line connecting the coordinate to the origin (0,0) following this snail-like path.
+#
+# Examples
+# snail_length(-2, -1) => 19
+# Explanation:
+#
+# The spiral reaches (-2,-1) after completing two full spirals, shown in the image above.
+#
+# The value at (-2,-1) is 19.
+#
+# snail_length(8,10) => 382
+# Explanation:
+#
+# The spiral fills points in expanding square layers around the origin.
+#
+# By the time the spiral reaches the outer boundary containing (8,10), all previous layers are already filled.
+#
+# From the structure of the outer ring, (10,10) is reached first, and (8,10) is a few steps back along the same edge, giving a final value of 382.
+#
+# Constraints
+# x
+# ,
+# y
+# ∈
+# [
+# −
+# 5
+# ∗
+# 1
+# 0
+# 4
+# ,
+# 5
+# ∗
+# 1
+# 0
+# 4
+# ]
+# x,y∈[−5∗10
+# 4
+#  ,5∗10
+# 4
+#  ]
+#
+# A naive solution that simply walks the line from (0,0) to (x,y) will NOT pass. You have to think of a faster solution.
+#
+# AlgorithmsMathematics
