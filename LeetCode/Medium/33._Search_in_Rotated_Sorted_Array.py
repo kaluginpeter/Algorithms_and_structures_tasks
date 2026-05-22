3278b9fd9f41a73e432f3e8f27c4279b66e1ade2
kaluginpeter <streetworkout200412@gmail.com>
2026-05-22 19:46:58 +0300

    Added solution "33. Search in Rotated Sorted Array"

--- LeetCode/Medium/33._Search_in_Rotated_Sorted_Array.py
+++ LeetCode/Medium/33._Search_in_Rotated_Sorted_Array.py
@@ -81,3 +81,44 @@ class Solution:
                 else:
                     left = mid + 1
         return -1
+
+
+# Python O(logN) O(1) BinarySearch
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            middle = (left + right) // 2
+            if nums[middle] == target:
+                return middle
+            if nums[middle] < nums[right]:
+                if nums[middle] < target <= nums[right]:
+                    left = middle + 1
+                else:
+                    right = middle - 1
+            else:
+                if nums[left] <= target < nums[middle]:
+                    right = middle - 1
+                else:
+                    left = middle + 1
+        return -1
+
+# C++ O(logN) O(1) BinarySearch
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+        while (left <= right) {
+            int middle = left + ((right - left) >> 1);
+            if (nums[middle] == target) return middle;
+            else if (nums[middle] < nums[right]) {
+                if (nums[middle] < target && target <= nums[right]) left = middle + 1;
+                else right = middle - 1;
+            } else {
+                if (nums[left] <= target && target < nums[middle]) right = middle - 1;
+                else left = middle + 1;
+            }
+        }
+        return -1;
+    }
+};
