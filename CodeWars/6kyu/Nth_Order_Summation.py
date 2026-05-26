dde21836d3c00848c358b77cd9101be081ea2c1f
kaluginpeter <streetworkout200412@gmail.com>
2026-05-26 18:44:50 +0300

    Added description "Nth Order Summation"

---
+++ CodeWars/6kyu/Nth_Order_Summation.py
@@ -0,0 +1,70 @@
+# Task
+# Consider a series of functions
+# S
+# m
+# (
+# n
+# )
+# S
+# m
+# ​
+#  (n) where:
+#
+# S
+# 0
+# (
+# n
+# )
+# =
+# 1
+# S
+# 0
+# ​
+#  (n)=1
+# S
+# m
+# +
+# 1
+# (
+# n
+# )
+# =
+# ∑
+# k
+# =
+# 1
+# n
+# S
+# m
+# (
+# k
+# )
+# S
+# m+1
+# ​
+#  (n)=
+# k=1
+# ∑
+# n
+# ​
+#  S
+# m
+# ​
+#  (k)
+#
+# Write a function s which takes two integer arguments m and n and returns the value defined by
+# S
+# m
+# (
+# n
+# )
+# S
+# m
+# ​
+#  (n).
+#
+# Inputs
+# 0 <= m <= 100
+# 1 <= n <= 10**100
+#
+# MathematicsPerformance
