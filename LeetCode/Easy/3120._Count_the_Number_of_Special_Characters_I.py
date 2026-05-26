61d5a6a9773626f6400fce7894330724e2fb45ef
kaluginpeter <streetworkout200412@gmail.com>
2026-05-26 18:47:55 +0300

    Added solution "3120. Count the Number of Special Characters I"

--- LeetCode/Easy/3120._Count_the_Number_of_Special_Characters_I.py
+++ LeetCode/Easy/3120._Count_the_Number_of_Special_Characters_I.py
@@ -51,3 +51,32 @@ class Solution:
             if i.lower() in ht and i.upper() in ht:
                 ans += 1
         return ans // 2
+
+
+# Python O(N) O(1) HashMap
+class Solution:
+    def numberOfSpecialChars(self, word: str) -> int:
+        lower: int = 0
+        upper: int = 0
+        for ch in word:
+            if ch.isupper(): upper |= 1 << (ord(ch) - 65)
+            else: lower |= 1 << (ord(ch) - 97)
+        return sum(bool(lower & (1 << bit)) and bool(upper & (1 << bit)) for bit in range(26))
+
+# C++ O(N) O(1) HashMap
+class Solution {
+public:
+    int numberOfSpecialChars(string word) {
+        uint32_t lower = 0, upper = 0;
+        for (char& ch : word) {
+            if (std::islower(ch)) lower |= 1 << (ch - 'a');
+            else upper |= 1 << (ch - 'A');
+        }
+        size_t output = 0;
+        for (size_t i = 0; i < 26; ++i) {
+            uint32_t bit = 1 << i;
+            if ((lower & bit) && (upper & bit)) ++output;
+        }
+        return output;
+    }
+};
