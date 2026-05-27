b03201f6c9b08b51a5dd36b359211ad979871776
kaluginpeter <streetworkout200412@gmail.com>
2026-05-27 22:17:53 +0300

    Added solution "3121. Count the Number of Special Characters II"

--- LeetCode/Medium/3121._Count_the_Number_of_Special_Characters_II.py
+++ LeetCode/Medium/3121._Count_the_Number_of_Special_Characters_II.py
@@ -54,3 +54,38 @@ class Solution:
             if (i.lower() in ht and i.upper() in ht) and ht[i.lower()] < ht[i.upper()]:
                 ans += 1
         return ans // 2
+
+
+# Python O(N) O(D) HashMap
+class Solution:
+    def numberOfSpecialChars(self, word: str) -> int:
+        lower: list[int] = [-1] * 26
+        upper: list[int] = [-1] * 26
+        for i in range(len(word)):
+            if word[i].islower(): lower[ord(word[i]) - 97] = i
+            elif upper[ord(word[i]) - 65] == -1: upper[ord(word[i]) - 65] = i
+        output: int = 0
+        for i in range(26):
+            if lower[i] != -1 and upper[i] != -1 and lower[i] < upper[i]:
+                output += 1
+        return output
+
+# C++ O(N) O(D) HashMap
+class Solution {
+public:
+    int numberOfSpecialChars(string word) {
+        std::array<int, 26> lower{}, upper{};
+        for (size_t i = 0; i < 26; ++i) {
+            lower[i] = -1; upper[i] = -1;
+        }
+        for (size_t i = 0; i < word.size(); ++i) {
+            if (std::islower(word[i])) lower[word[i] - 'a'] = i;
+            else if (upper[word[i] - 'A'] == -1) upper[word[i] - 'A'] = i;
+        }
+        int output = 0;
+        for (size_t i = 0; i < 26; ++i) {
+            if (lower[i] != -1 && upper[i] != -1 && lower[i] < upper[i]) ++output;
+        }
+        return output;
+    }
+};
