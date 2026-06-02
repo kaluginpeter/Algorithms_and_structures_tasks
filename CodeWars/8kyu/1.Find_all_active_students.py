b690c71a484b28ceb96be7917427bf13d76302f1
kaluginpeter <streetworkout200412@gmail.com>
2026-06-02 21:15:47 +0300

    Added description "1. Find all active students"

---
+++ CodeWars/8kyu/1.Find_all_active_students.py
@@ -0,0 +1,9 @@
+# Write a simple query to display student information of all active students.
+#
+# TABLE STRUCTURE:
+#
+# students
+# id (integer)	firstname (text)	lastname (text)	isactive (boolean)
+# Sort the results by increasing order of id.
+#
+# DatabasesSQL
