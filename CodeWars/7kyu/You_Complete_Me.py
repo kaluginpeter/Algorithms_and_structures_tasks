87c1f04da050402888f2bf6961c8e8751ffc49c7
kaluginpeter <streetworkout200412@gmail.com>
2026-06-07 21:26:15 +0300

    Added description "You Complete Me"

---
+++ CodeWars/7kyu/You_Complete_Me.py
@@ -0,0 +1,27 @@
+# Task: concatenate one or more letters to the end of a given input string to create a string that reads the same forward as backward.
+#
+# Specifications
+#
+# Letters can only be added to the right hand side of the input string.
+#
+# You are required to add at least one letter to the input string.
+#
+# The shorter return string is the correct one. ie.. (ab)=> abba is Wrong, while (ab)=> aba is Right.
+#
+# Inputs will consist of one or more letters, upper and lower case.
+#
+# Strings are case sensitive. ie..(Gn)=> gng is Wrong, while (Gn)=> GnG is right.
+#
+# Examples:
+#
+# (a) => a + a => aa
+#
+# (GG) => GG + G => GGG
+#
+# (Ab) => Ab + A => AbA
+#
+# (aba) => aba + ba => ababa
+#
+# (aab) => aab + aa => aabaa
+#
+# StringsAlgorithmsFundamentalsLogic
