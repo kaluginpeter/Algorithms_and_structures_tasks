8ec1016cb4ff642e95b2583501dbca4363295e03
kaluginpeter <streetworkout200412@gmail.com>
2026-06-08 15:48:01 +0300

    Added solution "Going backwards: Number from every possible sum of two digits"

--- CodeWars/6kyu/Going_backwards_Number_from_every_possible_sum_of_two_digits.py
+++ CodeWars/6kyu/Going_backwards_Number_from_every_possible_sum_of_two_digits.py
@@ -12,3 +12,19 @@
 #
 # ( if there is more than one possibility, just return any one of them )
 # Algorithms
+# Solution
+from math import isqrt
+
+def find_number(sums):
+    m = len(sums)
+    if m == 0: return 0
+    if m == 1:
+        s = sums[0]
+        a = min(9, s)
+        b = s - a
+        return int(f"{a}{b}")
+    n = (1 + isqrt(1 + 8 * m)) // 2
+    d1 = (sums[0] + sums[1] - sums[n - 1]) // 2
+    digits = [d1]
+    for i in range(n - 1): digits.append(sums[i] - d1)
+    return int("".join(map(str, digits)))
