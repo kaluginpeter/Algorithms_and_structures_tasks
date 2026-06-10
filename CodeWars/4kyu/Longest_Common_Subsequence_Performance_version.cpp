bb312fb1ee538d1cd1f8e6ccba042006f208c21c
kaluginpeter <streetworkout200412@gmail.com>
2026-06-10 21:19:40 +0300

    Added solution "Longest Common Subsequence (Performance version)"

--- CodeWars/4kyu/Longest_Common_Subsequence_Performance_version.cpp
+++ CodeWars/4kyu/Longest_Common_Subsequence_Performance_version.cpp
@@ -27,3 +27,36 @@ Wikipedia has an article that may be helpful.
 
 PerformanceStringsDynamic ProgrammingMemoizationAlgorithms
 */
+// Solution
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+string lcs(const string& x, const string& y)
+{
+    int n = x.size();
+    int m = y.size();
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= m; ++j) {
+            if (x[i - 1] == y[j - 1]) dp[i][j] = dp[i - 1][j - 1] + 1;
+            else dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+    string res;
+    int i = n;
+    int j = m;
+    while (i > 0 && j > 0) {
+        if (x[i - 1] == y[j - 1]) {
+            res.push_back(x[i - 1]);
+            --i;
+            --j;
+        } else if (dp[i - 1][j] >= dp[i][j - 1]) --i;
+        else --j;
+    }
+
+    reverse(res.begin(), res.end());
+    return res;
+}
