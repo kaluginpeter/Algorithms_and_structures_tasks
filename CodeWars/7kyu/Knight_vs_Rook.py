f19444e46ac684001caaee6d70ceff367ccc2674
kaluginpeter <streetworkout200412@gmail.com>
2026-06-13 20:52:52 +0300

    Added description "Knight vs Rook"

---
+++ CodeWars/7kyu/Knight_vs_Rook.py
@@ -0,0 +1,11 @@
+# Knight vs Rook
+# If you are not familiar with chess game you can learn more here.
+#
+# You will be provided with a knight's and a rook's positions on the board. Implement a function that tells us which piece can capture the other:
+#
+# "Rook" if the rook captures the knight
+# "Knight" if the knight captures the rook
+# "None" otherwise
+# Check the test cases and Happy coding :)
+#
+# Fundamentals
