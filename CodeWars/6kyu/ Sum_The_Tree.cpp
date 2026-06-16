8e70b01e1bded97c8cd4ea9b581d236d6ed09f19
kaluginpeter <streetworkout200412@gmail.com>
2026-06-16 23:16:57 +0300

    Added description "Sum The Tree"

---
+++ CodeWars/6kyu/ Sum_The_Tree.cpp
@@ -0,0 +1,26 @@
+/*
+Given a node object representing a binary tree:
+
+struct node
+{
+  int value;
+  node* left;
+  node* right;
+}
+write a function that returns the sum of all values, including the root. Absence of a node will be indicated with a null value.
+
+Examples:
+
+10
+| \
+1  2
+=> 13
+
+1
+| \
+0  0
+    \
+     2
+=> 3
+TreesRecursionBinary TreesBinary Search TreesData StructuresFundamentals
+*/
