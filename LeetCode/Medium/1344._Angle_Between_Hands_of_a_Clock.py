8afea6912aa8493b79a04c1c21a026dc168dd54b
kaluginpeter <streetworkout200412@gmail.com>
2026-06-18 22:50:36 +0300

    Added solution "1344. Angle Between Hands of a Clock"

--- LeetCode/Medium/1344._Angle_Between_Hands_of_a_Clock.py
+++ LeetCode/Medium/1344._Angle_Between_Hands_of_a_Clock.py
@@ -25,3 +25,18 @@
 #
 # 1 <= hour <= 12
 # 0 <= minutes <= 59
+# Solution
+# Python O(1) O(1) Math
+class Solution:
+    def angleClock(self, hour: int, minutes: int) -> float:
+        return min((diff:=(11 * (hour + minutes / 60)) % 12), 12 - diff) * 30
+
+# C++ O(1) O(1) Math
+class Solution {
+public:
+    double angleClock(int hour, int minutes) {
+        double x = hour + minutes / 60.0;
+        double diff = std::fmod(11.0 * x, 12.0);
+        return std::min(diff, 12.0 - diff) * 30.0;
+    }
+};
