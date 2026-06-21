76c9272b6c35bd3c796db354765360edeabddaed
kaluginpeter <streetworkout200412@gmail.com>
2026-06-21 22:14:47 +0300

    Added solutiokn "Guess Who?"

--- CodeWars/6kyu/Guess_Who.py
+++ CodeWars/6kyu/Guess_Who.py
@@ -113,3 +113,64 @@
 # Car Park Escape
 #
 # GamesFundamentals
+# Solution
+class GuessWho():
+
+    def __init__(self, character):
+        self.characteristic = [
+            ["Jean-Claude", ["Male", "Glasses", "Brown eyes", "Bald", "White hair", "Small mouth", "Small nose"]],
+            ["Pierre", ["Male", "Mustache", "Brown eyes", "Brown hair", "Big mouth", "Small nose"]],
+            ["Jean", ["Male", "White hair", "Big nose", "Big mouth", "Blue eyes"]],
+            ["Amelie", ["Female", "Hat", "Brown hair", "Small mouth", "Long hair", "Brown eyes", "Small nose"]],
+            ["Mirabelle", ["Female", "Black hair", "Earrings", "Small mouth", "Brown eyes", "Big nose"]],
+            ["Isabelle", ["Female", "Blonde hair", "Glasses", "Hat", "Small mouth", "Small nose", "Brown eyes"]],
+            ["Antonin", ["Male", "Brown eyes", "Black hair", "Small nose", "Big mouth"]],
+            ["Bernard", ["Male", "Brown eyes", "Brown hair", "Small nose", "Hat"]],
+            ["Owen", ["Male", "Blue eyes", "Blonde hair", "Small nose", "Small mouth"]],
+            ["Dylan", ["Male", "Brown eyes", "Blonde hair", "Small nose", "Small mouth", "Bald", "Beard"]],
+            ["Herbert", ["Male", "Brown eyes", "Blonde hair", "Big nose", "Small mouth", "Bald"]],
+            ["Christine", ["Female", "Blue eyes", "Blonde hair", "Small nose", "Small mouth", "Long hair"]],
+            ["Luc", ["Male", "Brown eyes", "White hair", "Small nose", "Small mouth", "Glasses"]],
+            ["Cecilian", ["Male", "Brown eyes", "Ginger hair", "Small nose", "Small mouth"]],
+            ["Lionel", ["Male", "Brown eyes", "Brown hair", "Big nose", "Big mouth", "Mustache"]],
+            ["Benoit", ["Male", "Brown eyes", "Brown hair", "Small mouth", "Small nose", "Mustache", "Beard"]],
+            ["Robert", ["Male", "Blue eyes", "Brown hair", "Big nose", "Big mouth"]],
+            ["Charline", ["Female", "Brown hair", "White hair", "Small nose", "Big mouth"]],
+            ["Renaud", ["Male", "Brown eyes", "Blonde hair", "Small nose", "Big mouth", "Mustache"]],
+            ["Michel", ["Male", "Brown eyes", "Blonde hair", "Small nose", "Big mouth", "Beard"]],
+            ["Pierre-Louis", ["Male", "Blue eyes", "Brown hair", "Small nose", "Small mouth", "Bald", "Glasses"]],
+            ["Etienne", ["Male", "Brown eyes", "Blonde hair", "Small nose", "Small mouth", "Glasses"]],
+            ["Henri", ["Male", "Brown eyes", "White hair", "Small nose", "Big mouth", "Hat"]],
+            ["Damien", ["Male", "Brown eyes", "Blonde hair", "Small nose", "Big mouth", "Hat"]]]
+
+        self.secret = character
+        self.turns = 0
+        self.possible = self.characteristic[:]
+
+        self.lookup = {
+            name: set(attrs)
+            for name, attrs in self.characteristic
+        }
+
+    def guess(self, guess):
+        self.turns += 1
+        if guess == self.secret: return [f"Correct! in {self.turns} turns"]
+        if guess in self.lookup:
+            self.possible = [
+                item for item in self.possible
+                if item[0] != guess
+            ]
+        else:
+            has_trait = guess in self.lookup[self.secret]
+            if has_trait:
+                self.possible = [
+                    item for item in self.possible
+                    if guess in item[1]
+                ]
+            else:
+                self.possible = [
+                    item for item in self.possible
+                    if guess not in item[1]
+                ]
+
+        return [name for name, _ in self.possible]
