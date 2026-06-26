9c832c621e9b662bf79e957428a8972b2d1b451b
kaluginpeter <streetworkout200412@gmail.com>
2026-06-26 16:13:02 +0300

    Added description "Maxie and Minnie"

---
+++ CodeWars/7kyu/Maxie_and_Minnie.py
@@ -0,0 +1,8 @@
+# Maxie is the largest number that can be obtained by swapping two digits of an integer, Minnie is the smallest number. Create a function that takes an integer and returns Maxie and Minnie. Leading zeroes are not permitted. All test cases are positive integers of three digits or more. Sometimes no swaps are needed.
+#
+# Examples:
+# swap(12340) → (42310,10342)
+# swap(98761) → (98761,18769)>>No swap needed for Maxie.
+# swap(9000) → (9000,9000)>>No swaps allowed.
+# swap(90888) → (98880,80889)
+# PuzzlesMathematics
