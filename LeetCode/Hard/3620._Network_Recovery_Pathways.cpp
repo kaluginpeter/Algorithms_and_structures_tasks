316d4bbc8af0b8db12313347c22f9f807623afe7
kaluginpeter <streetworkout200412@gmail.com>
2026-07-03 20:49:53 +0300

    Added solution "3620. Network Recovery Pathways"

--- LeetCode/Hard/3620._Network_Recovery_Pathways.cpp
+++ LeetCode/Hard/3620._Network_Recovery_Pathways.cpp
@@ -84,3 +84,55 @@ ui != vi
 online[i] is either true or false, and both online[0] and online[n − 1] are true.
 The given graph is a directed acyclic graph.
 */
+// Solution
+// C++ O(log2(10^9)VlogE) O(V + E) BinarySearch ShortestPath
+class Solution {
+public:
+    int findMaxPathScore(vector<vector<int>>& edges, vector<bool>& online,
+                         long long k) {
+        int n = online.size();
+        vector<vector<pair<int, int>>> g(n);
+        int l = INT_MAX, r = 0;
+        for (auto& edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            int w = edge[2];
+            if (!online[u] || !online[v]) continue;
+            g[u].push_back({v, w});
+            l = min(l, w);
+            r = max(r, w);
+        }
+
+        auto check = [&](int mid) -> bool {
+            vector<long long> dis(n, LLONG_MAX);
+            priority_queue<pair<long long, int>, vector<pair<long long, int>>,
+                           greater<>>
+                q;
+            dis[0] = 0;
+            q.push({0, 0});
+            while (!q.empty()) {
+                auto [d, u] = q.top();
+                q.pop();
+                if (d > k) return false;
+                if (u == n - 1) return true;
+                if (d > dis[u]) continue;
+                for (auto& [v, w] : g[u]) {
+                    if (w < mid) continue;
+                    if (dis[v] > dis[u] + w) {
+                        dis[v] = dis[u] + w;
+                        q.push({dis[v], v});
+                    }
+                }
+            }
+            return false;
+        };
+
+        if (!check(l)) return -1;
+        while (l <= r) {
+            int mid = l + ((r - l) >> 1);
+            if (check(mid)) l = mid + 1;
+            else r = mid - 1;
+        }
+        return r;
+    }
+};
