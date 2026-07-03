4a62898fd8e443469124cfcd58d73c5c4bc67e3e
kaluginpeter <streetworkout200412@gmail.com>
2026-07-03 20:45:02 +0300

    Added descirpiton "Create palindrome"

---
+++ CodeWars/7kyu/Create_palindrome.cpp
@@ -0,0 +1,19 @@
+/*
+Consider the string "adfa" and the following rules:
+
+a) each character MUST be changed either to the one before or the one after in alphabet.
+b) "a" can only be changed to "b" and "z" to "y".
+From our string, we get:
+
+"adfa" -> ["begb","beeb","bcgb","bceb"]
+
+Here is another example:
+"bd" -> ["ae","ac","ce","cc"]
+
+--We see that in each example, one of the outcomes is a palindrome. That is, "beeb" and "cc".
+You will be given a lowercase string and your task is to return True if at least one of the outcomes is a palindrome or False otherwise.
+
+More examples in test cases. Good luck!
+
+Algorithms
+*/
