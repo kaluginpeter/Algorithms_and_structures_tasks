f326dfc375db7132a8fb95cffb24181a608fc23a
kaluginpeter <streetworkout200412@gmail.com>
2026-07-04 23:05:38 +0300

    Added solution "2492. Minimum Score of a Path Between Two Cities"

--- LeetCode/Medium/2492._Minimum_Score_of_a_Path_Between_Two_Cities.cpp
+++ LeetCode/Medium/2492._Minimum_Score_of_a_Path_Between_Two_Cities.cpp
@@ -38,3 +38,28 @@ ai != bi
 There are no repeated edges.
 There is at least one path between 1 and n.
 */
+// Solution
+// C++ O(NM) O(N + M) DepthFirstSearch
+class Solution
+{
+public:
+    int minScore(int n, vector<vector<int>>& roads)
+    {
+        unordered_map<int,unordered_map<int,int>> graph;
+        for (auto r : roads) graph[r[0]][r[1]] = graph[r[1]][r[0]] = r[2];
+        unordered_set<int> vis;
+        function<int(int)> dfs;
+        dfs = [&](int n) -> int
+        {
+            int res = INT_MAX;
+            vis.insert(n);
+            for (auto[adj, scr] : graph[n])
+            {
+                if (!vis.count(adj)) res = min(res, dfs(adj));
+                res = min(res, scr);
+            }
+            return res;
+        };
+        return dfs(1);
+    }
+};
