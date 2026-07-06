a3b0fcc6b7d4154df16e4bcb89bda131a30b591d
kaluginpeter <streetworkout200412@gmail.com>
2026-07-06 17:18:05 +0300

    Added description "Simple Fun #358: Vertical Histogram Of Letters"

---
+++ CodeWars/5kyu/Simple_Fun_358_Vertical_Histogram_Of_Letters.py
@@ -0,0 +1,21 @@
+# Task
+# You are given a string s. Your task is to count the number of each letter (A-Z), and make a vertical histogram as result. Look at the following examples to understand the rules.
+#
+# Example
+# For s = "XXY YY ZZZ123ZZZ AAA BB C", the output should be:
+#
+#           *
+#           *
+#           *
+# *       * *
+# * *   * * *
+# * * * * * *
+# A B C X Y Z
+# Rules
+# You just need to count the uppercase letters. Any other character will be ignored.
+# Using * to represent the number of characters.
+# The order of output is form A to Z. Characters that do not appear in the string are ignored.
+# To beautify the histogram, there is a space between every pair of columns.
+# There are no extra spaces at the end of each row. Also, use "\n" to separate rows.
+# Happy Coding ^_^
+# FundamentalsData Visualization
