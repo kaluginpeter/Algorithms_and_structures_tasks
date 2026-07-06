8e26da88c062b738866292b9301bccc89d7ec6e0
kaluginpeter <streetworkout200412@gmail.com>
2026-07-06 17:18:37 +0300

    Added solution "Simple Fun #358: Vertical Histogram Of Letters"

--- CodeWars/5kyu/Simple_Fun_358_Vertical_Histogram_Of_Letters.py
+++ CodeWars/5kyu/Simple_Fun_358_Vertical_Histogram_Of_Letters.py
@@ -19,3 +19,21 @@
 # There are no extra spaces at the end of each row. Also, use "\n" to separate rows.
 # Happy Coding ^_^
 # FundamentalsData Visualization
+# Solution
+from collections import Counter
+
+def vertical_histogram_of(s: str) -> str:
+    cnt = Counter(c for c in s if 'A' <= c <= 'Z')
+
+    letters = [c for c in "ABCDEFGHIJKLMNOPQRSTUVWXYZ" if cnt[c] > 0]
+    if not letters: return ""
+
+    h = max(cnt[c] for c in letters)
+    rows = []
+
+    for level in range(h, 0, -1):
+        row = ["*" if cnt[c] >= level else " " for c in letters]
+        rows.append(" ".join(row).rstrip())
+
+    rows.append(" ".join(letters))
+    return "\n".join(rows)
