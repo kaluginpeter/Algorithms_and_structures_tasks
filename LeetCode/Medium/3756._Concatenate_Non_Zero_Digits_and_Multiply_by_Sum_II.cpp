9028c890bc90cd324435d41e983d857a87c51240
kaluginpeter <streetworkout200412@gmail.com>
2026-07-08 18:52:49 +0300

    Added solution "3756. Concatenate Non-Zero Digits and Multiply by Sum II"

--- LeetCode/Medium/3756._Concatenate_Non_Zero_Digits_and_Multiply_by_Sum_II.cpp
+++ LeetCode/Medium/3756._Concatenate_Non_Zero_Digits_and_Multiply_by_Sum_II.cpp
@@ -70,3 +70,42 @@ s consists of digits only.
 queries[i] = [li, ri]
 0 <= li <= ri < m
 */
+// Solution
+// C++ O(N) O(N) Prefix Math
+const int MOD = 1e9 + 7;
+const int MAX_N = 100001;
+long long pow10[MAX_N];
+
+int init = []() {
+    pow10[0] = 1;
+    for (int i = 1; i < MAX_N; ++i) {
+        pow10[i] = (pow10[i - 1] * 10) % MOD;
+    }
+    return 0;
+}();
+
+class Solution {
+public:
+    vector<int> sumAndMultiply(string s, vector<vector<int>>& queries) {
+        size_t n = s.size();
+        std::vector<int> sum(n + 1, 0);
+        std::vector<long long> x(n + 1, 0);
+        std::vector<int> cnt(n + 1, 0);
+        for (int i = 0; i < n; ++i) {
+            uint8_t d = s[i] - '0';
+            sum[i + 1] = sum[i] + d;
+            x[i + 1] = (d > 0) ? (x[i] * 10 + d) % MOD : x[i];
+            cnt[i + 1] = cnt[i] + (d > 0);
+        }
+        size_t m = queries.size();
+        std::vector<int> output(m, 0);
+        for (size_t i = 0; i < m; ++i) {
+            int l = queries[i][0], r = queries[i][1] + 1;
+            int length = cnt[r] - cnt[l];
+            long long val_x = (x[r] - x[l] * pow10[length] % MOD + MOD) % MOD;
+            long long val_sum = sum[r] - sum[l];
+            output[i] = (val_x * val_sum) % MOD;
+        }
+        return output;
+    }
+};
