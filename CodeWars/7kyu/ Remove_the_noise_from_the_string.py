59a6097f14743f752ecf71b9331f733a65ced292
kaluginpeter <streetworkout200412@gmail.com>
2026-07-09 18:41:14 +0300

    Added description "Remove the noise from the string"

---
+++ CodeWars/7kyu/ Remove_the_noise_from_the_string.py
@@ -0,0 +1,11 @@
+# We have a broken message machine that introduces noise to our incoming messages. We know that our messages can't be written using %$&/#·@|º\ª. Unfortunately our machine introduces noise, which means that our message arrives with characters like: %$&/#·@|º\ª within our original message.
+#
+# Your mission is to write a function which removes this noise from the message.
+#
+# Notice that noise can only be %$&/#·@|º\ª characters, other characters are not considered to be noise
+#
+# For example:
+#
+# remove_noise("h%e&·%$·llo w&%or&$l·$%d")
+# # returns hello world
+# Regular ExpressionsFundamentals
