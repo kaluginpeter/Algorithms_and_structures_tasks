c864d4ef98209e391e316c53e7b7874bc7c53f3a
kaluginpeter <streetworkout200412@gmail.com>
2026-07-10 18:41:41 +0300

    Added solution "Next polydivisible number"

--- CodeWars/6kyu/Next_polydivisible_number.py
+++ CodeWars/6kyu/Next_polydivisible_number.py
@@ -3,3 +3,22 @@
 # A number is polydivisible if its first digit is cleanly divisible by 1, its first two digits by 2, its first three by 3, and so on. There are finitely many polydivisible numbers.
 #
 # Algorithms
+# Solution
+from bisect import bisect_right
+
+nums = []
+
+def dfs(prefix, length):
+    if length > 25: return
+    if length: nums.append(prefix)
+    for d in range(10):
+        if length == 0 and d == 0: continue
+        x = prefix * 10 + d
+        if x % (length + 1) == 0: dfs(x, length + 1)
+
+dfs(0, 0)
+nums.sort()
+def next_num(n):
+    i = bisect_right(nums, n)
+    if i == len(nums): return None
+    return nums[i]
