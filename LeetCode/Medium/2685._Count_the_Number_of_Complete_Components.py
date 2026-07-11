3cfe0aee66711b83266d77e0a89e46436911a88c
kaluginpeter <streetworkout200412@gmail.com>
2026-07-11 20:06:54 +0300

    Added solution "2685. Count the Number of Complete Components"

--- LeetCode/Medium/2685._Count_the_Number_of_Complete_Components.py
+++ LeetCode/Medium/2685._Count_the_Number_of_Complete_Components.py
@@ -118,3 +118,109 @@ public:
         return completeComponents;
     }
 };
+
+# Python O(V^2 + V + E) O(V + E) DSU
+class Solution:
+    def dfs(self, start: int, adj_list: list[list[int]], components: list[int], component: int) -> int:
+        components[start] = component
+        edges: int = 1
+        q: list[int] = [start]
+        while q:
+            vertex: int = q.pop()
+            for neighbor in adj_list[vertex]:
+                if components[neighbor] == -1:
+                    edges += 1
+                    components[neighbor] = component
+                    q.append(neighbor)
+        return edges
+
+    def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
+        adj_list: list[list[int]] = [[] for _ in range(n)]
+        for u, v in edges:
+            adj_list[u].append(v)
+            adj_list[v].append(u)
+        components: list[int] = [-1] * n
+        components_edges: list[int] = []
+        component: int = 0
+        for vertex in range(n):
+            if components[vertex] == -1:
+                amount_edges: int = self.dfs(vertex, adj_list, components, component)
+                component += 1
+                components_edges.append(amount_edges)
+        complete_components: int = component
+        for vertex in range(n):
+            if components_edges[components[vertex]] != len(adj_list[vertex]) + 1:
+                if components_edges[components[vertex]] != -1:
+                    complete_components -= 1
+                    components_edges[components[vertex]] = -1
+        return complete_components
+
+# C++ O(V^2 + V + E) O(V + E) DSU
+struct PairHash {
+    size_t operator()(const std::pair<int, int>& p) const noexcept {
+        return std::hash<int>{}(p.first) ^
+               (std::hash<int>{}(p.second) << 1);
+    }
+};
+
+class DSU {
+private:
+    std::vector<int> rank, parent;
+public:
+    DSU (int n) {
+        rank.resize(n, 1);
+        for (int i = 0; i < n; ++i) parent.push_back(i);
+    };
+
+    int find(int x) {
+        while (x != parent[x]) {
+            parent[x] = parent[parent[x]];
+            x = parent[x];
+        }
+        return x;
+    };
+
+    void union_(int x, int y) {
+        int px = find(x), py = find(y);
+        if (px == py) return;
+        if (rank[px] >= rank[py]) {
+            rank[px] += rank[py];
+            parent[py] = px;
+        } else {
+            rank[py] += rank[px];
+            parent[px] = py;
+        }
+    };
+};
+
+class Solution {
+public:
+    int countCompleteComponents(int n, vector<vector<int>>& edges) {
+        DSU dsu = DSU(n);
+        std::unordered_set<std::pair<int, int>, PairHash> seen;
+        for (std::vector<int>& e : edges) {
+            dsu.union_(e[0], e[1]);
+            seen.insert({e[0], e[1]});
+            seen.insert({e[1], e[0]});
+        }
+        std::unordered_map<int, std::vector<int>> components;
+        for (int i = 0; i < n; ++i) {
+            components[dsu.find(i)].push_back(i);
+        }
+        int output = 0;
+        for (const auto& [component, vertices] : components) {
+            bool isValid = true;
+            for (int i = 0; i < vertices.size(); ++i) {
+                for (int j = i + 1; j < vertices.size(); ++j) {
+                    if (!seen.count({vertices[i], vertices[j]})) {
+                        isValid = false;
+                        break;
+                    }
+                }
+                if (!isValid) break;
+            }
+            if (isValid) ++output;
+        }
+        return output;
+    }
+};
