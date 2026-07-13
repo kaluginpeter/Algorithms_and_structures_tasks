834e1ca89ece7afe12337fa71469653470cea5d1
kaluginpeter <streetworkout200412@gmail.com>
2026-07-13 17:45:15 +0300

    Added description "Reversed Message"

---
+++ CodeWars/7kyu/Reversed_Message.py
@@ -0,0 +1,8 @@
+# Reverse a message so that the words and letters passed into it are made lower case and reversed. In addition, capitalise the first letter of the newly reversed words. If a number or symbol(!#,>) is now in the first position of the word, no capitalisation needs to occur.
+#
+# Example:
+#
+# reverseMessage('This is an example of a Reversed Message!')
+# Returns: '!egassem Desrever A Fo Elpmaxe Na Si Siht'
+#
+# FundamentalsStrings
