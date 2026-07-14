146721d51afc7a9f5f6e28b3805fc2c7c236f019
kaluginpeter <streetworkout200412@gmail.com>
2026-07-14 19:35:07 +0300

    Added solution "3336. Find the Number of Subsequences With Equal GCD"

--- LeetCode/Hard/3336._Find_the_Number_of_Subsequences_With_Equal_GCD.cpp
+++ LeetCode/Hard/3336._Find_the_Number_of_Subsequences_With_Equal_GCD.cpp
@@ -56,3 +56,33 @@ Constraints:
 1 <= nums.length <= 200
 1 <= nums[i] <= 200
 */
+// Solution
+// C++ O(NM^2logM) O(M^2) DynamicProgramming
+static constexpr int MOD = 1e9 + 7;
+class Solution {
+public:
+    int subsequencePairCount(vector<int>& nums) {
+        int m = *std::max_element(nums.begin(), nums.end());
+        std::vector<std::vector<int>> dp(m + 1, std::vector<int>(m + 1, 0));
+        dp[0][0] = 1;
+        for (int num : nums) {
+            std::vector<std::vector<int>> ndp(m + 1, std::vector<int>(m + 1, 0));
+            for (int j = 0; j <= m; ++j) {
+                int divisor1 = std::gcd(j, num);
+                for (int k = 0; k <= m; ++k) {
+                    if (!dp[j][k]) continue;
+                    int divisor2 = std::gcd(k, num);
+                    ndp[j][k] = (ndp[j][k] + dp[j][k]) % MOD;
+                    ndp[divisor1][k] = (ndp[divisor1][k] + dp[j][k]) % MOD;
+                    ndp[j][divisor2] = (ndp[j][divisor2] + dp[j][k]) % MOD;
+                }
+            }
+            dp.swap(ndp);
+        }
+        int output = 0;
+        for (int j = 1; j <= m; ++j) {
+            output = (output + dp[j][j]) % MOD;
+        }
+        return output;
+    }
+};
