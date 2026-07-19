472bb280897ce7e7e539cfe140c4054dc88307ee
kaluginpeter <streetworkout200412@gmail.com>
2026-07-19 23:36:01 +0300

    Added solution "1081. Smallest Subsequence of Distinct Characters"

--- LeetCode/Medium/1081._Smallest_Subsequence_of_Distinct_Characters.cpp
+++ LeetCode/Medium/1081._Smallest_Subsequence_of_Distinct_Characters.cpp
@@ -21,3 +21,26 @@ s consists of lowercase English letters.
 
 Note: This question is the same as 316: https://leetcode.com/problems/remove-duplicate-letters/
 */
+// Solution
+// C++ O(N) O(N) Greedy
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+        std::array<int, 26> cnt{}, vis{};
+        std::string res = "";
+        size_t n = s.size();
+        for (int i = 0; i < n; ++i) ++cnt[s[i] - 'a'];
+        for (int i = 0; i < n; ++i) {
+            --cnt[s[i] - 'a'];
+            if (!vis[s[i] - 'a']) {
+                while(!res.empty() && res.back() > s[i] && cnt[res.back() - 'a'] > 0) {
+                    vis[res.back() - 'a'] = 0;
+                    res.pop_back();
+                }
+                res += s[i];
+                vis[s[i] - 'a'] = 1;
+            }
+        }
+        return res;
+    }
+};
