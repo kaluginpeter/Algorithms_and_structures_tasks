abe302fc74a33b702440f4abac5167552601d06b
kaluginpeter <streetworkout200412@gmail.com>
2026-07-20 18:26:39 +0300

    Added solution "1260. Shift 2D Grid"

--- LeetCode/Easy/1260._Shift_2D_Grid.py
+++ LeetCode/Easy/1260._Shift_2D_Grid.py
@@ -34,3 +34,35 @@
 # -1000 <= grid[i][j] <= 1000
 # 0 <= k <= 100
 #
+# Solution
+# Python O(NM) O(NM) Matrix Math
+from copy import deepcopy
+class Solution:
+    def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
+        output: list[list[int]] = deepcopy(grid.copy())
+        n: int = len(grid)
+        m: int = len(grid[0])
+        mod: int = n * m
+        k %= mod
+        for i in range(n):
+            for j in range(m):
+                new_cell: int = (i * m + j + k) % mod
+                output[new_cell // m][new_cell % m] = grid[i][j]
+        return output
+
+# C++ O(NM) O(NM) Math Matrix
+class Solution {
+public:
+    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+        std::vector<std::vector<int>> output = grid;
+        size_t n = grid.size(), m = grid[0].size(), mod = n * m;
+        k %= mod;
+        for (size_t i = 0; i < n; ++i) {
+            for (size_t j = 0; j < m; ++j) {
+                size_t newCell = (i * m + j + k) % mod;
+                output[newCell / m][newCell % m] = grid[i][j];
+            }
+        }
+        return output;
+    }
+};
