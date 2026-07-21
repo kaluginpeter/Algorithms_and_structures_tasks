b3106555b6252f750c123c70d5f70f9122105946
kaluginpeter <streetworkout200412@gmail.com>
2026-07-21 18:30:17 +0300

    Added solution "3499. Maximize Active Section with Trade I"

--- LeetCode/Medium/3499._Maximize_Active_Section_with_Trade_I.py
+++ LeetCode/Medium/3499._Maximize_Active_Section_with_Trade_I.py
@@ -61,3 +61,61 @@
 #
 # 1 <= n == s.length <= 105
 # s[i] is either '0' or '1'
+# Solution
+# Python O(N) O(1) TwoPointers Prefix Suffix
+class Solution:
+    def maxActiveSectionsAfterTrade(self, s: str) -> int:
+        left: int = 0
+        middle: int = 0
+        right: int = 0
+        n: int = len(s)
+        prefix: int = 0
+        suffix: int = s.count('1')
+        output: int = 0
+        while middle < n:
+            prev_left: int = left
+            while left < n and s[left] == '0': left += 1
+            left_part: int = left - prev_left
+            middle = left
+            while middle < n and s[middle] == '1': middle += 1
+            middle_part: int = middle - left
+            right = middle
+            while right < n and s[right] == '0': right += 1
+            right_part: int = right - middle
+            left = middle
+            middle = right
+            suffix -= middle_part
+            if not left_part or not right_part:
+                output = max(output, prefix + middle_part + suffix)
+            else:
+                output = max(output, prefix + left_part + middle_part + right_part + suffix)
+            prefix += middle_part
+        return output
+
+# C++ O(N) O(1) Suffix Prefix TwoPointers
+class Solution {
+public:
+    int maxActiveSectionsAfterTrade(string s) {
+        size_t left = 0, middle = 0, right = 0;
+        size_t n = s.size();
+        int output = 0, prefix = 0, suffix = std::count(s.begin(), s.end(), '1');
+        while (middle < n) {
+            int prevLeft = left;
+            while (left < n && s[left] == '0') ++left;
+            int leftPart = left - prevLeft;
+            middle = left;
+            while (middle < n && s[middle] == '1') ++middle;
+            int middlePart = middle - left;
+            right = middle;
+            while (right < n && s[right] == '0') ++right;
+            int rightPart = right - middle;
+            left = middle;
+            middle = right;
+            suffix -= middlePart;
+            if (!leftPart || !rightPart) output = std::max(output, middlePart + prefix + suffix);
+            else output = std::max(output, leftPart + middlePart + rightPart + prefix + suffix);
+            prefix += middlePart;
+        }
+        return output;
+    }
+};
