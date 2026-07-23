7569807ccb59ccb19076423c14472838f57fc801
kaluginpeter <streetworkout200412@gmail.com>
2026-07-23 18:53:31 +0300

    Added description "Find last Fibonacci digit [hardcore version]"

---
+++ CodeWars/6kyu/ Find_last_Fibonacci_digit_hardcore_version.go
@@ -0,0 +1,12 @@
+/*
+Just like in the "father" kata, you will have to return the last digit of the nth element in the Fibonacci sequence (starting with 1,1, to be extra clear, not with 0,1 or other numbers).
+
+You will just get much bigger numbers, so good luck bruteforcing your way through it ;)
+
+LastFibDigit(1) == 1
+LastFibDigit(2) == 1
+LastFibDigit(3) == 2
+LastFibDigit(1000) == 5
+LastFibDigit(1000000) == 5
+Algorithms
+*/
