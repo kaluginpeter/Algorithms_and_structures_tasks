9bb607e1dc1e46263a497fa5d37ed466a4304172
kaluginpeter <streetworkout200412@gmail.com>
2026-07-25 19:04:41 +0300

    Added description "SHEEEEPS"

---
+++ CodeWars/7kyu/SHEEEEPS.py
@@ -0,0 +1,39 @@
+# HI! You have the array of sheeps:
+#
+# ['sheep', 'sheep', 'sheep', 'sheep'...]
+#
+# But somebody is "sick":
+#
+# ['shpee', 'sheep', 'hspee', 'sheep', 'pehes'...]
+#
+# You can help them:
+#
+# shpee => sheep
+#
+# pehes => sheep
+#
+# Because shpee and pehes have 1 s, 1 h, 2 e, 1 p.
+#
+# shep !=> sheep
+#
+# And:
+#
+# sheeep !=> sheep
+#
+# return array(list) with "sheep". if you can't help - delete.
+#
+# Hard register!!!
+# A!==a
+#
+# Example:
+# ShEep !=> sheep
+#
+# EXAMPLE:
+#
+# ['sheep', 'Shpee', 'pEhEs', 'PPh', 'heep', 'phees']
+# return:
+#
+# ['sheep', 'sheep']
+# Good luck!!!
+#
+# Arrays
