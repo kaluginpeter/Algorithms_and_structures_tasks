29a5b5c18512614e5fcc5a10254275e307790d78
kaluginpeter <streetworkout200412@gmail.com>
2026-07-28 19:29:28 +0300

    Added description "WeIrD StRiNg CaSe"

---
+++ CodeWars/6kyu/WeIrD_StRiNg_CaSe.cpp
@@ -0,0 +1,10 @@
+/*
+Write a function that accepts a string, and returns the same string with all even indexed characters in each word upper cased, and all odd indexed characters in each word lower cased. The indexing just explained is zero based, so the zeroth index is even, therefore that character should be upper cased. Indexing resets for each word. In other words, the first letter of every word is index 0 (even), so it must always be uppercase, etc.
+
+The passed in string will only consist of alphabetical characters and spaces(' '). Spaces will only be present if there are multiple words. Words will be separated by a single space(' ').
+
+Examples:
+"String" => "StRiNg"
+"Weird string case" => "WeIrD StRiNg CaSe"
+StringsAlgorithms
+*/
