4729063dca2d81792e4568c5285a4d473c86bc76
kaluginpeter <streetworkout200412@gmail.com>
2026-07-29 18:54:21 +0300

    Added solution "3518. Smallest Palindromic Rearrangement II"

--- LeetCode/Hard/3518._Smallest_Palindromic_Rearrangement_II.cpp
+++ LeetCode/Hard/3518._Smallest_Palindromic_Rearrangement_II.cpp
@@ -46,3 +46,50 @@ s consists of lowercase English letters.
 s is guaranteed to be palindromic.
 1 <= k <= 106
 */
+// Solution
+// C++ O(Nphi(logK)) O(N) String Math
+class Solution {
+private:
+    long long comb(long long n, long long m, long long k) {
+        long long c = 1;
+        m = std::min(m, n - m); // without repetitions
+        for (long long i = 1; i <= m; ++i) {
+            c = c * (n - i + 1) / i;
+            if (c > k) return k + 1;
+        }
+        return c;
+    }
+    long long perm(int n, const long long& k, std::array<int, 26>& hashmap) {
+        long long p = 1;
+        for (size_t i = 0; i < 26; ++i) {
+            if (!hashmap[i]) continue;
+            p *= comb(n, hashmap[i], k);
+            if (p > k) break;
+            n -= hashmap[i];
+        }
+        return p;
+    };
+public:
+    std::string smallestPalindrome(std::string s, long long k) {
+        size_t bound = s.length() >> 1; // floor rounding
+        std::array<int, 26> hashmap{};
+        for (size_t i = 0; i < bound; ++i) ++hashmap[s[i] - 'a'];
+        std::string left = "";
+        long long start = 1;
+        for (size_t pos = 0; pos < bound; ++pos) {
+            for (size_t i = 0; i < 26; ++i) {
+                if (!hashmap[i]) continue;
+                --hashmap[i];
+                long long perms = perm(bound - pos - 1, k, hashmap);
+                if (start + perms > k) {
+                    left += static_cast<char>(i + 'a');
+                    break;
+                }
+                ++hashmap[i];
+                start += perms;
+            }
+        }
+        if (left.length() < bound) return "";
+        return left + (s.length() & 1 ? std::string(1, s[bound]) : "") + std::string(left.rbegin(), left.rend());
+    }
+};
