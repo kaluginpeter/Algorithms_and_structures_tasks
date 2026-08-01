f6ce96fda0569769285acb905342a47c1826d97f
kaluginpeter <streetworkout200412@gmail.com>
2026-08-01 10:02:54 +0300

    Added solution "486. Predict the Winner"

--- LeetCode/Medium/486._Predict_the_Winner.cpp
+++ LeetCode/Medium/486._Predict_the_Winner.cpp
@@ -28,3 +28,20 @@ Constraints:
 1 <= nums.length <= 20
 0 <= nums[i] <= 107
 */
+// Solution
+// C++ O(N^2) O(N^2) Recursion
+class Solution {
+public:
+    bool predictTheWinner(vector<int>& A) {
+        int n = A.size();
+        vector dp(n, vector<int>(n, -1));
+        auto maxDiff = [&](this auto&& maxDiff, int i, int j) -> int {
+            int& res = dp[i][j];
+            if (res != -1) return res;
+            if (i == j) return res = A[i];
+            return res = max(A[i] - maxDiff(i + 1, j),
+                             A[j] - maxDiff(i, j - 1));
+        };
+        return maxDiff(0, n - 1) >= 0;
+    }
+};
