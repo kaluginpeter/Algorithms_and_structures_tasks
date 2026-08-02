b63eda098b84dfb5d77b5e42207ca51e7e6170a7
kaluginpeter <streetworkout200412@gmail.com>
2026-08-02 20:53:20 +0300

    Added solution "877. Stone Game"

--- LeetCode/Medium/877._Stone_Game.cpp
+++ LeetCode/Medium/877._Stone_Game.cpp
@@ -32,3 +32,17 @@ piles.length is even.
 1 <= piles[i] <= 500
 sum(piles[i]) is odd.
 */
+// Solution
+// C++ O(N^2) O(N^2) DynamicProgramming
+class Solution {
+public:
+    bool stoneGame(vector<int>& p) {
+        int n = p.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        for (int i = 0; i < n; i++) dp[i][i] = p[i];
+        for (int d = 1; d < n; d++)
+            for (int i = 0; i < n - d; i++)
+                dp[i][i + d] = max(p[i] - dp[i + 1][i + d], p[i + d] - dp[i][i + d - 1]);
+        return dp[0][n - 1] > 0;
+    }
+};
