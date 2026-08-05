eeb8a31030ac2ebc823899f078c1c37d3b38bab7
kaluginpeter <streetworkout200412@gmail.com>
2026-08-05 19:09:53 +0300

    Added description "Two Sum"

---
+++ CodeWars/6kyu/Two_Sum.go
@@ -0,0 +1,14 @@
+/*
+Write a function that takes an array of numbers (integers for the tests) and a target number. It should find two different items in the array that, when added together, give the target value. The indexes of these items should then be returned in a tuple / list (depending on your language) like so: (index1, index2).
+
+For the purposes of this kata, some tests may have multiple answers; any valid solutions will be accepted.
+
+The input will always be valid (numbers will be an array of length 2 or greater, and all of the items will be positive or negative integers; target will always be the sum of two different items from that array).
+
+Based on: https://leetcode.com/problems/two-sum/
+
+TwoSum([]int{1, 2, 3}, 4) // returns [2]int{0, 2}
+// the go translation has an issue where random tests accept either [2]int{0, 2} or [2]int{2, 0}, but fixed tests and sample tests demand the resulting slice to be sorted!
+// untill it's fixed, please sort your result in go.
+ArraysFundamentalsAlgorithms
+*/
