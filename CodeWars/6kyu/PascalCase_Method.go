26a2732c95fd6e69a1521b735cf4754a381eb8c1
kaluginpeter <streetworkout200412@gmail.com>
2026-08-06 18:25:07 +0300

    Added description "PascalCase Method"

---
+++ CodeWars/6kyu/PascalCase_Method.go
@@ -0,0 +1,12 @@
+/*
+Write a method (or function, depending on the language) that converts a string to PascalCase, that is, all words must have their first letter capitalized and spaces must be removed.
+
+Examples (input --> output):
+"hello case" --> "HelloCase"
+"pascal case word" --> "PascalCaseWord"
+Don't forget to rate this kata! Thanks :)
+
+Note: for historical reasons, the function is named camelCase() or similar in some languages, but it should actually perform conversion to PascalCase.
+
+FundamentalsAlgorithmsStrings
+*/
