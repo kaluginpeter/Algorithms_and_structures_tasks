693957d724379040881fd43e248ab660a8494e9b
kaluginpeter <streetworkout200412@gmail.com>
2026-08-07 17:52:17 +0300

    Added solution "3348. Smallest Divisible Digit Product II"

--- LeetCode/Hard/3348._Smallest_Divisible_Digit_Product_II.cpp
+++ LeetCode/Hard/3348._Smallest_Divisible_Digit_Product_II.cpp
@@ -46,3 +46,50 @@ num consists only of digits in the range ['0', '9'].
 num does not contain leading zeros.
 1 <= t <= 1014
 */
+// Solution
+// C++ O(N + DlogN) O(N) Prefix Math
+class Solution {
+public:
+    string smallestNumber(string num, long long t) {
+        long long temp = t;
+        for (int i = 2; i <= 9; ++i) {
+            while (temp % i == 0) temp /= i;
+        }
+        if (temp > 1) return "-1"; // the "t" is the prime number itself
+        int n = num.length();
+        std::vector<long long> rem(n + 1);
+        rem[0] = t;
+        int start = n - 1;
+        for (int i = 0; i < n; ++i) {
+            if (num[i] == '0') {
+                start = i;
+                break;
+            }
+            rem[i + 1] = rem[i] / std::gcd(rem[i], num[i] - '0');
+        }
+        if (rem[n] == 1) return num;
+        for (int i = start; i >= 0; --i) {
+            for (char d = num[i] + 1; d <= '9'; ++d) {
+                num[i] = d;
+                long long tmpT = rem[i] / std::gcd(rem[i], d - '0');
+                int k = 9;
+                for (int j = n - 1; j > i; --j) {
+                    while (tmpT % k) --k;
+                    tmpT /= k;
+                    num[j] = k + '0';
+                }
+                if (tmpT == 1) return num;
+            }
+        }
+        std::string output = "";
+        for (int i = 9; i > 1; --i) {
+            while (t % i == 0) {
+                output += i + '0';
+                t /= i;
+            }
+        }
+        output += std::string(std::max(n - static_cast<int>(output.length()) + 1, 0), '1');
+        std::reverse(output.begin(), output.end());
+        return output;
+    }
+};
