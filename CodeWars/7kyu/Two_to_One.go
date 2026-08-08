03ef36a6117d879f58a4e840264512d060bccd17
kaluginpeter <streetworkout200412@gmail.com>
2026-08-08 11:02:09 +0300

    Added description "Two to One"

---
+++ CodeWars/7kyu/Two_to_One.go
@@ -0,0 +1,12 @@
+/*
+Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string (alphabetical ascending), the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+Examples:
+a = "xyaabbbccccdefww"
+b = "xxxxyyyyabklmopq"
+longest(a, b) -> "abcdefklmopqwxy"
+
+a = "abcdefghijklmnopqrstuvwxyz"
+longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+Fundamentals
+*/
