fe903b33a0eef4510be451c12c692e1231b5f415
kaluginpeter <streetworkout200412@gmail.com>
2026-08-08 11:03:18 +0300

    Added solution "Two to One"

--- CodeWars/7kyu/Two_to_One.go
+++ CodeWars/7kyu/Two_to_One.go
@@ -10,3 +10,24 @@ a = "abcdefghijklmnopqrstuvwxyz"
 longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
 Fundamentals
 */
+// Solution
+package kata
+
+import "strings"
+
+func TwoToOne(s1 string, s2 string) string {
+    var hashmap [26]int
+    for _, char := range(s1) {
+        hashmap[char - 'a']++
+    }
+    for _, char := range(s2) {
+        hashmap[char - 'a']++
+    }
+    var output strings.Builder
+    for i := 0; i < 26; i++ {
+       if (hashmap[i] > 0) {
+           output.WriteByte(byte(i + 'a'));
+       }
+    }
+    return output.String()
+}
