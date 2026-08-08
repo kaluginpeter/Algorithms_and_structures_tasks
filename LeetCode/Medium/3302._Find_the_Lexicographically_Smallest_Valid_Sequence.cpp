cdf7b75a0945c632505a44c6860982eae169e8a2
kaluginpeter <streetworkout200412@gmail.com>
2026-08-08 10:58:22 +0300

    Added solution "3302. Find the Lexicographically Smallest Valid Sequence"

--- LeetCode/Medium/3302._Find_the_Lexicographically_Smallest_Valid_Sequence.cpp
+++ LeetCode/Medium/3302._Find_the_Lexicographically_Smallest_Valid_Sequence.cpp
@@ -63,3 +63,32 @@ Constraints:
 word1 and word2 consist only of lowercase English letters.
 
 */
+// Solution
+// C++ O(N + M) O(1) String
+class Solution {
+public:
+    vector<int> validSequence(string word1, string word2) {
+        int n = word1.length(), m = word2.length();
+        std::vector<int> last(m, -1);
+        int j = m - 1;
+        for (int i = n - 1; i >= 0; --i) {
+            if (j >= 0 && word1[i] == word2[j]) {
+                last[j] = i;
+                j -= 1;
+            }
+        }
+        std::vector<int> res;
+        int skip = 0;
+        j = 0;
+        for (int i = 0; i < n; ++i) {
+            if (j == m) break;
+            if (word1[i] == word2[j] ||
+                (skip == 0 && (j == m - 1 || i < last[j + 1]))) {
+                skip += (word1[i] != word2[j] ? 1 : 0);
+                res.push_back(i);
+                j += 1;
+            }
+        }
+        return j == m ? res : vector<int>();
+    }
+};
