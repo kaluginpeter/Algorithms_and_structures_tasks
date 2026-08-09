cc998937e0e2bc6afcdd4a4d923e91265700c147
kaluginpeter <streetworkout200412@gmail.com>
2026-08-09 21:35:24 +0300

    Added solution "1140. Stone Game II"

--- LeetCode/Medium/1140._Stone_Game_II.cpp
+++ LeetCode/Medium/1140._Stone_Game_II.cpp
@@ -36,3 +36,30 @@ Constraints:
 1 <= piles.length <= 100
 1 <= piles[i] <= 104
 */
+// Solution
+// C++ O(N^3) O(N^2) DynamicProgramming
+class Solution {
+public:
+    int stoneGameII(vector<int>& piles) {
+        vector<vector<int>> memo(piles.size(), vector<int>(piles.size()));
+        vector<int> suffixSum = piles;
+        for (int i = suffixSum.size() - 2; i >= 0; --i)
+            suffixSum[i] += suffixSum[i + 1];
+        return maxStones(suffixSum, 1, 0, memo);
+    }
+
+    int maxStones(vector<int>& suffixSum, int maxTillNow, int currIndex,
+                  vector<vector<int>>& memo) {
+        if (currIndex + 2 * maxTillNow >= suffixSum.size())
+            return suffixSum[currIndex];
+        if (memo[currIndex][maxTillNow] > 0) return memo[currIndex][maxTillNow];
+        int res = INT_MAX;
+
+        for (int i = 1; i <= 2 * maxTillNow; ++i) {
+            res = min(res, maxStones(suffixSum, max(i, maxTillNow),
+                                     currIndex + i, memo));
+        }
+        memo[currIndex][maxTillNow] = suffixSum[currIndex] - res;
+        return memo[currIndex][maxTillNow];
+    }
+};
