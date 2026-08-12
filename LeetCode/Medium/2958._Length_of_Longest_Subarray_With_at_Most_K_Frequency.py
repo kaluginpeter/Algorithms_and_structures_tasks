09d24ed8fd97afda2f3818e66cdd4fc0ff087712
kaluginpeter <streetworkout200412@gmail.com>
2026-08-12 18:41:34 +0300

    Added solution "2958. Length of Longest Subarray With at Most K Frequency"

--- LeetCode/Medium/2958._Length_of_Longest_Subarray_With_at_Most_K_Frequency.py
+++ LeetCode/Medium/2958._Length_of_Longest_Subarray_With_at_Most_K_Frequency.py
@@ -48,3 +48,37 @@ class Solution:
                 left += 1
             ans = max(ans, right + 1 - left)
         return ans
+
+
+# Python O(N) O(D) TwoPointers HashMap
+class Solution:
+    def maxSubarrayLength(self, nums: List[int], k: int) -> int:
+        freq: dict[int, int] = defaultdict(int)
+        left: int = 0
+        n: int = len(nums)
+        output: int = 0
+        for right in range(n):
+            freq[nums[right]] += 1
+            while freq[nums[right]] > k:
+                freq[nums[left]] -= 1
+                left += 1
+            output = max(output, right - left + 1)
+        return output
+
+# C++ O(N) O(D) HashMap TwoPointers
+class Solution {
+public:
+    int maxSubarrayLength(vector<int>& nums, int k) {
+        std::unordered_map<int, size_t> freq;
+        size_t n = nums.size(), left = 0, right = 0, output = 0;
+        for (size_t right = 0; right < n; ++right) {
+            ++freq[nums[right]];
+            while (freq[nums[right]] > k) {
+                --freq[nums[left]];
+                ++left;
+            }
+            output = std::max(output, right - left + 1);
+        }
+        return output;
+    }
+};
