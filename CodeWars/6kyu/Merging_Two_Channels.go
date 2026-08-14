e2fa586228a48d72d6a1256f824d107fea0bb7ca
kaluginpeter <streetworkout200412@gmail.com>
2026-08-14 18:24:45 +0300

    Added solution "Merging Two Channels"

--- CodeWars/6kyu/Merging_Two_Channels.go
+++ CodeWars/6kyu/Merging_Two_Channels.go
@@ -40,3 +40,37 @@ c := Merge(a, b)
 // channel c must be closed at this point
 Concurrency
 */
+// Solution
+package kata
+
+import "sync"
+
+func Merge(a <-chan string, b <-chan string) <-chan string {
+	out := make(chan string)
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+
+		for msg := range a {
+			out <- msg
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+
+		for msg := range b {
+			out <- msg
+		}
+	}()
+
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+
+	return out
+}
