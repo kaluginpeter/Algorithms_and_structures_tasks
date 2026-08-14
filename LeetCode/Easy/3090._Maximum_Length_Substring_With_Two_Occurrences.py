e37c3280143b654ab995f9dc04e3f89ba7a064ab
kaluginpeter <streetworkout200412@gmail.com>
2026-08-14 18:27:08 +0300

    Added solution "3090. Maximum Length Substring With Two Occurrences"

--- LeetCode/Easy/3090._Maximum_Length_Substring_With_Two_Occurrences.py
+++ LeetCode/Easy/3090._Maximum_Length_Substring_With_Two_Occurrences.py
@@ -40,3 +40,55 @@ class Solution:
                 left += 1
             ans = max(ans, right + 1 - left)
         return ans
+
+
+# Python O(N) O(D) SlidingWindow
+from array import array
+class Solution:
+    def maximumLengthSubstring(self, s: str) -> int:
+        n: int = len(s)
+        left: int = 0
+        output: int = 0
+        hashmap: list[int] = array('H', [0]) * 26
+        for right in range(n):
+            hashmap[ord(s[right]) - 97] += 1
+            while hashmap[ord(s[right]) - 97] > 2:
+                hashmap[ord(s[left]) - 97] -= 1
+                left += 1
+            output = max(output, right - left + 1)
+        return output
+
+# C++ O(N) O(D) SlidingWindow
+class Solution {
+public:
+    int maximumLengthSubstring(string s) {
+        size_t n = s.size(), left = 0, output = 0;
+        std::array<int, 26> hashmap{};
+        for (size_t right = 0; right < n; ++right) {
+            ++hashmap[s[right] - 'a'];
+            while (hashmap[s[right] - 'a'] > 2) {
+                --hashmap[s[left] - 'a'];
+                ++left;
+            }
+            output = std::max(output, right - left + 1);
+        }
+        return output;
+    }
+};
+
+# Go O(N) O(D) SlidingWindow
+func maximumLengthSubstring(s string) int {
+    var n int = len(s)
+    var left int = 0
+    var output int = 0
+    var hashmap [26]int
+    for right := 0; right < n; right++ {
+        hashmap[s[right] - 'a']++
+        for hashmap[s[right] - 'a'] > 2 {
+            hashmap[s[left] - 'a']--
+            left++
+        }
+        output = max(output, right - left + 1)
+    }
+    return output
+}
