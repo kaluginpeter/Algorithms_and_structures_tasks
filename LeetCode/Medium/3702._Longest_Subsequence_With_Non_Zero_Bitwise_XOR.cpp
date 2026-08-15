e38f3704a16086627ef41da7f7ee216c3a941d04
kaluginpeter <streetworkout200412@gmail.com>
2026-08-15 20:05:42 +0300

    Added solution "3702. Longest Subsequence With Non-Zero Bitwise XOR"

--- LeetCode/Medium/3702._Longest_Subsequence_With_Non_Zero_Bitwise_XOR.cpp
+++ LeetCode/Medium/3702._Longest_Subsequence_With_Non_Zero_Bitwise_XOR.cpp
@@ -32,3 +32,46 @@ Constraints:
 1 <= nums.length <= 105
 0 <= nums[i] <= 109
 */
+// Solution
+// C++ O(N) O(1) BitTheory
+class Solution {
+public:
+    int longestSubsequence(vector<int>& nums) {
+        int overall = 0;
+        for (int& num : nums) overall ^= num;
+        if (overall) return nums.size();
+        for (int& num : nums) {
+            if (overall ^ num) return nums.size() - 1;
+        }
+        return 0;
+    }
+};
+// Python O(N) O(1) BitTheory
+class Solution:
+    def longestSubsequence(self, nums: List[int]) -> int:
+        n: int = len(nums)
+        output: int = 0
+        for num in nums:
+            output ^= num
+        if output: return n
+        for num in nums:
+            if output ^ num: return n - 1
+        return 0
+
+// Go O(N) O(1) BitTheory
+func longestSubsequence(nums []int) int {
+    var output int = 0
+    var n int = len(nums)
+    for _, num := range(nums) {
+        output ^= num
+    }
+    if output > 0 {
+        return n
+    }
+    for _, num := range(nums) {
+        if output ^ num > 0 {
+            return n - 1
+        }
+    }
+    return 0
+}
