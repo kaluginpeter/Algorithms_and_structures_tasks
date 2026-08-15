bb08e4b53b46aa8ebd5561772f8c36c65472a497
kaluginpeter <streetworkout200412@gmail.com>
2026-08-15 20:09:34 +0300

    Added solution "Easy Diagonal"

--- CodeWars/6kyu/Easy_Diagonal.go
+++ CodeWars/6kyu/Easy_Diagonal.go
@@ -20,3 +20,23 @@ http://mathworld.wolfram.com/BinomialCoefficient.html
 
 Fundamentals
 */
+// Solution
+package kata
+
+func Diagonal(n, p int) int {
+	return binomial(n+1, p+1)
+}
+
+func binomial(n, k int) int {
+	if k > n-k {
+		k = n - k
+	}
+
+	result := 1
+
+	for i := 1; i <= k; i++ {
+		result = result * (n - k + i) / i
+	}
+
+	return result
+}
