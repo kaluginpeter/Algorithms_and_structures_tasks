0293435196203f8e5c48107fff872e0758f8e9cc
kaluginpeter <streetworkout200412@gmail.com>
2026-08-17 18:15:46 +0300

    Added solution "1563. Stone Game V"

--- LeetCode/Hard/1563._Stone_Game_V.cpp
+++ LeetCode/Hard/1563._Stone_Game_V.cpp
@@ -31,3 +31,36 @@ Constraints:
 1 <= stoneValue.length <= 500
 1 <= stoneValue[i] <= 106
 */
+// Solution
+// C++ O(N^3) O(N^2) DynamicProgramming
+class Solution {
+private:
+    std::vector<std::vector<int>> f;
+public:
+    int dfs(const vector<int>& stoneValue, int left, int right) {
+        if (left == right) return 0;
+        if (f[left][right]) return f[left][right];
+        int sum = std::accumulate(stoneValue.begin() + left, stoneValue.begin() + right + 1, 0);
+        int suml = 0;
+        for (int i = left; i < right; ++i) {
+            suml += stoneValue[i];
+            int sumr = sum - suml;
+            if (suml < sumr) {
+                f[left][right] = std::max(f[left][right], dfs(stoneValue, left, i) + suml);
+            } else if (suml > sumr) {
+                f[left][right] = std::max(f[left][right], dfs(stoneValue, i + 1, right) + sumr);
+            } else {
+                f[left][right] = std::max(f[left][right], std::max(dfs(stoneValue, left, i),
+                                            dfs(stoneValue, i + 1, right)) +
+                                            suml);
+            }
+        }
+        return f[left][right];
+    }
+
+    int stoneGameV(vector<int>& stoneValue) {
+        int n = stoneValue.size();
+        f.assign(n, std::vector<int>(n));
+        return dfs(stoneValue, 0, n - 1);
+    }
+};
