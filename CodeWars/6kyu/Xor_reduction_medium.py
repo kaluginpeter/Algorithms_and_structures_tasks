b8825d12c31fe78a4715820fbb01e082e4e58b68
kaluginpeter <streetworkout200412@gmail.com>
2026-08-22 10:42:00 +0300

    Added description "Xor reduction (medium)"

---
+++ CodeWars/6kyu/Xor_reduction_medium.py
@@ -0,0 +1,12 @@
+# Xor reduction (medium)
+# Given two integers, m and n, return the cumulative xor of all positive integers between them, inclusive. E.g. (0, 6), return 0^1^2^3^4^5^6 => 7.
+#
+# Constraints:
+# 0 <= m < n <= 10^15
+#
+# Hints
+# Try the easy variant first
+# Brute-force solutions will likely be rejected due to time-out.
+# Read more about xor.
+# Look for patterns for successive n values.
+# AlgorithmsPuzzles
