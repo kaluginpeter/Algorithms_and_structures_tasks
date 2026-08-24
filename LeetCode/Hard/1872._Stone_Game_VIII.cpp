695302c59c35012c8ae9d91137bc2c1cedc8e310
kaluginpeter <streetworkout200412@gmail.com>
2026-08-24 19:06:07 +0300

    Added solution "1872. Stone Game VIII"

--- LeetCode/Hard/1872._Stone_Game_VIII.cpp
+++ LeetCode/Hard/1872._Stone_Game_VIII.cpp
@@ -48,3 +48,19 @@ n == stones.length
 2 <= n <= 105
 -104 <= stones[i] <= 104
 */
+// Solution
+// C++ O(N) O(N) Dynamic Programming
+class Solution {
+public:
+    int stoneGameVIII(vector<int>& stones) {
+        int n = stones.size();
+        std::vector<int> pre;
+        std::partial_sum(stones.begin(), stones.end(), back_inserter(pre));
+        std::vector<int> f(n);
+        f[n - 1] = pre[n - 1];
+        for (int i = n - 2; i >= 1; --i) {
+            f[i] = std::max(f[i + 1], pre[i] - f[i + 1]);
+        }
+        return f[1];
+    }
+};
