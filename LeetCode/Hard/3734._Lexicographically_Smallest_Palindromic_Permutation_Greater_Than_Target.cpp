349e4496a944c4aaff6fbbaed4f7c58c123eae5a
kaluginpeter <streetworkout200412@gmail.com>
2026-08-28 19:13:13 +0300

    Added solution "3734. Lexicographically Smallest Palindromic Permutation Greater Than Target"

--- LeetCode/Hard/3734._Lexicographically_Smallest_Palindromic_Permutation_Greater_Than_Target.cpp
+++ LeetCode/Hard/3734._Lexicographically_Smallest_Palindromic_Permutation_Greater_Than_Target.cpp
@@ -54,3 +54,74 @@ s and target consist of only lowercase English letters.
  
 
 */
+// Solution
+// C++ O(N^2) O(N) Greedy
+class Solution {
+public:
+    string isPossible(int n, vector<int> freq, string cur, char &mid, string& target){
+        for(int i=25; i>=0; --i){
+            while(freq[i]){
+                cur += (char)('a'+i);
+                --freq[i];
+            }
+        }
+        if(mid!='#'){
+            string temp = cur;
+            cur += mid;
+            reverse(temp.begin(), temp.end());
+            cur.append(temp.begin(), temp.end());
+        }
+        else {
+            string temp = cur;
+            reverse(temp.begin(), temp.end());
+            cur.append(temp.begin(), temp.end());
+        }
+        return cur>target? cur : "";
+    }
+
+    string lexPalindromicPermutation(string s, string target) {
+        int n = s.size();
+        vector<int> freq(26, 0);
+        if(n==1){
+            if(s>target) return s;
+            else return "";
+        }
+        for(char c : s) ++freq[c-'a'];
+        char mid = '#';
+        int oddCount = 0;
+        for(int i=0; i<26; ++i){
+            if(freq[i]%2){
+                mid = (char)('a'+i);
+                --freq[i];
+                ++oddCount;
+            }
+            freq[i] /= 2; 
+            if(oddCount>=2) return "";
+        }
+
+        n /= 2; 
+        string res = "", prefix = "";
+        for(int i=0; i<n; ++i){
+            string cur = prefix;
+            bool isThereAny = false;
+            for(int j=0; j<26; ++j) {
+                if (freq[j]) {
+                    --freq[j];
+                    cur += (char)('a' + j);
+                    string isPos = isPossible(n, freq, cur, mid, target);
+                    if(isPos!=""){
+                        prefix = cur;
+                        isThereAny = true;
+                        if(res=="") res = isPos;
+                        else res = min(res, isPos);
+                        break;
+                    }
+                    ++freq[j];
+                    cur.pop_back();
+                }
+            }
+            if(!isThereAny) return "";
+        }
+        return res; 
+    }
+};
