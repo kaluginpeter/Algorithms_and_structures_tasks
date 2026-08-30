a49ab121e7f589dfd16f114a6b0e87c9a9dc2098
kaluginpeter <streetworkout200412@gmail.com>
2026-08-30 20:47:39 +0300

    Added solution "2091. Removing Minimum and Maximum From Array"

--- LeetCode/Medium/2091._Removing_Minimum_and_Maximum_From_Array.cpp
+++ LeetCode/Medium/2091._Removing_Minimum_and_Maximum_From_Array.cpp
@@ -42,3 +42,19 @@ Constraints:
 -105 <= nums[i] <= 105
 The integers in nums are distinct.
 */
+// Solution
+// C++ O(N) O(1) Greedy
+class Solution {
+public:
+    int minimumDeletions(vector<int>& nums) {
+        size_t n = nums.size(), x = 0, y = 0;
+        for (size_t i = 0; i < n; ++i) {
+            if (nums[x] < nums[i]) x = i;
+            if (nums[y] > nums[i]) y = i;
+        }
+        if (x > y) std::swap(x, y);
+        return std::min({
+            y + 1, (n - x), x + 1 + (n - y)
+        });
+    }
+};
