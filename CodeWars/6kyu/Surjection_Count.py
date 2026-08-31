d8a4dad811043b418d8cdc02b9f924add2ae3cca
kaluginpeter <streetworkout200412@gmail.com>
2026-08-31 18:50:30 +0300

    Added description "Surjection Count"

---
+++ CodeWars/6kyu/Surjection_Count.py
@@ -0,0 +1,55 @@
+# Notation and definition
+# Let 
+# [
+# n
+# ]
+# [n] be a set of all positive integers up to and including 
+# n
+# n, for all positive integers 
+# n
+# n.
+# A function 
+# f
+# :
+# X
+# →
+# Y
+# f:X→Y is considered surjective, if all elements in the codomain 
+# Y
+# Y are paired up with at least one element from the domain 
+# X
+# X.
+# Task
+# Given the inputs 
+# n
+# n and 
+# k
+# k, find the number of surjections for 
+# f
+# :
+# [
+# n
+# ]
+# →
+# [
+# k
+# ]
+# f:[n]→[k].
+
+# Examples
+# surjections(3, 2) -> 6
+# surjections(6, 2) -> 62
+# surjections(5, 3) -> 150
+# Constraints
+# 1
+# ≤
+# k
+# ≤
+# n
+# ≤
+# 300
+# 1≤k≤n≤300
+
+# Good luck!
+
+# Combinatorics
