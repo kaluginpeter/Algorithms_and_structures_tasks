b975970e5d6d344276e1b9ca0a1b6c601dc0f50b
kaluginpeter <streetworkout200412@gmail.com>
2026-09-02 19:23:06 +0300

    Added solution "Previous Fibonacci Number"

--- CodeWars/5kyu/Previous_Fibonacci_Number.py
+++ CodeWars/5kyu/Previous_Fibonacci_Number.py
@@ -20,3 +20,41 @@
 # This means that you should be prepared to handle numbers with up to 46_589_786 bits.
 
 # MathematicsNumber Theory
+# Solution
+import math
+
+import gmpy2
+_mpz = gmpy2.mpz
+
+_LOG2_PHI = math.log2((1 + 5 ** 0.5) / 2)
+_LOG2_SQRT5 = 0.5 * math.log2(5)
+
+
+def _fib_pair(k: int):
+    a, b = _mpz(0), _mpz(1) 
+    for bit in bin(k)[2:]:
+        c = a * (2 * b - a)  
+        d = a * a + b * b      
+        if bit == '1': a, b = d, c + d
+        else: a, b = c, d
+    return a, b
+
+
+def _estimate_k(n: int) -> int:
+    bl = n.bit_length()
+    shift = max(0, bl - 64)
+    top = int(n >> shift)
+    log2n = math.log2(top) + shift
+    return round((log2n + _LOG2_SQRT5) / _LOG2_PHI)
+
+
+def previous_fib(n: int) -> int | None:
+    if n == 0: return None
+    if n == 1: return 0  
+    k = max(_estimate_k(n), 2)
+    a, b = _fib_pair(k) 
+    n = _mpz(n)
+    while a != n:
+        if a > n: a, b = b - a, a    
+        else: a, b = b, a + b   
+    return int(b - a)
