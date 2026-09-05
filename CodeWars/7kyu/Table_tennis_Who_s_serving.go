14a49ca8ef47356c3608644c316a778fd6859bb3
kaluginpeter <streetworkout200412@gmail.com>
2026-09-05 19:27:04 +0300

    Added description "Table tennis: Who's serving?"

---
+++ CodeWars/7kyu/Table_tennis_Who_s_serving.go
@@ -0,0 +1,21 @@
+/*
+Your colleagues enjoy playing table tennis, but they often forget who should serve in each round. They ask you to create a program that will determine the serving player based on the current round number.
+
+Rules
+The game starts with the first round, or currentRound=1.
+Player 1 always starts the game by serving.
+A serving player alternates every two rounds
+currentRound cannot be a negative number or zero
+The original game rules are simplified: there is no "deuce" exception (service does not alternate at every point after '10-10')
+Examples
+The Player 1 serves in the 1st round
+The Player 1 serves in the 2nd round
+The Player 2 serves in the 3rd round
+The Player 2 serves in the 4th round
+The Player 1 serves in the 5th round
+The Player 1 serves in the 6tx round
+The Player 2 serves in the 7th round
+The Player 2 serves in the 8th round
+And so on
+Mathematics
+*/
