a852b2443a4973c2eb7b984067d9227de4fb3fe4
kaluginpeter <streetworkout200412@gmail.com>
2026-09-06 20:03:17 +0300

    Added solution "115. Distinct Subsequences"

--- LeetCode/Hard/115._Distinct_Subsequences.cpp
+++ LeetCode/Hard/115._Distinct_Subsequences.cpp
@@ -32,3 +32,21 @@ Constraints:
 1 <= s.length, t.length <= 1000
 s and t consist of English letters.
 */
+// Solution
+// C++ O(NM) O(NM) DynamicProgramming
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int m = s.length(), n = t.length();
+        if (m < n) return 0;
+        vector<vector<unsigned long long>> dp(m + 1, vector<unsigned long long>(n + 1));
+        for (int i = 0; i <= m; ++i) dp[i][n] = 1;
+        for (int i = m - 1; i >= 0; --i) {
+            for (int j = n - 1; j >= 0; --j) {
+                if (s[i] == t[j]) dp[i][j] = dp[i + 1][j + 1] + dp[i + 1][j];
+                else dp[i][j] = dp[i + 1][j];
+            }
+        }
+        return dp[0][0];
+    }
+};
