d3409910408b24356baef1f70e66fb328909ceb2
kaluginpeter <streetworkout200412@gmail.com>
2026-09-07 19:23:06 +0300

    Added description "Stop gninnipS My sdroW!"

---
+++ CodeWars/6kyu/Stop_gninnipS_My_sdroW.go
@@ -0,0 +1,10 @@
+/*
+Write a function that takes in a string of one or more words, and returns the same string, but with all words that have five or more letters reversed (just like the name of this kata). Strings passed in will consist of only letters and spaces. Words will be separated by exactly one space. There will be no leading or trailing spaces.
+
+Examples:
+
+"Hey fellow warriors"  --> "Hey wollef sroirraw" 
+"This is a test        --> "This is a test" 
+"This is another test" --> "This is rehtona test"
+StringsAlgorithms
+*/
