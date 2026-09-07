884f7a6c45336fb5479971ab4dd8a498f9c95462
kaluginpeter <streetworkout200412@gmail.com>
2026-09-07 19:23:29 +0300

    Added solution "Stop gninnipS My sdroW!"

--- CodeWars/6kyu/Stop_gninnipS_My_sdroW.go
+++ CodeWars/6kyu/Stop_gninnipS_My_sdroW.go
@@ -8,3 +8,34 @@ Examples:
 "This is another test" --> "This is rehtona test"
 StringsAlgorithms
 */
+// Solution
+package kata
+
+
+func SpinWords(str string) string {
+    var output []byte = make([]byte, 0)
+    var i, n int = 0, len(str)
+    for i < n {
+        if i > 0 {
+            output = append(output, ' ')
+        }
+        var tmp []byte = make([]byte, 0)
+        for i < n && (str[i] != ' ') {
+            tmp = append(tmp, str[i])
+            i++
+        }
+        i++
+        if (len(tmp) >= 5) {
+            var left, right int = 0, len(tmp) - 1
+            for left < right {
+                var tmpByte byte = tmp[left]
+                tmp[left] = tmp[right]
+                tmp[right] = tmpByte
+                left++
+                right--
+            }
+        }
+        output = append(output, tmp...)
+    }
+    return string(output)
+}// SpinWords
