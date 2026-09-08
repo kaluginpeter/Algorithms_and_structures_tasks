737fe4dde2cb4ace76fe2b26f6d88e96bbc77527
kaluginpeter <streetworkout200412@gmail.com>
2026-09-08 19:15:48 +0300

    Added solution "Array.diff"

--- CodeWars/6kyu/Array_diff.go
+++ CodeWars/6kyu/Array_diff.go
@@ -8,3 +8,20 @@ If a = [1, 2, 2, 2, 3] and b = [2], the result should be [1, 3].
 
 ArraysFundamentalsAlgorithms
 */
+// Solution
+package kata
+
+func ArrayDiff(a, b []int) []int {
+  var seen map[int]int = map[int]int{}
+  var output []int = []int{}
+  for _, val := range b {
+    seen[val]++
+  }
+  for _, val := range a {
+    _, ok := seen[val]
+    if !ok {
+      output = append(output, val)
+    }
+  }
+  return output
+}
