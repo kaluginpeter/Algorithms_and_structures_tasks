40c1fcb4b83fbfffff0e10ab6ecfbb896d24656c
kaluginpeter <streetworkout200412@gmail.com>
2026-09-08 19:15:28 +0300

    Added description "Array.diff"

---
+++ CodeWars/6kyu/Array_diff.go
@@ -0,0 +1,10 @@
+/*
+Implement a function that computes the difference between two lists. The function should remove all occurrences of elements from the first list (a) that are present in the second list (b). The order of elements in the first list should be preserved in the result.
+
+Examples
+If a = [1, 2] and b = [1], the result should be [2].
+
+If a = [1, 2, 2, 2, 3] and b = [2], the result should be [1, 3].
+
+ArraysFundamentalsAlgorithms
+*/
