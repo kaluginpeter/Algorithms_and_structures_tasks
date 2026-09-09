ffe387c6fb009988a7a262b62e7954522d785951
kaluginpeter <streetworkout200412@gmail.com>
2026-09-09 12:57:29 +0300

    Added solution "3871. Count Commas in Range II"

--- LeetCode/Medium/3871._Count_Commas_in_Range_II.go
+++ LeetCode/Medium/3871._Count_Commas_in_Range_II.go
@@ -35,3 +35,48 @@ Constraints:
 
 1 <= n <= 1015
 */
+// Solution
+// Go O(log10(N)) O(1) Math
+func countCommas(n int64) int64 {
+    var total, cur, step, commas int64 = 0, 1000, 0, 1
+    for cur <= n {
+        var nextCur int64 = min(cur * 10 - 1, n)
+        total += (nextCur - cur + 1) * commas
+        step++
+        if (step % 3 == 0) {
+            commas++
+        }
+        cur = nextCur + 1
+    }
+    return total
+}
+
+// C++ O(log10(N)) O(1) Math
+class Solution {
+public:
+    long long countCommas(long long n) {
+        long long output = 0, cur = 1000, step = 0, commas = 1;
+        while (cur <= n) {
+            long long nextCur = std::min(cur * 10 - 1, n);
+            output += (nextCur - cur + 1) * commas;
+            if (++step % 3 == 0) ++commas;
+            cur = nextCur + 1;
+        }
+        return output;
+    }
+};
+
+// Python O(log10(N)) O(1) Math
+class Solution:
+    def countCommas(self, n: int) -> int:
+        output: int = 0
+        cur: int = 1_000
+        commas: int = 1
+        step: int = 0
+        while cur <= n:
+            next_cur: int = min(cur * 10 - 1, n)
+            output += (next_cur - cur + 1) * commas
+            step += 1
+            if step % 3 == 0: commas += 1
+            cur = next_cur + 1
+        return output
