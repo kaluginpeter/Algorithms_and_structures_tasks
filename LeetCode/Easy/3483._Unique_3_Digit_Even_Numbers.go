1087da589d4969809b86aea6c4f7801b2a4ac33a
kaluginpeter <streetworkout200412@gmail.com>
2026-09-11 15:35:46 +0300

    Added solution "3483. Unique 3-Digit Even Numbers"

--- LeetCode/Easy/3483._Unique_3_Digit_Even_Numbers.go
+++ LeetCode/Easy/3483._Unique_3_Digit_Even_Numbers.go
@@ -44,3 +44,29 @@ Constraints:
 3 <= digits.length <= 10
 0 <= digits[i] <= 9
 */
+// Solution
+// Go O(N + D^2) O(D) HashMap Math
+func totalNumbers(digits []int) int {
+	var freq [10]int = [10]int{}
+	for _, d := range digits { freq[d]++ }
+	var overall int = 0
+	for d := 0; d <= 8; d += 2 {
+		if freq[d] == 0 { continue }
+		freq[d]--
+		var distinct, duplicates int = 0, 0
+		for digit := 0; digit < 10; digit++ {
+			if freq[digit] > 0 {
+				distinct++
+				if freq[digit] >= 2 { duplicates++ }
+			}
+		}
+		overall += distinct * (distinct - 1) + duplicates
+		if freq[0] > 0 {
+			var invalid int = distinct - 1
+			if freq[0] >= 2 { invalid++ }
+			overall -= invalid
+		}
+		freq[d]++
+	}
+	return overall
+}
