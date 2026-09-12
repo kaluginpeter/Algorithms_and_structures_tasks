bc1d207a5c65bffec5947a073403a67a9551fb6e
kaluginpeter <streetworkout200412@gmail.com>
2026-09-12 19:44:56 +0300

    Added solution "GCD sum"

--- CodeWars/7kyu/GCD_sum.go
+++ CodeWars/7kyu/GCD_sum.go
@@ -15,3 +15,21 @@ Good luck!
 
 Fundamentals
 */
+// Solution
+package kata
+
+func GCD(a, b int) int {
+	for b != 0 {
+		a, b = b, a % b
+	}
+	return a
+}
+
+func Solve(s int, g int) []int {
+	for i := 1; i <= s; i++ {
+    if GCD(i, s - i) == g {
+      return []int{min(i, s - i), max(i, s - i)}
+    }
+  }
+  return []int{-1, -1}
+}
