ef9a78636c3e2b67f5cb9c1c2b7042bd6ec7edbc
kaluginpeter <streetworkout200412@gmail.com>
2026-09-13 18:52:10 +0300

    Added solution "Highest and Lowest"

--- CodeWars/7kyu/Highest_and_Lowest.go
+++ CodeWars/7kyu/Highest_and_Lowest.go
@@ -11,3 +11,33 @@ There will always be at least one number in the input string.
 Output string must be two numbers separated by a single space, and highest number is first.
 FundamentalsStrings
 */
+// Solution
+package kata
+
+import "strconv"
+
+func HighAndLow(in string) string {
+  var mn, mx, acc int = 1e5, -1e5, 0
+  var isNeg bool = false;
+  for _, ch := range in {
+    if ch == ' ' {
+      if isNeg {
+        acc *= -1
+      }
+      mn = min(mn, acc)
+      mx = max(mx, acc)
+      acc = 0
+      isNeg = false
+    } else if ch == '-' {
+      isNeg = true
+    } else {
+      acc = acc * 10 + int(ch - '0')
+    }
+  }
+  if isNeg {
+    acc *= -1
+  }
+  mn = min(mn, acc)
+  mx = max(mx, acc)
+  return strconv.Itoa(mx) + " " + strconv.Itoa(mn)
+}
