f2fc6eb9dc10005873becaadfd72f56aef18d0eb
kaluginpeter <streetworkout200412@gmail.com>
2026-09-17 18:50:36 +0300

    Added solution "1477. Find Two Non-overlapping Sub-arrays Each With Target Sum"

--- LeetCode/Medium/1477._Find_Two_Non_overlapping_Sub_arrays_Each_With_Target_Sum.go
+++ LeetCode/Medium/1477._Find_Two_Non_overlapping_Sub_arrays_Each_With_Target_Sum.go
@@ -30,3 +30,68 @@ Constraints:
 1 <= arr[i] <= 1000
 1 <= target <= 108
 */
+// Solution
+// Go O(NlogN) O(N) BinarySearch Prefix HashMap
+func minSumOfLengths(arr []int, target int) int {
+    var hashmap map[int]int = map[int]int{}
+    hashmap[0] = -1
+    var prefix [][2]int = [][2]int{}
+    var n int = len(arr)
+    var acc, output int = 0, n + 1
+    for i := 0; i < n; i++ {
+        acc += arr[i]
+        // Find the smallest valid subarray from the left
+        start, was := hashmap[acc - target]; if was {
+            var left, right int = 0, len(prefix) - 1
+            for left <= right {
+                var middle int = left + ((right - left) >> 1)
+                if prefix[middle][0] > start {
+                    right = middle - 1
+                } else { left = middle + 1 }
+            }
+            if right >= 0 {
+                output = min(output, (i - start) + prefix[right][1])
+            }
+            // Compress the previous subarrays
+            var minSubArrayLength int = i - start
+            if len(prefix) == 0 || prefix[len(prefix) - 1][1] > minSubArrayLength {
+                prefix = append(prefix, [2]int{i, minSubArrayLength})
+            }
+        }
+        hashmap[acc] = i
+    }
+    if output == n + 1 { return -1 }
+    return output
+}
+
+// C++ O(NlogN) O(N) Prefix HashMap BinarySearch
+class Solution {
+public:
+    int minSumOfLengths(vector<int>& arr, int target) {
+        std::unordered_map<int, int> hashmap;
+        hashmap[0] = -1;
+        std::vector<std::pair<int, int>> prefix;
+        size_t n = arr.size();
+        int acc = 0, output = n + 1;
+        for (int i = 0; i < n; ++i) {
+            acc += arr[i];
+            if (hashmap.count(acc - target)) {
+                // Find the smallest valid subarray from the left
+                int start = hashmap[acc - target];
+                int left = 0, right = (int)prefix.size() - 1;
+                while (left <= right) {
+                    int middle = left + ((right - left) >> 1);
+                    if (prefix[middle].first > start) right = middle - 1;
+                    else left = middle + 1;
+                }
+                if (right >= 0) output = std::min(output, (i - start) + prefix[right].second);
+                // Compress the previous subarray
+                if (prefix.empty() || prefix.back().second > (i - start)) {
+                    prefix.push_back({i, i - start});
+                }
+            }
+            hashmap[acc] = i;
+        }
+        return (output == n + 1 ? -1 : output);
+    }
+};
