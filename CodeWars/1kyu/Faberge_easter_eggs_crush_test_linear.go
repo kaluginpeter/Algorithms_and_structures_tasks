a9ffc6dba6b11c009838469dfd228f684b85a4cb
kaluginpeter <streetworkout200412@gmail.com>
2026-09-18 18:02:22 +0300

    Added solution "Faberge easter eggs crush test [linear]"

--- CodeWars/1kyu/Faberge_easter_eggs_crush_test_linear.go
+++ CodeWars/1kyu/Faberge_easter_eggs_crush_test_linear.go
@@ -14,3 +14,34 @@ There are 150 random tests. You will need more than just a naive linear algorith
 
 PerformanceAlgorithmsMathematics
 */
+// Solution
+package kata
+
+import "math/big"
+
+const MOD int64 = 998244353
+
+func normalizeMod(x int64) int64 {
+	x %= MOD
+	if x < 0 { x += MOD }
+	return x
+}
+
+func Height(n, m *big.Int) int64 {
+	nInt := int(n.Int64())
+	mReduced := new(big.Int).Mod(m, big.NewInt(MOD))
+	mVal := mReduced.Int64()
+	inv := make([]int64, nInt+1)
+	var prev, output int64 = 1, 0
+	for i := 1; i <= nInt; i++ {
+		ii := int64(i)
+		if i > 1 {
+			inv[i] = normalizeMod(-(MOD/ii)*inv[MOD%ii])
+		} else { inv[i] = 1 }
+		term := normalizeMod(mVal - ii + 1)
+		prev = normalizeMod(prev * term)
+		prev = normalizeMod(prev * inv[i])
+		output = normalizeMod(output + prev)
+	}
+	return output
+}
